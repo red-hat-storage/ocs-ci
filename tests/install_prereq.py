82d58826441ba0d59f26d2e5426d3f544fc8daae
Sidhant Agrawal <sagrawal@redhat.com>
2018-02-23 16:15:42 +0530

    Install lsb-release package for ubuntu Signed-off-by: Sidhant Agrawal <sagrawal@redhat.com>

--- tests/install_prereq.py
+++ tests/install_prereq.py
@@ -8,7 +8,7 @@ from ceph.utils import update_ca_cert
 log = logging.getLogger(__name__)
 # rpm_pkgs = ['wget', 'git', 'epel-release', 'redhat-lsb', 'python-virtualenv', 'python-nose']
 rpm_pkgs = ['wget', 'git', 'python-virtualenv', 'python-nose']
-deb_pkgs = ['wget', 'git', 'python-virtualenv']
+deb_pkgs = ['wget', 'git', 'python-virtualenv', 'lsb-release']
 epel_rpm = 'https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm'
 epel_pkgs = ['python-pip']
 deb_all_pkgs = " ".join(deb_pkgs)
