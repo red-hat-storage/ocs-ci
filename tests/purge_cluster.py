ec5ecd178134f1ae4534e587f56b649c6d93107b
Coady LaCroix <clacroix@redhat.com>
2018-03-01 16:08:06 -0800

    purge_cluster test steps are now using the correct script and executing purge playbook from correct location
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- tests/purge_cluster.py
+++ tests/purge_cluster.py
@@ -3,13 +3,16 @@ from time import sleep
 
 from ceph.parallel import parallel
 
-logger = logging.getLogger(__name__)
-log = logger
+log = logging.getLogger(__name__)
 
 
 def run(**kw):
     log.info("Running test")
     ceph_nodes = kw.get('ceph_nodes')
+    iscsi_clients_node = None
+    installer_node = None
+    ansible_dir = '/usr/share/ceph-ansible'
+
     for ceph in ceph_nodes:
         if ceph.role == "installer":
             installer_node = ceph
@@ -18,19 +21,23 @@ def run(**kw):
         if ceph.role == "iscsi-clients":
             iscsi_clients_node = ceph
             break
+
     log.info("Purge Ceph cluster")
-    iscsi_clients_node.exec_command(cmd='sudo yum remove -y iscsi-initiator-utils')
-    iscsi_clients_node.exec_command(cmd='sudo yum remove -y device-mapper-multipath')
-    installer_node.exec_command(
-        cmd="cp ~/ceph-ansible/infrastructure-playbooks/purge-cluster.yml ~/ceph-ansible/")
+
+    if iscsi_clients_node:
+        iscsi_clients_node.exec_command(sudo=True, cmd='yum remove -y iscsi-initiator-utils')
+        iscsi_clients_node.exec_command(sudo=True, cmd='yum remove -y device-mapper-multipath')
+    installer_node.exec_command(sudo=True,
+                                cmd='cd {ansible_dir}; cp {ansible_dir}/infrastructure-playbooks/purge-cluster.yml .'
+                                .format(ansible_dir=ansible_dir))
     out, err = installer_node.exec_command(
-        cmd="cd ~/ceph-ansible ; ansible-playbook -i hosts purge-cluster.yml --extra-vars 'ireallymeanit=yes'",
+        cmd="cd {ansible_dir} ; ansible-playbook -i hosts purge-cluster.yml --extra-vars 'ireallymeanit=yes'"
+            .format(ansible_dir=ansible_dir),
         long_running=True)
-    output = err
-    output = output
-    if output == 0:
-        log.info("ansible-playbook purge cluster successfull")
-        installer_node.exec_command(sudo=True, cmd="rm -rf ~/ceph-ansible")
+
+    if err == 0:
+        log.info("ansible-playbook purge cluster successful")
+        installer_node.exec_command(sudo=True, cmd="rm -rf {ansible_dir}".format(ansible_dir=ansible_dir))
         with parallel() as p:
             for cnode in ceph_nodes:
                 if cnode.role != "installer":
@@ -42,6 +49,6 @@ def run(**kw):
 
 
 def reboot_node(client):
-    client.exec_command(cmd='sudo reboot', check_ec=False)
+    client.exec_command(sudo=True, cmd='reboot', check_ec=False)
     sleep(300)
     client.reconnect()
