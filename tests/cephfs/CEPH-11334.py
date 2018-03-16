c3c61c642f03be53218edb657371f6b2681af988
Shreekar Shastry <sshreeka@redhat.com>
2018-03-17 00:06:20 +0530

    Covers 11334

---
+++ tests/cephfs/CEPH-11334.py
@@ -0,0 +1,178 @@
+from tests.cephfs.cephfs_utils import FsUtils
+import timeit
+from ceph.ceph import CommandFailed
+import traceback
+import logging
+
+logger = logging.getLogger(__name__)
+log = logger
+
+
+def run(**kw):
+    try:
+        start = timeit.default_timer()
+        tc = '11334'
+        file_name = 'file'
+        log.info("Running cephfs %s test case" % (tc))
+        ceph_nodes = kw.get('ceph_nodes')
+        fs_util = FsUtils(ceph_nodes)
+        client_info, rc = fs_util.get_clients()
+        if rc == 0:
+            log.info("Got client info")
+        else:
+            raise CommandFailed("fetching client info failed")
+        client1 = []
+        client2 = []
+        client3 = []
+        client4 = []
+        client1.append(client_info['fuse_clients'][0])
+        client2.append(client_info['fuse_clients'][1])
+        client3.append(client_info['kernel_clients'][0])
+        client4.append(client_info['kernel_clients'][1])
+
+        rc1 = fs_util.auth_list(client1, client_info['mon_node'])
+        rc2 = fs_util.auth_list(client2, client_info['mon_node'])
+        rc3 = fs_util.auth_list(client3, client_info['mon_node'])
+        rc4 = fs_util.auth_list(client4, client_info['mon_node'])
+        print rc1, rc2, rc3, rc4
+        if rc1 == 0 and rc2 == 0 and rc3 == 0 and rc4 == 0:
+            log.info("got auth keys")
+        else:
+            raise CommandFailed("auth list failed")
+
+        rc1 = fs_util.fuse_mount(client1, client_info['mounting_dir'])
+        rc2 = fs_util.fuse_mount(client2, client_info['mounting_dir'])
+
+        if rc1 == 0 and rc2 == 0:
+            log.info("Fuse mount passed")
+        else:
+            raise CommandFailed("Fuse mount failed")
+
+        rc3 = fs_util.kernel_mount(
+            client3,
+            client_info['mounting_dir'],
+            client_info['mon_node_ip'])
+        rc4 = fs_util.kernel_mount(
+            client4,
+            client_info['mounting_dir'],
+            client_info['mon_node_ip'])
+        if rc3 == 0 and rc4 == 0:
+            log.info("kernel mount passed")
+        else:
+            raise CommandFailed("kernel mount failed")
+        vals, rc = fs_util.getfattr(
+            client1, client_info['mounting_dir'], file_name)
+
+        rc = fs_util.setfattr(
+            client1,
+            'stripe_unit',
+            '1048576',
+            client_info['mounting_dir'],
+            file_name)
+        if rc == 0:
+            log.info("Setfattr stripe_unit for file %s success" % file_name)
+        else:
+            raise CommandFailed(
+                "Setfattr stripe_unit for file %s success" %
+                file_name)
+        rc = fs_util.setfattr(
+            client1,
+            'stripe_count',
+            '8',
+            client_info['mounting_dir'],
+            file_name)
+        if rc == 0:
+            log.info("Setfattr stripe_count for file %s success" % file_name)
+        else:
+            raise CommandFailed(
+                "Setfattr stripe_count for file %s success" %
+                file_name)
+        rc = fs_util.setfattr(
+            client1,
+            'object_size',
+            '10485760',
+            client_info['mounting_dir'],
+            file_name)
+        if rc == 0:
+            log.info("Setfattr object_size for file %s success" % file_name)
+        else:
+            raise CommandFailed(
+                "Setfattr object_size for file %s success" %
+                file_name)
+        rc = fs_util.add_pool(
+            client_info['mon_node'],
+            'cephfs',
+            'new_data_pool')
+        if rc == 0:
+            log.info("Adding new pool to cephfs success")
+        else:
+            raise CommandFailed("Adding new pool to cephfs failed")
+        rc = fs_util.setfattr(
+            client1,
+            'pool',
+            'new_data_pool',
+            client_info['mounting_dir'],
+            file_name)
+        if rc == 0:
+            log.info("Setfattr pool for file %s success" % file_name)
+        else:
+            raise CommandFailed(
+                "Setfattr pool for file %s success" %
+                file_name)
+
+        vals, rc = fs_util.getfattr(
+            client1, client_info['mounting_dir'], file_name)
+        log.info("Read individual layout fields by using getfattr:")
+        for client in client1:
+            out, rc = client.exec_command(
+                cmd="sudo getfattr -n ceph.file.layout.pool %s%s" %
+                (client_info['mounting_dir'], file_name))
+            if vals['pool'] in out.read():
+                log.info("reading pool by getfattr successfull")
+            out, rc = client.exec_command(
+                cmd="sudo getfattr -n ceph.file.layout.stripe_unit  %s%s" %
+                (client_info['mounting_dir'], file_name))
+            if vals['stripe_unit'] in out.read():
+                log.info("reading stripe_unit by getfattr successfull")
+            out, rc = client.exec_command(
+                cmd="sudo getfattr -n ceph.file.layout.stripe_count  %s%s" %
+                (client_info['mounting_dir'], file_name))
+            if vals['stripe_count'] in out.read():
+                log.info("reading stripe_count by getfattr successfull")
+            out, rc = client.exec_command(
+                cmd="sudo getfattr -n ceph.file.layout.object_size  %s%s" %
+                (client_info['mounting_dir'], file_name))
+            if vals['object_size'] in out.read():
+                log.info("reading object_size by getfattr successfull")
+            break
+        log.info("Cleaning up!-----")
+        rc = fs_util.client_clean_up(
+            client_info['fuse_clients'],
+            client_info['kernel_clients'],
+            client_info['mounting_dir'],
+            'umount')
+        if rc == 0:
+            log.info("Client Cleaning up successfull")
+        else:
+            raise CommandFailed("Cleaning up failed")
+        rc = fs_util.mds_cleanup(client_info['mds_nodes'], None)
+        if rc == 0:
+            log.info("MDS clean up successfull")
+        else:
+            raise CommandFailed("MDS clean up failed")
+
+        print'Script execution time:------'
+        stop = timeit.default_timer()
+        total_time = stop - start
+        mins, secs = divmod(total_time, 60)
+        hours, mins = divmod(mins, 60)
+        print ("Hours:%d Minutes:%d Seconds:%f" % (hours, mins, secs))
+        return 0
+    except CommandFailed as e:
+        log.info(e)
+        log.info(traceback.format_exc())
+        return 1
+    except Exception as e:
+        log.info(e)
+        log.info(traceback.format_exc())
+        return 1
