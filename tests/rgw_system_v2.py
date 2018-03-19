b27b7fceaaa9adf09702aece558b9fec9a001e39
rakesh <rakesh@localhost.localdomain>
2018-03-19 18:52:44 +0530

    rgw system test with boto3 added

---
+++ tests/rgw_system_v2.py
@@ -0,0 +1,33 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def run(**kw):
+    log.info("Running test")
+    ceph_nodes = kw.get('ceph_nodes')
+    log.info("Running rgw tests v2")
+    rgw_client_nodes = []
+    for node in ceph_nodes:
+        if node.role == 'rgw':
+            rgw_client_nodes.append(node)
+    git_url = 'http://gitlab.cee.redhat.com/ceph/ceph-qe-scripts.git'
+    branch = ' -b wip-with-boto3'
+    git_clone = 'git clone ' + git_url + branch
+    rgw_node = rgw_client_nodes[0]
+    # cleanup any existing stale test dir
+    test_folder = 'rgw-tests'
+    rgw_node.exec_command(cmd='rm -rf ' + test_folder)
+    rgw_node.exec_command(cmd='mkdir ' + test_folder)
+    rgw_node.exec_command(cmd='cd ' + test_folder + ' ; ' + git_clone)
+    rgw_node.exec_command(cmd='sudo pip install boto3 names PyYaml ConfigParser')
+    config = kw.get('config')
+    script_name = config.get('script-name')
+    config_file_name = config.get('config-file-name')
+    timeout = config.get('timeout', 300)
+    out, err = rgw_node.exec_command(
+        cmd='sudo python ~/' + test_folder + '/ceph-qe-scripts/rgw/v2/tests/s3/' + script_name + ' -c ' + test_folder +
+            '/ceph-qe-scripts/rgw/v2/tests/s3/configs/' + config_file_name,
+        timeout=timeout)
+    log.info(out.read())
+    return 0
