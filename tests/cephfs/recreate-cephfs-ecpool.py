368424693a47d9312995d1ea0c592f8583fe9a57
sshreeka <sshreeka@redhat.com>
2018-09-10 15:57:45 +0530

    modified:   tests/cephfs/recreate-cephfs-ecpool.py: Added if condition, needed for filestore.

--- tests/cephfs/recreate-cephfs-ecpool.py
+++ tests/cephfs/recreate-cephfs-ecpool.py
@@ -14,12 +14,13 @@ def run(**kw):
     config = kw.get('config')
     bluestore = config.get('bluestore')
     k_and_m = config.get('ec-pool-k-m')
-    if bluestore is not None and k_and_m is None:
+    if (bluestore is not None and k_and_m is None) or (bluestore is None and k_and_m is None):
         log.info('tests will run on replicated pool')
         return 0
     elif bluestore is None and k_and_m is not None:
         log.error('Filestore does not support ecpools')
         return 1
+
     fs_info = fs_util.get_fs_info(client_info['mon_node'][0])
     fs_util.del_cephfs(
         client_info['mds_nodes'], fs_info.get('fs_name'))
