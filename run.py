287fe5e6a31cfeb62ce514f47317b833dfe1e283
vshevche <vshevche@redhat.com>
2018-11-14 19:55:31 +0200

    Improve cluster serialization
    - added store_cluster_state
    - added cluster serialization after each test module if --store set
    
    Signed-off-by: vshevche <vshevche@redhat.com>

--- run.py
+++ run.py
@@ -396,10 +396,7 @@ def run(args):
         ceph_clusters_file = 'rerun/ceph-snapshot-' + timestamp()
         if not os.path.exists(os.path.dirname(ceph_clusters_file)):
             os.makedirs(os.path.dirname(ceph_clusters_file))
-        cn = open(ceph_clusters_file, 'w+b')
-        pickle.dump(ceph_cluster_dict, cn)
-        cn.close()
-        log.info("ceph_clusters_file %s", ceph_clusters_file)
+        store_cluster_state(ceph_cluster_dict, ceph_clusters_file)
 
     sys.path.append(os.path.abspath('tests'))
     sys.path.append(os.path.abspath('tests/rados'))
@@ -524,6 +521,9 @@ def run(args):
                     service.log(time=timestamp(), message=traceback.format_exc(), level="ERROR")
                 log.error(traceback.format_exc())
                 rc = 1
+            finally:
+                if store:
+                    store_cluster_state(ceph_cluster_dict, ceph_clusters_file)
             if rc != 0:
                 break
         elapsed = (time.time() - start)
@@ -565,6 +565,13 @@ def run(args):
     print("\nAll test logs located here: {base}/{dir}".format(base=url_base, dir=run_dir_name))
     print_results(tcs)
     return jenkins_rc
+
+
+def store_cluster_state(ceph_cluster_object, ceph_clusters_file_name):
+    cn = open(ceph_clusters_file_name, 'w+b')
+    pickle.dump(ceph_cluster_object, cn)
+    cn.close()
+    log.info("ceph_clusters_file %s", ceph_clusters_file_name)
 
 
 if __name__ == '__main__':
