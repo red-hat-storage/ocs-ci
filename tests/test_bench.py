502929b1898befc50bb3bbf4c158293b7ad989ad
Shylesh Kumar <shmohan@redhat.com>
2019-04-16 16:34:35 -0700

    [ocs-ci]:Add radosbench task [ocs-ci]/radosbench: Addressed all the review comments for pull/4
    All the strings are now converted to 'f' strings
    Fixed all the paranthesis according to guidelines
    
    Signed-off-by: Shylesh Kumar <shmohan@redhat.com>

---
+++ tests/test_bench.py
@@ -0,0 +1,27 @@
+import os
+from kubernetes import client, config
+os.sys.path.append(os.path.dirname(os.getcwd()))
+
+from oc import pod, pod_exec
+from tests import test_radosbench as radosbench
+
+
+def run():
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    ret = v1.list_pod_for_all_namespaces(
+        watch=False,
+        label_selector='app=rook-ceph-tools'
+    )
+
+    for i in ret.items:
+        namespace = i.metadata.namespace
+        name = i.metadata.name
+        break
+
+    po = pod.Pod(name, namespace, roles=['client'])
+
+    return radosbench.run(
+        ceph_pods=[po],
+        config={'time': 10, 'cleanup': False}
+    )
