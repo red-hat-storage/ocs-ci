8013fe40774bf114e34269e6137ac4be75c540c3
Shylesh Kumar <shmohan@redhat.com>
2019-04-16 17:29:32 -0700

    [ocs-ci]: Hardening exec infrastrutre
    Currently exec api in kubernetes python client doesn't return
    err code for command execution. Hence relying on bash return
    values.
    
    Introducing long_running option for long running commands like
    rados bench etc. In order to figure out successful command or
    hung command we are relying on err code value.
    
    [ocs-ci]/pod-infra: Addressed all review comments for pull/4
    
    Introduced new 'f' strings for all the strings.
    Also followed coding guidelines for paranthesis
    
    Signed-off-by: Shylesh Kumar <shmohan@redhat.com>

--- oc/pod.py
+++ oc/pod.py
@@ -2,13 +2,14 @@
 
 Each pod in the openshift cluster will have a corresponding pod object.
 Few assumptions:
-    oc cluseter is up and running
+    oc cluster is up and running
 
 """
 
 import logging
 
 from .pod_exec import Exec, CmdObj
+from .Exceptions import CommandFailed
 
 logger = logging.getLogger(__name__)
 
@@ -16,12 +17,12 @@ logger = logging.getLogger(__name__)
 class Pod(object):
     """Handles per pod related context
 
-    Attributes:
-        name (str):      name of the pod in oc cluster
-        namespace(str):  openshift namespace where this pod lives
-        labels (list):      list of oc labels associated with pod
-        roles (list):       This could be oc roles like Master, etcd OR
-                            ceph roles like mon, osd etc
+        Attributes:
+            name (str):      name of the pod in oc cluster
+            namespace(str):  openshift namespace where this pod lives
+            labels (list):   list of oc labels associated with pod
+            roles (list):    This could be oc roles like Master, etcd OR
+                             ceph roles like mon, osd etc
 
 
     """
@@ -29,12 +30,12 @@ class Pod(object):
     def __init__(self, name=None, namespace=None, labels=None, roles=None):
         """Context detail per pod
 
-        Args:
-            name (string):      name of the pod in oc cluster
-            namespace (string): namespace in which pod lives
-            labels (list):      list of oc labels associated with pod
-            roles (list):       This could be oc roles like Master, etcd OR
-                                ceph roles like mon, osd etc
+            Args:
+                name (string):      name of the pod in oc cluster
+                namespace (string): namespace in which pod lives
+                labels (list):      list of oc labels associated with pod
+                roles (list):       This could be oc roles like Master, etcd OR
+                                   ceph roles like mon, osd etc
 
         """
         self._name = name
@@ -79,14 +80,24 @@ class Pod(object):
         timeout = kw.get('timeout', 60)
         wait = kw.get('wait', True)         # default synchronous execution
         check_ec = kw.get('check_ec', True)
+        long_running = kw.get('long_running', False)
 
-        cmd_obj = CmdObj(cmd, timeout, wait, check_ec)
+        cmd_obj = CmdObj(
+            cmd,
+            timeout,
+            wait,
+            check_ec,
+            long_running,
+        )
+
         runner = Exec()
-        stdout, stderr, err = runner.run(self.name,
-                                         self.namespace,
-                                         cmd_obj)
+        stdout, stderr, err = runner.run(
+            self.name,
+            self.namespace,
+            cmd_obj,
+        )
 
         if check_ec:
-            return (stdout, stderr, err)
+            return stdout, stderr, err
         else:
-            return (stdout, stderr)
+            return stdout, stderr
--- tests/test_pod_exec.py
+++ tests/test_pod_exec.py
@@ -8,21 +8,24 @@ from oc import pod, pod_exec
 def main():
     config.load_kube_config()
     v1 = client.CoreV1Api()
-    ret = v1.list_pod_for_all_namespaces(watch=False,
-                                        label_selector='app=rook-ceph-tools')
+    ret = v1.list_pod_for_all_namespaces(
+        watch=False,
+        label_selector='app=rook-ceph-tools'
+    )
+
     for i in ret.items:
         namespace = i.metadata.namespace
         name = i.metadata.name
         break
+
     cmd = "ceph osd df"
     po = pod.Pod(name, namespace)
-    out, err, ret = po.exec_command(cmd=cmd)
+
+    out, err, ret = po.exec_command(cmd=cmd, timeout=20)
     if out:
         print (out)
     if err:
         print (err)
     print (ret)
 
-if __name__ == "__main__":
-    main()
 
