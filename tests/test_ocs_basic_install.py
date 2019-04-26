60f0e8031f1831d7fff211c3f209f3dd00e45f79
Petr Balogh <pbalogh@redhat.com>
2019-04-26 09:23:09 +0200

    Coady's changes for ocs deployment

--- tests/test_ocs_basic_install.py
+++ tests/test_ocs_basic_install.py
@@ -4,12 +4,15 @@ import platform
 import random
 import subprocess
 import sys
+import time
 
 import ocs.defaults as default
 import requests
 import yaml
 from ocs.exceptions import UnsupportedOSType
 from jinja2 import Environment, FileSystemLoader
+from ocs.exceptions import CommandFailed
+
 from ocs.exceptions import CommandFailed
 
 log = logging.getLogger(__name__)
@@ -31,12 +34,12 @@ def run(**kwargs):
 
     # Generate install-config from template
     log.info("Generating install-config")
-    cluster_dir_parent = "/tmp"  # TODO: determine better place to create cluster directories, perhaps in project dir?
-    if test_data.get('cluster-name'):
-        cluster_name = test_data.get('cluster-name')
-    else:
-        cid = random.randint(10000, 99999)
-        cluster_name = f'ocs-ci-cluster-{cid}'
+    # TODO: determine better place to create cluster directories - (log dir?)
+    cluster_dir_parent = "/tmp"
+    base_name = test_data.get('cluster-name', 'ocs-ci-cluster')
+    cid = random.randint(10000, 99999)
+    cluster_name = f'{base_name}-{cid}'
+
     cluster_path = os.path.join(cluster_dir_parent, cluster_name)
     run_cmd(f"mkdir {cluster_path}")
 
@@ -80,7 +83,8 @@ def run(**kwargs):
     # Deploy cluster
     log.info("Deploying cluster")
     run_cmd(
-        f"./openshift-install create cluster --dir {cluster_path} "
+        f"./openshift-install create cluster "
+        f"--dir {cluster_path} "
         f"--log-level debug"
     )
 
@@ -90,12 +94,60 @@ def run(**kwargs):
     run_cmd("oc cluster-info")
 
     # TODO: Create cluster object, add to test_data for other tests to utilize
+    # TODO: Add volumes to worker nodes to support OSDs
     # TODO: Use Rook to install ceph on the cluster
+    # retrieve rook config from cluster_conf
+    rook_data = {}
+    if cluster_conf:
+        rook_data = cluster_conf.get('rook', {})
+
+    # render templates and create resources
+    create_rook_resource('common.yaml', rook_data, cluster_path)
+    run_cmd(
+        'oc label namespace openshift-storage '
+        '"openshift.io/cluster-monitoring=true"'
+    )
+    run_cmd(
+        "oc policy add-role-to-user view "
+        "system:serviceaccount:openshift-monitoring:prometheus-k8s "
+        "-n openshift-storage"
+    )
+    create_rook_resource('operator-openshift.yaml', rook_data, cluster_path)
+    wait_time = 120
+    log.info(f"Waiting {wait_time} seconds...")
+    time.sleep(wait_time)
+    run_cmd(
+        "oc wait --for condition=ready pod "
+        "-l app=rook-ceph-operator "
+        "-n openshift-storage "
+        "--timeout=120s"
+    )
+    run_cmd(
+        "oc wait --for condition=ready pod "
+        "-l app=rook-ceph-agent "
+        "-n openshift-storage "
+        "--timeout=120s"
+    )
+    run_cmd(
+        "oc wait --for condition=ready pod "
+        "-l app=rook-discover "
+        "-n openshift-storage "
+        "--timeout=120s"
+    )
+    create_rook_resource('cluster.yaml', rook_data, cluster_path)
+    create_rook_resource('toolbox.yaml', rook_data, cluster_path)
+    # TODO: need to split storage-manifest before passing to create?
+    # create_rook_resource('storage-manifest.yaml', rook_data, cluster_path)
+    # TODO: create service-monitor from template (need template)
+    # create_rook_resource("service-monitor.yaml", rook_data, cluster_path)
+    # TODO: create prometheus-rules from template (need template)
+    # create_rook_resource("prometheus-rules.yaml", rook_data, cluster_path)
 
     # Destroy cluster (if configured)
     destroy_cmd = (
-        f"./openshift-install destroy cluster --dir {cluster_path} --log-level"
-        f" debug"
+        f"./openshift-install destroy cluster "
+        f"--dir {cluster_path} "
+        f"--log-level debug"
     )
     if config.get("destroy-cluster"):
         log.info("Destroying cluster")
@@ -109,8 +161,9 @@ def run(**kwargs):
     else:
         log.info(f"Cluster directory is located here: {cluster_path}")
         log.info(
-            f"Skipping cluster destroy. To manually destroy the cluster "
-            f"execute the following cmd: {destroy_cmd}"
+            f"Skipping cluster destroy. "
+            f"To manually destroy the cluster execute the following cmd: "
+            f"{destroy_cmd}"
         )
 
     return 0
@@ -128,10 +181,15 @@ def run_cmd(cmd, **kwargs):
     """
     log.info(f"Executing command: {cmd}")
     r = subprocess.run(
-        cmd.split(), stdout=sys.stdout, stderr=sys.stderr, **kwargs
+        cmd.split(),
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        **kwargs
     )
     if r.returncode != 0:
-        raise CommandFailed(f"Error during execution of command: {cmd}")
+        raise CommandFailed(
+            f"Error during execution of command: {cmd}"
+        )
 
 
 def download_file(url, filename):
@@ -150,6 +208,20 @@ def download_file(url, filename):
     assert r.ok
 
 
+def to_nice_yaml(a, indent=2, *args, **kw):
+    """Make verbose, human readable yaml"""
+    # TODO: elaborate more in docstring on what this actually does
+    transformed = yaml.dump(
+        a,
+        Dumper=yaml.Dumper,
+        indent=indent,
+        allow_unicode=True,
+        default_flow_style=False,
+        **kw
+    )
+    return transformed
+
+
 def render_template(template_path, data):
     """
     Render a template with the given data.
@@ -165,6 +237,7 @@ def render_template(template_path, data):
         loader=FileSystemLoader(os.path.join(TOP_DIR, 'templates')),
         trim_blocks=True
     )
+    j2_env.filters['to_nice_yaml'] = to_nice_yaml
     j2_template = j2_env.get_template(template_path)
     return j2_template.render(**data)
 
@@ -181,3 +254,28 @@ def load_config_data(data_path):
     """
     with open(data_path, "r") as data_descriptor:
         return yaml.load(data_descriptor, Loader=yaml.FullLoader)
+
+
+def create_rook_resource(template_name, rook_data, cluster_path):
+    """"
+    Create a rook resource after rendering the specified template with
+    the rook data from cluster_conf.
+
+    Args:
+        template_name: name of the ocs-deployment config template.
+        rook_data: rook specific config from cluster_conf
+        cluster_path: path to cluster directory, where files will be written
+    """
+    base_name = template_name.split('.')[0]
+    template_path = os.path.join('ocs-deployment', template_name)
+    template = render_template(
+        template_path,
+        rook_data.get(base_name, {})
+    )
+    cfg_file = os.path.join(cluster_path, template_name)
+    with open(cfg_file, "w") as f:
+        f.write(template)
+    log.info(f"Creating rook resource from {template_name}")
+    # TODO: logging this just for testing purposes, change to run_cmd
+    log.info(f"oc create -f {cfg_file}")
+>>>>>>> 2993ae6... Coady's changes for ocs deployment
