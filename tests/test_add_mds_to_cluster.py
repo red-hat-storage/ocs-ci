18a4119df5a3269620ce69aaae7ea35253fe3362
Elad Ben Aharon <ebenahar@redhat.com>
2019-05-17 16:15:30 +0300

    WIP test modification change tp pytest example

--- tests/test_add_mds_to_cluster.py
+++ tests/test_add_mds_to_cluster.py
@@ -5,11 +5,10 @@ import logging
 import ocs.defaults as defaults
 import yaml
 import os
+import pytest
 
 from ocs import ocp
 from munch import munchify
-from ocs import exceptions
-from ocsci.enums import TestStatus
 from utility import utils, templating
 
 log = logging.getLogger(__name__)
@@ -23,70 +22,69 @@ OCP = ocp.OCP(
 )
 
 
-def create_ceph_fs(**kwargs):
+def verify_fs_exist(self):
+    """
+    Verifying if a ceph FS exist
+    """
+    assert OCP.get(self.fs_name), f"CephFS {self.fs_name} doesn't exist"
+    log.info(f"CephFS {self.fs_name} exist")
+
+
+@pytest.fixture()
+def create_ceph_fs(request):
     """
     Create a new Ceph File System
     """
+    fs_data = {}
+    request.node.cls.fs_name = 'my-cephfs1'
+    fs_data['fs_name'] = request.node.cls.fs_name
     file_y = templating.generate_yaml_from_jinja2_template_with_data(
-        PV_YAML, **kwargs
+        PV_YAML, **fs_data
     )
     with open(TEMP_YAML_FILE, 'w') as yaml_file:
         yaml.dump(file_y, yaml_file, default_flow_style=False)
     log.info(f"Creating a new Ceph FileSystem")
     assert OCP.create(yaml_file=TEMP_YAML_FILE)
-    return True
+    assert verify_fs_exist(request.node.cls.fs_name)
 
 
-def modify_fs(new_active_count):
-    """
-    Modifying a ceph FS yaml file
-    """
-    with open(TEMP_YAML_FILE, 'r') as yaml_file:
-        cephfs_obj = munchify(yaml.safe_load(yaml_file))
-    cephfs_obj.spec.metadataServer.activeCount = int(new_active_count)
-    with open(TEMP_YAML_FILE, 'w') as yaml_file:
-        yaml.dump(cephfs_obj.toDict(), yaml_file, default_flow_style=False)
-    log.info(f"Change the active_count to {new_active_count}")
-    return OCP.apply(yaml_file=TEMP_YAML_FILE)
-
-
-def delete_fs(fs_name):
+@pytest.fixture()
+def delete_fs(request):
     """
     Deleting a ceph FS
     """
     log.info(f"Deleting the file system")
     stat = OCP.delete(TEMP_YAML_FILE)
-    if CEPHFS_DELETED.format(cephfs_name=fs_name) in stat:
-        return True
-    return False
-
-
-def verify_fs_exist(fs_name):
-    """
-    Verifying if a ceph FS exist
-    """
-    try:
-        OCP.get(fs_name)
-    except exceptions.CommandFailed:
-        log.info(f"CephFS {fs_name} doesn't exist")
-        return False
-    log.info(f"CephFS {fs_name} exist")
-    return True
-
-
-def run(**kwargs):
-    """
-    A simple function to exercise a resource creation through api-client
-    """
-    fs_data = {}
-    fs_name = 'my-cephfs1'
-    fs_data['fs_name'] = fs_name
-    # fs_data['active_count'] = '1'
-    new_active_count = '3'
-    assert create_ceph_fs(**fs_data)
-    assert verify_fs_exist(fs_name)
-    assert modify_fs(new_active_count)
-    assert delete_fs(fs_name)
-    assert not verify_fs_exist(fs_name)
+    assert not CEPHFS_DELETED.format(cephfs_name=request.node.cls.fs_name) in stat
+    assert not verify_fs_exist(request.node.cls.fs_name)
     utils.delete_file(TEMP_YAML_FILE)
-    return TestStatus.PASSED
+
+
+@pytest.mark.usefixtures(
+    create_ceph_fs.__name__,
+    delete_fs.__name__
+)
+class TestModifyFS:
+    """
+    TBD
+    """
+    new_active_count = '3'
+
+    def verify_fs_exist(self):
+        """
+        Verifying if a ceph FS exist
+        """
+        assert OCP.get(self.fs_name), f"CephFS {self.fs_name} doesn't exist"
+        log.info(f"CephFS {self.fs_name} exist")
+
+    def test_modify_fs(self):
+        """
+        Modifying a ceph FS yaml file
+        """
+        with open(TEMP_YAML_FILE, 'r') as yaml_file:
+            cephfs_obj = munchify(yaml.safe_load(yaml_file))
+        cephfs_obj.spec.metadataServer.activeCount = int(new_active_count)
+        with open(TEMP_YAML_FILE, 'w') as yaml_file:
+            yaml.dump(cephfs_obj.toDict(), yaml_file, default_flow_style=False)
+        log.info(f"Change the active_count to {new_active_count}")
+        assert OCP.apply(yaml_file=TEMP_YAML_FILE)
