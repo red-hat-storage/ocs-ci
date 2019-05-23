01ba11d77819fef026b8cfd949628aba7e8da969
Shrivaibavi Raghaventhiran <sraghave@redhat.com>
2019-05-23 16:13:57 +0530

    Modified it as a function for future purposes
    Signed-off-by: Shrivaibavi Raghaventhiran <sraghave@redhat.com>

--- tests/test_pod_exec.py
+++ tests/test_pod_exec.py
@@ -1,24 +1,39 @@
+"""
+This module contains the function to run command on any pod.
+By default it runs on rook-ceph-tool box pod 
+and the default the command is "ceph osd df"
+"""
+
 import os
 from kubernetes import client, config
 os.sys.path.append(os.path.dirname(os.getcwd()))
-
 from ocs import pod
 
+def run_command_on_pod(cmd, label_selector='app=rook-ceph-tools'):
+    """
+    This function provides the ability to execute commands on any pod,
+    by default the command runs on tool-box pod
 
-def main():
+    Args:
+        cmd (str): The command to execute on the pod
+        label_selector (str): The label of the pod
+                        By default the label will be "app=rook-ceph-tools"
+
+    Returns:
+        ret (int): The return value
+        out (str): If output exists
+        err (str): if any errors
+    """
+
     config.load_kube_config()
     v1 = client.CoreV1Api()
-    ret = v1.list_pod_for_all_namespaces(
-        watch=False,
-        label_selector='app=rook-ceph-tools'
-    )
+    ret = v1.list_pod_for_all_namespaces(watch=False, label_selector=label_selector)
 
     for i in ret.items:
         namespace = i.metadata.namespace
         name = i.metadata.name
         break
 
-    cmd = "ceph osd df"
     po = pod.Pod(name, namespace)
 
     out, err, ret = po.exec_command(cmd=cmd, timeout=20)
@@ -27,3 +42,8 @@ def main():
     if err:
         print(err)
     print(ret)
+
+def run(**kwargs):
+
+  output = run_command_on_pod(cmd='ceph osd df')
+  return 0
