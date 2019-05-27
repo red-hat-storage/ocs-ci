2c0a984123da6d0548ce627355df8557e061f56e
Martin Bukatovič <mbukatov@redhat.com>
2019-05-27 18:30:32 +0200

    adding some simple unit tests

--- unittests/test_pytest.py
+++ unittests/test_pytest.py
@@ -1,7 +1,27 @@
+# -*- coding: utf-8 -*-
+
 import logging
+
+import pytest
+
 
 logger = logging.getLogger(__name__)
 
 
 def test_pytest_works():
     logger.info("This is prove that pytest works")
+
+
+def test_help_message(testdir):
+    """
+    Check that ``py.test --help`` output lists custom options of
+    ocscilib pytest plugin.
+    """
+    result = testdir.runpytest('--help')
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*--ocsci-conf*',
+        '*--cluster-conf*',
+        '*--cluster-path*',
+        '*--cluster-name*',
+        ])
