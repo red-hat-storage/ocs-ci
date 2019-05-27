4d71e89dc9d858598a50dc548f544b11618df7dd
Martin Bukatovič <mbukatov@redhat.com>
2019-05-27 19:50:13 +0200

    unittests: test of config parametrization

--- unittests/test_pytest.py
+++ unittests/test_pytest.py
@@ -43,3 +43,36 @@ def test_simple(testdir):
     result.stdout.fnmatch_lines(['*::test_foo PASSED*'])
     # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0
+
+
+def test_config_parametrize(testdir):
+    """
+    Parametrization via config values, use case described in
+    https://github.com/red-hat-storage/ocs-ci/pull/61#issuecomment-494866745
+    """
+    # create a temporary pytest test module
+    testdir.makepyfile(textwrap.dedent("""\
+        import pytest
+
+        from ocsci import config as ocsci_config
+
+        @pytest.mark.parametrize("item", ocsci_config.DEMO)
+        def test_demo_parametrized_config(item):
+            assert item is not None
+        """))
+    # create config file
+    conf_file = testdir.makefile(".yaml", textwrap.dedent("""\
+        DEMO:
+         - 1
+         - 2
+        """))
+    # run pytest with the following cmd args
+    result = testdir.runpytest('-v', f'--ocsci-conf={conf_file}')
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        'collecting*collected 2 items',
+        '*test_demo_parametrized_config?1? PASSED*',
+        '*test_demo_parametrized_config?2? PASSED*',
+        ])
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
