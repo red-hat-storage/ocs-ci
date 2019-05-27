ca2743ffb301ad67bc05f90bfc3789b237139d9f
fbalak <fbalak@redhat.com>
2019-05-27 13:28:21 +0200

    add docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 TEMPLATE_DIR = os.path.join('templates', 'ocs-deployment')
 SC_CEPHFS_TEMPLATE = os.path.join(TEMPLATE_DIR, 'storageclass.cephfs.yaml')
 
+
 @pytest.fixture(
     params=[{
         'storageclass_name': 'invalid-storageclass',
@@ -28,22 +29,34 @@ SC_CEPHFS_TEMPLATE = os.path.join(TEMPLATE_DIR, 'storageclass.cephfs.yaml')
         'node_stage_secret_namespace': 'invalid_node_stage_secret_namespace',
         'mounter': 'invalid_mounter',
         'reclaim_policy': 'Delete'
-        }]
+        }]  # TODO: add more test case parameters
     )
 def invalid_cephfs_storageclass(tmpdir, request):
+    """
+    Creates StorageClass with CephFS filesystem that have invalid parameters.
+    Storageclass is removed at the end of test.
+
+    Returns:
+        str: Name of created StorageClass
+    """
     storageclass = ocp.OCP(
         kind='storageclass',
         namespace=defaults.ROOK_CLUSTER_NAMESPACE
     )
-    logger.info(f"SETUP - creating storageclass {request.param['storageclass_name']}")
+    logger.info(
+        f"SETUP - creating storageclass "
+        f"{request.param['storageclass_name']}")
     sc_yaml_content = templating.generate_yaml_from_jinja2_template_with_data(
         SC_CEPHFS_TEMPLATE,
-        **request.param # storageclass template parameters
+        **request.param  # storageclass template parameters
     )
-    temp_sc_file= tmpdir.join('storageclass.cephfs.yaml')
+    temp_sc_file = tmpdir.join('storageclass.cephfs.yaml')
     temp_sc_file.write(yaml.dump(sc_yaml_content))
     storageclass.create(yaml_file=temp_sc_file)
+
     yield request.param['storageclass_name']
 
-    logger.info(f"TEARDOWN - removing storageclass {request.param['storageclass_name']}")
+    logger.info(
+            f"TEARDOWN - removing storageclass "
+            f"{request.param['storageclass_name']}")
     storageclass.delete(yaml_file=temp_sc_file)
--- tests/test_storageclass_invalid.py
+++ tests/test_storageclass_invalid.py
@@ -15,8 +15,13 @@ logger = logging.getLogger(__name__)
 TEMPLATE_DIR = 'templates/ocs-deployment'
 PVC_TEMPLATE = os.path.join(TEMPLATE_DIR, 'PersistentVolumeClaim.yaml')
 
+
 @tier1
 def test_storageclass_cephfs_invalid(invalid_cephfs_storageclass, tmpdir):
+    """
+    Test that Persistent Volume Claim can not be created from misconfigured
+    CephFS Storage Class.
+    """
     pvc = ocp.OCP(
         kind='pvc',
         namespace=defaults.ROOK_CLUSTER_NAMESPACE
@@ -27,7 +32,7 @@ def test_storageclass_cephfs_invalid(invalid_cephfs_storageclass, tmpdir):
         pvc_name=pvc_name,
         storageclass_name=invalid_cephfs_storageclass
     )
-    temp_pvc_file= tmpdir.join('pvc.yaml')
+    temp_pvc_file = tmpdir.join('pvc.yaml')
     temp_pvc_file.write(
         yaml.dump(pvc_yaml_content))
     pvc.create(yaml_file=temp_pvc_file)
