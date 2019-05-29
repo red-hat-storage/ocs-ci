7472b385f0a1e15e22222d2a7afba06aa645cc31
Elad Ben Aharon <ebenahar@redhat.com>
2019-05-29 14:47:48 +0300

    Function for unique naming (#116)
    This function will return a unique name for the given resource

---
+++ tests/helpers.py
@@ -0,0 +1,23 @@
+"""
+Helper functions file for OCS QE
+"""
+import datetime
+
+
+def create_unique_resource_name(resource_description, resource_type):
+    """
+    Creates a unique object name by using the object_description
+    and object_type, as well as the current date/time string.
+
+    Args:
+        resource_description (str): The user provided object description
+        resource_type (str): The type of object for which the unique name
+            will be created. For example: project, pvc, etc
+
+    Returns:
+        str: A unique name
+    """
+    current_date_time = (
+        datetime.datetime.now().strftime("%d%H%M%S%f")
+    )
+    return f"{resource_type}_{resource_description[:23]}_{current_date_time[:10]}"
