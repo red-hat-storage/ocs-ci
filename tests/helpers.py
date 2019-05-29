13172a834e94242429e72bc800c36695acafa23a
Elad Ben Aharon <ebenahar@redhat.com>
2019-05-29 13:06:41 +0300

    fix Raz and Prasad comments

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,14 +4,14 @@ Helper functions file for OCS QE
 import datetime
 
 
-def create_unique_object_name(object_description, object_type):
+def create_unique_object_name(resource_description, resource_type):
     """
     Creates a unique object name by using the object_description
     and object_type, as well as the current date/time string.
 
     Args:
-        object_description (str): The user provided object description
-        object_type (str): The type of object for which the unique name
+        resource_description (str): The user provided object description
+        resource_type (str): The type of object for which the unique name
             will be created. For example: project, pvc, etc
 
     Returns:
@@ -20,4 +20,4 @@ def create_unique_object_name(object_description, object_type):
     current_date_time = (
         datetime.datetime.now().strftime("%d%H%M%S%f")
     )
-    return f"{object_type}_{object_description[:23]}_{current_date_time[:10]}"
+    return f"{resource_type}_{resource_description[:23]}_{current_date_time[:10]}"
