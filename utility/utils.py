568b182e33069b4f29b7f01b222003e475c0dd27
Prasad Desala <tdesala@redhat.com>
2019-05-30 17:55:41 +0530

    Update utils.py
    Fixed tox issues.

--- utility/utils.py
+++ utility/utils.py
@@ -634,7 +634,7 @@ def run_cmd(cmd, **kwargs):
         CommandFailed: In case the command execution fails
 
     Returns:
-        tuple (str, str, int): Decoded stdout, decoded stderr, and 
+        tuple (str, str, int): Decoded stdout, decoded stderr, and
                                return code of command
 
     """
