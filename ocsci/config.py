4c305ecdee103278dfa7046bf489746f80f2bb0b
Petr Balogh <pbalogh@redhat.com>
2019-06-01 23:05:20 +0200

    Remove warning of pytest for rewriting modules
    Cause we are loading config and defaults from our run_ocsci.py wrapper
    we need to add PYTEST_DONT_REWRITE msg to dosctring of modules where we
    don't want to see warning.
    
    Documentation:
    https://docs.pytest.org/en/latest/reference.html#pytest-dont-rewrite-module-docstring
    
    Reference where I found about this workarround is mentioned here:
    https://github.com/pytest-dev/pytest-cov/issues/148#issuecomment-423423605

--- ocsci/config.py
+++ ocsci/config.py
@@ -1,12 +1,16 @@
-# Following config variables will be loaded by ocscilib pytest plugin.
-# See the conf/ocsci/default_config.yaml for default values.
-# The data are combined from multiple sources and then exposed to the following
-# variables.
-#
-# You can see the logic in ./pytest_customization/plugins/ocscilib.py.
+"""
+Following config variables will be loaded by ocscilib pytest plugin.
+See the conf/ocsci/default_config.yaml for default values.
+The data are combined from multiple sources and then exposed to the following
+variables.
 
-# Those have lowest priority and are overwritten and filled with data loaded
-# during the config phase.
+You can see the logic in ./pytest_customization/plugins/ocscilib.py.
+
+Those have lowest priority and are overwritten and filled with data loaded
+during the config phase.
+
+PYTEST_DONT_REWRITE - avoid pytest to rewrite, keep this msg here please!
+"""
 RUN = dict(cli_params={})  # this will be filled with CLI parameters data
 DEPLOYMENT = {}  # deployment related data
 REPORTING = {}  # reporting related data
