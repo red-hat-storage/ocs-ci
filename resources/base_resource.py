4acc3c010caa70b2e454e187f1ad9ff052a1b6fb
Elad Ben Aharon <ebenahar@redhat.com>
2019-06-05 18:32:03 +0300

    consolidation with resources change

--- resources/base_resource.py
+++ resources/base_resource.py
@@ -1,36 +1,39 @@
 """
-General OCP object
+General OCS object
 """
 import logging
+import yaml
+import tempfile
 from ocs.ocp import OCP
-
-from ocs import defaults
+from utility import utils
+from utility import templating
 
 log = logging.getLogger(__name__)
 
 
 class BaseOCSClass(object):
     """
-    Base OCSClass inherited by StorageClass, CephFilesystem, secrete,PVC etc
+    Base OCSClass
     """
 
-    def __init__(self, api_version=defaults.API_VERSION,
-                 kind='Service', namespace=None
-                 ):
+    def __init__(self, **kwargs):
         """
         Initializer function
 
         Args:
-            api_version (str): TBD
-            kind (str): TBD
-            namespace (str): The name of the namespace to use
+            kwargs (dict):
         """
-        self._api_version = api_version
-        self._kind = kind
-        self._namespace = namespace
+        self.data = kwargs
+        self._api_version = self.data.get('api_version')
+        self._kind = self.data.get('kind')
+        self._namespace = self.data.get('namespace')
+        self._name = self.data.get('metadata').get('name')
         self.ocp = OCP(
-            api_version=self.api_version, kind=self.kind,
-            namespace=self.namespace
+            api_version=self._api_version, kind=self.kind,
+            namespace=self._namespace
+        )
+        self.temp_yaml = tempfile.NamedTemporaryFile(
+            mode='w+', prefix=self._kind, delete=False
         )
 
     @property
@@ -46,5 +49,29 @@ class BaseOCSClass(object):
         return self._namespace
 
     @property
-    def ocp(self):
-        return self.ocp
+    def name(self):
+        return self._name
+
+    def reload(self):
+        self.data = templating.load_yaml_to_dict(self.temp_yaml.name)
+
+    def get(self):
+        return self.ocp.get(resource_name=self.name)
+
+    def create(self):
+        log.info(f"Adding {self.kind} with name {self.name}")
+        templating.dump_dict_to_temp_yaml(self.data, self.temp_yaml.name)
+        return self.ocp.create(yaml_file=self.temp_yaml.name)
+
+    def delete(self):
+        self.ocp.delete(resource_name=self.name)
+
+    def apply(self, **data):
+        with open(self.temp_yaml.name, 'w') as yaml_file:
+            yaml.dump(data, yaml_file)
+        assert self.ocp.apply(yaml_file=self.temp_yaml.name), (
+            f"Failed to apply changes {data}"
+        )
+
+    def delete_temp_yaml_file(self):
+        utils.delete_file(self.temp_yaml.name)
--- tests/helpers.py
+++ tests/helpers.py
@@ -4,7 +4,7 @@ Helper functions file for OCS QE
 import logging
 import datetime
 from ocs import defaults
-from resources import ceph_file_system
+from resources.base_resource import BaseOCSClass
 
 logger = logging.getLogger(__name__)
 
@@ -17,7 +17,6 @@ def create_unique_resource_name(resource_description, resource_type):
     Args:
         resource_description (str): The user provided object description
         resource_type (str): The type of object for which the unique name
-
             will be created. For example: project, pvc, etc
 
     Returns:
@@ -26,62 +25,61 @@ def create_unique_resource_name(resource_description, resource_type):
     current_date_time = (
         datetime.datetime.now().strftime("%d%H%M%S%f")
     )
-    return (
-        f"{resource_type}-{resource_description[:23]}-{current_date_time[:10]}"
-    )
+    return f"{resource_type}_{resource_description[:23]}_{current_date_time[:10]}"
 
 
-def create_resource(resource_obj, desired_status=defaults.STATUS_AVAILABLE):
+def create_ocs_obj(**kwargs):
+    """
+    Create an instance of BaseOCSClass
+
+    Args:
+        kwargs (dict): Dictionary of the OCS resource
+
+    Returns:
+        BaseOCSClass: An instance of BaseOCSClass
+
+    """
+    return BaseOCSClass(**kwargs)
+
+
+def create_resource(desired_status=defaults.STATUS_AVAILABLE, **kwargs):
     """
     Create a resource
 
     Args:
-        resource_obj (str): The resource object (e.g. CephBlockPool)
         desired_status (str): The status of the resource to wait for
+        kwargs (dict): Dictionary of the OCS resource
 
     Raises:
         AssertionError: In case of any failure
     """
-    created_resource = resource_obj.create()
+    ocs_obj = create_ocs_obj(**kwargs)
+    created_resource = ocs_obj.create()
     assert created_resource, (
         f"Failed to create resource {created_resource.metadata.name}"
     )
-    assert resource_obj.ocp.wait_for_resource(
+    assert ocs_obj.ocp.wait_for_resource(
         condition=desired_status, resource_name=created_resource.metadata.name
-    ), f"{resource_obj.kind} {resource_obj.metadata.name} failed to reach status Available"
+    ), f"{ocs_obj.kind} {ocs_obj.metadata.name} failed to reach status Available"
+    return ocs_obj
 
 
-def create_ceph_file_system(fs_name, project_name, **kwargs):
+def apply_change_on_resource(ocs_obj, **kwargs):
     """
-    Create a Ceph file system
+    Apply changes on an OCS resource
 
     Args:
-        fs_name: The name of the new Ceph file system
-        project_name: The nam of the project/namespace of
-            which the Ceph file system belongs to
-
-    Returns:
-        CephFileSystem: A CephFileSystem object
-
-    Raises:
-        AssertionError: In case of any failure
-
+        ocs_obj (BaseOCSClass): An instance of BaseOCSClass
+        kwargs (dict): Dictionary of the OCS resource with the changes to apply
     """
-    fs_kwargs = defaults.CEPHFILESYSTEM_DICT.copy()
-    fs_kwargs['metadata']['cephblockpool_name'] = fs_name
-    fs_kwargs['metadata']['namespace'] = project_name
-    fs_kwargs.update(kwargs)
-    cephfs_obj = ceph_file_system.CephFileSystem(**fs_kwargs)
-    create_resource(cephfs_obj)
-    return cephfs_obj
+    ocs_obj.apply(**kwargs)
 
 
-def delete_ceph_file_system(cephfs_obj):
+def delete_resource(resource_obj):
     """
-    Delete a Ceph File System
+    Delete an OCS resource
 
     Args:
-        cephfs_obj: A CephFileSystem object
-
+        resource_obj: A BaseOCSClass object
     """
-    cephfs_obj.delete()
+    resource_obj.delete()
