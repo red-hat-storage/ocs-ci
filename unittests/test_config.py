843ac23c120cd705870cbf3f3fe6268e6393a3ca
Zack Cerza <zack@redhat.com>
2019-06-06 17:22:45 -0600

    Move logic from run_ocsci.py to ocsci.main
    This enables unit testing of that logic.
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- unittests/test_config.py
+++ unittests/test_config.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-import ocs
-import ocsci
-import run_ocsci
+import ocs.defaults
+import ocsci.config
+import ocsci.main
 
 
 class TestConfig(object):
     def test_defaults(self):
-        run_ocsci.init_ocsci_conf()
+        ocsci.main.init_ocsci_conf()
         config_sections = [i for i in dir(ocsci.config) if not i.startswith('_')]
         for section_name in config_sections:
             if section_name == 'CONFIG':
