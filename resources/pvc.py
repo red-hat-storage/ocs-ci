456cb3a29bef920061718924533d5088ba63d1fe
ratamir <ratamir@redhat.com>
2019-06-07 22:27:26 +0300

    Move all functions under PVC resource object

--- resources/pvc.py
+++ resources/pvc.py
@@ -1,84 +1,67 @@
 """
-General PVC helper functions
+General PVC object
 """
+import tempfile
 import logging
 
-from ocs.defaults import PVC, API_VERSION
-from ocs.ocp import OCP
 from resources.ocs import OCS
 
 log = logging.getLogger(__name__)
 
 
-def get_pvc_status(pvc_name, namespace):
+class PVC(OCS):
     """
-    Returns the PVC status
-
-    Args:
-         pvc_name (str): PVC name to check status
-         namespace (str): The namespace this PVC lives in
-
-    Returns:
-        str: PVC status
+    A basic PersistentVolumeClaim kind resource
     """
-    oc_client = OCP(api_version=API_VERSION, kind=PVC, namespace=namespace)
-    pvc = oc_client.get(resource_name=pvc_name)
-    return pvc.get('status').get('phase')
 
+    def __init__(self, **kwargs):
+        """
+        Initializer function
+        kwargs:
+            See parent class for kwargs information
+        """
+        super(PVC, self).__init__(**kwargs)
+        self.temp_yaml = tempfile.NamedTemporaryFile(
+            mode='w+', prefix='PVC_', delete=False
+        )
 
-def get_backed_pv(pvc_name, namespace):
-    """
-    Returns the backed PV name of pvc_name in namespace
+    def get_status(self):
+        """
+        Returns the PVC status
 
-    Args:
-         pvc_name (str): PVC name to get its backed PV
-         namespace (str): The namespace this PVC lives in
+        Returns:
+            str: PVC status
+        """
+        return self.data.get('status').get('phase')
 
-    Returns:
-        str: PV name
-    """
-    oc_client = OCP(api_version=API_VERSION, kind=PVC, namespace=namespace)
-    pvc = oc_client.get(resource_name=pvc_name)
-    return pvc.get('spec').get('volumeName')
+    def get_backed_pv(self):
+        """
+        Returns the backed PV name of pvc_name in namespace
 
+        Returns:
+            str: PV name
+        """
+        return self.data.get('spec').get('volumeName')
 
-def get_pvc_size(pvc_name, namespace):
-    """
-    Returns the PVC size pvc_name in namespace
+    def get_size(self):
+        """
+        Returns the PVC size pvc_name in namespace
 
-    Args:
-         pvc_name (str): PVC name to get its size
-         namespace (str): The namespace this PVC lives in
+        Returns:
+            int: PVC size
+        """
+        #  [:-2] -> to remove the 'Gi' from the size (e.g. '5Gi --> '5')
+        return int(self.data.get('status').get('capacity').get('storage')[:-2])
 
-    Returns:
-        int: PVC size
-    """
-    oc_client = OCP(api_version=API_VERSION, kind=PVC, namespace=namespace)
-    pvc = oc_client.get(resource_name=pvc_name)
-    #  [:-2] to remove the 'Gi' from the size (e.g. '5Gi --> '5')
-    return int(pvc.get('status').get('capacity').get('storage')[:-2])
+    def resize_pvc(self, new_size, verify=False):
+        """
+        Returns the PVC size pvc_name in namespace
 
-
-def resize_pvc(pvc_name, new_size, verify=False):
-    """
-    Returns the PVC size pvc_name in namespace
-
-    Args:
-         pvc_name (str): PVC name to get its size
-         new_size (int): The new size for the PVC
-         verify (bool): In order to check if the resize succeeded of not
-
-    Returns:
-        bool: True if operation succeeded, False otherwise
-    """
-    oc_client = OCP(api_version=API_VERSION, kind=PVC)
-    pvc = oc_client.get(resource_name=pvc_name)
-    pvc_obj = OCS(**pvc)
-    pvc_obj.data['status']['capacity']['storage'] = f"{new_size}Gi"
-    pvc_obj.apply(**pvc_obj.data)
-    if verify:
-        pvc_obj.reload()
-        return int(
-            pvc_obj.data.get('status').get('capacity').get('storage')[:-2]
-        ) == new_size
-    return True
+        Returns:
+            bool: True if operation succeeded, False otherwise
+        """
+        self.data['status']['capacity']['storage'] = f"{new_size}Gi"
+        self.apply(**self.data)
+        if verify:
+            return self.get_size() == new_size
+        return True
