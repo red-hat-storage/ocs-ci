a9f469609f4a4b4ad5dfb2be3057ea4f68b82a36
Raz Tamir <ratamir@redhat.com>
2019-06-10 19:51:38 +0300

    Add helper functions for pvc (#174)
    * Add helper functions for pvc
    * Move all functions under PVC resource object

---
+++ resources/pvc.py
@@ -0,0 +1,67 @@
+"""
+General PVC object
+"""
+import tempfile
+import logging
+
+from resources.ocs import OCS
+
+log = logging.getLogger(__name__)
+
+
+class PVC(OCS):
+    """
+    A basic PersistentVolumeClaim kind resource
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Initializer function
+        kwargs:
+            See parent class for kwargs information
+        """
+        super(PVC, self).__init__(**kwargs)
+
+    @property
+    def size(self):
+        """
+        Returns the PVC size pvc_name in namespace
+
+        Returns:
+            int: PVC size
+        """
+        #  [:-2] -> to remove the 'Gi' from the size (e.g. '5Gi --> '5')
+        return int(self.data.get('status').get('capacity').get('storage')[:-2])
+
+    @property
+    def status(self):
+        """
+        Returns the PVC status
+
+        Returns:
+            str: PVC status
+        """
+        return self.data.get('status').get('phase')
+
+    @property
+    def backed_pv(self):
+        """
+        Returns the backed PV name of pvc_name in namespace
+
+        Returns:
+            str: PV name
+        """
+        return self.data.get('spec').get('volumeName')
+
+    def resize_pvc(self, new_size, verify=False):
+        """
+        Returns the PVC size pvc_name in namespace
+
+        Returns:
+            bool: True if operation succeeded, False otherwise
+        """
+        self.data['status']['capacity']['storage'] = f"{new_size}Gi"
+        self.apply(**self.data)
+        if verify:
+            return self.get_size() == new_size
+        return True
