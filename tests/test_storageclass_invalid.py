912ae045d9acef829a30f05158b06f33336a3a3e
fbalak <fbalak@redhat.com>
2019-06-11 18:01:24 +0200

    use constants for pvc status

--- tests/test_storageclass_invalid.py
+++ tests/test_storageclass_invalid.py
@@ -3,7 +3,7 @@ import os.path
 
 from resources.ocs import OCS
 from tests import helpers
-from ocs import defaults
+from ocs import constants, defaults
 from ocs.exceptions import TimeoutExpiredError
 
 
@@ -34,16 +34,16 @@ def test_storageclass_cephfs_invalid(invalid_cephfs_storageclass, tmpdir):
 
     pvc_status = pvc.get()['status']['phase']
     logger.debug(f"Status of PVC {pvc_name} after creation: {pvc_status}")
-    assert pvc_status == 'Pending'
+    assert pvc_status == constants.STATUS_PENDING
 
     try:
         logger.info(
             f"Wait 60 seconds for status of PVC {pvc_name} "
-            f"to change to Bound (it shouldn't change)"
+            f"to change to {constants.STATUS_BOUND} (it shouldn't change)"
         )
         pvc_status_changed = pvc.ocp.wait_for_resource(
             resource_name=pvc_name,
-            condition="Bound",
+            condition=constants.STATUS_BOUND,
             timeout=60,
             sleep=20
         )
@@ -55,7 +55,11 @@ def test_storageclass_cephfs_invalid(invalid_cephfs_storageclass, tmpdir):
 
     pvc_status = pvc.get()['status']['phase']
     logger.info(f"Status of PVC {pvc_name} after 60 seconds: {pvc_status}")
-    assert pvc_status == 'Pending', f"PVC {pvc_name} hasn't reached status Pending"
+    assert_msg = (
+        f"PVC {pvc_name} hasn't reached status "
+        f"{constants.STATUS_PENDING}"
+    )
+    assert pvc_status == constants.STATUS_PENDING, assert_msg
 
     logger.info(f"Deleting PVC {pvc_name}")
     pvc.delete()
