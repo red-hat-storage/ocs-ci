81cd961d1e434f0953cbdd1db75d494230a2062a
Zack Cerza <zack@redhat.com>
2019-06-19 12:19:44 -0600

    Move config initialization to Config class
    Updates based on files passed in via the CLI will still happen in
    main.py, but via an interface provided by Config itself. This commit
    also drops support for jinja2 in config files.
    
    If a module needs to access the default configuration for any purpose,
    it may call config.get_defaults().
    
    Also add some more unit tests to prove correctness.
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- ocsci/__init__.py
+++ ocsci/__init__.py
@@ -8,7 +8,17 @@ under section PYTEST_DONT_REWRITE
 """
 # Use the new python 3.7 dataclass decorator, which provides an object similar
 # to a namedtuple, but allows type enforcement and defining methods.
+import collections
+import os
+import yaml
 from dataclasses import dataclass, field, fields
+
+from ocs import constants
+
+DEFAULT_CONFIG_PATH = os.path.join(
+    constants.TOP_DIR,
+    "conf/ocsci/default_config.yaml"
+)
 
 
 @dataclass
@@ -22,9 +32,33 @@ class Config:
         self.reset()
 
     def reset(self):
+        """
+        Clear all configuration data and load defaults
+        """
         for f in fields(self):
             setattr(self, f.name, f.default_factory())
-        self.RUN['cli_params'] = dict()
+        self.update(self.get_defaults())
+
+    def get_defaults(self):
+        """
+        Return a fresh copy of the default configuration
+        """
+        with open(DEFAULT_CONFIG_PATH) as file_stream:
+            return yaml.safe_load(file_stream)
+
+    def update(self, user_dict: dict):
+        """
+        Override configuration items with items in user_dict, without wiping
+        out non-overridden items
+        """
+        field_names = [f.name for f in fields(self)]
+        for k, v in user_dict.items():
+            if k not in field_names:
+                raise ValueError(
+                    f"{k} is not a valid config section. "
+                    f"Valid sections: {field_names}")
+            section = getattr(self, k)
+            merge_dict(section, v)
 
     def to_dict(self):
         # We don't use dataclasses.asdict() here, because that function appears
@@ -34,6 +68,44 @@ class Config:
         return {
             name: getattr(self, name) for name in field_names
         }
+
+
+def merge_dict(orig: dict, new: dict) -> dict:
+    """
+    Update a dict recursively, with values from 'new' being merged into 'orig'.
+
+    Args:
+        orig (dict): The object that will receive the update
+        new  (dict): The object which is the source of the update
+
+    Example:
+    orig = {
+        'dict': {'one': 1, 'two': 2},
+        'list': [1, 2],
+        'string': 's',
+    }
+    new = {
+        'dict': {'one': 'one', 'three': 3},
+        'list': [0],
+        'string': 'x',
+    }
+    merge_dict(orig, new) ->
+    {
+        'dict': {'one': 'one', 'two': 2, 'three': 3}
+        'list': [0],
+        'string', 'x',
+    }
+    """
+    for k, v in new.items():
+        if isinstance(orig, collections.Mapping):
+            if isinstance(v, collections.Mapping):
+                r = merge_dict(orig.get(k, dict()), v)
+                orig[k] = r
+            else:
+                orig[k] = v
+        else:
+            orig = {k: v}
+    return orig
 
 
 config = Config()
--- ocsci/main.py
+++ ocsci/main.py
@@ -1,18 +1,10 @@
-import collections
 import os
 
-from jinja2 import Template
 import pytest
 import yaml
 
 import ocsci
 from utility import utils
-
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-OCSCI_DEFAULT_CONFIG = os.path.join(
-    HERE, "../conf/ocsci/default_config.yaml"
-)
 
 
 def get_param(param, arguments, default=None):
@@ -34,75 +26,25 @@ def get_param(param, arguments, default=None):
     return default
 
 
-def init_ocsci_conf(arguments=None, default_config=OCSCI_DEFAULT_CONFIG):
+def init_ocsci_conf(arguments=None):
     """
-    Function to init the default config for OCS CI
+    Update the config object with any files passed via the CLI
 
     Args:
         arguments (list): Arguments for pytest execution
-        default_config (str): Default config data
-
     """
-    if arguments is None:
-        arguments = []
+    if not arguments:
+        return
     custom_config = get_param('--ocsci-conf', arguments)
     cluster_config = get_param('--cluster-conf', arguments)
-    config_data = ocsci.config.to_dict()
-    with open(default_config) as file_stream:
-        default_config_data = yaml.safe_load(file_stream)
-    update_dict_recursively(config_data, default_config_data)
     if custom_config:
         with open(custom_config) as file_stream:
             custom_config_data = yaml.safe_load(file_stream)
-        update_dict_recursively(config_data, custom_config_data)
+            ocsci.config.update(custom_config_data)
     if cluster_config:
         with open(cluster_config) as file_stream:
             cluster_config_data = yaml.safe_load(file_stream)
-        update_dict_recursively(config_data, cluster_config_data)
-    rendered_config = render_yaml_with_j2_context(config_data)
-    for key, value in rendered_config.items():
-        if key.isupper():
-            setattr(ocsci.config, key, value)
-
-
-def update_dict_recursively(d, u):
-    """
-    Update dict recursively to not delete nested dict under second and more
-    nested level. This function is changing the origin dictionary cause of
-    operations are done on top of it and dict is a mutable object.
-
-    Args:
-        d (dict): Dict to update
-        u (dict): Other dict used for update d dict
-
-    Returns:
-        dict: returning updated dictionary (changes are also done on dict `d`)
-    """
-    for k, v in u.items():
-        if isinstance(d, collections.Mapping):
-            if isinstance(v, collections.Mapping):
-                r = update_dict_recursively(d.get(k, {}), v)
-                d[k] = r
-            else:
-                d[k] = u[k]
-        else:
-            d = {k: u[k]}
-    return d
-
-
-def render_yaml_with_j2_context(yaml_data):
-    """
-    Render yaml template with own context.
-
-    Args:
-        yaml_data (dict): Yaml data
-
-    Returns:
-        dict: rendered data
-    """
-    template = Template(yaml.dump(yaml_data))
-    out = template.render(**yaml_data)
-    return yaml.safe_load(out)
+            ocsci.config.update(cluster_config_data)
 
 
 def main(arguments):
