9b22116df0a4f386c1c3ba1ded69c91b01fd46b8
Andrew Schoen <aschoen@redhat.com>
2019-06-19 16:24:56 -0500

    use the new config module in the cluster session fixture
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- tests/ecosystem/deployment/test_ocs_basic_install.py
+++ tests/ecosystem/deployment/test_ocs_basic_install.py
@@ -1,6 +1,6 @@
 import logging
 
-from ocsci.config import ENV_DATA
+from ocsci import config
 from ocsci.testlib import deployment
 from utility.utils import is_cluster_running
 
@@ -9,4 +9,4 @@ log = logging.getLogger(__name__)
 
 @deployment
 def test_cluster_is_running():
-    assert is_cluster_running(ENV_DATA['cluster_path'])
+    assert is_cluster_running(config.ENV_DATA['cluster_path'])
