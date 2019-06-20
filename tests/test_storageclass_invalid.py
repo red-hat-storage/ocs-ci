03715180668f9e735d44dd108232a908579ee407
Elena Bondarenko <ebondare@redhat.com>
2019-06-20 13:46:13 +0200

    Move dictionaries outside of fixture decorator

--- tests/test_storageclass_invalid.py
+++ tests/test_storageclass_invalid.py
@@ -13,43 +13,50 @@ from resources.ocs import OCS
 
 logger = logging.getLogger(__name__)
 
+OCS_311_CEPHFS_PARAMS = {
+    'template_dir': constants.TEMPLATE_CSI_FS_DIR,
+    'values': {
+        'storageclass_name': 'invalid-storageclass',
+        'provisioner': "invalid_provisioner",
+        'monitors': 'invalid_monitors',
+        'provision_volume': "invalid_provisioner_volume",
+        'ceph_pool': 'invalid_pool',
+        'root_path': 'invalid_root_path',
+        'provisioner_secret_name': 'invalid_provisioner_secret_name',
+        'provisioner_secret_namespace': 'invalid_provisioner_secret_namespace',
+        'node_stage_secret_name': 'invalid_node_stage_secret_name',
+        'node_stage_secret_namespace': 'invalid_node_stage_secret_namespace',
+        'mounter': 'invalid_mounter',
+        'reclaim_policy': 'Delete'
+    }
+}
+
+OCS_341_RBD_PARAMS = {
+    'template_dir': constants.TEMPLATE_CSI_RBD_DIR,
+    'values': {
+        'storageclass_name': 'invalid-storageclass',
+        'provisioner': "invalid_provisioner",
+        'monitors': 'invalid_monitors',
+        'pool': 'invalid_pool',
+        'imageFormat': 'invalid_format',
+        'imageFeatures': 'invalid_features',
+        'provisioner_secret_name': 'invalid_provisioner_secret_name',
+        'provisioner_secret_namespace': 'invalid_provisioner_secret_namespace',
+        'node_stage_secret_name': 'invalid_node_stage_secret_name',
+        'node_stage_secret_namespace': 'invalid_node_stage_secret_namespace',
+        'mounter': 'invalid_mounter',
+        'reclaim_policy': 'Delete'
+    }
+}
+
 
 @pytest.fixture(
-    params=[pytest.param({
-            'template_dir': constants.TEMPLATE_CSI_FS_DIR,
-            'values': {
-                'storageclass_name': 'invalid-storageclass',
-                'provisioner': "invalid_provisioner",
-                'monitors': 'invalid_monitors',
-                'provision_volume': "invalid_provisioner_volume",
-                'ceph_pool': 'invalid_pool',
-                'root_path': 'invalid_root_path',
-                'provisioner_secret_name': 'invalid_provisioner_secret_name',
-                'provisioner_secret_namespace': 'invalid_provisioner_secret_namespace',
-                'node_stage_secret_name': 'invalid_node_stage_secret_name',
-                'node_stage_secret_namespace': 'invalid_node_stage_secret_namespace',
-                'mounter': 'invalid_mounter',
-                'reclaim_policy': 'Delete'
-            }
-            },
-        marks=pytest.mark.polarion_id("OCS-331")),
-        pytest.param({
-            'template_dir': constants.TEMPLATE_CSI_RBD_DIR,
-            'values': {
-                'storageclass_name': 'invalid-storageclass',
-                'provisioner': "invalid_provisioner",
-                'monitors': 'invalid_monitors',
-                'pool': 'invalid_pool',
-                'imageFormat': 'invalid_format',
-                'imageFeatures': 'invalid_features',
-                'provisioner_secret_name': 'invalid_provisioner_secret_name',
-                'provisioner_secret_namespace': 'invalid_provisioner_secret_namespace',
-                'node_stage_secret_name': 'invalid_node_stage_secret_name',
-                'node_stage_secret_namespace': 'invalid_node_stage_secret_namespace',
-                'mounter': 'invalid_mounter',
-                'reclaim_policy': 'Delete'
-            }
-        },
+    params=[pytest.param(
+        OCS_311_CEPHFS_PARAMS,
+        marks=pytest.mark.polarion_id("OCS-331")
+    ),
+        pytest.param(
+        OCS_341_RBD_PARAMS,
         marks=pytest.mark.polarion_id("OCS-341"))
     ],  # TODO: add more test case parameters
     ids=["CephFS", "RBD"]
