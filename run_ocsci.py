741f9b6c1f6d4d95d2f0c6d27f9e7010c8e3bc26
Daniel Horak <dahorak@redhat.com>
2019-06-20 13:00:19 +0200

    configure PATH from RUN['bin_dir']
    - use RUN['bin_dir'] for PATH configuration
    - move PATH configuration from run_ocsci.py to ocsci/main.py

--- run_ocsci.py
+++ run_ocsci.py
@@ -1,13 +1,9 @@
-import os
 import sys
 
 import ocsci.main
 
 
 def main():
-    HERE = os.path.abspath(os.path.dirname(__file__))
-    from utility.utils import add_path_to_env_path
-    add_path_to_env_path(os.path.join(HERE, 'bin'))
     arguments = sys.argv[1:]
     exit(ocsci.main.main(arguments))
 
