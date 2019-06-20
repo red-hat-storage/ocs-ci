695af4cc9d71dffd4f7c59514da352128cf9d4c9
Elena Bondarenko <ebondare@redhat.com>
2019-06-20 14:50:04 +0200

    Put back fixture deleted by mistake

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,17 @@
 import logging
+import pytest
+
+from ocsci import config
 from utility.environment_check import environment_checker  # noqa: F401
 
 
 logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def polarion_testsuite_properties(record_testsuite_property):
+    """
+    Configures polarion testsuite properties for junit xml
+    """
+    polarion_project_id = config.REPORTING['polarion']['project_id']
+    record_testsuite_property('polarion-projct-id', polarion_project_id)
