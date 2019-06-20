0b5601c8ccc41b8c15a15a4366418b95e1244ac8
Andrew Schoen <aschoen@redhat.com>
2019-06-20 11:50:19 -0500

    resolve issues with port of deployment test to cluster fixture
    This cleans up some mistakes made during the rebase and port
    of the latest changes to the deployment test to the cluster
    session fixture.
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ import copy
 from ocsci import config
 from utility.environment_check import environment_checker  # noqa: F401
 from oc.openshift_ops import OCP
-from ocs import constants, ocp
+from ocs import constants, ocp, defaults
 from ocs.exceptions import CommandFailed, CephHealthException
 from ocs.utils import create_oc_resource, apply_oc_resource
 from utility import templating
@@ -22,60 +22,6 @@ from tests import helpers
 
 log = logging.getLogger(__name__)
 
-POD = ocp.OCP(kind=constants.POD, namespace=config.ENV_DATA['cluster_namespace'])
-CFS = ocp.OCP(
-    kind=constants.CEPHFILESYSTEM, namespace=config.ENV_DATA['cluster_namespace']
-)
-CEPH_OBJ = None
-
-
-@pytest.fixture(
-    params=[{
-        'storageclass_name': 'invalid-storageclass',
-        'provisioner': "invalid_provisioner",
-        'monitors': 'invalid_monitors',
-        'provision_volume': "invalid_provisioner_volume",
-        'ceph_pool': 'invalid_pool',
-        'root_path': 'invalid_root_path',
-        'provisioner_secret_name': 'invalid_provisioner_secret_name',
-        'provisioner_secret_namespace': 'invalid_provisioner_secret_namespace',
-        'node_stage_secret_name': 'invalid_node_stage_secret_name',
-        'node_stage_secret_namespace': 'invalid_node_stage_secret_namespace',
-        'mounter': 'invalid_mounter',
-        'reclaim_policy': 'Delete'
-    }]  # TODO: add more test case parameters
-)
-def invalid_cephfs_storageclass(request):
-    """
-    Creates StorageClass with CephFS filesystem that have invalid parameters.
-    Storageclass is removed at the end of test.
-
-    Returns:
-        str: Name of created StorageClass
-    """
-    log.info(
-        f"SETUP - creating storageclass "
-        f"{request.param['storageclass_name']}"
-    )
-    yaml_path = os.path.join(
-        constants.TEMPLATE_CSI_FS_DIR, "storageclass.yaml"
-    )
-    yaml_data = yaml.safe_load(open(yaml_path, 'r'))
-    yaml_data.update(request.param)
-    storageclass = OCS(**yaml_data)
-    sc_data = storageclass.create()
-
-    log.debug('Check that storageclass has assigned creationTimestamp')
-    assert sc_data['metadata']['creationTimestamp']
-
-    yield sc_data
-
-    log.info(
-        f"TEARDOWN - removing storageclass "
-        f"{request.param['storageclass_name']}"
-    )
-    storageclass.delete()
-
 
 def polarion_testsuite_properties(record_testsuite_property):
     """
@@ -87,7 +33,7 @@ def polarion_testsuite_properties(record_testsuite_property):
 
 def cluster_teardown():
     log.info("Destroying the test cluster")
-    destroy_cluster(ENV_DATA['cluster_path'])
+    destroy_cluster(config.ENV_DATA['cluster_path'])
     log.info("Destroying the test cluster complete")
 
 
@@ -239,6 +185,11 @@ def cluster(request):
     )
     create_oc_resource('cluster.yaml', cluster_path, _templating, config.ENV_DATA)
 
+    POD = ocp.OCP(kind=constants.POD, namespace=config.ENV_DATA['cluster_namespace'])
+    CFS = ocp.OCP(
+        kind=constants.CEPHFILESYSTEM, namespace=config.ENV_DATA['cluster_namespace']
+    )
+
     # Check for the Running status of Ceph Pods
     run_cmd(
         f"oc wait --for condition=ready pod "
@@ -278,9 +229,8 @@ def cluster(request):
     fs_data = copy.deepcopy(defaults.CEPHFILESYSTEM_DICT)
     fs_data['metadata']['namespace'] = config.ENV_DATA['cluster_namespace']
 
-    global CEPH_OBJ
-    CEPH_OBJ = OCS(**fs_data)
-    CEPH_OBJ.create()
+    ceph_obj = OCS(**fs_data)
+    ceph_obj.create()
     assert POD.wait_for_resource(
         condition=constants.STATUS_RUNNING, selector='app=rook-ceph-mds',
         resource_count=2, timeout=600
