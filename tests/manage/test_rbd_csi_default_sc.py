faa38d5f2576d9bf473cd49cdefeacea6012e3d9
Parikshith <pbyregow@localhost.localdomain>
2019-06-28 16:16:03 +0530

    Fixing issue #264

--- tests/manage/test_rbd_csi_default_sc.py
+++ tests/manage/test_rbd_csi_default_sc.py
@@ -8,6 +8,8 @@ import pytest
 from ocsci.testlib import tier1, ManageTest
 from tests import helpers
 from ocs import constants
+from utility import templating
+from resources.pvc import PVC
 
 log = logging.getLogger(__name__)
 
@@ -37,7 +39,7 @@ def teardown():
     Tearing down the environment
     """
     log.info("Deleting PVC")
-    PVC.delete()
+    PVC_OBJ.delete()
 
     log.info("Deleting StorageClass")
     STORAGE_CLASS.delete()
@@ -58,11 +60,14 @@ class TestBasicPVCOperations(ManageTest):
     """
 
     def test_ocs_347(self):
-        global PVC, STORAGE_CLASS
+        global PVC_OBJ, STORAGE_CLASS
         log.info("Creating RBD StorageClass")
         STORAGE_CLASS = helpers.create_storage_class(
             constants.CEPHBLOCKPOOL, 'rbd', SECRET.name
         )
 
         log.info("Creating a PVC")
-        PVC = helpers.create_pvc(STORAGE_CLASS.name)
+        rbd_pvc = templating.load_yaml_to_dict(constants.CSI_RBD_PVC_YAML)
+        PVC_OBJ = PVC(**rbd_pvc)
+        PVC_OBJ.create()
+        assert 'Bound' in PVC_OBJ.status
