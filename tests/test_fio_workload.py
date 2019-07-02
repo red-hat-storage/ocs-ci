40af0037003fa9329c005cbe93cc3050031ac49c
Shylesh Kumar <shmohan@redhat.com>
2019-07-02 15:35:25 -0700

    Cleanup pod and pvc resources after test, also introduced a helper to find pv for pvc
    Signed-off-by: Shylesh Kumar <shmohan@redhat.com>

--- tests/test_fio_workload.py
+++ tests/test_fio_workload.py
@@ -6,6 +6,7 @@ from ocs_ci.ocs import workload
 from ocs_ci.framework.testlib import ManageTest
 from ocs_ci.utility.utils import TimeoutSampler
 from ocs_ci.utility import templating
+from tests import helpers
 
 
 from tests.fixtures import (
@@ -16,12 +17,43 @@ from tests.fixtures import (
 logger = logging.getLogger(__name__)
 
 
+@pytest.fixture(scope='class')
+def test_fixture(request):
+    """
+    Fixture only for teardown
+    """
+
+    self = request.node.cls
+
+    def finalizer():
+        teardown(self)
+    request.addfinalizer(finalizer)
+    setup(self)
+
+
+def setup(self):
+    pass
+
+
+def teardown(self):
+    logger.info(f"Deleting pod {self.pod_obj.name}")
+    self.pod_obj.delete()
+    self.pvc_obj.reload()
+    pv = helpers.get_pv_for_pvc(self.pvc_obj)
+    logger.info(f"PV for PVC is {pv.name}")
+    logger.info(f"Deleting PVC {self.pvc_obj.name}")
+    self.pvc_obj.delete()
+    logger.info(f"Deleting PV {pv.name}")
+    pv.delete()
+
+
 @pytest.mark.usefixtures(
     create_rbd_secret.__name__,
     create_ceph_block_pool.__name__,
     create_rbd_storageclass.__name__,
     create_pvc.__name__,
-    create_pod.__name__
+    create_pod.__name__,
+    test_fixture.__name__,
 )
 class TestFIOWorkload(ManageTest):
 
@@ -34,7 +66,7 @@ class TestFIOWorkload(ManageTest):
         work_load = 'fio'
         storage_type = 'fs'
         # few io parameters for Fio
-        runtime = 10
+        runtime = 50
         size = '200M'
 
         wl = workload.WorkLoad(
