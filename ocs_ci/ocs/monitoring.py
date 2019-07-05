a6aec6f23cab07667459cebf652fbf9b4119c5e0
Akarsha <akrai@redhat.com>
2019-07-05 13:10:44 +0530

    Using exec_cmd_on_pod func

--- ocs_ci/ocs/monitoring.py
+++ ocs_ci/ocs/monitoring.py
@@ -4,8 +4,8 @@ from ocs_ci.ocs.ocp import OCP
 from ocs_ci.utility import templating
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.resources.pvc import get_all_pvcs
+from ocs_ci.ocs.resources.pod import get_resource_obj
 from tests import helpers
-from ocs_ci.utility.utils import run_cmd
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +14,9 @@ def create_configmap_cluster_monitoring_pod(sc_name):
     """
     Create a configmap named cluster-monitoring-config
     and configure pvc on monitoring pod
+
+    Args:
+        sc_name (str): Name of the storage class
     """
     logger.info("Creating configmap cluster-monitoring-config")
     config_map = templating.load_yaml_to_dict(
@@ -47,11 +50,15 @@ def validate_pvc_created_and_bound_on_monitoring_pods():
 def validate_pvc_are_mounted_on_monitoring_pods(pod_list):
     """
     Validate created pvc are mounted on monitoring pods
+
+    Args:
+        pod_list (list): List of the pods where pvc are mounted
     """
     for pod in pod_list:
-        mount_point = run_cmd(
-            cmd=f"oc rsh -n openshift-monitoring {pod} df -kh"
+        pod_obj = get_resource_obj(
+            name=pod, kind='Pod', namespace='openshift-monitoring'
         )
+        mount_point = pod_obj.exec_cmd_on_pod(command="df -kh")
         assert "/dev/rbd" in mount_point
     logger.info("Verified all pvc are mounted on monitoring pods")
 
@@ -59,6 +66,9 @@ def validate_pvc_are_mounted_on_monitoring_pods(pod_list):
 def validate_monitoring_pods_are_respinned_and_running_state(pods_list):
     """
     Validate monitoring pods are respinned and running state
+
+    Args:
+        pod_list (list): List of the pods where pvc are mounted
     """
     ocp = OCP(api_version='v1', kind='Pod', namespace='openshift-monitoring')
     assert ocp.wait_for_resource(
--- ocs_ci/ocs/resources/pod.py
+++ ocs_ci/ocs/resources/pod.py
@@ -13,6 +13,8 @@ import base64
 
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs import constants, defaults, workload
+from ocs_ci.ocs import constants, defaults
+from ocs_ci.utility import templating
 from ocs_ci.framework import config
 from ocs_ci.ocs.exceptions import CommandFailed
 from ocs_ci.ocs.resources.ocs import OCS
@@ -455,3 +457,20 @@ def get_osd_pods(osd_label=constants.OSD_APP_LABEL, namespace=None):
     osds = get_pods_having_label(osd_label, namespace)
     osd_pods = [Pod(**osd) for osd in osds]
     return osd_pods
+
+
+def get_resource_obj(name, kind, namespace=None):
+    """
+    Returns the pod obj for the given pod
+
+    Args:
+        name (str): name of the resources
+        kind (str): kind of the resources
+
+    Returns:
+        obj : obj of the given name of the rsource
+    """
+    ocp_obj = OCP(api_version='v1', kind=kind, namespace=namespace)
+    ocp_dict = ocp_obj.get(resource_name=name)
+    pod_obj = Pod(**ocp_dict)
+    return pod_obj
