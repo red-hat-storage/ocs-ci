0e4249c3093f96e33b1617672442e7b713bba1fa
Warren Usui <wusui@redhat.com>
2019-07-06 07:02:33 +0000

    test_pvresize_with_node_down.py
    Code review changes

--- tests/manage/test_pvresize_with_node_down.py
+++ tests/manage/test_pvresize_with_node_down.py
@@ -11,8 +11,7 @@ from ocs_ci.ocs import ocp, constants
 from ocs_ci.framework import config
 from ocs_ci.framework.testlib import tier1, ManageTest
 from ocs_ci.ocs.resources.pvc import PVC
-from ocs_ci.ocs.resources.pod import Pod
-from ocs_ci.ocs.resources.pod import get_ceph_tools_pod
+from ocs_ci.ocs.resources.pod import Pod, get_ceph_tools_pod
 from ocs_ci.utility.utils import run_cmd
 from ocs_ci.ocs.exceptions import CommandFailed
 from ocs_ci.utility import templating
@@ -37,19 +36,6 @@ def test_fixture(request):
     setup()
 
 
-def check_obj(obj_type):
-    """
-    Check pvc and pod.  Only one of each will be created in this test.
-
-    Return metadata
-    """
-    cmd_str = f'oc -n {config.ENV_DATA["my_namespace"]} get {obj_type} -o json'
-    objs = json.loads(run_cmd(cmd_str))
-    assert len(objs['items']) == 1
-    log.info(f"{obj_type} created successfully.")
-    return objs['items'][0]['metadata']
-
-
 def setup():
     """
     Setting up the environment for the test
@@ -70,12 +56,6 @@ def setup():
     global OUR_PVC
     OUR_PVC = PVC(**pvc_data)
     OUR_PVC.create()
-    pvc_info = check_obj('pvc')['annotations']
-    while 'pv.kubernetes.io/bind-completed' not in pvc_info:
-        sleep(1)
-        pvc_info = check_obj('pvc')['annotations']
-    assert pvc_info['pv.kubernetes.io/bind-completed'] == 'yes'
-    assert pvc_info['pv.kubernetes.io/bound-by-controller'] == 'yes'
 
     pod_data = templating.load_yaml_to_dict(constants.CSI_RBD_POD_YAML)
     pod_data['metadata']['namespace'] = config.ENV_DATA["my_namespace"]
@@ -89,8 +69,7 @@ def setup():
     global POD
     POD = Pod(**pod_data)
     POD.create()
-    pod_info = check_obj('pod')
-    assert pod_info['namespace'] == OCS_BUG_ID
+
     global CEPH_TOOL
     CEPH_TOOL = get_ceph_tools_pod()
 
@@ -125,7 +104,7 @@ def cmd_on_my_namespace(real_cmd):
     Args:
         real_cmd (str): oc command to be run ('get pods', for example)
     """
-    FAIL_LIMIT = 100
+    fail_limit = 100
     setup_cmd = f'rsh {OCS_BUG_ID}-pod {real_cmd}'
     full_cmd = f'oc -n {config.ENV_DATA["my_namespace"]} {setup_cmd}'
     not_run = True
@@ -137,7 +116,7 @@ def cmd_on_my_namespace(real_cmd):
         except CommandFailed as ex:
             log.info(f'{ex} raised on {setup_cmd}')
             fcount += 1
-            if fcount > FAIL_LIMIT:
+            if fcount > fail_limit:
                 raise CommandFailed(f'Too many failures: {setup_cmd}')
             sleep(5)
 
@@ -152,9 +131,9 @@ def test_add_stuff_to_rbd():
     """
     df_info = CEPH_TOOL.exec_ceph_cmd('ceph df')
     prev_avail = df_info['stats']['total_avail_bytes']
-    BIG_FILE = '/usr/lib/x86_64-linux-gnu/libicudata.so.57.1'
+    big_file = '/usr/lib/x86_64-linux-gnu/libicudata.so.57.1'
     for i in range(0, 10):
-        param = f'cp {BIG_FILE} /mnt/{OCS_BUG_ID}/xx.{i}'
+        param = f'cp {big_file} /mnt/{OCS_BUG_ID}/xx.{i}'
         cmd_on_my_namespace(param)
     df_info = CEPH_TOOL.exec_ceph_cmd('ceph df')
     now_avail = df_info['stats']['total_avail_bytes']
@@ -169,7 +148,7 @@ def update_pvc():
     Then read the pv so that we are sure that the change has taken effect
     before increasing the size of the pvc
     """
-    NEW_SIZE = '200Gi'
+    new_size = '200Gi'
     cmd_front = f"oc -n {config.ENV_DATA['my_namespace']} patch"
     cmd1_middle = 'storageclass/"rook-ceph-block"'
     cmd2_middle = 'pvc/ocs-pvc'
@@ -179,7 +158,7 @@ def update_pvc():
     run_cmd('oc get pv')
     cmd_2 = ' '.join([cmd_front, cmd2_middle, '--patch'])
     cmd_2 += " '{\"spec\": {\"resources\": {\"requests\": {\"storage\": \""
-    cmd_2 += NEW_SIZE
+    cmd_2 += new_size
     cmd_2 += "\"}}}}'"
     run_cmd(cmd_2)
 
@@ -189,11 +168,11 @@ def update_pvc():
     test_fixture.__name__,
 )
 @pytest.mark.polarion_id("OCS-368")
-class TestOcs368(ManageTest):
+class TestPvResizeWithNodeDown(ManageTest):
     """
     Actual OCS-368 test.
     """
-    def test_ocs_368(self):
+    def test_pv_resize_with_node_down(self):
         """
         The test_add_stuff_to_rbd call makes sure that there is data written
         to the rbd pool
@@ -206,6 +185,8 @@ class TestOcs368(ManageTest):
 
         After all OSDs are up, we wait until ceph is HEALTHY
         """
+        delay_secs = 300
+        delay_sec_per_hour = 4
         test_add_stuff_to_rbd()
         pvc_chg = threading.Thread(target=update_pvc)
         pvc_chg.start()
@@ -213,16 +194,21 @@ class TestOcs368(ManageTest):
         cmd_str = f'oc -n {config.ENV_DATA["cluster_namespace"]} {cmd_str}'
         run_cmd(cmd_str)
         pvc_chg.join()
+        retry_count = 0
+        osd_still_down = True
         while True:
-            sleep(300)
+            sleep(delay_secs)
             sval = CEPH_TOOL.exec_ceph_cmd('ceph -s')
             num_osds = sval['osdmap']['osdmap']['num_osds']
             num_up_osds = sval['osdmap']['osdmap']['num_up_osds']
             if num_osds == num_up_osds:
-                break
-        while True:
+                if osd_still_down:
+                    osd_still_down = False
+                    log.info("All osds are back up.")
             health = CEPH_TOOL.exec_ceph_cmd('ceph health')
             if health['status'] == 'HEALTH_OK':
                 break
-            sleep(300)
-            break
+            retry_count += 1
+            if retry_count >= delay_sec_per_hour:
+                log.error("Timeout -- Ceph not fully recovered")
+                break
