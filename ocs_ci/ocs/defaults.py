3a932073f2a5c4d3ed017a932a0d07d423b8f433
fbalak <fbalak@redhat.com>
2019-07-09 16:55:18 +0200

    add monitoring definitions to defaults and constants

--- ocs_ci/ocs/defaults.py
+++ ocs_ci/ocs/defaults.py
@@ -18,8 +18,11 @@ OPENSHIFT_REST_CLIENT_API_VERSION = 'v1'
 INSTALLER_VERSION = '4.1.4'
 CLIENT_VERSION = INSTALLER_VERSION
 ROOK_CLUSTER_NAMESPACE = 'openshift-storage'
+OCS_MONITORING_NAMESPACE = 'openshift-monitoring'
 KUBECONFIG_LOCATION = 'auth/kubeconfig'  # relative from cluster_dir
 API_VERSION = "v1"
 CEPHFILESYSTEM_NAME = 'ocsci-cephfs'
 
 TEMP_YAML = os.path.join(constants.TEMPLATE_DIR, "temp.yaml")
+
+PROMETHEUS_ROUTE = 'prometheus-k8s'
