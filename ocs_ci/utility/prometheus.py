abba1c9975f01d38211c94e5e21ce02dbaeecb99
fbalak <fbalak@redhat.com>
2019-07-09 16:56:09 +0200

    get prometheus endpoint
    Signed-off-by: Warren Usui <wusui@redhat.com>

--- ocs_ci/utility/prometheus.py
+++ ocs_ci/utility/prometheus.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from ocs_ci.framework import config
+from ocs_ci.ocs import constants, defaults
 from ocs_ci.ocs.ocp import OCP
 
 logger = logging.getLogger(name=__file__)
@@ -10,16 +11,17 @@ logger = logging.getLogger(name=__file__)
 
 class PrometheusAPI(object):
     """
-    This is wrapper class for Prometheus API
+    This is wrapper class for Prometheus API.
     """
 
     _token = None
     _user = None
     _password = None
+    _endpoint = None
 
     def __init__(self, user=None, password=None):
         """
-        Constructor for PrometheusAPI class
+        Constructor for PrometheusAPI class.
 
         Args:
             user (str): OpenShift username used to connect to API
@@ -40,15 +42,23 @@ class PrometheusAPI(object):
                     filename
                 )
         self._password = password
-        self.refresh_token()
+        self.refresh_connection()
 
-    def refresh_token(self):
+    def refresh_connection(self):
         """
-        Login into OCP and refresh access token.
+        Login into OCP, refresh endpoint and token.
         """
-        ocp = OCP()
+        ocp = OCP(
+            kind=constants.ROUTE,
+            namespace=defaults.OCS_MONITORING_NAMESPACE
+        )
         assert ocp.login(self._user, self._password), 'Login to OCP failed'
         self._token = ocp.get_user_token()
+        route_obj = ocp.get(
+            resource_name=defaults.PROMETHEUS_ROUTE
+        )
+        self._endpoint = route_obj['spec']['host']
+        logger.info(self._endpoint)
 
     def alerts(self):
         """
