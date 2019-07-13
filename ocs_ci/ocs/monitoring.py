adacb8a61c0990647f3a0fbbda4f21ab8fe7f8dc
Akarsha Rai <akrai@redhat.com>
2019-07-13 11:24:31 +0530

    TC-OCS-491: Configuring persistent volume on monitoring pods (#355)

---
+++ ocs_ci/ocs/monitoring.py
@@ -0,0 +1,101 @@
+import logging
+import yaml
+
+from ocs_ci.ocs.ocp import OCP
+from ocs_ci.utility import templating
+from ocs_ci.ocs import constants
+from ocs_ci.ocs.resources.pvc import get_all_pvcs, PVC
+from ocs_ci.ocs.resources.pod import get_pod_obj
+from tests import helpers
+
+logger = logging.getLogger(__name__)
+
+
+def create_configmap_cluster_monitoring_pod(sc_name):
+    """
+    Create a configmap named cluster-monitoring-config
+    and configure pvc on monitoring pod
+
+    Args:
+        sc_name (str): Name of the storage class
+    """
+    logger.info("Creating configmap cluster-monitoring-config")
+    config_map = templating.load_yaml_to_dict(
+        constants.CONFIGURE_PVC_ON_MONITORING_POD
+    )
+    config = yaml.load(config_map['data']['config.yaml'])
+    config['prometheusK8s']['volumeClaimTemplate']['spec']['storageClassName'] = sc_name
+    config['alertmanagerMain']['volumeClaimTemplate']['spec']['storageClassName'] = sc_name
+    config = yaml.dump(config)
+    config_map['data']['config.yaml'] = config
+    assert helpers.create_resource(**config_map, wait=False)
+    ocp = OCP('v1', 'ConfigMap', 'openshift-monitoring')
+    assert ocp.get(resource_name='cluster-monitoring-config')
+    logger.info("Successfully created configmap cluster-monitoring-config")
+
+
+def validate_pvc_created_and_bound_on_monitoring_pods():
+    """
+    Validate pvc's created and bound in state
+    on monitoring pods
+    """
+    logger.info("Verify pvc are created")
+    pvc_list = get_all_pvcs(namespace='openshift-monitoring')
+    logger.info(f"PVC list {pvc_list}")
+    # Check all pvc's are in bound state
+    for pvc in pvc_list['items']:
+        assert pvc['status']['phase'] == constants.STATUS_BOUND, (
+            f"PVC {pvc['metadata']['name']} is not Bound"
+        )
+    logger.info('Verified: Created PVCs are in Bound state.')
+
+
+def validate_pvc_are_mounted_on_monitoring_pods(pod_list):
+    """
+    Validate created pvc are mounted on monitoring pods
+
+    Args:
+        pod_list (list): List of the pods where pvc are mounted
+    """
+    for pod in pod_list:
+        pod_obj = get_pod_obj(
+            name=pod, namespace='openshift-monitoring'
+        )
+        mount_point = pod_obj.exec_cmd_on_pod(command="df -kh")
+        assert "/dev/rbd" in mount_point, f"pvc is not mounted on pod {pod}"
+    logger.info("Verified all pvc are mounted on monitoring pods")
+
+
+def validate_monitoring_pods_are_respinned_and_running_state(pods_list):
+    """
+    Validate monitoring pods are respinned and running state
+
+    Args:
+        pod_list (list): List of the pods where pvc are mounted
+    """
+    ocp = OCP(api_version='v1', kind='Pod', namespace='openshift-monitoring')
+    assert ocp.wait_for_resource(
+        condition=constants.STATUS_PENDING, resource_name=pods_list[0]
+    ), f"failed to reach pod {pods_list[0]} "
+    f"desired status {constants.STATUS_PENDING}"
+    for pod in pods_list:
+        assert ocp.wait_for_resource(
+            condition=constants.STATUS_RUNNING, resource_name=pod
+        ), f"failed to reach pod {pod} "
+        f"desired status {constants.STATUS_RUNNING}"
+
+
+def get_list_pvc_objs_created_on_monitoring_pods():
+    """
+    Returns list of pvc objects
+    """
+    pvc_list = get_all_pvcs(namespace='openshift-monitoring')
+    ocp_pvc_obj = OCP(
+        kind=constants.PVC, namespace='openshift-monitoring'
+    )
+    pvc_obj_list = []
+    for pvc in pvc_list['items']:
+        pvc_dict = ocp_pvc_obj.get(resource_name=pvc.get('metadata').get('name'))
+        pvc_obj = PVC(**pvc_dict)
+        pvc_obj_list.append(pvc_obj)
+    return pvc_obj_list
---
+++ tests/e2e/test_running_cluster_monitoring_with_persistent_storage.py
@@ -0,0 +1,77 @@
+import logging
+
+import pytest
+
+from ocs_ci.ocs.ocp import OCP
+from ocs_ci.framework.testlib import tier1, E2ETest
+from ocs_ci.ocs.resources.pvc import delete_pvcs
+from ocs_ci.ocs.monitoring import (
+    create_configmap_cluster_monitoring_pod,
+    validate_pvc_created_and_bound_on_monitoring_pods,
+    validate_pvc_are_mounted_on_monitoring_pods,
+    validate_monitoring_pods_are_respinned_and_running_state,
+    get_list_pvc_objs_created_on_monitoring_pods
+)
+from tests.fixtures import (
+    create_rbd_storageclass, create_ceph_block_pool,
+    create_rbd_secret
+)
+
+logger = logging.getLogger(__name__)
+ocp = OCP('v1', 'ConfigMap', 'openshift-monitoring')
+
+
+@pytest.fixture()
+def test_fixture(request):
+    """
+    Setup and teardown
+    """
+    self = request.node.cls
+
+    def finalizer():
+        teardown(self)
+    request.addfinalizer(finalizer)
+
+
+def teardown(self):
+    """
+    Delete pvc and config map created
+    """
+    assert ocp.delete(resource_name='cluster-monitoring-config')
+    pvc_obj_list = get_list_pvc_objs_created_on_monitoring_pods()
+    assert delete_pvcs(pvc_obj_list)
+
+
+@pytest.mark.usefixtures(
+    create_rbd_secret.__name__,
+    create_ceph_block_pool.__name__,
+    create_rbd_storageclass.__name__,
+    test_fixture.__name__
+)
+class TestRunningClusterMonitoringWithPersistentStorage(E2ETest):
+    """
+    Configure the persistent volume claim on monitoring
+    """
+    pods_list = ['prometheus-k8s-0', 'prometheus-k8s-1',
+                 'alertmanager-main-0', 'alertmanager-main-1',
+                 'alertmanager-main-2']
+
+    @tier1
+    def test_running_cluster_mointoring_with_persistent_stoarge(self):
+        """
+        A test case to configure the persistent volume on monitoring pods
+        """
+
+        # Create configmap cluster-monitoring-config
+        create_configmap_cluster_monitoring_pod(self.sc_obj.name)
+
+        # Validate the pods are respinned and in running state
+        validate_monitoring_pods_are_respinned_and_running_state(
+            self.pods_list
+        )
+
+        # Validate the pvc is created on monitoring pods
+        validate_pvc_created_and_bound_on_monitoring_pods()
+
+        # Validate the pvc are mounted on pods
+        validate_pvc_are_mounted_on_monitoring_pods(self.pods_list)
