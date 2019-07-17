670c1a47c5b79b82a723274aafad2c09f5babca9
Elad Ben Aharon <ebenahar@redhat.com>
2019-07-17 20:00:41 +0300

    Run IO multiple pods (#402)
    Add test to run on IO on multiple pods, add/udpate new fixtures
    
    Signed-off-by: Warren Usui <wusui@redhat.com>

--- tests/manage/test_pvc_disruptive.py
+++ tests/manage/test_pvc_disruptive.py
@@ -5,7 +5,7 @@ from tests import helpers, disruption_helpers
 from ocs_ci.framework.testlib import ManageTest, tier4
 from tests.fixtures import (
     create_rbd_storageclass, create_ceph_block_pool, create_cephfs_storageclass,
-    create_rbd_secret, create_cephfs_secret
+    create_rbd_secret, create_cephfs_secret, create_project
 )
 
 logger = logging.getLogger(__name__)
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 DISRUPTION_OPS = disruption_helpers.Disruptions()
 
 
+@pytest.mark.usefixtures(create_project.__name__)
 class BaseDisruption(ManageTest):
     """
     Base class for PVC related disruption tests
@@ -27,7 +28,7 @@ class BaseDisruption(ManageTest):
         """
         DISRUPTION_OPS.set_resource(resource=resource_to_delete)
         self.pvc_obj = helpers.create_pvc(
-            sc_name=self.sc_obj.name, wait=False
+            sc_name=self.sc_obj.name, namespace=self.namespace, wait=False
         )
         if operation_to_disrupt == 'create_pvc':
             DISRUPTION_OPS.delete_resource()
@@ -37,7 +38,8 @@ class BaseDisruption(ManageTest):
         )
 
         self.pod_obj = helpers.create_pod(
-            interface_type=constants.CEPHBLOCKPOOL, pvc_name=self.pvc_obj.name, wait=False
+            interface_type=constants.CEPHBLOCKPOOL, pvc_name=self.pvc_obj.name, wait=False,
+            namespace=self.namespace
         )
         if operation_to_disrupt == 'create_pod':
             DISRUPTION_OPS.delete_resource()
@@ -69,15 +71,33 @@ class TestRBDDisruption(BaseDisruption):
     @pytest.mark.parametrize(
         argnames=["operation_to_disrupt", "resource_to_delete"],
         argvalues=[
-            pytest.param(*['create_pvc', 'mgr'], marks=pytest.mark.polarion_id("OCS-568")),
-            pytest.param(*['create_pod', 'mgr'], marks=pytest.mark.polarion_id("OCS-569")),
-            pytest.param(*['run_io', 'mgr'], marks=pytest.mark.polarion_id("OCS-570")),
-            pytest.param(*['create_pvc', 'mon'], marks=pytest.mark.polarion_id("OCS-561")),
-            pytest.param(*['create_pod', 'mon'], marks=pytest.mark.polarion_id("OCS-562")),
-            pytest.param(*['run_io', 'mon'], marks=pytest.mark.polarion_id("OCS-563")),
-            pytest.param(*['create_pvc', 'osd'], marks=pytest.mark.polarion_id("OCS-565")),
-            pytest.param(*['create_pod', 'osd'], marks=pytest.mark.polarion_id("OCS-554")),
-            pytest.param(*['run_io', 'osd'], marks=pytest.mark.polarion_id("OCS-566")),
+            pytest.param(
+                *['create_pvc', 'mgr'], marks=pytest.mark.polarion_id("OCS-568")
+            ),
+            pytest.param(
+                *['create_pod', 'mgr'], marks=pytest.mark.polarion_id("OCS-569")
+            ),
+            pytest.param(
+                *['run_io', 'mgr'], marks=pytest.mark.polarion_id("OCS-570")
+            ),
+            pytest.param(
+                *['create_pvc', 'mon'], marks=pytest.mark.polarion_id("OCS-561")
+            ),
+            pytest.param(
+                *['create_pod', 'mon'], marks=pytest.mark.polarion_id("OCS-562")
+            ),
+            pytest.param(
+                *['run_io', 'mon'], marks=pytest.mark.polarion_id("OCS-563")
+            ),
+            pytest.param(
+                *['create_pvc', 'osd'], marks=pytest.mark.polarion_id("OCS-565")
+            ),
+            pytest.param(
+                *['create_pod', 'osd'], marks=pytest.mark.polarion_id("OCS-554")
+            ),
+            pytest.param(
+                *['run_io', 'osd'], marks=pytest.mark.polarion_id("OCS-566")
+            ),
 
         ]
     )
@@ -102,18 +122,42 @@ class TestFSDisruption(BaseDisruption):
     @pytest.mark.parametrize(
         argnames=["operation_to_disrupt", "resource_to_delete"],
         argvalues=[
-            pytest.param(*['create_pvc', 'mgr'], marks=pytest.mark.polarion_id("OCS-555")),
-            pytest.param(*['create_pod', 'mgr'], marks=pytest.mark.polarion_id("OCS-558")),
-            pytest.param(*['run_io', 'mgr'], marks=pytest.mark.polarion_id("OCS-559")),
-            pytest.param(*['create_pvc', 'mon'], marks=pytest.mark.polarion_id("OCS-560")),
-            pytest.param(*['create_pod', 'mon'], marks=pytest.mark.polarion_id("OCS-550")),
-            pytest.param(*['run_io', 'mon'], marks=pytest.mark.polarion_id("OCS-551")),
-            pytest.param(*['create_pvc', 'osd'], marks=pytest.mark.polarion_id("OCS-552")),
-            pytest.param(*['create_pod', 'osd'], marks=pytest.mark.polarion_id("OCS-553")),
-            pytest.param(*['run_io', 'osd'], marks=pytest.mark.polarion_id("OCS-549")),
-            pytest.param(*['create_pvc', 'mds'], marks=pytest.mark.polarion_id("OCS-564")),
-            pytest.param(*['create_pod', 'mds'], marks=pytest.mark.polarion_id("OCS-567")),
-            pytest.param(*['run_io', 'mds'], marks=pytest.mark.polarion_id("OCS-556")),
+            pytest.param(
+                *['create_pvc', 'mgr'], marks=pytest.mark.polarion_id("OCS-555")
+            ),
+            pytest.param(
+                *['create_pod', 'mgr'], marks=pytest.mark.polarion_id("OCS-558")
+            ),
+            pytest.param(
+                *['run_io', 'mgr'], marks=pytest.mark.polarion_id("OCS-559")
+            ),
+            pytest.param(
+                *['create_pvc', 'mon'], marks=pytest.mark.polarion_id("OCS-560")
+            ),
+            pytest.param(
+                *['create_pod', 'mon'], marks=pytest.mark.polarion_id("OCS-550")
+            ),
+            pytest.param(
+                *['run_io', 'mon'], marks=pytest.mark.polarion_id("OCS-551")
+            ),
+            pytest.param(
+                *['create_pvc', 'osd'], marks=pytest.mark.polarion_id("OCS-552")
+            ),
+            pytest.param(
+                *['create_pod', 'osd'], marks=pytest.mark.polarion_id("OCS-553")
+            ),
+            pytest.param(
+                *['run_io', 'osd'], marks=pytest.mark.polarion_id("OCS-549")
+            ),
+            pytest.param(
+                *['create_pvc', 'mds'], marks=pytest.mark.polarion_id("OCS-564")
+            ),
+            pytest.param(
+                *['create_pod', 'mds'], marks=pytest.mark.polarion_id("OCS-567")
+            ),
+            pytest.param(
+                *['run_io', 'mds'], marks=pytest.mark.polarion_id("OCS-556")
+            ),
         ]
     )
     def test_disruptive_file(self, operation_to_disrupt, resource_to_delete):
---
+++ tests/manage/test_run_io_multiple_pods.py
@@ -0,0 +1,99 @@
+import pytest
+import logging
+import random
+from ocs_ci.ocs import constants
+
+from ocs_ci.framework.testlib import ManageTest, tier2
+from tests.fixtures import (
+    create_rbd_storageclass, create_ceph_block_pool,
+    create_rbd_secret, create_pods, create_pvcs, create_project,
+    create_cephfs_secret, create_cephfs_storageclass
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture()
+def init_pvc_size(request):
+    """
+    Initialize the PVC size for PVC creation
+    """
+    class_instance = request.node.cls
+    class_instance.pvc_size_int = getattr(
+        class_instance, 'pvc_size_int', random.randint(1, 10)
+    )
+    class_instance.pvc_size = f'{class_instance.pvc_size_int}Gi'
+
+
+@tier2
+@pytest.mark.usefixtures(
+    create_project.__name__,
+    init_pvc_size.__name__,
+)
+class BaseRunIOMultiplePods(ManageTest):
+    """
+    Run IO on multiple pods in parallel
+    """
+    num_of_pvcs = 10
+    pvc_size_int = 5
+    interface = None
+
+    def run_io_multiple_pods(self):
+        """
+        Run IO on multiple pods in parallel
+        """
+
+        for pod in self.pod_objs:
+            pod.run_io('fs', f'{self.pvc_size_int - 1}G')
+
+        for pod in self.pod_objs:
+            fio_result = pod.get_fio_results()
+            logger.info(f"IOPs after FIO for pod {pod.name}:")
+            logger.info(
+                f"Read: {fio_result.get('jobs')[0].get('read').get('iops')}"
+            )
+            logger.info(
+                f"Write: {fio_result.get('jobs')[0].get('write').get('iops')}"
+            )
+
+
+@tier2
+@pytest.mark.usefixtures(
+    create_rbd_secret.__name__,
+    create_ceph_block_pool.__name__,
+    create_rbd_storageclass.__name__,
+    create_pvcs.__name__,
+    create_pods.__name__
+)
+class TestRunIOMultiplePodsRBD(BaseRunIOMultiplePods):
+    """
+    Run IO on multiple pods in parallel - RBD
+    """
+    interface = constants.CEPHBLOCKPOOL
+    storage_type = 'block'
+
+    def test_run_io_multiple_pods_rbd(self):
+        """
+        """
+        self.run_io_multiple_pods()
+
+
+@tier2
+@pytest.mark.usefixtures(
+    create_cephfs_secret.__name__,
+    create_cephfs_storageclass.__name__,
+    create_pvcs.__name__,
+    create_pods.__name__
+)
+class TestRunIOMultiplePodsFS(BaseRunIOMultiplePods):
+    """
+    Run IO on multiple pods in parallel - CephFS
+    """
+    interface = constants.CEPHFILESYSTEM
+    storage_type = 'fs'
+
+    def test_run_io_multiple_pods_fs(self):
+        """
+        """
+        self.run_io_multiple_pods()
