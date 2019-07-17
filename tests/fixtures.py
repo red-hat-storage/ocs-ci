245f52e92a602984f6d446d93c246548fd971464
Elad Ben Aharon <ebenahar@redhat.com>
2019-07-17 20:00:41 +0300

    Run IO multiple pods (#402)
    Add test to run on IO on multiple pods, add/udpate new fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,6 +1,6 @@
 import pytest
 from tests import helpers
-from ocs_ci.ocs import constants
+from ocs_ci.ocs import constants, ocp
 
 
 @pytest.fixture()
@@ -16,6 +16,9 @@ def create_rbd_secret(request):
         """
         if hasattr(class_instance, 'rbd_secret_obj'):
             class_instance.rbd_secret_obj.delete()
+            class_instance.rbd_secret_obj.ocp.wait_for_delete(
+                class_instance.rbd_secret_obj.name
+            )
 
     request.addfinalizer(finalizer)
 
@@ -38,6 +41,9 @@ def create_cephfs_secret(request):
         """
         if hasattr(class_instance, 'cephfs_secret_obj'):
             class_instance.cephfs_secret_obj.delete()
+            class_instance.cephfs_secret_obj.ocp.wait_for_delete(
+                class_instance.cephfs_secret_obj.name
+            )
 
     request.addfinalizer(finalizer)
 
@@ -80,13 +86,16 @@ def create_rbd_storageclass(request):
         """
         if class_instance.sc_obj.get():
             class_instance.sc_obj.delete()
+            class_instance.sc_obj.ocp.wait_for_delete(
+                class_instance.sc_obj.name
+            )
 
     request.addfinalizer(finalizer)
 
     class_instance.sc_obj = helpers.create_storage_class(
         interface_type=constants.CEPHBLOCKPOOL,
         interface_name=class_instance.cbp_obj.name,
-        secret_name=class_instance.rbd_secret_obj.name
+        secret_name=class_instance.rbd_secret_obj.name,
     )
     assert class_instance.sc_obj, "Failed to create storage class"
 
@@ -104,6 +113,9 @@ def create_cephfs_storageclass(request):
         """
         if class_instance.sc_obj.get():
             class_instance.sc_obj.delete()
+            class_instance.sc_obj.ocp.wait_for_delete(
+                class_instance.sc_obj.name
+            )
 
     request.addfinalizer(finalizer)
 
@@ -116,6 +128,34 @@ def create_cephfs_storageclass(request):
 
 
 @pytest.fixture()
+def create_project(request):
+    """
+    Create a new project
+    """
+    class_instance = request.node.cls
+
+    def finalizer():
+        """
+        Delete the project
+        """
+        class_instance.project_obj.delete(resource_name=class_instance.namespace)
+        class_instance.project_obj.wait_for_delete(class_instance.namespace)
+
+    request.addfinalizer(finalizer)
+
+    class_instance.namespace = helpers.create_unique_resource_name(
+        'test', 'namespace'
+    )
+    class_instance.project_obj = ocp.OCP(
+        kind='Project', namespace=class_instance.namespace
+    )
+
+    assert class_instance.project_obj.new_project(class_instance.namespace), (
+        f'Failed to create new project {class_instance.namespace}'
+    )
+
+
+@pytest.fixture()
 def create_pvc(request):
     """
     Create a persistent Volume Claim
@@ -123,7 +163,7 @@ def create_pvc(request):
     class_instance = request.node.cls
 
     class_instance.pvc_obj = helpers.create_pvc(
-        sc_name=class_instance.sc_obj.name
+        sc_name=class_instance.sc_obj.name, namespace=class_instance.namespace
     )
 
 
@@ -152,7 +192,8 @@ def create_rbd_pod(request):
     class_instance = request.node.cls
     class_instance.pod_obj = helpers.create_pod(
         interface_type=constants.CEPHBLOCKPOOL,
-        pvc_name=class_instance.pvc_obj.name
+        pvc_name=class_instance.pvc_obj.name,
+        namespace=class_instance.namespace
     )
 
 
@@ -171,3 +212,57 @@ def delete_pod(request):
             class_instance.pod_obj.delete()
 
     request.addfinalizer(finalizer)
+
+
+@pytest.fixture()
+def create_pvcs(request):
+    """
+    Create multiple PVCs
+    """
+    class_instance = request.node.cls
+
+    def finalizer():
+        """
+        Delete multiple PVCs
+        """
+        if hasattr(class_instance, 'pvc_objs'):
+            for pvc_obj in class_instance.pvc_objs:
+                pvc_obj.delete()
+            for pvc_obj in class_instance.pvc_objs:
+                pvc_obj.ocp.wait_for_delete(pvc_obj.name)
+
+    request.addfinalizer(finalizer)
+
+    class_instance.pvc_objs = helpers.create_multiple_pvcs(
+        sc_name=class_instance.sc_obj.name, number_of_pvc=class_instance.num_of_pvcs,
+        size=class_instance.pvc_size, namespace=class_instance.namespace
+    )
+
+
+@pytest.fixture()
+def create_pods(request):
+    """
+    Create multiple pods
+    """
+    class_instance = request.node.cls
+
+    def finalizer():
+        """
+        Delete multiple pods
+        """
+        if hasattr(class_instance, 'pod_objs'):
+            for pod in class_instance.pod_objs:
+                pod.delete()
+
+    request.addfinalizer(finalizer)
+
+    class_instance.pod_objs = [
+        helpers.create_pod(
+            interface_type=class_instance.interface, pvc_name=pvc_obj.name,
+            wait=False, namespace=class_instance.namespace
+        ) for pvc_obj in class_instance.pvc_objs
+    ]
+    for pod in class_instance.pod_objs:
+        assert helpers.wait_for_resource_state(
+            pod, constants.STATUS_RUNNING
+        ), f"Pod {pod} failed to reach {constants.STATUS_RUNNING}"
