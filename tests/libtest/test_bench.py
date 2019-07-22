61e5349aa95e5202458a5f61830f7d16aeb9a6e8
Shylesh Kumar <shmohan@redhat.com>
2019-07-21 22:21:31 -0700

    Introducing marker libtest for tests which requires cluster and are libraries
    Signed-off-by: Shylesh Kumar <shmohan@redhat.com>

--- tests/libtest/test_bench.py
+++ tests/libtest/test_bench.py
@@ -2,9 +2,11 @@ import os
 os.sys.path.append(os.path.dirname(os.getcwd()))
 
 from ocs_ci.ocs.resources import pod
-from tests import test_radosbench as radosbench
+from ocs_ci.framework.testlib import libtest
+from tests.libtest import test_radosbench as radosbench
 
 
+@libtest
 def test_run():
     tools_pod = pod.get_ceph_tools_pod()
     tools_pod.add_role(role='client')
--- tests/libtest/test_radosbench.py
+++ tests/libtest/test_radosbench.py
@@ -1,5 +1,7 @@
 import logging
 import random
+
+
 from ocs_ci.ocs.exceptions import CommandFailed
 
 log = logging.getLogger(__name__)
