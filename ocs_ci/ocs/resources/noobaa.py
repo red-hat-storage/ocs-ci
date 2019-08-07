19267720c4533ada408a3aecead47e7ed8638f3e
Ben <benq3000@gmail.com>
2019-08-07 14:24:22 +0300

    Fix several things
    Move noobaa.py to ocs/resources/
    Rename ocp_obj2 to secret_ocp_obj to better describe its roll
    Wrap long lines
    Add description to the noobaa_obj method
    Add basic logging to the test
    
    Signed-off-by: Ben <benq3000@gmail.com>
    Signed-off-by: Ben <belimele@redhat.com>

--- ocs_ci/ocs/resources/noobaa.py
+++ ocs_ci/ocs/resources/noobaa.py
@@ -29,11 +29,13 @@ class NooBaa(object):
             results.get('items')[0].get('status').get('accounts')
             .get('admin').get('secretRef').get('name')
         )
-        ocp_obj2 = OCP(kind='secret', namespace='noobaa')
-        results2 = ocp_obj2.get(creds_secret_name)
+        secret_ocp_obj = OCP(kind='secret', namespace='noobaa')
+        results2 = secret_ocp_obj.get(creds_secret_name)
 
-        noobaa_access_key = base64.b64decode(results2.get('data').get('AWS_ACCESS_KEY_ID')).decode('utf-8')
-        noobaa_secret_key = base64.b64decode(results2.get('data').get('AWS_SECRET_ACCESS_KEY')).decode('utf-8')
+        noobaa_access_key = base64.b64decode(
+            results2.get('data').get('AWS_ACCESS_KEY_ID')).decode('utf-8')
+        noobaa_secret_key = base64.b64decode(
+            results2.get('data').get('AWS_SECRET_ACCESS_KEY')).decode('utf-8')
 
         self._ocp_resource = ocp_obj
         self._s3_resource = boto3.resource(
