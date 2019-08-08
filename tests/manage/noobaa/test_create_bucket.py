c9c037cff927d7f719ca3f41c3a5bb17bd92a68c
Ben <belimele@redhat.com>
2019-08-08 14:38:56 +0300

    Add empty lines between Returns and """
    Signed-off-by: Ben <belimele@redhat.com>

--- tests/manage/noobaa/test_create_bucket.py
+++ tests/manage/noobaa/test_create_bucket.py
@@ -16,6 +16,7 @@ def noobaa_obj():
     Returns a NooBaa resource that's connected to the S3 endpoint
     Returns:
         s3_res: A NooBaa resource
+
     """
     s3_res = noobaa.NooBaa()
     return s3_res
