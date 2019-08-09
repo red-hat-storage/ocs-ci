372cf9ee16fd24b4a526fdd69536d69d8d264bbb
Parikshith <pbyregow@redhat.com>
2019-08-09 21:02:53 +0530

    Updating OCS-347 with fixtures, removing global variable and handling the leftover PV (#534)
    * Updating OCS-347 with fixtures, removing global variable and handling the leftover PV
    
    Signed-off-by: Parikshith <pbyregow@redhat.com>
    
    * Added teardown for pod,pvc and sc in fixture
    
    Signed-off-by: Parikshith <pbyregow@redhat.com>

--- tests/manage/storageclass/test_rbd_csi_default_sc.py
+++ tests/manage/storageclass/test_rbd_csi_default_sc.py
@@ -8,53 +8,52 @@ import pytest
 from ocs_ci.framework.testlib import tier1, ManageTest
 from tests import helpers
 from ocs_ci.ocs import constants
+from ocs_ci.ocs.exceptions import ResourceLeftoversException
+from tests.fixtures import (
+    create_ceph_block_pool, create_rbd_secret,
+)
 
 log = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope='class')
-def test_fixture(request):
+@pytest.fixture()
+def resources(request):
     """
-    This is a test fixture
+       Delete the resources created during the test
+       Returns:
+           tuple: empty lists of resources
     """
+    pod, pvc, storageclass = ([] for _ in range(3))
+
     def finalizer():
-        teardown()
+        """
+            Delete the resources created during the test
+            """
+        failed_to_delete = []
+        for resource_type in pod, pvc, storageclass:
+            for resource in resource_type:
+                resource.delete()
+                try:
+                    resource.ocp.wait_for_delete(resource.name)
+                except TimeoutError:
+                    failed_to_delete.append(resource)
+                if resource.kind == constants.PVC:
+                    log.info("Checking whether PV is deleted")
+                    assert helpers.validate_pv_delete(resource.backed_pv)
+        if failed_to_delete:
+            raise ResourceLeftoversException(
+                f"Failed to delete resources: {failed_to_delete}"
+            )
+
     request.addfinalizer(finalizer)
-    setup()
 
-
-def setup():
-    """
-    Setting up the environment - Creating Secret
-    """
-    global SECRET, POOL
-    log.info("Creating RBD Pool")
-    POOL = helpers.create_ceph_block_pool()
-
-    log.info("Creating RBD Secret")
-    SECRET = helpers.create_secret(constants.CEPHBLOCKPOOL)
-
-
-def teardown():
-    """
-    Tearing down the environment
-    """
-    log.info("Deleting PVC")
-    PVC.delete()
-
-    log.info("Deleting StorageClass")
-    STORAGE_CLASS.delete()
-
-    log.info("Deleting Secret")
-    SECRET.delete()
-
-    log.info("Delete RBD Pool")
-    POOL.delete()
+    return pod, pvc, storageclass
 
 
 @tier1
 @pytest.mark.usefixtures(
-    test_fixture.__name__,
+    create_ceph_block_pool.__name__,
+    create_rbd_secret.__name__,
 )
 @pytest.mark.polarion_id("OCS-347")
 class TestBasicPVCOperations(ManageTest):
@@ -63,12 +62,30 @@ class TestBasicPVCOperations(ManageTest):
     with rbd pool
     """
 
-    def test_ocs_347(self):
-        global PVC, STORAGE_CLASS
-        log.info("Creating RBD StorageClass")
-        STORAGE_CLASS = helpers.create_storage_class(
-            constants.CEPHBLOCKPOOL, POOL.name, SECRET.name
-        )
+    def test_ocs_347(self, resources):
+        pod, pvc, storageclass = resources
 
+        log.info("Creating RBD StorageClass")
+        storageclass.append(
+            helpers.create_storage_class(
+                interface_type=constants.CEPHBLOCKPOOL,
+                interface_name=self.cbp_obj.name,
+                secret_name=self.rbd_secret_obj.name,
+            )
+        )
         log.info("Creating a PVC")
-        PVC = helpers.create_pvc(sc_name=STORAGE_CLASS.name)
+        pvc.append(
+            helpers.create_pvc(
+                sc_name=storageclass[0].name, wait=True,
+            )
+        )
+        log.info(
+            f"Creating a pod on with pvc {pvc[0].name}"
+        )
+        pod.append(
+            helpers.create_pod(
+                interface_type=constants.CEPHBLOCKPOOL, pvc_name=pvc[0].name,
+                desired_status=constants.STATUS_RUNNING, wait=True,
+                pod_dict_path=constants.NGINX_POD_YAML
+            )
+        )
