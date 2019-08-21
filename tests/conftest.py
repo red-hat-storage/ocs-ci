bec7b80874e1f0aca4ab3b3eac1728e560831bda
Elad Ben Aharon <ebenahar@redhat.com>
2019-08-21 16:20:34 +0300

    Change PVC disruptive tests to factory
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -406,7 +406,7 @@ def teardown_factory(request):
         """
         Delete the resources created in the test
         """
-        for instance in instances:
+        for instance in instances[::-1]:
             if not instance.is_deleted:
                 instance.delete()
                 instance.ocp.wait_for_delete(
