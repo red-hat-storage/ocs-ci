95ac57c613cef6681350d6abd2410cca48d45581
Shreekar <sshreeka@redhat.com>
2019-08-22 19:39:44 +0530

    tests/manage/pv_services/test_raw_block_pv.py  - Updated test to use configure reclaim policy  - Updated teardown code to use teardown_factory()  - Updated IO results
    Signed-off-by: Shreekar <sshreeka@redhat.com>

--- tests/manage/pv_services/test_raw_block_pv.py
+++ tests/manage/pv_services/test_raw_block_pv.py
@@ -2,151 +2,103 @@ import logging
 import random
 from concurrent.futures import ThreadPoolExecutor
 import pytest
+from ocs_ci.ocs.resources.pod import get_fio_rw_iops
 
 from ocs_ci.framework.testlib import tier1, ManageTest
-from ocs_ci.ocs.exceptions import ResourceLeftoversException
-from ocs_ci.ocs import constants, defaults, ocp
+from ocs_ci.ocs import constants
 
 from tests.fixtures import (
     create_ceph_block_pool,
     create_rbd_secret,
-    create_project
+    create_project,
+    create_rbd_storageclass
 )
 from tests import helpers
 
 log = logging.getLogger(__name__)
 
 
-@pytest.fixture()
-def resources(request):
-    """
-       Delete the resources created during the test
-       Returns:
-           tuple: empty lists of resources
-       """
-    pods, pvcs, storageclass, rp = ([] for _ in range(4))
-
-    def finalizer():
-        """
-            Delete the resources created during the test
-            """
-        failed_to_delete = []
-        for pod_list in pods:
-            for pod in pod_list:
-                log.info(pod.delete())
-                try:
-                    pod.ocp.wait_for_delete(pod.name)
-                except TimeoutError:
-                    failed_to_delete.append(pod)
-
-        for pvc in pvcs:
-            log.info(pvc.delete())
-            if rp[0] == 'Delete':
-                helpers.validate_pv_delete(pvc.backed_pv)
-            else:
-                ocp_pv_obj = ocp.OCP(
-                    kind=constants.PV,
-                    namespace=defaults.ROOK_CLUSTER_NAMESPACE
-                )
-                ocp_pv_obj.delete(resource_name=pvc.backed_pv)
-            try:
-                pvc.ocp.wait_for_delete(pvc.name)
-            except TimeoutError:
-                failed_to_delete.append(pvc)
-        for sc in storageclass:
-            log.info(sc.delete())
-            try:
-                sc.ocp.wait_for_delete(sc.name)
-            except TimeoutError:
-                failed_to_delete.append(sc)
-
-        if failed_to_delete:
-            raise ResourceLeftoversException(
-                f"Failed to delete resources: {failed_to_delete}"
-            )
-
-    request.addfinalizer(finalizer)
-
-    return pods, pvcs, storageclass, rp
-
-
 @tier1
 @pytest.mark.usefixtures(
     create_project.__name__,
-    create_rbd_secret.__name__,
-    create_ceph_block_pool.__name__,
 )
-@pytest.mark.parametrize(
-    argnames=["reclaim_policy"],
-    argvalues=[
-        pytest.param(
-            *[constants.RECLAIM_POLICY_DELETE],
-            marks=pytest.mark.polarion_id("OCS-750")
-        ),
-        pytest.param(
-            *[constants.RECLAIM_POLICY_RETAIN],
-            marks=pytest.mark.polarion_id("OCS-751"))
-    ]
-)
-class TestRawBlockPV(ManageTest):
+class BaseRawBlockPV(ManageTest):
+    """
+    Base class for creating pvc,pods and run IOs
+    """
 
-    def test_raw_block_pv(self, reclaim_policy, resources):
+    def raw_block_pv(self):
         """
         Testing basic creation of app pod with RBD RWX raw block pv support
         """
-        pods, pvcs, storageclass, rp = resources
-        rp.append(reclaim_policy)
-
-        storageclass.append(
-            helpers.create_storage_class(
-                interface_type=constants.CEPHBLOCKPOOL,
-                interface_name=self.cbp_obj.name,
-                secret_name=self.rbd_secret_obj.name,
-                reclaim_policy=reclaim_policy
-            )
-        )
-
+        worker_nodes = helpers.get_worker_nodes()
         pvc_mb = helpers.create_pvc(
-            sc_name=storageclass[0].name, size='500Mi',
+            sc_name=self.sc_obj.name, size='500Mi',
             access_mode=constants.ACCESS_MODE_RWX,
-            wait=True, namespace=self.namespace,
+            namespace=self.namespace,
             volume_mode='Block'
         )
 
         pvc_gb = helpers.create_pvc(
-            sc_name=storageclass[0].name, size='10Gi',
+            sc_name=self.sc_obj.name, size='10Gi',
             access_mode=constants.ACCESS_MODE_RWX,
-            wait=True, namespace=self.namespace,
+            namespace=self.namespace,
             volume_mode='Block'
         )
 
         pvc_tb = helpers.create_pvc(
-            sc_name=storageclass[0].name, size='1Ti',
+            sc_name=self.sc_obj.name, size='1Ti',
             access_mode=constants.ACCESS_MODE_RWX,
-            wait=True, namespace=self.namespace,
+            namespace=self.namespace,
             volume_mode='Block'
         )
 
-        pvcs.extend([pvc_mb, pvc_gb, pvc_tb])
+        pvcs = [pvc_mb, pvc_gb, pvc_tb]
+
+        for pvc in pvcs:
+            helpers.wait_for_resource_state(
+                resource=pvc, state=constants.STATUS_BOUND)
+
+        pvs = [pvc.backed_pv_obj for pvc in pvcs]
+
+        pod_dict = constants.CSI_RBD_RAW_BLOCK_POD_YAML
         pvc_mb_pods = [(helpers.create_pod(
-            interface_type=constants.CEPHBLOCKPOOL, pvc_name=pvc_mb.name,
-            wait=True, namespace=self.namespace,
-            pod_dict_path=constants.CSI_RBD_RAW_BLOCK_POD_YAML
-        )) for _ in range(3)]
+            interface_type=constants.CEPHBLOCKPOOL,
+            pvc_name=pvc_mb.name,
+            namespace=self.namespace,
+            raw_block_pv=True,
+            pod_dict_path=pod_dict,
+            node_name=random.choice(
+                worker_nodes))) for _ in range(3)
+        ]
 
         pvc_gb_pods = [(helpers.create_pod(
-            interface_type=constants.CEPHBLOCKPOOL, pvc_name=pvc_gb.name,
-            wait=True, namespace=self.namespace,
-            pod_dict_path=constants.CSI_RBD_RAW_BLOCK_POD_YAML
-        ))for _ in range(3)]
+            interface_type=constants.CEPHBLOCKPOOL,
+            pvc_name=pvc_gb.name,
+            namespace=self.namespace,
+            raw_block_pv=True,
+            pod_dict_path=pod_dict,
+            node_name=random.choice(
+                worker_nodes))) for _ in range(3)
+        ]
 
         pvc_tb_pods = [(helpers.create_pod(
-            interface_type=constants.CEPHBLOCKPOOL, pvc_name=pvc_tb.name,
-            wait=True, namespace=self.namespace,
-            pod_dict_path=constants.CSI_RBD_RAW_BLOCK_POD_YAML
-        ))for _ in range(3)]
+            interface_type=constants.CEPHBLOCKPOOL,
+            pvc_name=pvc_tb.name,
+            namespace=self.namespace,
+            raw_block_pv=True,
+            pod_dict_path=pod_dict,
+            node_name=random.choice(
+                worker_nodes))) for _ in range(3)
+        ]
 
-        pods.extend([pvc_mb_pods, pvc_gb_pods, pvc_tb_pods])
+        def flatten(l):
+            return [item for sublist in l for item in sublist]
+        pods = [pvc_mb_pods, pvc_gb_pods, pvc_tb_pods]
+        pods = flatten(pods)
+        for pod in pods:
+            helpers.wait_for_resource_state(
+                resource=pod, state=constants.STATUS_RUNNING, timeout=120)
         storage_type = 'block'
 
         with ThreadPoolExecutor() as p:
@@ -165,3 +117,40 @@ class TestRawBlockPV(ManageTest):
                 p.submit(
                     pod.run_io, storage_type=storage_type, size=f'{random.randint(10,15)}G',
                 )
+
+        for pod in pods:
+            get_fio_rw_iops(pod)
+        return pods, pvcs, pvs
+
+
+@tier1
+@pytest.mark.polarion_id("OCS-750")
+@pytest.mark.usefixtures(
+    create_rbd_secret.__name__,
+    create_ceph_block_pool.__name__,
+    create_rbd_storageclass.__name__,
+)
+class TestRawBlockPVRetain(BaseRawBlockPV):
+    reclaim_policy = constants.RECLAIM_POLICY_RETAIN
+
+    def test_raw_block_pv(self, teardown_factory):
+        pods, pvcs, pvs = self.raw_block_pv()
+        teardown_factory(pvs)
+        teardown_factory(pvcs)
+        teardown_factory(pods)
+
+
+@tier1
+@pytest.mark.polarion_id("OCS-751")
+@pytest.mark.usefixtures(
+    create_rbd_secret.__name__,
+    create_ceph_block_pool.__name__,
+    create_rbd_storageclass.__name__,
+)
+class TestRawBlockPVDelete(BaseRawBlockPV):
+    reclaim_policy = constants.RECLAIM_POLICY_DELETE
+
+    def test_raw_block_pv(self, teardown_factory):
+        pods, pvcs, _ = self.raw_block_pv()
+        teardown_factory(pvcs)
+        teardown_factory(pods)
