f85677b281f1842927c7f0c7ead6b5bd2cb05dc8
Parikshith <pbyregow@redhat.com>
2019-08-24 00:49:31 +0530

    Test to create, delete and list multiple s3 buckets (#595)
    Signed-off-by: Parikshith <pbyregow@redhat.com>

---
+++ tests/manage/noobaa/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+from ocs_ci.ocs.resources import noobaa
+
+
+@pytest.fixture()
+def noobaa_obj():
+    """
+    Returns a NooBaa resource that's connected to the S3 endpoint
+
+    Returns:
+        s3_res: A NooBaa resource
+
+    """
+    s3_res = noobaa.NooBaa()
+    return s3_res
---
+++ tests/manage/noobaa/test_bucket_deletion.py
@@ -0,0 +1,61 @@
+import logging
+import pytest
+
+from tests.helpers import create_unique_resource_name
+from ocs_ci.framework import config
+from ocs_ci.framework.pytest_customization.marks import tier1
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture()
+def create_buckets(request, noobaa_obj):
+    """
+    Creates multiple buckets
+    """
+    created_buckets = []
+
+    def verify_bucket():
+        """
+        Verifies whether buckets exists after deletion
+
+        """
+        for bucket in created_buckets:
+            logger.info(f"Verifying whether bucket: {bucket.name} exists"
+                        f" after deletion")
+            assert noobaa_obj.s3_verify_bucket_exists(bucket) is False
+
+    request.addfinalizer(verify_bucket)
+
+    bucket_name = create_unique_resource_name(
+        resource_description='bucket', resource_type='s3'
+    )
+    logger.info(f'Creating bucket: {bucket_name}')
+    created_buckets.append(
+        noobaa_obj.s3_create_bucket(bucketname=bucket_name)
+    )
+
+    return created_buckets
+
+
+@pytest.mark.skipif(condition=True, reason="NooBaa is not deployed")
+@pytest.mark.filterwarnings(
+    'ignore::urllib3.exceptions.InsecureRequestWarning'
+)
+@tier1
+class TestBucketDeletion:
+    """
+    Test bucket Creation Deletion of buckets
+    """
+
+    @pytest.mark.skipif(
+        condition=config.ENV_DATA['platform'] != 'AWS',
+        reason="Tests are not running on AWS deployed cluster"
+    )
+    def test_s3_bucket_delete(self, noobaa_obj, create_buckets):
+        """
+        Test deletion of bucket using the S3 SDK
+        """
+        for bucket in create_buckets:
+            logger.info(f"Deleting bucket: {bucket.name}")
+            noobaa_obj.s3_delete_bucket(bucket)
