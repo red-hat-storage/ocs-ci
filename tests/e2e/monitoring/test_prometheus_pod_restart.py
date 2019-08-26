6dd8d4e2a468d47f77b08118d4dea43b8e16efd9
Akarsha <akrai@redhat.com>
2019-08-26 14:20:06 +0530

    Addressing review comments
    Signed-off-by: Akarsha <akrai@redhat.com>

--- tests/e2e/monitoring/test_prometheus_pod_restart.py
+++ tests/e2e/monitoring/test_prometheus_pod_restart.py
@@ -4,149 +4,106 @@ import pytest
 
 from ocs_ci.ocs import constants, ocp, defaults
 from ocs_ci.framework.testlib import tier4, E2ETest
-from tests.fixtures import (
-    create_rbd_storageclass, create_ceph_block_pool,
-    create_rbd_secret
-)
-from tests.helpers import create_pvc, create_pod, create_unique_resource_name
+from tests import helpers
 from ocs_ci.ocs.monitoring import (
-    collected_metrics_for_created_pvc,
-    check_used_size_of_prometheus_pod
-)
+    check_pvcdata_collected_on_prometheus,)
 from ocs_ci.ocs.resources import pvc, pod
 
 logger = logging.getLogger(__name__)
 
 
 @pytest.fixture()
-def test_fixture(request):
+def test_fixture(request, storageclass_factory):
     """
     Setup and teardown
-
-    """
-    self = request.node.cls
-
-    def finalizer():
-        teardown(self)
-    request.addfinalizer(finalizer)
-    setup(self)
-
-
-def setup(self):
-    """
-    Create projects, pvcs and app pods
-
     """
 
-    # Initializing
-    self.namespace_list = []
-    self.pvc_objs = []
-    self.pod_objs = []
+    def teardown():
 
-    assert create_project_and_pvc_and_check_metrics_are_collected(self)
+        # Delete created app pods and pvcs
+        assert pod.delete_pods(pod_objs)
+        assert pvc.delete_pvcs(pvc_objs)
 
+        # Switch to default project
+        ret = ocp.switch_to_default_rook_cluster_project()
+        assert ret, 'Failed to switch to default rook cluster project'
 
-def teardown(self):
-    """
-    Delete app pods and PVCs
-    Delete project
+        # Delete created projects
+        for prj in namespace_list:
+            prj.delete(resource_name=prj.namespace)
 
-    """
-    # Delete created app pods and PVCs
-    assert pod.delete_pods(self.pod_objs)
-    assert pvc.delete_pvcs(self.pvc_objs)
+    request.addfinalizer(teardown)
 
-    # Switch to default project
-    ret = ocp.switch_to_default_rook_cluster_project()
-    assert ret, 'Failed to switch to default rook cluster project'
+    # Create a storage class
+    sc = storageclass_factory()
 
-    # Delete projects created
-    for prj in self.namespace_list:
-        prj_obj = ocp.OCP(kind='Project', namespace=prj)
-        prj_obj.delete(resource_name=prj)
+    # Create projects
+    namespace_list = helpers.create_multilpe_projects(number_of_project=2)
 
+    # Create pvcs
+    pvc_objs = [helpers.create_pvc(
+        sc_name=sc.name, namespace=each_namespace.namespace
+    ) for each_namespace in namespace_list]
+    for pvc_obj in pvc_objs:
+        helpers.wait_for_resource_state(pvc_obj, constants.STATUS_BOUND)
+        pvc_obj.reload()
 
-def create_project_and_pvc_and_check_metrics_are_collected(self):
-    """
-    Creates projects, pvcs and app pods
-
-    """
-
-    # Create new project
-    self.namespace = create_unique_resource_name('test', 'namespace')
-    self.project_obj = ocp.OCP(kind='Project', namespace=self.namespace)
-    assert self.project_obj.new_project(self.namespace), (
-        f'Failed to create new project {self.namespace}'
-    )
-
-    # Create PVCs
-    self.pvc_obj = create_pvc(
-        sc_name=self.sc_obj.name, namespace=self.namespace
-    )
-
-    # Create pod
-    self.pod_obj = create_pod(
+    # Create app pods
+    pod_objs = [helpers.create_pod(
         interface_type=constants.CEPHBLOCKPOOL,
-        pvc_name=self.pvc_obj.name, namespace=self.namespace
-    )
+        pvc_name=each_pvc.name, namespace=each_pvc.namespace
+    ) for each_pvc in pvc_objs]
+    for pod_obj in pod_objs:
+        helpers.wait_for_resource_state(pod_obj, constants.STATUS_RUNNING)
+        pod_obj.reload()
 
-    self.namespace_list.append(self.namespace)
-    self.pvc_objs.append(self.pvc_obj)
-    self.pod_objs.append(self.pod_obj)
-
-    # Check for the created pvc metrics is collected
-    for pvc_obj in self.pvc_objs:
-        assert collected_metrics_for_created_pvc(pvc_obj.name), (
+    # Check for the created pvc metrics on prometheus pod
+    for pvc_obj in pvc_objs:
+        assert check_pvcdata_collected_on_prometheus(pvc_obj.name), (
             f"On prometheus pod for created pvc {pvc_obj.name} related data is not collected"
         )
-    return True
+
+    return namespace_list, pvc_objs, pod_objs, sc
 
 
-@pytest.mark.usefixtures(
-    create_rbd_secret.__name__,
-    create_ceph_block_pool.__name__,
-    create_rbd_storageclass.__name__,
-    test_fixture.__name__
-)
 @pytest.mark.polarion_id("OCS-576")
-class TestRespinPrometheusPod(E2ETest):
+class TestPrometheusPodRestart(E2ETest):
     """
     Prometheus pod restart should not have any functional impact,
     i.e the data/metrics shouldn't be lost after the restart of prometheus pod.
     """
 
     @tier4
-    def test_respinning_ceph_pods_and_interaction_with_prometheus_pod(self):
+    def test_monitoring_after_restarting_prometheus_pod(self, test_fixture):
         """
         Test case to validate prometheus pod restart
         should not have any functional impact
         """
+        namespace_list, pvc_objs, pod_objs, sc = test_fixture
 
         # Get the prometheus pod
         pod_obj = pod.get_all_pods(namespace=defaults.OCS_MONITORING_NAMESPACE, selector=['prometheus'])
 
-        # Get the pvc which mounted on prometheus[0] pod
-        pod_info = pod_obj[0].get()
-        pvc_name = pod_info['spec']['volumes'][0]['persistentVolumeClaim']['claimName']
+        for pod_object in pod_obj:
+            # Get the pvc which mounted on prometheus pod
+            pod_info = pod_object.get()
+            pvc_name = pod_info['spec']['volumes'][0]['persistentVolumeClaim']['claimName']
 
-        # Check the used space of prometheus[0] pod
-        initial_used_size = check_used_size_of_prometheus_pod(pod_obj[0])
+            # Restart the prometheus pod
+            pod_object.delete(force=True)
+            POD = ocp.OCP(kind=constants.POD, namespace=defaults.OCS_MONITORING_NAMESPACE)
+            assert POD.wait_for_resource(
+                condition='Running', selector=f'app=prometheus', timeout=60
+            )
 
-        # Respin one of the prometheus pod
-        pod_obj[0].delete(force=True)
-        POD = ocp.OCP(kind=constants.POD, namespace=defaults.OCS_MONITORING_NAMESPACE)
-        assert POD.wait_for_resource(
-            condition='Running', selector=f'app=prometheus', timeout=300
-        )
+            # Check the same pvc is mounted on new pod
+            pod_info = pod_object.get()
+            assert pod_info['spec']['volumes'][0]['persistentVolumeClaim']['claimName'] in pvc_name, (
+                f"Old pvc not found after restarting the prometheus pod {pod_object.name}"
+            )
 
-        # Check the same pvc is mounted on new pod
-        pod_info = pod_obj[0].get()
-        assert pod_info['spec']['volumes'][0]['persistentVolumeClaim']['claimName'] in pvc_name
-
-        # Check the used space of prometheus[0] pod should be equal or increased after respinning pod
-        # i.e, the data/metrics should not be deleted which was collected before
-        used_size_after_respin = check_used_size_of_prometheus_pod(pod_obj[0])
-        assert used_size_after_respin >= initial_used_size
-
-        # Check for the created pvc metrics after respinning ceph pods
-        assert create_project_and_pvc_and_check_metrics_are_collected(self)
+        # Check for the created pvc metrics are present after restarting prometheus pod
+        for pvc_obj in pvc_objs:
+            assert check_pvcdata_collected_on_prometheus(pvc_obj.name), (
+                f"On prometheus pod for created pvc {pvc_obj.name} related data is not collected"
+            )
