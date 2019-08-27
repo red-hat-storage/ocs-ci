4b4c6f4c909d18adffc1639dfb841c19007153d2
Sidhant Agrawal <sagrawal@redhat.com>
2019-08-27 20:04:32 +0530

    Update test_pvc_assign_pod_node.py - Use pod.verify_node_name for checking node - Remove parametrize from test_rwx_pvc_assign_pod_node() - Minor Optimizations
    Update pod.py
    - improved logging message in verify_node_name
    
    Signed-off-by: Sidhant Agrawal <sagrawal@redhat.com>

--- tests/manage/pv_services/test_pvc_assign_pod_node.py
+++ tests/manage/pv_services/test_pvc_assign_pod_node.py
@@ -11,27 +11,6 @@ from tests import helpers
 logger = logging.getLogger(__name__)
 
 
-def verify_pod_node(pod_obj, node_name):
-    """
-    Verifies that the pod is running on a particular node
-
-    Args:
-        pod_obj (Pod): The pod object
-        node_name (str): The name of node to check
-
-    Returns:
-        bool: True if the pod is running on a particular node, False otherwise
-    """
-
-    logger.info(f"Checking whether the pod is running on node: {node_name}")
-    actual_node = pod_obj.get().get('spec').get('nodeName')
-    logger.info(f"The pod is running on node: {actual_node}")
-    if actual_node == node_name:
-        return True
-    else:
-        return False
-
-
 class TestPvcAssignPodNode(ManageTest):
     """
     Automates the following test cases:
@@ -86,7 +65,7 @@ class TestPvcAssignPodNode(ManageTest):
             resource=pod_obj, state=constants.STATUS_RUNNING, timeout=120
         )
         pod_obj.reload()
-        assert verify_pod_node(pod_obj, selected_node), (
+        assert pod.verify_node_name(pod_obj, selected_node), (
             'Pod is running on a different node than the selected node'
         )
 
@@ -96,21 +75,12 @@ class TestPvcAssignPodNode(ManageTest):
         pod.get_fio_rw_iops(pod_obj)
 
     @tier1
-    @pytest.mark.parametrize(
-        argnames=["interface"],
-        argvalues=[
-            pytest.param(
-                *[constants.CEPHFILESYSTEM],
-                marks=pytest.mark.polarion_id("OCS-1258")
-            )
-        ]
-    )
-    def test_rwx_pvc_assign_pod_node(
-        self, interface, pvc_factory, teardown_factory
-    ):
+    @pytest.mark.polarion_id("OCS-1258")
+    def test_rwx_pvc_assign_pod_node(self, pvc_factory, teardown_factory):
         """
         Test assign nodeName to a pod using RWX pvc
         """
+        interface = constants.CEPHFILESYSTEM
         worker_nodes_list = helpers.get_worker_nodes()
 
         # Create a RWX PVC
@@ -120,57 +90,45 @@ class TestPvcAssignPodNode(ManageTest):
         )
 
         # Create two pods on selected nodes
+        pod_list = []
         selected_nodes = random.sample(worker_nodes_list, k=2)
-
         logger.info(
-            f"Creating first pod on node: {selected_nodes[0]} "
-            f"with pvc {pvc_obj.name}"
+            f"Creating {len(selected_nodes)} pods with pvc {pvc_obj.name}"
         )
-        pod_obj1 = helpers.create_pod(
-            interface_type=interface, pvc_name=pvc_obj.name,
-            namespace=pvc_obj.namespace, node_name=selected_nodes[0],
-            pod_dict_path=constants.NGINX_POD_YAML
-        )
-        teardown_factory(pod_obj1)
-
-        logger.info(
-            f"Creating second pod on node: {selected_nodes[1]} "
-            f"with pvc {pvc_obj.name}"
-        )
-        pod_obj2 = helpers.create_pod(
-            interface_type=interface, pvc_name=pvc_obj.name,
-            namespace=pvc_obj.namespace, node_name=selected_nodes[1],
-            pod_dict_path=constants.NGINX_POD_YAML
-        )
-        teardown_factory(pod_obj2)
+        for node in selected_nodes:
+            logger.info(f"Creating pod on node: {node}")
+            pod_obj = helpers.create_pod(
+                interface_type=interface, pvc_name=pvc_obj.name,
+                namespace=pvc_obj.namespace, node_name=node,
+                pod_dict_path=constants.NGINX_POD_YAML
+            )
+            pod_list.append(pod_obj)
+            teardown_factory(pod_obj)
 
         # Confirm that both pods are running on the selected_nodes
-        helpers.wait_for_resource_state(
-            resource=pod_obj1, state=constants.STATUS_RUNNING, timeout=120
-        )
-        pod_obj1.reload()
-        assert verify_pod_node(pod_obj1, selected_nodes[0]), (
-            'First Pod is running on a different node than the selected node'
-        )
+        logger.info('Checking whether pods are running on the selected nodes')
+        for index in range(0, len(selected_nodes)):
+            pod_obj = pod_list[index]
+            selected_node = selected_nodes[index]
+            helpers.wait_for_resource_state(
+                resource=pod_obj, state=constants.STATUS_RUNNING,
+                timeout=120
+            )
+            pod_obj.reload()
+            assert pod.verify_node_name(pod_obj, selected_node), (
+                f"Pod {pod_obj.name} is running on a different node "
+                f"than the selected node"
+            )
 
-        helpers.wait_for_resource_state(
-            resource=pod_obj2, state=constants.STATUS_RUNNING, timeout=120
-        )
-        pod_obj2.reload()
-        assert verify_pod_node(pod_obj2, selected_nodes[1]), (
-            'Second Pod is running on a different node than the selected node'
-        )
-
-        # Run IOs
+        # Run IOs on all pods. FIO Filename is kept same as pod name
         with ThreadPoolExecutor() as p:
-            logger.info(f"Running IO on first pod {pod_obj1.name}")
-            p.submit(
-                pod_obj1.run_io, storage_type='fs', size='512M', runtime=30
-            )
-            logger.info(f"Running IO on second pod {pod_obj2.name}")
-            p.submit(
-                pod_obj2.run_io, storage_type='fs', size='512M', runtime=30
-            )
+            for pod_obj in pod_list:
+                logger.info(f"Running IO on pod {pod_obj.name}")
+                p.submit(
+                    pod_obj.run_io, storage_type='fs', size='512M',
+                    runtime=30, fio_filename=pod_obj.name
+                )
 
-        pod.get_fio_rw_iops(pod_obj1)
-        pod.get_fio_rw_iops(pod_obj2)
+        # Check IO from all pods
+        for pod_obj in pod_list:
+            pod.get_fio_rw_iops(pod_obj)
