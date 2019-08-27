8ca4286a6656fd9b7269d50b5b6fa47972a9eb8d
Shrivaibavi Raghaventhiran <sraghave@redhat.com>
2019-08-27 19:23:42 +0530

    Fixing uninstall openshift-logging
    Signed-off-by: Shrivaibavi Raghaventhiran <sraghave@redhat.com>

--- ocs_ci/utility/uninstall_openshift_logging.py
+++ ocs_ci/utility/uninstall_openshift_logging.py
@@ -5,8 +5,24 @@ import logging
 
 from ocs_ci.ocs import constants, ocp
 from ocs_ci.ocs.resources.pod import get_all_pods, verify_pod_exists
-
+from ocs_ci.ocs.exceptions import UnexpectedBehaviour
+from ocs_ci.utility.retry import retry
 logger = logging.getLogger(__name__)
+
+
+@retry(UnexpectedBehaviour, 5, 30, 2)
+def check_pod_vanished(pod_list):
+    # for pod in pod_list:
+    #     if not verify_pod_exists(
+    #         namespace='openshift-logging', pod_name=pod.name
+    #     ):
+    #         return
+    #     logger.info(f"The pod {pod.name} still exists, Retrying ... ")
+    #     raise UnexpectedBehaviour
+    pod_list = get_all_pods(namespace='openshift-logging')
+    if pod_list:
+        return
+    raise UnexpectedBehaviour
 
 
 def uninstall_cluster_logging():
@@ -22,20 +38,19 @@ def uninstall_cluster_logging():
 
     # Deleting the clusterlogging instance
     clusterlogging_obj = ocp.OCP(
-        kind='ClusterLogging', namespace='openshift-logging'
+        kind=constants.CLUSTER_LOGGING, namespace='openshift-logging'
     )
     assert clusterlogging_obj.delete(resource_name='instance')
 
     # Verifying the pods if exists after deleting instance
+    pod_obj = ocp.OCP(
+        kind=constants.POD, namespace='openshift-logging'
+    )
     pod_list = [
         pod for pod in pod_list if not pod.name.startswith('cluster-logging-operator')
     ]
-    for pod in pod_list:
-        ocp.OCP.wait_for_delete(resource_name=pod, timeout=10, sleep=3)
-        verify_pod_exists(
-            namespace='openshift-logging', pod_name=pod.name
-        )
 
+    check_pod_vanished(pod_list)
     # Deleting the projects
     openshift_logging_namespace = ocp.OCP(
         kind=constants.NAMESPACES, namespace='openshift-logging'
@@ -50,5 +65,3 @@ def uninstall_cluster_logging():
     if openshift_operators_redhat_namespace.get():
         assert openshift_operators_redhat_namespace.delete(resource_name='openshift-operators-redhat')
         logger.info(f"The project openshift-opertors-redhat got deleted successfully")
-    else:
-        logger.error("The project does not exists")
