7d8b265fdfada5a3f80217124276324070cd348a
Filip Balak <fbalak@redhat.com>
2019-08-27 17:05:45 +0200

    add test_ceph_monitor_stopped testcase
    Signed-off-by: Filip Balak <fbalak@redhat.com>

--- tests/manage/monitoring/prometheus/test_deployment_status.py
+++ tests/manage/monitoring/prometheus/test_deployment_status.py
@@ -29,3 +29,44 @@ def test_ceph_manager_stopped(workload_stop_ceph_mgr):
         label=target_label,
         measure_end_time=workload_stop_ceph_mgr.get('stop')
     )
+
+
+@tier4
+@pytest.mark.polarion_id("OCS-904")
+def test_ceph_monitor_stopped(workload_stop_ceph_mon):
+    """
+    Test that there is appropriate alert related to ceph monitor quorum
+    when there is even number of ceph monitors and that this alert
+    is cleared when monitors are back online.
+    """
+    api = prometheus.PrometheusAPI()
+
+    # get alerts from time when manager deployment was scaled down
+    alerts = workload_stop_ceph_mon.get('prometheus_alerts')
+    target_label = 'CephMonQuorumAtRisk'
+    target_msg = 'Storage quorum at risk'
+    for target_label, target_msg, target_states, target_severity in [
+        (
+            'CephMonQuorumAtRisk',
+            'Storage quorum at risk',
+            ['pending'],
+            'error'
+        ),
+        (
+            'CephClusterWarningState',
+            'Storage cluster is in degraded state',
+            ['pending', 'firing'],
+            'warning'
+        )
+    ]:
+        prometheus.check_alert_list(
+            label=target_label,
+            msg=target_msg,
+            alerts=alerts,
+            states=target_states,
+            severity=target_severity
+        )
+        api.check_alert_cleared(
+            label=target_label,
+            measure_end_time=workload_stop_ceph_mon.get('stop')
+        )
