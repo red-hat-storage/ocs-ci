00b6c2c1c70b7dae2061c3dccf7b3f146a7839d5
Prasad Desala <tdesala@redhat.com>
2019-08-29 23:04:26 +0530

    Fixed Comments
    Signed-off-by: Prasad Desala <tdesala@redhat.com>

--- ocs_ci/ocs/machine.py
+++ ocs_ci/ocs/machine.py
@@ -2,6 +2,7 @@ import re
 import logging
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs.resources.ocs import OCS
+from ocs_ci.ocs import constants
 
 
 log = logging.getLogger(__name__)
@@ -12,13 +13,15 @@ def get_machine_objs(machine_names=None):
     Get machine objects by machine names
 
     Args:
-        machine_names (list): The machine names to get their objects for.
+        machine_names (list): The machine names to get their objects
                               If None, will return all cluster machines
 
     Returns:
         list: Cluster machine OCP objects
     """
-    machines_obj = OCP(kind='machine', namespace='openshift-machine-api')
+    machines_obj = OCP(
+        kind='Machine', namespace=constants.OPENSHIFT_MACHINE_API_NAMESPACE
+    )
     machine_dicts = machines_obj.get()['items']
     if not machine_names:
         return [OCS(**machines_obj) for machines_obj in machine_dicts]
@@ -30,7 +33,7 @@ def get_machine_objs(machine_names=None):
         ]
 
 
-def get_machines(machine_type='worker'):
+def get_machines(machine_type=constants.WORKER_MACHINE):
     """
     Get cluster's machines according to the machine type (e.g. worker, master)
 
@@ -61,14 +64,37 @@ def delete_machine(machine_name):
     Raises:
         CommandFailed: In case yaml_file and resource_name wasn't provided
     """
-    machine_obj = OCP(kind='machine', namespace='openshift-machine-api')
+    machine_obj = OCP(
+        kind='machine', namespace=constants.OPENSHIFT_MACHINE_API_NAMESPACE
+    )
+    log.info(f"Deleting machine {machine_name}")
     return machine_obj.delete(resource_name=machine_name)
 
 
-def delete_worker_and_check_state_of_new_spinned_machine(machine_name):
+def get_machine_type(machine_name):
     """
-    Deletes a worker machine and checks the state of the newly spinned
-    worker machine
+    Get the machine type (e.g. worker, master)
+
+    Args:
+        machine_name (str): Name of the machine
+
+    Returns:
+        str: Type of the machine
+    """
+    machines_obj = get_machine_objs()
+    for machine in machines_obj:
+        if machine.get().get('metadata').get('name') == machine_name:
+            machine_type = machine.get().get('metadata').get(
+                'labels').get('machine.openshift.io/cluster-api-machine-role')
+            log.info(f"{machine_name} is a {machine_type} type")
+            return machine_type
+        break
+
+
+def delete_machine_and_check_state_of_new_spinned_machine(machine_name):
+    """
+    Deletes a machine and checks the state of the newly spinned
+    machine
 
     Args:
         machine_name (str): Name of the machine you want to delete
@@ -79,19 +105,23 @@ def delete_worker_and_check_state_of_new_spinned_machine(machine_name):
     assert delete_machine(machine_name), (
         f"Failed to delete machine {machine_name}"
     )
-    machines = get_machines(machine_type='worker')
+    if get_machine_type(machine_name) == constants.MASTER_MACHINE:
+        machines = get_machines(machine_type=constants.MASTER_MACHINE)
+    else:
+        machines = get_machines(machine_type=constants.WORKER_MACHINE)
     for machine in machines:
         if re.match(machine.name[:-6], machine_name):
-            log.info(f"Worker machine {machine_name.name} spinned new pod")
+            log.info(f"New spinned machine name is {machine.name}")
             new_machine = machine
             break
     if new_machine is not None:
         log.info(
-            f"Checking the state of new spinned worker {new_machine.name}"
+            f"Checking the state of new spinned machine {new_machine.name}"
         )
         state = new_machine.get().get(
             'metadata').get('annotations').get(
             'machine.openshift.io/instance-state'
         )
-        return state == 'running'
+        log.info(f"{new_machine.name} is in {state} state")
+        return state == constants.STATUS_RUNNING.islower()
     return False
