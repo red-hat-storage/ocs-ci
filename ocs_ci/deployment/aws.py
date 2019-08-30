d622dfedc8b86f29dc413ccab58cb815f94b9901
Shylesh Kumar <shmohan@redhat.com>
2019-08-30 15:31:15 -0700

    Execution of aws_upi_install script
    Signed-off-by: Shylesh Kumar <shmohan@redhat.com>

--- ocs_ci/deployment/aws.py
+++ ocs_ci/deployment/aws.py
@@ -5,7 +5,12 @@ on AWS platform
 import json
 import logging
 import os
+import sys
 import traceback
+import shutil
+from subprocess import Popen, PIPE
+
+import pytest
 
 from ocs_ci.deployment.ocp import OCPDeployment as BaseOCPDeployment
 from ocs_ci.framework import config
@@ -16,6 +21,7 @@ from ocs_ci.ocs.exceptions import SameNamePrefixClusterAlreadyExistsException
 
 from ocs_ci.utility.utils import run_cmd, clone_repo
 from .deployment import Deployment
+from ocs_ci.ocs import exceptions
 
 logger = logging.getLogger(__name__)
 
@@ -194,6 +200,24 @@ class AWSUPI(AWSBase):
     class OCPDeployment(BaseOCPDeployment):
         def __init__(self):
             super(AWSUPI.OCPDeployment, self).__init__()
+            self.upi_repo_path = os.path.join(
+                constants.EXTERNAL_DIR, 'openshift-misc',
+            )
+            self.upi_unique_path = os.path.join(
+                constants.EXTERNAL_DIR,
+                f"aws_upi_deploy_{config.RUN['run_id']}"
+            )
+            try:
+                os.mkdir(self.upi_unique_path)
+            except OSError:
+                logger.error("Already a cluster exists with same runid")
+                pytest.fail("Cluster with same runid exists")
+
+            self.upi_script_path = os.path.join(
+                self.upi_unique_path,
+                f'openshift-misc/v3-launch-templates/functionality-testing'
+                f'/aos-4_2/hosts/'
+            )
 
         def deploy_prereq(self):
             """
@@ -210,7 +234,7 @@ class AWSUPI(AWSBase):
                 'route53_domain_name': config.ENV_DATA['base_domain'],
                 'vm_type_masters': config.ENV_DATA['master_instance_type'],
                 'vm_type_workers': config.ENV_DATA['worker_instance_type'],
-                'num_workers': 3
+                'num_workers': "3"
             }
             for key, value in upi_env_vars.items():
                 os.environ[key] = value
@@ -221,8 +245,42 @@ class AWSUPI(AWSBase):
 
             # git clone repo from openshift-qe repo
             clone_repo(
-                constants.OCP_QE_MISC_REPO,
-                os.path.join(constants.EXTERNAL_DIR, 'openshift-misc')
+                constants.OCP_QE_MISC_REPO, self.upi_repo_path
+            )
+
+            # copy openshift-misc cloned repos to this run dir
+            shutil.copytree(
+                self.upi_repo_path,
+                os.path.join(self.upi_unique_path, "openshift-misc")
+            )
+
+            # create install-dir inside upi_script_path
+            os.mkdir(os.path.join(self.upi_script_path, "install-dir"))
+
+            # NOT A CLEAN APPROACH: copy openshift-install and oc binary to
+            # script path because upi script expectes it to be present in
+            # script dir
+            bindir = os.path.join(os.getcwd(), 'bin')
+            shutil.copy2(
+                os.path.join(bindir, 'openshift-install'),
+                self.upi_script_path,
+            )
+            shutil.copy2(
+                os.path.join(bindir, 'oc'), self.upi_script_path
+            )
+            # copy install-config.yaml from cluster_path to
+            # upi_script_path/install-dir
+            shutil.copy2(
+                os.path.join(self.cluster_path, "install-config.yaml"),
+                os.path.join(self.upi_script_path, "install-dir/")
+            )
+            # Remove current cluster_path dir and make it as symlink to
+            # upi_script_path/install-dir/ so that further operations
+            # operates transparently
+            shutil.rmtree(self.cluster_path, ignore_errors=True)
+            os.symlink(
+                os.path.join(self.upi_script_path, "install-dir/"),
+                self.cluster_path.rstrip('/'),
             )
 
         def deploy(self, log_cli_level='DEBUG'):
@@ -238,7 +296,37 @@ class AWSUPI(AWSBase):
                 f"Openshift-installer will be using loglevel:{log_cli_level}"
             )
 
-            #TODO: call "upi_on_aws-install.sh" here
+            # Invoke upi_on_aws-install.sh
+            cidir = os.getcwd()
+            logger.info("Changing CWD")
+            try:
+                os.chdir(self.upi_script_path)
+            except OSError:
+                logger.exception(
+                    f"Failed to change CWD to {self.upi_script_path} "
+                )
+            logger.info(f"CWD changed to {self.upi_script_path}")
+
+            with open("./upi_on_aws-install.sh", "r") as fd:
+                buf = fd.read()
+            data = buf.replace("openshift-qe-upi", "ocs-qe-upi")
+            with open("./upi_on_aws-install.sh", "w") as fd:
+                fd.write(data)
+
+            sys.path.append(self.upi_script_path)
+            proc = Popen(
+                [f'{self.upi_script_path}/upi_on_aws-install.sh'],
+                stdout=PIPE, stderr=PIPE,
+            )
+            stdout, stderr = proc.communicate()
+
+            # Change dir back to ocs-ci dir
+            os.chdir(cidir)
+
+            if proc.returncode:
+                logger.error(stderr)
+                raise exceptions.CommandFailed("upi install script failed")
+            logger.info(stdout)
 
     def deploy_ocp(self, log_cli_level='DEBUG'):
         """
@@ -250,6 +338,8 @@ class AWSUPI(AWSBase):
         """
         super(AWSUPI, self).deploy_ocp(log_cli_level)
         volume_size = config.ENV_DATA.get('DEFAULT_EBS_VOLUME_SIZE', 100)
+        # TODO: Implement add_volume for UPI
+        # existing function looks for terraform files
         self.add_volume(volume_size)
 
     def destroy_cluster(self, log_level="DEBUG"):
