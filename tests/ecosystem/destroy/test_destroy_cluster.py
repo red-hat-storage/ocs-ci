d1cde7a0000517e0642c27d8406dd8da106b9660
Daniel Horak <dahorak@redhat.com>
2019-09-06 16:04:06 +0200

    cleanup destroy cluster test
    - do not destroy cluster twice in test_destroy_cluster test
    - remove old destroy_cluster function
    
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

--- tests/ecosystem/destroy/test_destroy_cluster.py
+++ tests/ecosystem/destroy/test_destroy_cluster.py
@@ -1,8 +1,7 @@
 import logging
 
-from ocs_ci.framework import config
 from ocs_ci.framework.testlib import EcosystemTest, destroy
-from ocs_ci.utility.utils import destroy_cluster
+from ocs_ci.framework import config
 
 log = logging.getLogger(__name__)
 
@@ -10,5 +9,13 @@ log = logging.getLogger(__name__)
 @destroy
 class TestDestroy(EcosystemTest):
     def test_destroy_cluster(self, log_cli_level):
-        log.info("Running OCS cluster destroy")
-        destroy_cluster(config.ENV_DATA['cluster_path'], log_cli_level)
+        teardown = config.RUN['cli_params'].get('teardown')
+        if teardown:
+            log.info(
+                "Cluster will be destroyed during teardown part of this test."
+            )
+        else:
+            log.warning(
+                "Command line parameter --teardown was not provided, "
+                "cluster will not be destroyed!"
+            )
