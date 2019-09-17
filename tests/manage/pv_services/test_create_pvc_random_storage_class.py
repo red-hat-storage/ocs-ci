48b1d48055a577d72f13483dcfa00c09beedd040
prsurve <prsurve@redhat.com>
2019-09-17 08:23:12 +0530

    - fix as per comments - Updated to make use of factory fixture
    Signed-off-by: prsurve <prsurve@redhat.com>

--- tests/manage/pv_services/test_create_pvc_random_storage_class.py
+++ tests/manage/pv_services/test_create_pvc_random_storage_class.py
@@ -1,75 +1,35 @@
 """
 A test for creating pvc with random sc
 """
-import logging
 import random
 
 import pytest
 
 from ocs_ci.ocs import constants
 from ocs_ci.framework.testlib import ManageTest, tier2
-from tests import helpers
-from tests.fixtures import (
-    create_rbd_secret, create_cephfs_secret
-)
-
-log = logging.getLogger(__name__)
 
 
 @tier2
-@pytest.mark.usefixtures(
-    create_rbd_secret.__name__,
-    create_cephfs_secret.__name__,
-)
 @pytest.mark.polarion_id("OCS-288")
 class TestCreatePVCRandomStorageClass(ManageTest):
     """
     Creating PVC with random SC
     """
 
-    def test_create_pvc_with_random_sc(self, teardown_factory):
+    def test_create_pvc_with_random_sc(self, secret_factory, storageclass_factory, pvc_factory):
         sc_list = []
+        rbd_secret_obj = secret_factory(interface=constants.CEPHBLOCKPOOL)
+        cephfs_secret_obj = secret_factory(interface=constants.CEPHFILESYSTEM)
+
+        # Create Rbd Sc
         for i in range(5):
-            log.info(f"Creating cephblockpool")
-            cbp_obj = helpers.create_ceph_block_pool()
-            log.info(
-                f"{cbp_obj.name} created successfully"
-            )
-            log.info(
-                f"Creating a RBD storage class using {cbp_obj.name}"
-            )
-            rbd_sc_obj = helpers.create_storage_class(
-                interface_type=constants.CEPHBLOCKPOOL,
-                interface_name=cbp_obj.name,
-                secret_name=self.rbd_secret_obj.name
-            )
-            log.info(
-                f"StorageClass: {rbd_sc_obj.name} "
-                f"created successfully using {cbp_obj.name}"
-            )
+            rbd_sc_obj = storageclass_factory(interface=constants.CEPHBLOCKPOOL, secret=rbd_secret_obj)
             sc_list.append(rbd_sc_obj)
-            teardown_factory(cbp_obj)
-            teardown_factory(rbd_sc_obj)
-        log.info("Creating CephFs Storageclass")
-        cephfs_sc_obj = helpers.create_storage_class(
-            interface_type=constants.CEPHFILESYSTEM,
-            interface_name=helpers.get_cephfs_data_pool_name(),
-            secret_name=self.cephfs_secret_obj.name
-        )
+
+        # Create CephFs Sc
+        cephfs_sc_obj = storageclass_factory(interface=constants.CEPHFILESYSTEM, secret=cephfs_secret_obj)
         sc_list.append(cephfs_sc_obj)
-        teardown_factory(cephfs_sc_obj)
 
         # Create PVCs randomly with sc
-        pvc_list = []
         for i in range(20):
-            sc_name = random.choice(sc_list)
-            log.info(f"Creating a PVC using {sc_name.name}")
-            pvc_obj = helpers.create_pvc(sc_name.name)
-            log.info(
-                f"PVC: {pvc_obj.name} created successfully using "
-                f"{sc_name.name}"
-            )
-            pvc_list.append(pvc_obj)
-            teardown_factory(pvc_obj)
-            helpers.wait_for_resource_state(pvc_obj, constants.STATUS_BOUND)
-            pvc_obj.reload()
+            pvc_factory(storageclass=random.choice(sc_list))
