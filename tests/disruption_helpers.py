48b5d22c211a0203267acf75e5ea639dd06dffb1
Jilju Joy <jijoy@redhat.com>
2019-09-21 10:57:54 +0530

    Adding test cases OCS-945 OCS-952
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/disruption_helpers.py
+++ tests/disruption_helpers.py
@@ -20,33 +20,47 @@ class Disruptions:
 
     def set_resource(self, resource):
         self.resource = resource
+        resource_count = 0
         if self.resource == 'mgr':
             self.resource_obj = pod.get_mgr_pods()
-            self.type = 'rook-ceph'
+            self.selector = constants.MGR_APP_LABEL
         if self.resource == 'mon':
             self.resource_obj = pod.get_mon_pods()
-            self.type = 'rook-ceph'
+            self.selector = constants.MON_APP_LABEL
         if self.resource == 'osd':
             self.resource_obj = pod.get_osd_pods()
-            self.type = 'rook-ceph'
+            self.selector = constants.OSD_APP_LABEL
         if self.resource == 'mds':
             self.resource_obj = pod.get_mds_pods()
-            self.type = 'rook-ceph'
+            self.selector = constants.MDS_APP_LABEL
         if self.resource == 'cephfsplugin':
             self.resource_obj = pod.get_plugin_pods(
                 interface=constants.CEPHFILESYSTEM
             )
-            self.type = 'csi'
+            self.selector = constants.CSI_CEPHFSPLUGIN_LABEL
         if self.resource == 'rbdplugin':
             self.resource_obj = pod.get_plugin_pods(
                 interface=constants.CEPHBLOCKPOOL
             )
-            self.type = 'csi'
-        self.resource_count = len(self.resource_obj)
+            self.selector = constants.CSI_RBDPLUGIN_LABEL
+        if self.resource == 'cephfsplugin_provisioner':
+            self.resource_obj = [pod.plugin_provisioner_leader(
+                interface=constants.CEPHFILESYSTEM
+            )]
+            self.selector = constants.CSI_CEPHFSPLUGIN_PROVISIONER_LABEL
+            resource_count = len(pod.get_cephfsplugin_provisioner_pods())
+        if self.resource == 'rbdplugin_provisioner':
+            self.resource_obj = [pod.plugin_provisioner_leader(
+                interface=constants.CEPHBLOCKPOOL
+            )]
+            self.selector = constants.CSI_RBDPLUGIN_PROVISIONER_LABEL
+            resource_count = len(pod.get_rbdfsplugin_provisioner_pods())
+
+        self.resource_count = resource_count or len(self.resource_obj)
 
     def delete_resource(self):
         self.resource_obj[0].delete(force=True)
         assert POD.wait_for_resource(
-            condition='Running', selector=f'app={self.type}-{self.resource}',
+            condition='Running', selector=self.selector,
             resource_count=self.resource_count, timeout=300
         )
