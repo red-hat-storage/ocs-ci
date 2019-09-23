eb48936664330c1f79093e2ba2569e314ff7987f
Shreekar <sshreeka@redhat.com>
2019-09-23 17:48:38 +0530

    tests/manage/cluster/cluster_expansion/test_add_capacity.py  - Test for adding capacity via cli  - Updated var osd_count to use new function pod.get_pod_count()  - Added assertion message for wait_for_resource func  - Added doctring to the class and to the functions  - Updated get_storage_cluster() with namespace parameter  - Added support for checking environment is IPI OR UPI  - Added ignore_leftovers marker Signed-off-by: Shreekar <sshreeka@redhat.com>

--- tests/manage/cluster/cluster_expansion/test_node_expansion.py
+++ tests/manage/cluster/cluster_expansion/test_node_expansion.py
@@ -1,15 +1,18 @@
 import logging
+import pytest
 
 from ocs_ci.utility.utils import TimeoutSampler
 from tests import helpers
-from ocs_ci.framework.testlib import tier1, ManageTest
+from ocs_ci.framework.testlib import tier1, ignore_leftovers, ManageTest
 from ocs_ci.ocs import machine as machine_utils
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.node import wait_for_nodes_status
+from ocs_ci.framework import config
 
 logger = logging.getLogger(__name__)
 
 
+@ignore_leftovers
 @tier1
 class TestAddNode(ManageTest):
     """
@@ -19,27 +22,31 @@ class TestAddNode(ManageTest):
         """
         Test for adding worker nodes to the cluster while IOs
         """
-        before_replica_counts = dict()
-        count = 2
-        machines = machine_utils.get_machinesets()
-        for machine in machines:
-            before_replica_counts.update({machine: machine_utils.get_replica_count(machine)})
-        worker_nodes_before = helpers.get_worker_nodes()
-        logger.info(f'The worker nodes number before adding a new node is {len(worker_nodes_before)}')
-        after_replica_counts = dict()
-        for machine in machines:
-            machine_utils.add_node(machine, count=count)
-            after_replica_counts.update(({machine: machine_utils.get_replica_count(machine)}))
-        logger.info(after_replica_counts)
-        for sample in TimeoutSampler(
-            timeout=300, sleep=3, func=helpers.get_worker_nodes
-        ):
-            if len(sample) == count * len(machines):
-                break
+        dt = config.ENV_DATA['deployment_type']
+        if dt == 'ipi':
+            before_replica_counts = dict()
+            count = 2
+            machines = machine_utils.get_machinesets()
+            for machine in machines:
+                before_replica_counts.update({machine: machine_utils.get_replica_count(machine)})
+            worker_nodes_before = helpers.get_worker_nodes()
+            logger.info(f'The worker nodes number before adding a new node is {len(worker_nodes_before)}')
+            after_replica_counts = dict()
+            for machine in machines:
+                machine_utils.add_node(machine, count=count)
+                after_replica_counts.update(({machine: machine_utils.get_replica_count(machine)}))
+            logger.info(after_replica_counts)
+            for sample in TimeoutSampler(
+                timeout=300, sleep=3, func=helpers.get_worker_nodes
+            ):
+                if len(sample) == count * len(machines):
+                    break
 
-        worker_nodes_after = helpers.get_worker_nodes()
-        logger.info(f'The worker nodes number after adding a new node is {len(worker_nodes_after)}')
-        wait_for_nodes_status(
-            node_names=worker_nodes_after, status=constants.NODE_READY
-        )
+            worker_nodes_after = helpers.get_worker_nodes()
+            logger.info(f'The worker nodes number after adding a new node is {len(worker_nodes_after)}')
+            wait_for_nodes_status(
+                node_names=worker_nodes_after, status=constants.NODE_READY
+            )
+        else:
+            pytest.skip("UPI not yet supported")
         # ToDo run IOs
