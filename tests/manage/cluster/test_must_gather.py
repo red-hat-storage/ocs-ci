d06f9a96c109a4dcc260a8957d470b67a0ccb22b
aviadp <apolak@redhat.com>
2019-10-08 21:00:08 +0300

    file created with basic test template
    Signed-off-by: aviadp <apolak@redhat.com>
    
    basic test created - untested
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    fixed directory location
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    basic template of test file
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    First test Created - compare openshit-storage pods to logs directories
    
     method, fixed path
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    added error no. to exception
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    temp commit - to be squashed
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    end of day commit - to be squashed
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    first test created - compare logs directory to openshift-storage pods
    
    Signed-off-by: aviadp <apolak@redhat.com>

---
+++ tests/manage/cluster/test_must_gather.py
@@ -0,0 +1,101 @@
+import os
+import logging
+
+import pytest
+
+from ocs_ci.framework.testlib import ManageTest, tier4
+from ocs_ci.ocs import openshift_ops
+from ocs_ci.ocs.utils import collect_ocs_logs
+
+logger = logging.getLogger(__name__)
+
+
+@tier4
+@pytest.mark.polarion_id("OCS-1583")
+class TestMustGather(ManageTest):
+
+    @pytest.fixture(autouse=True)
+    def init_ocp(self):
+        """
+        init OCP() object
+        """
+
+        self.ocs = openshift_ops.OCP()
+
+    def test_must_gather(self):
+
+        # Make logs root directory
+        logger.info("Creating logs Directory")
+        directory = self.make_directory()
+        logger.info(f"Creating {directory} - Done!")
+
+        # Collect OCS logs
+        logger.info("Collecting Logs")
+        collect_ocs_logs(dir_name=directory, ocp=False)
+        logger.info("Collecting logs - Done!")
+
+        # Compare running pods list to "/pods" subdirectories
+        logger.info("Checking logs tree")
+        logs = self.get_log_directories(directory)
+        pods = self.get_ocs_pods()
+        logger.info(f"Logs: {logs}")
+        logger.info(f"pods list: {pods}")
+        assert set(sorted(logs)) == set(sorted(pods))
+
+        # Verify logs file are not empty
+
+    def make_directory(self):
+        """
+
+        Check if directory to store must gather logs already exist
+        and use new directory if so.
+
+        Returns:
+            str: Logs directory
+
+        """
+        index = 1
+        directory = f"/tmp/mg_test{index}"
+        while os.path.isdir(directory + "_ocs_logs"):
+            index += 1
+            directory = f"/tmp/mg_test{index}"
+
+        return directory
+
+    def get_log_directories(self, directory):
+        """
+
+        Get list of subdirectories contains openshift-storage pod's logs
+
+        Args:
+            directory: (str): location of must gather logs
+
+        Returns:
+            list: Subdirectories of "pods" directory
+
+        """
+        list_dir = list()
+        for dir_name, subdir_list, files_list in os.walk(directory + "_ocs_logs"):
+            if dir_name[-4:] == "pods":
+                list_dir = os.listdir(dir_name)
+                break
+
+        return list_dir
+
+    def get_ocs_pods(self):
+        """
+
+        Get list of openshift-storage pods
+
+        Returns:
+            list: pods in openshift-storage namespace
+
+        """
+
+        pods = (self.ocs.get_pods(namespace='openshift-storage'))
+        pods_list = list()
+        for pod in pods:
+            if "example" not in pod:
+                pods_list.append(pod)
+
+        return pods_list
