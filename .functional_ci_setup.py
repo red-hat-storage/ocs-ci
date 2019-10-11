fc4a05e5033fa2aeb4d5bd9aecc4207ce6875284
Petr Balogh <pbalogh@redhat.com>
2019-10-11 14:19:44 +0200

    Rename ocs_operator_image to ocs_registry_image
    Fixes: #891
    
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- .functional_ci_setup.py
+++ .functional_ci_setup.py
@@ -103,7 +103,7 @@ def get_ocsci_conf():
         conf_obj['REPORTING']['us_ds'] = 'DS'
     if env.get("OCS_OPERATOR_DEPLOYMENT") == "true":
         conf_obj['DEPLOYMENT'] = dict(
-            ocs_operator_image=env['OCS_REGISTRY_IMAGE'],
+            ocs_registry_image=env['OCS_REGISTRY_IMAGE'],
             ocs_operator_deployment=True,
             ocs_operator_storage_cluster_cr="http://pkgs.devel.redhat.com/cgit/containers/ocs-registry/plain/ocs_v1_storagecluster_cr.yaml?h=ocs-4.2-rhel-8",
             ocs_operator_olm="http://pkgs.devel.redhat.com/cgit/containers/ocs-registry/plain/deploy-with-olm.yaml?h=ocs-4.2-rhel-8",
