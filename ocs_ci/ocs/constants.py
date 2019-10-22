9d565956829691bdc04254aab1a6a79341b3cf8e
Vasu Kulkarni <vasu@redhat.com>
2019-10-21 21:49:51 -0700

    Fix registry filesystem name to use cluster name as prefix from ENV
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -112,7 +112,7 @@ UPI_INSTALL_SCRIPT = "upi_on_aws-install.sh"
 
 DEFAULT_SECRET = 'rook-ceph-csi'
 DEFAULT_BLOCKPOOL = 'rbd'
-DEFAULT_SC_CEPHFS = "rook-ceph-cephfs"
+DEFAULT_SC_CEPHFS = "cephfs"
 DEFAULT_ROUTE_CRT = "router-certs-default"
 IMAGE_REGISTRY_RESOURCE_NAME = "cluster"
 
