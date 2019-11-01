ad5f6c058243a81a55ccd7f6bc92bad275768de3
Wayne Sun <gsun@redhat.com>
2019-11-01 17:19:54 -0400

    Parametrize bucket creation test cases
    Update test function with extra parameter amount and interface.
    Parametrize tests with current sets and three new sets.
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>

--- tests/manage/mcg/test_bucket_creation.py
+++ tests/manage/mcg/test_bucket_creation.py
@@ -12,40 +12,41 @@ logger = logging.getLogger(__name__)
 
 @filter_insecure_request_warning
 @aws_platform_required
-@acceptance
-@tier1
+@pytest.mark.parametrize(
+    argnames="amount,interface",
+    argvalues=[
+        pytest.param(
+            *[3, 'S3'],
+            marks=[pytest.mark.polarion_id("OCS-1298"), tier1, acceptance]
+        ),
+        pytest.param(
+            *[3, 'CLI'], marks=[tier1, acceptance, noobaa_cli_required]
+        ),
+        pytest.param(
+            *[3, 'OC'], marks=[tier1, acceptance]
+        ),
+        pytest.param(
+            *[10, 'S3'],
+        ),
+        pytest.param(
+            *[10, 'CLI'], marks=noobaa_cli_required
+        ),
+        pytest.param(
+            *[10, 'OC'],
+        ),
+    ]
+)
 class TestBucketCreation:
     """
     Test creation of a bucket
     """
-    @pytest.mark.polarion_id("OCS-1298")
-    def test_s3_bucket_creation(self, mcg_obj, bucket_factory):
+    def test_bucket_creation(self, mcg_obj, bucket_factory, amount, interface):
         """
-        Test bucket creation using the S3 SDK
+        Test bucket creation using the S3 SDK, OC command or MCG CLI
         """
-        assert set(
-            bucket.name for bucket in bucket_factory(3, 'S3')
-        ).issubset(
-            mcg_obj.s3_list_all_bucket_names()
+        bucket_set = set(
+            bucket.name for bucket in bucket_factory(amount, interface)
         )
-
-    @noobaa_cli_required
-    def test_cli_bucket_creation(self, mcg_obj, bucket_factory):
-        """
-        Test bucket creation using the MCG CLI
-        """
-        assert set(
-            bucket.name for bucket in bucket_factory(3, 'CLI')
-        ).issubset(
-            mcg_obj.cli_list_all_bucket_names()
-        )
-
-    def test_oc_bucket_creation(self, mcg_obj, bucket_factory):
-        """
-        Test bucket creation using OC commands
-        """
-        assert set(
-            bucket.name for bucket in bucket_factory(3, 'OC')
-        ).issubset(
-            mcg_obj.oc_list_all_bucket_names()
+        assert bucket_set.issubset(
+            getattr(mcg_obj, f'{interface.lower()}_list_all_bucket_names')()
         )
