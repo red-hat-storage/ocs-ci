c2f1d2ce5d920bd46cd92757ba9b1fa2ff84e80c
Filip Balak <fbalak@redhat.com>
2019-11-15 18:05:09 +0100

    improve readability of test_resources.py
    Signed-off-by: Filip Balak <fbalak@redhat.com>

--- tests/ecosystem/upgrade/test_resources.py
+++ tests/ecosystem/upgrade/test_resources.py
@@ -1,6 +1,7 @@
 import logging
 import pytest
 
+from concurrent.futures import ThreadPoolExecutor
 from ocs_ci.framework.pytest_customization.marks import (
     ignore_leftovers, order_pre_upgrade, order_post_upgrade
 )
@@ -18,6 +19,7 @@ def test_pre_upgrade_pods(pre_upgrade_rbd_pods, pre_upgrade_cephfs_pods):
     assert pre_upgrade_rbd_pods
     assert pre_upgrade_cephfs_pods
 
+
 @order_post_upgrade
 @pytest.mark.polarion_id("OCS-1862")
 def test_pod_io(
@@ -28,18 +30,26 @@ def test_pod_io(
     """
     Test IO on multiple pods at the same time.
     """
+    log.info(f"RBD pods created before upgrade: {pre_upgrade_rbd_pods}")
+    log.info(f"CephFS pods created before upgrade: {pre_upgrade_cephfs_pods}")
+    log.info(f"Pods created after upgrade: {post_upgrade_pods}")
     pods = pre_upgrade_rbd_pods + pre_upgrade_cephfs_pods + post_upgrade_pods
+
+    # Run IOs on all pods
+    with ThreadPoolExecutor() as executor:
+        for pod in pods:
+            log.info(f"Running fio on {pod.name}")
+            executor.submit(
+                pod.run_io(
+                    storage_type='fs',
+                    size='1GB',
+                    runtime=30,
+                )
+            )
     for pod in pods:
-          log.info(f"Running fio on {pod.name}")
-          pod.run_io(
-              storage_type='fs',
-              size='1GB',
-              runtime=30,
-          )
-    for pod in pods:
-          log.info("Waiting for results from {pod.name}")
-          fio_result = pod.get_fio_results()
-          reads = fio_result.get('jobs')[0].get('read').get('iops')
-          writes = fio_result.get('jobs')[0].get('write').get('iops')
-          assert reads, f"There are no reads from pod {pod.name}"
-          assert writes, f"There are no writes from pod {pod.name}"
+        log.info(f"Waiting for results from {pod.name}")
+        fio_result = pod.get_fio_results()
+        reads = fio_result.get('jobs')[0].get('read').get('iops')
+        writes = fio_result.get('jobs')[0].get('write').get('iops')
+        assert reads, f"There are no reads from pod {pod.name}"
+        assert writes, f"There are no writes from pod {pod.name}"
