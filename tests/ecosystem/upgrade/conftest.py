699fd0d2e916f9d57e77d1e1d11f3a625994e48c
Filip Balak <fbalak@redhat.com>
2019-11-18 12:49:14 +0100

    merge pre upgrade pod fixtures
    Signed-off-by: Filip Balak <fbalak@redhat.com>

--- tests/ecosystem/upgrade/conftest.py
+++ tests/ecosystem/upgrade/conftest.py
@@ -7,33 +7,24 @@ log = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope='session')
-def pre_upgrade_rbd_pods(request, pod_factory_session):
+def pre_upgrade_pods(request, pod_factory_session):
     """
-    Generate RBD pods for tests before upgrade is executed.
+    Generate RBD and CephFS pods for tests before upgrade is executed.
 
     Returns:
         list: List of pods with RBD interface
     """
-    return [
+    rbd_pods = [
         pod_factory_session(
             interface=constants.CEPHBLOCKPOOL
         ) for _ in range(2)
     ]
-
-
-@pytest.fixture(scope='session')
-def pre_upgrade_cephfs_pods(request, pod_factory_session):
-    """
-    Generate CephFS pods for tests before upgrade is executed.
-
-    Returns:
-        list: List of pods with CephFS interface
-    """
-    return [
+    cephfs_pods = [
         pod_factory_session(
             interface=constants.CEPHFILESYSTEM
         ) for _ in range(2)
     ]
+    return rbd_pods + cephfs_pods
 
 
 @pytest.fixture
