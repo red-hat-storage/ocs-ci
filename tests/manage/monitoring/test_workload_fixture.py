f66f79f0e581c01bb056d08126e8a60e4a27af5b
Martin Bukatovič <mbukatov@redhat.com>
2019-11-22 14:44:55 +0100

    address review feedback
    Signed-off-by: Martin Bukatovič <mbukatov@redhat.com>

--- tests/manage/monitoring/test_workload_fixture.py
+++ tests/manage/monitoring/test_workload_fixture.py
@@ -3,7 +3,7 @@ import logging
 
 import pytest
 
-from ocs_ci.utility.prometheus import PrometheusAPI
+# from ocs_ci.utility.prometheus import PrometheusAPI
 # from ocs_ci.ocs.ocp import OCP
 # from tests import helpers
 
@@ -14,11 +14,15 @@ logger = logging.getLogger(__name__)
 @pytest.mark.libtest
 def test_workload_rbd(workload_storageutilization_50p_rbd):
     logger.info(workload_storageutilization_50p_rbd)
-    prometheus = PrometheusAPI()
 
 
 @pytest.mark.libtest
-def test_workload_rbd_again(workload_storageutilization_50p_rbd):
+def test_workload_rbd_in_some_other_way(workload_storageutilization_50p_rbd):
+    """
+    This test case is using the same workload fixture as the previous one.
+    These workload fixtures are designed to execute only once, so that both
+    this and the previous test are using the same workload.
+    """
     logger.info(workload_storageutilization_50p_rbd)
 
 
