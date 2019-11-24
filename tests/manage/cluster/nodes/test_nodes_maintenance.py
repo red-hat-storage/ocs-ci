3b36cc0957ecf80b544920c1b1da56f3a62d7d80
Elad Ben Aharon <ebenahar@localhost.localdomain>
2019-11-24 13:29:14 +0200

    Adjust node maintenance tests to abstract nodes platform class
    Signed-off-by: Elad Ben Aharon <ebenahar@localhost.localdomain>

--- tests/manage/cluster/nodes/test_nodes_maintenance.py
+++ tests/manage/cluster/nodes/test_nodes_maintenance.py
@@ -1,7 +1,10 @@
 import logging
 import pytest
 
-from ocs_ci.ocs import constants, node
+from ocs_ci.ocs import constants
+from ocs_ci.ocs.node import (
+    drain_nodes, schedule_nodes, get_typed_nodes, wait_for_nodes_status, get_node_objs
+)
 from ocs_ci.utility import aws
 from ocs_ci.framework.testlib import tier1, tier2, ManageTest, bugzilla, aws_platform_required
 
@@ -11,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(autouse=True)
-def schedule_nodes(request):
+def schedule_nodes_teardown(request):
     """
     Make sure that all cluster's nodes are in 'Ready' state and if not,
     change them back to 'Ready' state by marking them as scheduble
@@ -19,12 +22,12 @@ def schedule_nodes(request):
     """
     def finalizer():
         scheduling_disabled_nodes = [
-            n.name for n in node.get_node_objs() if n.ocp.get_resource_status(
+            n.name for n in get_node_objs() if n.ocp.get_resource_status(
                 n.name
             ) == constants.NODE_READY_SCHEDULING_DISABLED
         ]
         if scheduling_disabled_nodes:
-            node.schedule_nodes(scheduling_disabled_nodes)
+            schedule_nodes(scheduling_disabled_nodes)
     request.addfinalizer(finalizer)
 
 
@@ -62,11 +65,11 @@ class TestNodesMaintenance(ManageTest):
 
         """
         # Get 1 node
-        typed_nodes = node.get_typed_nodes(node_type=node_type, num_of_nodes=1)
+        typed_nodes = get_typed_nodes(node_type=node_type, num_of_nodes=1)
         typed_node_name = typed_nodes[0].name
 
         # Maintenance the node (unschedule and drain)
-        node.drain_nodes([typed_node_name])
+        drain_nodes([typed_node_name])
 
         # Check basic cluster functionality by creating resources
         # (pools, storageclasses, PVCs, pods - both CephFS and RBD),
@@ -75,7 +78,7 @@ class TestNodesMaintenance(ManageTest):
         self.sanity_helpers.delete_resources()
 
         # Mark the node back to schedulable
-        node.schedule_nodes([typed_node_name])
+        schedule_nodes([typed_node_name])
 
         # Perform cluster and Ceph health checks
         self.sanity_helpers.health_check()
@@ -90,12 +93,12 @@ class TestNodesMaintenance(ManageTest):
         ]
     )
     def test_node_maintenance_restart_activate(
-        self, ec2_instances, aws_obj, pvc_factory, pod_factory, node_type
+        self, nodes, pvc_factory, pod_factory, node_type
     ):
         """
         OCS-1292/OCS-1293:
         - Maintenance (mark as unscheduable and drain) 1 worker/master node
-        - Restart the node's ec2 instance
+        - Restart the node
         - Mark the node as scheduable
         - Check cluster and Ceph health
         - Check cluster functionality by creating and deleting resources
@@ -103,24 +106,21 @@ class TestNodesMaintenance(ManageTest):
 
         """
         # Get 1 node
-        typed_node = node.get_typed_nodes(node_type=node_type, num_of_nodes=1)
+        typed_node = get_typed_nodes(node_type=node_type, num_of_nodes=1)
         assert typed_node, f"Failed to find a {node_type} node for the test"
         typed_node_name = typed_node[0].name
 
         # Maintenance the node (unschedule and drain). The function contains logging
-        node.drain_nodes([typed_node_name])
+        drain_nodes([typed_node_name])
 
-        instance = aws.get_instances_ids_and_names(typed_node)
-        assert instance, f"Failed to get ec2 instances for node {typed_node_name}"
+        # Restarting the node
+        nodes.restart_nodes(nodes=typed_node, wait=True)
 
-        # Restarting ec2 instance
-        aws_obj.restart_ec2_instances(instances=instance, wait=True)
-
-        node.wait_for_nodes_status(
+        wait_for_nodes_status(
             node_names=[typed_node_name], status=constants.NODE_READY_SCHEDULING_DISABLED
         )
         # Mark the node back to schedulable
-        node.schedule_nodes([typed_node_name])
+        schedule_nodes([typed_node_name])
 
         # Check cluster and Ceph health and checking basic cluster
         # functionality by creating resources (pools, storageclasses,
@@ -150,16 +150,16 @@ class TestNodesMaintenance(ManageTest):
 
         """
         # Get 2 nodes
-        typed_nodes = node.get_typed_nodes(node_type=nodes_type, num_of_nodes=2)
+        typed_nodes = get_typed_nodes(node_type=nodes_type, num_of_nodes=2)
         assert typed_nodes, f"Failed to find a {nodes_type} node for the test"
 
         typed_node_names = [typed_node.name for typed_node in typed_nodes]
 
         # Maintenance the nodes (unschedule and drain)
-        node.drain_nodes(typed_node_names)
+        drain_nodes(typed_node_names)
 
         # Mark the nodes back to schedulable
-        node.schedule_nodes(typed_node_names)
+        schedule_nodes(typed_node_names)
 
         # Perform cluster and Ceph health checks
         self.sanity_helpers.health_check()
@@ -185,7 +185,7 @@ class TestNodesMaintenance(ManageTest):
         """
         # Get 1 node from each type
         nodes = [
-            node.get_typed_nodes(
+            get_typed_nodes(
                 node_type=node_type, num_of_nodes=1
             )[0] for node_type in ['worker', 'master']
         ]
@@ -194,7 +194,7 @@ class TestNodesMaintenance(ManageTest):
         node_names = [typed_node.name for typed_node in nodes]
 
         # Maintenance the nodes (unschedule and drain)
-        node.drain_nodes(node_names)
+        drain_nodes(node_names)
 
         # Check basic cluster functionality by creating resources
         # (pools, storageclasses, PVCs, pods - both CephFS and RBD),
@@ -203,7 +203,7 @@ class TestNodesMaintenance(ManageTest):
         self.sanity_helpers.delete_resources()
 
         # Mark the nodes back to schedulable
-        node.schedule_nodes(node_names)
+        schedule_nodes(node_names)
 
         # Perform cluster and Ceph health checks
         self.sanity_helpers.health_check()
