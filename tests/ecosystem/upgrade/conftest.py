2fc6f31009b63178ebdfe866e6915fb4aa924637
Filip Balak <fbalak@redhat.com>
2019-11-27 16:25:17 +0100

    specify reclaim policy and add default storageclasses
    Signed-off-by: Filip Balak <fbalak@redhat.com>

--- tests/ecosystem/upgrade/conftest.py
+++ tests/ecosystem/upgrade/conftest.py
@@ -6,7 +6,14 @@ from ocs_ci.ocs import constants, resources
 log = logging.getLogger(__name__)
 
 
-def create_pods(interface, pvc_factory, pod_factory, count, access_mode):
+def create_pods(
+    interface,
+    pvc_factory,
+    pod_factory,
+    storageclass,
+    count,
+    access_mode,
+):
     """
     Create pods for upgrade testing. pvc_factory and pod_factory have to be
     in the same scope.
@@ -15,9 +22,10 @@ def create_pods(interface, pvc_factory, pod_factory, count, access_mode):
         interface (str): CephBlockPool or CephFileSystem
         pvc_factory (function): Function for creating PVCs
         pod_factory (function): Function for creating pods
+        storageclass (obj): Storageclass to use
         count (int): Number of pods to create
         access_mode (str): ReadWriteOnce, ReadOnlyMany or ReadWriteMany.
-            This decides the access mode to be used for the PVC.
+            This decides the access mode to be used for the PVC
 
     Return:
         list: List of generated pods
@@ -26,9 +34,9 @@ def create_pods(interface, pvc_factory, pod_factory, count, access_mode):
     # https://github.com/red-hat-storage/ocs-ci/issues/1056
     # is resolved
     if interface == constants.CEPHBLOCKPOOL:
-        sc_name = "ocs-storagecluster-ceph-rbd"
+        sc_name = 'ocs-storagecluster-ceph-rbd'
     elif interface == constants.CEPHFILESYSTEM:
-        sc_name = "ocs-storagecluster-cephfs"
+        sc_name = 'ocs-storagecluster-cephfs'
     else:
         raise AttributeError(f"Interface '{interface}' is invalid")
 
@@ -36,15 +44,10 @@ def create_pods(interface, pvc_factory, pod_factory, count, access_mode):
         f"Creating {count} pods via {interface} using {access_mode}"
         f" access mode and {sc_name} storageclass")
     metadata = {'name': sc_name}
-    sc = resources.ocs.OCS(
-        kind=constants.STORAGECLASS,
-        metadata=metadata
-    )
-    sc.reload()
     pvcs = [
         pvc_factory(
-            storageclass=sc,
-            access_mode=access_mode
+            storageclass=storageclass,
+            access_mode=access_mode,
         ) for _ in range(count)
     ]
     pods = [
@@ -57,7 +60,42 @@ def create_pods(interface, pvc_factory, pod_factory, count, access_mode):
 
 
 @pytest.fixture(scope='session')
-def pre_upgrade_pods(request, pvc_factory_session, pod_factory_session):
+def default_storageclasses(request, teardown_factory_session):
+    """
+    Returns dictionary with storageclasses. Keys represent reclaim policy of
+    storageclass. There are two storageclasses for each key. First is RBD based
+    and the second one is CephFS based.
+    """
+    scs = {
+        constants.RECLAIM_POLICY_DELETE: [],
+        constants.RECLAIM_POLICY_RETAIN: []
+    }
+    for sc_name in (
+        'ocs-storagecluster-ceph-rbd',
+        'ocs-storagecluster-cephfs'
+    ):
+        sc = resources.ocs.OCS(
+            kind=constants.STORAGECLASS,
+            metadata={'name': sc_name}
+        )
+        sc.reload()
+        scs[constants.RECLAIM_POLICY_DELETE].append(sc)
+        sc.data['reclaimPolicy'] = constants.RECLAIM_POLICY_RETAIN
+        sc.data['metadata']['name'] += '-retain'
+        sc._name = sc.data['metadata']['name']
+        sc.create()
+        teardown_factory_session(sc)
+        scs[constants.RECLAIM_POLICY_RETAIN].append(sc)
+    return scs
+
+
+@pytest.fixture(scope='session')
+def pre_upgrade_pods(
+    request,
+    pvc_factory_session,
+    pod_factory_session,
+    default_storageclasses
+):
     """
     Generate RBD and CephFS pods for tests before upgrade is executed.
 
@@ -65,36 +103,43 @@ def pre_upgrade_pods(request, pvc_factory_session, pod_factory_session):
         list: List of pods with RBD interface
     """
     pods = []
-    for access_mode in [
-        constants.ACCESS_MODE_RWO,
-    ]:
-        rbd_pods = create_pods(
-            interface=constants.CEPHBLOCKPOOL,
-            pvc_factory=pvc_factory_session,
-            pod_factory=pod_factory_session,
-            count=20,
-            access_mode=access_mode
-        )
-        pods.extend(rbd_pods)
 
-    for access_mode in [
-        constants.ACCESS_MODE_RWO,
-        constants.ACCESS_MODE_RWX
-    ]:
-        cephfs_pods = create_pods(
-            interface=constants.CEPHFILESYSTEM,
-            pvc_factory=pvc_factory_session,
-            pod_factory=pod_factory_session,
-            count=20,
-            access_mode=access_mode
-        )
-        pods.extend(cephfs_pods)
+    for reclaim_policy in (
+        constants.RECLAIM_POLICY_DELETE,
+        constants.RECLAIM_POLICY_RETAIN
+    ):
+        for access_mode in [
+            constants.ACCESS_MODE_RWO,
+        ]:
+            rbd_pods = create_pods(
+                interface=constants.CEPHBLOCKPOOL,
+                pvc_factory=pvc_factory_session,
+                pod_factory=pod_factory_session,
+                storageclass=default_storageclasses.get(reclaim_policy)[0],
+                count=1,
+                access_mode=access_mode,
+            )
+            pods.extend(rbd_pods)
+
+        for access_mode in [
+            constants.ACCESS_MODE_RWO,
+            constants.ACCESS_MODE_RWX
+        ]:
+            cephfs_pods = create_pods(
+                interface=constants.CEPHFILESYSTEM,
+                pvc_factory=pvc_factory_session,
+                pod_factory=pod_factory_session,
+                storageclass=default_storageclasses.get(reclaim_policy)[1],
+                count=1,
+                access_mode=access_mode,
+            )
+            pods.extend(cephfs_pods)
 
     return pods
 
 
 @pytest.fixture
-def post_upgrade_pods(pvc_factory, pod_factory):
+def post_upgrade_pods(pvc_factory, pod_factory, default_storageclasses):
     """
     Generate pods for tests.
 
@@ -102,29 +147,35 @@ def post_upgrade_pods(pvc_factory, pod_factory):
         list: List of pods with RBD and CephFS interface
     """
     pods = []
-    for access_mode in [
-        constants.ACCESS_MODE_RWO,
-    ]:
-        rbd_pods = create_pods(
-            interface=constants.CEPHBLOCKPOOL,
-            pvc_factory=pvc_factory,
-            pod_factory=pod_factory,
-            count=1,
-            access_mode=access_mode
-        )
-        pods.extend(rbd_pods)
+    for reclaim_policy in (
+        constants.RECLAIM_POLICY_DELETE,
+        constants.RECLAIM_POLICY_RETAIN
+    ):
+        for access_mode in [
+            constants.ACCESS_MODE_RWO,
+        ]:
+            rbd_pods = create_pods(
+                interface=constants.CEPHBLOCKPOOL,
+                pvc_factory=pvc_factory,
+                pod_factory=pod_factory,
+                storageclass=default_storageclasses.get(reclaim_policy)[0],
+                count=1,
+                access_mode=access_mode,
+            )
+            pods.extend(rbd_pods)
 
-    for access_mode in [
-        constants.ACCESS_MODE_RWO,
-        constants.ACCESS_MODE_RWX
-    ]:
-        cephfs_pods = create_pods(
-            interface=constants.CEPHFILESYSTEM,
-            pvc_factory=pvc_factory,
-            pod_factory=pod_factory,
-            count=1,
-            access_mode=access_mode
-        )
-        pods.extend(cephfs_pods)
+        for access_mode in [
+            constants.ACCESS_MODE_RWO,
+            constants.ACCESS_MODE_RWX
+        ]:
+            cephfs_pods = create_pods(
+                interface=constants.CEPHFILESYSTEM,
+                pvc_factory=pvc_factory,
+                pod_factory=pod_factory,
+                storageclass=default_storageclasses.get(reclaim_policy)[1],
+                count=1,
+                access_mode=access_mode,
+            )
+            pods.extend(cephfs_pods)
 
     return pods
