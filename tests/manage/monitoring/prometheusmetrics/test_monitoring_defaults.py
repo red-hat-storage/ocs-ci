bcb7e53cf4e9cae6c8760bfb30ae304cf504a36c
Martin Bukatovič <mbukatov@redhat.com>
2019-12-04 15:56:06 +0100

    test case for OCS-1265
    Signed-off-by: Martin Bukatovič <mbukatov@redhat.com>

--- tests/manage/monitoring/prometheusmetrics/test_monitoring_defaults.py
+++ tests/manage/monitoring/prometheusmetrics/test_monitoring_defaults.py
@@ -10,6 +10,7 @@ from datetime import datetime
 import pytest
 import yaml
 
+from ocs_ci.ocs import constants, defaults, ocp
 from ocs_ci.ocs.resources import pod
 from ocs_ci.utility.prometheus import PrometheusAPI
 
@@ -50,3 +51,35 @@ def test_monitoring_enabled():
     for metric in result:
         _ , value = metric['value']
         assert int(value) >= 0
+
+
+@pytest.mark.polarion_id("OCS-1265")
+def test_ceph_mgr_dashboard_not_deployed():
+    """
+    Check that `ceph mgr dashboard`_ is not deployed after installation of OCS
+    (this is upstream rook feature not supported in downstream OCS).
+
+    .. _`ceph mgr dashboard`: https://rook.io/docs/rook/v1.0/ceph-dashboard.html
+    """
+    logger.info("Checking that there is no ceph mgr dashboard pod deployed")
+    ocp_pod = ocp.OCP(
+        kind=constants.POD,
+        namespace=defaults.ROOK_CLUSTER_NAMESPACE)
+    # if there is no "items" in the reply, OCS is very broken
+    ocs_pods= ocp_pod.get()['items']
+    for pod in ocs_pods:
+        # just making the assumptions explicit
+        assert pod['kind'] == constants.POD
+        pod_name = pod['metadata']['name']
+        msg = "ceph mgr dashboard should not be deployed as part of OCS"
+        assert "dashboard" not in pod_name, msg
+        assert "ceph-mgr-dashboard" not in pod_name, msg
+
+    logger.info("Checking that there is no ceph mgr dashboard route")
+    ocp_route = ocp.OCP(kind=constants.ROUTE)
+    for route in ocp_route.get(all_namespaces=True)['items']:
+        # just making the assumptions explicit
+        assert route['kind'] == constants.ROUTE
+        route_name = route['metadata']['name']
+        msg = "ceph mgr dashboard route should not be deployed as part of OCS"
+        assert "ceph-mgr-dashboard" not in route_name, msg
