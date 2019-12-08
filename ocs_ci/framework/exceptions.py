698fc55076a03d3268d51e2d8fbdfb6fd51d7562
Gil Klein <gklein@users.noreply.github.com>
2019-12-08 17:21:32 +0200

    Added a check for a min/max cluster name length
    Signed-off-by: Gil Klein <gklein@users.noreply.github.com>

--- ocs_ci/framework/exceptions.py
+++ ocs_ci/framework/exceptions.py
@@ -3,3 +3,15 @@ class ClusterPathNotProvidedError(Exception):
 
     def __str__(self):
         return "Please provide a --cluster-path that is empty or non-existant."
+
+
+class ClusterNameLengthError(Exception):
+
+    def __init__(self, name, min, max):
+        self.name = name
+        self.min = min
+        self.max = max
+
+    def __str__(self):
+        return (f"Cluster Name '{self.name}' is {len(self.name)} characters long "
+                f"while it should be {self.min}-{self.max} characters long")
