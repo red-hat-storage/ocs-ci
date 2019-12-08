02a4650a7c824181aab958d00d003ca6ec3e71dc
Gil Klein <gklein@users.noreply.github.com>
2019-12-08 17:21:32 +0200

    Added a check for a min/max cluster name length
    Signed-off-by: Gil Klein <gklein@users.noreply.github.com>

--- ocs_ci/framework/pytest_customization/ocscilib.py
+++ ocs_ci/framework/pytest_customization/ocscilib.py
@@ -13,7 +13,7 @@ from getpass import getuser
 import pytest
 
 from ocs_ci.framework import config as ocsci_config
-from ocs_ci.framework.exceptions import ClusterPathNotProvidedError
+from ocs_ci.framework.exceptions import ClusterPathNotProvidedError, ClusterNameLengthError
 from ocs_ci.ocs.exceptions import CommandFailed
 from ocs_ci.utility.utils import (
     dump_config_to_file,
@@ -25,7 +25,11 @@ from ocs_ci.utility.utils import (
 )
 from ocs_ci.ocs.utils import collect_ocs_logs
 from ocs_ci.ocs.resources.catalog_source import CatalogSource
-from ocs_ci.ocs.constants import OPERATOR_CATALOG_SOURCE_NAME
+from ocs_ci.ocs.constants import (
+    OPERATOR_CATALOG_SOURCE_NAME,
+    CLUSTER_NAME_MIN_CHARACTERS,
+    CLUSTER_NAME_MAX_CHARACTERS
+)
 
 __all__ = [
     "pytest_addoption",
@@ -193,6 +197,9 @@ def process_cluster_cli_params(config):
     Args:
         config (pytest.config): Pytest config object
 
+    Raises:
+        ClusterNameLengthError: If a cluster name is too short or too long
+
     """
     cluster_path = get_cli_param(config, 'cluster_path')
     if not cluster_path:
@@ -209,6 +216,13 @@ def process_cluster_cli_params(config):
     cluster_name = get_cli_param(config, 'cluster_name')
     if not cluster_name:
         cluster_name = f"ocs-ci-{getuser()[:8]}"
+    if (
+        len(cluster_name) < CLUSTER_NAME_MIN_CHARACTERS
+        or len(cluster_name) > CLUSTER_NAME_MAX_CHARACTERS
+    ):
+        raise ClusterNameLengthError(
+            cluster_name, CLUSTER_NAME_MIN_CHARACTERS, CLUSTER_NAME_MAX_CHARACTERS
+        )
     ocsci_config.RUN['cli_params']['teardown'] = get_cli_param(config, "teardown", default=False)
     ocsci_config.RUN['cli_params']['deploy'] = get_cli_param(config, "deploy", default=False)
     ocsci_config.RUN['cli_params']['io_in_bg'] = get_cli_param(config, "io_in_bg", default=False)
