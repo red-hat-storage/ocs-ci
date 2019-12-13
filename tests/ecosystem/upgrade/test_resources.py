1787adb44ad2f4b09c5a8515483839211ec95b21
Filip Balak <fbalak@redhat.com>
2019-12-13 13:45:04 +0100

    Add session scoped factory fixtures and pod IO test for upgrade (#1013)
    * add basic write io test after upgrade
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * create upgrade conftest
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * add pre_upgrade_cephfs_pods pre_upgrade_rbd_pods fixtures
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * add session scoped factory fixtures
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix scope of project_factory_class
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * improve readability of test_resources.py
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * remove unused imports
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * merge pre upgrade pod fixtures
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix typo
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix docstring
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * use original storageclass
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * add blank line
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix storageclass loading
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * remove unused import
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * specify access_mode for upgrade pods
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * specify reclaim policy and add default storageclasses
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * update add_pods function
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * add volume modes to constants
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * add volume modes to upgrade pod test
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix volume modes
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * add fio test for block devices
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix pvc_factory teardown for retain policy
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * add libtest for retain policy pvc
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * run fio on pods during upgrade
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix imports
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix fio execution
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * update upgrade markers
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * move default_storageclasses fixture, fix imports
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>
    
    * fix imports order
    
    Signed-off-by: Filip Balak <fbalak@redhat.com>

---
+++ tests/ecosystem/upgrade/test_resources.py
@@ -0,0 +1,95 @@
+from concurrent.futures import ThreadPoolExecutor
+import logging
+
+import pytest
+
+from ocs_ci.framework.pytest_customization.marks import (
+    ignore_leftovers, pre_upgrade, post_upgrade
+)
+
+log = logging.getLogger(__name__)
+
+
+@pre_upgrade
+@ignore_leftovers
+def test_start_pre_upgrade_pod_io(pre_upgrade_pods_running_io):
+    """
+    Confirm that there are pods created before upgrade.
+    """
+    assert pre_upgrade_pods_running_io
+
+
+@post_upgrade
+@pytest.mark.polarion_id("OCS-1862")
+def test_pod_io(
+    pre_upgrade_filesystem_pods,
+    post_upgrade_filesystem_pods,
+    pre_upgrade_block_pods,
+    post_upgrade_block_pods,
+    upgrade_fio_file
+):
+    """
+    Test IO on multiple pods at the same time and finish IO on pods that were
+    created before upgrade.
+    """
+    log.info(
+        f"Pods using filesystem created before upgrade: "
+        f"{pre_upgrade_filesystem_pods}"
+    )
+    log.info(
+        f"Pods using filesystem created after upgrade: "
+        f"{post_upgrade_filesystem_pods}"
+    )
+    log.info(
+        f"Pods using block device created before upgrade: "
+        f"{pre_upgrade_block_pods}"
+    )
+    log.info(
+        f"Pods using block device created after upgrade: "
+        f"{post_upgrade_block_pods}"
+    )
+
+    log.warning('Stopping fio for pre upgrade pods')
+    upgrade_fio_file.write_text('stop')
+
+    # Run IOs on filesystem pods
+    log.info('Starting fio on post upgrade fs pods')
+    pods = pre_upgrade_filesystem_pods + post_upgrade_filesystem_pods
+    with ThreadPoolExecutor() as executor:
+        for pod in pods:
+            log.info(f"Running fio on {pod.name}")
+            executor.submit(
+                pod.run_io(
+                    storage_type='fs',
+                    size='1GB',
+                    runtime=20,
+                )
+            )
+    for pod in pods:
+        log.info(f"Waiting for results from {pod.name}")
+        fio_result = pod.get_fio_results()
+        reads = fio_result.get('jobs')[0].get('read').get('iops')
+        writes = fio_result.get('jobs')[0].get('write').get('iops')
+        assert reads, f"There are no reads from pod {pod.name}"
+        assert writes, f"There are no writes from pod {pod.name}"
+
+    # Run IOs on block device pods
+    log.info('Starting fio on post upgrade block pods')
+    pods = pre_upgrade_block_pods + post_upgrade_block_pods
+    with ThreadPoolExecutor() as executor:
+        for pod in pods:
+            log.info(f"Running fio on {pod.name}")
+            executor.submit(
+                pod.run_io(
+                    storage_type='block',
+                    size='1024MB',
+                    runtime=20,
+                )
+            )
+    for pod in pods:
+        log.info(f"Waiting for results from {pod.name}")
+        fio_result = pod.get_fio_results()
+        reads = fio_result.get('jobs')[0].get('read').get('iops')
+        writes = fio_result.get('jobs')[0].get('write').get('iops')
+        assert reads, f"There are no reads from pod {pod.name}"
+        assert writes, f"There are no writes from pod {pod.name}"
