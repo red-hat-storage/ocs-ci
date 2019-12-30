84985f9b75260c329c572633adff30ed986b138f
Elad Ben Aharon <ebenahar@redhat.com>
2019-12-31 00:10:21 +0200

    small fix
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- new_csv.sh
+++ new_csv.sh
@@ -1,1 +1,1 @@
-oc get csv -o yaml | sed "s/quay.io\/rhceph-dev\/rhceph:4-7/registry.redhat.io\/rhceph-beta\/rhceph-4-rhel8/" | oc replace -f -
+oc -n openshift-storage get csv -o yaml | sed "s/quay\.io\/rhceph\-dev\/rhceph:4\-7/registry\.redhat\.io\/rhceph\-beta\/rhceph\-4\-rhel8/" | oc replace -f -
