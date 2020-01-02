acd065f97f0013829777b4eb4fd9cf5adfa8230d
Krishnaram Karthick <kramdoss@redhat.com>
2020-01-02 14:16:44 +0530

    Fixes teardown issue with test_multiple_pvc_creation_after_deletion_performance (#1229)
    Signed-off-by: Krishnaram Karthick <kramdoss@redhat.com>

--- tests/e2e/test_pvc_creation_performance.py
+++ tests/e2e/test_pvc_creation_performance.py
@@ -8,7 +8,7 @@ import ocs_ci.ocs.exceptions as ex
 import ocs_ci.ocs.resources.pvc as pvc
 from concurrent.futures import ThreadPoolExecutor
 from ocs_ci.framework.testlib import (
-    performance, E2ETest, polarion_id, bugzilla
+    performance, E2ETest, polarion_id
 )
 from tests import helpers
 from ocs_ci.ocs import defaults, constants
@@ -128,7 +128,6 @@ class TestPVCCreationPerformance(E2ETest):
 
     @pytest.mark.usefixtures(base_setup.__name__)
     @polarion_id('OCS-1270')
-    @bugzilla('1741612')
     def test_multiple_pvc_creation_after_deletion_performance(
         self, teardown_factory
     ):
@@ -147,6 +146,7 @@ class TestPVCCreationPerformance(E2ETest):
             size=self.pvc_size,
         )
         for pvc_obj in pvc_objs:
+            pvc_obj.reload()
             teardown_factory(pvc_obj)
         with ThreadPoolExecutor() as executor:
             for pvc_obj in pvc_objs:
@@ -154,8 +154,6 @@ class TestPVCCreationPerformance(E2ETest):
                     helpers.wait_for_resource_state, pvc_obj,
                     constants.STATUS_BOUND
                 )
-
-                executor.submit(pvc_obj.reload)
         log.info('Deleting 75% of the PVCs - 90 PVCs')
         assert pvc.delete_pvcs(pvc_objs[:number_of_pvcs], True), (
             "Deletion of 75% of PVCs failed"
@@ -167,6 +165,15 @@ class TestPVCCreationPerformance(E2ETest):
             number_of_pvc=number_of_pvcs,
             size=self.pvc_size,
         )
+        for pvc_obj in pvc_objs:
+            pvc_obj.reload()
+            teardown_factory(pvc_obj)
+        with ThreadPoolExecutor() as executor:
+            for pvc_obj in pvc_objs:
+                executor.submit(
+                    helpers.wait_for_resource_state, pvc_obj,
+                    constants.STATUS_BOUND
+                )
         start_time = helpers.get_start_creation_time(
             self.interface, pvc_objs[0].name
         )
@@ -175,11 +182,14 @@ class TestPVCCreationPerformance(E2ETest):
         )
         total = end_time - start_time
         total_time = total.total_seconds()
-        if total_time > 45:
+        logging.info(
+            f"{number_of_pvcs} PVCs creation time took {total_time} seconds"
+        )
+        if total_time > 135:
             raise ex.PerformanceException(
                 f"{number_of_pvcs} PVCs creation (after initial deletion of "
-                f"75%) time is {total_time} and greater than 45 seconds"
+                f"75%) time is {total_time} and greater than 135 seconds"
             )
         logging.info(
-            f"{number_of_pvcs} PVCs creation time took less than a 45 seconds"
+            f"{number_of_pvcs} PVCs creation time took less than a 135 seconds"
         )
