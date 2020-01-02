eff1520d1e0f5b5ed62e0faec679468b1c3daa1d
Elad Ben Aharon <ebenahar@redhat.com>
2020-01-02 16:42:14 +0200

    More nodes restart tests
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>
    
    Add more nodes restart tests
    
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>
    
    few fixes
    
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>
    
    Few fixes
    
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- tests/manage/z_cluster/nodes/test_nodes_restart.py
+++ tests/manage/z_cluster/nodes/test_nodes_restart.py
@@ -2,7 +2,7 @@ import logging
 import pytest
 
 from ocs_ci.framework.testlib import (
-    tier4, tier4a, ignore_leftovers, ManageTest, aws_platform_required
+    tier4, tier4a, ignore_leftovers, ManageTest, aws_platform_required, bugzilla
 )
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.node import get_node_objs
@@ -52,26 +52,41 @@ class TestNodesRestart(ManageTest):
     def test_nodes_restart(self, nodes, pvc_factory, pod_factory, force):
         """
         Test nodes restart (from the platform layer, i.e, EC2 instances, VMWare VMs)
+
         """
         ocp_nodes = get_node_objs()
         nodes.restart_nodes(nodes=ocp_nodes, force=force)
         self.sanity_helpers.health_check()
         self.sanity_helpers.create_resources(pvc_factory, pod_factory)
 
+    @bugzilla('1754287')
+    @pytest.mark.polarion_id("OCS-2015")
+    def test_rolling_nodes_restart(self, nodes, pvc_factory, pod_factory):
+        """
+        Test restart nodes one after the other and check health status in between
+
+        """
+        ocp_nodes = get_node_objs()
+        for node in ocp_nodes:
+            nodes.restart_nodes(nodes=[node])
+            self.sanity_helpers.health_check(cluster_check=False)
+        self.sanity_helpers.create_resources(pvc_factory, pod_factory)
+
     @pytest.mark.parametrize(
         argnames=["interface", "operation"],
         argvalues=[
-            pytest.param(*['rbd', 'create_resources'], marks=[pytest.mark.polarion_id("OCS-1138")]),
-            pytest.param(*['rbd', 'delete_resources'], marks=[pytest.mark.polarion_id("OCS-1241")]),
-            pytest.param(*['cephfs', 'create_resources'], marks=[pytest.mark.polarion_id("OCS-1139")]),
-            pytest.param(*['cephfs', 'delete_resources'], marks=[pytest.mark.polarion_id("OCS-1242")])
+            pytest.param(*['rbd', 'create_resources'], marks=pytest.mark.polarion_id("OCS-1138")),
+            pytest.param(*['rbd', 'delete_resources'], marks=pytest.mark.polarion_id("OCS-1241")),
+            pytest.param(*['cephfs', 'create_resources'], marks=pytest.mark.polarion_id("OCS-1139")),
+            pytest.param(*['cephfs', 'delete_resources'], marks=pytest.mark.polarion_id("OCS-1242"))
         ]
     )
-    def test_pv_provisioning_under_degraded_state(
+    def test_pv_provisioning_under_degraded_state_stop_provisioner_pod_node(
         self, nodes, pvc_factory, pod_factory, interface, operation
     ):
         """
-        Test PV provisioning under degraded state
+        Test PV provisioning under degraded state -
+        stop the node that has the provisioner pod running on
 
         OCS-1138:
         - Stop 1 worker node that has the RBD provisioner
@@ -121,13 +136,13 @@ class TestNodesRestart(ManageTest):
         elif interface == 'cephfs':
             provisioner_pods = pod.get_cephfsplugin_provisioner_pods()
         provisioner_pod = provisioner_pods[0]
-        # Workaround for BZ 1778488 - https://github.com/red-hat-storage/ocs-ci/issues/1222
+
+        # Making sure that the node is not running the rook operator pod:
         provisioner_node = pod.get_pod_node(provisioner_pod)
         rook_operator_pod = pod.get_operator_pods()[0]
         operator_node = pod.get_pod_node(rook_operator_pod)
         if operator_node.get().get('metadata').get('name') == provisioner_node.get().get('metadata').get('name'):
             provisioner_pod = provisioner_pods[1]
-        # End of workaround for BZ 1778488
 
         provisioner_pod_name = provisioner_pod.name
         logger.info(
@@ -163,12 +178,8 @@ class TestNodesRestart(ManageTest):
 
         # Wait for the provisioner pod to be started and reach running status
         logger.info(
-            f"Waiting for pod {provisioner_pod_name} to reach status Running"
+            f"Waiting for {interface} provisioner pod to reach status Running"
         )
-        logger.info(
-            f"Pod {provisioner_pod_name} has reached status Running"
-        )
-
         # After this change https://github.com/rook/rook/pull/3642/, there are
         # 2 provisioners for each interface
         assert provisioner_pod.ocp.wait_for_resource(
@@ -176,8 +187,9 @@ class TestNodesRestart(ManageTest):
             resource_count=2
         ), f"{interface} provisioner pod failed to reach status Running"
 
-        assert wait_for_ct_pod_recovery(), "Ceph tools pod failed to come up on another node"
-
+        logger.info(
+            f"{interface} provisioner pod has reached status Running"
+        )
         if operation == 'create_resources':
             # Cluster validation (resources creation and IO running)
             self.sanity_helpers.create_resources(pvc_factory, pod_factory)
@@ -187,6 +199,102 @@ class TestNodesRestart(ManageTest):
 
         # Starting the nodes
         nodes.start_nodes(nodes=[provisioner_node])
+
+        # Checking cluster and Ceph health
+        self.sanity_helpers.health_check()
+
+    @pytest.mark.parametrize(
+        argnames=["operation"],
+        argvalues=[
+            pytest.param(*['create_resources'], marks=[pytest.mark.polarion_id("OCS-2016")]),
+            pytest.param(*['delete_resources'], marks=[pytest.mark.polarion_id("OCS-2017")]),
+        ]
+    )
+    def test_pv_provisioning_under_degraded_state_stop_rook_operator_pod_node(
+        self, nodes, pvc_factory, pod_factory, operation
+    ):
+        """
+        Test PV provisioning under degraded state -
+        stop the node that has the rook operator pod running on
+
+        OCS-2016:
+        - Stop 1 worker node that has the rook ceph operator pod running on
+        - Wait for the rook ceph operator pod to come up again to running status
+        - Validate cluster functionality, without checking cluster and Ceph
+          health by creating resources and running IO
+        - Start the worker node
+        - Check cluster and Ceph health
+
+        OCS-2016:
+        - Stop 1 worker node that has the rook ceph operator pod running on
+        - Wait for the rook ceph operator pod to come up again to running status
+        - Validate cluster functionality, without checking cluster and Ceph
+          health by deleting resources
+        - Start the worker node
+        - Check cluster and Ceph health
+        """
+        if operation == 'delete_resources':
+            # Create resources that their deletion will be tested later
+            self.sanity_helpers.create_resources(pvc_factory, pod_factory)
+
+        rook_operator_pods = pod.get_operator_pods()
+        rook_operator_pod = rook_operator_pods[0]
+
+        rook_operator_pod_name = rook_operator_pod.name
+        logger.info(
+            f"rook operator pod found: {rook_operator_pod_name}"
+        )
+
+        # Get the node name that has the rook operator pod running on
+        operator_node = pod.get_pod_node(rook_operator_pod)
+        operator_node_name = operator_node.get().get('metadata').get('name')
+        logger.info(
+            f"{rook_operator_pod_name} pod is running on node {operator_node_name}"
+        )
+
+        # Stopping the node
+        nodes.stop_nodes(nodes=[operator_node])
+
+        # Wait for the rook operator pod to get to running status
+        selector = constants.OPERATOR_LABEL
+
+        # Wait for the rook operator pod to reach Terminating status
+        logger.info(
+            f"Waiting for pod {rook_operator_pod_name} to reach status Terminating"
+        )
+        assert rook_operator_pod.ocp.wait_for_resource(
+            timeout=600, resource_name=rook_operator_pod_name,
+            condition=constants.STATUS_TERMINATING
+        ), f"rook operator pod failed to reach status Terminating"
+        logger.info(
+            f"Pod {rook_operator_pod_name} has reached status Terminating"
+        )
+
+        # Wait for the rook operator pod to be started and reach running status
+        logger.info(
+            f"Waiting for pod {rook_operator_pod_name} to reach status Running"
+        )
+
+        assert rook_operator_pod.ocp.wait_for_resource(
+            timeout=600, condition=constants.STATUS_RUNNING, selector=selector,
+            resource_count=1
+        ), f"rook operator pod failed to reach status Running"
+        logger.info(
+            f"rook operator pod has reached status Running"
+        )
+
+        assert wait_for_ct_pod_recovery(), "Ceph tools pod failed to come up on another node"
+
+        if operation == 'create_resources':
+            # Cluster validation (resources creation and IO running)
+
+            self.sanity_helpers.create_resources(pvc_factory, pod_factory)
+        elif operation == 'delete_resources':
+            # Cluster validation (resources creation and IO running)
+            self.sanity_helpers.delete_resources()
+
+        # Starting the nodes
+        nodes.start_nodes(nodes=[operator_node])
 
         # Checking cluster and Ceph health
         self.sanity_helpers.health_check()
