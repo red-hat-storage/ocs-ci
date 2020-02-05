2bb00baf898f416cb1dcd720d0bfbaae7f43fbc2
Anna Sandler <asandler@hp-moonshot-03-c17.lab.eng.rdu2.redhat.com>
2020-02-05 17:15:53 +0200

    initial commit
    Signed-off-by: Anna Sandler <asandler@hp-moonshot-03-c17.lab.eng.rdu2.redhat.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -1,7 +1,9 @@
 """
 StorageCluster related functionalities
 """
-from ocs_ci.ocs.ocp import OCP
+from ocs_ci.ocs import constants, defaults, utils, cluster
+from ocs_ci.ocs.ocp import OCP, log
+from ocs_ci.ocs.resources.pod import get_osd_pods
 
 
 class StorageCluster(OCP):
@@ -23,3 +25,73 @@ class StorageCluster(OCP):
         super(StorageCluster, self).__init__(
             resource_name=resource_name, kind='StorageCluster', *args, **kwargs
         )
+
+
+def add_capacity(capacity_string):
+    """
+   Add storage capacity to the cluster
+
+   Args:
+       capacity_string(int): Size of the storage to add
+   Returns:
+       True if capacity was added, False if not
+
+   """
+    ocp = OCP(namespace=defaults.ROOK_CLUSTER_NAMESPACE, kind=constants.STORAGECLUSTER)
+    old_osd_count = cluster.get_osd_count
+    osd_size = parse_size_to_int(get_storage_cluster().get('spec').get('storageDeviceSets')[0].get('dataPVCTemplate').
+                                 get('spec').get('resources').get('resources').get('storage'))
+    replica = parse_size_to_int(get_storage_cluster().get('spec').get('storageDeviceSets')[0].get('replicas'))
+    capacity_to_add = parse_size_to_int(capacity_string)
+    sc = get_storage_cluster()
+
+    # adding the storage capacity to the cluster
+    ocp.patch(
+        resource_name=sc['metadata']['name'],
+        params=f'[{{"op": "replace", "path": "/spec/storageDeviceSets/0/count", '
+               f'"value":{capacity_to_add}}}]'
+    )
+
+    # cluster health check
+    if not utils.ceph_health_check:
+        log.info("Capacity was not added")
+        return False
+
+    # osd amount validation
+    new_osd_count = cluster.get_osd_count
+    expected = capacity_to_add / osd_size * replica + old_osd_count
+    if not expected == new_osd_count:
+        log.info("Capacity was not added")
+        return False
+
+    log.info(f"{capacity_string} was added")
+    return True
+
+
+def get_storage_cluster(namespace=defaults.ROOK_CLUSTER_NAMESPACE):
+    """
+   Get storage cluster name
+
+   Args:
+       namespace (str): Namespace of the resource
+   Returns:
+       yaml: Storage cluster yaml
+   """
+
+    sc_obj = OCP(kind=constants.STORAGECLUSTER, namespace=namespace)
+    return sc_obj.get()
+
+
+def parse_size_to_int(num):
+    """
+   Change capacity string to int
+
+   Args:
+       num (String): capacity in string format
+   Returns:
+       capacity (int) : capacity in int format (Gi)
+
+   """
+    place = num.find('G')
+    capacity = int(num[0, place])
+    return capacity
--- tests/manage/z_cluster/cluster_expansion/test_add_capacity.py
+++ tests/manage/z_cluster/cluster_expansion/test_add_capacity.py
@@ -1,13 +1,9 @@
 import logging
 import pytest
-from builtins import len
-
+from ocs_ci.framework.pytest_customization.marks import polarion_id, tier3
 from ocs_ci.framework.testlib import tier1, ignore_leftovers, ManageTest
-from ocs_ci.ocs import machine as machine_utils
-from ocs_ci.ocs import constants, ocp
-from ocs_ci.ocs import defaults
-from ocs_ci.framework import config
-from ocs_ci.ocs.node import get_typed_nodes
+from ocs_ci.ocs.resources import storage_cluster
+
 logger = logging.getLogger(__name__)
 
 
@@ -15,37 +11,28 @@ logger = logging.getLogger(__name__)
 @tier1
 class TestAddCapacity(ManageTest):
     """
-    Automates adding variable capacity to the cluster while IOs running
-    """
+   Automates adding variable capacity to the cluster while IOs running
+   """
+
     @pytest.mark.parametrize(
         argnames=[
-            "node_multiplier", "capacity"
+            "capacity", "expected"
         ],
         argvalues=[
-            pytest.param(
-                *[3, '2000Gi'],
-            ),
+            pytest.param(*['2000Gi', False], marks=[polarion_id(''), tier3]),
+            pytest.param(*['6144Gi', False], marks=[polarion_id(''), tier3]),
+            pytest.param(*['2048Gi', True], marks=[polarion_id(''), tier1]),
         ]
     )
-    def test_add_capacity(self, node_multiplier, capacity):
+    def test_add_capacity(self, capacity, expected):
         """
-        Test to add variable capacity to the OSD cluster while IOs running
+       Test to add variable capacity to the OSD cluster while IOs running
 
-        Args:
-        node_multiplier: the number of OSD to add per worker node
-        capacity: the storage capacity of each OSD
-        """
-        dt = config.ENV_DATA['deployment_type']
-        if dt == 'ipi':
-            storage_cluster = machine_utils.get_storage_cluster(namespace=defaults.ROOK_CLUSTER_NAMESPACE)
-            worker_nodes = len(get_typed_nodes())
-            machine_utils.add_capacity(storagecluster_name=storage_cluster, count=worker_nodes * node_multiplier)
-            machine_utils.add_storage_capacity(storagecluster_name=storage_cluster, capacity=capacity)
-            pod_obj = ocp.OCP(kind=constants.POD, namespace=defaults.ROOK_CLUSTER_NAMESPACE)
-            assert pod_obj.wait_for_resource(
-                condition=constants.STATUS_RUNNING, selector=constants.OSD_APP_LABEL,
-                resource_count=worker_nodes * node_multiplier, timeout=600
-            ), "OSD pods failed to reach RUNNING state"
-        else:
-            pytest.skip("UPI not yet supported")
-        # ToDo run IOs
+       Args:
+           capacity (String):the storage capacity of each OSD
+           expected (Boolean): Expected boolean statment from add_capacity function
+       """
+        returned_bool = storage_cluster.add_capacity(capacity)
+        assert returned_bool == expected(
+            logger.info("Test Failed")
+        )
