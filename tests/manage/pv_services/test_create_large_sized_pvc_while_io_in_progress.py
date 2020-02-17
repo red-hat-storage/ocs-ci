c829eafa3eed2cefa819af826182c88aa2dd9630
Prasad Desala <tdesala@redhat.com>
2020-02-17 11:25:53 +0530

    This patch automates BZ,
    Bug 1748939 - App pod stuck at ContainerCreating status
    
    Signed-off-by: Prasad Desala <tdesala@redhat.com>

---
+++ tests/manage/pv_services/test_create_large_sized_pvc_while_io_in_progress.py
@@ -0,0 +1,47 @@
+import pytest
+import logging
+
+from ocs_ci.ocs import constants
+from ocs_ci.ocs.resources import pod
+from ocs_ci.framework.testlib import ManageTest, tier1
+
+
+log = logging.getLogger(__name__)
+
+
+@tier1
+class TestCreateLargeSizedPVCWhileIOInProgress(ManageTest):
+    """
+    Create large sized PVC while IO is in progress
+
+    """
+    @pytest.mark.parametrize(
+        argnames=["interface"],
+        argvalues=[
+            pytest.param(
+                constants.CEPHBLOCKPOOL, marks=pytest.mark.polarion_id("OCS-2087")
+            ),
+            pytest.param(
+                constants.CEPHFILESYSTEM, marks=pytest.mark.polarion_id("OCS-2088")
+            )
+        ]
+    )
+    def test_create_large_sized_pvc_while_io_in_progress(
+            self, interface, pvc_factory,
+            pod_factory
+    ):
+        """
+        Flow is as below
+        *. Create a large sized PVC
+        *. Create an app pod and mount the PVC
+        *. Start IO to run in background
+        *. While IO is in-progress, repeat above all steps
+        test_cyclic_largesized_pvc_app
+        test_consecutive_largesized_pvc_and_app_pod_creation
+        """
+        # Repeating the above flow for 5 times
+        for i in range(5):
+            log.info(f"Creating {interface} based PVC")
+            pvc_obj = pvc_factory(interface=interface, size='500')
+            pod_obj = pod_factory(pvc=pvc_obj, interface=interface)
+            pod.run_io_in_bg(pod_obj)
