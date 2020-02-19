bb882fb15d3228b32f5557d17227fb3077f7b942
Lukas Zachar <lzachar@redhat.com>
2020-02-19 10:06:08 +0100

    Fixing comments from PR#1497
    Signed-off-by: Lukas Zachar <lzachar@redhat.com>

--- ocs_ci/ocs/precheck.py
+++ ocs_ci/ocs/precheck.py
@@ -1,25 +1,29 @@
 import logging
+
 from ocs_ci.ocs.resources.pod import get_osd_pods
+from ocs_ci.ocs.exceptions import UnexpectedBehaviour
 
 log = logging.getLogger(__name__)
 
 
-def check_max_osds_per_node(max_number_osds=3):
+def check_max_osds_per_node(max_number_osds=2):
     """
-    Function to check there is less than `max_number_osds` per node
+    Function to check there is less than or equal to `max_number_osds` per node
 
     Args:
         max_number_osds (int): upper bound
 
     Returns:
-        bool: True if condition is satisfied
+        True when satisfied, raises UnexpectedBehaviour otherwise
 
     """
     osd_on_node_count = {}
     for pod in get_osd_pods():
         node_name = pod.pod_data['spec']['nodeName']
         osd_on_node_count[node_name] = osd_on_node_count.get(node_name, 0) + 1
-    for count in osd_on_node_count.values():
-        if count >= max_number_osds:
-            return False
+    for node_name, count in osd_on_node_count.items():
+        if count > max_number_osds:
+            raise UnexpectedBehaviour(
+                f"Node {node_name} runs {count} OSDs, maximum {max_number_osds} is allowed "
+            )
     return True
