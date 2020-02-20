8c5ed40a08323ca341663c4106bdd41422210e1a
Shylesh Kumar <shmohan@redhat.com>
2020-02-19 22:42:02 -0800

    build ocp repo names dynamically
    Signed-off-by: Shylesh Kumar <shmohan@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -483,10 +483,6 @@ INSTALLER_DEFAULT_DNS = "1.1.1.1"
 # Config related constants
 config_keys_patterns_to_censor = ['passw', 'token', 'secret']
 
-# repos
-OCP4_2_REPO = os.path.join(REPO_DIR, "ocp_4_2.repo")
-OCP4_3_REPO = os.path.join(REPO_DIR, "ocp_4_3.repo")
-
 # packages
 RHEL_POD_PACKAGES = ["openssh-clients", "openshift-ansible", "openshift-clients", "jq"]
 
