aefc96052d5bca32cde07374f62de0be32467a6a
Coady LaCroix <clacroix@redhat.com>
2020-03-02 11:49:05 -0800

    Use same logging format as run-ci
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- ocs_ci/cleanup/aws/cleanup.py
+++ ocs_ci/cleanup/aws/cleanup.py
@@ -13,6 +13,10 @@ from ocs_ci.utility.aws import AWS
 from ocs_ci.cleanup.aws import defaults
 
 
+FORMAT = (
+    '%(asctime)s - %(threadName)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
