b53d313e1aff44d3df4b5004076fdc2b2e1fc696
Coady LaCroix <clacroix@redhat.com>
2020-03-03 11:39:52 -0800

    Changed hours type in excluded prefix dict from str to int
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- ocs_ci/cleanup/aws/defaults.py
+++ ocs_ci/cleanup/aws/defaults.py
@@ -2,7 +2,7 @@
 Defaults module for AWS cleanup
 """
 AWS_REGION = 'us-east-2'
-CLUSTER_PREFIXES_TO_EXCLUDE_FROM_DELETION = {'jnk': '36', 'dnd': '36'}
+CLUSTER_PREFIXES_TO_EXCLUDE_FROM_DELETION = {'jnk': 36, 'dnd': 36}
 MINIMUM_CLUSTER_RUNNING_TIME_FOR_DELETION = 10
 AWS_CLOUDFORMATION_TAG = 'aws:cloudformation:stack-id'
 CONFIRMATION_ANSWER = 'yes-i-am-sure-i-want-to-proceed'
