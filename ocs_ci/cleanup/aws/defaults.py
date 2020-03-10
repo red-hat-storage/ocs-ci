f6169d9847ab166de5665f117b247b0c813cc918
Coady LaCroix <clacroix@redhat.com>
2020-03-10 10:57:28 -0700

    AWS cleanup enhancements (#1595)
    * Use same logging format as run-ci
    
    * Add 'dnd' prefix to ignored list
    
    * Check if the cluster_name starts with the prefix instead of just containing it
    
    * Changed hours type in excluded prefix dict from str to int
    
    * Changed additional prefix rules from interactive prompt to repeatable CLI argument. Removed skip prefixes argument
    
    * Use triple quotes for long help messages
    
    * Better validation for prefix arg. Support hour value of 'never' to skip cleanup entirely. Added long running prefixes for 1 to 5 days. Use case-insensitive regex matching for prefixes.
    
    * Improved validation of --hours. Added hint about 'never' to help text for --prefix

--- ocs_ci/cleanup/aws/defaults.py
+++ ocs_ci/cleanup/aws/defaults.py
@@ -2,7 +2,10 @@
 Defaults module for AWS cleanup
 """
 AWS_REGION = 'us-east-2'
-CLUSTER_PREFIXES_TO_EXCLUDE_FROM_DELETION = {'jnk': '36'}
-MINIMUM_CLUSTER_RUNNING_TIME_FOR_DELETION = 10
+CLUSTER_PREFIXES_SPECIAL_RULES = {
+    'jnk': 36, 'DND': 'never', 'LR1': 24, 'LR2': 48, 'LR3': 72, 'LR4': 96,
+    'LR5': 120
+}
+MINIMUM_CLUSTER_RUNNING_TIME = 10
 AWS_CLOUDFORMATION_TAG = 'aws:cloudformation:stack-id'
 CONFIRMATION_ANSWER = 'yes-i-am-sure-i-want-to-proceed'
