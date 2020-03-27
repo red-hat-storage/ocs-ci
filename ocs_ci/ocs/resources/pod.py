b5f9430acb679d50098e113f19bea9c390fd3230
Petr Balogh <petr-balogh@users.noreply.github.com>
2020-03-27 16:40:57 +0100

    Fix get_all_pods function (#1727)
    * Fix get_all_pods function
    
    Fixes: #1726
    
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- ocs_ci/ocs/resources/pod.py
+++ ocs_ci/ocs/resources/pod.py
@@ -398,7 +398,7 @@ def get_all_pods(
     if selector:
         pods_new = [
             pod for pod in pods if
-            pod['metadata']['labels'].get(selector_label) in selector
+            pod['metadata'].get('labels', {}).get(selector_label) in selector
         ]
         pods = pods_new
     pod_objs = [Pod(**pod) for pod in pods]
