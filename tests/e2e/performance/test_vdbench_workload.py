c8fdebd4a2ec560270598371fc2e2b5dc02c1660
Avi liani <alayani@redhat.com>
2020-04-16 15:51:11 +0300

    fix typo
    Signed-off-by: Avi liani <alayani@redhat.com>

--- tests/e2e/performance/test_vdbench_workload.py
+++ tests/e2e/performance/test_vdbench_workload.py
@@ -211,7 +211,7 @@ class TestVDBenchWorkload(E2ETest):
             servers = round(tested_capacity / capacity_per_pod)
 
             """
-                To spread the application pods evenly on all workers or application nods and at least 2 app pods
+                To spread the application pods evenly on all workers or application nodes and at least 2 app pods
                 per node.
             """
             nodes = len(node.get_typed_nodes(node_type=constants.WORKER_MACHINE))
