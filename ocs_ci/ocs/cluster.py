b7011437f76a81dabf4171bf3003ffe962bceddb
ratamir <ratamir@redhat.com>
2020-04-26 19:12:39 +0300

    Fix retry call
    Signed-off-by: ratamir <ratamir@redhat.com>

--- ocs_ci/ocs/cluster.py
+++ ocs_ci/ocs/cluster.py
@@ -611,7 +611,7 @@ class CephCluster(object):
                 and states['count'] == total_pg_count
             )
 
-    @retry(UnexpectedBehaviour, tries=20, delay=10, backoff=1)
+    @retry(RebalanceException, tries=20, delay=10, backoff=1)
     def wait_for_rebalance_to_complete(self):
         """
         Calls get_rebalance_status_base with retries
