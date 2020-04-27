a8bb9c31c3f62267c58fb3134c00058c5d8d27e2
Martin Bukatovic <mbukatov@users.noreply.github.com>
2020-04-27 17:09:51 +0200

    log events in test namespaces before teardown (#1904)
    log events in test namespaces before teardown
    
    fixes https://github.com/red-hat-storage/ocs-ci/issues/749
    
    Signed-off-by: Martin Bukatovič <mbukatov@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -442,6 +442,30 @@ def project_factory_fixture(request):
         Delete the project
         """
         for instance in instances:
+            try:
+                ocp_event = ocp.OCP(kind="Event", namespace=instance.namespace)
+                events = ocp_event.get()
+                event_count = len(events['items'])
+                warn_event_count = 0
+                for event in events['items']:
+                    if event['type'] == "Warning":
+                        warn_event_count += 1
+                log.info(
+                    (
+                        "There were %d events in %s namespace before it's"
+                        " removal (out of which %d were of type Warning)."
+                        " For a full dump of this event list, see DEBUG logs."
+                    ),
+                    event_count,
+                    instance.namespace,
+                    warn_event_count
+                )
+            except Exception:
+                # we don't want any problem to disrupt the teardown itself
+                log.exception(
+                    "Failed to get events for project %s",
+                    instance.namespace
+                )
             ocp.switch_to_default_rook_cluster_project()
             instance.delete(resource_name=instance.namespace)
             instance.wait_for_delete(instance.namespace, timeout=300)
