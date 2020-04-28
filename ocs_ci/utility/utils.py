d147e0112490053627a62d5c67ed29ebc510154e
Pratik Surve <pratiksurve2130@gmail.com>
2020-04-28 21:44:29 +0530

    Add Support to run test over BAREMETAL
    Signed-off-by: Pratik Surve <pratiksurve2130@gmail.com>

--- ocs_ci/utility/utils.py
+++ ocs_ci/utility/utils.py
@@ -1212,6 +1212,13 @@ def get_testrun_name():
     ocs_version_string = f"OCS{ocs_version}" if ocs_version else ''
     worker_os = 'RHEL' if config.ENV_DATA.get('rhel_workers') else 'RHCOS'
     build_user = None
+    baremetal_config = None
+    if config.ENV_DATA.get('mon_type'):
+        baremetal_config = (
+            f"MON {config.ENV_DATA.get('mon_type').upper()} " 
+            f"OSD {config.ENV_DATA.get('osd_type').upper()} "
+        )
+
 
     if config.REPORTING.get('display_name'):
         testrun_name = config.REPORTING.get('display_name')
@@ -1220,6 +1227,12 @@ def get_testrun_name():
         testrun_name = (
             f"{config.ENV_DATA.get('platform', '').upper()} "
             f"{config.ENV_DATA.get('deployment_type', '').upper()} "
+        )
+        if baremetal_config:
+            testrun_name = f"{baremetal_config} {testrun_name}"
+
+        testrun_name = (
+            f"{testrun_name}"
             f"{get_az_count()}AZ "
             f"{worker_os} "
             f"{config.ENV_DATA.get('master_replicas')}M "
