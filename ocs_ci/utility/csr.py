5b1ac65ff6299c0c126c1183bbe937a9338e0380
vavuthu <vavuthu@redhat.com>
2020-04-28 11:54:14 +0530

    Adding more functionality
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/utility/csr.py
+++ ocs_ci/utility/csr.py
@@ -1,6 +1,7 @@
 import logging
 
 from ocs_ci.ocs import constants, exceptions, ocp
+from ocs_ci.ocs.node import get_all_nodes
 from ocs_ci.utility.retry import retry
 from ocs_ci.utility.utils import run_cmd, TimeoutSampler
 
@@ -29,6 +30,7 @@ def approve_pending_csr(expected=None):
     for pending_csrs in TimeoutSampler(300, 10, get_pending_csr):
         if not expected:
             if pending_csrs:
+                logger.info(f"Pending CSRs: {pending_csrs}")
                 approve_csrs(pending_csrs)
             break
         if len(pending_csrs) >= expected:
@@ -106,3 +108,64 @@ def approve_csrs(pending_csrs):
     cmd = f"{base_cmd} {csrs}"
     logger.info(f"Approving pending CSRs")
     run_cmd(cmd)
+
+
+def get_nodes_csr():
+    """
+    Fetches the CSRs
+
+    Returns:
+        dict: Dictionary with node as keys and CSRs as values
+            e.g:{
+                'control-plane-1': ['csr-6hx8z'],
+                'compute-2': ['csr-blz2n'],
+                'compute-0': ['csr-p46bz'],
+                'control-plane-2': ['csr-tzhn5'],
+                'control-plane-0': ['csr-wm4g5']
+                }
+
+    """
+    csr_nodes = {}
+    csr_data = get_csr_resource().get()
+    for item in csr_data['items']:
+        node = item['spec']['username'].split(":")[-1]
+        if node not in csr_nodes.keys():
+            csr_nodes[node] = []
+        csr_name = item['metadata']['name']
+        csr_nodes[node].append(csr_name)
+    return csr_nodes
+
+
+def wait_for_all_nodes_csr(timeout=900, sleep=10):
+    """
+    Wait for CSR to generate for nodes
+
+    Args:
+        timeout (int): Time in seconds to wait
+        sleep (int): Sampling time in seconds
+
+    Returns:
+         bool: True if all nodes are generated CSR
+
+    Raises:
+        TimeoutExpiredError: in case CSR not found
+
+    """
+    pending = False
+    all_nodes = get_all_nodes()
+    for csr_nodes in TimeoutSampler(
+            timeout=timeout, sleep=sleep, func=get_nodes_csr()
+    ):
+        pending_nodes = []
+        logger.debug(f"CSR data: {csr_nodes}")
+        for node in all_nodes:
+            if node not in csr_nodes.keys():
+                logger.info(f"{node} CSR is not generated")
+                pending = True
+                pending_nodes.append(node)
+        if not pending:
+            logger.info("CSR generated for all nodes in cluster")
+            return
+        logger.warning(
+            f"Nodes {pending_nodes} are not generated CSR. retrying again"
+        )
