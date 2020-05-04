c2488e66daafb1859a4a9dbeaacb45ed4512c5f7
Raz Tamir <ratamir@redhat.com>
2020-05-04 12:54:53 +0300

    Add sanity test post deployment (#1983)
    * Add sanity test post deployment
    
    Signed-off-by: ratamir <ratamir@redhat.com>
    
    * Add also health_check() validation
    
    Signed-off-by: ratamir <ratamir@redhat.com>

--- tests/ecosystem/deployment/test_deployment.py
+++ tests/ecosystem/deployment/test_deployment.py
@@ -5,13 +5,14 @@ from ocs_ci.framework.testlib import deployment, polarion_id
 from ocs_ci.ocs.resources.storage_cluster import ocs_install_verification
 from ocs_ci.utility.reporting import get_deployment_polarion_id
 from ocs_ci.utility.utils import is_cluster_running
+from tests.sanity_helpers import Sanity
 
 log = logging.getLogger(__name__)
 
 
 @deployment
 @polarion_id(get_deployment_polarion_id())
-def test_deployment():
+def test_deployment(pvc_factory, pod_factory):
     deploy = config.RUN['cli_params'].get('deploy')
     teardown = config.RUN['cli_params'].get('teardown')
     if not teardown or deploy:
@@ -19,6 +20,13 @@ def test_deployment():
         assert is_cluster_running(config.ENV_DATA['cluster_path'])
         if not config.ENV_DATA['skip_ocs_deployment']:
             ocs_install_verification()
+            # Check basic cluster functionality by creating resources
+            # (pools, storageclasses, PVCs, pods - both CephFS and RBD),
+            # run IO and delete the resources
+            sanity_helpers = Sanity()
+            sanity_helpers.health_check()
+            sanity_helpers.create_resources(pvc_factory, pod_factory)
+            sanity_helpers.delete_resources()
 
     if teardown:
         log.info(
