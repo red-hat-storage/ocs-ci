f4db90f87a2a13e15a0c6e8c64fa626d7a624ade
Filip Balak <fbalak@redhat.com>
2020-05-18 17:16:13 +0200

    remove duplicate fixture
    Signed-off-by: Filip Balak <fbalak@redhat.com>

--- tests/ecosystem/upgrade/conftest.py
+++ tests/ecosystem/upgrade/conftest.py
@@ -555,31 +555,6 @@ def pre_upgrade_pods_running_io(
 
 
 @pytest.fixture(scope='session')
-def fio_job_dict_mcg(fio_job_dict_session):
-    """
-    Fio job dictionary with configuration set for MCG workload.
-    """
-    fio_job_dict_mcg = copy.deepcopy(fio_job_dict_session)
-
-    job_name = 'mcg-workload'
-    config_name = f"{job_name}-config"
-    volume_name = f"{config_name}-vol"
-
-    fio_job_dict_mcg['metadata']['name'] = job_name
-    fio_job_dict_mcg['spec']['template']['metadata']['name'] = job_name
-
-    job_spec = fio_job_dict_mcg['spec']['template']['spec']
-    job_spec['volumes'][1]['name'] = volume_name
-    job_spec['volumes'][1]['configMap']['name'] = config_name
-    job_spec['containers'][0]['volumeMounts'][1]['name'] = volume_name
-
-    job_spec['volumes'].pop(0)
-    job_spec['containers'][0]['volumeMounts'].pop(0)
-
-    return fio_job_dict_mcg
-
-
-@pytest.fixture(scope='session')
 def fio_configmap_dict_mcg(fio_configmap_dict_session, fio_job_dict_mcg):
     """
     Fio configmap dictionary with configuration set for MCG workload.
