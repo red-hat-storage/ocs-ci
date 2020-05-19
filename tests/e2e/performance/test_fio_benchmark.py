41e916104d2f2004b2cd64cb55edbb4a3c27fdcf
Krishnaram Karthick <kramdoss@redhat.com>
2020-05-19 10:12:40 +0530

    Adds parameterization for random workload
    Signed-off-by: Krishnaram Karthick <kramdoss@redhat.com>

--- tests/e2e/performance/test_fio_benchmark.py
+++ tests/e2e/performance/test_fio_benchmark.py
@@ -6,7 +6,7 @@ import pytest
 from elasticsearch import Elasticsearch
 from ocs_ci.framework import config
 from ocs_ci.ocs.resources.ocs import OCS
-from ocs_ci.ocs.ocp import OCP
+from ocs_ci.ocs.ocp import OCP, get_ocs_version, get_build
 from ocs_ci.utility import templating
 from ocs_ci.utility.utils import run_cmd, TimeoutSampler
 from ocs_ci.ocs.utils import get_pod_name_by_pattern
@@ -14,6 +14,7 @@ from ocs_ci.ocs.ripsaw import RipSaw
 from ocs_ci.ocs import constants
 from ocs_ci.framework.testlib import E2ETest, performance
 from ocs_ci.ocs.resources import storage_cluster
+from tests.helpers import get_worker_nodes
 
 log = logging.getLogger(__name__)
 
@@ -30,37 +31,10 @@ def ripsaw(request):
     return ripsaw
 
 
-def get_ocs_version():
+def analyze_regression(io_pattern, es_username):
     """
-    Return the OCS Version
-    Returns:
-         str: The version of the OCS
-    """
-
-    ocp_cluster = OCP(
-        namespace=config.ENV_DATA['cluster_namespace'],
-        kind='', resource_name='csv')
-    return ocp_cluster.get()['items'][0]['spec']['version']
-
-
-def get_ocp_build():
-    """
-    Return the OCP Build Version
-    Returns:
-         str: The build version of the OCP
-    """
-    ocp_cluster = OCP(
-        namespace=config.ENV_DATA['cluster_namespace'],
-        kind='', resource_name='clusterversion')
-    return ocp_cluster.get()['items'][0]['status']['desired']['version']
-
-
-def analyze_regression(es_username):
-    """
-
     Args:
         es_username (str): ocs_build used in the CR object
-
     """
     es = Elasticsearch([{'host': constants.ES_SERVER_IP, 'port': constants.ES_SERVER_PORT}])
     # Fetch benchmark values for FIO
@@ -75,31 +49,39 @@ def analyze_regression(es_username):
         object_size = test_data['object_size']
         operation = test_data['operation']
         total_iops = test_data['total-iops']
-        std_dev = 'std-dev-' + object_size
-        variance = test_data[std_dev]
-        log.info(f"block_size: '{object_size}'\n"
-                    f"operation: {operation}\n"
-                    f"total_iops: {total_iops}\n"
-                    f"variance: {variance}"
-                    )
+        log.info(
+            f"io_pattern: {io_pattern}\n"
+            f"block_size: {object_size}\n"
+            f"operation: {operation}\n"
+            f"total_iops: {total_iops}\n"
+            )
         # Fail test if std deviation is above 5%
-        assert variance <= 5, f'variance - {variance} is greater than 5%'
-        if not test_data[std_dev] > 5:
-            bm_value = benchmark[object_size][operation]
-            difference = (float(total_iops) - float(bm_value)) / float(bm_value) * 100
-            # Fail test if 5% deviation from benchmark value
-            assert difference >= -5, f'{difference} from the benchmark value for {operation} with {object_size}'
+        if io_pattern == 'sequential':
+            std_dev = 'std-dev-' + object_size
+            variance = test_data[std_dev]
+            assert variance <= 5, f'variance - {variance} is greater than 5%'
+        bm_value = benchmark[object_size][operation]
+        difference = (float(total_iops) - float(bm_value)) / float(bm_value) * 100
+        log.info(f'deviation from bm {difference}')
+        # Fail test if 5% deviation from benchmark value
+        assert difference >= -5, f'{difference} from the benchmark value for {operation} with {object_size}'
 
 
 @performance
 @pytest.mark.parametrize(
-    argnames=["interface"],
+    argnames=["interface", "io_pattern"],
     argvalues=[
         pytest.param(
-            *[constants.CEPHBLOCKPOOL], marks=pytest.mark.polarion_id("OCS-844")
+            *[constants.CEPHBLOCKPOOL, 'sequential'], marks=pytest.mark.polarion_id("OCS-844")
         ),
         pytest.param(
-            *[constants.CEPHFILESYSTEM], marks=pytest.mark.polarion_id("OCS-845")
+            *[constants.CEPHBLOCKPOOL, 'random'], marks=pytest.mark.polarion_id("OCS-846")
+        ),
+        pytest.param(
+            *[constants.CEPHFILESYSTEM, 'sequential'], marks=pytest.mark.polarion_id("OCS-845")
+        ),
+        pytest.param(
+            *[constants.CEPHFILESYSTEM, 'random'], marks=pytest.mark.polarion_id("OCS-847")
         )
     ]
 )
@@ -107,7 +89,7 @@ class TestFIOBenchmark(E2ETest):
     """
     Run FIO perf test using ripsaw benchmark
     """
-    def test_fio_workload_simple(self, ripsaw, interface):
+    def test_fio_workload_simple(self, ripsaw, interface, io_pattern):
         """
         This is a basic fio perf test
         """
@@ -122,14 +104,19 @@ class TestFIOBenchmark(E2ETest):
         # Create fio benchmark
         log.info("Create resource file for fio workload")
         fio_cr = templating.load_yaml(constants.FIO_CR_YAML)
-        pvc_size = (storage_cluster.get_osd_size() * storage_cluster.get_deviceset_count()) / 2
+        fill_storage = (storage_cluster.get_osd_size() * storage_cluster.get_deviceset_count()) / 2
+        pvc_size = fill_storage / (3 * fio_cr['spec']['workload']['args']['servers'])
         # Todo: have pvc_size set to 'get_osd_pods_memory_sum * 5'
         #  once pr-2037 is merged
-        fio_cr['spec']['clustername'] = config.ENV_DATA['platform'] + get_ocp_build() + get_ocs_version()
-        fio_cr['spec']['test_user'] = get_ocs_version() + '_' + interface
+        fio_cr['spec']['clustername'] = config.ENV_DATA['platform'] + get_build() + get_ocs_version()
+        fio_cr['spec']['test_user'] = interface + io_pattern
         fio_cr['spec']['workload']['args']['storagesize'] = str(pvc_size)+'Gi'
         fio_cr['spec']['workload']['args']['filesize'] = str(int(pvc_size * 0.8))+'GiB'
         fio_cr['spec']['workload']['args']['storageclass'] = sc
+        if io_pattern == 'random':
+            fio_cr['spec']['workload']['args']['prefill'] = 'true'
+            fio_cr['spec']['workload']['args']['jobs'] = ['randwrite', 'randread']
+        fio_cr['spec']['workload']['args']['rook_ceph_drop_cache_pod_ip'] = get_worker_nodes()
         log.info(f'fio_cr: {fio_cr}')
         fio_cr_obj = OCS(**fio_cr)
         fio_cr_obj.create()
@@ -151,7 +138,7 @@ class TestFIOBenchmark(E2ETest):
         pod_obj.wait_for_resource(
             condition='Completed',
             resource_name=fio_client_pod,
-            timeout=3600,
+            timeout=18000,
             sleep=60,
         )
 
@@ -166,5 +153,6 @@ class TestFIOBenchmark(E2ETest):
         # Clean up fio benchmark
         log.info("Deleting FIO benchmark")
         fio_cr_obj.delete()
-        analyze_regression(es_username=fio_cr['spec']['test_user'])
+        analyze_regression(io_pattern, es_username=fio_cr['spec']['test_user'])
+
         # todo: push results to codespeed
