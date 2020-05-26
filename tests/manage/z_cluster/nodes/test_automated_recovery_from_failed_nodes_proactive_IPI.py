52b18d55a0f25692705158a99494343d038a0f4e
Elad Ben Aharon <ebenahar@redhat.com>
2020-05-26 16:43:39 +0300

    Fix proactive node replacement to validate also OSD prepare and DC pods
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- tests/manage/z_cluster/nodes/test_automated_recovery_from_failed_nodes_proactive_IPI.py
+++ tests/manage/z_cluster/nodes/test_automated_recovery_from_failed_nodes_proactive_IPI.py
@@ -134,29 +134,28 @@ class TestAutomatedRecoveryFromFailedNodes(ManageTest):
             namespace=defaults.ROOK_CLUSTER_NAMESPACE
         )
         for pod_obj in all_pod_obj:
-            if ('-1-deploy' or 'ocs-deviceset') not in pod_obj.name:
-                try:
-                    helpers.wait_for_resource_state(
-                        resource=pod_obj, state=constants.STATUS_RUNNING,
-                        timeout=200
+            state = constants.STATUS_RUNNING
+            if any(i in pod_obj.name for i in ['-1-deploy', 'ocs-deviceset']):
+                state = constants.STATUS_COMPLETED
+            try:
+                helpers.wait_for_resource_state(resource=pod_obj, state=state, timeout=200)
+            except ResourceWrongStatusException:
+                # 'rook-ceph-crashcollector' on the failed node stucks at
+                # pending state. BZ 1810014 tracks it.
+                # Ignoring 'rook-ceph-crashcollector' pod health check as
+                # WA and deleting its deployment so that the pod
+                # disappears. Will revert this WA once the BZ is fixed
+                if 'rook-ceph-crashcollector' in pod_obj.name:
+                    ocp_obj = ocp.OCP(
+                        namespace=defaults.ROOK_CLUSTER_NAMESPACE
                     )
-                except ResourceWrongStatusException:
-                    # 'rook-ceph-crashcollector' on the failed node stucks at
-                    # pending state. BZ 1810014 tracks it.
-                    # Ignoring 'rook-ceph-crashcollector' pod health check as
-                    # WA and deleting its deployment so that the pod
-                    # disappears. Will revert this WA once the BZ is fixed
-                    if 'rook-ceph-crashcollector' in pod_obj.name:
-                        ocp_obj = ocp.OCP(
-                            namespace=defaults.ROOK_CLUSTER_NAMESPACE
-                        )
-                        pod_name = pod_obj.name
-                        deployment_name = '-'.join(pod_name.split("-")[:-2])
-                        command = f"delete deployment {deployment_name}"
-                        ocp_obj.exec_oc_cmd(command=command)
-                        log.info(f"Deleted deployment for pod {pod_obj.name}")
-                    else:
-                        raise
+                    pod_name = pod_obj.name
+                    deployment_name = '-'.join(pod_name.split("-")[:-2])
+                    command = f"delete deployment {deployment_name}"
+                    ocp_obj.exec_oc_cmd(command=command)
+                    log.info(f"Deleted deployment for pod {pod_obj.name}")
+                else:
+                    raise
 
         # Check basic cluster functionality by creating resources
         # (pools, storageclasses, PVCs, pods - both CephFS and RBD),
