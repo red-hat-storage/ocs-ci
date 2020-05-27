ce5b6412c0e149d50a0dc2309b8728496b482934
Elad Ben Aharon <ebenahar@redhat.com>
2020-05-27 18:52:02 +0300

    skip test instead of remove it
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- tests/manage/z_cluster/cluster_expansion/test_add_capacity.py
+++ tests/manage/z_cluster/cluster_expansion/test_add_capacity.py
@@ -1,8 +1,8 @@
 import pytest
 
 from ocs_ci.framework import config
-from ocs_ci.framework.pytest_customization.marks import polarion_id
-from ocs_ci.framework.testlib import ignore_leftovers, ManageTest, tier1
+from ocs_ci.framework.pytest_customization.marks import polarion_id, pre_upgrade
+from ocs_ci.framework.testlib import ignore_leftovers, ManageTest, tier1, skipif_ocs_version
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs.resources import storage_cluster
@@ -44,6 +44,21 @@ class TestAddCapacity(ManageTest):
     Automates adding variable capacity to the cluster while IOs running
     """
     def test_add_capacity(self):
+        """
+        Test to add variable capacity to the OSD cluster while IOs running
+        """
+        add_capacity_test()
+
+
+@skipif_ocs_version('<4.5')
+@pre_upgrade
+@ignore_leftovers
+@polarion_id('OCS-1191')
+class TestAddCapacityPreUpgrade(ManageTest):
+    """
+    Automates adding variable capacity to the cluster while IOs running
+    """
+    def test_add_capacity_pre_upgrade(self):
         """
         Test to add variable capacity to the OSD cluster while IOs running
         """
