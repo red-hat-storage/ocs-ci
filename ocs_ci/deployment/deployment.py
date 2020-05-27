045b0a5821a2ce6dc54956ddac532df8425bb8d3
vavuthu <vavuthu@redhat.com>
2020-05-27 10:22:52 +0530

    changing parameter name
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -784,12 +784,12 @@ def setup_local_storage():
 
 
 @retry(AssertionError, 12, 10, 1)
-def verify_pvs_created(num_of_pvs):
+def verify_pvs_created(expected_pvs):
     """
     Verify that PVs were created and are in the Available state
 
     Args:
-        num_of_pvs (int): number of PVs to verify
+        expected_pvs (int): number of PVs to verify
 
     Raises:
         AssertionError: if any PVs are not in the Available state or if the
@@ -800,13 +800,13 @@ def verify_pvs_created(num_of_pvs):
     out = run_cmd("oc get pv -o json")
     pv_json = json.loads(out)
     assert pv_json['items'], (
-        f"No PVs created but we are expecting {num_of_pvs}"
+        f"No PVs created but we are expecting {expected_pvs}"
     )
 
     # check number of PVs created
     num_pvs = len(pv_json['items'])
-    assert num_pvs == num_of_pvs, (
-        f"{num_pvs} PVs created but we are expecting {num_of_pvs}"
+    assert num_pvs == expected_pvs, (
+        f"{num_pvs} PVs created but we are expecting {expected_pvs}"
     )
 
     # checks the state of PV
@@ -818,7 +818,7 @@ def verify_pvs_created(num_of_pvs):
             f"{pv_name} not in 'Available' state. Current state is {pv_state}"
         )
 
-    logger.debug("PVs, Workers: %s, %s", num_pvs, num_of_pvs)
+    logger.debug("PVs, Workers: %s, %s", num_pvs, expected_pvs)
 
 
 def get_device_paths(worker_names):
