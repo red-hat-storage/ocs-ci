266043cba66761b85590fccac7ee7a23a1d95b95
Petr Balogh <pbalogh@redhat.com>
2020-05-28 11:20:51 +0200

    Fix AWS cleanup and reduce time for 16 hours for PR validation
    Fixes: #2182
    
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- ocs_ci/cleanup/aws/defaults.py
+++ ocs_ci/cleanup/aws/defaults.py
@@ -3,7 +3,13 @@ Defaults module for AWS cleanup
 """
 AWS_REGION = 'us-east-2'
 CLUSTER_PREFIXES_SPECIAL_RULES = {
-    'jnk': 36, 'dnd': 'never', 'lr1': 24, 'lr2': 48, 'lr3': 72, 'lr4': 96,
+    'jnk-pr': 16,  # keep it as first item before jnk prefix for fist match
+    'jnk': 36,
+    'dnd': 'never',
+    'lr1': 24,
+    'lr2': 48,
+    'lr3': 72,
+    'lr4': 96,
     'lr5': 120
 }
 MINIMUM_CLUSTER_RUNNING_TIME = 10
