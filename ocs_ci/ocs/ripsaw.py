87967fdc715aa33f9f85c5accb5fe2ab8ad647f7
Krishnaram Karthick Ramdoss <kramdoss@redhat.com>
2020-06-02 08:54:19 +0530

    FIO test using ripsaw tool (#1895)
    * Adds FIO performance test using ripsaw
    
    Signed-off-by: Krishnaram Karthick <kramdoss@redhat.com>

--- ocs_ci/ocs/ripsaw.py
+++ ocs_ci/ocs/ripsaw.py
@@ -12,6 +12,7 @@ from subprocess import run, CalledProcessError
 from ocs_ci.utility.utils import run_cmd
 from ocs_ci.ocs.constants import RIPSAW_NAMESPACE
 
+
 log = logging.getLogger(__name__)
 
 
@@ -82,7 +83,6 @@ class RipSaw(object):
         Args:
             crd (str): Name of file to apply
         """
-        self.crd = crd
         self.dir += '/ripsaw'
         run('oc apply -f deploy', shell=True, check=True, cwd=self.dir)
         run(f'oc apply -f {crd}', shell=True, check=True, cwd=self.dir)
