4f24878aec7cf007b7bb6ded11ee3f7b1514d99c
Petr Balogh <pbalogh@redhat.com>
2020-06-09 21:49:11 +0200

    WA: Skip logging deployment for OCP 4.5
    W/A issue:
    https://github.com/red-hat-storage/ocs-ci/issues/2278
    
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- tests/e2e/logging/test_openshift-logging.py
+++ tests/e2e/logging/test_openshift-logging.py
@@ -15,6 +15,7 @@ from ocs_ci.utility.retry import retry
 from ocs_ci.framework.pytest_customization.marks import skipif_aws_i3
 from ocs_ci.framework.testlib import E2ETest, workloads, tier1, ignore_leftovers
 from ocs_ci.utility import deployment_openshift_logging as ocp_logging_obj
+from ocs_ci.utility.utils import get_ocp_version
 
 logger = logging.getLogger(__name__)
 
@@ -28,6 +29,13 @@ def setup_fixture(install_logging):
     logger.info("Testcases execution post deployment of openshift-logging")
 
 
+@pytest.mark.skipif(
+    get_ocp_version() == "4.5",
+    reason=(
+        "Skipping logging tests on OCP 4.5 cause of W/A issue: "
+        "https://github.com/red-hat-storage/ocs-ci/issues/2278"
+    )
+)
 @pytest.mark.usefixtures(
     setup_fixture.__name__
 )
