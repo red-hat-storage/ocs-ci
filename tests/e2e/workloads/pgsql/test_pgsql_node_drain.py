bdb4fce13b5582df09afef9028665554151fb307
OdedViner <61982127+OdedViner@users.noreply.github.com>
2020-06-15 16:33:32 +0300

    pgsql_debug (#2262)
    * pgsql_debug
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * Change default on get_node_utilization method
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * 1.Fix regular expression issue in "get_node_resource_utilization_from_adm_top" function
    2.Print table on functions "get_node_resource_utilization_from_adm_top" and "get_node_resource_utilization_from_oc_describe"
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * pgsql tests use global function to get node utilization.
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * Chnge code according to comments
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * validate_pgbench_run method
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * Fix code
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * pep8 fix
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * fix code
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * add print_table_node_resource_utilization function
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    * pep8 issue
    
    Signed-off-by: Oded Viner <odedviner@localhost.localdomain>
    
    Co-authored-by: Oded Viner <odedviner@localhost.localdomain>

--- tests/e2e/workloads/pgsql/test_pgsql_node_drain.py
+++ tests/e2e/workloads/pgsql/test_pgsql_node_drain.py
@@ -1,6 +1,6 @@
 import logging
 import pytest
-
+from datetime import datetime
 from ocs_ci.ocs import constants
 from ocs_ci.ocs import node
 from tests.sanity_helpers import Sanity
@@ -8,7 +8,7 @@ from ocs_ci.framework.testlib import (
     E2ETest, workloads
 )
 from ocs_ci.ocs.pgsql import Postgresql
-
+from ocs_ci.ocs.node import get_node_resource_utilization_from_adm_top
 
 log = logging.getLogger(__name__)
 
@@ -53,8 +53,16 @@ class TestPgSQLNodeReboot(E2ETest):
             replicas=3, transactions=transactions, clients=3
         )
 
+        # Start measuring time
+        start_time = datetime.now()
+
         # Wait for pgbench pod to reach running state
         pgsql.wait_for_pgbench_status(status=constants.STATUS_RUNNING)
+
+        # Check worker node utilization (adm_top)
+        get_node_resource_utilization_from_adm_top(
+            node_type='worker', print_table=True
+        )
 
         # Node drain with specific node type
         typed_nodes = node.get_typed_nodes(
@@ -73,6 +81,11 @@ class TestPgSQLNodeReboot(E2ETest):
 
         # Wait for pg_bench pod to complete
         pgsql.wait_for_pgbench_status(status=constants.STATUS_COMPLETED)
+
+        # Calculate the time from running state to completed state
+        end_time = datetime.now()
+        diff_time = end_time - start_time
+        log.info(f"\npgbench pod reached to completed state after {diff_time.seconds} seconds\n")
 
         # Get pgbench pods
         pgbench_pods = pgsql.get_pgbench_pods()
