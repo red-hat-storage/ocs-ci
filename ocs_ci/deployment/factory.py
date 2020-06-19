33ebfb90c9989820a9c4b7d8e05dce9cd679923f
Martin Bukatovič <mbukatov@redhat.com>
2020-06-19 12:07:59 +0200

    fixing flake8 long line error
    Signed-off-by: Martin Bukatovič <mbukatov@redhat.com>

--- ocs_ci/deployment/factory.py
+++ ocs_ci/deployment/factory.py
@@ -18,7 +18,11 @@ class DeploymentFactory(object):
         # A map all existing deployments and respective classes
         # should be put here
         self.cls_map = {
-            'aws_ipi': AWSIPI, 'aws_upi': AWSUPI, 'azure_ipi': AZUREIPI, 'vsphere_upi': VSPHEREUPI, 'baremetal_upi': BAREMETALUPI,
+            'aws_ipi': AWSIPI,
+            'aws_upi': AWSUPI,
+            'azure_ipi': AZUREIPI,
+            'vsphere_upi': VSPHEREUPI,
+            'baremetal_upi': BAREMETALUPI,
         }
 
     def get_deployment(self):
