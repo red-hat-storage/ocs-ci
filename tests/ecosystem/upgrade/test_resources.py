e379471e88d9bbe6fd11c9c05df9c6fcf7cc5ae3
Filip Balak <fbalak@redhat.com>
2020-06-22 16:10:04 +0200

    use multiregion_mirror_setup_session in test_storage_pods_running
    Signed-off-by: Filip Balak <fbalak@redhat.com>

--- tests/ecosystem/upgrade/test_resources.py
+++ tests/ecosystem/upgrade/test_resources.py
@@ -15,10 +15,14 @@ log = logging.getLogger(__name__)
 
 @post_upgrade
 @pytest.mark.polarion_id("OCS-2220")
-def test_storage_pods_running():
+def test_storage_pods_running(multiregion_mirror_setup_session):
     """
     Test that all pods from openshift-storage namespace have status Running
     or Completed after upgrade is completed.
+
+    multiregion_mirror_setup_session fixture is present during this test to
+    make sure that NooBaa backing stores from other upgrade tests were
+    not yet deleted. This is done to test scenario from BZ 1823775.
 
     """
     wait_for_storage_pods(timeout=10), 'Some pods were not in expected state'
