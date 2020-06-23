4df19aed3e157007f9c0f1403a49293eb0333a0a
Akarsha Rai <akrai@redhat.com>
2020-06-23 21:41:41 +0530

    Run amq benchmark pod and get test results (#2268)
    * Run amq benchmark pod and get test results
    
    Signed-off-by: Akarsha <akrai@redhat.com>

--- tests/e2e/workloads/amq/test_amq_streamer_creation.py
+++ tests/e2e/workloads/amq/test_amq_streamer_creation.py
@@ -1,10 +1,10 @@
 import logging
 import pytest
-import time
 
 from ocs_ci.framework.testlib import E2ETest, workloads
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.amq import AMQ
+from ocs_ci.utility import templating
 from tests.helpers import default_storage_class
 
 log = logging.getLogger(__name__)
@@ -29,13 +29,10 @@ class TestAMQBasics(E2ETest):
         argvalues=[
             pytest.param(
                 constants.CEPHBLOCKPOOL, marks=pytest.mark.polarion_id("OCS-2217")
-            ),
-            pytest.param(
-                constants.CEPHFILESYSTEM, marks=pytest.mark.polarion_id("OCS-2218")
             )
         ]
     )
-    def test_install_amq_cephfs(self, interface, test_fixture_amq):
+    def test_install_and_run_amq_benchmark(self, interface, test_fixture_amq):
         """
         Create amq cluster and run open messages on it
 
@@ -46,15 +43,11 @@ class TestAMQBasics(E2ETest):
         # Deploy amq cluster
         test_fixture_amq.setup_amq_cluster(sc.name)
 
-        # Run open messages
-        test_fixture_amq.create_messaging_on_amq()
-
-        # Wait for some time to generate msg
-        waiting_time = 60
-        log.info(f"Waiting for {waiting_time}sec to generate msg")
-        time.sleep(waiting_time)
-
-        # Check messages are sent and received
-        threads = test_fixture_amq.run_in_bg()
-        for t in threads:
-            t.join()
+        # Run benchmark
+        amq_workload_dict = templating.load_yaml(constants.AMQ_WORKLOAD_YAML)
+        amq_workload_dict['producersPerTopic'] = 3
+        amq_workload_dict['consumerPerSubscription'] = 3
+        result = test_fixture_amq.run_amq_benchmark(amq_workload_yaml=amq_workload_dict)
+        assert test_fixture_amq.validate_amq_benchmark(result, amq_workload_dict) is not None, (
+            "Benchmark did not completely run or might failed in between"
+        )
