e64ebc112c164a75e05257256ffc7f9574555b40
Filip Balak <fbalak@redhat.com>
2020-06-29 16:07:30 +0200

    improve logging
    Signed-off-by: Filip Balak <fbalak@redhat.com>

--- tests/ecosystem/upgrade/test_noobaa.py
+++ tests/ecosystem/upgrade/test_noobaa.py
@@ -8,6 +8,7 @@ from ocs_ci.framework.pytest_customization.marks import (
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.constants import BS_OPTIMAL
 from ocs_ci.ocs.exceptions import TimeoutExpiredError
+from ocs_ci.ocs.resources import pod
 from tests.manage.mcg.helpers import (
     retrieve_test_objects_to_pod, sync_object_directory
 )
@@ -35,7 +36,7 @@ def wait_for_active_pods(job, desired_count, timeout=3):
         bool: If job has desired number of active pods
 
     """
-    job_name = job.ocp.resource_name
+    job_name = job.name
     logger.info(f"Checking number of active pods for job {job_name}")
 
     def _retrieve_job_state():
@@ -58,6 +59,23 @@ def wait_for_active_pods(job, desired_count, timeout=3):
         logger.error(
             f"Job {job_name} doesn't have correct number of active pods ({desired_count})"
         )
+        job_pods = pod.get_pods_having_label(
+            f"job-name={job_name}",
+            job.namespace
+        )
+        for job_pod in job_pods:
+            logger.info(
+                f"Description of job pod {job_pod['metadata']['name']}: {job_pod}"
+            )
+            pod_logs = pod.get_pod_logs(
+                job_pod['metadata']['name'],
+                namespace=job_pod['metadata']['namespace']
+            )
+            logger.info(
+                f"Logs from job pod {job_pod['metadata']['name']}: {pod_logs}"
+            )
+
+
         return False
 
 
@@ -210,7 +228,9 @@ def test_start_upgrade_mcg_io(mcg_workload_job):
     Confirm that there is MCG workload job running before upgrade.
     """
     # wait a few seconds for fio job to start
-    assert wait_for_active_pods(mcg_workload_job, 1, timeout=20)
+    assert wait_for_active_pods(mcg_workload_job, 1, timeout=20), (
+        f"Job {mcg_workload_job.name} doesn't have any running pod"
+    )
 
 
 @post_upgrade
@@ -219,4 +239,6 @@ def test_upgrade_mcg_io(mcg_workload_job):
     """
     Confirm that there is MCG workload job running after upgrade.
     """
-    assert wait_for_active_pods(mcg_workload_job, 1)
+    assert wait_for_active_pods(mcg_workload_job, 1), (
+        f"Job {mcg_workload_job.name} doesn't have any running pod"
+    )
