f1c234ae78b6e11f451d813b3b1fcb085c442e56
aviadp <apolak@redhat.com>
2020-06-29 18:20:07 +0300

    rebase before pushing
    1st test created, moved test_upgrade content to ocs_upgeade.py
    
    in progress
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    function worker shutdown completed
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    first version of calling tier4 test from upgrade test
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    moved upgrade function to ocs_upgrade, some temporary function added
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    removed get_node_name function from test module
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    fixed docstring
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    abrupt and perm shutdown tests ready
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    using pytest parametrize marker instead of dictionary, fixed docstrings, removed function duplications
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    removed unused f string
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    fixed pytest.parametrize (removed unwanted list)
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    added nodes fixture to default test_upgrade function
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    fixed condition for rook ct pod
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    Signed-off-by: aviadp <apolak@redhat.com>
    
    added new module: disruptive_operations, split test functions, using args and kwargs instead specifit arguments, removed use of nodes fixture
    
    Signed-off-by: aviadp <apolak@redhat.com>

--- tests/ecosystem/upgrade/test_upgrade.py
+++ tests/ecosystem/upgrade/test_upgrade.py
@@ -1,19 +1,41 @@
 import logging
 
+import pytest
+
 from ocs_ci.framework.testlib import ocs_upgrade
-from ocs_ci.framework import config
-from ocs_ci.ocs.cluster import CephCluster, CephHealthMonitor
-from ocs_ci.ocs.resources.storage_cluster import (
-    ocs_install_verification
-)
-from ocs_ci.utility.utils import TimeoutSampler
-from ocs_ci.ocs.ocs_upgrade import (
-    verify_image_versions,
-    OCSUpgrade,
-)
-from ocs_ci.ocs.exceptions import TimeoutException
+from ocs_ci.ocs.disruptive_operations import worker_node_shutdown
+from ocs_ci.ocs.ocs_upgrade import run_ocs_upgrade
+
 
 log = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize(
+    argnames=["operation"], argvalues=[
+        pytest.param(
+            worker_node_shutdown, marks=pytest.mark.polarion_id("OCS-1579")
+        ),
+    ]
+)
+def test_worker_node_abrupt_shutdown(operation):
+    log.info("Starting disruptive test function: "
+             "test_worker_node_abrupt_shutdown")
+    abrupt = True
+    run_ocs_upgrade(operation, abrupt)
+
+
+@pytest.mark.parametrize(
+    argnames=["operation"], argvalues=[
+        pytest.param(
+            worker_node_shutdown, marks=pytest.mark.polarion_id("OCS-1575")
+        ),
+    ]
+)
+def test_worker_node_permanent_shutdown(operation):
+    log.info("Starting disruptive test function:"
+             " test_worker_node_permanent_shutdown")
+    abrupt = False
+    run_ocs_upgrade(operation, abrupt)
 
 
 @ocs_upgrade
@@ -22,45 +44,5 @@ def test_upgrade():
     Tests upgrade procedure of OCS cluster
 
     """
-    ceph_cluster = CephCluster()
-    upgrade_ocs = OCSUpgrade(
-        namespace=config.ENV_DATA['cluster_namespace'],
-        version_before_upgrade=config.ENV_DATA.get("ocs_version"),
-        ocs_registry_image=config.UPGRADE.get('upgrade_ocs_registry_image'),
-        upgrade_in_current_source=config.UPGRADE.get('upgrade_in_current_source', False)
-    )
-    upgrade_version = upgrade_ocs.get_upgrade_version()
-    assert upgrade_ocs.get_parsed_versions()[1] >= upgrade_ocs.get_parsed_versions()[0], (
-        f"Version you would like to upgrade to: {upgrade_version} "
-        f"is not higher or equal to the version you currently running: "
-        f"{upgrade_ocs.version_before_upgrade}"
-    )
-    csv_name_pre_upgrade = upgrade_ocs.get_csv_name_pre_upgrade()
-    pre_upgrade_images = upgrade_ocs.get_pre_upgrade_image(csv_name_pre_upgrade)
-    upgrade_ocs.load_version_config_file(upgrade_version)
-    with CephHealthMonitor(ceph_cluster):
-        channel = upgrade_ocs.set_upgrade_channel()
-        upgrade_ocs.set_upgrade_images()
-        upgrade_ocs.update_subscription(channel)
-        for sample in TimeoutSampler(
-            timeout=725,
-            sleep=5,
-            func=upgrade_ocs.check_if_upgrade_completed,
-            channel=channel,
-            csv_name_pre_upgrade=csv_name_pre_upgrade,
-        ):
-            try:
-                if sample:
-                    log.info("Upgrade success!")
-                    break
-            except TimeoutException:
-                raise TimeoutException("No new CSV found after upgrade!")
-        old_image = upgrade_ocs.get_images_post_upgrade(
-            channel, pre_upgrade_images, upgrade_version
-        )
-    verify_image_versions(old_image, upgrade_ocs.get_parsed_versions()[1])
-    ocs_install_verification(
-        timeout=600, skip_osd_distribution_check=True,
-        ocs_registry_image=upgrade_ocs.ocs_registry_image,
-        post_upgrade_verification=True,
-    )
+
+    run_ocs_upgrade()
