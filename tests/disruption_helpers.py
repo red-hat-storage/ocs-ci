d12f26cc8ceda53ddd92784d74d3e3a2acbef194
Elad Ben Aharon <ebenahar@redhat.com>
2020-07-12 18:32:02 +0300

    Few fixes in tests/disruption_helpers.py
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- tests/disruption_helpers.py
+++ tests/disruption_helpers.py
@@ -9,65 +9,54 @@ from ocs_ci.ocs.exceptions import TimeoutExpiredError
 
 log = logging.getLogger(__name__)
 
-POD = ocp.OCP(kind=constants.POD, namespace=config.ENV_DATA['cluster_namespace'])
-
 
 class Disruptions:
     """
     This class contains methods of disrupt operations
     """
-    resource = None
-    resource_obj = None
-    resource_count = 0
-    selector = None
-    daemon_pid = None
+    def __init__(self):
+        self.resource = None
+        self.resource_obj = None
+        self.resource_count = 0
+        self.selector = None
+        self.daemon_pid = None
+        self.pod_obj = (
+            ocp.OCP(kind=constants.POD, namespace=config.ENV_DATA['cluster_namespace'])
+        )
 
     def set_resource(self, resource):
         self.resource = resource
         resource_count = 0
-        if self.resource == 'mgr':
-            self.resource_obj = pod.get_mgr_pods()
-            self.selector = constants.MGR_APP_LABEL
-        if self.resource == 'mon':
-            self.resource_obj = pod.get_mon_pods()
-            self.selector = constants.MON_APP_LABEL
-        if self.resource == 'osd':
-            self.resource_obj = pod.get_osd_pods()
-            self.selector = constants.OSD_APP_LABEL
-        if self.resource == 'mds':
-            self.resource_obj = pod.get_mds_pods()
-            self.selector = constants.MDS_APP_LABEL
-        if self.resource == 'cephfsplugin':
-            self.resource_obj = pod.get_plugin_pods(
-                interface=constants.CEPHFILESYSTEM
-            )
-            self.selector = constants.CSI_CEPHFSPLUGIN_LABEL
-        if self.resource == 'rbdplugin':
-            self.resource_obj = pod.get_plugin_pods(
-                interface=constants.CEPHBLOCKPOOL
-            )
-            self.selector = constants.CSI_RBDPLUGIN_LABEL
-        if self.resource == 'cephfsplugin_provisioner':
-            self.resource_obj = [pod.plugin_provisioner_leader(
-                interface=constants.CEPHFILESYSTEM
-            )]
-            self.selector = constants.CSI_CEPHFSPLUGIN_PROVISIONER_LABEL
-            resource_count = len(pod.get_cephfsplugin_provisioner_pods())
-        if self.resource == 'rbdplugin_provisioner':
-            self.resource_obj = [pod.plugin_provisioner_leader(
-                interface=constants.CEPHBLOCKPOOL
-            )]
-            self.selector = constants.CSI_RBDPLUGIN_PROVISIONER_LABEL
-            resource_count = len(pod.get_rbdfsplugin_provisioner_pods())
-        if self.resource == 'operator':
-            self.resource_obj = pod.get_operator_pods()
-            self.selector = constants.OPERATOR_LABEL
-
+        resources_dict = {
+            'mgr': [pod.get_mgr_pods(), constants.MGR_APP_LABEL],
+            'mon': [pod.get_mon_pods(), constants.MON_APP_LABEL],
+            'osd': [pod.get_osd_pods(), constants.OSD_APP_LABEL],
+            'mds': [pod.get_mds_pods(), constants.MDS_APP_LABEL],
+            'cephfsplugin': [
+                pod.get_plugin_pods(interface=constants.CEPHFILESYSTEM),
+                constants.CSI_CEPHFSPLUGIN_LABEL
+            ],
+            'rbdplugin': [
+                pod.get_plugin_pods(interface=constants.CEPHBLOCKPOOL),
+                constants.CSI_RBDPLUGIN_LABEL
+            ],
+            'cephfsplugin_provisioner': [
+                [pod.plugin_provisioner_leader(interface=constants.CEPHFILESYSTEM)],
+                constants.CSI_RBDPLUGIN_PROVISIONER_LABEL
+            ],
+            'rbdplugin_provisioner': [
+                [pod.plugin_provisioner_leader(interface=constants.CEPHBLOCKPOOL)],
+                constants.CSI_RBDPLUGIN_PROVISIONER_LABEL,
+            ],
+            'operator': [pod.get_operator_pods(), constants.OPERATOR_LABEL]
+        }
+        self.resource_obj = resources_dict[self.resource][0]
+        self.selector = resources_dict[self.resource][1]
         self.resource_count = resource_count or len(self.resource_obj)
 
     def delete_resource(self, resource_id=0):
         self.resource_obj[resource_id].delete(force=True)
-        assert POD.wait_for_resource(
+        assert self.pod_obj.wait_for_resource(
             condition='Running', selector=self.selector,
             resource_count=self.resource_count, timeout=300
         )
