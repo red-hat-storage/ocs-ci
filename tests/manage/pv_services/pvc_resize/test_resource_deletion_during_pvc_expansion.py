3966a7eac09bdf44f0bac6b21682c657d35271d9
Jilju Joy <jijoy@redhat.com>
2020-07-20 20:42:48 +0530

    Update IO write rate to 10M
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/manage/pv_services/pvc_resize/test_resource_deletion_during_pvc_expansion.py
+++ tests/manage/pv_services/pvc_resize/test_resource_deletion_during_pvc_expansion.py
@@ -75,7 +75,7 @@ class TestResourceDeletionDuringPvcExpansion(ManageTest):
             )
             pod_obj.run_io(
                 storage_type=storage_type, size='4G', io_direction='write',
-                runtime=30, rate='100M', fio_filename=f'{pod_obj.name}_f1'
+                runtime=30, rate='10M', fio_filename=f'{pod_obj.name}_f1'
             )
 
         log.info("Wait for IO to complete on pods")
@@ -119,7 +119,7 @@ class TestResourceDeletionDuringPvcExpansion(ManageTest):
             )
             pod_obj.run_io(
                 storage_type=storage_type, size='10G', io_direction='write',
-                runtime=30, rate='100M', fio_filename=f'{pod_obj.name}_f2'
+                runtime=30, rate='10M', fio_filename=f'{pod_obj.name}_f2'
             )
 
         log.info("Wait for IO to complete on all pods")
