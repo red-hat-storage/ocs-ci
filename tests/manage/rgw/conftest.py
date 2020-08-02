f2d170e1807347f0983d6b6d29591e3320ba73eb
Ben <benq3000@gmail.com>
2020-08-02 16:02:53 +0300

    Move if to run only once
    Signed-off-by: Ben <benq3000@gmail.com>

--- tests/manage/rgw/conftest.py
+++ tests/manage/rgw/conftest.py
@@ -14,9 +14,9 @@ def pytest_collection_modifyitems(items):
         items: list of collected tests
 
     """
-    for item in items.copy():
-        if r'manage/rgw' in str(item.fspath):
-            if config.ENV_DATA['platform'].lower() not in ON_PREM_PLATFORMS:
+    if config.ENV_DATA['platform'].lower() not in ON_PREM_PLATFORMS:
+        for item in items.copy():
+            if r'manage/rgw' in str(item.fspath):
                 log.info(
                     f"Test {item} is removed from the collected items"
                     f" due to {config.ENV_DATA['platform'].lower()} not being an on-prem platform"
