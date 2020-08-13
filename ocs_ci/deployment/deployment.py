9d089e0cfd67db1c7a42d9eee24021c12e7d4129
Elad Ben Aharon <ebenahar@redhat.com>
2020-08-13 10:59:17 +0300

    Add functionality to deploy with infra worker nodes
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -211,6 +211,16 @@ class Deployment(object):
                 f"label nodes {workers_to_label} "
                 f"{constants.OPERATOR_NODE_LABEL} --overwrite"
             )
+            if config.DEPLOYMENT["infra_nodes"]:
+                logger.info(
+                    f"Label nodes: {workers_to_label} with label: "
+                    f"{constants.INFRA_NODE_LABEL}"
+                )
+                label_cmd = (
+                    f"label nodes {workers_to_label} "
+                    f"{constants.INFRA_NODE_LABEL} --overwrite"
+                )
+
             _ocp.exec_oc_cmd(command=label_cmd)
 
         workers_to_taint = " ".join(distributed_worker_nodes[:to_taint])
--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -626,6 +626,7 @@ ALERT_CLUSTERCRITICALLYFULL = 'CephClusterCriticallyFull'
 
 # OCS Deployment related constants
 OPERATOR_NODE_LABEL = "cluster.ocs.openshift.io/openshift-storage=''"
+INFRA_NODE_LABEL = "node-role.kubernetes.io/infra=''"
 TOPOLOGY_ROOK_LABEL = "topology.rook.io/rack"
 OPERATOR_NODE_TAINT = "node.ocs.openshift.io/storage=true:NoSchedule"
 OPERATOR_CATALOG_SOURCE_NAME = "ocs-catalogsource"
