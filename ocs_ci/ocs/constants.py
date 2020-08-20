8ed772b1921405b92dd0acb73177a554f3b60061
vavuthu <vavuthu@redhat.com>
2020-08-20 16:19:52 +0530

    Adding constants and utility functions for RHEL deployment for OCP 4.5 - vsphere
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -716,6 +716,7 @@ VSPHERE_NODE_USER = "core"
 VSPHERE_INSTALLER_BRANCH = "release-4.3"
 VSPHERE_INSTALLER_REPO = "https://github.com/openshift/installer.git"
 VSPHERE_SCALEUP_REPO = "https://code.engineering.redhat.com/gerrit/openshift-misc"
+VSPHERE_CLUSTER_LAUNCHER = "https://gitlab.cee.redhat.com/aosqe/cluster-launcher.git"
 VSPHERE_DIR = os.path.join(EXTERNAL_DIR, "installer/upi/vsphere/")
 INSTALLER_IGNITION = os.path.join(VSPHERE_DIR, "machine/ignition.tf")
 VM_IFCFG = os.path.join(VSPHERE_DIR, "vm/ifcfg.tmpl")
@@ -736,6 +737,14 @@ SCALEUP_VSPHERE_VARIABLES = os.path.join(SCALEUP_VSPHERE_DIR, "variables.tf")
 SCALEUP_VSPHERE_ROUTE53 = os.path.join(SCALEUP_VSPHERE_DIR, "route53/vsphere-rhel-dns.tf")
 SCALEUP_VSPHERE_ROUTE53_VARIABLES = os.path.join(SCALEUP_VSPHERE_DIR, "route53/variables.tf")
 SCALEUP_VSPHERE_MACHINE_CONF = os.path.join(SCALEUP_VSPHERE_DIR, "machines/vsphere-rhel-machine.tf")
+
+# cluster-launcher
+CLUSTER_LAUNCHER_VSPHERE_DIR = os.path.join(
+    EXTERNAL_DIR,
+    "cluster-launcher/v4-scaleup/ocp4-rhel-scaleup/"
+)
+CLUSTER_LAUNCHER_MACHINE_CONF = "vsphere/machines/vsphere-rhel-machine.tf"
+
 TERRAFORM_VARS = "terraform.tfvars"
 VM_DISK_TYPE = "thin"
 VM_DISK_MODE = "persistent"
@@ -797,6 +806,9 @@ SUPPORTED_BROWSERS = (CHROME_BROWSER)
 # Inventory
 INVENTORY_TEMPLATE = "inventory.yaml.j2"
 INVENTORY_FILE = "inventory.yaml"
+
+INVENTORY_TEMPLATE_HAPROXY = "inventory_haproxy.yaml.j2"
+INVENTORY_FILE_HAPROXY = "inventory_haproxy.yaml"
 
 # users
 VM_RHEL_USER = "test"
@@ -1067,6 +1079,7 @@ FILE_PATH = '/tmp/ceph.tar.gz'
 BOOTSTRAP_MODULE = "module.ipam_bootstrap"
 LOAD_BALANCER_MODULE = "module.ipam_lb"
 COMPUTE_MODULE = "module.ipam_compute"
+CONTROL_PLANE = "module.ipam_control_plane"
 
 # proxy location
 HAPROXY_LOCATION = "/etc/haproxy/haproxy.conf"
