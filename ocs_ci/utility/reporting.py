669086c402b909b3c452f7fe7090c68c979c7f6d
Coady LaCroix <clacroix@redhat.com>
2020-08-25 15:05:22 -0700

    Support polarion upgrade_id and decorate test_upgrade
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- ocs_ci/utility/reporting.py
+++ ocs_ci/utility/reporting.py
@@ -12,9 +12,6 @@ def get_deployment_polarion_id():
     polarion_config = config.REPORTING.get('polarion')
     if polarion_config:
         if config.UPGRADE.get('upgrade'):
-            if config.DEPLOYMENT.get('subscription_plan_approval') == 'Manual':
-                return polarion_config.get('upgrade_manual_id')
-            else:
-                return polarion_config.get('upgrade_auto_id')
+            return polarion_config.get('upgrade_id')
         else:
             return polarion_config.get('deployment_id')
