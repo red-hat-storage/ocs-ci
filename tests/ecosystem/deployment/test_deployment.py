d839310738b4162e0d76771466bb0f47972cd164
Ben Eli <belimele@redhat.com>
2020-08-31 16:54:37 +0300

    Revert MCGCLI pod commits (#2843)
    * Revert "make mcgcli_pod_fixture a prerequisite for mcg_obj fixture (#2825)"
    
    This reverts commit 1b071c3f69a35f0a80d21686674f0ce2e979e059.
    
    Signed-off-by: Ben <benq3000@gmail.com>
    
    * Revert "Skip mcg operations when deleting the cluster (#2804)"
    
    This reverts commit abe1b2860a51ce28cb051c47fe8e708ae1a1b8be.
    
    Signed-off-by: Ben <benq3000@gmail.com>
    
    * Revert "Skip downloading mcg cli for OCP deployments (#2797)"
    
    This reverts commit 7132584525c3120c18be3e86a565e9ce5ab57295.
    
    Signed-off-by: Ben <benq3000@gmail.com>
    
    * Revert "Create an MCG CLI pod to support macOS (#2768)"
    
    This reverts commit c136db23578458bd8896a735bba63cc95f676e27.
    
    Signed-off-by: Ben <benq3000@gmail.com>

--- tests/ecosystem/deployment/test_deployment.py
+++ tests/ecosystem/deployment/test_deployment.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 
 @deployment
 @polarion_id(get_deployment_polarion_id())
-def test_deployment(pvc_factory, pod_factory, mcgcli_pod_fixture):
+def test_deployment(pvc_factory, pod_factory):
     deploy = config.RUN['cli_params'].get('deploy')
     teardown = config.RUN['cli_params'].get('teardown')
     if not teardown or deploy:
