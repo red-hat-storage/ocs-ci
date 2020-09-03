ccf9db499cf5303e8dd982872d772985369f29fb
Coady LaCroix <clacroix@redhat.com>
2020-09-03 09:48:25 -0700

    Upgrade polarion ids (#2805)
    * Support polarion upgrade_id and decorate test_upgrade
    
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>
    
    * Add polarion upgrade_id for several upgrade scenarios
    
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>
    
    * Fix how we determine upgrade from deployment when retrieving polarion_id
    
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- tests/ecosystem/upgrade/test_upgrade.py
+++ tests/ecosystem/upgrade/test_upgrade.py
@@ -2,10 +2,10 @@ import logging
 
 import pytest
 
-from ocs_ci.framework.testlib import ocs_upgrade
+from ocs_ci.framework.testlib import ocs_upgrade, polarion_id
 from ocs_ci.ocs.disruptive_operations import worker_node_shutdown
 from ocs_ci.ocs.ocs_upgrade import run_ocs_upgrade
-
+from ocs_ci.utility.reporting import get_polarion_id
 
 log = logging.getLogger(__name__)
 
@@ -49,6 +49,7 @@ def test_worker_node_permanent_shutdown(teardown):
 
 
 @ocs_upgrade
+@polarion_id(get_polarion_id(upgrade=True))
 def test_upgrade():
     """
     Tests upgrade procedure of OCS cluster
