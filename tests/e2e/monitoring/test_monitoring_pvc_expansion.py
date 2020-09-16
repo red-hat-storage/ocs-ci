99a61e766379d24259c342eea7f1666ee1a9fc0d
Oded Viner <oviner@redhat.com>
2020-09-16 13:11:44 +0300

    test monitoring pvc expansion (OCS-640)
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- tests/e2e/monitoring/test_monitoring_pvc_expansion.py
+++ tests/e2e/monitoring/test_monitoring_pvc_expansion.py
@@ -1,60 +1,48 @@
-"""
-This testcase starts with minimum number of osds(one osd)
-on each node and slowly scaling it into 6 osds and then reboot worker
-nodes
-"""
-
 import logging
 import pytest
 
-from ocs_ci.ocs import constants
-from ocs_ci.framework.testlib import scale, E2ETest, ignore_leftovers
-from ocs_ci.framework.pytest_customization.marks import (
-    skipif_external_mode, skipif_aws_i3
-)
-
+from ocs_ci.framework.testlib import tier4
+from ocs_ci.ocs import ocp, constants
+from ocs_ci.framework.testlib import E2ETest
+from ocs_ci.ocs.resources.pvc import get_pvc_obj
+from ocs_ci.ocs.monitoring import prometheus_health_check
 
 logger = logging.getLogger(__name__)
 
 
-@scale
-@ignore_leftovers
-@skipif_external_mode
-@pytest.mark.parametrize(
-    argnames=["interface"],
-    argvalues=[
-        pytest.param(
-            constants.CEPHBLOCKPOOL, marks=pytest.mark.polarion_id("OCS-XXXX")
-        ),
-        pytest.param(
-            constants.CEPHFILESYSTEM, marks=pytest.mark.polarion_id("OCS-XXXX")
+@tier4
+@pytest.mark.polarion_id("OCS-640")
+class TestMonitoringPvcExpansion(E2ETest):
+    """
+    Full Fill my-prometheus-claim-prometheus-k8s-0 pvc (90%)
+    Increase PVC size and check prometheus health is ok
+    """
+
+    def test_monitoring_pvc_expansion(self, dc_pod_factory):
+        """
+        test monitoring pvc expansion
+        """
+        # Get my-prometheus-claim-prometheus-k8s-0 pvc object
+        pvc_obj = get_pvc_obj(
+            namespace=constants.MONITORING_NAMESPACE,
+            name='my-prometheus-claim-prometheus-k8s-0'
         )
-    ]
-)
-@skipif_aws_i3
-class TestScaleOSDsRebootNodes(E2ETest):
-    """
-    Add first set of OSD to a minimum cluster with 50%
-    of storage utilization and wait for rebalance
-    Reboot worker nodes after rebalance
-    """
-    num_of_pvcs = 10
-    pvc_size = 5
+        pvc_obj.project = ocp.OCP(namespace=constants.MONITORING_NAMESPACE)
 
-    def test_scale_osds_reboot_nodes(self, interface, project_factory, multi_pvc_factory, dc_pod_factory):
-        """
-        Check storage utilization, if its less then runs IO,
-        Scale osds from 3-6, check for rebalance and reboot workers
-        """
-        from ocs_ci.ocs import ocp, constants
-        ocp_obj = ocp.OCP(kind='project')
-        project_obj = ocp_obj.get(resource_name=constants.MONITORING_NAMESPACE)
+        # Get pvc size
+        pvc_size = pvc_obj.size
 
-        from ocs_ci.ocs.resources.pvc import get_all_pvc_objs
-        pvc_objs = get_all_pvc_objs(namespace=constants.MONITORING_NAMESPACE)
-        pvc_obj = pvc_objs[0]
-        pvc_obj.project = project_obj
+        # Create FIO Pod and Fill 90% from the storage
         dc_pod_obj = dc_pod_factory(pvc=pvc_obj)
         dc_pod_obj.run_io(
-            storage_type='fs', size='3G', runtime='60', fio_filename=f'{dc_pod_obj.name}_io'
+            storage_type='fs', size=int(0.9 * pvc_size),
+            runtime='60', fio_filename=f'{dc_pod_obj.name}_io'
+        )
+
+        # Increase storage my-prometheus-claim-prometheus-k8s-0 pvc
+        pvc_obj.resize_pvc(new_size=int(self.pvc_size * 1.4), verify=True)
+
+        # Validate the prometheus health is ok
+        assert prometheus_health_check(), (
+            "Prometheus cluster health is not OK"
         )
