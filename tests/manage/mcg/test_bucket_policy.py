75537424840fcdad590413445fedb31d8988ccfd
Parikshith <pbyregow@redhat.com>
2020-10-02 18:04:09 +0530

    length of lines
    Signed-off-by: Parikshith <pbyregow@redhat.com>

--- tests/manage/mcg/test_bucket_policy.py
+++ tests/manage/mcg/test_bucket_policy.py
@@ -267,8 +267,13 @@ class TestS3BucketPolicy(ManageTest):
 
         # Put object using OBC account(as an Admin)
         if float(config.ENV_DATA['ocs_version']) >= 4.6:
-            logger.info(f'Putting an object on bucket: {obc_obj.bucket_name} using user: {obc_obj.obc_account}')
-            assert s3_put_object(obc_obj, obc_obj.bucket_name, object_key, data), "Failed: Put Object"
+            logger.info(
+                f'Putting an object on bucket: {obc_obj.bucket_name} using user:'
+                f' {obc_obj.obc_account}'
+            )
+            assert s3_put_object(
+                obc_obj, obc_obj.bucket_name, object_key, data
+            ), "Failed: Put Object"
 
         # Verifying whether Get, Delete object is allowed
         logger.info(f'Getting object on bucket: {obc_obj.bucket_name} with user: {user.email_id}')
