34ed777b8f6530c0447a92756c8f2c087bee9c41
Yulia Persky <ypersky@redhat.com>
2020-10-02 15:24:08 +0300

    added buzgilla mark
    Signed-off-by: Yulia Persky <ypersky@redhat.com>

--- tests/e2e/performance/test_single_pvc_clone_perf.py
+++ tests/e2e/performance/test_single_pvc_clone_perf.py
@@ -14,6 +14,9 @@ from ocs_ci.framework.testlib import (
 from ocs_ci.ocs.resources import pvc
 from ocs_ci.ocs.resources import pod
 from tests import helpers
+from ocs_ci.framework.pytest_customization.marks import (
+    bugzilla
+)
 
 logger = logging.getLogger(__name__)
 
@@ -49,6 +52,7 @@ logger = logging.getLogger(__name__)
     ]
 )
 @performance
+@bugzilla('1882504')
 class TestPVCSingleClonePerformance(E2ETest):
     """
     Test to verify single PVC clone creation and deletion performance
