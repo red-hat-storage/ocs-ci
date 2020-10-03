d2199e7a5a90ff1256248a2336748a04b6ae1305
Yulia Persky <ypersky@redhat.com>
2020-10-03 15:58:53 +0300

    removed bugzilla marker
    Signed-off-by: Yulia Persky <ypersky@redhat.com>

--- tests/e2e/performance/test_single_pvc_clone_perf.py
+++ tests/e2e/performance/test_single_pvc_clone_perf.py
@@ -14,9 +14,7 @@ from ocs_ci.framework.testlib import (
 from ocs_ci.ocs.resources import pvc
 from ocs_ci.ocs.resources import pod
 from tests import helpers
-from ocs_ci.framework.pytest_customization.marks import (
-    bugzilla
-)
+
 
 logger = logging.getLogger(__name__)
 
@@ -52,7 +50,6 @@ logger = logging.getLogger(__name__)
     ]
 )
 @performance
-@bugzilla('1882504')
 class TestPVCSingleClonePerformance(E2ETest):
     """
     Test to verify single PVC clone creation and deletion performance
