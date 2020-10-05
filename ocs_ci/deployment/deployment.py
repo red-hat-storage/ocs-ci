c532bdea3952e9a460181abb6038d20aae043757
Pratik Surve <prsurve@redhat.com>
2020-10-05 13:29:43 +0530

    Add log message
    Signed-off-by: Pratik Surve <prsurve@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -995,6 +995,7 @@ def setup_local_storage(storageclass):
             "Creating optional operators CatalogSource and ImageContentSourcePolicy"
         )
         run_cmd(f"oc create -f {optional_operators_yaml.name}")
+        logger.info("Sleeping for 60 sec to start update machineconfigpool status")
         # sleep here to start update machineconfigpool status
         time.sleep(60)
         wait_for_machineconfigpool_status('all')
