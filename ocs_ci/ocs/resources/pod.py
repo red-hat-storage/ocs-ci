8556a140f965bde0ba19112fc841baf3531da319
Itzhak Kave <yitzhak@localhost.localdomain>
2020-10-06 13:44:25 +0300

    start implement node replacement for vSphere LSO
    Signed-off-by: Itzhak Kave <yitzhak@localhost.localdomain>

--- ocs_ci/ocs/resources/pod.py
+++ ocs_ci/ocs/resources/pod.py
@@ -1693,3 +1693,22 @@ def check_toleration_on_pods(toleration_key=constants.TOLERATION_KEY):
             logger.error(
                 f"The pod {resource_name} does not have toleration {toleration_key}"
             )
+
+
+def get_mon_deployments(osd_label=constants.OSD_APP_LABEL, namespace=None):
+    """
+    Fetches info about osd deployments in the cluster
+
+    Args:
+        osd_label (str): label associated with osd deployments
+            (default: defaults.OSD_APP_LABEL)
+        namespace (str): Namespace in which ceph cluster lives
+            (default: defaults.ROOK_CLUSTER_NAMESPACE)
+
+    Returns:
+        list: OSD deployment OCS instances
+    """
+    namespace = namespace or config.ENV_DATA['cluster_namespace']
+    mons = get_deployments_having_label(osd_label, namespace)
+    mon_deployments = [OCS(**mon) for mon in mons]
+    return mon_deployments
