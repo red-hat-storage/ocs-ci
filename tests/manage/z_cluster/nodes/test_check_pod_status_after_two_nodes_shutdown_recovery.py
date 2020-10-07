8f4f1623eadd4292c60ae3c970c68ec21f69670a
Shrivaibavi Raghaventhiran <sraghave@redhat.com>
2020-10-07 18:28:05 +0530

    Some log messages and doc text changes
    Signed-off-by: Shrivaibavi Raghaventhiran <sraghave@redhat.com>

--- tests/manage/z_cluster/nodes/test_check_pod_status_after_two_nodes_shutdown_recovery.py
+++ tests/manage/z_cluster/nodes/test_check_pod_status_after_two_nodes_shutdown_recovery.py
@@ -2,13 +2,12 @@ import logging
 import pytest
 import time
 
-from ocs_ci.ocs import constants
 from ocs_ci.framework.testlib import (
     ManageTest, tier4c, ignore_leftovers
 )
 from tests.sanity_helpers import Sanity
 from ocs_ci.ocs.node import (
-    wait_for_nodes_status, get_node_objs, get_typed_nodes
+    wait_for_nodes_status, get_typed_nodes
 )
 from ocs_ci.utility.retry import retry
 from ocs_ci.ocs.exceptions import CommandFailed, ResourceWrongStatusException
@@ -23,8 +22,8 @@ log = logging.getLogger(__name__)
 @tier4c
 class TestOCSWorkerNodeShutdown(ManageTest):
     """
-    Test case validate both the MDS pods not running on same node
-    post shutdown and recovery
+    Test case validate both the MDS pods rbd and cephfs plugin Provisioner
+    pods and not running on same node post shutdown and recovery
 
     """
 
@@ -37,34 +36,8 @@ class TestOCSWorkerNodeShutdown(ManageTest):
 
         self.sanity_helpers = Sanity()
 
-    @pytest.fixture(autouse=True)
-    def teardown(self, request, nodes):
-        """
-        Restart nodes that are in status NotReady
-        for situations in which the test failed in between
-
-        """
-
-        def finalizer():
-
-            # Validate all nodes are in READY state
-            not_ready_nodes = [
-                n for n in get_node_objs() if n
-                .ocp.get_resource_status(n.name) == constants.NODE_NOT_READY
-            ]
-            log.warning(
-                f"Nodes in NotReady status found: {[n.name for n in not_ready_nodes]}"
-            )
-            if not_ready_nodes:
-                nodes.restart_nodes_by_stop_and_start(not_ready_nodes)
-                wait_for_nodes_status()
-
-            log.info("All nodes are in Ready status")
-
-        request.addfinalizer(finalizer)
-
     @pytest.mark.polarion_id("OCS-2315")
-    def test_node_after_shutdown_and_recovery_worker_node(self, nodes):
+    def test_node_after_shutdown_and_recovery_worker_node(self, nodes, node_restart_teardown):
         """
         Test case to check MDS pods rbd and cephfs plugin Provisioner
         pods not running on same node post shutdown and recovery node
@@ -124,11 +97,14 @@ class TestOCSWorkerNodeShutdown(ManageTest):
         assert len(set(mds_running_nodes_after_recovery)) == len(
             mds_running_nodes_after_recovery
         ), "MDS running on same node, Not expected!!!"
+        log.info("MDS pods not running on same node")
 
         assert len(set(rbd_provisioner_running_nodes_after_recovery)) == len(
             rbd_provisioner_running_nodes_after_recovery
         ), "rbd plugin provisioner pods running on Same node, Not expected"
+        log.info("RBD plugin provisioner pods not running on same node")
 
         assert len(set(cephfs_provisioner_running_nodes_after_recovery)) == len(
             cephfs_provisioner_running_nodes_after_recovery
         ), "cephfs plugin provisioner pods running on Same node, Not expected"
+        log.info("CEPHFS plugin provisioner pods not running on same node")
