2d6115beacbaa97918fae3ef692991bb45d7c4ae
Prajith Kesava Prasad <54884015+pkesavap@users.noreply.github.com>
2020-10-08 18:33:47 +0530

    Add new platform RHV support for testing
    Signed-off-by: Prajith Kesava Prasad <pkesavap@redhat.com>

--- ocs_ci/deployment/rhv.py
+++ ocs_ci/deployment/rhv.py
@@ -2,11 +2,11 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class RHVIPI:
     """
     Base structure class to handle RHV IPI specific deployment
     """
 
     def __init__(self):
-        logger.info("RHV IPI");
-
+        logger.info("RHV IPI")
