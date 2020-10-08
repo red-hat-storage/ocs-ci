e8a4ab31a295b950a0983686f9c42974c1a1bff8
Yulia Persky <ypersky@redhat.com>
2020-10-08 19:58:55 +0300

    added speed calculation for clone creation and deletion
    Signed-off-by: Yulia Persky <ypersky@redhat.com>

--- tests/e2e/performance/test_single_pvc_clone_perf.py
+++ tests/e2e/performance/test_single_pvc_clone_perf.py
@@ -1,5 +1,6 @@
 """
-Test to verify clone creation and deletion performance
+Test to verify clone creation and deletion performance for PVC with data written to it.
+Performance is this test is measured by collecting clone creation/deletion speed.
 """
 import logging
 import pytest
@@ -22,35 +23,36 @@ logger = logging.getLogger(__name__)
     argnames=["interface_type", "pvc_size", "file_size"],
     argvalues=[
         pytest.param(
-            *[constants.CEPHBLOCKPOOL, '1', '600M'], marks=pytest.mark.polarion_id('OCS-2356')
+            *[constants.CEPHBLOCKPOOL, '1', '600'], marks=pytest.mark.polarion_id('OCS-2356')
         ),
         pytest.param(
-            *[constants.CEPHBLOCKPOOL, '25', '15GB'], marks=pytest.mark.polarion_id('OCS-2340')
+            *[constants.CEPHBLOCKPOOL, '25', '15000'], marks=pytest.mark.polarion_id('OCS-2340')
         ),
         pytest.param(
-            *[constants.CEPHBLOCKPOOL, '50', '30GB'], marks=pytest.mark.polarion_id('OCS-2357')
+            *[constants.CEPHBLOCKPOOL, '50', '30000'], marks=pytest.mark.polarion_id('OCS-2357')
         ),
         pytest.param(
-            *[constants.CEPHBLOCKPOOL, '100', '60GB'], marks=pytest.mark.polarion_id('OCS-2358')
+            *[constants.CEPHBLOCKPOOL, '100', '60000'], marks=pytest.mark.polarion_id('OCS-2358')
         ),
         pytest.param(
-            *[constants.CEPHFILESYSTEM, '1', '600M'], marks=pytest.mark.polarion_id('2341')
+            *[constants.CEPHFILESYSTEM, '1', '600'], marks=pytest.mark.polarion_id('2341')
         ),
         pytest.param(
-            *[constants.CEPHFILESYSTEM, '25', '15GB'], marks=pytest.mark.polarion_id('2355')
+            *[constants.CEPHFILESYSTEM, '25', '15000'], marks=pytest.mark.polarion_id('2355')
         ),
         pytest.param(
-            *[constants.CEPHFILESYSTEM, '50', '30GB'], marks=pytest.mark.polarion_id('2359')
+            *[constants.CEPHFILESYSTEM, '50', '30000'], marks=pytest.mark.polarion_id('2359')
         ),
         pytest.param(
-            *[constants.CEPHFILESYSTEM, '100', '60GB'], marks=pytest.mark.polarion_id('2360')
+            *[constants.CEPHFILESYSTEM, '100', '60000'], marks=pytest.mark.polarion_id('2360')
         )
     ]
 )
 @performance
 class TestPVCSingleClonePerformance(E2ETest):
     """
-    Test to verify single PVC clone creation and deletion performance
+    Test to verify clone creation and deletion performance for PVC with data written to it.
+    Performance is this test is measured by collecting clone creation/deletion speed.
     """
 
     @pytest.fixture(autouse=True)
@@ -78,22 +80,28 @@ class TestPVCSingleClonePerformance(E2ETest):
 
     def test_clone_create_delete_performance(self, interface_type, pvc_size, file_size, teardown_factory):
         """
+        Write data (60% of PVC capacity) to the PVC created in setup
         Create single clone for an existing pvc,
-        Measure clone creation times
+        Measure clone creation time and speed
         Delete the created clone
-        Measure clone deletion time
-        Note: by increasing max_num_of_clones value you increase created clones number
+        Measure clone deletion time and speed
+        Note: by increasing max_num_of_clones value you increase number of the clones to be created/deleted
         """
-        logger.info(f"Running IO on pod {self.pod_obj.name}")
+
         file_name = self.pod_obj.name
-        logger.info(f"File {file_name} of {file_size} size was created during IO.")
-        self.pod_obj.run_io(
-            storage_type='fs', size=file_size, fio_filename=file_name, io_direction='wo', runtime=180, bs='1024K'
-        )
+        logger.info(f'Starting IO on the POD {self.pod_obj.name}')
+        # Going to run only write IO to fill the PVC for the before creating a clone
+        file_size_str = f'{file_size}M'
+        self.pod_obj.fillup_fs(size=file_size_str, fio_filename=file_name)
 
         # Wait for fio to finish
-        self.pod_obj.get_fio_results()
-        logger.info(f"Io completed on pod {self.pod_obj.name}.")
+        fio_result = self.pod_obj.get_fio_results()
+        err_count = fio_result.get('jobs')[0].get('error')
+        assert err_count == 0, (
+            f"IO error on pod {self.pod_obj.name}. "
+            f"FIO result: {fio_result}."
+        )
+        logger.info('IO on the PVC Finished')
 
         # Verify presence of the file on pvc
         file_path = pod.get_file_path(self.pod_obj, file_name)
@@ -101,7 +109,7 @@ class TestPVCSingleClonePerformance(E2ETest):
         assert pod.check_file_existence(self.pod_obj, file_path), (
             f"File {file_name} does not exist"
         )
-        logger.info(f"File {file_name} exists in {self.pod_obj.name}")
+        logger.info(f"File {file_name} exists in {self.pod_obj.name}.")
 
         max_num_of_clones = 1
         clone_creation_time_measures = []
@@ -114,58 +122,70 @@ class TestPVCSingleClonePerformance(E2ETest):
             clone_yaml = constants.CSI_CEPHFS_PVC_CLONE_YAML
 
         # creating single clone ( or many one by one if max_mum_of_clones > 1)
-        logger.info(f"Start creating {max_num_of_clones} clones on {interface_type} PVC of size {pvc_size}GB.")
+        logger.info(f"Start creating {max_num_of_clones} clones on {interface_type} PVC of size {pvc_size} GB.")
 
         for i in range(max_num_of_clones):
             logger.info(f'Start creation of clone number {i + 1}.')
             cloned_pvc_obj = pvc.create_pvc_clone(sc_name, parent_pvc, clone_yaml, storage_size=pvc_size + "Gi")
             teardown_factory(cloned_pvc_obj)
             helpers.wait_for_resource_state(cloned_pvc_obj, constants.STATUS_BOUND, timeout)
+
             cloned_pvc_obj.reload()
             logger.info(f"Clone with name {cloned_pvc_obj.name} for pvc {parent_pvc} was created.")
             clones_list.append(cloned_pvc_obj)
             create_time = helpers.measure_pvc_creation_time(
                 interface_type, cloned_pvc_obj.name
             )
-            logger.info(f"Clone number {i+1} time creation is {create_time} secs.")
+            creation_speed = int( int(file_size) / create_time)
+            logger.info(f"Clone number {i+1} creation time is {create_time} secs.")
+            logger.info(f"Clone number {i+1} creation speed is {creation_speed} MB/sec.")
             clone_creation_time_measures.append(create_time)
 
-        logger.info(f"Finished creating {max_num_of_clones} clones on {interface_type} PVC of size {pvc_size}GB.")
+        logger.info(f"Finished creating and taking measures for {max_num_of_clones} "
+                    f"clones on {interface_type} PVC of size {pvc_size} GB.")
 
-        logger.info(f"Printing clone creation times for {max_num_of_clones} clones "
-                    f"on {interface_type} PVC of size {pvc_size}GB:")
+        logger.info(f"Printing clone creation time and speed for {max_num_of_clones} clones "
+                    f"on {interface_type} PVC of size {pvc_size} GB:")
 
         for i in range(max_num_of_clones):
-            logger.info(f"Clone number {i+1} creation time is {clone_creation_time_measures[i]} secs")
+            logger.info(f"Clone number {i+1} creation time is {clone_creation_time_measures[i]} secs.")
+            logger.info(f"Clone number {i+1} creation speed is "
+                        f"{int( int (file_size) / clone_creation_time_measures[i])} MB/sec.")
 
-        logger.info(f"Finished printing {max_num_of_clones} clone creation times "
-                    f"for {pvc_size}GB {interface_type} PVC.")
+        logger.info(f"Finished printing {max_num_of_clones} clone creation times and speed"
+                    f"for {pvc_size} GB {interface_type} PVC with {file_size} MB of data.")
 
-        # deleting one by one and measuring deletion times for each one of the clones create above
+        # deleting one by one and measuring deletion times and speed for each one of the clones create above
         # in case of single clone will run one time
         clone_deletion_time_measures = []
 
-        logger.info(f"Start deleting {max_num_of_clones} clones on {interface_type} PVC of size {pvc_size}GB.")
+        logger.info(f"Start deleting {max_num_of_clones} clones on {interface_type} PVC of size {pvc_size} GB.")
 
         for i in range(max_num_of_clones):
             cloned_pvc_obj = clones_list[i]
             pvc_reclaim_policy = cloned_pvc_obj.reclaim_policy
             cloned_pvc_obj.delete()
-            logger.info(f'Deletion of clone number {i + 1} , the clone name is {cloned_pvc_obj.name}')
+            logger.info(f"Deletion of clone number {i + 1} , the clone name is {cloned_pvc_obj.name}.")
             cloned_pvc_obj.ocp.wait_for_delete(cloned_pvc_obj.name, timeout)
             if pvc_reclaim_policy == constants.RECLAIM_POLICY_DELETE:
                 helpers.validate_pv_delete(cloned_pvc_obj.backed_pv)
             delete_time = helpers.measure_pvc_deletion_time(
                 interface_type, cloned_pvc_obj.backed_pv)
             logger.info(f"Clone number {i + 1} deletion time is {delete_time} secs.")
+
+            deletion_speed = int( int(file_size) / delete_time)
+            logger.info(f"Clone number {i+1} deletion speed is {deletion_speed} MB/sec.")
             clone_deletion_time_measures.append(delete_time)
 
-        logger.info(f"Finished deleting {max_num_of_clones} clones on {interface_type} PVC of size {pvc_size}GB.")
+        logger.info(f"Finished deleting and taking measures for {max_num_of_clones} clones "
+                    f"on {interface_type} PVC of size {pvc_size} GB.")
 
-        logger.info(f"Clone deletion times for {interface_type} PVC of size {pvc_size}GB are:")
+        logger.info(f"Clone deletion time and speed for {interface_type} PVC of size {pvc_size} GB are:")
         for i in range(max_num_of_clones):
             logger.info(f"Clone number {i+1} deletion time is {clone_deletion_time_measures[i]} secs.")
+            logger.info(f"Clone number {i + 1} deletion speed is "
+                        f"{int( int (file_size) / clone_deletion_time_measures[i])} MB/sec.")
 
-        logger.info(f"Finished printing clone deletion times for {interface_type} PVC of size {pvc_size}GB.")
+        logger.info(f"Finished printing clone deletion times and speed for {interface_type} PVC of size {pvc_size} GB.")
 
         logger.info("test_clones_creation_performance finished successfully.")
