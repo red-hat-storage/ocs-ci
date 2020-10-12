cdf7b78327bfb73f387c36295b3af68aaedd5c7b
Oded Viner <oviner@redhat.com>
2020-10-12 12:19:43 +0300

    *Create dict: must_gather_4_5, must_gather_4_6 *Change slice to re *Add file content check *Add function get_ocs_parsed_version, returns ocs version as float
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- ocs_ci/ocs/must_gather.py
+++ ocs_ci/ocs/must_gather.py
@@ -2,11 +2,13 @@ import os
 import logging
 import shutil
 import tempfile
-
+import re
 from pathlib import Path
+
 from ocs_ci.ocs.resources import pod
 from ocs_ci.ocs.utils import collect_ocs_logs
-from ocs_ci.ocs.const_must_gather import must_gather_files
+from ocs_ci.ocs.const_must_gather import must_gather_base, must_gather_version
+from ocs_ci.ocs.ocp import get_ocs_parsed_version
 
 
 logger = logging.getLogger(__name__)
@@ -18,10 +20,10 @@ class MustGather(object):
     """
     def __init__(self):
         self.root = None
-        self.pod_path = None
         self.temp_folder = None
         self.empty_files = list()
         self.files_not_exist = list()
+        self.files_content_issue = list()
 
     def collect_must_gather(self):
         """
@@ -53,14 +55,23 @@ class MustGather(object):
         Make sure all the relevant files exist
 
         """
-        for file in must_gather_files:
-            file_path = os.path.join(self.root, file)
-            if not Path(file_path).is_file():
-                logger.error(f"log file {file} does not exist!")
-                self.files_not_exist.append(file)
-            elif Path(file_path).stat().st_size == 0:
-                logger.error(f"log file {file} empty!")
-                self.empty_files.append(file)
+        version = get_ocs_parsed_version()
+        must_gather_v = must_gather_version[version]
+        for must_gather in (must_gather_base, must_gather_v):
+            for file, content in must_gather.items():
+                file_path = os.path.join(self.root, file)
+                if not Path(file_path).is_file():
+                    self.files_not_exist.append(file)
+                elif Path(file_path).stat().st_size == 0:
+                    self.empty_files.append(file)
+                elif re.search(r'\.yaml$', file):
+                    with open(file_path, 'r') as f:
+                        if 'kind' not in f.read().lower():
+                            self.files_content_issue.append(file)
+                elif content is not None:
+                    with open(file_path) as f:
+                        if content not in f.read():
+                            self.files_content_issue.append(file)
 
     def compare_running_pods(self):
         """
@@ -70,10 +81,10 @@ class MustGather(object):
         pods_obj = pod.get_all_pods(namespace='openshift-storage')
         pods_name = [pod.name for pod in pods_obj]
         for dir_name, subdir_list, files_list in os.walk(self.root):
-            if dir_name[-22:] == "openshift-storage/pods":
-                self.pod_path = dir_name
+            if re.search('openshift-storage/pods$', dir_name):
+                pod_path = dir_name
 
-        pod_files = [pod_file for pod_file in os.listdir(self.pod_path)]
+        pod_files = [pod_file for pod_file in os.listdir(pod_path)]
 
         assert set(sorted(pod_files)) == set(sorted(pods_name)), (
             f"List of openshift-storage pods are not equal to list of logs "
@@ -85,7 +96,7 @@ class MustGather(object):
         Delete temporary folder.
 
         """
-        if self.temp_folder[-9:] == '_ocs_logs':
+        if re.search('_ocs_logs$', self.temp_folder):
             shutil.rmtree(
                 path=self.temp_folder,
                 ignore_errors=False,
@@ -97,15 +108,18 @@ class MustGather(object):
         Print Invalid Files
 
         """
-        if len(self.empty_files) > 0:
-            for file in self.empty_files:
-                logger.error(f'empty file:{file}')
-        if len(self.files_not_exist) > 0:
+        error = ''
+        if len(self.empty_files) + len(self.files_not_exist) + len(
+            self.files_content_issue
+        ) != 0:
             for file in self.files_not_exist:
-                logger.error(f'empty file:{file}')
+                error += f'file not exist:{file}\n'
+            for file in self.empty_files:
+                error += f'empty file:{file}\n'
+            for file in self.files_content_issue:
+                error += f'content issue:{file}\n'
 
-        if len(self.empty_files) == 0 or len(self.files_not_exist) == 0:
-            raise ValueError
+            raise ValueError(error)
 
     def validate_must_gather(self):
         """
