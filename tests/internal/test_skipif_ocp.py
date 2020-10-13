ff6616330c2675f10f0f396cba2d1f9ba9169d27
Avi Liani <alayani@redhat.com>
2020-10-13 17:33:54 +0300

    skipif_ocp get the version from running cluster and not from config file
    Signed-off-by: Avi Liani <alayani@redhat.com>

---
+++ tests/internal/test_skipif_ocp.py
@@ -0,0 +1,27 @@
+import logging
+
+from ocs_ci.framework.testlib import BaseTest, skipif_ocp_version
+
+log = logging.getLogger(__name__)
+
+
+class TestSkipifOCP(BaseTest):
+    """
+    Tests to check the skipif_ocp marker
+    """
+
+    @skipif_ocp_version('<4.7')
+    def test_skipif_ocp(self):
+        """
+        Simple test to verify that skipif_ocp marker is working
+        """
+
+        log.error('Test did not skipped')
+
+    @skipif_ocp_version('<4.5')
+    def test_skipif_ocp_need2run(self):
+        """
+        Simple test to verify that skipif_ocp marker is working
+        """
+
+        log.info('Test did not skipped and is running')
