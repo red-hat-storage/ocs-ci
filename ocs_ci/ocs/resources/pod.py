96586ff41576fa3a97a397c90b30da8ddc1b189d
Itzhak Kave <yitzhak@localhost.localdomain>
2020-10-13 20:28:08 +0300

    add function to scale down deployments
    Signed-off-by: Itzhak Kave <yitzhak@localhost.localdomain>

--- ocs_ci/ocs/resources/pod.py
+++ ocs_ci/ocs/resources/pod.py
@@ -1712,3 +1712,7 @@ def get_mon_deployments(osd_label=constants.OSD_APP_LABEL, namespace=None):
     mons = get_deployments_having_label(osd_label, namespace)
     mon_deployments = [OCS(**mon) for mon in mons]
     return mon_deployments
+
+
+def get_deployment_name(pod_name):
+    return '-'.join(pod_name.split("-")[:-2])
