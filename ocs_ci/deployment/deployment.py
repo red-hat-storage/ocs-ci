2877a37abddb242493288b2a4098a8c11dffa1fb
Petr Balogh <petr-balogh@users.noreply.github.com>
2020-10-30 22:12:58 +0100

    Enable rotational disks for LSO PSI deployment (#3231)
    Fixes: #3230
    
    Enable rotational disk devices for LSO deployment over PSI +
    adding configuration file for enable this option.
    
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -1106,7 +1106,10 @@ def setup_local_storage(storageclass):
         # Since we don't have datastore with SSD on our current VMware machines, localvolumeset doesn't detect
         # NonRotational disk. As a workaround we are setting Rotational to device MechanicalProperties to detect
         # HDD disk
-        if platform == constants.VSPHERE_PLATFORM:
+        if (
+            platform == constants.VSPHERE_PLATFORM
+            or config.ENV_DATA.get("local_storage_allow_rotational_disks")
+        ):
             logger.info("Adding Rotational for deviceMechanicalProperties spec to detect HDD disk")
             lvs_data['spec']['deviceInclusionSpec']['deviceMechanicalProperties'].append("Rotational")
 
