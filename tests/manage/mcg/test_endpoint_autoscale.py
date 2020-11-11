701cb82c50b0de3dc17757a32681db2ca3144f87
Tiffany Nguyen <tunguyen@redhat.com>
2020-11-11 01:00:02 -0800

    Add options for FIO profile to generate S3 load for noobaa autoscale (#3276)
    * Add options for FIO profile to generate S3 load to >80% of CPU utilization
    
    Signed-off-by: Tiffany Nguyen <tunguyen@redhat.com>

--- tests/manage/mcg/test_endpoint_autoscale.py
+++ tests/manage/mcg/test_endpoint_autoscale.py
@@ -21,7 +21,12 @@ class TestEndpointAutoScale(MCGTest):
     def test_scaling_under_load(self, mcg_job_factory):
         self._assert_endpoint_count(1)
 
-        options = {}
+        options = {
+            'create': [('name', 'job1'), ('name', 'job2'), ('name', 'job3'), ('runtime', '900')],
+            'job1': [('iodepth', '4'), ('rw', 'randrw'), ('bs', '32k'), ('size', '64m'), ('numjobs', '4')],
+            'job2': [('iodepth', '16'), ('rw', 'randrw'), ('bs', '64k'), ('size', '512m'), ('numjobs', '4')],
+            'job3': [('iodepth', '32'), ('rw', 'randrw'), ('bs', '128k'), ('size', '1024m'), ('numjobs', '4')]
+        }
         job = mcg_job_factory(custom_options=options)
         self._assert_endpoint_count(2)
 
@@ -37,5 +42,5 @@ class TestEndpointAutoScale(MCGTest):
             condition=constants.STATUS_RUNNING,
             selector=constants.NOOBAA_ENDPOINT_POD_LABEL,
             dont_allow_other_resources=True,
-            timeout=300,
+            timeout=500,
         )
