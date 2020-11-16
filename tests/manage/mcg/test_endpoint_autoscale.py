b896911a5fa313a906573919f4ae21cdf189b8ad
Elad Ben Aharon <ebenahar@redhat.com>
2020-11-16 17:11:01 +0200

    Test Noobaa endpoint auto scale post node stop and start
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- tests/manage/mcg/test_endpoint_autoscale.py
+++ tests/manage/mcg/test_endpoint_autoscale.py
@@ -1,11 +1,22 @@
-# import pytest
+import pytest
+import logging
 
-from ocs_ci.framework.testlib import MCGTest, tier1, skipif_ocs_version
+from ocs_ci.framework.testlib import (
+    MCGTest,
+    tier1,
+    tier4,
+    tier4a,
+    skipif_ocs_version,
+    polarion_id,
+)
 from ocs_ci.ocs import constants, defaults, ocp
+from ocs_ci.ocs.resources.pod import get_pods_having_label, Pod, get_pod_node
+from ocs_ci.ocs.node import wait_for_nodes_status
+from ocs_ci.ocs import cluster
+
+logger = logging.getLogger(__name__)
 
 
-# @pytest.mark.polarion_id("OCS-XXXX")
-@tier1
 class TestEndpointAutoScale(MCGTest):
     """
     Test MCG endpoint auto-scaling
@@ -17,11 +28,9 @@ class TestEndpointAutoScale(MCGTest):
     MIN_ENDPOINT_COUNT = 1
     MAX_ENDPOINT_COUNT = 2
 
-    @skipif_ocs_version("<4.5")
-    def test_scaling_under_load(self, mcg_job_factory):
-        self._assert_endpoint_count(1)
-
-        options = {
+    @pytest.fixture()
+    def options(self):
+        return {
             "create": [
                 ("name", "job1"),
                 ("name", "job2"),
@@ -50,6 +59,13 @@ class TestEndpointAutoScale(MCGTest):
                 ("numjobs", "4"),
             ],
         }
+
+    @tier1
+    @skipif_ocs_version("<4.5")
+    @polarion_id("OCS-2402")
+    def test_scaling_under_load(self, mcg_job_factory, options):
+        self._assert_endpoint_count(1)
+
         job = mcg_job_factory(custom_options=options)
         self._assert_endpoint_count(2)
 
@@ -67,3 +83,59 @@ class TestEndpointAutoScale(MCGTest):
             dont_allow_other_resources=True,
             timeout=500,
         )
+
+    @tier4
+    @tier4a
+    @polarion_id("OCS-2422")
+    def test_auto_scale_with_stop_and_start_node(self, mcg_job_factory, nodes, options):
+        """"""
+        cl_obj = cluster.CephCluster()
+
+        self._assert_endpoint_count(1)
+        job = mcg_job_factory(custom_options=options)
+        self._assert_endpoint_count(2)
+
+        nodes.stop_nodes()
+
+        ep_pod_objs = get_pods_having_label(
+            label=constants.NOOBAA_ENDPOINT_POD_LABEL,
+            namespace=defaults.ROOK_CLUSTER_NAMESPACE,
+        )
+        ep_pod_obj = Pod(**ep_pod_objs[0])
+        # Retrieve the node object on which the pod resides
+        node = get_pod_node(ep_pod_obj)
+        # Drain the node
+        nodes.stop_nodes([node])
+
+        wait_for_nodes_status(
+            [node.get().get("metadata").get("name")], status=constants.NODE_NOT_READY
+        )
+        # Retrieve the new pod that should've been created post node power off
+
+        # Wait for the endpoint pod to reach Terminating status
+        logger.info(f"Waiting for pod {ep_pod_obj.name} to reach status Terminating")
+        assert ep_pod_obj.ocp.wait_for_resource(
+            timeout=600,
+            resource_name=ep_pod_obj.name,
+            condition=constants.STATUS_TERMINATING,
+        ), f"Noobaa endpoint pod {ep_pod_obj.name} failed to reach status Terminating"
+        logger.info(f"Pod {ep_pod_obj.name} has reached status Terminating")
+
+        # Wait for 2 Noobaa endpoint pods to be started and reach running status
+        logger.info("Waiting for 2 Noobaa endpoint pods to reach status Running")
+
+        self._assert_endpoint_count(2)
+
+        logger.info("2 Noobaa endpoint pods failed to reach status Running")
+        nodes.start_nodes([node])
+
+        wait_for_nodes_status(
+            [node.get().get("metadata").get("name")], status=constants.NODE_READY
+        )
+
+        # Check the NB status to verify the system is healthy
+        cl_obj.wait_for_noobaa_health_ok()
+
+        job.delete()
+        job.ocp.wait_for_delete(resource_name=job.name, timeout=60)
+        self._assert_endpoint_count(1)
