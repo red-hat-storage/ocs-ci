a618378965a3c122f1beb6058fd8db0d1d6e5548
Avi Liani <alayani@redhat.com>
2020-11-18 15:08:07 +0200

    enforce black format
    Signed-off-by: Avi Liani <alayani@redhat.com>

--- tests/e2e/performance/test_pvc_creation_performance.py
+++ tests/e2e/performance/test_pvc_creation_performance.py
@@ -8,15 +8,13 @@ import statistics
 import ocs_ci.ocs.exceptions as ex
 import ocs_ci.ocs.resources.pvc as pvc
 from concurrent.futures import ThreadPoolExecutor
-from ocs_ci.framework.testlib import (
-    performance, E2ETest, polarion_id, bugzilla
-)
+from ocs_ci.framework.testlib import performance, E2ETest, polarion_id, bugzilla
 from ocs_ci.helpers import helpers
 from ocs_ci.ocs import defaults, constants
 from ocs_ci.utility.utils import exec_cmd
 from ocs_ci.ocs.resources.pvc import (
     get_all_pvcs_in_storageclass,
-    get_all_pvs_in_storageclass
+    get_all_pvs_in_storageclass,
 )
 
 from ocs_ci.utility.performance_dashboard import push_to_pvc_time_dashboard
@@ -36,7 +34,8 @@ class TestPVCCreationPerformance(E2ETest):
     """
     Test to verify PVC creation performance
     """
-    pvc_size = '1Gi'
+
+    pvc_size = "1Gi"
 
     def get_pv_img_count(self, pool_name, interface):
         """
@@ -83,7 +82,7 @@ class TestPVCCreationPerformance(E2ETest):
         :return:
         """
 
-        results = {'img': 0, 'pvc': 0, 'pv': 0}
+        results = {"img": 0, "pvc": 0, "pv": 0}
 
         pool_name = get_storage_pool_name(self.interface, namespace)
         log.debug(f"The pool name is {pool_name}")
@@ -111,7 +110,9 @@ class TestPVCCreationPerformance(E2ETest):
         else:
             raise Exception(f"Invalid argument '{test_state}")
 
-    def create_mutiple_pvcs_statistics(self, num_of_samples, teardown_factory, pvc_size):
+    def create_mutiple_pvcs_statistics(
+        self, num_of_samples, teardown_factory, pvc_size
+    ):
         """
 
         Creates number (samples_num) of PVCs, measures creation time for each PVC and returns list of creation times.
@@ -127,10 +128,9 @@ class TestPVCCreationPerformance(E2ETest):
         """
         time_measures = []
         for i in range(num_of_samples):
-            log.info(f'Start creation of PVC number {i + 1}.')
+            log.info(f"Start creation of PVC number {i + 1}.")
 
-            pvc_obj = helpers.create_pvc(
-                sc_name=self.sc_obj.name, size=pvc_size)
+            pvc_obj = helpers.create_pvc(sc_name=self.sc_obj.name, size=pvc_size)
             helpers.wait_for_resource_state(pvc_obj, constants.STATUS_BOUND)
             pvc_obj.reload()
             teardown_factory(pvc_obj)
@@ -143,9 +143,7 @@ class TestPVCCreationPerformance(E2ETest):
         return time_measures
 
     @pytest.fixture()
-    def base_setup(
-        self, request, interface_iterate, storageclass_factory
-    ):
+    def base_setup(self, request, interface_iterate, storageclass_factory):
         """
         A setup phase for the test
 
@@ -160,22 +158,12 @@ class TestPVCCreationPerformance(E2ETest):
     @pytest.mark.parametrize(
         argnames=["pvc_size"],
         argvalues=[
-            pytest.param(
-                *['1Gi'], marks=pytest.mark.polarion_id("OCS-1225")
-            ),
-            pytest.param(
-                *['10Gi'], marks=pytest.mark.polarion_id("OCS-2010")
-            ),
-            pytest.param(
-                *['100Gi'], marks=pytest.mark.polarion_id("OCS-2011")
-            ),
-            pytest.param(
-                *['1Ti'], marks=pytest.mark.polarion_id("OCS-2008")
-            ),
-            pytest.param(
-                *['2Ti'], marks=pytest.mark.polarion_id("OCS-2009")
-            ),
-        ]
+            pytest.param(*["1Gi"], marks=pytest.mark.polarion_id("OCS-1225")),
+            pytest.param(*["10Gi"], marks=pytest.mark.polarion_id("OCS-2010")),
+            pytest.param(*["100Gi"], marks=pytest.mark.polarion_id("OCS-2011")),
+            pytest.param(*["1Ti"], marks=pytest.mark.polarion_id("OCS-2008")),
+            pytest.param(*["2Ti"], marks=pytest.mark.polarion_id("OCS-2009")),
+        ],
     )
     @pytest.mark.usefixtures(base_setup.__name__)
     def test_pvc_creation_measurement_performance(self, teardown_factory, pvc_size):
@@ -191,12 +179,14 @@ class TestPVCCreationPerformance(E2ETest):
             teardown factory: A fixture used when we want a new resource that was created during the tests
                                to be removed in the teardown phase.
             pvc_size: Size of the created PVC
-         """
+        """
         num_of_samples = 3
         accepted_deviation_percent = 5
         accepted_create_time = 3
 
-        create_measures = self.create_mutiple_pvcs_statistics(num_of_samples, teardown_factory, pvc_size)
+        create_measures = self.create_mutiple_pvcs_statistics(
+            num_of_samples, teardown_factory, pvc_size
+        )
         log.info(f"Current measures are {create_measures}")
 
         for i in range(num_of_samples):
@@ -207,7 +197,9 @@ class TestPVCCreationPerformance(E2ETest):
 
         average = statistics.mean(create_measures)
         st_deviation = statistics.stdev(create_measures)
-        log.info(f"The average creation time for the sampled {num_of_samples} PVCs is {average}.")
+        log.info(
+            f"The average creation time for the sampled {num_of_samples} PVCs is {average}."
+        )
 
         st_deviation_percent = abs(st_deviation - average) / average * 100.0
         if st_deviation > accepted_deviation_percent:
@@ -218,10 +210,8 @@ class TestPVCCreationPerformance(E2ETest):
         push_to_pvc_time_dashboard(self.interface, "creation", st_deviation)
 
     @pytest.mark.usefixtures(base_setup.__name__)
-    @polarion_id('OCS-1620')
-    def test_multiple_pvc_creation_measurement_performance(
-        self, teardown_factory
-    ):
+    @polarion_id("OCS-1620")
+    def test_multiple_pvc_creation_measurement_performance(self, teardown_factory):
         """
         Measuring PVC creation time of 120 PVCs in 180 seconds
 
@@ -237,7 +227,7 @@ class TestPVCCreationPerformance(E2ETest):
         initial_count = self.pvc_count_validation(
             storgeclass=self.sc_obj.name,
             namespace=defaults.ROOK_CLUSTER_NAMESPACE,
-            test_state="start"
+            test_state="start",
         )
         initial_count["to_create"] = number_of_pvcs
 
@@ -246,7 +236,7 @@ class TestPVCCreationPerformance(E2ETest):
             namespace=defaults.ROOK_CLUSTER_NAMESPACE,
             number_of_pvc=number_of_pvcs,
             size=self.pvc_size,
-            burst=True
+            burst=True,
         )
         for pvc_obj in pvc_objs:
             pvc_obj.reload()
@@ -254,48 +244,39 @@ class TestPVCCreationPerformance(E2ETest):
         with ThreadPoolExecutor(max_workers=5) as executor:
             for pvc_obj in pvc_objs:
                 executor.submit(
-                    helpers.wait_for_resource_state, pvc_obj,
-                    constants.STATUS_BOUND
+                    helpers.wait_for_resource_state, pvc_obj, constants.STATUS_BOUND
                 )
 
                 executor.submit(pvc_obj.reload)
         start_time = helpers.get_provision_time(
-            self.interface, pvc_objs, status='start'
+            self.interface, pvc_objs, status="start"
         )
-        end_time = helpers.get_provision_time(
-            self.interface, pvc_objs, status='end'
-        )
+        end_time = helpers.get_provision_time(self.interface, pvc_objs, status="end")
         total = end_time - start_time
         total_time = total.total_seconds()
         test_results = self.pvc_count_validation(
             storgeclass=self.sc_obj.name,
             namespace=defaults.ROOK_CLUSTER_NAMESPACE,
             test_state="end",
-            **initial_count
+            **initial_count,
         )
         if test_results:
             log.info("Test Finished OK")
         else:
             msg = "Not all resources created successfully"
             log.error(msg)
-            import time
-            time.sleep(240)
             raise Exception(msg)
         if total_time > 180:
             raise ex.PerformanceException(
                 f"{number_of_pvcs} PVCs creation time is {total_time} and "
                 f"greater than 180 seconds"
             )
-        logging.info(
-            f"{number_of_pvcs} PVCs creation time took {total_time} seconds"
-        )
+        logging.info(f"{number_of_pvcs} PVCs creation time took {total_time} seconds")
 
     @pytest.mark.usefixtures(base_setup.__name__)
-    @polarion_id('OCS-1270')
-    @bugzilla('1741612')
-    def test_multiple_pvc_creation_after_deletion_performance(
-        self, teardown_factory
-    ):
+    @polarion_id("OCS-1270")
+    @bugzilla("1741612")
+    def test_multiple_pvc_creation_after_deletion_performance(self, teardown_factory):
         """
         Measuring PVC creation time of 75% of initial PVCs (120) in the same
         rate after deleting 75% of the initial PVCs.
@@ -309,42 +290,39 @@ class TestPVCCreationPerformance(E2ETest):
         initial_number_of_pvcs = 120
         number_of_pvcs = math.ceil(initial_number_of_pvcs * 0.75)
 
-        log.info('Start creating new 120 PVCs')
+        log.info("Start creating new 120 PVCs")
         pvc_objs = helpers.create_multiple_pvcs(
             sc_name=self.sc_obj.name,
             namespace=defaults.ROOK_CLUSTER_NAMESPACE,
             number_of_pvc=initial_number_of_pvcs,
             size=self.pvc_size,
-            burst=True
+            burst=True,
         )
         for pvc_obj in pvc_objs:
             teardown_factory(pvc_obj)
         with ThreadPoolExecutor() as executor:
             for pvc_obj in pvc_objs:
                 executor.submit(
-                    helpers.wait_for_resource_state, pvc_obj,
-                    constants.STATUS_BOUND
+                    helpers.wait_for_resource_state, pvc_obj, constants.STATUS_BOUND
                 )
 
                 executor.submit(pvc_obj.reload)
-        log.info('Deleting 75% of the PVCs - 90 PVCs')
-        assert pvc.delete_pvcs(pvc_objs[:number_of_pvcs], True), (
-            "Deletion of 75% of PVCs failed"
-        )
-        log.info('Re-creating the 90 PVCs')
+        log.info("Deleting 75% of the PVCs - 90 PVCs")
+        assert pvc.delete_pvcs(
+            pvc_objs[:number_of_pvcs], True
+        ), "Deletion of 75% of PVCs failed"
+        log.info("Re-creating the 90 PVCs")
         pvc_objs = helpers.create_multiple_pvcs(
             sc_name=self.sc_obj.name,
             namespace=defaults.ROOK_CLUSTER_NAMESPACE,
             number_of_pvc=number_of_pvcs,
             size=self.pvc_size,
-            burst=True
+            burst=True,
         )
         start_time = helpers.get_provision_time(
-            self.interface, pvc_objs, status='start'
+            self.interface, pvc_objs, status="start"
         )
-        end_time = helpers.get_provision_time(
-            self.interface, pvc_objs, status='end'
-        )
+        end_time = helpers.get_provision_time(self.interface, pvc_objs, status="end")
         total = end_time - start_time
         total_time = total.total_seconds()
         if total_time > 45:
@@ -353,5 +331,5 @@ class TestPVCCreationPerformance(E2ETest):
                 f"75%) time is {total_time} and greater than 45 seconds"
             )
         logging.info(
-            f"{number_of_pvcs} PVCs creation time took less than a 45 seconds"
+            f"{number_of_pvcs} PVCs creation time took less than a 45 seconds."
         )
