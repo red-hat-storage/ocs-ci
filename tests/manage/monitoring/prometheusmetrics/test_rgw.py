1be74c91b2431651fc3bdb262bfb4ccbe8092a3b
Filip Balak <fbalak@redhat.com>
2020-11-19 16:30:47 +0100

    Add RGW metrics test
    Signed-off-by: Filip Balak <fbalak@redhat.com>

---
+++ tests/manage/monitoring/prometheusmetrics/test_rgw.py
@@ -0,0 +1,57 @@
+# -*- coding: utf8 -*-
+"""
+Test cases here performs Prometheus queries directly without a workload, to
+check that OCS Monitoring is configured and available as expected for RGW.
+"""
+
+import logging
+
+import pytest
+
+from ocs_ci.framework.testlib import skipif_ocs_version, tier4
+from ocs_ci.ocs import constants, defaults, ocp
+from ocs_ci.ocs import metrics
+from ocs_ci.ocs.resources.ocs import OCS
+from ocs_ci.utility.prometheus import PrometheusAPI
+
+
+logger = logging.getLogger(__name__)
+
+
+@skipif_ocs_version("<4.6")
+@tier4
+@pytest.mark.polarion_id("OCS-2385")
+def test_ceph_rgw_metrics_after_metrics_exporter_respin(rgw_deployments):
+    """
+    RGW metrics should be provided via OCP Prometheus even after
+    ocs-metrics-exporter pod is respinned.
+
+    """
+    logger.info("Respin ocs-metrics-exporter pod")
+    pod_obj = ocp.OCP(kind=constants.POD, namespace=defaults.ROOK_CLUSTER_NAMESPACE)
+    metrics_pods = pod_obj.get(selector="app.kubernetes.io/name=ocs-metrics-exporter")[
+        "items"
+    ]
+    assert len(metrics_pods) == 1
+    metrics_pod_data = metrics_pods[0]
+    metrics_pod = OCS(**metrics_pod_data)
+    metrics_pod.delete(force=True)
+
+    logger.info("Wait for ocs-metrics-exporter pod to come up")
+    assert pod_obj.wait_for_resource(
+        condition="Running",
+        selector="app.kubernetes.io/name=ocs-metrics-exporter",
+        resource_count=1,
+        timeout=600,
+    )
+
+    logger.info("Collect RGW metrics")
+    prometheus = PrometheusAPI()
+    list_of_metrics_without_results = metrics.get_missing_metrics(
+        prometheus, metrics.ceph_rgw_metrics
+    )
+    msg = (
+        "OCS Monitoring should provide some value(s) for tested rgw metrics, "
+        "so that the list of metrics without results is empty."
+    )
+    assert list_of_metrics_without_results == [], msg
