da5573f9d11fcfa724943144e406a8630cd1eb72
Oded Viner <oviner@redhat.com>
2020-11-29 23:29:07 +0200

    Change pod app (from fedora pod to http server pod)
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- ocs_ci/ocs/resources/pvc.py
+++ ocs_ci/ocs/resources/pvc.py
@@ -477,8 +477,9 @@ def get_pvc_obj(name, namespace=None):
 
     Returns:
         obj : A pvc object
+
     """
-    ocp_obj = OCP(api_version='v1', kind=constants.PVC, namespace=namespace)
+    ocp_obj = OCP(api_version="v1", kind=constants.PVC, namespace=namespace)
     ocp_dict = ocp_obj.get(resource_name=name)
     pvc_obj = PVC(**ocp_dict)
     return pvc_obj
--- tests/e2e/monitoring/test_monitoring_pvc_expansion.py
+++ tests/e2e/monitoring/test_monitoring_pvc_expansion.py
@@ -1,48 +1,67 @@
 import logging
 import pytest
 
-from ocs_ci.framework.testlib import tier4
-from ocs_ci.ocs import ocp, constants
+from ocs_ci.framework.testlib import tier4a
+from ocs_ci.ocs import constants
 from ocs_ci.framework.testlib import E2ETest
 from ocs_ci.ocs.resources.pvc import get_pvc_obj
 from ocs_ci.ocs.monitoring import prometheus_health_check
+from ocs_ci.ocs.resources.pod import get_used_space_on_mount_point
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
-@tier4
+@tier4a
 @pytest.mark.polarion_id("OCS-640")
 class TestMonitoringPvcExpansion(E2ETest):
     """
-    Full Fill my-prometheus-claim-prometheus-k8s-0 pvc (90%)
+    Full Fill my-prometheus-claim-prometheus-k8s-0 pvc
     Increase PVC size and check prometheus health is ok
+
     """
 
-    def test_monitoring_pvc_expansion(self, dc_pod_factory):
+    def test_monitoring_pvc_expansion(self, pod_factory):
         """
         test monitoring pvc expansion
+
         """
+
         # Get my-prometheus-claim-prometheus-k8s-0 pvc object
         pvc_obj = get_pvc_obj(
             namespace=constants.MONITORING_NAMESPACE,
-            name='my-prometheus-claim-prometheus-k8s-0'
+            name="my-prometheus-claim-prometheus-k8s-0",
         )
-        pvc_obj.project = ocp.OCP(namespace=constants.MONITORING_NAMESPACE)
 
-        # Get pvc size
+        log.info(f"Creating an app pod and mount {pvc_obj.name}")
+        interface_type = constants.CEPHBLOCKPOOL
+        pod_obj = pod_factory(interface=interface_type, pvc=pvc_obj)
+        log.info(f"{pod_obj.name} created successfully and mounted {pvc_obj.name}")
+
+        # Get pvc used percentage
         pvc_size = pvc_obj.size
-
-        # Create FIO Pod and Fill 90% from the storage
-        dc_pod_obj = dc_pod_factory(pvc=pvc_obj)
-        dc_pod_obj.run_io(
-            storage_type='fs', size=int(0.9 * pvc_size),
-            runtime='60', fio_filename=f'{dc_pod_obj.name}_io'
+        used_percentage = get_used_space_on_mount_point(pod_obj)
+        used_space = pvc_size * float(used_percentage.strip("%")) / 100
+        free_space = pvc_size - used_space
+        log.info(
+            f"\n{pvc_obj.name} size is {pvc_size}G\n"
+            f"used_space = {used_space}G\n"
+            f"free_space = {free_space}G\n"
         )
+
+        log.info(f"Running FIO on {pod_obj.name}")
+        pod_obj.run_io(
+            storage_type="fs",
+            size=f"{int(free_space)}G",
+            runtime=1000,
+            io_direction="write",
+            fio_filename=f"{pod_obj.name}_f2",
+        )
+
+        # Check the prometheus health
+        prometheus_health_check()
 
         # Increase storage my-prometheus-claim-prometheus-k8s-0 pvc
-        pvc_obj.resize_pvc(new_size=int(self.pvc_size * 1.4), verify=True)
+        pvc_obj.resize_pvc(new_size=int(pvc_size * 1.2), verify=True)
 
         # Validate the prometheus health is ok
-        assert prometheus_health_check(), (
-            "Prometheus cluster health is not OK"
-        )
+        assert prometheus_health_check(), "Prometheus cluster health is not OK"
