49a24717f7eb997a613301e8e0a37af24d76ef1e
Ben <benq3000@gmail.com>
2020-11-30 15:35:57 +0200

    Blackify
    Signed-off-by: Ben <benq3000@gmail.com>

--- tests/manage/mcg/test_bucket_deletion.py
+++ tests/manage/mcg/test_bucket_deletion.py
@@ -117,7 +117,9 @@ class TestBucketDeletion(MCGTest):
         """
         if bucketclass_dict:
             bucketclass = bucket_class_factory(bucketclass_dict)
-            buckets = bucket_factory(amount, interface, bucketclass_dict=bucketclass.name)
+            buckets = bucket_factory(
+                amount, interface, bucketclass_dict=bucketclass.name
+            )
         else:
             buckets = bucket_factory(amount, interface)
         for bucket in buckets:
