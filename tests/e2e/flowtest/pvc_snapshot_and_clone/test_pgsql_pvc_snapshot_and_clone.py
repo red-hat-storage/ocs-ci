059efb37b4b8d380ce925fe0ad8860a90ce4be40
Akarsha <akrai@redhat.com>
2020-12-15 13:22:59 +0530

    Flow based test scenario for pgsql snapshot and clone
    Signed-off-by: Akarsha <akrai@redhat.com>

---
+++ tests/e2e/flowtest/pvc_snapshot_and_clone/test_pgsql_pvc_snapshot_and_clone.py
@@ -0,0 +1,55 @@
+import logging
+import pytest
+
+from ocs_ci.framework.testlib import (
+    skipif_ocs_version,
+    skipif_ocp_version,
+    E2ETest,
+    flowtests,
+)
+
+
+log = logging.getLogger(__name__)
+
+
+@flowtests
+@skipif_ocs_version("<4.6")
+@skipif_ocp_version("<4.6")
+@pytest.mark.polarion_id("OCS-2310")
+class TestPvcSnapshotAndCloneWithBaseOperation(E2ETest):
+    """
+    Tests Story/Flow based test scenario for pgsql snapshot and clone
+    """
+
+    # @pytest.fixture(autouse=True)
+    # def pgsql_setup(
+    #     self,
+    #     pgsql_factory_fixture,
+    #     multi_snapshot_factory,
+    #     multi_snapshot_restore_factory,
+    #     multi_pvc_clone_factory,
+    # ):
+
+    def test_pvc_snapshot_and_clone(
+        self, pgsql_factory_fixture, multiple_snapshot_and_clone_of_postgres_pvc_factory
+    ):
+        """
+        1. Deploy PGSQL workload
+        2. Take a snapshot of the pgsql PVC.
+        3. Create a new PVC out of that snapshot or restore snapshot
+        4. Create a clone of restored snapshot
+        5. Attach a new pgsql pod to it.
+         5. Resize cloned pvc
+        7. Create snapshots of cloned pvc and restore those snapshots
+        8. Attach a new pgsql pod to it and Resize the new restored pvc
+
+        """
+
+        # Deploy PGSQL workload
+        log.info("Deploying pgsql workloads")
+        pgsql = pgsql_factory_fixture(replicas=3, clients=3, transactions=600)
+
+        log.info("Starting multiple creation & clone of postgres PVC")
+        multiple_snapshot_and_clone_of_postgres_pvc_factory(
+            pvc_size_new=25, pgsql=pgsql
+        )
