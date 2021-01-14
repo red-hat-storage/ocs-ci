f39b32a69f284d83d961751944246624899dae97
vavuthu <vavuthu@redhat.com>
2021-01-14 13:53:02 +0530

    remove workaround for bug https://bugzilla.redhat.com/show_bug.cgi?id=1910738
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/deployment/vmware.py
+++ ocs_ci/deployment/vmware.py
@@ -572,16 +572,13 @@ class VSPHEREUPI(VSPHEREBASE):
 
         # get datastore type and configure chrony for all nodes ONLY if
         # datastore type is vsan
-        # skip configuring chrony for OCP version 4.7, more details in
-        # bug 1910738
-        if Version.coerce(get_ocp_version()) < Version.coerce("4.7"):
-            datastore_type = self.vsphere.get_datastore_type_by_name(
-                self.datastore, self.datacenter
+        datastore_type = self.vsphere.get_datastore_type_by_name(
+            self.datastore, self.datacenter
+        )
+        if datastore_type != constants.VMFS:
+            configure_chrony_and_wait_for_machineconfig_status(
+                node_type="all", timeout=1800
             )
-            if datastore_type != constants.VMFS:
-                configure_chrony_and_wait_for_machineconfig_status(
-                    node_type="all", timeout=1800
-                )
 
     def destroy_cluster(self, log_level="DEBUG"):
         """
