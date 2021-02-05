10afdba87bcefc44866197b51bd37cd05950bf83
Oded Viner <oviner@redhat.com>
2021-02-05 02:23:22 +0200

    WA to issue: https://github.com/red-hat-storage/ocs-ci/issues/3820
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -268,8 +268,10 @@ def ocs_install_verification(
             "Verifying ceph osd tree output and checking for device set PVC names "
             "in the output."
         )
-
-        if config.DEPLOYMENT.get("local_storage"):
+        # https://github.com/red-hat-storage/ocs-ci/issues/3820
+        if config.DEPLOYMENT.get("local_storage") or config.DEPLOYMENT.get(
+            "ui_deployment"
+        ):
             deviceset_pvcs = [osd.get_node() for osd in get_osd_pods()]
             # removes duplicate hostname
             deviceset_pvcs = list(set(deviceset_pvcs))
