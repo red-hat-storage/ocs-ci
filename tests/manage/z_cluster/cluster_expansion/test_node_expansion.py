7fe7d6a654f01c8307a66dc22fb26d22aadbda1e
Itzhak Kave <yitzhak@localhost.localdomain>
2021-02-08 12:41:35 +0200

    Add the test of add-node in VMWare since it's now fixed
    Signed-off-by: Itzhak Kave <yitzhak@localhost.localdomain>

--- tests/manage/z_cluster/cluster_expansion/test_node_expansion.py
+++ tests/manage/z_cluster/cluster_expansion/test_node_expansion.py
@@ -54,17 +54,17 @@ class TestAddNode(ManageTest):
                 )
 
         elif config.ENV_DATA["platform"].lower() == constants.VSPHERE_PLATFORM:
-            pytest.skip(
-                "Skipping add node in Vmware platform due to "
-                "https://bugzilla.redhat.com/show_bug.cgi?id=1844521"
+            logger.info(
+                f"The worker nodes number before expansion {len(node.get_worker_nodes())}"
             )
-            # Issue to remove skip code https://github.com/red-hat-storage/ocs-ci/issues/2403
-            # logger.info(f'The worker nodes number before expansion {len(node.get_worker_nodes())}')
-            # if config.ENV_DATA.get('rhel_user'):
-            #     pytest.skip("Skipping add RHEL node, code unavailable")
-            # node_type = constants.RHCOS
-            # assert add_new_node_and_label_upi(node_type, new_nodes), "Add node failed"
-            # logger.info(f'The worker nodes number after expansion {len(node.get_worker_nodes())}')
+            if config.ENV_DATA.get("rhel_user"):
+                pytest.skip("Skipping add RHEL node, code unavailable")
+            node_type = constants.RHCOS
+            assert add_new_node_and_label_upi(node_type, new_nodes), "Add node failed"
+            logger.info(
+                f"The worker nodes number after expansion {len(node.get_worker_nodes())}"
+            )
+
         ceph_cluster_obj = CephCluster()
         assert ceph_cluster_obj.wait_for_rebalance(
             timeout=3600
