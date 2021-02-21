57fbe5c3da70b8ae73318e501259083156579948
Oded Viner <oviner@redhat.com>
2021-02-21 16:54:33 +0200

    Killing OSD MON MGR deamons
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- tests/manage/z_cluster/test_kill_ceph_mon_deamon.py
+++ tests/manage/z_cluster/test_kill_ceph_mon_deamon.py
@@ -2,10 +2,16 @@ import logging
 import pytest
 
 from ocs_ci.framework.testlib import ManageTest, tier1, bugzilla
-from ocs_ci.ocs.resources.pod import get_pod_node, get_mon_pods, get_ceph_tools_pod
 from ocs_ci.utility.utils import run_cmd
 from ocs_ci.utility.utils import TimeoutSampler
 from ocs_ci.helpers.helpers import verify_cli_cmd_output
+from ocs_ci.ocs.resources.pod import (
+    get_pod_node,
+    get_mon_pods,
+    get_ceph_tools_pod,
+    get_mgr_pods,
+    get_osd_pods,
+)
 
 log = logging.getLogger(__name__)
 
@@ -14,11 +20,11 @@ log = logging.getLogger(__name__)
 @bugzilla("1904917")
 class TestKillCephMonDaemon(ManageTest):
     """
-    Verify coredump getting generated for ceph mon daemon crash
+    Verify coredump getting generated for ceph daemon crash
 
     """
 
-    @pytest.fixture(autouse=True)
+    @pytest.fixture(scope="function")
     def teardown(self, request):
         """
         Archive a crash report so that it is no longer considered for the
@@ -33,62 +39,72 @@ class TestKillCephMonDaemon(ManageTest):
 
         request.addfinalizer(finalizer)
 
-    def test_kill_ceph_mon_process(self):
+    @pytest.mark.parametrize(
+        argnames=["daemon_type"],
+        argvalues=[
+            pytest.param(*["mon"], marks=pytest.mark.polarion_id("OCS-2491")),
+            pytest.param(*["mgr"], marks=pytest.mark.polarion_id("OCS-2491")),
+            pytest.param(*["osd"], marks=pytest.mark.polarion_id("OCS-2491")),
+        ],
+    )
+    def test_kill_ceph_mon_process(self, daemon_type):
         """
-        Verify coredumpctl list updated after killing ceph mon daemon
+        Verify coredumpctl list updated after killing daemon
 
         """
-        log.info("Get Node name where mon pod running")
-        # mon_pods = get_mon_pods()
-        # mon_pod = mon_pods[0]
-        # node_obj = get_pod_node(mon_pod)
-        mon_pod_nodes = [get_pod_node(pod) for pod in get_mon_pods()]
-        node_obj = mon_pod_nodes[0]
+        log.info(f"Get Node name where {daemon_type} pod running")
+        if daemon_type == "mon":
+            mon_pod_nodes = [get_pod_node(pod) for pod in get_mon_pods()]
+            node_obj = mon_pod_nodes[0]
+        elif daemon_type == "mgr":
+            mgr_pod_nodes = [get_pod_node(pod) for pod in get_mgr_pods()]
+            node_obj = mgr_pod_nodes[0]
+        elif daemon_type == "osd":
+            osd_pod_nodes = [get_pod_node(pod) for pod in get_osd_pods()]
+            node_obj = osd_pod_nodes[0]
+
         node_name = node_obj.name
         cmd_gen = "oc debug node/" + node_name + " -- chroot /host "
 
-        log.info("find ceph-mon process-id")
-        cmd_ps = "ps -ef"
-        cmd = cmd_gen + cmd_ps
-        pid = ""
+        log.info(f"find ceph-{daemon_type} process-id")
+        cmd_pid = f"pidof ceph-{daemon_type}"
+        cmd = cmd_gen + cmd_pid
         out = run_cmd(cmd=cmd)
-        for line in out.split("\n"):
-            if (
-                ("setuser-match-path" in line)
-                and ("167" in line)
-                and ("ceph-mon" in line)
-            ):
-                pid = line.split()[1]
+        pid = out.strip()
         if not pid.isnumeric():
-            raise Exception("The ceph-mon process-id was not found.")
+            raise Exception(f"The ceph-{daemon_type} process-id was not found.")
 
-        log.info(f"Kill ceph-mon process-id {pid}")
+        log.info(f"Kill ceph-{daemon_type} process-id {pid}")
         cmd_kill = f"kill -11 {pid}"
         cmd = cmd_gen + cmd_kill
         run_cmd(cmd=cmd)
 
-        log.info(
-            "Verify that we have a crash event for ceph-mon crash ['ceph crash ls' command]"
-        )
+        log.info(f"Verify that we have a crash event for ceph-{daemon_type} crash")
         sample = TimeoutSampler(
             timeout=600,
             sleep=10,
             func=verify_cli_cmd_output,
             cmd="ceph crash ls",
-            expected_output_lst=["mon."],
+            expected_output_lst=[daemon_type],
             cephtool_cmd=True,
         )
         if not sample.wait_for_func_status(True):
-            raise Exception("ceph mon process does not killed")
+            raise Exception(
+                f"ceph-{daemon_type} process does not exist on crash list (tool pod)"
+            )
 
-        log.info("Verify coredumpctl list updated after killing ceph mon daemon")
+        log.info(
+            f"Verify coredumpctl list updated after killing ceph-{daemon_type} daemon"
+        )
         sample = TimeoutSampler(
             timeout=600,
             sleep=10,
             func=verify_cli_cmd_output,
             cmd="coredumpctl list",
-            expected_output_lst=["ceph-mon"],
+            expected_output_lst=[daemon_type],
             debug_node=node_name,
         )
         if not sample.wait_for_func_status(True):
-            raise Exception("coredump not getting generated for ceph mon daemon crash")
+            raise Exception(
+                f"coredump not getting generated for ceph-{daemon_type} daemon crash"
+            )
