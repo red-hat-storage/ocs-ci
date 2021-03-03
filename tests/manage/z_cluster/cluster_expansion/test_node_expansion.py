3839140ecbd47de792b00053900c13063c1b5af4
Shrivaibavi Raghaventhiran <30822772+Shrivaibavi@users.noreply.github.com>
2021-03-03 13:54:46 +0530

    Automates BZ-1794387 Provide additional Toleration for Ceph CSI driver DS (#3790)
    * Automates BZ 1794387 Provide additional Toleration for Ceph CSI driver DS
    
    Signed-off-by: Shrivaibavi Raghaventhiran <sraghave@redhat.com>

--- tests/manage/z_cluster/cluster_expansion/test_node_expansion.py
+++ tests/manage/z_cluster/cluster_expansion/test_node_expansion.py
@@ -1,10 +1,6 @@
 import logging
 
 from ocs_ci.framework.testlib import tier1, ignore_leftovers, ManageTest
-from ocs_ci.ocs import machine as machine_utils
-from ocs_ci.framework import config
-from ocs_ci.ocs.node import add_new_node_and_label_it, add_new_node_and_label_upi
-from ocs_ci.ocs import constants, node
 from ocs_ci.ocs.cluster import CephCluster
 from ocs_ci.framework.pytest_customization.marks import skipif_openshift_dedicated
 
@@ -19,53 +15,12 @@ class TestAddNode(ManageTest):
     Automates adding worker nodes to the cluster while IOs
     """
 
-    def test_add_node(self):
+    def test_add_ocs_node(self, add_nodes):
         """
-        Test for adding worker nodes to the cluster while IOs
+        Test to add ocs nodes and wait till rebalance is completed
+
         """
-        new_nodes = 3
-        if config.ENV_DATA["platform"].lower() in constants.CLOUD_PLATFORMS:
-            dt = config.ENV_DATA["deployment_type"]
-            if dt == "ipi":
-                machines = machine_utils.get_machinesets()
-                logger.info(
-                    f"The worker nodes number before expansion {len(node.get_worker_nodes())}"
-                )
-                for machine in machines:
-                    add_new_node_and_label_it(machine)
-                logger.info(
-                    f"The worker nodes number after expansion {len(node.get_worker_nodes())}"
-                )
-
-            else:
-                logger.info(
-                    f"The worker nodes number before expansion {len(node.get_worker_nodes())}"
-                )
-                if config.ENV_DATA.get("rhel_workers"):
-                    node_type = constants.RHEL_OS
-                else:
-                    node_type = constants.RHCOS
-                assert add_new_node_and_label_upi(
-                    node_type, new_nodes
-                ), "Add node failed"
-                logger.info(
-                    f"The worker nodes number after expansion {len(node.get_worker_nodes())}"
-                )
-
-        elif config.ENV_DATA["platform"].lower() == constants.VSPHERE_PLATFORM:
-            logger.info(
-                f"The worker nodes number before expansion {len(node.get_worker_nodes())}"
-            )
-            if config.ENV_DATA.get("rhel_user"):
-                node_type = constants.RHEL_OS
-            else:
-                node_type = constants.RHCOS
-
-            assert add_new_node_and_label_upi(node_type, new_nodes), "Add node failed"
-            logger.info(
-                f"The worker nodes number after expansion {len(node.get_worker_nodes())}"
-            )
-
+        add_nodes(ocs_nodes=True)
         ceph_cluster_obj = CephCluster()
         assert ceph_cluster_obj.wait_for_rebalance(
             timeout=3600
