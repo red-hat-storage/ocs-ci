ba642bb78879cebbb50a01f590f0e91420c3983e
Shylesh Kumar Mohan <shmohan@redhat.com>
2021-03-24 22:01:07 -0700

    Pick machine list based on machine type
    Fixes #3990
    
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- ocs_ci/ocs/node.py
+++ ocs_ci/ocs/node.py
@@ -829,10 +829,11 @@ def delete_and_create_osd_node_ipi(osd_node_name):
     drain_nodes([osd_node_name])
     log.info("Getting machine name from specified node name")
     machine_name = machine.get_machine_from_node_name(osd_node_name)
+    machine_type = machine.get_machine_type(machine_name)
     log.info(f"Node {osd_node_name} associated machine is {machine_name}")
     log.info(f"Deleting machine {machine_name} and waiting for new machine to come up")
     machine.delete_machine_and_check_state_of_new_spinned_machine(machine_name)
-    new_machine_list = machine.get_machines()
+    new_machine_list = machine.get_machines(machine_type=machine_type)
     for machines in new_machine_list:
         # Trimming is done to get just machine name
         # eg:- machine_name:- prsurve-40-ocs-43-kbrvf-worker-us-east-2b-nlgkr
