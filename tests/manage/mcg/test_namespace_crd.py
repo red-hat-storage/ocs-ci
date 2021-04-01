32927f23bbf5bcfc554d85d1fc76b477a4173126
Ben <belimele@redhat.com>
2021-04-01 13:51:49 +0300

    Pass NSS list instead of object in `test_namespace_bucket_creation_with_many_resources_crd`
    Signed-off-by: Ben <belimele@redhat.com>

--- tests/manage/mcg/test_namespace_crd.py
+++ tests/manage/mcg/test_namespace_crd.py
@@ -513,7 +513,7 @@ class TestNamespace(MCGTest):
         """
         logger.info("Create namespace resources and verify health")
         nss_tup = ("oc", {"aws": [(100, self.DEFAULT_REGION)]})
-        ns_resources = namespace_store_factory(*nss_tup)[0]
+        ns_resources = namespace_store_factory(*nss_tup)
 
         logger.info("Create the namespace bucket with many namespace resources")
         bucketclass_dict = {
