d455db721fc3a482e4f17c4a3381057c0c9b4f9a
Amit Berner <aberner@redhat.com>
2021-04-20 09:45:30 +0300

    redundant imports
    Signed-off-by: Amit Berner <aberner@redhat.com>

--- tests/manage/rgw/test_bucket_deletion.py
+++ tests/manage/rgw/test_bucket_deletion.py
@@ -2,7 +2,7 @@ import logging
 
 import botocore
 import pytest
-from ocs_ci.ocs.bucket_utils import retrieve_test_objects_to_pod, sync_object_directory
+from ocs_ci.ocs.bucket_utils import sync_object_directory
 
 from ocs_ci.framework import config
 from ocs_ci.framework.pytest_customization.marks import acceptance, tier1, tier3
