c3143545e84d97e134108ab75d944006eef534c2
Oded Viner <oviner@redhat.com>
2021-04-21 00:21:38 +0300

    Remove teardown step [ceph crash archive-all]
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- tests/manage/z_cluster/test_osd_heap_profile.py
+++ tests/manage/z_cluster/test_osd_heap_profile.py
@@ -1,9 +1,9 @@
 import logging
 import pytest
 import time
+import random
 
 from ocs_ci.framework.testlib import ManageTest, tier2, skipif_ocs_version
-from ocs_ci.utility.utils import ceph_health_check
 from ocs_ci.ocs.resources.pod import get_ceph_tools_pod, get_osd_pods, get_osd_pod_id
 
 log = logging.getLogger(__name__)
@@ -17,26 +17,6 @@ class TestOSDHeapProfile(ManageTest):
     Test osd heap profile created on '/var/log/ceph/'.
 
     """
-
-    @pytest.fixture(scope="function", autouse=True)
-    def teardown(self, request):
-        """
-        Archive a crash report so that it is no longer considered for the
-        RECENT_CRASH health check and does not appear in the
-        crash ls-new output
-
-        """
-
-        def finalizer():
-            logging.info("Silence the ceph warnings by “archiving” the crash")
-            tool_pod = get_ceph_tools_pod()
-            tool_pod.exec_ceph_cmd(ceph_cmd="ceph crash archive-all", format=None)
-            logging.info(
-                "Perform Ceph and cluster health checks after silencing the ceph warnings"
-            )
-            ceph_health_check()
-
-        request.addfinalizer(finalizer)
 
     def test_osd_heap_profile(self):
         """
@@ -52,22 +32,38 @@ class TestOSDHeapProfile(ManageTest):
             -rw-r--r--. 1 ceph ceph 295891 Apr 11 14:33 osd.0.profile.0001.heap
 
         """
-        log.info("Start heap profiler for osd-0")
+        strings_err = ["error", "fail"]
+        osd_pods = get_osd_pods()
+        osd_id = str(random.randint(0, len(osd_pods) - 1))
+
+        log.info(f"Start heap profiler for osd-{osd_id}")
         pod_tool = get_ceph_tools_pod()
-        pod_tool.exec_sh_cmd_on_pod(command="ceph tell osd.0 heap start_profiler")
+        out = pod_tool.exec_sh_cmd_on_pod(
+            command=f"ceph tell osd.{osd_id} heap start_profiler"
+        )
+        logging.info(out)
+        for string_err in strings_err:
+            if string_err in out.lower():
+                raise (ValueError, f"{out}")
 
         time.sleep(10)
 
         log.info("Dump heap profile")
-        pod_tool.exec_sh_cmd_on_pod(command="ceph tell osd.0 heap dump")
+        out = pod_tool.exec_sh_cmd_on_pod(command=f"ceph tell osd.{osd_id} heap dump")
+        logging.info(out)
+        for string_err in strings_err:
+            if string_err in out.lower():
+                raise (ValueError, f"{out}")
 
-        log.info("Get osd-0 pod object")
-        osd_pods = get_osd_pods()
+        log.info(f"Get osd-{osd_id} pod object")
         for osd_pod in osd_pods:
-            if get_osd_pod_id(osd_pod) == "0":
-                osd_pod_0 = osd_pod
+            if get_osd_pod_id(osd_pod) == osd_id:
+                osd_pod_profile = osd_pod
 
-        log.info("Verify osd.0.profile log exist on /var/log/ceph/")
-        out = osd_pod_0.exec_cmd_on_pod(command="ls -ltr /var/log/ceph/")
-        if "osd.0.profile" not in out:
-            raise Exception("osd.0.profile log does not exist on /var/log/ceph/")
+        log.info(f"Verify osd.{osd_id}.profile log exist on /var/log/ceph/")
+        out = osd_pod_profile.exec_cmd_on_pod(command="ls -ltr /var/log/ceph/")
+        log.info(out)
+        if f"osd.{osd_id}.profile" not in out:
+            raise Exception(
+                f"osd.{osd_id}.profile log does not exist on /var/log/ceph/\n{out}"
+            )
