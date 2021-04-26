64e9adfeef79d515666bf839f2c663f18fbad352
Jilju Joy <jijoy@redhat.com>
2021-04-26 13:34:10 +0530

    change rate
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/manage/pv_services/test_ceph_daemon_kill_during_resource_creation.py
+++ tests/manage/pv_services/test_ceph_daemon_kill_during_resource_creation.py
@@ -264,7 +264,7 @@ class TestDaemonKillDuringResourceCreation(ManageTest):
                 storage_type=storage_type,
                 size="2G",
                 runtime=30,
-                rate="1m,10m",
+                rate="1m,1m",
                 fio_filename=f"{pod_obj.name}_io_file1",
             )
         log.info("FIO started on all pods.")
@@ -310,7 +310,7 @@ class TestDaemonKillDuringResourceCreation(ManageTest):
                 storage_type=storage_type,
                 size="1G",
                 runtime=10,
-                rate="1m,10m",
+                rate="1m,1m",
                 fio_filename=f"{pod_obj.name}_io_file2",
             )
 
