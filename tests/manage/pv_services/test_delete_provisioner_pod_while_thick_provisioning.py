538b12088fdf0ae731c6a026bbd2216850d60057
Jilju Joy <jijoy@redhat.com>
2021-05-17 16:01:53 +0530

    Test to delete rbd provisioner leader pod while thick provisioning is progressing
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/manage/pv_services/test_delete_provisioner_pod_while_thick_provisioning.py
+++ tests/manage/pv_services/test_delete_provisioner_pod_while_thick_provisioning.py
@@ -1,21 +1,16 @@
 import logging
 from concurrent.futures import ThreadPoolExecutor
 import pytest
-from functools import partial
 
 from ocs_ci.framework.testlib import ManageTest, tier4, tier4a, polarion_id
-from ocs_ci.framework import config
-from ocs_ci.helpers.helpers import verify_volume_deleted_in_backend
-from ocs_ci.ocs import constants, node
+from ocs_ci.helpers.helpers import verify_volume_deleted_in_backend, default_thick_storage_class
+from ocs_ci.ocs import constants
 from ocs_ci.ocs.resources.pod import get_fio_rw_iops
 from ocs_ci.ocs.resources.pvc import get_all_pvcs
-from ocs_ci.ocs.resources import pod
-from ocs_ci.utility.utils import TimeoutSampler, ceph_health_check
 from ocs_ci.helpers import helpers, disruption_helpers
 
 
 logger = logging.getLogger(__name__)
-
 DISRUPTION_OPS = disruption_helpers.Disruptions()
 
 
@@ -43,34 +38,31 @@ class TestDeleteProvisionerPodWhileThickProvisioning(ManageTest):
         pod_factory,
     ):
         """
-        Test to delete rbd provisioner leader pod while creating a PVC using thick provision enabled storage class
+        Test to delete RBD provisioner leader pod while creating a PVC using thick provision enabled storage class
         """
         pvc_size = 5
-
-        DISRUPTION_OPS.set_resource(resource="rbdplugin_provisioner", leader_type="provisioner")
-
-
         executor = ThreadPoolExecutor()
+        DISRUPTION_OPS.set_resource(resource="rbdplugin_provisioner", leader_type="provisioner")
 
         # Start creation of PVC
         pvc_create = executor.submit(
             pvc_factory,
             interface=constants.CEPHBLOCKPOOL,
             project=self.proj_obj,
-            storageclass=None,
+            storageclass=default_thick_storage_class,
             size=pvc_size,
             access_mode=constants.ACCESS_MODE_RWO,
             status="",
         )
 
-        # Ensure PVC is being created before deleting the resource
+        # Ensure that the PVC is being created before deleting the rbd provisioner pod
         ret = helpers.wait_for_resource_count_change(
             get_all_pvcs, 0, self.proj_obj.namespace, "increase"
         )
         assert ret, "Wait timeout: PVC is not being created."
         logger.info("PVC creation has started.")
         DISRUPTION_OPS.delete_resource()
-
+        logger.info("Deleted RBD provisioner leader pod.")
         pvc_obj = pvc_create.result()
 
         # Confirm that the PVC is Bound
@@ -80,38 +72,38 @@ class TestDeleteProvisionerPodWhileThickProvisioning(ManageTest):
         pvc_obj.reload()
         logger.info(f"Verified: PVC {pvc_obj.name} reached Bound state.")
 
-        image_uid = pvc_obj.image_uuid
-
-        # Create pod
+        # Create pod and run IO
         pod_obj = pod_factory(
             interface=constants.CEPHBLOCKPOOL,
             pvc=pvc_obj,
             status=constants.STATUS_RUNNING,
         )
-
         pod_obj.run_io(
             storage_type="fs", size=f"{pvc_size-1}G", fio_filename=f"{pod_obj.name}_io"
         )
 
+        # Get IO result
         get_fio_rw_iops(pod_obj)
 
+        logger.info(f"Deleting pod {pod_obj.name}")
         pod_obj.delete()
-
         pod_obj.ocp.wait_for_delete(
             pod_obj.name, 180
         ), f"Pod {pod_obj.name} is not deleted"
 
+        # Fetch image id for verification
+        image_uid = pvc_obj.image_uuid
+
+        logger.info(f"Deleting PVC {pvc_obj.name}")
         pvc_obj.delete()
         pvc_obj.ocp.wait_for_delete(
             pvc_obj.name
         ), f"PVC {pvc_obj.name} is not deleted"
         logger.info(f"Verified: PVC {pvc_obj.name} is deleted.")
 
+        # Verify the rbd image is deleted
         verify_volume_deleted_in_backend(
             interface=constants.CEPHBLOCKPOOL, image_uuid=image_uid, pool_name=constants.DEFAULT_BLOCKPOOL,
             timeout=300
         )
-
-
-
-
+        logger.info("Verified: RBD image is deleted")
