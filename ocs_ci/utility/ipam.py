e3a2d18804a663faedb02f9abb9b5310ec47f9c3
vavuthu <vavuthu@redhat.com>
2021-06-02 11:27:11 +0530

    Changing base domain to qe.rh-ocs.com and cluster name to sync with ocs-ci cluster name parameter
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/utility/ipam.py
+++ ocs_ci/utility/ipam.py
@@ -7,6 +7,8 @@ import os
 import requests
 
 from ocs_ci.framework import config
+from ocs_ci.ocs.exceptions import IPAMReleaseUpdateFailed, IPAMAssignUpdateFailed
+from ocs_ci.utility.retry import retry
 
 logger = logging.getLogger(__name__)
 
@@ -46,6 +48,7 @@ class IPAM(object):
             ConnectionError: in case of Connection Error
             Timeout: in case of Timeout
             RequestException: Any Exception from requests
+            IPAMAssignUpdateFailed: if it fails to assign IP
 
         """
         endpoint = os.path.join("http://", self.ipam, "api/getFreeIP.php?")
@@ -74,7 +77,7 @@ class IPAM(object):
 
         if "Error" in res.text:
             logger.error(f"Error in assigning IP to host. Error: {res.text}")
-            raise
+            raise IPAMAssignUpdateFailed(f"Failed to assign IP to {host}")
         else:
             logger.info(f"Successfully assigned IP to {host}")
             return res.text
@@ -93,12 +96,16 @@ class IPAM(object):
         """
         return [self.assign_ip(host, subnet) for host in hosts]
 
+    @retry(IPAMReleaseUpdateFailed, tries=5, delay=3, backoff=1)
     def release_ip(self, hostname):
         """
         Release IP from IPAM server
 
         Args:
             hostname (str): Hostname to release IP
+
+        Raises:
+            IPAMReleaseUpdateFailed: If it fails to release IP from IPAM server
 
         """
         # release the IP
@@ -107,6 +114,10 @@ class IPAM(object):
         res = requests.post(endpoint, data=payload)
         if res.status_code == "200":
             logger.info(f"Successfully released {hostname} IP from IPAM server")
+        else:
+            raise IPAMReleaseUpdateFailed(
+                f"Failed to release {hostname} IP from IPAM server"
+            )
 
     def release_ips(self, hosts):
         """
