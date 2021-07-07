d0b961ee641897c65fafaab16851af2134d23f43
Ben Eli <belimele@redhat.com>
2021-07-07 16:31:01 +0300

    Remove manual exposure of RGW service on OCS 4.7+ (#3928)
    Signed-off-by: Ben <belimele@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1783,6 +1783,14 @@ def rgw_endpoint(request):
     oc = ocp.OCP(kind=constants.SERVICE, namespace=config.ENV_DATA["cluster_namespace"])
     rgw_service = oc.get(selector=constants.RGW_APP_LABEL)["items"]
     if rgw_service:
+        if float(config.ENV_DATA["ocs_version"]) > 4.6:
+            log.info("RGW service should be exposed by default. Skipping exposure.")
+            default_rgw_route = ocp.OCP(
+                kind=constants.ROUTE, namespace=config.ENV_DATA["cluster_namespace"]
+            ).get(resource_name=constants.RGW_DEFAULT_ROUTE_NAME)
+            default_rgw_host = default_rgw_route["status"]["ingress"][0]["host"]
+            return f"http://{default_rgw_host}"
+
         if config.DEPLOYMENT["external_mode"]:
             rgw_service = constants.RGW_SERVICE_EXTERNAL_MODE
         else:
@@ -1796,7 +1804,11 @@ def rgw_endpoint(request):
         route = oc.get(resource_name="noobaa-mgmt")
         router_hostname = route["status"]["ingress"][0]["routerCanonicalHostname"]
         rgw_hostname = f"rgw.{router_hostname}"
-        oc.exec_oc_cmd(f"expose service/{rgw_service} --hostname {rgw_hostname}")
+        try:
+            oc.exec_oc_cmd(f"expose service/{rgw_service} --hostname {rgw_hostname}")
+        except CommandFailed as cmdfailed:
+            if "AlreadyExists" in str(cmdfailed):
+                log.warning("RGW route already exists.")
         # new route is named after service
         rgw_endpoint = oc.get(resource_name=rgw_service)
         endpoint_obj = OCS(**rgw_endpoint)
