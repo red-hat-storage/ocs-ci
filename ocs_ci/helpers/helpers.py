a2b80713d2595a4356785b78b0b1aedd0b0ca5df
shylesh <shmohan@redhat.com>
2021-07-12 03:31:17 -0700

    Helper functions for encrypted storageclass creation with KMS vault (#4463)
    * Helper functions for encrypted storageclass creation with KMS vault
    
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- ocs_ci/helpers/helpers.py
+++ ocs_ci/helpers/helpers.py
@@ -504,6 +504,7 @@ def create_storage_class(
     sc_name=None,
     provisioner=None,
     rbd_thick_provision=False,
+    encrypted=False,
 ):
     """
     Create a storage class
@@ -538,6 +539,12 @@ def create_storage_class(
         )
         if rbd_thick_provision:
             sc_data["parameters"]["thickProvision"] = "true"
+        if encrypted:
+            # Avoid circular imports
+            from ocs_ci.utility.kms import get_encryption_kmsid
+
+            sc_data["parameters"]["encrypted"] = "true"
+            sc_data["parameters"]["encryptionKMSID"] = get_encryption_kmsid()
     elif interface_type == constants.CEPHFILESYSTEM:
         sc_data = templating.load_yaml(constants.CSI_CEPHFS_STORAGECLASS_YAML)
         sc_data["parameters"]["csi.storage.k8s.io/node-stage-secret-name"] = secret_name
