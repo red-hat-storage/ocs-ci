49e9a443f0cb6ecbe5a69dfd666d756b4a833704
Oded Viner <oviner@redhat.com>
2021-07-13 17:51:52 +0300

    The function wait_for_pods_to_be_running returns True or False, I added an assert statement
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- tests/manage/z_cluster/test_rook_operator_restart_during_mon_failover.py
+++ tests/manage/z_cluster/test_rook_operator_restart_during_mon_failover.py
@@ -36,7 +36,7 @@ class TestDrainNodeMon(ManageTest):
     5.Verify the number of mon pods is 3 for (1400 seconds)
     6.Respin  rook-ceph operator pod
     7.Uncordon the node
-    8.Wait for mon and osd pods to be on running state
+    8.Wait for all the pods in openshift-storage to be running
     9.Verify pdb status, disruptions_allowed=1, max_unavailable_mon=1
 
     """
@@ -87,7 +87,7 @@ class TestDrainNodeMon(ManageTest):
         schedule_nodes([node_name])
 
         logging.info("Wait for all the pods in openshift-storage to be running.")
-        wait_for_pods_to_be_running()
+        assert wait_for_pods_to_be_running(timeout=300)
 
         sample = TimeoutSampler(
             timeout=100,
