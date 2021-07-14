red-hat-storage/ocs-ci

commit bc81735b59cfd81496215abc7af3ebac54901ed8

Fix ovirt-dependencies
Signed-off-by: Petr Balogh <pbalogh@redhat.com>

ocs_ci/deployment/factory.py
@@ -1,16 +1,8 @@
 import logging
 
 from ocs_ci.framework import config
+from ocs_ci.ocs import constants
 from ocs_ci.ocs import exceptions
-from ocs_ci.deployment.ibm import IBMDeployment
-from ocs_ci.deployment.ibmcloud import IBMCloud
-from .aws import AWSIPI, AWSUPI, AWSUPIFlexy
-from .azure import AZUREIPI
-from .gcp import GCPIPI
-from .vmware import VSPHEREUPI, VSPHEREIPI
-from .baremetal import BAREMETALUPI, BaremetalPSIUPI
-from .openshift_dedicated import OpenshiftDedicated
-from .rhv import RHVIPI
 
 logger = logging.getLogger(__name__)
 
@@ -21,23 +13,64 @@ class DeploymentFactory(object):
     """
 
     def __init__(self):
-        # A map all existing deployments and respective classes
-        # should be put here
-        self.cls_map = {
-            "aws_ipi": AWSIPI,
-            "aws_upi": AWSUPI,
-            "aws_upi_flexy": AWSUPIFlexy,
-            "azure_ipi": AZUREIPI,
-            "vsphere_upi": VSPHEREUPI,
-            "vsphere_ipi": VSPHEREIPI,
-            "baremetalpsi_upi_flexy": BaremetalPSIUPI,
-            "baremetal_upi": BAREMETALUPI,
-            "gcp_ipi": GCPIPI,
-            "powervs_upi": IBMDeployment,
-            "ibm_cloud_managed": IBMCloud,
-            "openshiftdedicated_managed": OpenshiftDedicated,
-            "rhv_ipi": RHVIPI,
-        }
+        self.deployment_platform = config.ENV_DATA["platform"]
+        self.cls_map = {}
+        # A map of all existing deployments and respective classes
+        # should be put here, but only in the condition if that platform is used.
+        # We want to avoid unused imports for other platforms, so dependencies
+        # for other platforms can be avoided when importing this factory module.
+        if self.deployment_platform == constants.AWS_PLATFORM:
+            from .aws import AWSIPI, AWSUPI, AWSUPIFlexy
+
+            self.cls_map.update(
+                {
+                    "aws_ipi": AWSIPI,
+                    "aws_upi": AWSUPI,
+                    "aws_upi_flexy": AWSUPIFlexy,
+                }
+            )
+        elif self.deployment_platform == constants.VSPHERE_PLATFORM:
+            from .vmware import VSPHEREUPI, VSPHEREIPI
+
+            self.cls_map.update(
+                {
+                    "vsphere_upi": VSPHEREUPI,
+                    "vsphere_ipi": VSPHEREIPI,
+                }
+            )
+        elif self.deployment_platform == constants.AZURE_PLATFORM:
+            from .azure import AZUREIPI
+
+            self.cls_map["azure_ipi"] = AZUREIPI
+        elif self.deployment_platform == constants.GCP_PLATFORM:
+            from .gcp import GCPIPI
+
+            self.cls_map["gcp_ipi"] = GCPIPI
+        elif self.deployment_platform == constants.IBMCLOUD_PLATFORM:
+            from ocs_ci.deployment.ibmcloud import IBMCloud
+
+            self.cls_map["ibm_cloud_managed"] = IBMCloud
+        elif self.deployment_platform == constants.IBM_POWER_PLATFORM:
+            from ocs_ci.deployment.ibm import IBMDeployment
+
+            self.cls_map["powervs_upi"] = IBMDeployment
+        elif self.deployment_platform == constants.BAREMETAL_PLATFORM:
+            from .baremetal import BAREMETALUPI, BaremetalPSIUPI
+
+            self.cls_map.update(
+                {
+                    "baremetalpsi_upi_flexy": BaremetalPSIUPI,
+                    "baremetal_upi": BAREMETALUPI,
+                }
+            )
+        elif self.deployment_platform == constants.OPENSHIFT_DEDICATED_PLATFORM:
+            from .openshift_dedicated import OpenshiftDedicated
+
+            self.cls_map["openshiftdedicated_managed"] = OpenshiftDedicated
+        elif self.deployment_platform == constants.RHV_PLATFORM:
+            from .rhv import RHVIPI
+
+            self.cls_map["rhv_ipi"] = RHVIPI
 
     def get_deployment(self):
         """
@@ -46,18 +79,17 @@ class DeploymentFactory(object):
         deployment_platform may look like 'aws', 'vmware', 'baremetal'
         deployment_type may be like 'ipi' or 'upi'
         """
-        deployment_platform = config.ENV_DATA["platform"]
         deployment_type = config.ENV_DATA["deployment_type"]
         flexy_deployment = config.ENV_DATA["flexy_deployment"]
         deployment_cls_key = (
-            f"{deployment_platform.lower()}" f"_" f"{deployment_type.lower()}"
+            f"{self.deployment_platform.lower()}" f"_" f"{deployment_type.lower()}"
         )
         if flexy_deployment:
             deployment_cls_key = f"{deployment_cls_key}_flexy"
         logger.info(f"Deployment key = {deployment_cls_key}")
         logger.info(
             f"Current deployment platform: "
-            f"{deployment_platform}, "
+            f"{self.deployment_platform}, "
             f"deployment type: {deployment_type}, "
             f"flexy_deployment: {flexy_deployment}"
         )

ocs_ci/ocs/platform_nodes.py
@@ -17,16 +17,7 @@ from ocs_ci.deployment.vmware import (
 )
 from ocs_ci.ocs.exceptions import TimeoutExpiredError
 from ocs_ci.framework import config, merge_dict
-from ocs_ci.utility import (
-    aws,
-    vsphere,
-    templating,
-    baremetal,
-    azure_utils,
-    powernodes,
-    rhv,
-    ibmcloud,
-)
+from ocs_ci.utility import templating
 from ocs_ci.utility.csr import approve_pending_csr
 from ocs_ci.utility.load_balancer import LoadBalancer
 from ocs_ci.utility.retry import retry
@@ -197,6 +188,8 @@ class VMWareNodes(NodesBase):
 
     def __init__(self):
         super(VMWareNodes, self).__init__()
+        from ocs_ci.utility import vsphere
+
         self.cluster_name = config.ENV_DATA.get("cluster_name")
         self.server = config.ENV_DATA["vsphere_server"]
         self.user = config.ENV_DATA["vsphere_user"]
@@ -424,7 +417,10 @@ class AWSNodes(NodesBase):
 
     def __init__(self):
         super(AWSNodes, self).__init__()
-        self.aws = aws.AWS()
+        from ocs_ci.utility import aws as aws_utility
+
+        self.aws_utility = aws_utility
+        self.aws = aws_utility.AWS()
         self.az = AZInfo()
 
     def get_ec2_instances(self, nodes):
@@ -438,7 +434,7 @@ class AWSNodes(NodesBase):
             dict: The EC2 instances dicts (IDs and names)
 
         """
-        return aws.get_instances_ids_and_names(nodes)
+        return self.aws_utility.get_instances_ids_and_names(nodes)
 
     def get_data_volumes(self):
         """
@@ -449,7 +445,7 @@ class AWSNodes(NodesBase):
 
         """
         pvs = get_deviceset_pvs()
-        return aws.get_data_volumes(pvs)
+        return self.aws_utility.get_data_volumes(pvs)
 
     def get_node_by_attached_volume(self, volume):
         """
@@ -1695,6 +1691,8 @@ class BaremetalNodes(NodesBase):
 
     def __init__(self):
         super(BaremetalNodes, self).__init__()
+        from ocs_ci.utility import baremetal
+
         self.baremetal = baremetal.BAREMETAL()
 
     def stop_nodes(self, nodes, force=True):
@@ -1809,6 +1807,8 @@ class IBMPowerNodes(NodesBase):
 
     def __init__(self):
         super(IBMPowerNodes, self).__init__()
+        from ocs_ci.utility import powernodes
+
         self.powernodes = powernodes.PowerNodes()
 
     def stop_nodes(self, nodes, force=True):
@@ -1922,6 +1922,8 @@ class AZURENodes(NodesBase):
 
     def __init__(self):
         super(AZURENodes, self).__init__()
+        from ocs_ci.utility import azure_utils
+
         self.azure = azure_utils.AZURE()
 
     def stop_nodes(self, nodes, timeout=540, wait=True, force=True):
@@ -2190,6 +2192,8 @@ class RHVNodes(NodesBase):
 
     def __init__(self):
         super(RHVNodes, self).__init__()
+        from ocs_ci.utility import rhv
+
         self.rhv = rhv.RHV()
 
     def get_rhv_vm_instances(self, nodes):
@@ -2345,6 +2349,8 @@ class IBMCloud(NodesBase):
     """
 
     def __init__(self):
+        from ocs_ci.utility import ibmcloud
+
         super(IBMCloud, self).__init__()
         self.ibmcloud = ibmcloud.IBMCloud()
 

commit 2016e67794f6c939ecf2c4ccdc8bfdfb27c8a37d

Fix regression in DeploymentFactory
bc81735 broke instances where ENV_DATA.platform is not all lower-case.

Signed-off-by: Zack Cerza <zack@redhat.com>

ocs_ci/deployment/factory.py
@@ -13,7 +13,7 @@ class DeploymentFactory(object):
     """
 
     def __init__(self):
-        self.deployment_platform = config.ENV_DATA["platform"]
+        self.deployment_platform = config.ENV_DATA["platform"].lower()
         self.cls_map = {}
         # A map of all existing deployments and respective classes
         # should be put here, but only in the condition if that platform is used.