d24d050dae2741bc3ebab2145f7c935baaf4f7d4
OdedViner <61982127+OdedViner@users.noreply.github.com>
2021-07-15 08:25:05 +0300

    GSS bug,Verify the num of mon pods is 3 when drain node (#4440)
    Signed-off-by: Oded Viner <oviner@redhat.com>

---
+++ tests/manage/z_cluster/test_rook_operator_restart_during_mon_failover.py
@@ -0,0 +1,119 @@
+import logging
+import pytest
+
+from ocs_ci.utility.utils import TimeoutSampler, ceph_health_check
+from ocs_ci.ocs.node import drain_nodes, schedule_nodes
+from ocs_ci.helpers.helpers import verify_pdb_mon
+from ocs_ci.ocs.resources.pod import (
+    get_mon_pods,
+    get_operator_pods,
+    wait_for_pods_to_be_running,
+)
+from ocs_ci.framework.testlib import (
+    ManageTest,
+    tier4a,
+    bugzilla,
+    skipif_ocs_version,
+    skipif_external_mode,
+    ignore_leftovers,
+)
+
+log = logging.getLogger(__name__)
+
+
+@tier4a
+@skipif_external_mode
+@skipif_ocs_version("<4.6")
+@bugzilla("1959983")
+@ignore_leftovers
+@pytest.mark.polarion_id("OCS-2572")
+class TestDrainNodeMon(ManageTest):
+    """
+    1.Get worker node name where monitoring pod run
+    2.Verify pdb status, disruptions_allowed=1, max_unavailable_mon=1
+    3.Drain node where monitoring pod run
+    4.Verify pdb status, disruptions_allowed=0, max_unavailable_mon=1
+    5.Verify the number of mon pods is 3 for (1400 seconds)
+    6.Respin  rook-ceph operator pod
+    7.Uncordon the node
+    8.Wait for all the pods in openshift-storage to be running
+    9.Verify pdb status, disruptions_allowed=1, max_unavailable_mon=1
+
+    """
+
+    def test_rook_operator_restart_during_mon_failover(self, node_drain_teardown):
+        """
+        Verify the number of monitoring pod is three when drain node
+
+        """
+        sample = TimeoutSampler(
+            timeout=100,
+            sleep=10,
+            func=verify_pdb_mon,
+            disruptions_allowed=1,
+            max_unavailable_mon=1,
+        )
+        if sample.wait_for_func_status(result=True):
+            assert "the expected pdb state is not equal to actual pdb state"
+
+        log.info("Get worker node name where monitoring pod run")
+        mon_pod_objs = get_mon_pods()
+        node_name = mon_pod_objs[0].data["spec"]["nodeName"]
+
+        drain_nodes([node_name])
+
+        sample = TimeoutSampler(
+            timeout=100,
+            sleep=10,
+            func=verify_pdb_mon,
+            disruptions_allowed=0,
+            max_unavailable_mon=1,
+        )
+        if sample.wait_for_func_status(result=True):
+            assert "the expected pdb state is not equal to actual pdb state"
+
+        timeout = 1400
+        log.info(f"Verify the number of mon pods is 3 for {timeout} seconds")
+        sample = TimeoutSampler(
+            timeout=timeout, sleep=10, func=self.check_mon_pods_eq_3
+        )
+        if sample.wait_for_func_status(result=True):
+            assert "There are more than 3 mon pods."
+
+        log.info("Respin pod rook-ceph operator pod")
+        rook_ceph_operator_pod_obj = get_operator_pods()
+        rook_ceph_operator_pod_obj[0].delete()
+
+        schedule_nodes([node_name])
+
+        logging.info("Wait for all the pods in openshift-storage to be running.")
+        assert wait_for_pods_to_be_running(timeout=300)
+
+        sample = TimeoutSampler(
+            timeout=100,
+            sleep=10,
+            func=verify_pdb_mon,
+            disruptions_allowed=1,
+            max_unavailable_mon=1,
+        )
+        if sample.wait_for_func_status(result=True):
+            assert "the expected pdb state is not equal to actual pdb state"
+
+        ceph_health_check()
+
+    def check_mon_pods_eq_3(self):
+        """
+        Get number of monitoring pods
+
+        Returns:
+            bool: False if number of mon pods is 3, True otherwise
+
+        """
+        mon_pod_list = get_mon_pods()
+        if len(mon_pod_list) == 3:
+            return False
+        else:
+            log.info(f"There are {len(mon_pod_list)} mon pods")
+            for mon_pod in mon_pod_list:
+                log.info(f"{mon_pod.name}")
+            return True
