c677f4bdf4e53edeebfa914e6bf6f38b28c0b6be
aviadp <apolak@redhat.com>
2021-07-18 18:21:35 +0300

    added new test that run verify_rgw_restart_count_session fixture
    Signed-off-by: aviadp <apolak@redhat.com>

--- tests/manage/monitoring/prometheusmetrics/test_rgw.py
+++ tests/manage/monitoring/prometheusmetrics/test_rgw.py
@@ -8,7 +8,7 @@ import logging
 
 import pytest
 
-from ocs_ci.framework.testlib import skipif_ocs_version, tier4, tier4a
+from ocs_ci.framework.testlib import skipif_ocs_version, tier1, tier4, tier4a
 from ocs_ci.ocs import constants, defaults, ocp
 from ocs_ci.ocs import metrics
 from ocs_ci.ocs.resources.ocs import OCS
@@ -56,3 +56,14 @@ def test_ceph_rgw_metrics_after_metrics_exporter_respin(rgw_deployments):
         "so that the list of metrics without results is empty."
     )
     assert list_of_metrics_without_results == [], msg
+
+
+@tier1
+@pytest.mark.post_ocp_upgrade
+@pytest.mark.run(order=1)
+@pytest.mark.polarion_id("OCS-2584")
+def verify_rgw_pods_restart_count(verify_rgw_restart_count_session):
+    """
+    This test starting session scope fixture, that verify RGW pod restarts count
+    """
+    logger.info("Starting RGW pod restart counts")
