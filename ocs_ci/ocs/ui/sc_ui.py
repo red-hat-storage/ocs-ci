11190ba5dbcb353bfb1f428a2c75b8d8ab5ff266
Aman Agrawal <amagrawa@redhat.com>
2021-07-28 16:49:49 +0530

    PV encryption
    Signed-off-by: Aman Agrawal <amagrawa@redhat.com>

--- ocs_ci/ocs/ui/sc_ui.py
+++ ocs_ci/ocs/ui/sc_ui.py
@@ -1,6 +1,13 @@
 import logging
 import time
 
+import pyautogui
+from pyautogui import write, press
+# from seleniumbase import BaseCase
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 from ocs_ci.ocs.ui.base_ui import PageNavigator
 from ocs_ci.ocs.ui.views import locators
 from ocs_ci.utility.utils import get_ocp_version, get_running_ocp_version
@@ -8,6 +15,8 @@ from ocs_ci.ocs.ui.base_ui import PageNavigator
 from ocs_ci.ocs.ui.views import locators
 from ocs_ci.utility.utils import get_ocp_version, get_running_ocp_version
 from ocs_ci.ocs import constants
+from selenium.webdriver.support import expected_conditions as ec
+from ocs_ci.ocs.ui.ui_utils import format_locator
 
 logger = logging.getLogger(__name__)
 
@@ -19,14 +28,12 @@ class PVEncryptionUI(PageNavigator):
         ocp_version = get_ocp_version()
         self.pvc_loc = locators[ocp_version]["storage_class"]
 
-    def create_storage_class_with_encryption_ui(self):
-
+    def create_storage_class_with_encryption_ui(self, sc_name="test-storage-class"):
         self.navigate_storageclasses_page()
-
         logger.info("Create Storage Class")
         self.do_click(self.pvc_loc["create-sc"])
         logger.info("Storage Class Name")
-        self.do_send_keys(self.pvc_loc["sc-name"], "test-storage-class")
+        self.do_send_keys(self.pvc_loc["sc-name"], f"{sc_name}")
         logger.info("Storage Class Description")
         self.do_send_keys(self.pvc_loc["sc-description"], "this is a test storage class")
         logger.info("Storage Class Reclaim Policy")
@@ -45,71 +52,41 @@ class PVEncryptionUI(PageNavigator):
         logger.info("Storage Class Connection Details")
         self.do_click(self.pvc_loc["connections-details"])
         logger.info("Storage Class Service Name")
+        self.do_clear(self.pvc_loc["service-name"])
         self.do_send_keys(self.pvc_loc["service-name"], "test-service")
         logger.info("Storage Class Address")
         self.do_clear(self.pvc_loc["kms-address"])
         self.do_send_keys(self.pvc_loc["kms-address"], "https://www.test-service.com")
         logger.info("Storage Class Port")
+        self.do_clear(self.pvc_loc["kms-port"])
         self.do_send_keys(self.pvc_loc["kms-port"], "007")
-        logger.info("Click on Save")
-        self.do_click(self.pvc_loc["save-btn"])
-        # logger.info("Checking Selection of PVC Expansion")
-        # pvc_expansion_check = self.pvc_loc["reclaim-policy-delete"]
-        # # if not pvc_expansion_check.is_selected():
-        # self.do_click(self.pvc_loc["pvc-expansion-check"])
+        logger.info("Click on Advanced Settings")
+        self.do_click(self.pvc_loc["advanced-settings"])
+        logger.info("Enter Backend Path")
+        self.do_send_keys(self.pvc_loc["backend-path"], "IDon'tKnow")
+        logger.info("Enter TLS Server Name")
+        self.do_send_keys(self.pvc_loc["tls-server-name"], "http://vault.qe.rh-ocs.com/")
+        logger.info("Enter Vault Enterprise Namespace")
+        self.do_send_keys(self.pvc_loc["vault-enterprise-namespace"], "kms-test-namespace")
+        logger.info("Selecting CA Certificate")
+        self.do_click(self.pvc_loc["browse-ca-certificate"])
+        time.sleep(2)
+        write('/home/amagrawa/kms-cert/cert.pem')
+        press('enter')
+        logger.info("CA Certificate Selected")
+        logger.info("Selecting Client Certificate")
+        self.do_click(self.pvc_loc["browse-client-certificate"])
+        write('/home/amagrawa/kms-cert/fullchain.pem')
+        time.sleep(2)
+        press('enter')
+        logger.info("Client Certificate Selected")
+        logger.info("Selecting Client Private Key")
+        self.do_click(self.pvc_loc["browse-client-private-key"])
+        write('/home/amagrawa/kms-cert/privkey.pem')
+        time.sleep(2)
+        press('enter')
+        logger.info("Private Key Selected")
+        logger.info("Saving Key Management Service Advanced Settings")
+        self.do_click(self.pvc_loc["save-advanced-settings"])
         logger.info("Creating Storage Class with Encryption")
         self.do_click(self.pvc_loc["create"])
-
-    def create_pvc_ui(
-        self, project_name, sc_type, pvc_name, access_mode, pvc_size, vol_mode
-    ):
-        """
-        Create PVC via UI.
-        Args:
-            project_name (str): name of test project
-            sc_type (str): storage class type
-            pvc_name (str): the name of pvc
-            access_mode (str): access mode
-            pvc_size (str): the size of pvc (GB)
-            vol_mode (str): volume mode type
-        """
-        self.navigate_persistentvolumeclaims_page()
-
-        logger.info(f"Search test project {project_name}")
-        self.do_click(self.pvc_loc["pvc_project_selector"])
-        self.do_send_keys(self.pvc_loc["search-project"], text=project_name)
-
-        logger.info(f"Select test project {project_name}")
-        self.do_click(format_locator(self.pvc_loc["test-project-link"], project_name))
-
-        logger.info("Click on 'Create Persistent Volume Claim'")
-        self.do_click(self.pvc_loc["pvc_create_button"])
-
-        logger.info("Select Storage Class")
-        self.do_click(self.pvc_loc["pvc_storage_class_selector"])
-        self.do_click(self.pvc_loc[sc_type])
-
-        logger.info("Select PVC name")
-        self.do_send_keys(self.pvc_loc["pvc_name"], pvc_name)
-
-        logger.info("Select Access Mode")
-        self.do_click(self.pvc_loc[access_mode])
-
-        logger.info("Select PVC size")
-        self.do_send_keys(self.pvc_loc["pvc_size"], text=pvc_size)
-
-        if (
-            sc_type
-            in (
-            constants.DEFAULT_STORAGECLASS_RBD_THICK,
-            constants.DEFAULT_STORAGECLASS_RBD,
-        )
-            and access_mode == "ReadWriteOnce"
-        ):
-            logger.info(f"Test running on OCP version: {get_running_ocp_version()}")
-
-            logger.info(f"Selecting Volume Mode of type {vol_mode}")
-            self.do_click(self.pvc_loc[vol_mode])
-
-        logger.info("Create PVC")
-        self.do_click(self.pvc_loc["pvc_create"])
