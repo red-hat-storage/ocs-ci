916f4599351f6c76042e5aaf59d7797004740e6f
Aman Agrawal <amagrawa@redhat.com>
2021-08-03 15:28:50 +0530

    Fixes
    Signed-off-by: Aman Agrawal <amagrawa@redhat.com>

--- ocs_ci/ocs/ui/helpers_ui.py
+++ ocs_ci/ocs/ui/helpers_ui.py
@@ -68,7 +68,7 @@ def format_locator(locator, string_to_insert):
     Args:
         locator (tuple): (GUI element needs to operate on (str), type (By))
         string_to_insert (str): Name of the variable (string) which contains the dynamic web element
-            when generated on certain action.
+            when generated on certain action
 
     return:
         formats the locator using .format() function which takes string to be inserted as an argument
