5bace46e4a4e403ac0064eed1302a8d90d1c1891
Itzhak Kave <yitzhak.kave@gmail.com>
2021-08-24 12:08:22 +0300

    Change the condition to wait for the new osd node to come up after node replacement process (#4741)
    * Wait for the new osd node when we have minmum of OCS nodes, or when we have an extra OCS worker nodes to replace the old osd node
    
    Signed-off-by: Itzhak Kave <ikave@redhat.com>
    
    * Make the log messages more concise
    
    Signed-off-by: Itzhak Kave <ikave@redhat.com>
    
    * make the log message more elaborative
    
    Signed-off-by: Itzhak Kave <ikave@redhat.com>
    
    Co-authored-by: Itzhak Kave <ikave@redhat.com>

--- tests/manage/z_cluster/nodes/test_node_replacement_proactive.py
+++ tests/manage/z_cluster/nodes/test_node_replacement_proactive.py
@@ -58,8 +58,40 @@ def check_node_replacement_verification_steps(
         AssertionError: If the node replacement verification steps failed.
 
     """
-    new_osd_node_name = node.wait_for_new_osd_node(old_osd_node_names, timeout=1500)
-    assert new_osd_node_name, "New osd node not found"
+    min_osd_nodes = 3
+    num_of_old_osd_nodes = len(old_osd_node_names)
+    ocs_nodes = node.get_ocs_nodes()
+    num_of_old_ocs_nodes = len(ocs_nodes)
+
+    if num_of_old_osd_nodes <= min_osd_nodes:
+        log.info(
+            f"We have {num_of_old_osd_nodes} osd nodes in the cluster - which is the minimum number "
+            f"of osd nodes. Wait for the new created worker node to appear in the osd nodes"
+        )
+        timeout = 1500
+        new_osd_node_name = node.wait_for_new_osd_node(old_osd_node_names, timeout)
+        assert new_osd_node_name, (
+            f"New osd node not found after the node replacement process "
+            f"while waiting for {timeout} seconds"
+        )
+    elif num_of_old_osd_nodes < num_of_old_ocs_nodes:
+        num_of_extra_old_ocs_nodes = num_of_old_ocs_nodes - num_of_old_osd_nodes
+        log.info(
+            f"We have {num_of_extra_old_ocs_nodes} existing extra OCS worker nodes in the cluster"
+            f"Wait for one of the existing OCS nodes to appear in the osd nodes"
+        )
+        timeout = 600
+        new_osd_node_name = node.wait_for_new_osd_node(old_osd_node_names, timeout)
+        assert new_osd_node_name, (
+            f"New osd node not found after the node replacement process "
+            f"while waiting for {timeout} seconds"
+        )
+    else:
+        log.info(
+            f"We have more than {min_osd_nodes} osd nodes in the cluster, and also we don't have "
+            f"an existing extra OCS worker nodes in the cluster. Don't wait for the new osd node"
+        )
+        new_osd_node_name = None
 
     assert node.node_replacement_verification_steps_ceph_side(
         old_node_name, new_node_name, new_osd_node_name
