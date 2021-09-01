e2764696cfbed640f1a275e4d35a83744c7ed3f3
OdedViner <61982127+OdedViner@users.noreply.github.com>
2021-09-01 09:15:25 +0300

    GSS bz 1974343, Verify expected log exist after upgrade (#4714)
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- tests/ecosystem/upgrade/test_resources.py
+++ tests/ecosystem/upgrade/test_resources.py
@@ -3,15 +3,22 @@ import logging
 import pytest
 
 from ocs_ci.framework import config
+from ocs_ci.ocs.resources.pod import get_pod_logs
 from ocs_ci.framework.pytest_customization.marks import (
     ignore_leftovers,
     pre_upgrade,
     post_upgrade,
     skipif_aws_creds_are_missing,
+    bugzilla,
 )
 from ocs_ci.ocs import constants
 from ocs_ci.ocs import ocp
-from ocs_ci.ocs.resources.pod import wait_for_storage_pods
+from ocs_ci.ocs.resources.pod import (
+    wait_for_storage_pods,
+    get_osd_pods,
+    get_mon_pods,
+    get_mgr_pods,
+)
 from ocs_ci.helpers import helpers
 
 log = logging.getLogger(__name__)
@@ -90,3 +97,29 @@ def test_pod_io(
         helpers.wait_for_resource_state(pod, constants.STATUS_RUNNING, timeout=600)
         job_name = pod.get_labels().get("job-name")
         job_obj.delete(resource_name=job_name)
+
+
+@post_upgrade
+@bugzilla("1974343")
+@pytest.mark.polarion_id("OCS-2629")
+def test_pod_log_after_upgrade():
+    """
+    Check OSD/MON/MGR pod logs after upgrade and verify the expected log exist
+
+    """
+    pod_objs = get_osd_pods() + get_mon_pods() + get_mgr_pods()
+    pod_names = [osd_pod_obj.name for osd_pod_obj in pod_objs]
+    expected_log_after_upgrade = "set uid:gid to 167:167 (ceph:ceph)"
+    logging.info(
+        f"Check that the log '{expected_log_after_upgrade}' "
+        f"appears after the osd/mon/mg pod is initialized"
+    )
+    for pod_name in pod_names:
+        pod_logs = get_pod_logs(pod_name=pod_name, all_containers=True)
+        assert expected_log_after_upgrade in pod_logs, (
+            f"The expected log after upgrade '{expected_log_after_upgrade}' does not exist"
+            f" on pod {pod_name}"
+        )
+    logging.info(
+        f"The log '{expected_log_after_upgrade}' appears in all relevant pods."
+    )
