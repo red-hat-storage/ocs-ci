8ea3c0f9f64f12c7d1950554a48429766b479551
vavuthu <vavuthu@redhat.com>
2021-09-09 18:18:15 +0530

    moving shutdown_wait_timeout to vm module
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/deployment/vmware.py
+++ ocs_ci/deployment/vmware.py
@@ -416,7 +416,7 @@ class VSPHEREUPI(VSPHEREBASE):
             # generate terraform variable file
             generate_terraform_vars_and_update_machine_conf()
 
-            # Add shutdown_wait_timeout to compute and control-plane VM's
+            # Add shutdown_wait_timeout to VM's
             add_shutdown_wait_timeout()
 
             # sync guest time with host
@@ -1093,26 +1093,20 @@ def update_gw(str_to_replace, config_file):
 
 def add_shutdown_wait_timeout():
     """
-    Add shutdown_wait_timeout to compute and control-plane VM's
+    Add shutdown_wait_timeout to VM's
+
     shutdown_wait_timeout is the amount of time, in minutes, to wait for a graceful guest shutdown
     when making necessary updates to the virtual machine. If force_power_off is set to true, the VM will be
     force powered-off after this timeout, otherwise an error is returned. Default: 3 minutes.
 
     """
-    with open(constants.VSPHERE_MAIN, "r") as fd:
+    with open(constants.VM_MAIN, "r") as fd:
         obj = hcl2.load(fd)
-        count = 0
-        for index in range(len(obj["module"]) - 1, -1, -1):
-            if "compute_vm" in obj["module"][index].keys():
-                obj["module"][index]["compute_vm"]["shutdown_wait_timeout"] = 10
-                count += 1
-            if "control_plane_vm" in obj["module"][index].keys():
-                obj["module"][index]["control_plane_vm"]["shutdown_wait_timeout"] = 10
-                count += 1
-            if count == 2:
-                break
-    dump_data_to_json(obj, f"{constants.VSPHERE_MAIN}.json")
-    os.rename(constants.VSPHERE_MAIN, f"{constants.VSPHERE_MAIN}.backup")
+        obj["resource"][0]["vsphere_virtual_machine"]["vm"][
+            "shutdown_wait_timeout"
+        ] = 10
+    dump_data_to_json(obj, f"{constants.VM_MAIN}.json")
+    os.rename(constants.VM_MAIN, f"{constants.VM_MAIN}.backup")
 
 
 def update_dns():
