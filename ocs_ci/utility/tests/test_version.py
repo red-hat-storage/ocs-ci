f207804f5ec51889d06d90f0ef5a35e1631032a1
Petr Balogh <pbalogh@redhat.com>
2021-09-22 16:41:18 +0200

    Add test for version module
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

---
+++ ocs_ci/utility/tests/test_version.py
@@ -0,0 +1,65 @@
+from semantic_version import Version
+
+import pytest
+
+from ocs_ci.utility import version
+
+
+@pytest.mark.parametrize(
+    "product_version, only_major_minor, ignore_pre_release, expected",
+    [
+        ("4.4", False, False, Version.coerce("4.4")),
+        ("4.4.5", True, False, version.VERSION_4_4),
+        ("4.4.5", True, True, version.VERSION_4_4),
+        ("4.4.5-182.ci", True, False, version.VERSION_4_4),
+        ("4.4.5-182.ci", False, False, Version.coerce("4.4.5-182.ci")),
+    ],
+)
+def test_get_semantic_version(
+    product_version, only_major_minor, ignore_pre_release, expected
+):
+    """
+    This test is suppose to test if the get_semantic_version returns
+    expected values for different combinations of paramters and different values
+    of string version provided to that function.
+    """
+
+    tested_version = version.get_semantic_version(
+        product_version, only_major_minor, ignore_pre_release
+    )
+    assert tested_version == expected
+
+
+@pytest.mark.parametrize(
+    "product_version, only_major_minor, ignore_pre_release, expected",
+    [
+        ("4.4", False, False, "4.4.0"),
+        ("4.4.5", True, False, "4.4"),
+        ("4.4.5", True, True, "4.4"),
+        ("4.4.5-182.ci", True, False, "4.4"),
+        ("4.4.5-182.ci", False, False, "4.4.5-182.ci"),
+        ("4.4.5-182.ci", False, True, "4.4.5"),
+    ],
+)
+def test_get_semantic_version_string_values(
+    product_version, only_major_minor, ignore_pre_release, expected
+):
+    """
+    This test is suppose to test if the get_semantic_version returns
+    expected values which after the sting formatting are the same as the expected.
+    Testing all different combinations of parameters and values.
+    """
+    tested_version = version.get_semantic_version(
+        product_version, only_major_minor, ignore_pre_release
+    )
+    assert f"{tested_version}" == expected
+
+
+def test_compare_from_get_semantic_version():
+    """
+    This teest is testing that semantic version comparison works as expected and
+    version 4.5.11 is lower than 4.11. Which in the float will not work, but in semantic
+    versions it should be fine.
+    """
+    tested_version = version.get_semantic_version("4.5.11", only_major_minor=True)
+    assert tested_version < Version.coerce("4.11")
