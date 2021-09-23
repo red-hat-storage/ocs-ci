ed3c8a784f8485f33abe14983d8f6f0994b929c9
Suchita Gatfane <sgatfane@redhat.com>
2021-09-23 11:51:04 +0530

    Add marker for aws based platforms only test
    ROSA is Redhat openshift service on AWS. Hence the test
    applicable on aws should be consider for ROSA platform as well
    
    Signed-off-by: Suchita Gatfane <sgatfane@redhat.com>

--- tests/manage/z_cluster/nodes/test_nodes_maintenance.py
+++ tests/manage/z_cluster/nodes/test_nodes_maintenance.py
@@ -27,7 +27,7 @@ from ocs_ci.framework.testlib import (
     tier4,
     tier4b,
     ManageTest,
-    aws_platform_required,
+    aws_based_platform_required,
     ignore_leftovers,
     ipi_deployment_required,
     skipif_bm,
@@ -313,7 +313,7 @@ class TestNodesMaintenance(ManageTest):
 
     @tier4
     @tier4b
-    @aws_platform_required
+    @aws_based_platform_required
     @ipi_deployment_required
     @pytest.mark.parametrize(
         argnames=["interface"],
