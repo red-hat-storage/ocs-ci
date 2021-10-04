647a0d3e69b1d83e96d64ff8879c43ce657019dc
Daniel Horak <dahorak@redhat.com>
2021-10-04 14:19:44 +0200

    discon.cl: ODF 4.9 - add newly required packages
    - OCS/ODF 4.9 is now split between three operators, except ocs-operator
      there is also odf-operator and noobaa-operator
    - fixes https://github.com/red-hat-storage/ocs-ci/issues/4882
    
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -1150,7 +1150,10 @@ DISCON_CL_REQUIRED_PACKAGES = [
     "cluster-logging",
     "elasticsearch-operator",
     "local-storage-operator",
+    "noobaa-operator",
     "ocs-operator",
+    "odf-multicluster-orchestrator",
+    "odf-operator",
 ]
 
 # PSI-openstack constants
