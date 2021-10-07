75bb5c0c206b9d0ec91386298406180577958119
Aman Agrawal <amagrawa@redhat.com>
2021-10-07 22:42:12 +0530

    fix
    Signed-off-by: Aman Agrawal <amagrawa@redhat.com>

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         "pyopenssl",
         "pyparsing ==2.4.7",
         "mysql-connector-python==8.0.27",
+        "pyautogui==0.9.53",
         "pytest-repeat",
         "pexpect>=4.8.0",
         # googleapis-common-protos 1.56.2 needs to have protobuf<4.0.0>=3.15.0
