cea6e2268639563540f1952ffbce4260d275be65
Shylesh Kumar Mohan <shmohan@redhat.com>
2021-10-13 21:38:34 -0700

    add looping after conflict resolution
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- ocs_ci/framework/main.py
+++ ocs_ci/framework/main.py
@@ -236,21 +236,23 @@ def tokenize_per_cluster_args(args, nclusters):
 def main(argv=None):
     arguments = argv or sys.argv[1:]
     init_ocsci_conf(arguments)
-    pytest_logs_dir = utils.ocsci_log_path()
-    utils.create_directory_path(framework.config.RUN["log_dir"])
-    launch_name = reporting.get_rp_launch_name()
-    arguments.extend(
-        [
-            "-p",
-            "ocs_ci.framework.pytest_customization.ocscilib",
-            "-p",
-            "ocs_ci.framework.pytest_customization.marks",
-            "-p",
-            "ocs_ci.framework.pytest_customization.reports",
-            "--logger-logsdir",
-            pytest_logs_dir,
-            "--rp-launch",
-            launch_name,
-        ]
-    )
+    for i in range(framework.config.nclusters):
+        framework.config.switch_ctx(i)
+        pytest_logs_dir = utils.ocsci_log_path()
+        utils.create_directory_path(framework.config.RUN["log_dir"])
+        launch_name = reporting.get_rp_launch_name()
+        arguments.extend(
+            [
+                "-p",
+                "ocs_ci.framework.pytest_customization.ocscilib",
+                "-p",
+                "ocs_ci.framework.pytest_customization.marks",
+                "-p",
+                "ocs_ci.framework.pytest_customization.reports",
+                "--logger-logsdir",
+                pytest_logs_dir,
+                "--rp-launch",
+                launch_name,
+            ]
+        )
     return pytest.main(arguments)
