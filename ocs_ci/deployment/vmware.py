dc017ffefb723fdcaacbe8683219dd6a3e51de74
vavuthu <vavuthu@redhat.com>
2021-11-25 16:09:20 +0530

    move constants.VM_MAIN json file if it already exists
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/deployment/vmware.py
+++ ocs_ci/deployment/vmware.py
@@ -714,6 +714,16 @@ class VSPHEREUPI(VSPHEREBASE):
         )
 
         clone_openshift_installer()
+        rename_files = [constants.VSPHERE_MAIN, constants.VM_MAIN]
+        for each_file in rename_files:
+            if os.path.exists(f"{each_file}.backup") and os.path.exists(
+                f"{each_file}.json"
+            ):
+                os.rename(
+                    f"{each_file}.json",
+                    f"{each_file}.json.backup",
+                )
+
         if os.path.exists(f"{constants.VSPHERE_MAIN}.backup") and os.path.exists(
             f"{constants.VSPHERE_MAIN}.json"
         ):
