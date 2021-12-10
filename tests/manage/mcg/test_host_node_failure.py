65812aac3285c1ddf768eec2ac6282e6fb975fc4
Ben Eli <belimele@redhat.com>
2021-12-10 16:13:10 +0100

    Extend the MCG DB node failure recovery timeout (#5195)
    We're currently seeing failures after waiting 600 seconds, but the pod seems to reach a Running state after 660~ second, and thus, the timeout was extended

--- tests/manage/mcg/test_host_node_failure.py
+++ tests/manage/mcg/test_host_node_failure.py
@@ -183,7 +183,7 @@ class TestNoobaaSTSHostNodeFailure(ManageTest):
             condition=constants.STATUS_RUNNING,
             selector=self.labels_map[noobaa_sts],
             resource_count=1,
-            timeout=600 if noobaa_sts == constants.NOOBAA_DB_STATEFULSET else 60,
+            timeout=800 if noobaa_sts == constants.NOOBAA_DB_STATEFULSET else 60,
             sleep=30 if noobaa_sts == constants.NOOBAA_DB_STATEFULSET else 3,
         )
 
