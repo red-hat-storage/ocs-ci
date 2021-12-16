3672eb382205ef7e1c027e34928b751bff56efc1
Martin Bukatovič <mbukatov@redhat.com>
2021-12-16 19:37:36 +0100

    data consistency test case
    fixes https://github.com/red-hat-storage/ocs-ci/issues/5242
    
    Signed-off-by: Martin Bukatovič <mbukatov@redhat.com>

--- ocs_ci/ocs/resources/objectconfigfile.py
+++ ocs_ci/ocs/resources/objectconfigfile.py
@@ -22,10 +22,33 @@ import time
 import yaml
 
 from ocs_ci.utility.utils import run_cmd
-from ocs_ci.ocs.exceptions import CommandFailed
+from ocs_ci.ocs.exceptions import CommandFailed, NotFoundError
 
 
 logger = logging.getLogger(name=__file__)
+
+
+def link_spec_volume(spec_dict, volume_name, pvc_name):
+    """
+    Find volume of given name in given spec dict, and set given pvc name as
+    a pvc for the volume in the spec.
+
+    Args:
+        spec_dict (dict): dictionary with a container/template spec
+        volume_name (str): name of the volume in the spec dict to link
+        pvc_name (str): name of the target pvc (for the given volume)
+
+    Raises:
+        NotFoundError when given volume is not found in given spec
+    """
+    is_pvc_linked = False
+    for vol in spec_dict["volumes"]:
+        if vol["name"] == volume_name:
+            vol["persistentVolumeClaim"]["claimName"] = pvc_name
+            is_pvc_linked = True
+            break
+    if not is_pvc_linked:
+        raise NotFoundError("volume %s not found in given spec")
 
 
 class ObjectConfFile:
---
+++ ocs_ci/templates/workloads/logwriter/README.md
@@ -0,0 +1,15 @@
+# Simple log reader/writer IO workload
+
+Files in this directory are k8s yaml files, not jinja templates.
+
+This workload can write a line with timestamp and a checksum of a previous line
+into a log file periodically, and read such log file and verify that content of
+the file is consistent (verifying the checksums there).
+
+The goal of this workload is to test:
+
+- IO running on all nodes of a cluster via k8s `Deployment`
+- that there are no holes in the log during disruptions a cluster should handle
+  (as evidence whether IO was affected)
+- data consistency of the written data
+- read/write IO stress such workload can create
