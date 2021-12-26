33822f9e7571e7afc90df619b53769fa5793a943
Amit Berner <aberner@redhat.com>
2021-12-26 16:46:20 +0200

    black
    Signed-off-by: Amit Berner <aberner@redhat.com>

--- ocs_ci/ocs/bucket_utils.py
+++ ocs_ci/ocs/bucket_utils.py
@@ -200,9 +200,9 @@ def rm_object_recursive(podobj, target, mcg_obj, option=""):
 
 def get_db_scc_and_sa():
     if version.get_semantic_ocs_version_from_config() < version.VERSION_4_9:
-        return constants.NOOBAA_RESOURCE_NAME, constants.NOOBAA_SERVICE_ACCOUNT
+        return constants.NOOBAA_SERVICE_ACCOUNT_NAME, constants.NOOBAA_SERVICE_ACCOUNT
     else:
-        return constants.NOOBAA_ENDPOINT_DEPLOYMENT, constants.NOOBAA_DB_SERVICE_ACCOUNT
+        return constants.NOOBAA_DB_SERVICE_ACCOUNT_NAME, constants.NOOBAA_DB_SERVICE_ACCOUNT
 
 
 def get_rgw_restart_counts():
