665c25a165de057719a5c88ce3eada96051a1f8f
Petr Balogh <pbalogh@redhat.com>
2022-01-05 15:27:31 +0100

    Improving manual approval strategy installation and wait for CSV
    Fixes: #5290
    
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -398,8 +398,9 @@ class Deployment(object):
         time.sleep(90)
         if subscription_plan_approval == "Manual":
             wait_for_install_plan_and_approve(self.namespace)
-            logger.info("Sleeping for 60 seconds after approving install plan for OCS")
-            time.sleep(60)
+            csv_name = package_manifest.get_current_csv(channel=custom_channel)
+            csv = CSV(resource_name=csv_name, namespace=self.namespace)
+            csv.wait_for_phase("Installing", timeout=60)
 
     def get_arbiter_location(self):
         """
