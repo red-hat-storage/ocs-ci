d12aab9d627d9311ba8afe91012efdf10264ab9b
Parikshith Byregowda <pbyregow@redhat.com>
2022-02-07 18:50:17 +0530

    Moved to new test for this flow
    Signed-off-by: Parikshith Byregowda <pbyregow@redhat.com>

--- tests/manage/mcg/test_verify_noobaa_status.py
+++ tests/manage/mcg/test_verify_noobaa_status.py
@@ -1,11 +1,11 @@
 import logging
 import re
 
-from ocs_ci.framework.pytest_customization.marks import tier1
+from ocs_ci.framework.pytest_customization.marks import tier1, skipif_ocs_version
 from ocs_ci.framework.pytest_customization.marks import skipif_openshift_dedicated
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.resources.pod import get_pod_logs
-from ocs_ci.framework.testlib import polarion_id, bugzilla, version
+from ocs_ci.framework.testlib import polarion_id, bugzilla
 from ocs_ci.framework.pytest_customization.marks import skipif_managed_service
 
 log = logging.getLogger(__name__)
@@ -14,33 +14,43 @@ log = logging.getLogger(__name__)
 @tier1
 @polarion_id("OCS-2084")
 @bugzilla("1799077")
-@bugzilla("2004130")
 @skipif_openshift_dedicated
 @skipif_managed_service
 def test_verify_noobaa_status_cli(mcg_obj_session):
     """
     Verify noobaa status output is clean without any errors using the noobaa cli
     """
-    ocs_version_semantic = version.get_semantic_ocs_version_from_config()
-
     # Get noobaa status
     status = mcg_obj_session.exec_mcg_cmd("status").stderr
     for line in status.split("\n"):
         if "Not Found" in line:
             assert "Optional" in line, f"Error in noobaa status output- {line}"
-        if (
-            ocs_version_semantic >= version.VERSION_4_8
-            and 'Service "noobaa-db"' in line
-        ):
-            assert (
-                "Old noobaa-db service is logged" in line
-            ), f"Error in MCG Cli status output- {line}"
     log.info("Verified: noobaa status does not contain any error.")
 
-    # verify noobaa db logs for #bz2004130
-    if ocs_version_semantic >= version.VERSION_4_8:
-        pattern = "Not found: Service noobaa-db"
-        noobaa_db_log = get_pod_logs(pod_name=constants.NB_DB_NAME_47_AND_ABOVE)
+
+@tier1
+@skipif_ocs_version("<4.8")
+@polarion_id("OCS-2748")
+@bugzilla("2004130")
+@skipif_openshift_dedicated
+@skipif_managed_service
+def test_verify_noobaa_db_service(mcg_obj_session):
+    """
+    Validates whether MCG cli and noobaa db logs does not check 'noobaa-db'
+    """
+    # Get noobaa status
+    status = mcg_obj_session.exec_mcg_cmd("status").stderr
+    for line in status.split("\n"):
         assert (
-            re.search(pattern=pattern, string=noobaa_db_log) is None
-        ), f"Error: {pattern} msg found in the noobaa db logs."
+            'Service "noobaa-db"' not in status
+        ), f"Error in MCG Cli status output- {line}"
+    log.info(
+        "Verified: noobaa status does not contain error related to `noobaa-db` service."
+    )
+
+    # verify noobaa db logs
+    pattern = "Not found: Service noobaa-db"
+    noobaa_db_log = get_pod_logs(pod_name=constants.NB_DB_NAME_47_AND_ABOVE)
+    assert (
+        re.search(pattern=pattern, string=noobaa_db_log) is None
+    ), f"Error: {pattern} msg found in the noobaa db logs."
