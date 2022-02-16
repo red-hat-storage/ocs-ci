d7a823bff6dd24433e054508e22bff36e75301e3
Parikshith Byregowda <pbyregow@redhat.com>
2022-02-16 12:46:27 +0530

    Sanity
    Signed-off-by: Parikshith Byregowda <pbyregow@redhat.com>

--- tests/manage/z_cluster/test_hugepages.py
+++ tests/manage/z_cluster/test_hugepages.py
@@ -1,14 +1,15 @@
 import logging
 import pytest
 
+from ocs_ci.helpers.sanity_helpers import Sanity
 from ocs_ci.ocs import constants
 from ocs_ci.utility.utils import (
     enable_huge_pages,
     disable_huge_pages,
-    verify_huge_pages,
 )
 from ocs_ci.ocs.node import (
     wait_for_nodes_status,
+    get_nodes,
 )
 from ocs_ci.ocs.resources.pod import (
     wait_for_pods_to_be_running,
@@ -39,13 +40,18 @@ class TestHugePages(E2ETest):
     """
 
     @pytest.fixture(scope="function", autouse=True)
-    def huge_pages_teardown(self, request):
+    def huge_pages_setup(self, request):
         """
-        Removes huge pages and verifies all pods are up
+        Initializes sanity
 
         """
+        self.sanity_helpers = Sanity()
 
         def finalizer():
+            """
+            Removes huge pages on worker nodes and verifies all pods are up
+
+            """
             disable_huge_pages()
 
             wait_for_nodes_status(status=constants.NODE_READY, timeout=600)
@@ -56,18 +62,38 @@ class TestHugePages(E2ETest):
         request.addfinalizer(finalizer)
 
     def test_hugepages_post_odf_deployment(
-        self,
+        self, pvc_factory, pod_factory, bucket_factory, rgw_bucket_factory
     ):
         """
         Test to verify that after enabling huge pages the nodes come up with
         higher page size and all odf cluster pods come back up.
 
         """
+        # Applies huge pages on the cluster nodes
         enable_huge_pages()
 
+        log.info("Wait for all worker node to be READY state")
         wait_for_nodes_status(status=constants.NODE_READY, timeout=600)
 
-        assert verify_huge_pages()
+        nodes = get_nodes()
+        for node in nodes:
+            assert (
+                node.get()["status"]["allocatable"]["hugepages-2Mi"] == "64Mi"
+            ), f"Huge pages is not applied on {node.name}"
 
-        log.info("Wait for all pods to be in running state")
+        log.info("Wait for all storage cluster pods to be in running state")
         wait_for_pods_to_be_running(timeout=600)
+
+        # Creating Resources
+        log.info("Creating Resources using sanity helpers")
+        self.sanity_helpers.create_resources(
+            pvc_factory, pod_factory, bucket_factory, rgw_bucket_factory, False
+        )
+
+        # Deleting Resources
+        log.info("Deleting the resources created")
+        self.sanity_helpers.delete_resources()
+
+        # Verify everything running fine
+        log.info("Verifying All resources are Running and matches expected result")
+        self.sanity_helpers.health_check(tries=120)
