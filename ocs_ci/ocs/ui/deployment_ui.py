2ea89300e0a490739b994dba5a8390bb255e03ea
Oded Viner <oviner@redhat.com>
2022-02-28 12:36:10 +0200

    LSO Deployment via UI
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- ocs_ci/ocs/ui/deployment_ui.py
+++ ocs_ci/ocs/ui/deployment_ui.py
@@ -10,7 +10,10 @@ from ocs_ci.ocs.exceptions import TimeoutExpiredError
 from ocs_ci.framework import config
 from ocs_ci.ocs import constants, defaults
 from ocs_ci.ocs.node import get_worker_nodes
-from ocs_ci.deployment.helpers.lso_helpers import add_disk_for_vsphere_platform
+from ocs_ci.deployment.helpers.lso_helpers import (
+    add_disk_for_vsphere_platform,
+    create_optional_operators_catalogsource_non_ga,
+)
 
 
 logger = logging.getLogger(__name__)
@@ -377,7 +380,7 @@ class DeploymentUI(PageNavigator):
         self.navigate_operatorhub_page()
         self.navigate_installed_operators_page()
         logger.info(f"Search {operator} operator installed")
-        if self.ocp_version in ("4.7", "4.8", "4.9"):
+        if self.ocp_version in ("4.7", "4.8", "4.9", "4.10"):
             self.do_send_keys(
                 locator=self.dep_loc["search_operator_installed"],
                 text=operator,
@@ -408,6 +411,7 @@ class DeploymentUI(PageNavigator):
 
         """
         if config.DEPLOYMENT.get("local_storage"):
+            create_optional_operators_catalogsource_non_ga()
             add_disk_for_vsphere_platform()
         self.install_local_storage_operator()
         self.install_ocs_operator()
