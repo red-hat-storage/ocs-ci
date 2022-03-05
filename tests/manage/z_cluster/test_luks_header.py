11909ba61c490dadea2fedfb32395151c0b28c5f
mashetty <mashetty@redhat.com>
2022-03-05 11:39:43 +0530

    to run whoami command
    Signed-off-by: mashetty <mashetty@redhat.com>

---
+++ tests/manage/z_cluster/test_luks_header.py
@@ -0,0 +1,15 @@
+import random
+
+import logging
+from ocs_ci.ocs import node
+from ocs_ci.utility.vsphere_nodes import VSPHERENode
+
+
+def test_luks_header():
+
+    nodes = node.get_all_nodes()
+    node_interested = random.choice(nodes)
+    logging.info("CHECKING USER OF NODE: {}".format(node_interested))
+    vm_node = VSPHERENode(host=node_interested, user="mahesh")
+    user_logged = vm_node.get_username()
+    logging.info("OUTPUT: {}".format(user_logged))
