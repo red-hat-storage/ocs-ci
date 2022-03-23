d0ca688941e96249da19b171f9c96d88e785805b
Filip Balak <fbalak@redhat.com>
2022-03-23 08:48:26 +0100

    update condition for sub patch
    Signed-off-by: Filip Balak <fbalak@redhat.com>

--- ocs_ci/utility/rosa.py
+++ ocs_ci/utility/rosa.py
@@ -12,6 +12,7 @@ import re
 from ocs_ci.framework import config
 from ocs_ci.ocs import ocp
 from ocs_ci.ocs.exceptions import (
+    CommandFailed,
     ManagedServiceAddonDeploymentError,
     UnsupportedPlatformVersionError,
 )
@@ -235,6 +236,7 @@ def install_odf_addon(cluster):
 
     utils.run_cmd(cmd)
     counter = 0
+    sub_updated = False
     for addon_info in utils.TimeoutSampler(
         4000, 30, get_addon_info, cluster, addon_name
     ):
@@ -242,11 +244,15 @@ def install_odf_addon(cluster):
         # https://bugzilla.redhat.com/show_bug.cgi?id=2066211
         if config.ENV_DATA.get("cluster_type", "").lower() == "provider":
             counter += 1
-            if counter == 5:
-                patch = ' \'{"spec": {"sourceNamespace":"openshift-storage"}}\' '
-                ocp.OCP(namespace="openshift-storage").exec_oc_cmd(
-                    f"patch sub odf-csi-addons-operator -p {patch} --type=merge"
-                )
+            if counter > 5 and not sub_updated:
+                try:
+                    patch = ' \'{"spec": {"sourceNamespace":"openshift-storage"}}\' '
+                    ocp.OCP(namespace="openshift-storage").exec_oc_cmd(
+                        f"patch sub odf-csi-addons-operator -p {patch} --type=merge"
+                    )
+                    sub_updated = True
+                except CommandFailed:
+                    logger.info("odf-csi-addons-operator was not yet updated")
         logger.info(f"Current addon installation info: " f"{addon_info}")
         if "ready" in addon_info:
             logger.info(f"Addon {addon_name} was installed")
