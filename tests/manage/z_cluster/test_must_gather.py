10d8e3c6143e8e37570d4ca248ec131528c6ddc5
Jilju Joy <jijoy@redhat.com>
2022-04-07 18:34:17 +0530

    Address comments
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/manage/z_cluster/test_must_gather.py
+++ tests/manage/z_cluster/test_must_gather.py
@@ -2,12 +2,7 @@ import logging
 import pytest
 
 from ocs_ci.framework import config
-from ocs_ci.framework.testlib import (
-    ManageTest,
-    tier1,
-    skipif_external_mode,
-    skipif_managed_service,
-)
+from ocs_ci.framework.testlib import ManageTest, tier1, skipif_external_mode
 from ocs_ci.ocs.must_gather.must_gather import MustGather
 from ocs_ci.ocs.must_gather.const_must_gather import GATHER_COMMANDS_VERSION
 
@@ -40,10 +35,7 @@ class TestMustGather(ManageTest):
                 *["JSON"],
                 marks=[pytest.mark.polarion_id("OCS-1583"), skipif_external_mode]
             ),
-            pytest.param(
-                *["OTHERS"],
-                marks=[pytest.mark.polarion_id("OCS-1583"), skipif_managed_service]
-            ),
+            pytest.param(*["OTHERS"], marks=pytest.mark.polarion_id("OCS-1583")),
         ],
     )
     @pytest.mark.skipif(
