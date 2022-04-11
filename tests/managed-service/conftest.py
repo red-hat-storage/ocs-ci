d78114ebc56c86e7e84d7fc692c3b74999342224
Oded Viner <oviner@redhat.com>
2022-04-11 13:10:24 +0300

    fix code
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- tests/managed-service/conftest.py
+++ tests/managed-service/conftest.py
@@ -11,8 +11,8 @@ logger = logging.getLogger(__name__)
 def get_consumer_clusters():
     logger.info("Get Consumer Clusters on setup")
     consumer_clusters = list()
-    for consumer_cluster_index in range(config.nclusters):
-        config.switch_ctx(consumer_cluster_index)
-        if config.ENV_DATA["cluster_type"] == "consumer":
-            consumer_clusters.append(consumer_cluster_index)
+    for index in range(config.nclusters):
+        if config.clusters[index].ENV_DATA["cluster_type"] == "consumer":
+            consumer_clusters.append(index)
+            config.consumer_test = list()
     config.index_consumer_clusters = consumer_clusters
