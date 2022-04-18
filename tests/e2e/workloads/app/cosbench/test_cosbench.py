2175a02d9eedc3f9c6d5d1b90cbde0d8cacb8005
Parikshith Byregowda <pbyregow@redhat.com>
2022-04-18 16:06:15 +0530

    Cosbench workload: small, medium and large size objects
    Signed-off-by: Parikshith Byregowda <pbyregow@redhat.com>

--- tests/e2e/workloads/app/cosbench/test_cosbench.py
+++ tests/e2e/workloads/app/cosbench/test_cosbench.py
@@ -23,29 +23,45 @@ def cosbench(request):
 @pytest.mark.polarion_id("OCS-2529")
 class TestCosbenchWorkload(E2ETest):
     """
-    Test cosbench workloads on MCG
+    Test Cosbench workloads on MCG
+
     """
 
-    def test_cosbench_workload_simple(self, cosbench):
+    @pytest.mark.parametrize(
+        argnames="objects, size, size_unit",
+        argvalues=[
+            pytest.param(
+                *[10000, 4, "KB"],
+            ),
+            pytest.param(
+                *[1000, 2, "MB"],
+            ),
+            pytest.param(
+                *[500, 8, "MB"],
+            ),
+        ],
+    )
+    def test_cosbench_workload_simple(self, cosbench, objects, size, size_unit):
         """
         Tests basic Cosbench workload.
         Creates and deletes objects and buckets.
+
         """
         bucket_prefix = "mcg-bucket-"
         buckets = 5
-        objects = 10
 
         # Deployment of cosbench
         cosbench.setup_cosbench()
 
         # Create initial containers and objects
         cosbench.run_init_workload(
-            prefix=bucket_prefix, containers=buckets, objects=objects, validate=True
+            prefix=bucket_prefix, containers=buckets, objects=objects, validate=True, size=size,
+            size_unit=size_unit, timeout=1200
         )
 
         # Dispose containers and objects
         cosbench.run_cleanup_workload(
-            prefix=bucket_prefix, containers=buckets, objects=objects, validate=True
+            prefix=bucket_prefix, containers=buckets, objects=objects, validate=True, timeout=1200
         )
 
     def test_cosbench_workload_operations(self, cosbench):
