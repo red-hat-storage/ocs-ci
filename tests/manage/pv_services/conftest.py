5a7b30effbea34b4e4457879740f2cd55d216efe
Jilju Joy <jijoy@redhat.com>
2022-04-22 12:09:58 +0530

    Skip snapshot tests on MS platform (#5680)
    * Skip snapshot tests on MS using bugzilla marker
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/manage/pv_services/conftest.py
+++ tests/manage/pv_services/conftest.py
@@ -3,6 +3,7 @@ import pytest
 
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.resources import pod
+from ocs_ci.framework import config
 
 log = logging.getLogger(__name__)
 
@@ -169,3 +170,21 @@ def create_pvcs_and_pods(multi_pvc_factory, pod_factory, service_account_factory
         return pvcs, pods
 
     return factory
+
+
+def pytest_collection_modifyitems(items):
+    """
+    Skip tests in a directory based on conditions
+
+    Args:
+        items: list of collected tests
+
+    """
+    if config.ENV_DATA["platform"].lower() in constants.MANAGED_SERVICE_PLATFORMS:
+        for item in items.copy():
+            if "manage/pv_services/pvc_snapshot" in str(item.fspath):
+                log.info(
+                    f"Test {item} is removed from the collected items. PVC snapshot is not supported on"
+                    f" {config.ENV_DATA['platform'].lower()} due to the bug 2069367"
+                )
+                items.remove(item)
