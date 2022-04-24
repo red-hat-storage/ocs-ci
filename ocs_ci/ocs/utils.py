f3ce1016220fcbad7e7361bbc5bb374501cb0c2d
Oded Viner <oviner@redhat.com>
2022-04-24 16:14:52 +0300

    Get the output mg-helper pod in failure
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- ocs_ci/ocs/utils.py
+++ ocs_ci/ocs/utils.py
@@ -902,17 +902,39 @@ def run_must_gather(log_dir_path, image, command=None):
             cmd, out_yaml_format=False, timeout=must_gather_timeout
         )
     except CommandFailed as ex:
+        log.error(get_helper_pods_output())
         log.error(
             f"Failed during must gather logs! Error: {ex}"
             f"Must-Gather Output: {mg_output}"
         )
     except TimeoutExpired as ex:
+        log.error(get_helper_pods_output())
         log.error(
             f"Timeout {must_gather_timeout}s for must-gather reached, command"
             f" exited with error: {ex}"
             f"Must-Gather Output: {mg_output}"
         )
     return mg_output
+
+
+def get_helper_pods_output():
+    """
+    Get the output of "oc describe mg-helper pods"
+
+    Returns:
+        output_describe_mg_helper (str): the output of "oc describe mg-helper pods"
+
+    """
+    from ocs_ci.ocs.resources.pod import get_pod_obj
+
+    output_describe_mg_helper = ""
+    helper_pods = get_pod_name_by_pattern(pattern="helper")
+    for helper_pod in helper_pods:
+        helper_pod_obj = get_pod_obj(name=helper_pod, namespace=None)
+        output_describe_mg_helper += (
+            f"****helper pod {helper_pod} describe****\n{helper_pod_obj.describe()}\n"
+        )
+    return output_describe_mg_helper
 
 
 def collect_noobaa_db_dump(log_dir_path):
