f8db469f77c9976a24fada798f567f2cc138af96
Oded Viner <oviner@redhat.com>
2022-06-07 17:15:04 +0300

    Change file name
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- tests/managed-service/test_acceptance.py
+++ tests/managed-service/test_acceptance.py
@@ -15,11 +15,9 @@ class TestAcceptanceMS(ManageTest):
 
     """
 
-    def test_acceptance_managed_service(
-        self, pvc_factory, pod_factory, teardown_factory
-    ):
+    def test_acceptance(self, pvc_factory, pod_factory, teardown_factory):
         """
-        Acceptance test Managed Service
+        Acceptance test
         1.Create pvc with all relevant modes
         2.Create new FIO pod for each pvc
         3.Run FIO with verify flag
@@ -53,12 +51,9 @@ class TestAcceptanceMS(ManageTest):
                 constants.VOLUME_MODE_BLOCK,
             ),
         ]
-        pod_objs = list()
-        pvc_objs = list()
+        self.pod_objs = list()
+        self.pvc_objs = list()
         for mode in modes:
-            logger.info(
-                f"Created new pvc sc_name={mode[0]} size=2Gi, access_mode={mode[1]}, volume_mode={mode[2]}"
-            )
             pvc_obj = pvc_factory(
                 interface=mode[0],
                 access_mode=mode[1],
@@ -66,7 +61,11 @@ class TestAcceptanceMS(ManageTest):
                 volume_mode=mode[2],
                 status=constants.STATUS_BOUND,
             )
-            pvc_objs.append(pvc_obj)
+            logger.info(
+                f"Created new pvc {pvc_obj.name}  sc_name={mode[0]} size=2Gi, "
+                f"access_mode={mode[1]}, volume_mode={mode[2]}"
+            )
+            self.pvc_objs.append(pvc_obj)
             if mode[2] == constants.VOLUME_MODE_BLOCK:
                 pod_dict_path = constants.CSI_RBD_RAW_BLOCK_POD_YAML
                 storage_type = constants.WORKLOAD_STORAGE_TYPE_BLOCK
@@ -75,6 +74,9 @@ class TestAcceptanceMS(ManageTest):
                 pod_dict_path = constants.NGINX_POD_YAML
                 storage_type = constants.WORKLOAD_STORAGE_TYPE_FS
                 raw_block_pv = False
+            logger.info(
+                f"Created new pod sc_name={mode[0]} size=2Gi, access_mode={mode[1]}, volume_mode={mode[2]}"
+            )
             pod_obj = pod_factory(
                 interface=mode[0],
                 pvc=pvc_obj,
@@ -87,9 +89,9 @@ class TestAcceptanceMS(ManageTest):
                 size="1GB",
                 verify=True,
             )
-            pod_objs.append(pod_obj)
+            self.pod_objs.append(pod_obj)
 
-        for pod_obj in pod_objs:
+        for pod_obj in self.pod_objs:
             fio_result = pod_obj.get_fio_results()
             logger.info("IOPs after FIO:")
             reads = fio_result.get("jobs")[0].get("read").get("iops")
@@ -97,7 +99,18 @@ class TestAcceptanceMS(ManageTest):
             logger.info(f"Read: {reads}")
             logger.info(f"Write: {writes}")
 
-        for pvc_obj in pvc_objs:
+        self.clone_pvc(teardown_factory=teardown_factory)
+        self.resize_pvc(pvc_size_new=3)
+
+    def clone_pvc(self, teardown_factory):
+        """
+        Clone PVC
+        """
+        for pvc_obj in self.pvc_objs:
+            logger.info(
+                f"Clone pvc {pvc_obj.name} sc_name={pvc_obj.storageclass.name} size=2Gi, "
+                f"access_mode={pvc_obj.access_mode},volume_mode={pvc_obj.get_pvc_vol_mode}"
+            )
             clone_yaml = (
                 constants.CSI_CEPHFS_PVC_CLONE_YAML
                 if pvc_obj.backed_sc == constants.CEPHFILESYSTEM_SC
@@ -116,15 +129,25 @@ class TestAcceptanceMS(ManageTest):
             helpers.wait_for_resource_state(cloned_pvc_obj, constants.STATUS_BOUND)
             cloned_pvc_obj.reload()
 
-        pvc_size_new = 3
-        for pvc_obj in pvc_objs:
-            logger.info(f"Expanding size of PVC {pvc_obj.name} to {pvc_size_new}G")
+    def resize_pvc(self, pvc_size_new):
+        """
+        Resize PVC
+
+        Args:
+            pvc_size_new (int): new pvc size
+
+        """
+        for pvc_obj in self.pvc_objs:
+            logger.info(
+                f"Resize pvc {pvc_obj.name} sc_name={pvc_obj.storageclass.name}, "
+                f"resize from {pvc_obj.size} to {pvc_size_new}, access_mode="
+                f"{pvc_obj.access_mode},volume_mode={pvc_obj.get_pvc_vol_mode}"
+            )
             pvc_obj.resize_pvc(new_size=pvc_size_new, verify=True)
 
         logger.info(f"Verified: Size of all PVCs are expanded to {pvc_size_new}G")
-
         logger.info("Verifying new size on pods.")
-        for pod_obj in pod_objs:
+        for pod_obj in self.pod_objs:
             if pod_obj.pvc.get_pvc_vol_mode == "Block":
                 logger.info(
                     f"Skipping check on pod {pod_obj.name} as volume " f"mode is Block."
