2793a1aeb1bc7d6ecc8532508318db0c6d17b87b
Avdhoot <asagare@redhat.com>
2022-07-08 18:33:03 +0530

    added all the changes to be suggested
    Signed-off-by: Avdhoot <asagare@redhat.com>

--- tests/manage/z_cluster/nodes/test_node_kernel_crash_ceph_fsync.py
+++ tests/manage/z_cluster/nodes/test_node_kernel_crash_ceph_fsync.py
@@ -4,7 +4,7 @@ import pytest
 
 from time import sleep
 from ocs_ci.ocs import constants, node
-from ocs_ci.framework.testlib import E2ETest, tier2, bugzilla, polarion_id
+from ocs_ci.framework.testlib import E2ETest, tier1, bugzilla, polarion_id
 from ocs_ci.ocs.exceptions import ResourceWrongStatusException
 from ocs_ci.ocs.node import get_worker_nodes
 from concurrent.futures import ThreadPoolExecutor
@@ -13,7 +13,7 @@ from ocs_ci.helpers import helpers
 log = logging.getLogger(__name__)
 
 
-@tier2
+@tier1
 @bugzilla("2075068")
 @polarion_id("OCS-3947")
 class TestKernelCrash(E2ETest):
@@ -21,20 +21,21 @@ class TestKernelCrash(E2ETest):
     Tests to verify kernel crash
     """
 
-    pvc_size = 1
     result_dir = constants.MOUNT_POINT + "/mydir"
+    END = 125
 
     def creates_files(self, pod_obj):
         while True:
             pod_obj.exec_sh_cmd_on_pod(
-                "for i in {1..125}; do dd if=/dev/zero of=%s/emp_$i.txt bs=1M count=1; done"
-                % self.result_dir
+                "for ((i=1;i<=%d;i++)); do dd if=/dev/zero of=%s/emp_$i.txt bs=1M count=1; done"
+                % (self.END, self.result_dir)
             )
 
     def remove_files(self, pod_obj):
         while True:
             pod_obj.exec_sh_cmd_on_pod(
-                "for i in {1..125}; rm %s/emp_$i.txt ; done" % self.result_dir
+                "for ((i=1;i<=%d;i++));rm -f %s/emp_$i.txt ; done"
+                % (self.END, self.result_dir)
             )
 
     @pytest.mark.parametrize(
@@ -54,8 +55,8 @@ class TestKernelCrash(E2ETest):
         """
         1. Create 1GiB PVC
         2. Attach PVC to an application pod
-        3. Copy files Files_create_delete.py and fsync.py to pod
-        4. Execute parallel Files_create_delete.py and fsync.py
+        3. Copy file fsync.py to pod
+        4. Execute create delete file operation parallely with fsync.py
         5. Check Node gets Panic or not
         """
 
@@ -76,7 +77,6 @@ class TestKernelCrash(E2ETest):
         pod_obj = pod_factory(
             interface=interface_type,
             pvc=pvc_obj,
-            status=constants.STATUS_RUNNING,
             pod_dict_path=constants.NGINX_POD_YAML,
         )
 
@@ -86,26 +86,31 @@ class TestKernelCrash(E2ETest):
         log.info("Files copied successfully ")
 
         commands = [
-            "mkdir" + " " + self.result_dir,
+            f"mkdir {self.result_dir}",
             "apt-get update",
         ]
         for cmd in commands:
-            pod_obj.exec_cmd_on_pod(command=f"{cmd}")
+            pod_obj.exec_cmd_on_pod(command=cmd)
         pod_obj.exec_sh_cmd_on_pod(command="apt-get install python -y")
         log.info("Starting creation and deletion of files on volume")
 
         # Create and delete files on mount point
-        executor = ThreadPoolExecutor(max_workers=5)
-        executor.submit(self.creates_files, pod_obj)
+        create_executor = ThreadPoolExecutor(max_workers=1)
+        create_executor.submit(self.creates_files, pod_obj)
         sleep(3)
+
         log.info("Started deletion of files on volume")
-        executor.submit(self.remove_files, pod_obj)
-        executor.submit(pod_obj.exec_sh_cmd_on_pod, command="python fsync.py")
+        delete_executor = ThreadPoolExecutor(max_workers=1)
+        delete_executor.submit(self.remove_files, pod_obj)
+
+        ThreadPoolExecutor(max_workers=1).submit(
+            pod_obj.exec_sh_cmd_on_pod, command="python fsync.py"
+        )
 
         # Check Node gets Panic or not
         try:
             node.wait_for_nodes_status(
-                selected_node, status=constants.NODE_NOT_READY, timeout=120
+                selected_node, status=constants.NODE_NOT_READY, timeout=60
             )
 
         except ResourceWrongStatusException:
