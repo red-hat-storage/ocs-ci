748e19b4b4fb9b3fdd2d96de6952e3e556e4e34f
Aviadp <apolak@redhat.com>
2022-07-19 19:19:34 +0300

    test ready - do not merge yet
    Signed-off-by: Aviadp <apolak@redhat.com>

--- tests/lvmo/test_lvm_alerts.py
+++ tests/lvmo/test_lvm_alerts.py
@@ -1,18 +1,17 @@
+import time
 import logging
-import json
+
 
 import pytest
 
-from pprint import pprint
 from ocs_ci.ocs import constants
 from ocs_ci.framework.testlib import skipif_ocs_version, ManageTest
-from ocs_ci.utility.utils import get_ocp_version
 from ocs_ci.ocs.cluster import LVM
 from ocs_ci.ocs.node import get_worker_nodes
-from ocs_ci.ocs.ocp import OCP
 from ocs_ci.utility.prometheus import PrometheusAPI
 
 log = logging.getLogger(__name__)
+new_prom = PrometheusAPI()
 
 topolvm_metrics = [
     "topolvm_thinpool_data_percent",
@@ -22,108 +21,71 @@ topolvm_metrics = [
     "topolvm_volumegroup_size_bytes",
 ]
 
-topolvm_alerts = []
+topolvm_alerts = {
+    "vg_data_75_precent": "VolumeGroupUsageAtThresholdNearFull",
+    "vg_data_85_precent": "VolumeGroupUsageAtThresholdCritical",
+    "tp_data_75_precent": "ThinPoolDataUsageAtThresholdNearFull",
+    "tp_data_85_precent": "ThinPoolDataUsageAtThresholdCritical",
+    "metadata_75_precent": "ThinPoolMetaDataUsageAtThresholdNearFull",
+    "metadata_85_precent": "ThinPoolMetaDataUsageAtThresholdCritical",
+}
 
 
 def get_thin_provisioning_alerts():
-    new_prom = PrometheusAPI()
+    """
+    Get the list of alerts that active in the cluster
+
+    Returns:
+        list: alrets name
+
+    """
     alert_full = new_prom.get("alerts")
     alerts_data = alert_full.json().get("data").get("alerts")
     alerts_names = list()
     for entity in alerts_data:
-        pprint(entity.get("labels").get("alertname"))
+        log.debug(entity.get("labels").get("alertname"))
         alerts_names.append(entity.get("labels").get("alertname"))
 
     return alerts_names
 
 
-def parse_topolvm_metrics(metric_name):
+def check_for_alert(alert_name):
     """
-    Returns the value of the specified metric name
+    Check to see if a given alert is available
 
     Args:
-        metric_name (string): metric name to be paesed
+        alert_name (str): Alert name
 
     Returns:
-        str: metric values represened as string
+    bool: True if alert is available else False
+
     """
+    if alert_name in get_thin_provisioning_alerts():
 
-    new_prom = PrometheusAPI()
-    metric_full = new_prom.query(metric_name)
-    metric_value = metric_full[0].get("value")[1]
+        return True
 
-    log.info(f"{metric_name} : {metric_value}")
-    return metric_value
+    return False
 
 
-# functions below to be moved to LVMCluster class after merge of PR#6163
-def parse_lvs_cmd(node_name):
-    """_summary_
+def parse_topolvm_metrics(metrics):
+    """
+    Returns the name and value of topolvm metrics
 
     Args:
-        node_name (str): name of the node to run 'lvs' command
+        metric_name (list): metrics name to be paesed
 
     Returns:
-        list: list contains dictionaries with each logical volue attributes
+        dict: topolvm metrics by: names: value
     """
+    metrics_short = dict()
 
-    oc_obj = OCP()
-    output = oc_obj.exec_oc_debug_cmd(
-        node_name, cmd_list=["sudo lvs --reportformat json"]
-    )
-    output_json = json.loads(output)
-    output_info = output_json.get("report")[0].get("lv")
-    log.debug(f"lvs json output: {output_info}")
-    log.info(f"total {len(output_info)} logical volume detected on node: {node_name}")
-    return output_info
+    for metric_name in metrics:
+        metric_full = new_prom.query(metric_name)
+        metric_value = metric_full[0].get("value")[1]
+        log.info(f"{metric_name} : {metric_value}")
+        metrics_short[metric_name] = metric_value
 
-
-def logical_volumes_data_by_name(lvs):
-    lvs_by_name = dict()
-    for lv in lvs:
-        lvs_by_name[lv.get("lv_name")] = lv
-        log.info(f"LVS_BY_NAME: {lvs_by_name}")
-
-    return lvs_by_name
-
-
-def get_logical_volume_names(lvs):
-    lvs_names = list()
-    for lv in lvs:
-        lvs_names.append(lv.get("lv_name"))
-
-    return lvs_names
-
-
-def get_lv_size(lv_name, lvs_info):
-    lv = logical_volumes_data_by_name(lvs_info)
-    size = lv.get(lv_name).get("lv_size")
-    char_to_replace = {"<": "", "g": ""}
-    return size.translate(str.maketrans(char_to_replace))
-
-
-def get_lv_data_precent(lv_name, lvs_info):
-    lv = logical_volumes_data_by_name(lvs_info)
-    return lv.get(lv_name).get("data_percent")
-
-
-def get_lv_vg_name(lv_name, lvs_info):
-    lv = logical_volumes_data_by_name(lvs_info)
-    return lv.get(lv_name).get("vg_name")
-
-
-def get_lv_metadata_precent(lv_name, lvs_info):
-    lv = logical_volumes_data_by_name(lvs_info)
-    return lv.get(lv_name).get("metadata_percent")
-
-
-def get_lv_thin_pool_name(lv_name, lvs_info):
-    lv = logical_volumes_data_by_name(lvs_info)
-    return lv.get(lv_name).get("pool_lv")
-
-
-########################################################################
-# End of part that need to be moved into cluster.py
+    return metrics_short
 
 
 class TestLvmCapacityAlerts(ManageTest):
@@ -132,12 +94,18 @@ class TestLvmCapacityAlerts(ManageTest):
 
     """
 
-    ocp_version = get_ocp_version()
     access_mode = constants.ACCESS_MODE_RWO
     volume_mode = constants.VOLUME_MODE_FILESYSTEM
     volume_binding_mode = constants.WFFC_VOLUMEBINDINGMODE
     node_name = get_worker_nodes()[0]
-    lvs_info = parse_lvs_cmd(node_name)
+
+    @pytest.fixture()
+    def init_lvm(self):
+        self.lvm = LVM()
+        disk1 = self.lvm.pv_data["pv_list"][0]
+        self.disk_size = self.lvm.pv_data[disk1]["pv_size"]
+        self.thin_pool_size = float(self.lvm.get_thin_pool1_size())
+        self.pvc_size = int(self.thin_pool_size)
 
     @pytest.fixture()
     def storageclass(
@@ -153,7 +121,7 @@ class TestLvmCapacityAlerts(ManageTest):
         self.proj = self.proj_obj.namespace
 
     @pytest.fixture()
-    def pvc(self, pvc_factory_class, lvs_info=lvs_info):
+    def pvc(self, pvc_factory_class):
         log.info("Fixture PVC called")
         volume_mode = self.volume_mode
         self.status = constants.STATUS_PENDING
@@ -161,7 +129,7 @@ class TestLvmCapacityAlerts(ManageTest):
             project=self.proj_obj,
             interface=None,
             storageclass=self.sc_obj,
-            size=int(float(get_lv_size("thin-pool-1", lvs_info))),
+            size=self.pvc_size,
             status=self.status,
             access_mode=self.access_mode,
             volume_mode=volume_mode,
@@ -173,56 +141,88 @@ class TestLvmCapacityAlerts(ManageTest):
         self.pod_obj = pod_factory_class(pvc=self.pvc_obj, raw_block_pv=self.block)
 
     @skipif_ocs_version("<4.10")
-    def test_thin_pool_capacity_alert(self, namespace, storageclass, pvc, pod):
-        # for metric_name in topolvm_metrics:
-        #     parse_topolvm_metrics(metric_name)
-        # get_thin_provisioning_alerts()
-        # node_name = "apolak-jon29"
-        # pprint(parse_lvs_cmd(node_name))
+    def test_thin_pool_capacity_alert(
+        self, namespace, init_lvm, storageclass, pvc, pod
+    ):
+        """
+
+        Test to verify thin pool capacity alert:
+        1. run io up to 70, check alerts - no alert expected
+        2. run io up to 76, check alerts
+        3. run io up to 86, check alerts - critical alert expected
+
+        """
         log.info("Test Started successfully")
-        lvm = LVM()
-        log.info(f"LVMCluster version is {lvm.get_lvm_version()}")
+        log.info(f"LVMCluster version is {self.lvm.get_lvm_version()}")
         log.info(
-            f"Lvm thin-pool overprovisionRation is {lvm.get_lvm_thin_pool_config_overprovision_ratio()}"
+            f"Lvm thin-pool overprovisionRation is {self.lvm.get_lvm_thin_pool_config_overprovision_ratio()}"
         )
         log.info(
-            f"Lvm thin-pool sizePrecent is {lvm.get_lvm_thin_pool_config_size_percent()}"
+            f"Lvm thin-pool sizePrecent is {self.lvm.get_lvm_thin_pool_config_size_percent()}"
         )
-        lvs_info = parse_lvs_cmd(self.node_name)
-        size_full = get_lv_size("thin-pool-1", lvs_info)
-        size_to_70 = f"{int(float(size_full)*0.7)}Gi"
-        size_to_76 = f"{int(float(size_full)*0.06)}Gi"
-        size_to_86 = f"{int(float(size_full)*0.1)}Gi"
+        size_to_70 = f"{int(float(self.thin_pool_size)*0.7)}Gi"
+        size_to_76 = f"{int(float(self.thin_pool_size)*0.06)}Gi"
+        size_to_86 = f"{int(float(self.thin_pool_size)*0.1)}Gi"
         sizes_list = [
-            {"size_to_fill": size_to_70, "file_name": "run-to-70"},
-            {"size_to_fill": size_to_76, "file_name": "run-to-76"},
-            {"size_to_fill": size_to_86, "file_name": "run-to-86"},
+            {
+                "size_to_fill": size_to_70,
+                "file_name": "run-to-70",
+                "pvc_expected_size": f"{float(self.pvc_size)*0.7}",
+                "alert": None,
+            },
+            {
+                "size_to_fill": size_to_76,
+                "file_name": "run-to-76",
+                "pvc_expected_size": f"{float(self.pvc_size)*0.76}",
+                "alert": topolvm_alerts.get("tp_data_75_precent"),
+            },
+            {
+                "size_to_fill": size_to_86,
+                "file_name": "run-to-86",
+                "pvc_expected_size": f"{float(self.pvc_size)*0.86}",
+                "alert": topolvm_alerts.get("tp_data_85_precent"),
+            },
         ]
 
-        log.info(f"LV Size:{size_full}")
+        log.info(f"LV Size:{self.thin_pool_size}")
+        self.metric_data = dict()
         for size in sizes_list:
-            log.info({size.get("size_to_fill")}, {size.get("file_name")})
+            log.info(
+                f"{size.get('size_to_fill')}, {size.get('file_name')}, {size.get('pvc_expected_size')}"
+            )
             self.pod_obj.run_io(
                 storage_type="fs",
                 size=size.get("size_to_fill"),
-                io_direction="w",
                 rw_ratio=0,
                 jobs=1,
-                runtime=1800,
+                runtime=0,
                 depth=4,
-                rate="250m",
-                rate_process="poisson",
+                rate="1250m",
+                rate_process=None,
                 fio_filename=size.get("file_name"),
-                bs="4M",
+                bs="100M",
                 end_fsync=0,
                 invalidate=None,
-                buffer_compress_percentage=None,
                 buffer_pattern=None,
-                readwrite=None,
+                readwrite="write",
                 direct=1,
                 verify=False,
-                fio_installed=False,
             )
-            log.info(self.pod_obj.get_fio_results(timeout=1800))
-            for metric in topolvm_metrics:
-                parse_topolvm_metrics(metric)
+            self.pod_obj.get_fio_results()
+            # This is a workaround for BZ-2108018
+            log.info("Wait 10 minutes for metrics refresh interval")
+            time.sleep(600)
+            # End of workaround
+            self.lvm.compare_percent_data_from_pvc(
+                self.pvc_obj, float(size["pvc_expected_size"])
+            )
+
+            val = parse_topolvm_metrics(topolvm_metrics)
+            self.metric_data = val
+            log.info(self.metric_data)
+            log.info(f"getting alerts: {get_thin_provisioning_alerts()}")
+            if size["file_name"] == "run-to-70":
+                assert not check_for_alert(size.get("alert")), "Alert already exists"
+            else:
+                log.info(f"size: {size['file_name']}")
+                assert check_for_alert(size.get("alert")), "Alert not found"
