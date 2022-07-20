0f970b21b13f04f91f11d4d7b65d0e5abf9a23fa
Elad Ben Aharon <ebenahar@redhat.com>
2022-07-20 13:11:03 +0300

    add rbd_mirror_die_after_seconds to rook ceph configmap
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -1642,6 +1642,20 @@ mon_data_avail_warn = 15
 osd_memory_target_cgroup_limit_ratio = 0.8
 """
 
+ROOK_CEPH_CONFIG_VALUES_411 = """
+[global]
+bdev_flock_retry = 20
+mon_osd_full_ratio = .85
+mon_osd_backfillfull_ratio = .8
+mon_osd_nearfull_ratio = .75
+mon_max_pg_per_osd = 600
+mon_pg_warn_max_object_skew = 0
+mon_data_avail_warn = 15
+rbd_mirror_die_after_seconds = 3600
+[osd]
+osd_memory_target_cgroup_limit_ratio = 0.8
+"""
+
 CEPH_DEBUG_CONFIG_VALUES = """
 [mon]
 debug_mon = 20
