fa535c1757a8e5b08d6a155e93efce79f167ebf2
Coady LaCroix <clacroix@redhat.com>
2022-07-26 15:27:40 -0700

    Add squad marker decorator values to junit item properties
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -185,8 +185,10 @@ def pytest_collection_modifyitems(session, items):
     # Add squad markers to each test item based on filepath
     for item in items:
         # check, if test already have squad marker manually assigned
-        if any(map(lambda x: "_squad" in x.name, item.iter_markers())):
-            continue
+        for marker in item.iter_markers():
+            if "_squad" in marker.name:
+                squad = marker.name.split("_")[0]
+                item.user_properties.append(("squad", squad))
         for squad, paths in constants.SQUADS.items():
             for _path in paths:
                 # Limit the test_path to the tests directory
