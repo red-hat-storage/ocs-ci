efd030c4925b72eeb209f9373f4eea9ada3b677f
Aviadp <apolak@redhat.com>
2022-08-02 17:23:49 +0300

    lowered sampler wait time
    Signed-off-by: Aviadp <apolak@redhat.com>

--- tests/lvmo/test_lvm_alerts.py
+++ tests/lvmo/test_lvm_alerts.py
@@ -191,7 +191,7 @@ class TestLvmCapacityAlerts(ManageTest):
 
             for sample in TimeoutSampler(
                 120,
-                30,
+                10,
                 self.lvm.validate_metrics_vs_operating_system_stats,
                 "topolvm_thinpool_data_percent",
                 self.lvm.get_thin_pool1_data_percent(),
