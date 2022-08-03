deb348018f48f181af841706a732d6ffe38c584f
Elena Bondarenko <ebondare@redhat.com>
2022-08-03 15:20:03 +0200

    Fix small issue
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/utility/rosa.py
+++ ocs_ci/utility/rosa.py
@@ -9,7 +9,7 @@ import logging
 import os
 import re
 
-from ocs_ci.framework import config, MultiClusterConfig
+from ocs_ci.framework import config
 from ocs_ci.ocs import constants, defaults, ocp
 from ocs_ci.ocs.exceptions import (
     ManagedServiceAddonDeploymentError,
@@ -609,11 +609,11 @@ def post_onboarding_verification():
     are in Ready status
     2. CephBlockPool and Subvolumegroup with the correct id are in Ready status
     """
+    logger.info("Starting post-onboarding verification")
     if config.multicluster:
         restore_ctx_index = config.cur_index
-
         consumer_ids = []
-        consumer_indexes = MultiClusterConfig.get_consumer_indexes_list()
+        consumer_indexes = config.get_consumer_indexes_list()
         for cluster_index in consumer_indexes:
             config.switch_ctx(cluster_index)
             clusterversion_yaml = ocp.OCP(
