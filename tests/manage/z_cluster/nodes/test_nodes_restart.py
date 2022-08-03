e6faa5f7f06989fc973614bc1ec2393e275095d5
Itzhak Kave <yitzhak.kave@gmail.com>
2022-08-03 11:02:22 +0300

    Use 'SanityExternalCluster' in case we use the external mode (#6257)
    Signed-off-by: yitzhak12 <ikave@redhat.com>

--- tests/manage/z_cluster/nodes/test_nodes_restart.py
+++ tests/manage/z_cluster/nodes/test_nodes_restart.py
@@ -17,8 +17,12 @@ from ocs_ci.framework.testlib import (
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.node import get_node_objs, get_nodes
 from ocs_ci.ocs.resources import pod
-from ocs_ci.helpers.sanity_helpers import Sanity
-from ocs_ci.helpers.helpers import wait_for_ct_pod_recovery, get_pv_names
+from ocs_ci.helpers.sanity_helpers import Sanity, SanityExternalCluster
+from ocs_ci.helpers.helpers import (
+    wait_for_ct_pod_recovery,
+    get_pv_names,
+    storagecluster_independent_check,
+)
 from ocs_ci.ocs.ocp import OCP
 
 
@@ -39,7 +43,10 @@ class TestNodesRestart(ManageTest):
         Initialize Sanity instance
 
         """
-        self.sanity_helpers = Sanity()
+        if storagecluster_independent_check():
+            self.sanity_helpers = SanityExternalCluster()
+        else:
+            self.sanity_helpers = Sanity()
 
     @pytest.fixture(autouse=True)
     def teardown(self, request, nodes):
