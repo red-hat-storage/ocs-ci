b1f0bc4d8c6c78055f6a8603411893585fdcc151
Jilju Joy <jijoy@redhat.com>
2022-08-08 15:29:24 +0530

    Unblock snapshot tests in MS if ODF version is 4.11 or above
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/helpers/helpers.py
+++ ocs_ci/helpers/helpers.py
@@ -2863,12 +2863,16 @@ def default_volumesnapshotclass(interface_type):
             if external
             else constants.DEFAULT_VOLUMESNAPSHOTCLASS_RBD
         )
+        if config.ENV_DATA["platform"].lower() in constants.MANAGED_SERVICE_PLATFORMS:
+            resource_name = constants.DEFAULT_VOLUMESNAPSHOTCLASS_RBD_MS
     elif interface_type == constants.CEPHFILESYSTEM:
         resource_name = (
             constants.DEFAULT_EXTERNAL_MODE_VOLUMESNAPSHOTCLASS_CEPHFS
             if external
             else constants.DEFAULT_VOLUMESNAPSHOTCLASS_CEPHFS
         )
+        if config.ENV_DATA["platform"].lower() in constants.MANAGED_SERVICE_PLATFORMS:
+            resource_name = constants.DEFAULT_VOLUMESNAPSHOTCLASS_CEPHFS_MS
     base_snapshot_class = OCP(
         kind=constants.VOLUMESNAPSHOTCLASS, resource_name=resource_name
     )
