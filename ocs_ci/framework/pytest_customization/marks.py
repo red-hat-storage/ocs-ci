5cf88e09f2dce5e8bdef1ac80ef4c619388c3926
yitzhak12 <ikave@redhat.com>
2022-08-08 15:18:22 +0300

    Fix the ms markers to check the platform of the default index
    Signed-off-by: yitzhak12 <ikave@redhat.com>

--- ocs_ci/framework/pytest_customization/marks.py
+++ ocs_ci/framework/pytest_customization/marks.py
@@ -215,7 +215,8 @@ managed_service_required = pytest.mark.skipif(
 
 ms_provider_required = pytest.mark.skipif(
     not (
-        config.ENV_DATA["platform"].lower() in MANAGED_SERVICE_PLATFORMS
+        config.default_cluster_ctx.ENV_DATA["platform"].lower()
+        in MANAGED_SERVICE_PLATFORMS
         and config.default_cluster_ctx.ENV_DATA["cluster_type"].lower() == "provider"
     ),
     reason="Test runs ONLY on managed service provider cluster",
@@ -223,7 +224,8 @@ ms_provider_required = pytest.mark.skipif(
 
 ms_consumer_required = pytest.mark.skipif(
     not (
-        config.ENV_DATA["platform"].lower() in MANAGED_SERVICE_PLATFORMS
+        config.default_cluster_ctx.ENV_DATA["platform"].lower()
+        in MANAGED_SERVICE_PLATFORMS
         and config.default_cluster_ctx.ENV_DATA["cluster_type"].lower() == "consumer"
     ),
     reason="Test runs ONLY on managed service consumer cluster",
@@ -274,13 +276,13 @@ skipif_openshift_dedicated = pytest.mark.skipif(
 )
 
 skipif_ms_provider = pytest.mark.skipif(
-    config.ENV_DATA["platform"].lower() in MANAGED_SERVICE_PLATFORMS
+    config.default_cluster_ctx.ENV_DATA["platform"].lower() in MANAGED_SERVICE_PLATFORMS
     and config.default_cluster_ctx.ENV_DATA["cluster_type"].lower() == "provider",
     reason="Test will not run on Managed service provider cluster",
 )
 
 skipif_ms_consumer = pytest.mark.skipif(
-    config.ENV_DATA["platform"].lower() in MANAGED_SERVICE_PLATFORMS
+    config.default_cluster_ctx.ENV_DATA["platform"].lower() in MANAGED_SERVICE_PLATFORMS
     and config.default_cluster_ctx.ENV_DATA["cluster_type"].lower() == "consumer",
     reason="Test will not run on Managed service consumer cluster",
 )
