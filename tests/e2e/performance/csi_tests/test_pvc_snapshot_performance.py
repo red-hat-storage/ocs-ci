b12cd3720b09462e9d064e93146d03ad8a27370e
Avi Layani <alayani@redhat.com>
2022-08-16 16:20:38 +0300

    fix the fio workload type to create the file
    Signed-off-by: Avi Layani <alayani@redhat.com>

--- tests/e2e/performance/csi_tests/test_pvc_snapshot_performance.py
+++ tests/e2e/performance/csi_tests/test_pvc_snapshot_performance.py
@@ -82,7 +82,7 @@ class TestPvcSnapshotPerformance(PASTest):
             )
             helpers.wait_for_resource_state(self.pod_object, constants.STATUS_RUNNING)
             self.pod_object.reload()
-            self.pod_object.workload_setup(self.interface, jobs=1, fio_installed=True)
+            self.pod_object.workload_setup("fs", jobs=1, fio_installed=True)
         except Exception as e:
             log.error(
                 f"Pod on PVC {self.pvc_obj.name} was not created, exception {str(e)}"
