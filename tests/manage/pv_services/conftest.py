31ef0e73b0d832969c71f3638a58d1952137105c
Jilju Joy <jijoy@redhat.com>
2022-08-17 16:18:39 +0530

    Unblock snapshot tests in MS
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/manage/pv_services/conftest.py
+++ tests/manage/pv_services/conftest.py
@@ -18,11 +18,23 @@ def pytest_collection_modifyitems(items):
     ocs_version = version.get_semantic_ocs_version_from_config()
 
     if config.ENV_DATA["platform"].lower() in constants.MANAGED_SERVICE_PLATFORMS:
+        skip_snapshot_tests = [
+            "manage/pv_services/pvc_snapshot/test_pvc_snapshot_when_snapshotclass_deleted.py",
+            "manage/pv_services/pvc_snapshot/test_restore_snapshot_using_different_sc.py",
+            "manage/pv_services/pvc_snapshot/test_verify_rbd_trash_purge.py",
+        ]
         for item in items.copy():
-            if (
-                "manage/pv_services/pvc_snapshot" in str(item.fspath)
-                and ocs_version < version.VERSION_4_11
-            ):
+            if "manage/pv_services/pvc_snapshot" in str(item.fspath):
+                # Remove all tests in "manage/pv_services/pvc_snapshot/" if OCS version is below 4.11
+                # If OCS version is 4.11 or above, remove only the tests given in the list 'skip_snapshot_tests'
+                if (
+                    all(
+                        testpath not in str(item.fspath)
+                        for testpath in skip_snapshot_tests
+                    )
+                    and ocs_version >= version.VERSION_4_11
+                ):
+                    continue
                 log.debug(
                     f"Test {item} is removed from the collected items. PVC snapshot is not supported on"
                     f" {config.ENV_DATA['platform'].lower()} due to the bug 2069367"
