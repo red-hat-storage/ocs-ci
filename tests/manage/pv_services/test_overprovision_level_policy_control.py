6e3dfa2b17dfab3c3549fae2ad6bf8c1d0ffd039
oviner <oviner@redhat.com>
2022-09-15 11:53:22 +0300

    fix code
    Signed-off-by: oviner <oviner@redhat.com>

--- tests/manage/pv_services/test_overprovision_level_policy_control.py
+++ tests/manage/pv_services/test_overprovision_level_policy_control.py
@@ -12,8 +12,8 @@ from ocs_ci.framework.testlib import (
     ManageTest,
     tier1,
     bugzilla,
-    skipif_managed_service,
     skipif_ocs_version,
+    skipif_managed_service,
 )
 
 log = logging.getLogger(__name__)
@@ -39,7 +39,6 @@ def setup_sc(storageclass_factory_class):
 @bugzilla("2024545")
 @pytest.mark.polarion_id("OCS-4472")
 @skipif_managed_service
-@skipif_ocs_version("<4.10")
 class TestOverProvisionLevelPolicyControl(ManageTest):
     """
     Test OverProvision Level Policy Control
