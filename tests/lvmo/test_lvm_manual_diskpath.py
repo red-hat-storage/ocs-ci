4291a8aee2ee46ff53704f0a35dc5cfe521268d6
Aviadp <apolak@redhat.com>
2022-09-19 21:19:35 +0300

    added docstring, added dynamic number of disks selection
    Signed-off-by: Aviadp <apolak@redhat.com>

--- tests/lvmo/test_lvm_manual_diskpath.py
+++ tests/lvmo/test_lvm_manual_diskpath.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 from ocs_ci.ocs import constants
-from ocs_ci.framework import config
 from ocs_ci.utility.templating import load_yaml, dump_data_to_temp_yaml
 from ocs_ci.utility.lvmo_utils import (
     delete_lvm_cluster,
@@ -22,14 +21,14 @@ LVMCLUSTER_TEMPLATE = os.path.join(
     constants.TEMPLATE_DEPLOYMENT_DIR_LVMO, "lvm-cluster-default.yaml"
 )
 
-storage_disks_count = config.DEPLOYMENT.get("lvmo_disks")
-
 
 def create_lvm_cluster_cr_with_device_selector(disks):
-    """_summary_
+    """
+    Creates LVMCluster configuration file with deviceSelector and disks list
 
     Args:
-        disks (list): _description_
+        disks (list): list of disks (str) that selected for lvm-cluster
+
     """
     lvm_cr_dict = load_yaml(LVMCLUSTER_TEMPLATE)
     lvm_cr_dict["spec"]["storage"]["deviceClasses"][0]["deviceSelector"] = {
@@ -44,13 +43,19 @@ def create_lvm_cluster_cr_with_device_selector(disks):
 @skipif_lvm_not_installed
 @skipif_ocs_version("<4.12")
 def test_create_lvm_cluster_w_manual_disk_selection(by="name"):
+    """
+    Test creation of lvm cluster with manual disk selection,
+    by disks name or by disks path
 
+    """
     if by == "name":
         disks = get_blockdevices()
     elif by == "path":
         disks = get_disks_by_path()
 
-    lvm_cr = create_lvm_cluster_cr_with_device_selector(disks[:3])
+    disks_to_use = int(len(disks) / 2) + 1
+    log.info(f"Creating cluster with {disks_to_use} disks")
+    lvm_cr = create_lvm_cluster_cr_with_device_selector(disks[:disks_to_use])
     delete_lvm_cluster()
     oc_obj = OCP()
     oc_obj.create(yaml_file=lvm_cr)
