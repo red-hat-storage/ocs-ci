3642ad65c86961ff361a85687b1f9033456f1e16
am-agrawa <amagrawa@redhat.com>
2022-09-19 16:20:39 +0530

    fix pr_check failure caused by black formatting
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -262,8 +262,8 @@ DEFAULT_VOLUMESNAPSHOTCLASS_RBD_MS = f"{DEFAULT_CLUSTERNAME}-ceph-rbd"
 
 # encoded value of 'admin'
 ADMIN_USER = "admin"
-GB = 1024 ** 3
-GB2KB = 1024 ** 2
+GB = 1024**3
+GB2KB = 1024**2
 GB2MB = 1024
 
 # Reclaim Policy
@@ -879,7 +879,7 @@ SNO_BOOTSTRAP_IGN = "bootstrap-in-place-for-live-iso.ign"
 TERRAFORM_IGNITION_PROVIDER_VERSION = "v2.1.0"
 
 # Minimum storage needed for vSphere Datastore in bytes.
-MIN_STORAGE_FOR_DATASTORE = 1.1 * 1024 ** 4
+MIN_STORAGE_FOR_DATASTORE = 1.1 * 1024**4
 
 # vSphere related constants
 VSPHERE_NODE_USER = "core"
@@ -1386,9 +1386,9 @@ AUTH_CONFIG_DOCS = (
 TP_CONVERSION = {" B/s": 0.000000976562, " KiB/s": 0.000976562, " MiB/s": 1}
 
 BYTES_IN_KB = 1024
-BYTES_IN_MB = 1024 ** 2
-BYTES_IN_GB = 1024 ** 3
-BYTES_IN_TB = 1024 ** 4
+BYTES_IN_MB = 1024**2
+BYTES_IN_GB = 1024**3
+BYTES_IN_TB = 1024**4
 
 
 # LSO
@@ -1481,7 +1481,7 @@ MAX_OSDS = 18
 
 # Minimum cluster requirements in term of node specs
 MIN_NODE_CPU = 16
-MIN_NODE_MEMORY = 64 * 10 ** 9
+MIN_NODE_MEMORY = 64 * 10**9
 
 # aws tags
 AWS_CLOUDFORMATION_TAG = "aws:cloudformation:stack-name"
