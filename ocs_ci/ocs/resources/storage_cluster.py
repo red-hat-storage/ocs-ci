e2beeef09b29b3ddd9420d7c96aca0e9336ba104
Jilju Joy <jijoy@redhat.com>
2022-09-28 19:59:28 +0530

    Verify topology in MS
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -1355,9 +1355,13 @@ def verify_provider_topology():
     1. Verify replica count
     2. Verify total size
     3. Verify OSD size
-    4. Verify worker node instance count
+    4. Verify OSD running worker nodes count
     5. Verify worker node instance type
     6. Verify OSD count
+    7. Verify OSD cpu
+    8. Verify machine pools
+    9. Verify OSD running nodes are part of the correct machinepool
+    10. Verify that other pods are not running on OSD nodes
 
     """
     size = f"{config.ENV_DATA.get('size', 4)}Ti"
@@ -1368,26 +1372,10 @@ def verify_provider_topology():
         "4Ti": {"total_size": 12, "osd_count": 3, "instance_count": 3},
         "8Ti": {"total_size": 24, "osd_count": 6, "instance_count": 3},
         "12Ti": {"total_size": 36, "osd_count": 9, "instance_count": 6},
-        "16Ti": {
-            "total_size": 48,
-            "osd_count": 12,
-            "instance_count": 6,
-        },
-        "20Ti": {
-            "total_size": 60,
-            "osd_count": 15,
-            "instance_count": 9,
-        },
-        "48Ti": {
-            "total_size": 144,
-            "osd_count": 36,
-            "instance_count": 18,
-        },
-        "96Ti": {
-            "total_size": 288,
-            "osd_count": 72,
-            "instance_count": 36,
-        },
+        "16Ti": {"total_size": 48, "osd_count": 12, "instance_count": 6},
+        "20Ti": {"total_size": 60, "osd_count": 15, "instance_count": 9},
+        "48Ti": {"total_size": 144, "osd_count": 36, "instance_count": 18},
+        "96Ti": {"total_size": 288, "osd_count": 72, "instance_count": 36},
     }
     cluster_namespace = constants.OPENSHIFT_STORAGE_NAMESPACE
     storage_cluster = StorageCluster(
@@ -1400,6 +1388,7 @@ def verify_provider_topology():
         int(storage_cluster.data["spec"]["storageDeviceSets"][0]["replica"])
         == replica_count
     ), "Replica count is not as expected"
+    log.info("Verified replica count")
 
     # Verify total size
     ct_pod = get_ceph_tools_pod()
@@ -1411,6 +1400,7 @@ def verify_provider_topology():
     assert (
         total_size == size_map[size]["total_size"]
     ), f"Total size {total_size}Ti is not matching the expected total size {size_map[size]['total_size']}Ti"
+    log.info(f"Verified that the total size is {size_map[size]['total_size']}Ti")
 
     # Verify OSD size
     osd_pvc_objs = get_all_pvc_objs(
@@ -1420,6 +1410,7 @@ def verify_provider_topology():
         assert (
             pvc_obj.get()["status"]["capacity"]["storage"] == f"{osd_size}Ti"
         ), f"Size of OSD PVC {pvc_obj.name} is not {osd_size}Ti"
+    log.info(f"Verified that the size of each OSD is {osd_size}Ti")
 
     # Verify OSD running worker nodes count
     osd_nodes = get_osd_running_nodes()
@@ -1427,6 +1418,7 @@ def verify_provider_topology():
         f"Worker node instance count is not as expected. Actual instance count is {len(osd_nodes)}. "
         f"Expected {size_map[size]['instance_count']}. List of worker nodes : {osd_nodes}"
     )
+    log.info("Verified the number of worker nodes where OSD is running.")
 
     # Verify worker node instance type
     worker_node_names = get_worker_nodes()
@@ -1439,12 +1431,14 @@ def verify_provider_topology():
             .get("beta.kubernetes.io/instance-type")
             == instance_type
         ), f"Instance type of the worker node {node_obj.name} is not {instance_type}"
+    log.info(f"Verified that the instance type of wokeer nodes is {instance_type}")
 
     # Verify OSD count
     osd_count = get_osd_count()
     assert (
         osd_count == size_map[size]["osd_count"]
-    ), f"OSD count is not as expected. Actual:{osd_count}. Expected:{size_map[size]['osd_count']} "
+    ), f"OSD count is not as expected. Actual:{osd_count}. Expected:{size_map[size]['osd_count']}"
+    log.info(f"Verified that the OSD count is {size_map[size]['osd_count']}")
 
     # Verify OSD cpu
     osd_cpu_limit = 2
@@ -1460,6 +1454,7 @@ def verify_provider_topology():
                 assert (
                     container["resources"]["requests"]["cpu"] == osd_cpu_request
                 ), f"OSD pod {osd_pod.name} container osd doesn't have cpu request {osd_cpu_request}"
+    log.info(f"Verified OSD CPU")
 
     # Verify machine pools
     cmd = f"rosa list machinepool --cluster={config.ENV_DATA['cluster_name']} -o yaml"
@@ -1468,6 +1463,7 @@ def verify_provider_topology():
     assert (
         len(machine_pool_info_list) == 2
     ), f"Number of machinepools is not 2. Machinepools details: {machine_pool_info_list}"
+
     machine_pool_ids = []
     ceph_osd_nodepool_info = None
     for machine_pool_info in machine_pool_info_list:
@@ -1478,6 +1474,7 @@ def verify_provider_topology():
         if "node.ocs.openshift.io/osd" in machine_pool_info.get("labels", {}):
             ceph_osd_nodepool_info = machine_pool_info
     log.info(f"Machinepool IDs: {machine_pool_ids}")
+
     assert ceph_osd_nodepool_info, "OSD node pool not found"
     log.info(f"OSD node pool machinepool id is {ceph_osd_nodepool_info['id']}")
     assert ceph_osd_nodepool_info["replicas"] == size_map[size]["instance_count"], (
@@ -1490,8 +1487,9 @@ def verify_provider_topology():
         f"Verification of taints failed for machinepool {ceph_osd_nodepool_info['id']}. "
         f"Machinepool info: {ceph_osd_nodepool_info}"
     )
+    log.info(f"Verified taints on machinepool {ceph_osd_nodepool_info['id']}")
 
-    # Verify OSD running nodes
+    # Verify OSD running nodes are part of the correct machinepool
     osd_node_objs = get_node_objs(osd_nodes)
     for node_obj in osd_node_objs:
         annotation = (
@@ -1502,7 +1500,10 @@ def verify_provider_topology():
         )
         assert (
             ceph_osd_nodepool_info["id"] in annotation
-        ), f"OSD running node part of the machinepool {ceph_osd_nodepool_info['id']}"
+        ), f"OSD running node {node_obj.name} is part of the machinepool {ceph_osd_nodepool_info['id']}"
+    log.info(
+        f"OSD running nodes are part of the machinepool {ceph_osd_nodepool_info['id']}"
+    )
 
     # Verify that other pods are not running on OSD nodes
     for node_obj in osd_node_objs:
@@ -1511,6 +1512,7 @@ def verify_provider_topology():
             assert pod_name.startswith(
                 ("rook-ceph-osd", "rook-ceph-crashcollector")
             ), f"Pod {pod_name} is running on OSD running node {node_obj.name}"
+    log.info("Verified that other pods are not running on OSD nodes")
 
 
 def verify_provider_resources():
