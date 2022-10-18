a73f64e72b040cc8145229522564c914ea8845b4
Jilju Joy <jijoy@redhat.com>
2022-10-18 20:10:15 +0530

    Update OSD expected state in test check_automated_recovery_from_drain_node
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/manage/z_cluster/nodes/test_automated_recovery_from_failed_nodes_reactive_ms.py
+++ tests/manage/z_cluster/nodes/test_automated_recovery_from_failed_nodes_reactive_ms.py
@@ -11,6 +11,7 @@ from ocs_ci.framework.testlib import (
 
 from ocs_ci.framework import config
 from ocs_ci.ocs import machine, constants
+from ocs_ci.ocs.machine import get_labeled_nodes
 from ocs_ci.ocs.resources.pod import (
     wait_for_pods_to_be_in_statuses,
     check_pods_after_node_replacement,
@@ -179,8 +180,11 @@ def check_automated_recovery_from_drain_node(nodes):
     new_osd_pods = wait_for_osd_pods_having_ids(osd_ids=old_osd_pod_ids)
     new_osd_pod_names = [p.name for p in new_osd_pods]
 
+    osd_labelled_nodes = set(get_labeled_nodes(constants.OSD_NODE_LABEL))
+    osd_running_nodes = set(get_osd_running_nodes())
+
     wnodes = get_worker_nodes()
-    if len(wnodes) <= 3:
+    if len(wnodes) <= 3 or osd_labelled_nodes == osd_running_nodes:
         expected_pods_status = constants.STATUS_PENDING
     else:
         expected_pods_status = constants.STATUS_RUNNING
@@ -199,7 +203,7 @@ def check_automated_recovery_from_drain_node(nodes):
     schedule_nodes([osd_node_name])
     log.info(f"Successfully scheduled the node {osd_node_name}")
 
-    if len(wnodes) <= 3:
+    if len(wnodes) <= 3 or osd_labelled_nodes == osd_running_nodes:
         assert wait_for_osd_ids_come_up_on_node(osd_node_name, old_osd_pod_ids)
         log.info(
             f"the osd ids {old_osd_pod_ids} Successfully come up on the node {osd_node_name}"
