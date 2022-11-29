5ab380b4de3eed6c2083f2db3932ce60295e0c63
Parikshith Byregowda <pbyregow@redhat.com>
2022-11-29 17:54:48 +0530

    conflict
    Signed-off-by: Parikshith Byregowda <pbyregow@redhat.com>

--- tests/e2e/kcs/test_noobaadb_pw_reset.py
+++ tests/e2e/kcs/test_noobaadb_pw_reset.py
@@ -1,4 +1,5 @@
 import logging
+from time import sleep
 
 import pytest
 
@@ -8,18 +9,18 @@ from ocs_ci.framework.testlib import (
     skipif_managed_service,
 )
 from ocs_ci.helpers.helpers import (
-    modify_deployment_replica_count,
-    modify_statefulset_replica_count,
+    wait_for_resource_state,
+    scale_nb_resources,
 )
 
 from ocs_ci.ocs import constants, defaults, ocp
-
+from ocs_ci.ocs.resources.pod import get_noobaa_pods
 
 logger = logging.getLogger(__name__)
 
 
 @tier3
-@pytest.mark.polarion_id("")
+@pytest.mark.polarion_id("OCS-4662")
 @skipif_managed_service
 class TestNoobaaDbPw(E2ETest):
     """
@@ -35,35 +36,38 @@ class TestNoobaaDbPw(E2ETest):
         """
 
         def finalizer():
-            # TODO: Add condition
+            # Scaling up in case the test fails in between,
+            # does not matter if the replica is already 1
             scale_nb_resources(replica=1)
 
         request.addfinalizer(finalizer)
 
     def test_noobaadb_password_reset(self):
-        """"""
+        """
+        Verifies https://access.redhat.com/solutions/6648191
 
+        """
+        logger.info("Scaling down noobaa resources")
         scale_nb_resources(replica=0)
+        sleep(15)
 
-        cmd = "ALTER USER noobaa WITH PASSWORD 'myNewPassword';"
-        ocp.OCP().exec_oc_cmd(f"exec -it noobaa-db-pg-0 -- psql -d nbcore -c {cmd}")
-        sc_param = '{"stringData":{"password": myNewPassword}}'
-        nb_s3_route_obj = ocp.OCP(
+        alter_cmd = "ALTER USER noobaa WITH PASSWORD 'myNewPassword';"
+        ocp.OCP().exec_oc_cmd(
+            f"exec -it {constants.NB_DB_NAME_47_AND_ABOVE} -- psql -d nbcore -c {alter_cmd}"
+        )
+
+        nb_db_secret_obj = ocp.OCP(
             kind=constants.SECRET,
             namespace=defaults.ROOK_CLUSTER_NAMESPACE,
             resource_name="noobaa-db",
         )
-        nb_s3_route_obj.patch(params=sc_param, format_type="merge")
+        secret_param = '{"stringData":{"password": myNewPassword}}'
+        nb_db_secret_obj.patch(params=secret_param, format_type="merge")
+
+        logger.info("Scaling up Noobaa resources")
         scale_nb_resources(replica=1)
-
-
-def scale_nb_resources(replica=1):
-    modify_deployment_replica_count(
-        deployment_name="noobaa-operator", replica_count=replica
-    )
-    modify_deployment_replica_count(
-        deployment_name="noobaa-endpoint", replica_count=replica
-    )
-    modify_statefulset_replica_count(
-        statefulset_name="noobaa-core", replica_count=replica
-    )
+        sleep(20)
+        for noobaa_pod in get_noobaa_pods():
+            wait_for_resource_state(
+                resource=noobaa_pod, state=constants.STATUS_RUNNING, timeout=600
+            )
