313e5d0aee162f35a5b37b6323d1688990663b78
Sagi Hirshfeld <shirshfe@redhat.com>
2022-12-28 14:42:33 +0200

    Condition the search of the previous logs
    Signed-off-by: Sagi Hirshfeld <shirshfe@redhat.com>

--- tests/manage/mcg/test_kms.py
+++ tests/manage/mcg/test_kms.py
@@ -22,28 +22,32 @@ class TestNoobaaKMS(MCGTest):
         """
         Validate from logs that there is successfully used NooBaa with KMS integration.
         """
-        # Collect the current logs from the noobaa-operator pod
+        # Define which records to look for based on the version
+        target_records = []
+        if version.get_semantic_ocs_version_from_config() < version.VERSION_4_10:
+            target_records.append("found root secret in external KMS successfully")
+        else:
+            target_records.append("setKMSConditionStatus Init")
+            target_records.append("setKMSConditionStatus Sync")
+
+        # Get the noobaa-operator pod and it's relevant metadata
         operator_pod = pod.get_pods_having_label(
             label=constants.NOOBAA_OPERATOR_POD_LABEL,
             namespace=defaults.ROOK_CLUSTER_NAMESPACE,
         )[0]
         operator_pod_name = operator_pod["metadata"]["name"]
-        operator_logs = pod.get_pod_logs(pod_name=operator_pod_name)
-
-        # If the pod restarted, collect the previous logs as well
         restart_count = operator_pod["status"]["containerStatuses"][0]["restartCount"]
-        if restart_count > 0:
-            previous_logs = pod.get_pod_logs(pod_name=operator_pod_name, previous=True)
-            operator_logs = "".join(
-                (
-                    operator_logs,
-                    previous_logs,
-                )
-            )
 
-        # Check whether the logs contain a positive record of the integration
-        if version.get_semantic_ocs_version_from_config() < version.VERSION_4_10:
-            assert "found root secret in external KMS successfully" in operator_logs
-        else:
-            assert "setKMSConditionStatus Init" in operator_logs
-            assert "setKMSConditionStatus Sync" in operator_logs
+        # Look for the target records in the logs of the pod
+        targets_found = False
+        operator_logs = pod.get_pod_logs(pod_name=operator_pod_name)
+        targets_found = all(target in operator_logs for target in target_records)
+
+        # Also check the logs before the last pod restart
+        if not targets_found and restart_count > 0:
+            operator_logs = pod.get_pod_logs(pod_name=operator_pod_name, previous=True)
+            targets_found = all(target in operator_logs for target in target_records)
+
+        assert (
+            targets_found
+        ), "No records were found of the integration of NooBaa and KMS"
