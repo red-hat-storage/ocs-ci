2480748448af4d2690e816cea011d916d7b60e90
Jilju Joy <jijoy@redhat.com>
2022-12-30 12:21:40 +0530

    addres comments
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -8,7 +8,6 @@ import tempfile
 import yaml
 
 from jsonschema import validate
-from semantic_version import Version
 
 from ocs_ci.framework import config
 from ocs_ci.helpers.managed_services import (
@@ -1339,7 +1338,7 @@ def verify_managed_service_resources():
     if config.ENV_DATA["cluster_type"].lower() == "provider":
         verify_provider_storagecluster(sc_data)
         verify_provider_resources()
-        if get_ocs_osd_deployer_version() >= Version("2.0.11"):
+        if get_ocs_osd_deployer_version() >= get_semantic_version("2.0.11"):
             verify_provider_topology()
     else:
         verify_consumer_storagecluster(sc_data)
