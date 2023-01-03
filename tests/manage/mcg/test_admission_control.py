48d3fcd970c9aa408d4d250198a63cfb55072d39
Sagi Hirshfeld <shirshfe@redhat.com>
2023-01-03 08:32:21 +0200

    4.10 Cherry-pick
    Signed-off-by: Sagi Hirshfeld <shirshfe@redhat.com>

--- tests/manage/mcg/test_admission_control.py
+++ tests/manage/mcg/test_admission_control.py
@@ -157,7 +157,7 @@ class TestAdmissionWebhooks(MCGTest):
                             "secret": {"name": ""},
                         },
                     },
-                    "please provide a valid ARN or secret name",
+                    "secret name",
                 ],
                 marks=[tier3, polarion_id("OCS-2786")],
             ),
