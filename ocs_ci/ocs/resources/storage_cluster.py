a0cec78a50ca7c8b9c0561fe2e2b579e30ad07d1
Petr Balogh <pbalogh@redhat.com>
2023-01-25 13:10:24 +0100

    Add retry for OSD schema validation in post upgrade
    Fixes: #6917
    
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>
    Signed-off-by: suchita-g <sgatfane@redhat.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -8,7 +8,7 @@ import tempfile
 import yaml
 
 from jsonschema import validate
-
+from jsonschema.exceptions import ValidationError
 from ocs_ci.framework import config
 
 from ocs_ci.helpers.managed_services import verify_provider_topology
@@ -82,6 +82,40 @@ class StorageCluster(OCP):
         super(StorageCluster, self).__init__(
             resource_name=resource_name, kind="StorageCluster", *args, **kwargs
         )
+
+
+def verify_osd_tree_schema(ct_pod, deviceset_pvcs):
+    """
+    Verify Ceph OSD tree schema
+
+    Args:
+        ct_pod (:obj:`OCP`):  Object of the Ceph tools pod
+        deviceset_pvcs (list): List of strings of deviceset PVC names
+
+    """
+    _deviceset_pvcs = copy.deepcopy(deviceset_pvcs)
+    osd_tree = ct_pod.exec_ceph_cmd(ceph_cmd="ceph osd tree", format="json")
+    schemas = {
+        "root": constants.OSD_TREE_ROOT,
+        "rack": constants.OSD_TREE_RACK,
+        "host": constants.OSD_TREE_HOST,
+        "osd": constants.OSD_TREE_OSD,
+        "region": constants.OSD_TREE_REGION,
+        "zone": constants.OSD_TREE_ZONE,
+    }
+    schemas["host"]["properties"]["name"] = {"enum": _deviceset_pvcs}
+    for item in osd_tree["nodes"]:
+        validate(instance=item, schema=schemas[item["type"]])
+        if item["type"] == "host":
+            _deviceset_pvcs.remove(item["name"])
+    assert not _deviceset_pvcs, (
+        f"These device set PVCs are not given in ceph osd tree output "
+        f"- {_deviceset_pvcs}"
+    )
+    log.info(
+        "Verified ceph osd tree output. Device set PVC names are given in the "
+        "output."
+    )
 
 
 def ocs_install_verification(
@@ -424,29 +458,12 @@ def ocs_install_verification(
                 ]
         else:
             deviceset_pvcs = [pvc.name for pvc in get_deviceset_pvcs()]
-
-        osd_tree = ct_pod.exec_ceph_cmd(ceph_cmd="ceph osd tree", format="json")
-        schemas = {
-            "root": constants.OSD_TREE_ROOT,
-            "rack": constants.OSD_TREE_RACK,
-            "host": constants.OSD_TREE_HOST,
-            "osd": constants.OSD_TREE_OSD,
-            "region": constants.OSD_TREE_REGION,
-            "zone": constants.OSD_TREE_ZONE,
-        }
-        schemas["host"]["properties"]["name"] = {"enum": deviceset_pvcs}
-        for item in osd_tree["nodes"]:
-            validate(instance=item, schema=schemas[item["type"]])
-            if item["type"] == "host":
-                deviceset_pvcs.remove(item["name"])
-        assert not deviceset_pvcs, (
-            f"These device set PVCs are not given in ceph osd tree output "
-            f"- {deviceset_pvcs}"
+    if post_upgrade_verification:
+        retry((ValidationError), tries=3, delay=60)(verify_osd_tree_schema)(
+            ct_pod, deviceset_pvcs
         )
-        log.info(
-            "Verified ceph osd tree output. Device set PVC names are given in the "
-            "output."
-        )
+    else:
+        verify_osd_tree_schema(ct_pod, deviceset_pvcs)
 
     # TODO: Verify ceph osd tree output have osd listed as ssd
     # TODO: Verify ceph osd tree output have zone or rack based on AZ
