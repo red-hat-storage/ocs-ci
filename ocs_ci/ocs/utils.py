19cd77adc5a54ee4a2187d391502cd86d29396f6
OdedViner <oviner@redhat.com>
2023-02-10 16:35:04 +0200

    WIP: Test mg running time
    Signed-off-by: OdedViner <oviner@redhat.com>

--- ocs_ci/ocs/utils.py
+++ ocs_ci/ocs/utils.py
@@ -1043,18 +1043,9 @@ def collect_ocs_logs(dir_name, ocp=True, ocs=True, mcg=False, status_failure=Tru
         )
 
     if ocs:
-        latest_tag = ocsci_config.REPORTING.get(
-            "ocs_must_gather_latest_tag",
-            ocsci_config.REPORTING.get(
-                "default_ocs_must_gather_latest_tag",
-                ocsci_config.DEPLOYMENT["default_latest_tag"],
-            ),
-        )
+        latest_tag = "test-time"
         ocs_log_dir_path = os.path.join(log_dir_path, "ocs_must_gather")
-        ocs_must_gather_image = ocsci_config.REPORTING.get(
-            "ocs_must_gather_image",
-            ocsci_config.REPORTING["default_ocs_must_gather_image"],
-        )
+        ocs_must_gather_image = "quay.io/ocsci/mg-image"
         ocs_must_gather_image_and_tag = f"{ocs_must_gather_image}:{latest_tag}"
         if ocsci_config.DEPLOYMENT.get("disconnected"):
             ocs_must_gather_image_and_tag = mirror_image(ocs_must_gather_image_and_tag)
