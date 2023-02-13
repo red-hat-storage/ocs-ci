a167415033ddb72a3e62d61bf1db9980f41ec5f1
Itzhak Kave <yitzhak.kave@gmail.com>
2023-02-13 08:35:27 +0200

    Create a new decorator 'switch_to_default_cluster_index_at_last', 'Switch to the default cluster index in the 'create_scale_pods_and_pvcs_using_kube_job_on_ms_consumers' fixture finalizer (#7019)
    Signed-off-by: yitzhak12 <ikave@redhat.com>
    Co-authored-by: yitzhak12 <ikave@redhat.com>

--- ocs_ci/utility/decorators.py
+++ ocs_ci/utility/decorators.py
@@ -25,3 +25,26 @@ def switch_to_orig_index_at_last(func):
                 config.switch_ctx(orig_index)
 
     return inner
+
+
+def switch_to_default_cluster_index_at_last(func):
+    """
+    A decorator for switching to the default cluster index after the function execution.
+    This decorator will primarily be used in the 'teardown' and 'finalizer' methods when we want to make sure
+    that the next test will start with the default cluster index.
+
+    Args:
+        func (function): The function we want to decorate
+
+    """
+
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        finally:
+            default_cluster_index = config.ENV_DATA["default_cluster_context_index"]
+            if config.cur_index != default_cluster_index:
+                logger.info("Switching back to the default cluster index")
+                config.switch_ctx(default_cluster_index)
+
+    return inner
