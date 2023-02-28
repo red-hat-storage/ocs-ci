cf66393aec6bdfd8cf9d4304370ca8c2bfd7e8ce
OdedViner <oviner@redhat.com>
2023-02-28 12:49:21 +0200

    fix doc
    Signed-off-by: OdedViner <oviner@redhat.com>

--- Docker_files/ocsci_container/scripts/install-ocs-ci.sh
+++ Docker_files/ocsci_container/scripts/install-ocs-ci.sh
@@ -10,23 +10,19 @@ git clone "$OCSCI_REPO_URL" "$OCSCI_INSTALL_DIR"
 pushd "$OCSCI_INSTALL_DIR"
 
 # Checkout to relevant branch [the default is stable]
-if [ "$BRANCH_ID" == "stable" ]
+if [ "$BRANCH" == "" ]
 then
       echo checkout to stable branch
       git checkout stable
-elif [[ $BRANCH_ID == "master" ]]
-then
-      echo Working on master branch
 else
-      echo Checkout to PR $BRANCH_ID
-      git fetch origin pull/$BRANCH_ID/head:$BRANCH_ID
-      git checkout $BRANCH_ID
+      echo Checkout to branch $BRANCH
+      git checkout $BRANCH
 fi
 
 #Install dependencies
-$PIP_INSTALLATION install --upgrade pip setuptools
-$PIP_INSTALLATION install setuptools==65.5.0
-$PIP_INSTALLATION install -r requirements.txt
+$PIP_VERSION install --upgrade pip setuptools
+$PIP_VERSION install setuptools==65.5.0
+$PIP_VERSION install -r requirements.txt
 popd
 
 which run-ci
--- Docker_files/ocsci_container/scripts/run-ocs-ci.sh
+++ Docker_files/ocsci_container/scripts/run-ocs-ci.sh
@@ -6,9 +6,9 @@ mkdir -p /opt/cluster/logs_ocsci_container
 # Execute run-ci
 if [[ "$@" == *"--debug--"* ]]; then
   cd /opt/ocs-ci-debug
-  $PIP_INSTALLATION install --upgrade pip setuptools
-  $PIP_INSTALLATION install setuptools==65.5.0
-  $PIP_INSTALLATION install -r requirements.txt
+  $PIP_VERSION install --upgrade pip setuptools
+  $PIP_VERSION install setuptools==65.5.0
+  $PIP_VERSION install -r requirements.txt
   STR_TMP="$@"
   CMD=${STR_TMP:10:5000000}
   exec $CMD | tee /opt/cluster/logs_ocsci_container/output_$RANDOM.txt
