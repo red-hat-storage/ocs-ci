687bbe578357d75d1c0189733800d7fc03272891
Itzhak Kave <yitzhak.kave@gmail.com>
2023-03-01 13:41:27 +0200

    Update the test "test_rolling_terminate_and_recovery" for MS and non-MS clusters (#7113)
    * Generate nodes for a provider cluster using the function 'generate_nodes_for_provider_worker_node_tests' in case it's a provider cluster, Wait for the new worker node to be ready, label it in case of non-MS cluster
    
    Signed-off-by: yitzhak12 <ikave@redhat.com>
    
    * Add new worker node and label it in case of non-MS cluster, If it's not a provider cluster test rolling terminate two ocs worker nodes
    
    Signed-off-by: yitzhak12 <ikave@redhat.com>
    
    * Change current replica count to ready replica count before adding a new worker node
    
    Signed-off-by: yitzhak12 <ikave@redhat.com>
    
    * Add docstring for the function 'rolling_terminate_and_recovery_of_ocs_worker_nodes', Add log messages
    
    Signed-off-by: yitzhak12 <ikave@redhat.com>
    
    ---------
    
    Signed-off-by: yitzhak12 <ikave@redhat.com>
    Co-authored-by: yitzhak12 <ikave@redhat.com>

--- tests/manage/z_cluster/nodes/test_rolling_terminate_and_recovery.py
+++ tests/manage/z_cluster/nodes/test_rolling_terminate_and_recovery.py
@@ -1,5 +1,6 @@
 import logging
 import pytest
+import random
 
 
 from ocs_ci.framework.testlib import (
@@ -11,12 +12,22 @@ from ocs_ci.framework.testlib import (
     managed_service_required,
     ipi_deployment_required,
 )
+from ocs_ci.ocs.machine import (
+    get_machine_from_node_name,
+    get_machineset_from_machine_name,
+    get_ready_replica_count,
+    wait_for_ready_replica_count_to_reach_expected_value,
+    set_replica_count,
+)
 from ocs_ci.ocs.node import (
     get_ocs_nodes,
-    wait_for_node_count_to_reach_status,
     get_node_objs,
     recover_node_to_ready_state,
     consumers_verification_steps_after_provider_node_replacement,
+    generate_nodes_for_provider_worker_node_tests,
+    wait_for_new_worker_node_ipi,
+    get_worker_nodes,
+    add_new_node_and_label_it,
 )
 from ocs_ci.ocs.resources.pod import (
     check_pods_after_node_replacement,
@@ -73,18 +84,61 @@ class TestRollingWorkerNodeTerminateAndRecovery(ManageTest):
         request.addfinalizer(finalizer)
 
     def rolling_terminate_and_recovery_of_ocs_worker_nodes(self, nodes):
-        # Get OCS worker node objects
-        ocs_node_objs = get_ocs_nodes()
+        """
+        Test rolling termination and recovery of the OCS worker node for both MS and non-MS clusters.
 
-        # Start rolling shutdown and recovery of OCS worker nodes
+        1. Generate the OCS worker nodes for rolling terminate
+        2. Go over the OCS worker node in a loop.
+        3. Get the machine set of every worker node and the ready replica count before terminating it.
+        4. Terminate the worker node.
+        5. If we use an MS cluster, we will wait for the new worker node associated with the old node machine set
+        to come up automatically.
+        If we use a non-MS cluster, the recovery process does not always happen automatically, so we will perform
+        the following steps:
+            5.1. Ensure that the ready replica count of the old machine set is decreased by 1.
+            5.2. Set the current replica count of the old machine set to the new ready replica count(reducing
+            it by one also).
+            5.3. Add a new worker node for the old machine set(which means we increase the
+            current replica count again by 1), and label it with the OCS label.
+        6. Wait for the OCS pods to be running and Ceph health to be OK before the next iteration.
+
+        Args:
+            nodes (NodesBase): Instance of the relevant platform nodes class (e.g. AWSNodes, VMWareNodes)
+
+        """
+        # Get OCS worker node objects
+        if is_ms_provider_cluster():
+            ocs_node_objs = generate_nodes_for_provider_worker_node_tests()
+        else:
+            # If it's not a provider cluster, test rolling terminate two ocs worker nodes will suffice
+            ocs_node_objs = random.choices(get_ocs_nodes(), k=2)
+
+        log.info("Start rolling terminate and recovery of the OCS worker nodes")
         for node_obj in ocs_node_objs:
+            old_wnodes = get_worker_nodes()
+            log.info(f"Current worker nodes: {old_wnodes}")
+            machine_name = get_machine_from_node_name(node_obj.name)
+            machineset = get_machineset_from_machine_name(machine_name)
+            log.info(f"machineset name: {machineset}")
+            old_ready_rc = get_ready_replica_count(machineset)
+
             nodes.terminate_nodes(nodes=[node_obj], wait=True)
             log.info(f"Successfully terminated the node: {node_obj.name}")
+            if is_managed_service_cluster():
+                new_ocs_node = wait_for_new_worker_node_ipi(machineset, old_wnodes)
+            else:
+                expected_ready_rc = old_ready_rc - 1
+                wait_for_ready_replica_count_to_reach_expected_value(
+                    machineset, expected_ready_rc
+                )
+                log.info(
+                    "Change the current replica count to the expected ready replica count"
+                )
+                set_replica_count(machineset, expected_ready_rc)
+                new_ocs_node_names = add_new_node_and_label_it(machineset)
+                new_ocs_node = get_node_objs(new_ocs_node_names)[0]
 
-            log.info("Waiting for all the worker nodes to be ready...")
-            wait_for_node_count_to_reach_status(
-                node_count=len(ocs_node_objs), timeout=900
-            )
+            log.info(f"The new ocs node is: {new_ocs_node.name}")
             log.info("Waiting for all the pods to be running")
             assert check_pods_after_node_replacement(), "Not all the pods are running"
 
