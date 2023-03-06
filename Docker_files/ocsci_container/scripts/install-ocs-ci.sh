2b1694c98ca35a495f9485a978e60c9b9f2078be
OdedViner <61982127+OdedViner@users.noreply.github.com>
2023-03-06 14:44:54 +0200

    Makefile for OCS-CI-CONTAINER (#6819)
    * Add makefile and improvements for running ocs-ci in container
    
    Signed-off-by: OdedViner <oviner@redhat.com>

--- Docker_files/ocsci_container/scripts/install-ocs-ci.sh
+++ Docker_files/ocsci_container/scripts/install-ocs-ci.sh
@@ -10,23 +10,19 @@ git clone "$OCSCI_REPO_URL" "$OCSCI_INSTALL_DIR"
 pushd "$OCSCI_INSTALL_DIR"
 
 # Checkout to relevant branch [the default is stable]
-if [ "$BRANCH_ID" == "" ]
+if [ "$BRANCH" == "" ]
 then
       echo checkout to stable branch
       git checkout stable
-elif [[ $BRANCH_ID == "master" ]]
-then
-      echo Working on master branch
 else
-      echo Checkout to PR $BRANCH_ID
-      git fetch origin pull/$BRANCH_ID/head:$BRANCH_ID
-      git checkout $BRANCH_ID
+      echo Checkout to branch $BRANCH
+      git checkout $BRANCH
 fi
 
 #Install dependencies
-pip3.8 install --upgrade pip setuptools
-pip3.8 install setuptools==65.5.0
-pip3.8 install -r requirements.txt
+$PIP_VERSION install --upgrade pip setuptools
+$PIP_VERSION install setuptools==65.5.0
+$PIP_VERSION install -r requirements.txt
 popd
 
 which run-ci
