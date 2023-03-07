e4dc2475ef4736544512daeaf64d7d4f9011d350
Daniel Osypenko <dosypenk@redhat.com>
2023-03-07 11:22:11 +0200

    test run with any preexisted object buckets classes on cluster. review fixes
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- tests/manage/mcg/ui/test_mcg_ui.py
+++ tests/manage/mcg/ui/test_mcg_ui.py
@@ -304,9 +304,10 @@ class TestObcUserInterface(object):
             obc_bucketclass == bucketclass
         ), f"BucketClass mismatch. Expected: {bucketclass}, found: {obc_bucketclass}"
 
+        # covers BZ 2097772
         if verify_ob_removal and obc_ui_obj.ocp_version_semantic > version.VERSION_4_11:
             ObUI(setup_ui_class).delete_object_bucket_ui(
-                delete_via="three_dots", expect_fail=True
+                delete_via="three_dots", expect_fail=True, resource_name=obc_name
             )
 
         logger.info(f"Delete {obc_name}")
