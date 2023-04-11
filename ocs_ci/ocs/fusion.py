2e5bcc07eedce0c3cc00d14bb6ef03dea335dd38
fbalak <fbalak@redhat.com>
2023-04-11 15:07:17 +0200

    add fusion deployment functions
    Signed-off-by: fbalak <fbalak@redhat.com>

---
+++ ocs_ci/ocs/fusion.py
@@ -0,0 +1,63 @@
+import os
+import logging
+
+from ocs_ci.helpers import helpers
+from ocs_ci.utility.templating import load_yaml, Templating
+from ocs_ci.utility.utils import get_ocp_version
+from ocs_ci.ocs import constants
+
+logger = logging.getLogger(name=__file__)
+
+FUSION_TEMPLATE_DIR = "fusion-aas"
+
+
+def create_fusion_monitoring_resources():
+    """
+    Create resources used for Managed Fusion aaS Monitoring
+    """
+    templating = Templating(base_path=FUSION_TEMPLATE_DIR)
+    project_name = "managed-fusion"
+    logger.info(f"Creating {namespace_name} project")
+    helpers.create_project(project_name=project_name)
+    logger.info("Creating an OperatorGroup")
+    og_path = os.path.join(constants.TEMPLATE_DIR, FUSION_TEMPLATE_DIR, "operatorgroup.yaml")
+    og_data = templating.load_yaml(og_path)
+    helpers.create_resource(**og_data)
+    logger.info("Creating a CatalogSource")
+    template = _templating.render_template(
+        "catalogsource.yaml.j2",
+        config.ENV_DATA["fusion_catalogsource"],
+    )
+    helpers.create_resource(**template)
+    logger.info("Creating a Subscription")
+    og_path = os.path.join(constants.TEMPLATE_DIR, FUSION_TEMPLATE_DIR, "subscription.yaml")
+    og_data = templating.load_yaml(og_path)
+    helpers.create_resource(**og_data)
+    logger.info("Creating a monitoring secret")
+    secret_data = dict()
+    secret_data["pagerduty_config"] = config.ENV_DATA["pagerduty_config"]
+    secret_data["smtp_config"] = config.ENV_DATA["smtp_config"]
+    template = _templating.render_template(
+        "monitoringsecret.yaml.j2",
+        secret_data,
+    )
+    helpers.create_resource(**template)
+
+def deploy_odf():
+    """
+    Create openshift-storage namespace and deploy managedFusionOffering CR there.
+    """
+    templating = Templating(base_path=FUSION_TEMPLATE_DIR)
+    project_name = "openshift-storage"
+    logger.info(f"Creating {namespace_name} project")
+    helpers.create_project(project_name=project_name)
+    logger.info("Creating the offering CRD")
+    offering_data = dict()
+    offering_data["ocp_version"] = get_ocp_version()
+    offering_data["size"] = config.ENV_DATA["size"]
+    offering_data["onboarding_validation_key"] = config.AUTH["managed_service"]["public_key"]
+    template = _templating.render_template(
+        "monitoringsecret.yaml.j2",
+        secret_data,
+    )
+    helpers.create_resource(**template)
