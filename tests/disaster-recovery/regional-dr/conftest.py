27a4607630e276e8835cdd4aa7695159882ecd6b
Sidhant Agrawal <sagrawal@redhat.com>
2023-04-18 17:08:22 +0530

    Use constant for RDR mode
    Signed-off-by: Sidhant Agrawal <sagrawal@redhat.com>

--- tests/disaster-recovery/regional-dr/conftest.py
+++ tests/disaster-recovery/regional-dr/conftest.py
@@ -1,5 +1,6 @@
 import logging
 from ocs_ci.framework import config
+from ocs_ci.ocs import constants
 
 log = logging.getLogger(__name__)
 
@@ -12,7 +13,7 @@ def pytest_collection_modifyitems(items):
         items: list of collected tests
 
     """
-    if config.MULTICLUSTER.get("multicluster_mode") != "regional-dr":
+    if config.MULTICLUSTER.get("multicluster_mode") != constants.RDR_MODE:
         for item in items.copy():
             if "disaster-recovery/regional-dr" in str(item.fspath):
                 log.debug(
