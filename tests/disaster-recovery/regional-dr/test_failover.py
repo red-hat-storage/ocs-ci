c953ad8f15c13455aacb5243498aa3535636cff4
Aman Agrawal <84079241+am-agrawa@users.noreply.github.com>
2023-04-24 16:00:26 +0530

    Test Failover/Relocate from ACM UI for Regional DR (#7163)
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- tests/disaster-recovery/regional-dr/test_failover.py
+++ tests/disaster-recovery/regional-dr/test_failover.py
@@ -9,8 +9,22 @@ from ocs_ci.helpers import dr_helpers
 from ocs_ci.ocs.node import wait_for_nodes_status, get_node_objs
 from ocs_ci.ocs.resources.pod import wait_for_pods_to_be_running
 from ocs_ci.utility.utils import ceph_health_check
+from ocs_ci.ocs.acm.acm import AcmAddClusters
+from ocs_ci.helpers.dr_helpers_ui import (
+    dr_submariner_validation_from_ui,
+    check_cluster_status_on_acm_console,
+    failover_relocate_ui,
+    verify_failover_relocate_status_ui,
+)
+from ocs_ci.utility import version
 
 logger = logging.getLogger(__name__)
+
+polarion_id_primary_up = "OCS-4429"
+polarion_id_primary_down = "OCS-4426"
+if config.RUN.get("rdr_failover_via_ui"):
+    polarion_id_primary_up = "OCS-4741"
+    polarion_id_primary_down = "OCS-4742"
 
 
 @acceptance
@@ -25,15 +39,20 @@ class TestFailover:
         argnames=["primary_cluster_down"],
         argvalues=[
             pytest.param(
-                False, marks=pytest.mark.polarion_id("OCS-4429"), id="primary_up"
+                False,
+                marks=pytest.mark.polarion_id(polarion_id_primary_up),
+                id="primary_up",
             ),
             pytest.param(
-                True, marks=pytest.mark.polarion_id("OCS-4426"), id="primary_down"
+                True,
+                marks=pytest.mark.polarion_id(polarion_id_primary_down),
+                id="primary_down",
             ),
         ],
     )
     def test_failover(
         self,
+        setup_acm_ui,
         primary_cluster_down,
         nodes_multicluster,
         rdr_workload,
@@ -45,7 +64,18 @@ class TestFailover:
             1) Failover to secondary cluster when primary cluster is UP
             2) Failover to secondary cluster when primary cluster is DOWN
 
+        This test is also compatible to be run from ACM UI,
+        pass the yaml conf/ocsci/dr_ui.yaml to trigger it.
+
         """
+        if config.RUN.get("rdr_relocate_via_ui"):
+            ocs_version = version.get_semantic_ocs_version_from_config()
+            if ocs_version <= version.VERSION_4_12:
+                logger.error("ODF/ACM version isn't supported for Failover operation")
+                raise NotImplementedError
+
+        acm_obj = AcmAddClusters(setup_acm_ui)
+
         dr_helpers.set_current_primary_cluster_context(rdr_workload.workload_namespace)
         primary_cluster_index = config.cur_index
         node_objs = get_node_objs()
@@ -57,16 +87,46 @@ class TestFailover:
         logger.info(f"Waiting for {wait_time} minutes to run IOs")
         sleep(wait_time * 60)
 
+        primary_cluster_name = dr_helpers.get_current_primary_cluster_name(
+            rdr_workload.workload_namespace
+        )
+        secondary_cluster_name = dr_helpers.get_current_secondary_cluster_name(
+            rdr_workload.workload_namespace
+        )
+
+        if config.RUN.get("rdr_failover_via_ui"):
+            logger.info("Start the process of Failover from ACM UI")
+            config.switch_acm_ctx()
+            dr_submariner_validation_from_ui(acm_obj)
+
         # Stop primary cluster nodes
         if primary_cluster_down:
             logger.info("Stopping primary cluster nodes")
             nodes_multicluster[primary_cluster_index].stop_nodes(node_objs)
 
-        # Failover action
-        secondary_cluster_name = dr_helpers.get_current_secondary_cluster_name(
-            rdr_workload.workload_namespace
-        )
-        dr_helpers.failover(secondary_cluster_name, rdr_workload.workload_namespace)
+            # Verify if cluster is marked unknown on ACM console
+            if config.RUN.get("rdr_failover_via_ui"):
+                config.switch_acm_ctx()
+                check_cluster_status_on_acm_console(
+                    acm_obj,
+                    down_cluster_name=primary_cluster_name,
+                    expected_text="Unknown",
+                )
+        elif config.RUN.get("rdr_failover_via_ui"):
+            check_cluster_status_on_acm_console(acm_obj)
+
+        if config.RUN.get("rdr_failover_via_ui"):
+            # Failover via ACM UI
+            failover_relocate_ui(
+                acm_obj,
+                scheduling_interval=scheduling_interval,
+                workload_to_move=f"{rdr_workload.workload_name}-1",
+                policy_name=rdr_workload.dr_policy_name,
+                failover_or_preferred_cluster=secondary_cluster_name,
+            )
+        else:
+            # Failover action via CLI
+            dr_helpers.failover(secondary_cluster_name, rdr_workload.workload_namespace)
 
         # Verify resources creation on new primary cluster (failoverCluster)
         dr_helpers.set_current_primary_cluster_context(rdr_workload.workload_namespace)
@@ -99,5 +159,9 @@ class TestFailover:
         dr_helpers.wait_for_all_resources_deletion(rdr_workload.workload_namespace)
 
         dr_helpers.wait_for_mirroring_status_ok()
+
+        if config.RUN.get("rdr_relocate_via_ui"):
+            config.switch_acm_ctx()
+            verify_failover_relocate_status_ui(acm_obj)
 
         # TODO: Add data integrity checks
