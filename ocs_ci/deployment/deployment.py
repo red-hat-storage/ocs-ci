8ac9279d364fdeac25a33cde2978f6683e493607
prsurve <prsurve@redhat.com>
2023-04-24 10:42:23 +0530

    Add missing conf file
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -207,79 +207,83 @@ class Deployment(object):
         Returns:
 
         """
-        if config.ENV_DATA.get("deploy_gitops_operator", False):
+        if not config.ENV_DATA.get("deploy_gitops_operator"):
             return
-        logger.info("Creating GitOps Operator Subscription")
-        gitops_subscription_yaml_data = templating.load_yaml(
-            constants.GITOPS_SUBSCRIPTION_YAML
-        )
-        package_manifest = PackageManifest(
-            resource_name=constants.GITOPS_OPERATOR_NAME,
-        )
-        gitops_subscription_yaml_data["spec"][
-            "startingCSV"
-        ] = package_manifest.get_current_csv(
-            channel="latest", csv_pattern=constants.GITOPS_OPERATOR_NAME
-        )
 
-        gitops_subscription_manifest = tempfile.NamedTemporaryFile(
-            mode="w+", prefix="gitops_subscription_manifest", delete=False
-        )
-        templating.dump_data_to_temp_yaml(
-            gitops_subscription_yaml_data, gitops_subscription_manifest.name
-        )
-        run_cmd(f"oc create -f {gitops_subscription_manifest.name}")
-        logger.info("Sleeping for 90 seconds after subscribing to GitOps")
-        time.sleep(90)
-        csv_name = package_manifest.get_current_csv(channel="latest")
-        csv = CSV(resource_name=csv_name, namespace=constants.GITOPS_NAMESPACE)
-        csv.wait_for_phase("Succeeded", timeout=720)
-        logger.info("GitOps Operator Deployment Succeeded")
-
-        logger.info("Creating GitOps CLuster Resource")
-        run_cmd(f"oc create -f {constants.GITOPS_CLUSTER_YAML}")
-
-        logger.info("Creating GitOps CLuster Placement Resource")
-        run_cmd(f"oc create -f {constants.GITOPS_PLACEMENT_YAML}")
-
-        logger.info("Creating ManagedClusterSetBinding")
-
-        cluster_set = []
-        managed_clusters = (
-            ocp.OCP(kind=constants.ACM_MANAGEDCLUSTER).get().get("items", [])
-        )
-        # ignore local-cluster here
-        for i in managed_clusters:
-            if i["metadata"]["name"] != constants.ACM_LOCAL_CLUSTER:
-                cluster_set.append(
-                    i["metadata"]["labels"][constants.ACM_CLUSTERSET_LABEL]
-                )
-        if all(x == cluster_set[0] for x in cluster_set):
-            logger.info(f"Found the uniq clusterset {cluster_set[0]}")
-        else:
-            raise UnexpectedDeploymentConfiguration(
-                "There are more then one clusterset added to multiple managedcluters"
+        # Multicluster operations
+        if config.multicluster:
+            config.switch_acm_ctx()
+            logger.info("Creating GitOps Operator Subscription")
+            gitops_subscription_yaml_data = templating.load_yaml(
+                constants.GITOPS_SUBSCRIPTION_YAML
+            )
+            package_manifest = PackageManifest(
+                resource_name=constants.GITOPS_OPERATOR_NAME,
+            )
+            gitops_subscription_yaml_data["spec"][
+                "startingCSV"
+            ] = package_manifest.get_current_csv(
+                channel="latest", csv_pattern=constants.GITOPS_OPERATOR_NAME
             )
 
-        managedclustersetbinding_obj = templating.load_yaml(
-            constants.GITOPS_MANAGEDCLUSTER_SETBINDING_YAML
-        )
-        managedclustersetbinding_obj["spec"]["clusterSet"] = cluster_set[0]
-        managedclustersetbinding = tempfile.NamedTemporaryFile(
-            mode="w+", prefix="managedcluster_setbinding", delete=False
-        )
-        templating.dump_data_to_temp_yaml(
-            managedclustersetbinding_obj, managedclustersetbinding.name
-        )
-        run_cmd(f"oc create -f {managedclustersetbinding.name}")
+            gitops_subscription_manifest = tempfile.NamedTemporaryFile(
+                mode="w+", prefix="gitops_subscription_manifest", delete=False
+            )
+            templating.dump_data_to_temp_yaml(
+                gitops_subscription_yaml_data, gitops_subscription_manifest.name
+            )
+            run_cmd(f"oc create -f {gitops_subscription_manifest.name}")
+            logger.info("Sleeping for 90 seconds after subscribing to GitOps")
+            time.sleep(90)
+            csv_name = package_manifest.get_current_csv(channel="latest")
+            csv = CSV(resource_name=csv_name, namespace=constants.GITOPS_NAMESPACE)
+            csv.wait_for_phase("Succeeded", timeout=720)
+            logger.info("GitOps Operator Deployment Succeeded")
 
-        gitops_obj = ocp.OCP(
-            resource_name=constants.GITOPS_CLUSTER_NAME,
-            namespace=constants.GITOPS_CLUSTER_NAMESPACE,
-            kind=constants.GITOPS_CLUSTER,
-        )
-        gitops_obj._has_phase = True
-        gitops_obj.wait_for_phase("successful", timeout=720)
+            logger.info("Creating GitOps CLuster Resource")
+            run_cmd(f"oc create -f {constants.GITOPS_CLUSTER_YAML}")
+
+            logger.info("Creating GitOps CLuster Placement Resource")
+            run_cmd(f"oc create -f {constants.GITOPS_PLACEMENT_YAML}")
+
+            logger.info("Creating ManagedClusterSetBinding")
+
+            cluster_set = []
+            managed_clusters = (
+                ocp.OCP(kind=constants.ACM_MANAGEDCLUSTER).get().get("items", [])
+            )
+            # ignore local-cluster here
+            for i in managed_clusters:
+                if i["metadata"]["name"] != constants.ACM_LOCAL_CLUSTER:
+                    cluster_set.append(
+                        i["metadata"]["labels"][constants.ACM_CLUSTERSET_LABEL]
+                    )
+            if all(x == cluster_set[0] for x in cluster_set):
+                logger.info(f"Found the uniq clusterset {cluster_set[0]}")
+            else:
+                raise UnexpectedDeploymentConfiguration(
+                    "There are more then one clusterset added to multiple managedcluters"
+                )
+
+            managedclustersetbinding_obj = templating.load_yaml(
+                constants.GITOPS_MANAGEDCLUSTER_SETBINDING_YAML
+            )
+            managedclustersetbinding_obj["spec"]["clusterSet"] = cluster_set[0]
+            managedclustersetbinding = tempfile.NamedTemporaryFile(
+                mode="w+", prefix="managedcluster_setbinding", delete=False
+            )
+            templating.dump_data_to_temp_yaml(
+                managedclustersetbinding_obj, managedclustersetbinding.name
+            )
+            run_cmd(f"oc create -f {managedclustersetbinding.name}")
+
+            gitops_obj = ocp.OCP(
+                resource_name=constants.GITOPS_CLUSTER_NAME,
+                namespace=constants.GITOPS_CLUSTER_NAMESPACE,
+                kind=constants.GITOPS_CLUSTER,
+            )
+            gitops_obj._has_phase = True
+            gitops_obj.wait_for_phase("successful", timeout=720)
 
     def do_deploy_ocs(self):
         """
@@ -395,6 +399,7 @@ class Deployment(object):
             self.deploy_acm_hub()
         self.do_deploy_lvmo()
         self.do_deploy_submariner()
+
         self.do_gitops_deploy()
         self.do_deploy_ocs()
         self.do_deploy_rdr()
