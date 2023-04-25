87c1cb2e29bbb4385dbd9183a620b2cb03f2d34a
fbalak <fbalak@redhat.com>
2023-04-25 16:26:24 +0200

    update storageclass claim namespace handling
    Signed-off-by: fbalak <fbalak@redhat.com>

--- ocs_ci/ocs/resources/storageclassclaim.py
+++ ocs_ci/ocs/resources/storageclassclaim.py
@@ -89,7 +89,6 @@ def create_storageclassclaim(
                 "name": "storageclient",
                 "namespace": namespace,
             }
-            sc_claim_data["metadata"]["namespace"] = None
         else:
             sc_claim_data["metadata"]["namespace"] = namespace
 
