0d54216a23a5ffd8c974bd94b8c027e87571e46e
Aviadp <apolak@redhat.com>
2023-04-26 21:20:06 +0300

    Created Test for noobaa logs reduction as optional configuration
    Signed-off-by: Aviadp <apolak@redhat.com>

--- ocs_ci/ocs/resources/pod.py
+++ ocs_ci/ocs/resources/pod.py
@@ -1124,6 +1124,63 @@ def get_topolvm_node_pod(
     return Pod(**topolvm_node_pod[0])
 
 
+def get_noobaa_core_pod(
+    label: str = constants.NOOBAA_CORE_POD_LABEL,
+    namespace: str = config.ENV_DATA["cluster_namespace"],
+) -> Pod:
+    """
+    Get noobaa core pod
+
+    Args:
+        label (str): Label associated with noobaa-core pod
+        namespace (str): Namespace in which noobaa-core pod is residing
+
+    Returns:
+        Pod: Pod object of noobaa-core pod
+
+    """
+    noobaa_core_pod = get_pods_having_label(label, namespace)
+    return Pod(**noobaa_core_pod[0])
+
+
+def get_noobaa_endpoint_pod(
+    label: str = constants.NOOBAA_ENDPOINT_POD_LABEL,
+    namespace: str = config.ENV_DATA["cluster_namespace"],
+) -> Pod:
+    """
+    Get Noobaa endpoint pod
+
+    Args:
+        label (str): Label associated with noobaa endpoint pod
+        namespace (str): Namespace in which noobaa endpoint pod is residing
+
+    Returns:
+        Pod: Pod object of noobaa endpoint pod
+
+    """
+    noobaa_endpoint_pod = get_pods_having_label(label, namespace)
+    return Pod(**noobaa_endpoint_pod[0])
+
+
+def get_noobaa_operator_pod(
+    label: str = constants.NOOBAA_OPERATOR_POD_LABEL,
+    namespace: str = config.ENV_DATA["cluster_namespace"],
+) -> Pod:
+    """
+    get Noobaa operator pod
+
+    Args:
+        label (str): Label associated with noobaa operator pod
+        namespace (str): Namespace in which noobaa operator pod is residing
+
+    Returns:
+        Pod: Pod object of noobaa operator pod
+
+    """
+    noobaa_operator_opd = get_pods_having_label(label, namespace)
+    return Pod(**noobaa_operator_opd[0])
+
+
 def list_ceph_images(pool_name="rbd"):
     """
     Args:
--- tests/manage/mcg/test_mcg_log_level.py
+++ tests/manage/mcg/test_mcg_log_level.py
@@ -2,23 +2,102 @@ import pytest
 
 from logging import getLogger
 
+from ocs_ci.framework import config
 from ocs_ci.ocs.ocp import OCP
+from ocs_ci.ocs.resources.pod import (
+    get_noobaa_core_pod,
+    get_noobaa_endpoint_pod,
+    get_noobaa_operator_pod,
+    get_pod_logs,
+)
 from ocs_ci.framework.testlib import tier2, bugzilla
 from ocs_ci.ocs.constants import (
     CONFIGMAP,
     NOOBAA_CONFIGMAP,
+    POD,
+    NOOBAA_APP_LABEL,
+    STATUS_RUNNING,
 )
 
 log = getLogger(__name__)
+NAMESPACE = config.ENV_DATA.get("cluster_namespace", "openshift-storage")
 
 
 def get_noobaa_cfg_log_level(cfgmap: OCP) -> str:
+    """
+    Gets noobaa current log level from its configmap
+
+    Args:
+        cfgmap (OCP): OCP object of kind "configmap"
+
+    Returns:
+        str: value of NOOBAA_LOG_LEVEL
+
+    """
     return cfgmap.get(NOOBAA_CONFIGMAP).get("data").get("NOOBAA_LOG_LEVEL")
+
+
+def set_noobaa_cfg_log_level(cfgmap: OCP, log_level: str) -> None:
+    """
+    Patch noobaa configmap, to set log level
+
+    Args:
+        cfgmap (OCP): OCP object of kind "configmap"
+        log_level (str): value of NOOBAA_LOG_LEVEL to set
+
+    """
+    cmd = f'{{"data": {{"NOOBAA_LOG_LEVEL": "{log_level}"}}}}'
+    cfgmap.patch(resource_name=NOOBAA_CONFIGMAP, params=cmd, format_type="merge")
+
+
+def check_noobaa_logs(pod_name: str, text_to_search: str) -> bool:
+    """
+    Check if given text can be found in noobaa pod log
+
+    Args:
+        pod_name (str): pod name to check its logs
+        text_to_search (str): text to search for in given pod log
+
+    Returns:
+        bool: True if text is found in logs, False otherwise
+
+    """
+    logs = get_pod_logs(pod_name)
+    if text_to_search in logs:
+        return True
+    return False
 
 
 @tier2
 @bugzilla("1932846")
 @pytest.mark.polarion_id("TBD")
-def test_default_mcg_core_log_level() -> None:
-    cfgmap = OCP(kind=CONFIGMAP)
-    get_noobaa_cfg_log_level(cfgmap)
+class TestNoobaaLogLevel:
+    """
+    Test optional Noobaa (MCG) log leevl reduction, as validation of BZ-1932846
+
+    """
+
+    cfgmap = OCP(namespace=NAMESPACE, kind=CONFIGMAP, resource_name=NOOBAA_CONFIGMAP)
+    pod_obj = OCP(namespace=NAMESPACE, kind=POD, selector=NOOBAA_APP_LABEL)
+
+    @pytest.fixture(autouse=True)
+    def set_log_level_warn(self) -> None:
+        log_level = get_noobaa_cfg_log_level(self.cfgmap)
+        if log_level != "warn":
+            set_noobaa_cfg_log_level(cfgmap=self.cfgmap, log_level="warn")
+        self.pod_obj.wait_for_resource(condition=STATUS_RUNNING)
+
+    def test_mcg_core_log_level(self) -> None:
+        noobaa_core_pod = get_noobaa_core_pod()
+        noobaa_core_name = noobaa_core_pod.data.get("metadata").get("name")
+        assert not check_noobaa_logs(noobaa_core_name, "[1]")
+
+    def test_noobaa_operator_log_level(self) -> None:
+        noobaa_operator_pod = get_noobaa_operator_pod()
+        noobaa_operator_name = noobaa_operator_pod.data.get("metadata").get("name")
+        assert not check_noobaa_logs(noobaa_operator_name, "level=info")
+
+    def test_noobaa_endpoint_log_level(self) -> None:
+        noobaa_endpoint_pod = get_noobaa_endpoint_pod()
+        noobaa_endpoint_name = noobaa_endpoint_pod.data.get("metadata").get("name")
+        assert not check_noobaa_logs(noobaa_endpoint_name, "[1]")
