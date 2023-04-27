5f790580f5d839ac4ec6ccf6e52a2fcc30768246
OdedViner <61982127+OdedViner@users.noreply.github.com>
2023-04-27 17:38:22 +0300

    Fix OCS-CI container (#7502)
    * Fix OCS-CI container
    * Added data to docker ignore
    
    Signed-off-by: oviner <oviner@li-6f699a4c-1e9b-11b2-a85c-d701b8cb9024.ibm.com>

--- Docker_files/ocsci_container/build-image.sh
+++ Docker_files/ocsci_container/build-image.sh
@@ -6,9 +6,6 @@ source Docker_files/ocsci_container/scripts/common.sh
 PWD=$(pwd)
 
 $ENGINE_ARG build -t $IMAGE_NAME_ARG -f \
-${PWD}/Docker_files/ocsci_container/Dockerfile_ocsci \
-${PWD}/Docker_files/ocsci_container \
---build-arg BRANCH_ARG=$BRANCH_ARG \
---build-arg PIP_VERSION_ARG=$PIP_VERSION_ARG \
+${PWD}/Docker_files/ocsci_container/Containerfile.ci .\
 
-#make build-image BRANCH=release-4.12 IMAGE_NAME=quay.io/ocsci/ocs-ci-container:release-4.12 ENGINE=podman
+#make build-image IMAGE_NAME=quay.io/ocsci/ocs-ci-container:stable ENGINE=podman
