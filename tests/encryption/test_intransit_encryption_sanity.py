1776a42fb40dbfdf96b727e0601bf58f4e2eb9eb
paraggit <pakamble@redhat.com>
2023-05-01 22:27:38 +0530

    Added polarion ID to the tests case
    Signed-off-by: paraggit <pakamble@redhat.com>

--- tests/encryption/test_intransit_encryption_sanity.py
+++ tests/encryption/test_intransit_encryption_sanity.py
@@ -17,31 +17,35 @@ log = logging.getLogger(__name__)
 @skipif_intransit_encryption_notset
 class TestInTransitEncryptionSanity:
     def teardown(self):
-        log.info("*******")
         set_in_transit_encryption()
 
     @tier1
     @skipif_ocs_version("<4.12")
+    @pytest.mark.polarion_id("OCS-4861")
     def test_intransit_encryption_enable_disable_statetransition(self):
         """
-        1. Verify intransit Encryption is Enable on setup.
-        2. Start IO in background
-        3. Disable Encryption
-        4. Run verification JOb
-        5. enable encryption Again.
+        1. Verify in-transit Encryption is Enable on setup.
+        2. Disable Encryption
+        3. Verify in-transit encryption configuration is removed.
+        4. Enable encryption Again and verify it.
+        5. Verify in-transit encryption config is exists.
         """
         log.info("Verifying the in-transit encryption is enable on setup.")
         assert in_transit_encryption_verification()
 
-        log.info("Disabling the intransit encryption.")
+        log.info("Disabling the in-transit encryption.")
         set_in_transit_encryption(enabled=False)
-        log.info("Verifying the intransit encryption is disabled.")
+
+        # Verify that encryption is actually disabled by checking that a ValueError is raised.
+        log.info("Verifying the in-transit encryption is disabled.")
         with pytest.raises(ValueError):
             assert not in_transit_encryption_verification()
 
-        log.info("Re-enabling intransit encryption.")
+        log.info("Re-enabling in-transit encryption.")
         set_in_transit_encryption()
+
+        # Verify that encryption is enabled again after re-enabling it
         log.info(
-            "Verifying the intransit encryption config after enabling the cluster."
+            "Verifying the in-transit encryption config after enabling the cluster."
         )
         assert in_transit_encryption_verification()
