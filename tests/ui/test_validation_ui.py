ec902ce091423e38092f41e237b990d00f1eb609
Daniel Osypenko <dosypenk@redhat.com>
2023-05-02 16:08:28 +0300

    Skip ui tests for IBM cloud deployment. Cherry-pick 4.9 (#7521)
    * skip ui tests for IBM cloud deployment. Authorization to console not … (#7122)
    
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- tests/ui/test_validation_ui.py
+++ tests/ui/test_validation_ui.py
@@ -12,6 +12,7 @@ from ocs_ci.framework.pytest_customization.marks import (
     black_squad,
     skipif_external_mode,
     skipif_mcg_only,
+    skipif_ibm_cloud_managed,
 )
 from ocs_ci.ocs.ui.validation_ui import ValidationUI
 from ocs_ci.utility import version
@@ -19,6 +20,7 @@ from ocs_ci.utility import version
 logger = logging.getLogger(__name__)
 
 
+@skipif_ibm_cloud_managed
 class TestUserInterfaceValidation(object):
     """
     Test User Interface Validation
