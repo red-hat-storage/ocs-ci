376a0809aa62cf06bc8184ec33354bb74e417948
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2023-05-11 20:12:13 +0530

    Added ignore_leftovers to avoid ResourceLeftoversException
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- tests/manage/z_cluster/cluster_expansion/test_verify_ceph_csidriver_runs_on_non_ocs_nodes.py
+++ tests/manage/z_cluster/cluster_expansion/test_verify_ceph_csidriver_runs_on_non_ocs_nodes.py
@@ -6,7 +6,7 @@ from ocs_ci.framework.pytest_customization.marks import (
     skipif_flexy_deployment,
     skipif_managed_service,
 )
-from ocs_ci.framework.testlib import tier1, ManageTest
+from ocs_ci.framework.testlib import tier1, ManageTest, ignore_leftovers
 from ocs_ci.ocs.node import get_worker_nodes_not_in_ocs
 from ocs_ci.ocs.resources.pod import get_pod_node, get_plugin_pods
 
@@ -19,6 +19,7 @@ logger = logging.getLogger(__name__)
 @tier1
 @pytest.mark.polarion_id("OCS-2490")
 @pytest.mark.bugzilla("1794389")
+@ignore_leftovers
 class TestCheckTolerationForCephCsiDriverDs(ManageTest):
     """
     Check toleration for Ceph CSI driver DS on non ocs node
