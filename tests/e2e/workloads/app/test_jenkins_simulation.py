8785fb09fcfab55bac8ad140347fbf2dc04b096b
Vishakha Kathole <vkathole@redhat.com>
2023-05-26 11:59:47 +0530

    [Release-4.12 cherry pick] test fix up (#7385) (#7698)
    Signed-off-by: vkathole <vkathole@redhat.com>

--- tests/e2e/workloads/app/test_jenkins_simulation.py
+++ tests/e2e/workloads/app/test_jenkins_simulation.py
@@ -5,7 +5,7 @@ from ocs_ci.framework.testlib import ManageTest, workloads, polarion_id, bugzill
 from ocs_ci.ocs import constants, node
 from ocs_ci.utility import templating
 from ocs_ci.ocs.resources.pod import Pod
-from ocs_ci.ocs.resources import pod as res_pod
+from ocs_ci.ocs.resources import pod as cephfs_pod
 
 logger = logging.getLogger(__name__)
 
@@ -41,45 +41,62 @@ class TestJenkinsSimulation(ManageTest):
     @workloads
     @polarion_id("OCS-4668")
     @bugzilla("2096395")
-    def test_git_clone(self, pod):
+    @bugzilla("2132270")
+    def test_git_clone(self, pod, interface_iterate):
         """
         git clones a large repository
         Added test coverage for BZ #2096395
         """
-        csi_cephfsplugin_pod_objs = res_pod.get_all_pods(
-            namespace=constants.OPENSHIFT_STORAGE_NAMESPACE,
-            selector=["csi-cephfsplugin"],
-        )
+        if interface_iterate == constants.CEPHFILESYSTEM:
+            csi_cephfsplugin_pod_objs = cephfs_pod.get_all_pods(
+                namespace=constants.OPENSHIFT_STORAGE_NAMESPACE,
+                selector=["csi-cephfsplugin"],
+            )
 
-        relevant_pod_logs = None
-        func_calls = ["NodeStageVolume", "NodeGetVolumeStats"]
-        error_msg = "System has not been booted with systemd"
+            relevant_pod_logs = None
+            func_calls = ["NodeStageVolume", "NodeGetVolumeStats"]
+            error_msg = "System has not been booted with systemd"
+            inode_info = '"unit":2'
+            kubelet_volume_stats = "kubelet_volume_stats_inodes"
 
-        # Get the node running this pod
-        node_name = res_pod.get_pod_node(pod_obj=pod).name
+            # Get the node running this pod
+            node_name = cephfs_pod.get_pod_node(pod_obj=pod).name
 
-        # Get the csi_cephfsplugin pod running on this node
-        cephfsplugin_pod = node.get_node_pods(
-            node_name=node_name, pods_to_search=csi_cephfsplugin_pod_objs
-        )[0]
+            # Get the csi_cephfsplugin pod running on this node
+            cephfsplugin_pod = node.get_node_pods(
+                node_name=node_name, pods_to_search=csi_cephfsplugin_pod_objs
+            )[0]
 
-        pod_log = res_pod.get_pod_logs(
-            pod_name=cephfsplugin_pod.name, container="csi-cephfsplugin"
-        )
-        for f_call in func_calls:
-            if f_call in pod_log:
-                relevant_pod_logs = pod_log
-                logger.info(
-                    f"Found '{f_call}' call in logs on pod {cephfsplugin_pod.name}"
-                )
-                break
+            pod_log = cephfs_pod.get_pod_logs(
+                pod_name=cephfsplugin_pod.name, container="csi-cephfsplugin"
+            )
+            for f_call in func_calls:
+                if f_call in pod_log:
+                    relevant_pod_logs = pod_log
+                    logger.info(
+                        f"Found '{f_call}' call in logs on pod {cephfsplugin_pod.name}"
+                    )
+                    break
 
-        assert (
-            relevant_pod_logs
-        ), f"None of {func_calls} were not found on {cephfsplugin_pod.name} pod logs"
-        assert not (
-            error_msg in relevant_pod_logs
-        ), f"Logs should not contain the error message '{error_msg}'"
-        logger.info(f"Logs did not contain the error message '{error_msg}'")
+            assert (
+                relevant_pod_logs
+            ), f"None of {func_calls} were not found on {cephfsplugin_pod.name} pod logs"
+            assert not (
+                error_msg in relevant_pod_logs
+            ), f"Logs should not contain the error message '{error_msg}'"
+            logger.info(f"Logs did not contain the error message '{error_msg}'")
+
+            # Test coverage for BZ 2132270
+            assert not (
+                inode_info in relevant_pod_logs
+            ), f"Logs should not contain the message '{inode_info}'"
+            logger.info(f"Logs did not contain the error message '{inode_info}'")
+
+            assert not (
+                kubelet_volume_stats in relevant_pod_logs
+            ), f"Logs should not contain the message '{kubelet_volume_stats}'"
+            logger.info(
+                f"Logs did not contain the error message '{kubelet_volume_stats}'"
+            )
 
         pod.run_git_clone()
