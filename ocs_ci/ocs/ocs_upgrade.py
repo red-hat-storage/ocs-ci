490b05778d387a54e218d30b8328784134c3c99c
am-agrawa <amagrawa@redhat.com>
2023-05-30 15:59:07 +0530

    rebase on latest master
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- ocs_ci/ocs/ocs_upgrade.py
+++ ocs_ci/ocs/ocs_upgrade.py
@@ -576,7 +576,7 @@ def run_ocs_upgrade(
     )
 
     # Before ODF upgrade, login to OCP console and enable console plugin if not already,
-    # refresh the web console is pop-up is found
+    # also refresh the web console is pop-up is found
     semantic_upgrade_version = version.get_semantic_version(upgrade_version, True)
     if semantic_upgrade_version >= version.VERSION_4_9:
         validation_ui_obj = ValidationUI()
