b0a2c8e8034c23233d32246874d8539403a8793d
ikave <ikave@li-096b72cc-220d-11b2-a85c-86cd9212f92b.ibm.com>
2023-06-07 10:43:18 +0300

    Remove the '@skipif_vsphere_ipi' decorator
    Signed-off-by: ikave <ikave@li-096b72cc-220d-11b2-a85c-86cd9212f92b.ibm.com>

--- tests/manage/z_cluster/nodes/test_nodes_restart.py
+++ tests/manage/z_cluster/nodes/test_nodes_restart.py
@@ -10,7 +10,6 @@ from ocs_ci.framework.testlib import (
     cloud_platform_required,
     bugzilla,
     skipif_no_lso,
-    skipif_vsphere_ipi,
     skipif_ibm_cloud,
     skipif_managed_service,
 )
@@ -32,7 +31,6 @@ logger = logging.getLogger(__name__)
 
 
 @ignore_leftovers
-@skipif_vsphere_ipi
 @skipif_managed_service
 class TestNodesRestart(ManageTest):
     """
