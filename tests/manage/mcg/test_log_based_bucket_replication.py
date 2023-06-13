07c833174503d8d8fbc6cdb7fdd590128f2e4850
Sagi Hirshfeld <shirshfe@redhat.com>
2023-06-13 21:43:29 +0300

    test file progress
    Signed-off-by: Sagi Hirshfeld <shirshfe@redhat.com>

--- tests/manage/mcg/test_log_based_bucket_replication.py
+++ tests/manage/mcg/test_log_based_bucket_replication.py
@@ -1,9 +1,10 @@
 from ocs_ci.framework.testlib import MCGTest
-from ocs_ci.ocs.resources.mockup_aws_bucket_logger import MockupAwsBucketLogger
+from ocs_ci.ocs.resources.mockup_bucket_logger import MockupBucketLogger
 
-# from ocs_ci.ocs import constants
+from ocs_ci.ocs import constants
 
 from ocs_ci.framework.testlib import skipif_aws_creds_are_missing
+from ocs_ci.ocs.resources.replication_policy import LogBasedReplicationPolicy
 
 
 @skipif_aws_creds_are_missing
@@ -18,33 +19,39 @@ class TestLogBasedBucketReplication(MCGTest):
     def test_deletion_sync(
         self,
         awscli_pod_session,
-        cloud_uls_factory,
+        mcg_obj_session,
         bucket_factory,
     ):
         bucketclass_dict = {
             "interface": "OC",
             "namespace_policy_dict": {
                 "type": "Single",
-                "namespacestore_dict": {"aws": [(1, self.DEFAULT_AWS_REGION)]},
+                "namespacestore_dict": {
+                    constants.AWS_PLATFORM: [(1, self.DEFAULT_AWS_REGION)]
+                },
             },
         }
         target_bucket_name = bucket_factory(bucketclass=bucketclass_dict)[0].name
 
-        mockup_logger = MockupAwsBucketLogger(
-            awscli_pod_session, cloud_uls_factory, self.DEFAULT_AWS_REGION
+        mockup_logger = MockupBucketLogger(
+            awscli_pod=awscli_pod_session,
+            mcg_obj=mcg_obj_session,
+            bucket_factory=bucket_factory,
+            platform=constants.AWS_PLATFORM,
+            region=self.DEFAULT_AWS_REGION,
         )
-        logs_bucket_name = mockup_logger.logs_bucket_name
+        logs_bucket_name = mockup_logger.logs_bucket_uls_name
 
-        # TODO wait for replication_policy overhaul PR to get merged
-        replication_policy = (
-            "basic-replication-rule",
-            target_bucket_name,
-            logs_bucket_name,
-            None,
+        replication_policy = LogBasedReplicationPolicy(
+            destination_bucket=target_bucket_name,
+            sync_deletions=True,
+            logs_bucket=logs_bucket_name,
         )
 
         source_bucket_name = bucket_factory(
             1, bucketclass=bucketclass_dict, replication_policy=replication_policy
         )[0].name
+
+        mockup_logger.upload_test_objs_and_log(source_bucket_name)
 
         assert source_bucket_name
