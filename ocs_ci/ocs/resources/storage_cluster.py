5f4a990659972280a65f86d9065e3c75c2962dee
Elena Bondarenko <40801241+ebondare@users.noreply.github.com>
2023-06-14 11:12:45 +0200

    Add waiting to cephcluster check (#7748)
    * Add waiting to cephcluster check
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    * Fix copypaste error
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    * Fix wait_for_phase usage
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    * Fix working with cephcluster yaml
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    * Simplify code
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    ---------
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -1687,11 +1687,10 @@ def verify_provider_resources():
     cephcluster = OCP(
         kind="CephCluster", namespace=config.ENV_DATA["cluster_namespace"]
     )
+    log.info("Waiting for Cephcluster to be Ready")
+    cephcluster.wait_for_phase(phase=constants.STATUS_READY, timeout=600)
     cephcluster_yaml = cephcluster.get().get("items")[0]
-    log.info("Verifying that cephcluster is Ready and hostNetworking is True")
-    assert (
-        cephcluster_yaml["status"]["phase"] == "Ready"
-    ), f"Status of cephcluster ocs-storagecluster-cephcluster is {cephcluster_yaml['status']['phase']}"
+    log.info("Verifying that cephcluster's hostNetworking is True")
     assert cephcluster_yaml["spec"]["network"][
         "hostNetwork"
     ], f"hostNetwork is {cephcluster_yaml['spec']['network']['hostNetwork']}"
