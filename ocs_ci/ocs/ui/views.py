3abea2be48e54081d28954f3f7b44f11658a7d30
Daniel Osypenko <dosypenk@redhat.com>
2023-06-20 19:53:31 +0300

    locators 4.14 provided
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- ocs_ci/ocs/ui/views.py
+++ ocs_ci/ocs/ui/views.py
@@ -1471,6 +1471,39 @@ topology = {
 }
 
 locators = {
+    "4.14": {
+        "login": {**login, **login_4_11},
+        "page": {**page_nav, **page_nav_4_10},
+        "generic": generic_locators,
+        "add_capacity": {**add_capacity, **add_capacity_4_11, **add_capacity_4_12},
+        "deployment": {
+            **deployment,
+            **deployment_4_7,
+            **deployment_4_9,
+            **deployment_4_10,
+            **deployment_4_11,
+            **deployment_4_12,
+        },
+        "validation": {
+            **validation,
+            **validation_4_8,
+            **validation_4_9,
+            **validation_4_10,
+            **validation_4_11,
+        },
+        "obc": obc,
+        "pvc": {**pvc, **pvc_4_7, **pvc_4_8, **pvc_4_9, **pvc_4_12},
+        "acm_page": {
+            **acm_page_nav,
+            **acm_configuration,
+            **acm_configuration_4_11,
+            **acm_configuration_4_12,
+            **acm_configuration_4_13,
+        },
+        "block_pool": {**block_pool, **block_pool_4_12, **block_pool_4_13},
+        "storageclass": {**storageclass, **storageclass_4_9},
+        "bucketclass": bucketclass,
+    },
     "4.13": {
         "login": {**login, **login_4_11},
         "page": {**page_nav, **page_nav_4_10},
