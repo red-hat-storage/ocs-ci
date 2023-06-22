2171fdb7e303906f16522cebff82f09947a5a30a
Daniel Osypenko <dosypenk@redhat.com>
2023-06-22 17:35:24 +0300

    test_ceph_osd_low_ops_alert-test
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -920,6 +920,7 @@ ALERT_CLUSTERCRITICALLYFULL = "CephClusterCriticallyFull"
 ALERT_CLUSTEROBJECTSTORESTATE = "ClusterObjectStoreState"
 ALERT_KUBEHPAREPLICASMISMATCH = "KubeHpaReplicasMismatch"
 ALERT_KUBEPERSISTENTVOLUMEINODESFILLINGUP = "KubePersistentVolumeInodesFillingUp"
+ALERT_CEPHOSDSLOWOPS = "CephOSDSlowOps"
 
 # OCS Deployment related constants
 OPERATOR_NODE_LABEL = "cluster.ocs.openshift.io/openshift-storage=''"
