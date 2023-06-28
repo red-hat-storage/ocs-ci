bc6b11f728f5674b0471a748ce8083d2ffef123f
vavuthu <vavuthu@redhat.com>
2023-06-28 08:09:15 +0530

    Add squad for disaster recovery
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -1789,7 +1789,7 @@ SQUADS = {
     "Orange": ["/scale/"],
     "Black": ["/ui/"],
     "Yellow": ["/managed-service/"],
-    "Turquoise": ["/regional-dr/"],
+    "Turquoise": ["/disaster-recovery/"],
 }
 
 PRODUCTION_JOBS_PREFIX = ["jnk"]
