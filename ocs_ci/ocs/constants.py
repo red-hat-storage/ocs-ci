07f4f7dc76ae1087415cd3a092e48696071aaaa4
vavuthu <vavuthu@redhat.com>
2023-07-04 11:59:40 +0530

    disconnected packages for ODF 4.14 deployment
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -1567,9 +1567,13 @@ DISCON_CL_REQUIRED_PACKAGES_PER_ODF_VERSION = {
         "odf-operator",
     ],
 }
-# the list of packages for 4.13 seems to be the same as 4.12
+# the list of packages for 4.13 and 4.14 seems to be the same as 4.12
 DISCON_CL_REQUIRED_PACKAGES_PER_ODF_VERSION[
     "4.13"
+] = DISCON_CL_REQUIRED_PACKAGES_PER_ODF_VERSION["4.12"]
+
+DISCON_CL_REQUIRED_PACKAGES_PER_ODF_VERSION[
+    "4.14"
 ] = DISCON_CL_REQUIRED_PACKAGES_PER_ODF_VERSION["4.12"]
 
 
