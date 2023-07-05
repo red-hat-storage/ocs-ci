ce483c7c8a12976bc270edd7f4e3e1d6618572df
Akarsha-rai <akrai@redhat.com>
2023-07-05 09:40:42 +0530

    Assign squad marker 'turquoise' to MDR tests
    Signed-off-by: Akarsha-rai <akrai@redhat.com>

--- tests/disaster-recovery/mdr/test_app_failover_and_relocate.py
+++ tests/disaster-recovery/mdr/test_app_failover_and_relocate.py
@@ -3,7 +3,7 @@ import pytest
 import time
 
 
-from ocs_ci.framework.pytest_customization.marks import tier1
+from ocs_ci.framework.pytest_customization.marks import tier1, turquoise_squad
 from ocs_ci.framework import config
 from ocs_ci.ocs.acm.acm import AcmAddClusters
 from ocs_ci.ocs.dr.dr_workload import validate_data_integrity
@@ -38,6 +38,7 @@ polarion_id_primary_down = "OCS-4346"
 
 
 @tier1
+@turquoise_squad
 class TestApplicationFailoverAndRelocate:
     """
     Test Failover and Relocate actions for application
