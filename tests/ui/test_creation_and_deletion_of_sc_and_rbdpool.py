ad4621b95972246a8d2eabf3d8313a80f500edb6
Shivam Durgbuns <shivamdurgbuns@gmail.com>
2023-07-06 10:37:05 +0530

    Added new test to check the number of storage class in UI (#7935)
    * Added new test to check the number of storage class in UI
    Testing that two storage class exists in blockpool UI when two storage class are created using one rbd pool
    OCS QE -3890 : https://polarion.engineering.redhat.com/polarion/redirect/project/OpenShiftContainerStorage/workitem/question_mark/id=OCS-3890
    
    Testing that two storage class exists in blockpool UI when two storage class are created using one rbd pool
    
    Signed-off-by: Shivam Durgbuns <sdurgbun@redhat.com>

--- tests/ui/test_creation_and_deletion_of_sc_and_rbdpool.py
+++ tests/ui/test_creation_and_deletion_of_sc_and_rbdpool.py
@@ -9,12 +9,14 @@ from ocs_ci.framework.pytest_customization.marks import (
     skipif_external_mode,
     skipif_ocs_version,
     ui,
+    skipif_ibm_cloud_managed,
 )
 from ocs_ci.ocs.cluster import (
     get_percent_used_capacity,
     validate_compression,
     validate_replica_data,
 )
+from ocs_ci.ocs.constants import CEPHBLOCKPOOL
 
 log = logging.getLogger(__name__)
 
@@ -23,7 +25,8 @@ log = logging.getLogger(__name__)
 @tier1
 @skipif_external_mode
 @skipif_ocs_version("<4.9")
-class TestCreateNewScWithNeWRbDPool(ManageTest):
+@skipif_ibm_cloud_managed
+class TestRbDPool(ManageTest):
     """
     Create a new  Storage Class on a new rbd pool with
     different replica and compression options
@@ -143,3 +146,51 @@ class TestCreateNewScWithNeWRbDPool(ManageTest):
             ),
         }
         assert all(checks.values())
+
+    @pytest.mark.polarion_id("OCS-3890")
+    def test_multiple_sc_one_pool(
+        self,
+        setup_ui_class,
+        ceph_pool_factory,
+        storageclass_factory,
+    ):
+        """
+        This test function does below,
+        *. Creates 2 Storage Class with creating one rbd pool for both
+        *. Verify the UI for storage class and rbd
+        """
+
+        log.info("Creating new pool with replica2 and compression")
+        pool_obj = ceph_pool_factory(
+            interface=CEPHBLOCKPOOL,
+            compression="aggressive",
+        )
+
+        log.info(f"Creating first storageclass with pool {pool_obj.name}")
+        sc_obj1 = storageclass_factory(
+            interface=CEPHBLOCKPOOL,
+            new_rbd_pool=False,
+            pool_name=pool_obj.name,
+        )
+
+        log.info(f"Creating second storageclass with pool {pool_obj.name}")
+        sc_obj2 = storageclass_factory(
+            interface=CEPHBLOCKPOOL,
+            new_rbd_pool=False,
+            pool_name=pool_obj.name,
+        )
+
+        sc_obj_list = [sc_obj1, sc_obj2]
+
+        # Check if 2 storage class exists in the pool page
+        blockpool_ui_obj = BlockPoolUI()
+        assert blockpool_ui_obj.check_pool_existence(pool_obj.name)
+        assert (
+            blockpool_ui_obj.check_storage_class_attached(pool_obj.name) == 2
+        ), "The Storage class didnot matched."
+
+        # Check storage class existence in UI
+        storageclass_ui_obj = StorageClassUI()
+        for storageclass in sc_obj_list:
+            storageclass_name = storageclass.name
+            assert storageclass_ui_obj.verify_storageclass_existence(storageclass_name)
