76723fdde53e37fa2b686fd5621c28efa4d03ced
Mahesh Shetty <mashetty@redhat.com>
2023-07-13 10:05:41 +0530

    Test KCS: Disabling Multicloud Object Gateway external service and make sure disableLoadBalancerService isn't reconciled (#7979)
    * test KCS: Disabling Multicloud Object Gateway external service and make sure disableLoadBalancerService isn't reconciled
    
    Signed-off-by: Mahesh Shetty <mashetty@li-0ad238cc-3463-11b2-a85c-f1085c3fd17e.ibm.com>
    Co-authored-by: Mahesh Shetty <mashetty@li-0ad238cc-3463-11b2-a85c-f1085c3fd17e.ibm.com>

---
+++ tests/e2e/kcs/test_disable_mcg_external_service.py
@@ -0,0 +1,91 @@
+import pytest
+import logging
+
+from ocs_ci.utility.utils import TimeoutSampler
+from ocs_ci.ocs.ocp import OCP
+from ocs_ci.ocs import constants
+from ocs_ci.framework.pytest_customization.marks import (
+    tier2,
+    polarion_id,
+    bugzilla,
+    skipif_external_mode,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class TestDisableMCGExternalService:
+    @pytest.fixture()
+    def patch_noobaa_object(self, request):
+
+        # get noobaa object
+        noobaa_ocp_obj = OCP(
+            kind="noobaa",
+            namespace=constants.OPENSHIFT_STORAGE_NAMESPACE,
+            resource_name="noobaa",
+        )
+
+        # patch noobaa object
+        noobaa_ocp_obj.patch(
+            resource_name="noobaa",
+            params='{"spec": {"disableLoadBalancerService": true }}',
+            format_type="merge",
+        )
+
+        # scale up noobaa endpoints
+        noobaa_ocp_obj.patch(
+            resource_name="noobaa",
+            params='{"spec": {"multiCloudGateway": {"endpoints": {"minCount": 2,"maxCount": 4}}}}',
+            format_type="merge",
+        )
+
+        def finalizer():
+            noobaa_ocp_obj.patch(
+                resource_name="noobaa",
+                params='{"spec": {"disableLoadBalancerService": false }}',
+                format_type="merge",
+            )
+            noobaa_ocp_obj.patch(
+                resource_name="noobaa",
+                params='{"spec": {"multiCloudGateway": {"endpoints": {"minCount": 1,"maxCount": 2}}}}',
+                format_type="merge",
+            )
+
+        request.addfinalizer(finalizer)
+        return noobaa_ocp_obj
+
+    @tier2
+    @bugzilla("2186171")
+    @polarion_id("OCS-4932")
+    @skipif_external_mode
+    def test_disable_mcg_external_service(self, patch_noobaa_object):
+        """
+        Test KCS https://access.redhat.com/articles/6970745
+        Make sure disableLoadBalancerService is not reconciled and verify it works as expected
+        """
+        # verify disableLoadBalancerService is reconciled
+        assert (
+            str(patch_noobaa_object.get()["spec"]["disableLoadBalancerService"])
+            == "True"
+        ), "disableLoadBalancerService is reconciled back to false"
+
+        # verify that services are now Cluster IP
+        service_obj = OCP(
+            kind="service", namespace=constants.OPENSHIFT_STORAGE_NAMESPACE
+        )
+        services = ["s3", "sts"]
+
+        def check_svc_type():
+            for svc in services:
+                if (
+                    str(service_obj.get(resource_name=svc)["spec"]["type"])
+                    != "ClusterIP"
+                ):
+                    return False
+            return True
+
+        sample = TimeoutSampler(timeout=60, sleep=10, func=check_svc_type)
+        assert sample.wait_for_func_status(
+            result=True
+        ), f"Services {services} isn't switched to ClusterIP service"
+        logger.info(f"Services {services} switched to ClusterIP")
