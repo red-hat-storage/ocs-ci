4e6b8c28c640af971b4314a4333403faa526a8f9
Shrivaibavi Raghaventhiran <30822772+Shrivaibavi@users.noreply.github.com>
2023-07-25 22:28:36 +0530

    Cherry pick PR 7862-8018-release-4.12 (#8067)
    Signed-off-by: Shrivaibavi Raghaventhiran <sraghave@redhat.com>

--- tests/ecosystem/upgrade/test_logging_upgrade.py
+++ tests/ecosystem/upgrade/test_logging_upgrade.py
@@ -11,7 +11,7 @@ from ocs_ci.utility.deployment_openshift_logging import (
 from ocs_ci.ocs.exceptions import CommandFailed
 from ocs_ci.framework import config
 from ocs_ci.ocs.resources.packagemanifest import PackageManifest
-from ocs_ci.framework.pytest_customization.marks import post_ocp_upgrade, magenta_squad
+from ocs_ci.framework.pytest_customization.marks import magenta_squad
 from ocs_ci.ocs.resources.pod import get_all_pods
 from ocs_ci.ocs import constants, ocp
 from ocs_ci.utility.retry import retry
@@ -148,7 +148,6 @@ def upgrade_info(channel):
 @pytest.mark.skip(
     reason="Skip due to issue https://github.com/red-hat-storage/ocs-ci/issues/6610"
 )
-@post_ocp_upgrade
 @magenta_squad
 @pytest.mark.usefixtures(install_logging.__name__)
 @pytest.mark.polarion_id("OCS-2201")
