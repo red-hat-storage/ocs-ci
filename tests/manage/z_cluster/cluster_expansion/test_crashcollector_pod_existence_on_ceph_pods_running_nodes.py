644d7e833c175373822176c6bfc58b8c80f6656b
ikave <ikave@li-096b72cc-220d-11b2-a85c-86cd9212f92b.ibm.com>
2023-08-27 17:17:26 +0300

    Move the method 'is_node_rack_or_zone_exist' to the 'ocs_ci/ocs/node.py' file
    Signed-off-by: ikave <ikave@li-096b72cc-220d-11b2-a85c-86cd9212f92b.ibm.com>

--- tests/manage/z_cluster/cluster_expansion/test_crashcollector_pod_existence_on_ceph_pods_running_nodes.py
+++ tests/manage/z_cluster/cluster_expansion/test_crashcollector_pod_existence_on_ceph_pods_running_nodes.py
@@ -5,7 +5,7 @@ from ocs_ci.framework.pytest_customization.marks import (
     skipif_managed_service,
     skipif_bm,
 )
-from ocs_ci.ocs.node import drain_nodes, schedule_nodes
+from ocs_ci.ocs.node import drain_nodes, schedule_nodes, is_node_rack_or_zone_exist
 from ocs_ci.helpers.helpers import get_failure_domin
 from ocs_ci.framework import config
 from ocs_ci.ocs import constants
@@ -53,21 +53,6 @@ class TestAddNodeCrashCollector(ManageTest):
 
     """
 
-    def is_node_rack_or_zone_exist(self, failure_domain, node_name):
-        """
-        Check if the node rack/zone exist
-
-        Args:
-            failure_domain (str): The failure domain
-            node_name (str): The node name
-
-        Returns:
-            bool: True if the node rack/zone exist. False otherwise
-
-        """
-        node_obj = get_node_objs([node_name])[0]
-        return get_node_rack_or_zone(failure_domain, node_obj) is not None
-
     def test_crashcollector_pod_existence_on_ceph_pods_running_nodes(
         self, add_nodes, node_drain_teardown
     ):
@@ -96,7 +81,7 @@ class TestAddNodeCrashCollector(ManageTest):
         sample = TimeoutSampler(
             timeout=timeout,
             sleep=10,
-            func=self.is_node_rack_or_zone_exist,
+            func=is_node_rack_or_zone_exist,
             node_name=new_node_name,
             failure_domain=failure_domain,
         )
