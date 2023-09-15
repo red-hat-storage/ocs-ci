3b3454bac68f044c4610df04a0840cf9d35db9cf
OdedViner <61982127+OdedViner@users.noreply.github.com>
2023-09-15 08:44:09 +0300

    UI Deployment, add a new step 'Data Protection' (#8449)
    * UI Deployment, add a new step 'Data Protection'
    
    Signed-off-by: oviner <oviner@redhat.com>

--- ocs_ci/ocs/ui/deployment_ui.py
+++ ocs_ci/ocs/ui/deployment_ui.py
@@ -258,6 +258,8 @@ class DeploymentUI(PageNavigator):
 
         self.configure_encryption()
 
+        self.configure_data_protection()
+
         self.create_storage_cluster()
 
     def install_internal_cluster(self):
@@ -304,6 +306,8 @@ class DeploymentUI(PageNavigator):
             self.configure_in_transit_encryption()
             self.configure_encryption()
 
+        self.configure_data_protection()
+
         self.create_storage_cluster()
 
     def create_storage_cluster(self):
@@ -339,6 +343,14 @@ class DeploymentUI(PageNavigator):
                 status=True, locator=self.dep_loc["wide_encryption"]
             )
         self.do_click(self.dep_loc["next"], enable_screenshot=True)
+
+    def configure_data_protection(self):
+        """
+        Configure Data Protection
+
+        """
+        if self.ocs_version_semantic >= version.VERSION_4_14:
+            self.do_click(self.dep_loc["next"], enable_screenshot=True)
 
     def enable_taint_nodes(self):
         """
