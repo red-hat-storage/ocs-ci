2c721495e04d1c971f74d0516f7d77860d0f202c
Itzhak Kave <ikave@ibm.com>
2023-09-20 18:14:13 +0300

    Add log info of the pod Yaml
    Signed-off-by: Itzhak Kave <ikave@ibm.com>

--- tests/manage/pv_services/test_cephfs_pvc_with_necessary_scc_permissions.py
+++ tests/manage/pv_services/test_cephfs_pvc_with_necessary_scc_permissions.py
@@ -23,6 +23,8 @@ def validate_permissions(pod_obj):
         f"output of command 'ls -l /etc/healing-controller.d/' "
         f"on the pod {pod_obj.name}: {cmd_output}"
     )
+    log.info("Pod Yaml:")
+    log.info(pod_obj.describe())
     cmd_output = cmd_output.split()
     assert "root" in cmd_output[4] and cmd_output[13], "Owner is not set to root "
     assert "9999" in cmd_output[5] and cmd_output[14], "Owner group is not set to 9999"
