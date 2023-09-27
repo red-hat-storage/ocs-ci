25eaff6ec5bbd4e9feac9f0f208acad27ba71d7c
Sagi Hirshfeld <shirshfe@redhat.com>
2023-09-27 11:16:36 +0300

    Resolve merge-conflict (#8281)
    Signed-off-by: Sagi Hirshfeld <shirshfe@redhat.com>

--- ocs_ci/ocs/resources/mcg.py
+++ ocs_ci/ocs/resources/mcg.py
@@ -8,7 +8,6 @@ import tempfile
 from time import sleep
 
 import boto3
-import requests
 from botocore.client import ClientError
 
 from ocs_ci.framework import config
@@ -336,18 +335,20 @@ class MCG:
             The server's response
 
         """
-        logger.info(f"Sending MCG RPC query:\n{api} {method} {params}")
-        payload = {
-            "api": api,
-            "method": method,
-            "params": params,
-            "auth_token": self.noobaa_token,
-        }
-        return requests.post(
-            url=self.mgmt_endpoint,
-            data=json.dumps(payload),
-            verify=retrieve_verification_mode(),
+
+        logger.info(f"Sending MCG RPC query via mcg-cli:\n{api} {method} {params}")
+
+        cli_output = self.exec_mcg_cmd(
+            f"api {api} {method} '{json.dumps(params)}' -ojson"
         )
+
+        # This class is needed to add a json method to the response dict
+        # which is needed to support existing usage
+        class CLIResponseDict(dict):
+            def json(self):
+                return self
+
+        return CLIResponseDict({"reply": json.loads(cli_output.stdout)})
 
     def check_data_reduction(self, bucketname, expected_reduction_in_bytes):
         """
