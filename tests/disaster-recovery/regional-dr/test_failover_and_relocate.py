aeff12a1da2915c78f5d5a571bed0e43a5388482
Jilju Joy <jijoy@redhat.com>
2023-10-04 22:26:58 +0530

    Test case OCS-4761
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/disaster-recovery/regional-dr/test_failover_and_relocate.py
+++ tests/disaster-recovery/regional-dr/test_failover_and_relocate.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from datetime import datetime
 from time import sleep
 
 import pytest
@@ -15,9 +17,11 @@ from ocs_ci.helpers.dr_helpers_ui import (
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.acm.acm import AcmAddClusters
 from ocs_ci.ocs.node import wait_for_nodes_status, get_node_objs
+from ocs_ci.ocs.resources.drpc import DRPC
 from ocs_ci.ocs.resources.pod import wait_for_pods_to_be_running
+from ocs_ci.ocs.utils import get_active_acm_index
 from ocs_ci.utility import version
-from ocs_ci.utility.utils import ceph_health_check
+from ocs_ci.utility.utils import ceph_health_check, TimeoutSampler
 
 logger = logging.getLogger(__name__)
 
@@ -101,8 +105,13 @@ class TestFailoverAndRelocate:
         acm_obj = AcmAddClusters()
         if workload_type == constants.SUBSCRIPTION:
             rdr_workload = dr_workload(num_of_subscription=1)[0]
+            drpc_obj = DRPC(namespace=rdr_workload.workload_namespace)
         else:
             rdr_workload = dr_workload(num_of_subscription=0, num_of_appset=1)[0]
+            drpc_obj = DRPC(
+                namespace=constants.GITOPS_CLUSTER_NAMESPACE,
+                resource_name=f"{rdr_workload.appset_placement_name}-drpc",
+            )
 
         primary_cluster_name = dr_helpers.get_current_primary_cluster_name(
             rdr_workload.workload_namespace, workload_type
@@ -120,6 +129,37 @@ class TestFailoverAndRelocate:
         wait_time = 2 * scheduling_interval  # Time in minutes
         logger.info(f"Waiting for {wait_time} minutes to run IOs")
         sleep(wait_time * 60)
+
+        # Set cluster_kubeconfig value for 'drpc_obj' object to fetch the details without switching the cluster context
+        acm_cluster_kubeconfig = os.path.join(
+            config.clusters[get_active_acm_index()].ENV_DATA["cluster_path"],
+            config.clusters[get_active_acm_index()].RUN.get("kubeconfig_location"),
+        )
+        drpc_obj.cluster_kubeconfig = acm_cluster_kubeconfig
+
+        # Get lastGroupSyncTime before failover
+        drpc_data = drpc_obj.get()
+        last_group_sync_time = drpc_data.get("status").get("lastGroupSyncTime")
+        logger.info(
+            f"The value of lastGroupSyncTime before failover is {last_group_sync_time}."
+        )
+
+        # Verify lastGroupSyncTime before failover
+        time_format = "%Y-%m-%dT%H:%M:%SZ"
+        last_group_sync_time = datetime.strptime(last_group_sync_time, time_format)
+        current_time = datetime.strptime(
+            datetime.utcnow().strftime(time_format), time_format
+        )
+        time_since_last_sync = (
+            current_time - last_group_sync_time
+        ).total_seconds() / 60
+        logger.info(
+            f"Before failover - Time in minutes since the last sync {time_since_last_sync}"
+        )
+        assert (
+            time_since_last_sync < scheduling_interval
+        ), "Before failover - Time since last sync is greater than the scheduling interval."
+        logger.info("Verified lastGroupSyncTime before failover.")
 
         if config.RUN.get("rdr_failover_via_ui"):
             logger.info("Start the process of Failover from ACM UI")
@@ -201,6 +241,44 @@ class TestFailoverAndRelocate:
         logger.info(f"Waiting for {wait_time} minutes to run IOs")
         sleep(wait_time * 60)
 
+        # Get lastGroupSyncTime after failover
+        for drpc_data in TimeoutSampler(60, 3, drpc_obj.get):
+            post_failover_last_group_sync_time = drpc_data.get("status").get(
+                "lastGroupSyncTime"
+            )
+            if post_failover_last_group_sync_time:
+                logger.info("After failover - Obtained lastGroupSyncTime.")
+                if post_failover_last_group_sync_time != last_group_sync_time:
+                    logger.info(
+                        "After failover - Verified: lastGroupSyncTime after is different from initial value."
+                    )
+                    break
+            logger.info(
+                "lastGroupSyncTime is not available after failover or it is equal to the previous value. Retrying."
+            )
+        logger.info(
+            f"The value of lastGroupSyncTime after failover is {last_group_sync_time}."
+        )
+
+        # Verify lastGroupSyncTime after failover
+        time_format = "%Y-%m-%dT%H:%M:%SZ"
+        post_failover_last_group_sync_time = datetime.strptime(
+            post_failover_last_group_sync_time, time_format
+        )
+        current_time = datetime.strptime(
+            datetime.utcnow().strftime(time_format), time_format
+        )
+        time_since_last_sync = (
+            current_time - post_failover_last_group_sync_time
+        ).total_seconds() / 60
+        logger.info(
+            f"After failover - Time in minutes since the last sync is {time_since_last_sync}"
+        )
+        assert (
+            time_since_last_sync < scheduling_interval
+        ), "After failover - Time since last sync is greater than the scheduling interval."
+        logger.info("Verified lastGroupSyncTime after failover.")
+
         # Relocate action
         if config.RUN.get("rdr_relocate_via_ui"):
             logger.info("Start the process of Relocate from ACM UI")
@@ -249,3 +327,44 @@ class TestFailoverAndRelocate:
             )
 
         # TODO: Add data integrity checks
+
+        # Get lastGroupSyncTime after relocate
+        for drpc_data in TimeoutSampler(60, 3, drpc_obj.get):
+            post_relocate_last_group_sync_time = drpc_data.get("status").get(
+                "lastGroupSyncTime"
+            )
+            if post_relocate_last_group_sync_time:
+                logger.info("After relocate - Obtained lastGroupSyncTime.")
+                if (
+                    post_relocate_last_group_sync_time
+                    != post_failover_last_group_sync_time
+                ):
+                    logger.info(
+                        "After relocate - Verified: lastGroupSyncTime after is different from initial value."
+                    )
+                    break
+            logger.info(
+                "lastGroupSyncTime is not available after relocate or it is equal to the previous value. Retrying."
+            )
+        logger.info(
+            f"The value of lastGroupSyncTime after relocate is {post_relocate_last_group_sync_time}."
+        )
+
+        # Verify lastGroupSyncTime after relocate
+        time_format = "%Y-%m-%dT%H:%M:%SZ"
+        post_relocate_last_group_sync_time = datetime.strptime(
+            post_relocate_last_group_sync_time, time_format
+        )
+        current_time = datetime.strptime(
+            datetime.utcnow().strftime(time_format), time_format
+        )
+        time_since_last_sync = (
+            current_time - post_relocate_last_group_sync_time
+        ).total_seconds() / 60
+        logger.info(
+            f"After relocate - Time in minutes since the last sync is {time_since_last_sync}"
+        )
+        assert (
+            time_since_last_sync < scheduling_interval
+        ), "After relocate - Time since last sync is greater than the scheduling interval."
+        logger.info("Verified lastGroupSyncTime after relocate.")
