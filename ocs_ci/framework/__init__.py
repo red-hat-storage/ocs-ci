cdeecd9478ce6551a936beaccc2665dfaf9c6c80
suchita-g <sgatfane@redhat.com>
2023-11-15 11:02:04 +0530

    Fixing import error in init.py
    Signed-off-by: suchita-g <sgatfane@redhat.com>

--- ocs_ci/framework/__init__.py
+++ ocs_ci/framework/__init__.py
@@ -13,7 +13,6 @@ import yaml
 import logging
 from collections.abc import Mapping
 from dataclasses import dataclass, field, fields
-from ocs_ci.ocs.constants import HCI_CLIENT, HCI_PROVIDER
 from ocs_ci.ocs.exceptions import ClusterNotFoundException
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -385,7 +384,7 @@ class MultiClusterConfig:
 
         """
         cluster_types = [cluster.ENV_DATA["cluster_type"] for cluster in self.clusters]
-        return HCI_CLIENT in cluster_types
+        return "hci_client" in cluster_types
 
     def hci_provider_exist(self):
         """
@@ -396,7 +395,7 @@ class MultiClusterConfig:
 
         """
         cluster_types = [cluster.ENV_DATA["cluster_type"] for cluster in self.clusters]
-        return HCI_PROVIDER in cluster_types
+        return "provider" in cluster_types
 
     def is_cluster_type_exist(self, cluster_type):
         """
