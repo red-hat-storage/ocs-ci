60dde0ed5d404b2e27a11c7e132c2d20d5c08f0a
Sagi Hirshfeld <shirshfe@redhat.com>
2023-12-03 19:58:13 +0200

    Use logging.getLogger for the libtest
    Signed-off-by: Sagi Hirshfeld <shirshfe@redhat.com>

--- tests/libtest/test_awscli_leftovers.py
+++ tests/libtest/test_awscli_leftovers.py
@@ -4,6 +4,8 @@ import logging
 from ocs_ci.framework.testlib import libtest
 from tests.conftest import awscli_pod_fixture
 
+logger = logging.getLogger(__name__)
+
 
 @pytest.fixture(scope="function")
 def awscli_pod_function(request):
@@ -11,11 +13,11 @@ def awscli_pod_function(request):
 
 
 @libtest
-def test_set_up_session_awscli_pod(awscli_pod_session):
-    logging.info("Setting up awscli resources leftovers for the next test")
+def test_set_up_session_scope_awscli_pod(awscli_pod_session):
+    logger.info("Setting up awscli resources leftovers for the next test")
 
 
 @libtest
-def test_set_up_function_awscli_pod(awscli_pod_function):
+def test_set_up_function_scope_awscli_pod(awscli_pod_function):
     assert awscli_pod_function
-    logging.info("The awscli_pod_fixture has succeeded despite the leftovers")
+    logger.info("The awscli_pod_fixture has succeeded despite the leftovers")
