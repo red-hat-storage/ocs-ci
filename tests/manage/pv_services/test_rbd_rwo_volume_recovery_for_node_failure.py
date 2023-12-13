842368f70f8f9ae5bae06eaf39029d08d6751f47
pintojoy <jopinto@redhat.com>
2023-12-13 18:58:54 +0530

    Fixing tox issues
    Signed-off-by: pintojoy <jopinto@redhat.com>

--- tests/manage/pv_services/test_rbd_rwo_volume_recovery_for_node_failure.py
+++ tests/manage/pv_services/test_rbd_rwo_volume_recovery_for_node_failure.py
@@ -8,14 +8,19 @@ from ocs_ci.helpers import helpers
 from ocs_ci.ocs import constants
 from ocs_ci.utility import templating
 from ocs_ci.ocs.ocp import OCP
-from ocs_ci.framework.testlib import E2ETest
+from ocs_ci.framework.testlib import (
+    skipif_ocs_version,
+    ManageTest,
+    skipif_bm,
+    skipif_ibm_cloud,
+    skipif_managed_service,
+)
 from ocs_ci.ocs.resources.deployment import Deployment
 from ocs_ci.helpers.sanity_helpers import Sanity
 
-# from ocs_ci.ocs.exceptions import PodNotCreated, CommandFailed
 from ocs_ci.ocs.resources import pod as res_pod
 from ocs_ci.ocs.platform_nodes import PlatformNodesFactory
-from ocs_ci.utility.utils import run_cmd, update_container_with_mirrored_image
+from ocs_ci.utility.utils import run_cmd
 from ocs_ci.ocs.resources.pod import (
     Pod,
     get_pods_having_label,
@@ -24,22 +29,28 @@ from ocs_ci.ocs.resources.pod import (
     get_deployments_having_label,
 )
 from ocs_ci.framework.pytest_customization.marks import (
-    tier1,
-    bugzilla,
+    tier4b,
     polarion_id,
     brown_squad,
 )
 from ocs_ci.ocs.node import (
-    get_ocs_nodes,
     taint_nodes,
     wait_for_nodes_status,
+    label_nodes,
 )
 
 log = logging.getLogger(__name__)
 
 
 @brown_squad
-class TestRWOVolumeRecover(E2ETest):
+@tier4b
+@skipif_managed_service
+@skipif_ocs_version("<4.14")
+class TestRWOVolumeRecover(ManageTest):
+    """
+    RHSTOR-2500- Enable faster RWO recovery in case of node failures
+    """
+
     @pytest.fixture(autouse=True)
     def init_sanity(self):
         """
@@ -58,14 +69,24 @@ class TestRWOVolumeRecover(E2ETest):
         pod_factory,
     ):
         """
-        This is the setup for setting up the simple-app DeploymentConfig,
-        service-account and pvc
+        Identify the nodes and start DeploymentConfig based app pods using
+        PVC with ReadWriteOnce (RWO) access mode on selected nodes
+
+        Args:
+            service_account_factory : A fixture to create service account
+            teardown_factory : A fixture for teardown
+            project_factory: A fixture to create new project
+            pvc_factory: A fixture to create new PVCs
+            pod_factory: A fixture to create deploymentconfig pods
+
+        Returns:
+            deployment config, app pod object and pvc object
         """
 
         # create a project for simple-app deployment
         project = project_factory(project_name="test-project")
 
-        # create pvc
+        # create a pvc
         pvc = pvc_factory(
             project=project,
             access_mode=constants.ACCESS_MODE_RWO,
@@ -86,10 +107,8 @@ class TestRWOVolumeRecover(E2ETest):
             "persistentVolumeClaim"
         ]["claimName"] = pvc.name
 
-        # check this line
-        update_container_with_mirrored_image(simple_app_data)
-
         simple_app_dc = helpers.create_resource(**simple_app_data)
+
         teardown_factory(simple_app_dc)
 
         self.simple_app_dc_obj = Deployment(
@@ -108,24 +127,6 @@ class TestRWOVolumeRecover(E2ETest):
 
         return self.simple_app_dc_obj, self.simple_app_pod, pvc.backed_pv_obj
 
-    def get_app_pod_obj(self):
-        """
-        Get cephfs app pod
-
-        Returns:
-            object: app pod instance
-
-        """
-        pod_obj_list = res_pod.get_all_pods(
-            namespace=config.ENV_DATA["cluster_namespace"],
-            selector=[self.pod_selector],
-            selector_label=constants.DEPLOYMENTCONFIG,
-        )
-        pod_name = self.pod_selector + "-1-deploy"
-        for pod_obj in pod_obj_list:
-            if pod_name not in pod_obj.name:
-                return pod_obj
-
     def get_fence_status(self):
         """
 
@@ -135,7 +136,8 @@ class TestRWOVolumeRecover(E2ETest):
         log.info("Verifying network fence is created")
         out = run_cmd("oc get networkfences.csiaddons.openshift.io -o json")
         nf_json = json.loads(out)
-        log.info(f"nf_json{nf_json}")
+
+        # Veify that items key exist and is not empty, if yes display the fence state
         if "items" in nf_json and nf_json["items"]:
             if (
                 "spec" in nf_json["items"][0]
@@ -165,7 +167,7 @@ class TestRWOVolumeRecover(E2ETest):
     def set_configmap_watch_for_node_failure_rook_ceph_operator(self, configmap_value):
         """
 
-        Set ROOK_LOG_LEVEL on configmap of rook-ceph-operator
+        Set ROOK_WATCH_FOR_NODE_FAILURE on configmap of rook-ceph-operator
 
         """
         configmap_obj = OCP(
@@ -177,15 +179,186 @@ class TestRWOVolumeRecover(E2ETest):
         params = f'{{"data": {{"ROOK_WATCH_FOR_NODE_FAILURE": "{configmap_value}"}}}}'
         configmap_obj.patch(params=params, format_type="merge")
 
+    @skipif_bm
+    @skipif_ibm_cloud
+    @polarion_id("OCS-5284")
     def test_rwo_volume_recovery_post_node_failure(self):
         """
-        Apply selinux relabeling solution on existing PV.
-
-        Args:
-            pvc_obj(PVC object): ocs_ci.ocs.resources.pvc.PVC instance kind.
-
+        OCS-5284/OCS-5286/OCS-5288:
+        - Start DeploymentConfig based app pods on 1 node
+        - Taint the node (where app pods are running) with label nodeshutdown:NoExecute
+        - Turn off the node
+        - Verify that networkfence is created and cidr ip is added in osd blocklist command
+        - Verify that app pod is running on new node
+        - Remove the taint from the node and power on the node
+        - verify that networkfence and cidr ip is removed post taint removal
         """
         nodes = PlatformNodesFactory().get_nodes_platform()
+        node = get_pod_node(self.simple_app_pod)
+        log.info(f"{self.simple_app_pod.name} pod is scheduled on node {node}")
+        log.info(f"Selected node is '{node.name}'")
+
+        log.info(f"Shutting down node '{node.name}'")
+        nodes.stop_nodes([node])
+        wait_for_nodes_status(node_names=[node.name], status=constants.NODE_NOT_READY)
+        log.info(f"The node '{node.name}' reached '{constants.NODE_NOT_READY}' status")
+
+        log.info("Taint node with nodeshutdown:NoExecute")
+        taint_nodes(
+            nodes=[node.name],
+            taint_label="node.kubernetes.io/out-of-service=nodeshutdown:NoExecute",
+        )
+
+        time.sleep(600)
+
+        # Get networkfence status
+        self.get_fence_status()
+        assert (
+            self.fence_state == "Fenced"
+        ), "Assertion failed: Fencing failed, fence_state is not 'Fenced'"
+
+        # Check that cidr ip is added in ceph osd blocklist command
+        self.get_cidr_ip_from_ceph_blocklist()
+        assert (
+            "range" in self.ceph_osd_blockslist_ls
+        ), "Assertion failed: CIDR ip is not added in osd blocklist"
+
+        time.sleep(540)
+
+        # Verify that app pod is scheduled on new node
+        node = get_pod_node(self.simple_app_pod).name
+        log.info(f"{self.simple_app_pod.name} pod is scheduled on node {node}")
+        nodes.start_nodes(nodes=[node])
+        wait_for_nodes_status(node_names=[node.name], timeout=600)
+        self.sanity_helpers.health_check(cluster_check=False, tries=60)
+
+        log.info("Checking storage pods status")
+        wait_for_pods_to_be_running(timeout=60)
+
+        log.info("Removing the taint from the node")
+        taint_nodes(
+            nodes=[node.name],
+            taint_label="node.kubernetes.io/out-of-service=nodeshutdown:NoExecute-",
+        )
+
+        log.info("Verifying networkfence status and CIDR ip removal status")
+
+        # Verify that network fence and cidr ips are removed post taint removal
+        self.get_fence_status()
+        assert (
+            self.fence_state != "Fenced"
+        ), "Assertion failed: Failed to remove the Network Fence"
+
+        self.get_cidr_ip_from_ceph_blocklist()
+        assert (
+            "range" not in self.ceph_osd_blockslist_ls
+        ), "Assertion failed: CIDR ip is still present in osd blocklist"
+
+    @skipif_bm
+    @skipif_ibm_cloud
+    @polarion_id("OCS-5285")
+    def test_rwo_volume_recovery_post_node_failure_with_configmap_false(self):
+        """
+        OCS-5285:
+        - Change the watch_for_node_failure value rook ceph operator config map to false
+        - Start DeploymentConfig based app pods on 1 node
+        - Taint the node (where app pods are running) with label nodeshutdown:NoExecute
+        - Turn off the node
+        - Verify that networkfence is not created and cidr ip is not added in osd blocklist command
+        - Verify that nodeloass recovery fails
+        - Remove the taint from the node and power on the node
+        - Change the watch_for_node_failure value rook ceph operator config map to true
+        """
+        log.info("Setting rook configmap value to FALSE")
+        self.set_configmap_watch_for_node_failure_rook_ceph_operator(
+            configmap_value=False
+        )
+
+        nodes = PlatformNodesFactory().get_nodes_platform()
+
+        # Get the node object where app pod is running
+        node = get_pod_node(self.simple_app_pod)
+        log.info(f"{self.simple_app_pod.name} pod is scheduled on node {node}")
+        log.info(f"Selected node is '{node.name}'")
+        log.info(f"Shutting down node '{node.name}'")
+        nodes.stop_nodes([node])
+        wait_for_nodes_status(node_names=[node.name], status=constants.NODE_NOT_READY)
+        log.info(f"The node '{node.name}' reached '{constants.NODE_NOT_READY}' status")
+
+        log.info("Taint node with nodeshutdown:NoExecute label")
+        taint_nodes(
+            nodes=[node.name],
+            taint_label="node.kubernetes.io/out-of-service=nodeshutdown:NoExecute",
+        )
+
+        time.sleep(600)
+
+        # Get networkfence status
+        self.get_fence_status()
+        assert self.fence_state != "Fenced", "Assertion failed: Network Fence exists"
+
+        # Check that cidr ip is added in ceph osd blocklist command
+        self.get_cidr_ip_from_ceph_blocklist()
+        assert (
+            "range" not in self.ceph_osd_blockslist_ls
+        ), "Assertion failed: CIDR ip is added in osd blocklist"
+
+        time.sleep(1200)
+
+        # start the node
+        nodes.start_nodes(nodes=[node])
+        wait_for_nodes_status(node_names=[node.name], timeout=600)
+
+        log.info("Removing the taint from the node")
+        taint_nodes(
+            nodes=[node.name],
+            taint_label="node.kubernetes.io/out-of-service=nodeshutdown:NoExecute-",
+        )
+
+        self.sanity_helpers.health_check(cluster_check=False, tries=60)
+        log.info("Checking storage pods status")
+        # Validate storage pods are running
+        wait_for_pods_to_be_running(timeout=60)
+
+        log.info("Setting rook configmap value to default")
+        self.set_configmap_watch_for_node_failure_rook_ceph_operator(
+            configmap_value=True
+        )
+
+    @skipif_bm
+    @skipif_ibm_cloud
+    @polarion_id("OCS-5287")
+    def test_rwo_volume_recovery_post_node_failure_with_rook_operator(self):
+        """
+        OCS-5287:
+        - Start DeploymentConfig based app pods on a node where rook operator pod is running
+        - Taint the node (where app pods are running) with label nodeshutdown:NoExecute
+        - Turn off the node
+        - Verify that networkfence is created and cidr ip is added in osd blocklist command
+        - Verify that app pod is running on new node
+        - Remove the taint from the node and power on the node
+        - verify that networkfence and cidr ip is removed post taint removal
+        """
+        nodes = PlatformNodesFactory().get_nodes_platform()
+        rook_operator_pods = res_pod.get_operator_pods()
+        rook_operator_pod = rook_operator_pods[0]
+
+        rook_operator_pod_name = rook_operator_pod.name
+        log.info(f"rook operator pod found: {rook_operator_pod_name}")
+
+        # Get the node name that has the rook operator pod running on
+        operator_node = res_pod.get_pod_node(rook_operator_pod)
+        operator_node_name = operator_node.get().get("metadata").get("name")
+        log.info(
+            f"{rook_operator_pod_name} pod is running on node {operator_node_name}"
+        )
+        for node1 in nodes:
+            if node1.name != operator_node_name:
+                label_nodes(nodes=node1, label="ssd")
+
+        pod_deleted = self.simple_app_pod.delete(wait=False)
+        assert pod_deleted, f"Failed to delete pod {self.simple_app_pod.name}"
+
         node = get_pod_node(self.simple_app_pod)
         log.info(f"{self.simple_app_pod.name} pod is scheduled on node {node}")
         log.info(f"Selected node is '{node.name}'")
@@ -247,81 +420,6 @@ class TestRWOVolumeRecover(E2ETest):
             "range" not in self.ceph_osd_blockslist_ls
         ), "Assertion failed: CIDR ip is still present in osd blocklist"
 
-    def test_rwo_volume_recovery_post_node_failure_with_configmap_false(self):
-        """
-        Apply selinux relabeling solution on existing PV.
-
-        Args:
-            pvc_obj(PVC object): ocs_ci.ocs.resources.pvc.PVC instance kind.
-
-        """
-        log.info("Setting rook configmap value to FALSE")
-        self.set_configmap_watch_for_node_failure_rook_ceph_operator(
-            configmap_value=False
-        )
-
-        nodes = PlatformNodesFactory().get_nodes_platform()
-
-        # Get the node object where app pod is running
-        node = get_pod_node(self.simple_app_pod)
-        log.info(f"{self.simple_app_pod.name} pod is scheduled on node {node}")
-        log.info(f"Selected node is '{node.name}'")
-        log.info(f"Shutting down node '{node.name}'")
-        nodes.stop_nodes([node])
-        wait_for_nodes_status(node_names=[node.name], status=constants.NODE_NOT_READY)
-        log.info(f"The node '{node.name}' reached '{constants.NODE_NOT_READY}' status")
-
-        log.info("Taint node with nodeshutdown:NoExecute label")
-        taint_nodes(
-            nodes=[node.name],
-            taint_label="node.kubernetes.io/out-of-service=nodeshutdown:NoExecute",
-        )
-
-        time.sleep(600)
-
-        self.get_fence_status()
-
-        assert (
-            self.fence_state != "Fenced"
-        ), "Assertion failed: Failed to remove the Network Fence"
-
-        """
-        log.info("Verifying network fence is not created")
-        out = run_cmd("oc get networkfences.csiaddons.openshift.io -o json")
-        nf_json = json.loads(out)
-
-        assert (
-            "items" not in nf_json or not nf_json["items"]
-        ), "Assertion failed: 'items' is present or not an empty list"
-        log.info(f"nf_json{nf_json}")
-        fence_state = nf_json["items"][0]["spec"]["fenceState"]
-        if(fence_state == "Fenced"):
-            log.error(f"Networkfence state is {fence_state}")
-        """
-        self.get_cidr_ip_from_ceph_blocklist()
-
-        assert (
-            "range" in self.ceph_osd_blockslist_ls
-        ), "Assertion failed: CIDR ip is not added in osd blocklist"
-
-        time.sleep(1200)
-        """
-        node = get_pod_node(self.simple_app_pod).name
-        log.info(f"{self.simple_app_pod.name} pod is scheduled on node {node}")
-        """
-        nodes.start_nodes(nodes=[node])
-        wait_for_nodes_status(node_names=[node.name], timeout=600)
-        log.info("Removing the taint from the node")
-        taint_nodes(
-            nodes=[node.name],
-            taint_label="node.kubernetes.io/out-of-service=nodeshutdown:NoExecute-",
-        )
-        self.sanity_helpers.health_check(cluster_check=False, tries=60)
-        log.info("Checking storage pods status")
-        # Validate storage pods are running
-        wait_for_pods_to_be_running(timeout=60)
-
-        log.info("Setting rook configmap value to default")
-        self.set_configmap_watch_for_node_failure_rook_ceph_operator(
-            configmap_value=True
-        )
+        for node1 in nodes:
+            if node1.name != operator_node_name:
+                label_nodes(nodes=node1, label="ssd-")
