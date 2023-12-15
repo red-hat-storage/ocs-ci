1e5056735799954d425fd350caa5034840a912cb
Shylesh Kumar Mohan <shmohan@redhat.com>
2023-12-15 17:00:43 +0000

    Add skipif_z_stream markers to DR hub and DR cluster operator upgrade tests
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- tests/functional/upgrade/test_upgrade.py
+++ tests/functional/upgrade/test_upgrade.py
@@ -2,18 +2,27 @@ import logging
 
 import pytest
 
-from ocs_ci.framework.pytest_customization.marks import purple_squad, multicluster_roles
+from ocs_ci.framework.pytest_customization.marks import (
+    purple_squad,
+    multicluster_roles,
+    skipif_z_stream_upgrade,
+)
 from ocs_ci.framework.testlib import (
     ocs_upgrade,
     polarion_id,
     mco_upgrade,
     dr_hub_upgrade,
+    dr_cluster_operator_upgrade,
     acm_upgrade,
 )
 from ocs_ci.ocs.acm_upgrade import ACMUpgrade
 from ocs_ci.ocs.disruptive_operations import worker_node_shutdown, osd_node_reboot
 from ocs_ci.ocs.ocs_upgrade import run_ocs_upgrade
-from ocs_ci.ocs.dr_upgrade import MultiClusterOrchestratorUpgrade, DRHubUpgrade
+from ocs_ci.ocs.dr_upgrade import (
+    DRClusterOperatorUpgrade,
+    MultiClusterOrchestratorUpgrade,
+    DRHubUpgrade,
+)
 from ocs_ci.utility.reporting import get_polarion_id
 
 log = logging.getLogger(__name__)
@@ -73,8 +82,8 @@ def test_osd_reboot(teardown, upgrade_stats):
 @purple_squad
 @ocs_upgrade
 @polarion_id(get_polarion_id(upgrade=True))
-@multicluster_roles(["mdr_all_odf"])
-def test_upgrade(upgrade_stats):
+@multicluster_roles(["mdr-all-odf"])
+def test_upgrade(zone_rank, role_rank, config_index, upgrade_stats):
     """
     Tests upgrade procedure of OCS cluster
 
@@ -85,8 +94,8 @@ def test_upgrade(upgrade_stats):
 
 @purple_squad
 @mco_upgrade
-@multicluster_roles(["mdr_all_acm"])
-def test_mco_upgrade():
+@multicluster_roles(["mdr-all-acm"])
+def test_mco_upgrade(zone_rank, role_rank, config_index):
     """
     Test upgrade procedure for multicluster orchestrator operator
 
@@ -97,8 +106,9 @@ def test_mco_upgrade():
 
 @purple_squad
 @dr_hub_upgrade
-@multicluster_roles(["mdr_all_acm"])
-def test_dr_hub_upgrade():
+@skipif_z_stream_upgrade
+@multicluster_roles(["mdr-all-acm"])
+def test_dr_hub_upgrade(zone_rank, role_rank, config_index):
     """
     Test upgrade procedure for DR hub operator
 
@@ -108,9 +118,22 @@ def test_dr_hub_upgrade():
 
 
 @purple_squad
+@dr_cluster_operator_upgrade
+@skipif_z_stream_upgrade
+@multicluster_roles(["mdr-all-odf"])
+def test_dr_cluster_upgrade(zone_rank, role_rank, config_index):
+    """
+    Test upgrade procedure for DR cluster operator
+
+    """
+    dr_cluster_upgrade_obj = DRClusterOperatorUpgrade()
+    dr_cluster_upgrade_obj.run_upgrade()
+
+
+@purple_squad
 @acm_upgrade
-@multicluster_roles(["mdr_all_acm"])
-def test_acm_upgrade():
+@multicluster_roles(["mdr-all-acm"])
+def test_acm_upgrade(zone_rank, role_rank, config_index):
     """
     Test upgrade procedure for ACM operator
 
