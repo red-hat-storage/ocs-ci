0369914decfb776cddd9e36b21d4370b04353198
Elena Bondarenko <ebondare@redhat.com>
2023-12-15 13:39:26 +0100

    Start re-structure of resource checks
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -2010,23 +2010,27 @@ def verify_consumer_resources():
 
     # Verify the default Storageclassclaims
     if ocs_version >= version.VERSION_4_11:
-        storage_class_claim = OCP(
-            kind=constants.STORAGECLASSCLAIM,
-            namespace=config.ENV_DATA["cluster_namespace"],
+        verify_storageclassclaims()
+
+
+def verify_storageclassclaims():
+    storage_class_claim = OCP(
+        kind=constants.STORAGECLASSCLAIM,
+        namespace=config.ENV_DATA["cluster_namespace"],
+    )
+    for sc_claim in [
+        constants.DEFAULT_STORAGECLASS_RBD,
+        constants.DEFAULT_STORAGECLASS_CEPHFS,
+    ]:
+        sc_claim_phase = storage_class_claim.get_resource(
+            resource_name=sc_claim, column="PHASE"
         )
-        for sc_claim in [
-            constants.DEFAULT_STORAGECLASS_RBD,
-            constants.DEFAULT_STORAGECLASS_CEPHFS,
-        ]:
-            sc_claim_phase = storage_class_claim.get_resource(
-                resource_name=sc_claim, column="PHASE"
-            )
-            assert sc_claim_phase == constants.STATUS_READY, (
-                f"The phase of the storageclassclaim {sc_claim} is {sc_claim_phase}. "
-                f"Expected phase is '{constants.STATUS_READY}'"
-            )
-            log.info(f"Storageclassclaim {sc_claim} is {constants.STATUS_READY}")
-        log.info("Verified the status of the default storageclassclaims")
+        assert sc_claim_phase == constants.STATUS_READY, (
+            f"The phase of the storageclassclaim {sc_claim} is {sc_claim_phase}. "
+            f"Expected phase is '{constants.STATUS_READY}'"
+        )
+        log.info(f"Storageclassclaim {sc_claim} is {constants.STATUS_READY}")
+    log.info("Verified the status of the default storageclassclaims")
 
 
 def verify_managed_service_networkpolicy():
