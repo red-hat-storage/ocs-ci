f4408c6e8610405df512e953c6a6ec0e14c83da7
oviner <oviner@redhat.com>
2023-12-20 10:53:57 +0200

    fix code
    Signed-off-by: oviner <oviner@redhat.com>

--- tests/functional/z_cluster/test_rook_ceph_osd_flapping.py
+++ tests/functional/z_cluster/test_rook_ceph_osd_flapping.py
@@ -1,5 +1,6 @@
 import logging
 import pytest
+import time
 
 
 from ocs_ci.utility.utils import TimeoutSampler
@@ -34,6 +35,7 @@ class TestRookCephOsdFlapping(ManageTest):
     @pytest.fixture(autouse=True)
     def teardown(self, request):
         def finalizer():
+            self.osd_pod_obj.delete()
             ceph_health_check()
 
         request.addfinalizer(finalizer)
@@ -43,20 +45,23 @@ class TestRookCephOsdFlapping(ManageTest):
         Test Process:
 
         1.Get osd id
-        2.Mark an osd down manually
-        3.Verify osd is down
+        2.Mark an osd down manually 6 times
+        3.Verify osd is down with "csph -s" command ["1 osds down"]
         4.Verify osd log contain "osd_max_markdown_count 5 in last" string
-        5.Scale up osd
+        5.Reset osd pod
         6.Verify ceph status is OK
         """
         log.info("Get One OSD ID")
         osd_pod_objs = pod.get_osd_pods()
-        self.osd_pod_id = pod.get_osd_pod_id(osd_pod_objs[0])
+        self.osd_pod_obj = osd_pod_objs[0]
+        osd_pod_id = pod.get_osd_pod_id(self.osd_pod_obj)
         ct_pod = pod.get_ceph_tools_pod()
-        log.info(f"Mark an osd {self.osd_pod_id} down manually")
-        ct_pod.exec_ceph_cmd(f"ceph osd down osd.{self.osd_pod_id}")
+        log.info(f"Mark an osd {osd_pod_id} down manually")
+        for _ in range(6):
+            time.sleep(5)
+            ct_pod.exec_ceph_cmd(f"ceph osd down osd.{osd_pod_id}")
 
-        log.info(f"Verify osd {self.osd_pod_id} is down")
+        log.info(f"Verify osd {osd_pod_id} is down")
         sample = TimeoutSampler(
             timeout=10,
             sleep=1,
@@ -68,24 +73,28 @@ class TestRookCephOsdFlapping(ManageTest):
             raise ValueError("OSD DEBUG Log does not exist")
 
         log.info("Verify osd logs")
+        expected_string = (
+            "The OSD pod will sleep for 24 hours. "
+            "Restart the pod manually once the flapping issue is fixed"
+        )
         osd_pod_log = pod.get_pod_logs(
-            pod_name=osd_pod_objs[0].name, all_containers=True
+            pod_name=self.osd_pod_obj.name, all_containers=True
         )
         assert (
-            "osd_max_markdown_count 5 in last" in osd_pod_log
-        ), "The expected log 'osd_max_markdown_count 5 in last' is not found in osd log"
+            expected_string in osd_pod_log
+        ), f"The expected log {expected_string} is not found in osd log"
 
-    def verify_output_ceph_tool_pod(self, command, expected_strings):
+    def verify_output_ceph_tool_pod(self, command, expected_string):
         """
 
         Args:
-            command:
-            expected_strings:
+            command (str): the command we run in ceph tool pod
+            expected_string (str): the expected string in the output
 
         Returns:
-
+            bool: True if the output contain the expected_string otherwise False
 
         """
         ct_pod = pod.get_ceph_tools_pod()
         output_ceph_command = ct_pod.exec_ceph_cmd(command)
-        return expected_strings in output_ceph_command
+        return expected_string in output_ceph_command
