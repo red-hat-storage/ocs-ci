6c9bb48da6f10588c4a404b05b1d7c31eec1a88a
Jilju Joy <jijoy@redhat.com>
2024-01-08 19:26:13 +0530

    skip cephcluster check for Provider-client
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/helpers/sanity_helpers.py
+++ ocs_ci/helpers/sanity_helpers.py
@@ -44,7 +44,11 @@ class Sanity:
         if not (
             config.ENV_DATA["mcg_only_deployment"]
             or (
-                config.ENV_DATA.get("platform") == constants.FUSIONAAS_PLATFORM
+                config.ENV_DATA.get("platform")
+                in [
+                    constants.FUSIONAAS_PLATFORM
+                    + constants.HCI_PROVIDER_CLIENT_PLATFORMS
+                ]
                 and config.ENV_DATA["cluster_type"].lower() == "consumer"
             )
         ):
