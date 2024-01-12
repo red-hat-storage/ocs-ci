54c19073a7164deb402a1c89a9523b7ae65f8917
Sidhant Agrawal <sagrawal@redhat.com>
2024-01-12 17:10:32 +0530

    Add tests to verify ApplicationSet-based application failover
    Signed-off-by: Sidhant Agrawal <sagrawal@redhat.com>

--- tests/functional/disaster-recovery/regional-dr/test_failover.py
+++ tests/functional/disaster-recovery/regional-dr/test_failover.py
@@ -42,27 +42,37 @@ class TestFailover:
     """
 
     @pytest.mark.parametrize(
-        argnames=["primary_cluster_down", "pvc_interface"],
+        argnames=["primary_cluster_down", "pvc_interface", "workload_type"],
         argvalues=[
             pytest.param(
-                *[False, constants.CEPHBLOCKPOOL],
+                *[False, constants.CEPHBLOCKPOOL, constants.SUBSCRIPTION],
                 marks=pytest.mark.polarion_id(polarion_id_primary_up),
-                id="primary_up",
+                id="primary_up-rbd-subscription",
             ),
             pytest.param(
-                *[True, constants.CEPHBLOCKPOOL],
+                *[True, constants.CEPHBLOCKPOOL, constants.SUBSCRIPTION],
                 marks=pytest.mark.polarion_id(polarion_id_primary_down),
-                id="primary_down",
+                id="primary_down-rbd-subscription",
             ),
             pytest.param(
-                *[False, constants.CEPHFILESYSTEM],
+                *[False, constants.CEPHBLOCKPOOL, constants.APPLICATION_SET],
+                marks=pytest.mark.polarion_id("OCS-5006"),
+                id="primary_up-rbd-appset",
+            ),
+            pytest.param(
+                *[True, constants.CEPHBLOCKPOOL, constants.APPLICATION_SET],
+                marks=pytest.mark.polarion_id("OCS-5008"),
+                id="primary_down-rbd-appset",
+            ),
+            pytest.param(
+                *[False, constants.CEPHFILESYSTEM, constants.SUBSCRIPTION],
                 marks=pytest.mark.polarion_id("OCS-4729"),
-                id="primary_up_cephfs",
+                id="primary_up-cephfs-subscription",
             ),
             pytest.param(
-                *[True, constants.CEPHFILESYSTEM],
+                *[True, constants.CEPHFILESYSTEM, constants.SUBSCRIPTION],
                 marks=pytest.mark.polarion_id("OCS-4726"),
-                id="primary_down_cephfs",
+                id="primary_down-cephfs-subscription",
             ),
         ],
     )
@@ -70,6 +80,7 @@ class TestFailover:
         self,
         primary_cluster_down,
         pvc_interface,
+        workload_type,
         setup_acm_ui,
         dr_workload,
         nodes_multicluster,
@@ -88,18 +99,23 @@ class TestFailover:
         if config.RUN.get("rdr_failover_via_ui"):
             acm_obj = AcmAddClusters()
 
-        rdr_workload = dr_workload(num_of_subscription=1, pvc_interface=pvc_interface)[
-            0
-        ]
+        if workload_type == constants.SUBSCRIPTION:
+            rdr_workload = dr_workload(
+                num_of_subscription=1, pvc_interface=pvc_interface
+            )[0]
+        else:
+            rdr_workload = dr_workload(
+                num_of_subscription=0, num_of_appset=1, pvc_interface=pvc_interface
+            )[0]
 
         primary_cluster_name = dr_helpers.get_current_primary_cluster_name(
-            rdr_workload.workload_namespace
+            rdr_workload.workload_namespace, workload_type
         )
         config.switch_to_cluster_by_name(primary_cluster_name)
         primary_cluster_index = config.cur_index
         primary_cluster_nodes = get_node_objs()
         secondary_cluster_name = dr_helpers.get_current_secondary_cluster_name(
-            rdr_workload.workload_namespace
+            rdr_workload.workload_namespace, workload_type
         )
 
         if pvc_interface == constants.CEPHFILESYSTEM:
@@ -111,7 +127,7 @@ class TestFailover:
             config.switch_to_cluster_by_name(primary_cluster_name)
 
         scheduling_interval = dr_helpers.get_scheduling_interval(
-            rdr_workload.workload_namespace
+            rdr_workload.workload_namespace, workload_type
         )
         wait_time = 2 * scheduling_interval  # Time in minutes
         logger.info(f"Waiting for {wait_time} minutes to run IOs")
@@ -149,7 +165,14 @@ class TestFailover:
             )
         else:
             # Failover action via CLI
-            dr_helpers.failover(secondary_cluster_name, rdr_workload.workload_namespace)
+            dr_helpers.failover(
+                secondary_cluster_name,
+                rdr_workload.workload_namespace,
+                workload_type,
+                rdr_workload.appset_placement_name
+                if workload_type != constants.SUBSCRIPTION
+                else None,
+            )
 
         # Verify resources creation on secondary cluster (failoverCluster)
         config.switch_to_cluster_by_name(secondary_cluster_name)
