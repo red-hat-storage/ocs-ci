d14870cc30139928ab3f867421cd1e63afffc7b3
Elena Bondarenko <ebondare@redhat.com>
2024-01-17 18:17:58 +0100

    Generate client token
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/ocs/ui/base_ui.py
+++ ocs_ci/ocs/ui/base_ui.py
@@ -147,6 +147,7 @@ class BaseUI:
             locators, self.ocp_version, "add_capacity"
         )
         self.topology_loc = self.deep_get(locators, self.ocp_version, "topology")
+        self.client_loc = self.deep_get(locators, self.ocp_version, "client")
 
     def __repr__(self):
         return f"{self.__class__.__name__} Web Page"
