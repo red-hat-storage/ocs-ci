9d255ed2e1397ec27b8a005a0b7e9fe35400a5b9
Parag Kamble <pakamble@redhat.com>
2024-01-21 16:27:38 +0530

    Replacing crypt device check from pod to node. (#9177)
    Signed-off-by: Parag Kamble <pakamble@redhat.com>

--- tests/functional/pv/pv_encryption/test_rbd_pv_encryption.py
+++ tests/functional/pv/pv_encryption/test_rbd_pv_encryption.py
@@ -23,6 +23,7 @@ from ocs_ci.ocs.exceptions import (
 )
 from ocs_ci.utility import kms
 from semantic_version import Version
+from ocs_ci.ocs.node import verify_crypt_device_present_onnode
 
 
 log = logging.getLogger(__name__)
@@ -163,12 +164,10 @@ class TestRbdPvEncryption(ManageTest):
 
         # Verify whether encrypted device is present inside the pod and run IO
         for vol_handle, pod_obj in zip(vol_handles, pod_objs):
-            if pod_obj.exec_sh_cmd_on_pod(
-                command=f"lsblk | grep {vol_handle} | grep crypt"
-            ):
-                log.info(f"Encrypted device found in {pod_obj.name}")
-            else:
-                log.error(f"Encrypted device not found in {pod_obj.name}")
+            node = pod_obj.get_node()
+            assert verify_crypt_device_present_onnode(
+                node, vol_handle
+            ), f"Crypt devicve {vol_handle} not found on node:{node}"
 
             pod_obj.run_io(
                 storage_type="block",
