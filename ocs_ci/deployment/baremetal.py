6cba84caefc3fe6547f80388b3a9152e6652534c
Daniel Horak <dahorak@redhat.com>
2024-01-26 14:38:51 +0100

    move BMBaseOCPDeployment directly to baremetal module
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

--- ocs_ci/deployment/baremetal.py
+++ ocs_ci/deployment/baremetal.py
@@ -48,97 +48,98 @@ class BAREMETALBASE(Deployment):
     def __init__(self):
         super().__init__()
 
-    class BMBaseOCPDeployment(BaseOCPDeployment):
-        def __init__(self):
-            super().__init__()
-            self.helper_node_details = config.AUTH["baremetal"]
-            self.mgmt_details = config.AUTH["ipmi"]
 
-        def deploy_prereq(self):
-            """
-            Pre-Requisites for Bare Metal deployments
-            """
-            # check for BM status
-            logger.info("Checking BM Status")
-            status = self.check_bm_status_exist()
-            if status == constants.BM_STATUS_PRESENT:
-                pytest.fail(
-                    f"BM Cluster still present and locked by {self.get_locked_username()}"
-                )
+class BMBaseOCPDeployment(BaseOCPDeployment):
+    def __init__(self):
+        super().__init__()
+        self.helper_node_details = config.AUTH["baremetal"]
+        self.mgmt_details = config.AUTH["ipmi"]
 
-            # update BM status
-            logger.info("Updating BM Status")
-            result = self.update_bm_status(constants.BM_STATUS_PRESENT)
-            assert (
-                result == constants.BM_STATUS_RESPONSE_UPDATED
-            ), "Failed to update request"
-
-        def check_bm_status_exist(self):
-            """
-            Check if BM Cluster already exist
-
-            Returns:
-                str: response status
-            """
-            headers = {"content-type": "application/json"}
-            response = requests.get(
-                url=self.helper_node_details["bm_status_check"], headers=headers
+    def deploy_prereq(self):
+        """
+        Pre-Requisites for Bare Metal deployments
+        """
+        # check for BM status
+        logger.info("Checking BM Status")
+        status = self.check_bm_status_exist()
+        if status == constants.BM_STATUS_PRESENT:
+            pytest.fail(
+                f"BM Cluster still present and locked by {self.get_locked_username()}"
             )
-            return response.json()[0]["status"]
 
-        def get_locked_username(self):
-            """
-            Get name of user who has locked baremetal resource
+        # update BM status
+        logger.info("Updating BM Status")
+        result = self.update_bm_status(constants.BM_STATUS_PRESENT)
+        assert (
+            result == constants.BM_STATUS_RESPONSE_UPDATED
+        ), "Failed to update request"
 
-            Returns:
-                str: username
-            """
-            headers = {"content-type": "application/json"}
-            response = requests.get(
-                url=self.helper_node_details["bm_status_check"], headers=headers
-            )
-            return response.json()[0]["user"]
+    def check_bm_status_exist(self):
+        """
+        Check if BM Cluster already exist
 
-        def update_bm_status(self, bm_status):
-            """
-            Update BM status when cluster is deployed/teardown
+        Returns:
+            str: response status
+        """
+        headers = {"content-type": "application/json"}
+        response = requests.get(
+            url=self.helper_node_details["bm_status_check"], headers=headers
+        )
+        return response.json()[0]["status"]
 
-            Args:
-                bm_status (str): Status to be updated
+    def get_locked_username(self):
+        """
+        Get name of user who has locked baremetal resource
 
-            Returns:
-                str: response message
-            """
-            if bm_status == constants.BM_STATUS_PRESENT:
-                now = datetime.today().strftime("%Y-%m-%d")
-                payload = {
-                    "status": bm_status,
-                    "cluster_name": config.ENV_DATA["cluster_name"],
-                    "creation_date": now,
-                }
-            else:
-                payload = {
-                    "status": bm_status,
-                    "cluster_name": "null",
-                    "creation_date": "null",
-                }
-            headers = {"content-type": "application/json"}
-            response = requests.put(
-                url=self.helper_node_details["bm_status_check"],
-                json=payload,
-                headers=headers,
-            )
-            return response.json()["message"]
+        Returns:
+            str: username
+        """
+        headers = {"content-type": "application/json"}
+        response = requests.get(
+            url=self.helper_node_details["bm_status_check"], headers=headers
+        )
+        return response.json()[0]["user"]
 
-        def destroy(self, log_level=""):
-            """
-            Destroy OCP cluster
-            """
-            logger.info("Updating BM status")
-            result = self.update_bm_status(constants.BM_STATUS_ABSENT)
-            assert (
-                result == constants.BM_STATUS_RESPONSE_UPDATED
-            ), "Failed to update request"
+    def update_bm_status(self, bm_status):
+        """
+        Update BM status when cluster is deployed/teardown
+
+        Args:
+            bm_status (str): Status to be updated
+
+        Returns:
+            str: response message
+        """
+        if bm_status == constants.BM_STATUS_PRESENT:
+            now = datetime.today().strftime("%Y-%m-%d")
+            payload = {
+                "status": bm_status,
+                "cluster_name": config.ENV_DATA["cluster_name"],
+                "creation_date": now,
+            }
+        else:
+            payload = {
+                "status": bm_status,
+                "cluster_name": "null",
+                "creation_date": "null",
+            }
+        headers = {"content-type": "application/json"}
+        response = requests.put(
+            url=self.helper_node_details["bm_status_check"],
+            json=payload,
+            headers=headers,
+        )
+        return response.json()["message"]
+
+    def destroy(self, log_level=""):
+        """
+        Destroy OCP cluster
+        """
+        logger.info("Updating BM status")
+        result = self.update_bm_status(constants.BM_STATUS_ABSENT)
+        assert (
+            result == constants.BM_STATUS_RESPONSE_UPDATED
+        ), "Failed to update request"
 
 
 class BAREMETALUPI(BAREMETALBASE):
@@ -150,7 +151,7 @@ class BAREMETALUPI(BAREMETALBASE):
         logger.info("BAREMETAL UPI")
         super().__init__()
 
-    class OCPDeployment(BAREMETALBASE.BMBaseOCPDeployment):
+    class OCPDeployment(BMBaseOCPDeployment):
         def __init__(self):
             super().__init__()
             self.aws = aws.AWS()
