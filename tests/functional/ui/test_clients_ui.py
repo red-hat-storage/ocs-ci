4cd4d08576ab9c22768e14306f284881a99e1160
Elena Bondarenko <ebondare@redhat.com>
2024-01-26 16:02:02 +0100

    Fix tox
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- tests/functional/ui/test_clients_ui.py
+++ tests/functional/ui/test_clients_ui.py
@@ -1,9 +1,15 @@
 import logging
-import pytest
 
-from ocs_ci.framework.testlib import ManageTest, ui
+from ocs_ci.framework.testlib import (
+    ManageTest,
+    ui,
+    runs_on_provider,
+    hci_provider_and_client_required,
+)
 from ocs_ci.framework.pytest_customization.marks import black_squad
 from ocs_ci.ocs.ui.client_ui import verify_clients_on_dashboard
+
+logger = logging.getLogger(__name__)
 
 
 @ui
@@ -14,9 +20,12 @@ class TestClientUI(ManageTest):
 
     """
 
+    @hci_provider_and_client_required
+    @runs_on_provider
     def test_clients_on_dashboard(self, setup_ui_class):
         """
         Test that the number of connected or disconnected clients on the dashboard is correct
 
         """
+        logger.info("Verifying number of clients on the dashboard")
         verify_clients_on_dashboard()
