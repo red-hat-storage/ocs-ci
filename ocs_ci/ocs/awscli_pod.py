7493f356ebc98c15885e0f66ab20a9704e1bbe79
fbalak <fbalak@redhat.com>
2024-02-06 15:26:46 +0100

    Add mcg test write to bucket from client
    Signed-off-by: fbalak <fbalak@redhat.com>

---
+++ ocs_ci/ocs/awscli_pod.py
@@ -0,0 +1,100 @@
+"""
+Methods used in awscli_pod fixtures in tests/conftest.py
+"""
+import logging
+
+from ocs_ci.framework import config
+from ocs_ci.helpers.helpers import (
+    create_resource,
+    create_unique_resource_name,
+    wait_for_resource_state,
+)
+from ocs_ci.helpers.proxy import update_container_with_proxy_env
+from ocs_ci.ocs import constants
+from ocs_ci.ocs.ocp import OCP
+from ocs_ci.ocs.resources.pod import get_pods_having_label, Pod
+from ocs_ci.utility import templating
+from ocs_ci.utility.utils import update_container_with_mirrored_image
+
+log = logging.getLogger(__name__)
+
+
+def create_awscli_pod(scope_name=None):
+    """
+    Create AWS cli pod and its resources.
+
+    Args:
+        scope_name (str): The name of the fixture's scope
+
+    Returns:
+        object: awscli_pod_obj
+    """
+    # Create the service-ca configmap to be mounted upon pod creation
+    service_ca_data = templating.load_yaml(constants.AWSCLI_SERVICE_CA_YAML)
+    resource_type = scope_name or "caconfigmap"
+    service_ca_configmap_name = create_unique_resource_name(
+        constants.AWSCLI_SERVICE_CA_CONFIGMAP_NAME, resource_type
+    )
+    service_ca_data["metadata"]["name"] = service_ca_configmap_name
+    service_ca_data["metadata"]["namespace"] = config.ENV_DATA["cluster_namespace"]
+    s3cli_label_k, s3cli_label_v = constants.S3CLI_APP_LABEL.split("=")
+    service_ca_data["metadata"]["labels"] = {s3cli_label_k: s3cli_label_v}
+    log.info("Trying to create the AWS CLI service CA")
+    service_ca_configmap = create_resource(**service_ca_data)
+    OCP(
+        namespace=config.ENV_DATA["cluster_namespace"], kind="ConfigMap"
+    ).wait_for_resource(
+        resource_name=service_ca_configmap.name, column="DATA", condition="1"
+    )
+
+    log.info("Creating the AWS CLI StatefulSet")
+    awscli_sts_dict = templating.load_yaml(constants.S3CLI_MULTIARCH_STS_YAML)
+    awscli_sts_dict["spec"]["template"]["spec"]["volumes"][0]["configMap"][
+        "name"
+    ] = service_ca_configmap_name
+    awscli_sts_dict["metadata"]["namespace"] = config.ENV_DATA["cluster_namespace"]
+    update_container_with_mirrored_image(awscli_sts_dict)
+    update_container_with_proxy_env(awscli_sts_dict)
+    s3cli_sts_obj = create_resource(**awscli_sts_dict)
+
+    log.info("Verifying the AWS CLI StatefulSet is running")
+    assert s3cli_sts_obj, "Failed to create S3CLI STS"
+    awscli_pod_obj = retry(IndexError, tries=3, delay=15)(
+        lambda: Pod(
+            **get_pods_having_label(
+                constants.S3CLI_LABEL, config.ENV_DATA["cluster_namespace"]
+            )[0]
+        )
+    )()
+    wait_for_resource_state(awscli_pod_obj, constants.STATUS_RUNNING, timeout=180)
+    return awscli_pod_obj
+
+
+def awscli_pod_cleanup():
+    """
+    Delete AWS cli pod resources.
+    """
+    log.info("Deleting the AWS CLI StatefulSet")
+    ocp_sts = OCP(
+        kind="StatefulSet",
+        namespace=config.ENV_DATA["cluster_namespace"],
+    )
+    try:
+        ocp_sts.delete(resource_name=constants.S3CLI_STS_NAME)
+    except CommandFailed as e:
+        if "NotFound" not in str(e):
+            log.info("The AWS CLI STS was not found, assuming it was already deleted")
+    except TimeoutError:
+        log.warning("Standard deletion of the AWS CLI STS timed-out, forcing deletion")
+        ocp_sts.delete(resource_name=constants.S3CLI_STS_NAME, force=True)
+
+    log.info("Deleting the AWS CLI service CA")
+    ocp_cm = OCP(
+        kind="ConfigMap",
+        namespace=config.ENV_DATA["cluster_namespace"],
+    )
+    awscli_service_ca_query = ocp_cm.get(selector=constants.S3CLI_APP_LABEL).get(
+        "items"
+    )
+    if awscli_service_ca_query:
+        ocp_cm.delete(resource_name=awscli_service_ca_query[0]["metadata"]["name"])
--- tests/functional/object/mcg/test_provider_client.py
+++ tests/functional/object/mcg/test_provider_client.py
@@ -1,5 +1,6 @@
 import logging
 
+from ocs_ci.framework import config
 from ocs_ci.framework.pytest_customization.marks import (
     tier1,
     red_squad,
@@ -8,10 +9,22 @@ from ocs_ci.framework.pytest_customization.marks import (
     provider_client_ms_platform_required,
 )
 from ocs_ci.ocs import constants
+from ocs_ci.ocs.bucket_utils import sync_object_directory
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.framework.testlib import polarion_id
 
 log = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def return_to_original_context():
+    """
+    Make sure that original context is restored after the test.
+    """
+    original_cluster = config.cluster_ctx.MULTICLUSTER["multicluster_index"]
+    return
+    log.info(f"Switching back to original cluster with index {original_cluster}")
+    config.switch_ctx(original_cluster)
 
 
 @mcg
@@ -39,3 +52,37 @@ def test_verify_backingstore_uses_rgw(mcg_obj_session):
         "backingstore status noobaa-default-backing-store"
     ).stdout
     assert f"endpoint: {rgw_endpoint}" in backingstore_data
+
+
+@mcg
+@red_squad
+@tier1
+@runs_on_provider
+@provider_client_ms_platform_required
+@pytest.mark.polarion_id("OCS-5214")
+def test_write_file_to_bucket_on_client(
+    bucket_factory, mcg_obj, awscli_pod_client_session, return_to_original_context
+):
+    """
+    Test object IO using the S3 SDK on bucket created on provider and used on client.
+    """
+    awscli_pod, client_cluster = awscli_pod_client_session
+    # Retrieve a list of all objects on the test-objects bucket and
+    # downloads them to the pod
+    bucketname = bucket_factory(1, interface="S3")[0].name
+    full_object_path = f"s3://{bucketname}"
+
+    provider_cluster = config.cluster_ctx.MULTICLUSTER["multicluster_index"]
+    config.switch_ctx(client_cluster)
+    log.info(f"Switched to client cluster with index {client_cluster}")
+    downloaded_files = awscli_pod.exec_cmd_on_pod(
+        f"ls -A1 {constants.AWSCLI_TEST_OBJ_DIR}"
+    ).split(" ")
+    # Write all downloaded objects to the new bucket
+    sync_object_directory(
+        awscli_pod, constants.AWSCLI_TEST_OBJ_DIR, full_object_path, mcg_obj
+    )
+
+    assert set(downloaded_files).issubset(
+        obj.key for obj in mcg_obj.s3_list_all_objects_in_bucket(bucketname)
+    )
