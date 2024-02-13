fecde3898c2ed4ef58635d9bc75a416e38255483
Daniel Osypenko <dosypenk@redhat.com>
2024-02-13 13:56:03 +0200

    ui -deco added to one test
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- tests/functional/ui/test_capacity_breakdown_ui.py
+++ tests/functional/ui/test_capacity_breakdown_ui.py
@@ -127,6 +127,7 @@ class TestCapacityBreakdownUI(ManageTest):
             project_name=project_name, pod_name=pod_name
         ), "The Project/Pod not created on Capacity Breakdown"
 
+    @ui
     @black_squad
     @tier3
     @bugzilla("2225223")
