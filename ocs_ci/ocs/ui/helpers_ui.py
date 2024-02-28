5e40e1d1f8d97ee1cf85ea2fc6ab3669d9fab247
Daniel Osypenko <dosypenk@redhat.com>
2024-02-28 16:18:35 +0200

    copied dict with locators from 4.15 to 4.16
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- ocs_ci/ocs/ui/helpers_ui.py
+++ ocs_ci/ocs/ui/helpers_ui.py
@@ -131,6 +131,7 @@ def ui_add_capacity_conditions():
         "4.13",
         "4.14",
         "4.15",
+        "4.16",
     ):
         logger.info(
             f"Add capacity via UI is not supported when the OCP version [{ocp_version}]"
