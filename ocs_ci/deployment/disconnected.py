5560330743118a959ea90bf30c6a3d3a0ddcf632
Daniel Horak <dahorak@redhat.com>
2024-02-29 11:27:39 +0100

    extend timeout for oc mirror command execution
    fixes: https://github.com/red-hat-storage/ocs-ci/issues/9379
    
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

--- ocs_ci/deployment/disconnected.py
+++ ocs_ci/deployment/disconnected.py
@@ -277,7 +277,7 @@ def mirror_index_image_via_oc_mirror(index_image, packages, icsp=None):
     if icsp:
         cmd += " --continue-on-error --skip-missing"
     try:
-        exec_cmd(cmd, timeout=7200)
+        exec_cmd(cmd, timeout=18000)
     except CommandFailed:
         # if icsp is configured, the oc mirror command might fail (return non 0 rc),
         # even though we use --continue-on-error and --skip-missing arguments
