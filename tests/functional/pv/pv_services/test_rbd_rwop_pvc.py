b04dee815d041634f1b657624929813605dfe7b9
Keerthana Manoharan <kmanohar@redhat.com>
2024-03-07 19:58:07 +0530

    Added factory methods to the setup

--- tests/functional/pv/pv_services/test_rbd_rwop_pvc.py
+++ tests/functional/pv/pv_services/test_rbd_rwop_pvc.py
@@ -14,6 +14,7 @@ from ocs_ci.framework.testlib import (
 from ocs_ci.helpers import helpers
 from ocs_ci.ocs.exceptions import UnexpectedBehaviour
 from ocs_ci.utility.utils import run_cmd
+
 log = logging.getLogger(__name__)
 
 
@@ -21,23 +22,23 @@ log = logging.getLogger(__name__)
 @skipif_ocs_version("<4.15")
 class TestRwopPvc(ManageTest):
     """
-    Tests RWOP RBD PVC
+    Tests ReadWriteOncePod RBD PVC
     """
 
     @pytest.fixture(autouse=True)
-    def setup(self, project_factory, create_pvcs_and_pods):
+    def setup(self, project_factory, pvc_factory, pod_factory):
         """
         Create PVC and pods
 
         """
-        pvc_size_gi = 10
-        self.pvc, self.pod = create_pvcs_and_pods(
-           pvc_size=pvc_size_gi,
-           access_modes_rbd=[constants.ACCESS_MODE_RWOP],
-           num_of_rbd_pvc=1,
-           num_of_cephfs_pvc=0,
+        self.pvc_obj = pvc_factory(
+            interface=constants.CEPHBLOCKPOOL,
+            access_mode=constants.ACCESS_MODE_RWOP,
+            size=10,
         )
-
+        self.pod_obj = pod_factory(
+            pvc=self.pvc_obj,
+        )
 
     @polarion_id("OCS-5456")
     @tier1
@@ -47,27 +48,22 @@ class TestRwopPvc(ManageTest):
 
         """
         # Verify that PVCs are reusable by creating new pods
-        interface = constants.CEPHBLOCKPOOL
+        log.info(f"PVC obj {self.pvc_obj}")
         new_pod_obj = helpers.create_pods(
-            self.pvc,
+            [self.pvc_obj],
             pod_factory,
-            interface,
+            constants.CEPHBLOCKPOOL,
         )
 
-        for pod_obj in new_pod_obj:
-            self.pod_obj = pod_obj
-            yaml_output = run_cmd(f"oc get pod " +str(pod_obj.name)+ " -o yaml", timeout=60)
-            log.info(f"yaml_output of the pod {self.pod_obj.name} - {yaml_output}")
+        yaml_output = run_cmd(
+            "oc get pod " + str(new_pod_obj[0].name) + " -o yaml", timeout=60
+        )
+        log.info(f"yaml_output of the pod {self.pod_obj.name} - {yaml_output}")
 
         # Validating the pod status
         results = yaml.safe_load(yaml_output)
-        log.info(f"results {results['status']['phase']}")
-        if results ["status"]["phase"] != "Pending":
-           raise UnexpectedBehaviour(
-            f"Pod {self.pod_obj.name} using RWOP pvc {self.pvc} is not in Pending state"
-        )
-
-
-
-
-
+        log.info(f"Status of the Pod : {results['status']['phase']}")
+        if results["status"]["phase"] != "Pending":
+            raise UnexpectedBehaviour(
+                f"Pod {self.pod_obj.name} using RWOP pvc {self.pvc_obj.name} is not in Pending state"
+            )
