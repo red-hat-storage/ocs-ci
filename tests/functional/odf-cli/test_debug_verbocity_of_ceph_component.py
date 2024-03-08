68ad1b2451e8039d2227b40734c8b43085c9c5d8
Parag Kamble <pakamble@redhat.com>
2024-03-09 01:39:43 +0530

    Removed odf cli binary code since it was already merged in the master
    Signed-off-by: Parag Kamble <pakamble@redhat.com>

--- tests/functional/odf-cli/test_debug_verbocity_of_ceph_component.py
+++ tests/functional/odf-cli/test_debug_verbocity_of_ceph_component.py
@@ -27,45 +27,20 @@ class TestDebugVerbosityOfCephComponents:
             6. Test Setting up log level beyond limit.
 
         """
-        # Initial log level
-        log_level = 10
+        log_levels = (50, 99, 0, 100)
 
-        # Setting up and verifying the log level value with the odf CLI tool
-        log.info(
-            f"Setting log level to {log_level} for service: {service}, subsystem: {subsystem}"
-        )
-        assert odf_cli_set_log_level(service, log_level, subsystem)
-        assert log_level == get_ceph_log_level(
-            service, subsystem
-        ), f"Log level set by ODF CLI ({log_level}) does not match with the value reported by Ceph"
+        for log_level in log_levels:
+            # Setting up and verifying the log level value with the odf CLI tool
+            log.info(
+                f"Setting log level to {log_level} for service: {service}, subsystem: {subsystem}"
+            )
 
-        # Overriding log level with different value.
-        log_level = 50
-        log.info(f"Overriding log level with a new value: {log_level}")
-        assert odf_cli_set_log_level(service, log_level, subsystem)
-        assert log_level == get_ceph_log_level(
-            service, subsystem
-        ), f"Log level set by ODF CLI ({log_level}) does not match with the value reported by Ceph"
-
-        # Setting up log level to upper limit 99
-        log_level = 99
-        log.info(f"Setting log level to upper limit: {log_level}")
-        assert odf_cli_set_log_level(service, log_level, subsystem)
-        assert log_level == get_ceph_log_level(
-            service, subsystem
-        ), f"Log level set by ODF CLI ({log_level}) does not match with the value reported by Ceph"
-
-        # Setting up log level to lower limit 0
-        log_level = 0
-        log.info(f"Setting log level to lower limit: {log_level}")
-        assert odf_cli_set_log_level(service, log_level, subsystem)
-        assert log_level == get_ceph_log_level(
-            service, subsystem
-        ), f"Log level set by ODF CLI ({log_level}) does not match with the value reported by Ceph"
-
-        # Setting up log level beyond limit
-        log_level = 100
-        log.info(f"Setting log level beyond limit: {log_level}")
-        with pytest.raises(CommandFailed):
-            odf_cli_set_log_level(service, log_level, subsystem)
-        log.info("Log level beyond the limit was not set as expected.")
+            if log_level > 99:
+                with pytest.raises(CommandFailed):
+                    odf_cli_set_log_level(service, log_level, subsystem)
+                    log.info("Log level beyond the limit was not set as expected.")
+            else:
+                assert odf_cli_set_log_level(service, log_level, subsystem)
+                assert log_level == get_ceph_log_level(
+                    service, subsystem
+                ), f"Log level set by ODF CLI ({log_level}) does not match with the value reported by Ceph"
