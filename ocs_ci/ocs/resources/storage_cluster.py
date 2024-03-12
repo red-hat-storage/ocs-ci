799bdc05d80a468c7dc5ce1254e078f23420f2ad
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2024-03-12 16:21:37 +0530

    updated the deployment to run it as part of jenkins build
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -2813,3 +2813,19 @@ def wait_for_storage_client_connected(timeout=180, sleep=10):
         timeout=timeout,
         sleep=sleep,
     )
+def check_storage_client_status(namespace=constants.OPENSHIFT_STORAGE_CLIENT_NAMESPACE):
+    """
+    Check storageclient status
+
+    Inputs:
+        namespace(str): Namespace where the storage client is created
+
+    Returns:
+        storageclient_status(str): storageclient phase
+
+    """
+    cmd = (
+        f"oc get storageclient -n {namespace} " "-o=jsonpath='{.items[*].status.phase}'"
+    )
+    storageclient_status = run_cmd(cmd=cmd)
+    return storageclient_status
