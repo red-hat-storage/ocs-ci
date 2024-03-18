88533c1f1911ca4c499d991fe6b48d01d6bc23ea
Aviadp <apolak@redhat.com>
2024-03-18 18:00:09 +0200

    current progress - to squish
    Signed-off-by: Aviadp <apolak@redhat.com>

--- tests/functional/storageclass/test_replica1.py
+++ tests/functional/storageclass/test_replica1.py
@@ -2,16 +2,19 @@ import pytest
 from logging import getLogger
 
 from ocs_ci.framework import config
-from ocs_ci.ocs.resources.pod import Pod
+from ocs_ci.ocs.resources.pod import get_pods_having_label, delete_pods
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs.resources.storage_cluster import (
     set_non_resilient_pool,
     validate_non_resilient_pool,
     StorageCluster,
 )
-from ocs_ci.ocs.constants import DEFAULT_CEPHBLOCKPOOL
+from ocs_ci.ocs.constants import DEFAULT_CEPHBLOCKPOOL, OSD_APP_LABEL
+from ocs_ci.helpers.helpers import delete_storageclasses
 
 log = getLogger(__name__)
+
+REPLICA1_STORAGECLASS = "ocs-storagecluster-ceph-non-resilient-rbd"
 
 
 # WIP - move functions to right modoule #
@@ -30,27 +33,29 @@ def get_failures_domain_name(cephblockpool: OCP) -> list[str]:
     return failure_domains
 
 
-def get_replica_1_osd(osd: Pod) -> str:
-    pass
+def get_replica_1_osds() -> list[OCP]:
+    replica1_osds = list()
+    all_osds = get_pods_having_label(label=OSD_APP_LABEL)
+    for domain in get_failures_domain_name():
+        for osd in all_osds:
+            if osd.get(selector=f"ceph.rook.io/DeviceSet={domain}"):
+                replica1_osds.append(osd)
+
+    return replica1_osds
 
 
-def delete_replica_1_osd(osd: Pod):
-    pass
-
-
-def count_osd_pods(osd: Pod) -> int:
-    pass
+def count_osd_pods(osd: OCP) -> int:
+    return len(get_pods_having_label(label=OSD_APP_LABEL))
 
 
 def delete_replica_1_sc() -> None:
-    pass
+    delete_storageclasses([REPLICA1_STORAGECLASS])
 
 
 def delete_replica_1_storagecluster() -> None:
     pass
 
 
-# test
 class TestReplicaOne:
     @pytest.fixture(scope="class")
     def setup_replica_1(self, project_factory, pvc_factory, pod_factory):
@@ -71,7 +76,7 @@ class TestReplicaOne:
             set_non_resilient_pool(storage_cluster, enable=False)
             delete_replica_1_sc()
             delete_replica_1_storagecluster()
-            delete_replica_1_osd()
+            delete_pods(get_replica_1_osds())
             # delete_pod_and_pvc
 
     def test_configure_replica1(self):
