452941001b47768208f424888ceb5daf6ad8b952
Aviadp <apolak@redhat.com>
2024-03-19 20:44:56 +0200

    current progress - to squish3
    Signed-off-by: Aviadp <apolak@redhat.com>

--- tests/functional/storageclass/test_replica1.py
+++ tests/functional/storageclass/test_replica1.py
@@ -9,8 +9,13 @@ from ocs_ci.ocs.resources.storage_cluster import (
     validate_non_resilient_pool,
     StorageCluster,
 )
-from ocs_ci.ocs.constants import DEFAULT_CEPHBLOCKPOOL, OSD_APP_LABEL
-from ocs_ci.helpers.helpers import delete_storageclasses
+from ocs_ci.ocs.constants import (
+    DEFAULT_CEPHBLOCKPOOL,
+    OSD_APP_LABEL,
+    CEPHBLOCKPOOL,
+    ACCESS_MODE_RWO,
+)
+from ocs_ci.helpers.helpers import delete_storageclasses, delete_cephblockpools
 
 log = getLogger(__name__)
 
@@ -44,41 +49,58 @@ def get_replica_1_osds() -> list[OCP]:
     return replica1_osds
 
 
-def count_osd_pods(osd: OCP) -> int:
+def count_osd_pods() -> int:
     return len(get_pods_having_label(label=OSD_APP_LABEL))
 
 
 def delete_replica_1_sc() -> None:
     delete_storageclasses([REPLICA1_STORAGECLASS])
 
+    delete_cephblockpools
 
-def delete_replica_1_storagecluster() -> None:
-    pass
+
+def create_replica1_pvc(pvc_factory, project_factory):
+    proj_obj = project_factory()
+    proj = proj_obj.namespace
+    pvc_factory(
+        project=proj,
+        interface=None,
+        storageclass=REPLICA1_STORAGECLASS,
+        size="1G",
+        access_mode=ACCESS_MODE_RWO,
+    )
+
+
+def create_pod_on_failure_domain(pod_factory, failure_domain: str):
+    pvc = create_replica1_pvc()
+    pod_factory(pvc=pvc, nodeSelector="topology.kubernetes.io/zone: f{failure_domain}")
+
+
+@pytest.fixture(scope="function")
+def setup_rellica1(request, pod_factory):
+    storage_cluster = StorageCluster(
+        resource_name=config.ENV_DATA["storage_cluster_name"],
+        namespace=config.ENV_DATA["cluster_namespace"],
+    )
+
+    set_non_resilient_pool(storage_cluster)
+    validate_non_resilient_pool(storage_cluster)
+    cephblockpools = OCP(kind=CEPHBLOCKPOOL)
+    testing_pod = pod_factory
+    log.info(testing_pod)
+
+    def teardown() -> None:
+        # delete workload pod
+        # delete pvc
+        set_non_resilient_pool(storage_cluster, enable=False)
+        delete_replica_1_sc()
+        delete_cephblockpools(cephblockpools)
+        delete_pods(get_replica_1_osds())
+
+        request.addfinalizer(teardown)
 
 
 class TestReplicaOne:
-    @pytest.fixture(scope="class")
-    def setup_replica_1(self, project_factory, pvc_factory, pod_factory):
-        storage_cluster = StorageCluster(
-            resource_name=config.ENV_DATA["storage_cluster_name"],
-            namespace=config.ENV_DATA["cluster_namespace"],
-        )
-        set_non_resilient_pool(storage_cluster)
-        # try:
-        validate_non_resilient_pool(storage_cluster)
-        # except:
-        #     Exception  # TODO: add proper exception
-        # create_new_project()
-        # create_pvc_on_zone1 https://hackmd.io/0PE0vG1RToCk8jKUgJJVkg#Create-a-pvc
-        # create pod on zone1 https://hackmd.io/0PE0vG1RToCk8jKUgJJVkg#Create-a-pod-to-consume-the-pvc
-
-        def finalizer() -> None:
-            set_non_resilient_pool(storage_cluster, enable=False)
-            delete_replica_1_sc()
-            delete_replica_1_storagecluster()
-            delete_pods(get_replica_1_osds())
-            # delete_pod_and_pvc
-
     def test_configure_replica1(self):
         pass
         # check ceph health
