25271a00b5b548d9fd6c58f753ca404af144599b
Aviadp <apolak@redhat.com>
2024-03-26 22:52:51 +0200

    progress
    Signed-off-by: Aviadp <apolak@redhat.com>

--- tests/functional/storageclass/test_replica1.py
+++ tests/functional/storageclass/test_replica1.py
@@ -2,7 +2,7 @@ import pytest
 from logging import getLogger
 
 from ocs_ci.framework import config
-from ocs_ci.ocs.resources.pod import get_pods_having_label, delete_pods
+from ocs_ci.ocs.resources.pod import get_pods_having_label, get_ceph_tools_pod
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs.resources.storage_cluster import (
     set_non_resilient_pool,
@@ -15,18 +15,25 @@ from ocs_ci.ocs.constants import (
     CEPHBLOCKPOOL,
     ACCESS_MODE_RWO,
     STORAGECLASS,
+    DEPLOYMENT,
 )
 from ocs_ci.helpers.helpers import create_pvc
+from ocs_ci.ocs.exceptions import CommandFailed
 
 
 log = getLogger(__name__)
 
 REPLICA1_STORAGECLASS = "ocs-storagecluster-ceph-non-resilient-rbd"
-config.ENV_DATA["worker_availability_zones"] = ["us-south-1", "us-south-2"]
+config.ENV_DATA["worker_availability_zones"] = [
+    "us-south-1",
+    "us-south-2",
+    "us-south-3",
+]
 
 
 # WIP - move functions to right modoule #
-def get_failures_domain_name(cpb_object: OCP) -> list[str]:
+def get_failures_domain_name() -> list[str]:
+    cpb_object = OCP(kind=CEPHBLOCKPOOL)
     failure_domains = list()
     cephblockpools_names = list()
     prefix = DEFAULT_CEPHBLOCKPOOL
@@ -47,15 +54,47 @@ def get_failures_domain_name(cpb_object: OCP) -> list[str]:
     return failure_domains
 
 
-def get_replica_1_osds() -> list[OCP]:
+def get_replica_1_osds() -> list[str]:
     replica1_osds = list()
     all_osds = get_pods_having_label(label=OSD_APP_LABEL)
-    for domain in get_failures_domain_name():
+    for domain in config.ENV_DATA["worker_availability_zones"]:
         for osd in all_osds:
-            if osd.get(selector=f"ceph.rook.io/DeviceSet={domain}"):
-                replica1_osds.append(osd)
-
+            if osd["metadata"]["labels"]["ceph.rook.io/DeviceSet"] == domain:
+                replica1_osds.append(osd["metadata"]["name"])
+    log.info(replica1_osds)
     return replica1_osds
+
+
+def get_replica1_osd_deployment() -> list[str]:
+    dep_obj = OCP(kind=DEPLOYMENT)
+    deployments = dep_obj.get()["items"]
+    replica1_osd_deployment = list()
+    osd_deployment = list()
+    for deployment in deployments:
+        if (
+            "metadata" in deployment
+            and "labels" in deployment["metadata"]
+            and "app.kubernetes.io/name" in deployment["metadata"]["labels"]
+        ):
+            if deployment["metadata"]["labels"]["app.kubernetes.io/name"] == "ceph-osd":
+                osd_deployment.append(deployment)
+
+    for deployment in osd_deployment:
+        if (
+            deployment["metadata"]["labels"]["ceph.rook.io/DeviceSet"]
+            in config.ENV_DATA["worker_availability_zones"]
+        ):
+            log.info(deployment["metadata"]["name"])
+            replica1_osd_deployment.append(deployment["metadata"]["name"])
+
+    return replica1_osd_deployment
+
+
+def delete_replica1_osd_deployment(deployments_name=list[str]) -> None:
+    deployment_obj = OCP(kind=DEPLOYMENT)
+    for deployment in deployments_name:
+        deployment_obj.delete(resource_name=deployment)
+        log.info(f"Deleting {deployment}")
 
 
 def count_osd_pods() -> int:
@@ -64,15 +103,26 @@ def count_osd_pods() -> int:
 
 def delete_replica_1_sc() -> None:
     sc_obj = OCP(kind=STORAGECLASS, resource_name=REPLICA1_STORAGECLASS)
-    sc_obj.delete(resource_name=REPLICA1_STORAGECLASS)
+    try:
+        sc_obj.delete(resource_name=REPLICA1_STORAGECLASS)
+    except CommandFailed as e:
+        if "Error is Error from server (NotFound)" in str(e):
+            log.info(
+                f"{REPLICA1_STORAGECLASS} not found, assuming it was already deleted"
+            )
+        else:
+            log.error("Failed to delete storage class")
 
 
 def delete_replica1_cephblockpools(cpb_object: OCP):
+    toolbox_pod = get_ceph_tools_pod()
     for i in range(0, len((cpb_object.data["items"]))):
-        resourcename = cpb_object.data["items"][i]["metadata"]["name"]
-        if resourcename != DEFAULT_CEPHBLOCKPOOL:
-            cpb_object.delete(resource_name=resourcename)
-            log.info(f"deleting {resourcename}")
+        replica1_pool_name = cpb_object.data["items"][i]["metadata"]["name"]
+        if replica1_pool_name != DEFAULT_CEPHBLOCKPOOL:
+            command = f"ceph osd pool rm {replica1_pool_name} {replica1_pool_name} --yes-i-really-really-mean-it"
+            toolbox_pod.exec_cmd_on_pod(command)
+
+            log.info(f"deleting {replica1_pool_name}")
 
 
 def create_replica1_pvc(project_factory):
@@ -92,7 +142,11 @@ def create_pod_on_failure_domain(project_factory, pod_factory, failure_domain: s
 
 
 @pytest.fixture(scope="function")
-def setup_rellica1(request, pod_factory, project_factory):
+def setup_rellica1(
+    request: pytest.FixtureRequest,
+    pod_factory,
+    project_factory,
+):
     log.info("setup fixture called")
     storage_cluster = StorageCluster(
         resource_name=config.ENV_DATA["storage_cluster_name"],
@@ -116,7 +170,6 @@ def setup_rellica1(request, pod_factory, project_factory):
         set_non_resilient_pool(storage_cluster, enable=False)
         delete_replica_1_sc()
         delete_replica1_cephblockpools(cephblockpools)
-        delete_pods(get_replica_1_osds())
 
     request.addfinalizer(teardown)
 
@@ -126,8 +179,18 @@ class TestReplicaOne:
         self,
     ):  # setup_rellica1):
         log.info("Starting Tier1 replica one test")
+        storage_cluster = StorageCluster(
+            resource_name=config.ENV_DATA["storage_cluster_name"],
+            namespace=config.ENV_DATA["cluster_namespace"],
+        )
         cephblockpools = OCP(kind=CEPHBLOCKPOOL)
-        get_failures_domain_name(cephblockpools)
+        set_non_resilient_pool(storage_cluster, enable=False)
+        delete_replica_1_sc()
+        log.info("StorageClass Deleted")
+        delete_replica1_cephblockpools(cephblockpools)
+        deployments_name = get_replica1_osd_deployment()
+        log.info(deployments_name)
+        delete_replica1_osd_deployment(deployments_name)
 
     def test_topology_validation(self):
         pass
