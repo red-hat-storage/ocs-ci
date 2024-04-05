d93f5c509a2084e058003bfc882b56717c9f1b6c
Elena Bondarenko <ebondare@redhat.com>
2024-04-05 13:54:30 +0200

    Fix non admin user UI test
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/ocs/ui/page_objects/page_navigator.py
+++ ocs_ci/ocs/ui/page_objects/page_navigator.py
@@ -420,8 +420,8 @@ class PageNavigator(BaseUI):
         """
         logger.info("Select the OCP administrator user role from the dropdown")
         if self.get_elements(self.generic_locators["developer_selected"]):
-            self.do_click(self.sc_loc["Developer_dropdown"])
-            self.do_click(self.sc_loc["select_administrator"], timeout=5)
+            self.do_click(self.validation_loc["Developer_dropdown"])
+            self.do_click(self.validation_loc["select_administrator"], timeout=5)
             logger.info("Administrator user is selected")
         elif self.get_elements(self.generic_locators["administrator_selected"]):
             logger.info("Administrator user was already selected")
