ca65fede9300e0c6073fb2e76190c5ff362f94ee
am-agrawa <amagrawa@redhat.com>
2024-04-15 18:48:07 +0530

    crash message
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- tests/manage/z_cluster/nodes/test_disk_failures.py
+++ tests/manage/z_cluster/nodes/test_disk_failures.py
@@ -199,7 +199,7 @@ class TestDiskFailures(ManageTest):
 
         is_daemon_recently_crash_warnings = run_cmd_verify_cli_output(
             cmd="ceph health detail",
-            expected_output_lst={"HEALTH_WARN", "1 daemons have recently crashed"},
+            expected_output_lst={"HEALTH_WARN", "daemons have recently crashed"},
             cephtool_cmd=True,
         )
         if is_daemon_recently_crash_warnings:
@@ -252,7 +252,7 @@ class TestDiskFailures(ManageTest):
 
         is_daemon_recently_crash_warnings = run_cmd_verify_cli_output(
             cmd="ceph health detail",
-            expected_output_lst={"HEALTH_WARN", "2 daemons have recently crashed"},
+            expected_output_lst={"HEALTH_WARN", "daemons have recently crashed"},
             cephtool_cmd=True,
         )
         if is_daemon_recently_crash_warnings:
