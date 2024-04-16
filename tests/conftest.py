494e20e1e3bacc1fbf5bf2a9259d7aae7aaf0a7c
Sidhant Agrawal <sagrawal@redhat.com>
2024-04-16 16:15:56 +0530

    Make setup_acm_ui fixture run only for UI tests
    Signed-off-by: Sidhant Agrawal <sagrawal@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -4868,6 +4868,8 @@ def setup_acm_ui(request):
 
 
 def setup_acm_ui_fixture(request):
+    if not ocsci_config.RUN.get("dr_action_via_ui"):
+        return
     restore_ctx_index = ocsci_config.cur_index
     ocsci_config.switch_acm_ctx()
     driver = login_to_acm()
