58730228e1d11e0d4851e04322c79a2b93c9e65d
Itzhak Kave <ikave@ibm.com>
2024-04-17 16:37:07 +0300

    Check resize osd pre-conditions
    Signed-off-by: Itzhak Kave <ikave@ibm.com>

--- tests/functional/z_cluster/cluster_expansion/test_resize_osd.py
+++ tests/functional/z_cluster/cluster_expansion/test_resize_osd.py
@@ -19,7 +19,12 @@ from ocs_ci.framework.testlib import (
     ManageTest,
     tier1,
 )
-from ocs_ci.ocs import constants
+from ocs_ci.ocs.constants import (
+    VOLUME_MODE_BLOCK,
+    MAX_RESIZE_OSD,
+    AWS_MAX_RESIZE_OSD_COUNT,
+    AWS_PLATFORM,
+)
 from ocs_ci.ocs.resources.osd_resize import (
     ceph_verification_steps_post_resize_osd,
     check_ceph_health_after_resize_osd,
@@ -32,9 +37,40 @@ from ocs_ci.ocs.resources.pod import (
 from ocs_ci.ocs.resources.pvc import get_deviceset_pvcs, get_deviceset_pvs
 from ocs_ci.ocs.resources.storage_cluster import resize_osd, get_storage_size
 from ocs_ci.helpers.sanity_helpers import Sanity
+from ocs_ci.utility.utils import convert_device_size
+from ocs_ci.framework import config
 
 
 logger = logging.getLogger(__name__)
+
+
+def check_resize_osd_pre_conditions():
+    """
+    Check the resize osd pre-conditions:
+    1. Check that the current storage size is less than the osd max size
+    2. If we use AWS, check that the osd resize count is no more than the AWS max resize count
+
+    If the conditions are not met, the test will be skipped.
+
+    """
+    current_storage_size = get_storage_size()
+    current_storage_size_in_gb = convert_device_size(current_storage_size, "GB", 1024)
+    max_storage_size_in_gb = convert_device_size(MAX_RESIZE_OSD, "GB", 1024)
+    if current_storage_size_in_gb >= max_storage_size_in_gb:
+        pytest.skip(
+            f"The current storage size {current_storage_size} is greater or equal to the "
+            f"max resize osd {MAX_RESIZE_OSD}"
+        )
+
+    config.RUN["resize_osd_count"] = config.RUN.get("resize_osd_count", 0)
+    logger.info(f"resize osd count = {config.RUN['resize_osd_count']}")
+    if (
+        config.ENV_DATA["platform"].lower() == AWS_PLATFORM
+        and config.RUN["resize_osd_count"] >= AWS_MAX_RESIZE_OSD_COUNT
+    ):
+        pytest.skip(
+            f"We can resize the osd no more than {AWS_MAX_RESIZE_OSD_COUNT} times when using aws platform"
+        )
 
 
 @brown_squad
@@ -59,6 +95,7 @@ class TestResizeOSD(ManageTest):
         Init all the data for the resize osd test
 
         """
+        check_resize_osd_pre_conditions()
         self.create_pvcs_and_pods = create_pvcs_and_pods
 
         self.old_osd_pods = get_osd_pods()
@@ -78,6 +115,32 @@ class TestResizeOSD(ManageTest):
             pvc_size=pvc_size, num_of_rbd_pvc=5, num_of_cephfs_pvc=5
         )
 
+    @pytest.fixture(autouse=True)
+    def teardown(self, request):
+        """
+        Check that the new osd size has increased and increase the resize osd count
+
+        """
+
+        def finalizer():
+            old_storage_size_in_gb = convert_device_size(
+                self.old_storage_size, "GB", 1024
+            )
+            new_storage_size_in_gb = convert_device_size(get_storage_size(), "GB", 1024)
+            logger.info(
+                f"old storage size in GB = {old_storage_size_in_gb}, "
+                f"new storage size in GB = {new_storage_size_in_gb}"
+            )
+            if new_storage_size_in_gb > old_storage_size_in_gb:
+                logger.info(
+                    "The osd size has increased successfully. Increasing the resize osd count by 1"
+                )
+                config.RUN["resize_osd_count"] += 1
+            else:
+                logger.warning("The osd size has not increased")
+
+        request.addfinalizer(finalizer)
+
     def run_io_on_pods(self, pods, size="1G", runtime=30):
         """
         Run IO on the pods
@@ -91,9 +154,7 @@ class TestResizeOSD(ManageTest):
         logger.info("Starting IO on all pods")
         for pod_obj in pods:
             storage_type = (
-                "block"
-                if pod_obj.pvc.volume_mode == constants.VOLUME_MODE_BLOCK
-                else "fs"
+                "block" if pod_obj.pvc.volume_mode == VOLUME_MODE_BLOCK else "fs"
             )
             rate = f"{random.randint(1, 5)}M"
             pod_obj.run_io(
