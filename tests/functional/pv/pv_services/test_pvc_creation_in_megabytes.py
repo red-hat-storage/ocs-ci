464793139693c12c0d68c71b9cd32e810acd69ab
nagendra202 <nagreddy@redhat.com>
2024-04-25 13:31:26 +0530

    modified condition Signed-off-by: nagendra202 <nagreddy@redhat.com>

--- tests/functional/pv/pv_services/test_pvc_creation_in_megabytes.py
+++ tests/functional/pv/pv_services/test_pvc_creation_in_megabytes.py
@@ -31,29 +31,29 @@ class TestPvcCreationInMegabytes:
         """
         access_mode = constants.ACCESS_MODE_RWX
         interface = constants.CEPHFILESYSTEM
-        given_pvc_size_mb = ["499", "500", "501"]
+        given_pvc_sizes_mb = ["499", "500", "501"]
         # conversion formulae for 'M' to bytes
         mega_bytes_to_bytes = 1000 * 1000
         # conversion formulae for 'Mi' to bytes
         mebi_bytes_to_bytes = 1024 * 1024
-        for size in given_pvc_size_mb:
-            log.info(f"Creating {interface} based PVC with the given size {size}M")
+        for size_mb in given_pvc_sizes_mb:
+            log.info(f"Creating {interface} based PVC with the given size {size_mb}M")
             # Creating PVC using pvc_factory
             pvc_obj = pvc_factory(
-                interface=interface, access_mode=access_mode, size=size, size_unit="M"
+                interface=interface,
+                access_mode=access_mode,
+                size=size_mb,
+                size_unit="M",
             )
             # getting pvc yaml using pvc_obj.get() and getting the pvc capacity from the yaml
-            actual_pvc_size_mib = (
-                pvc_obj.get().get("status").get("capacity").get("storage")
+            actual_pvc_size = pvc_obj.get().get("status").get("capacity").get("storage")
+            log.info(f"PVC created with the capacity of size {actual_pvc_size} ")
+            created_pvc_size_bytes = int(actual_pvc_size[:-2]) * mebi_bytes_to_bytes
+            requested_pvc_size_bytes = int(size_mb) * mega_bytes_to_bytes
+            assert created_pvc_size_bytes >= requested_pvc_size_bytes, (
+                f"Actual PVC size {created_pvc_size_bytes} bytes is less"
+                f" than the given PVC size {requested_pvc_size_bytes}"
             )
-            log.info(f"PVC created with the capacity of {actual_pvc_size_mib} ")
-            created_pvc_in_bytes = int(actual_pvc_size_mib[:-2]) * mebi_bytes_to_bytes
-            requested_pvc_in_bytes = int(size) * mega_bytes_to_bytes
-            if created_pvc_in_bytes >= requested_pvc_in_bytes:
-                log.info(
-                    f"Created PVC {created_pvc_in_bytes} bytes where the requested was {requested_pvc_in_bytes} bytes"
-                )
-            else:
-                assert (
-                    False
-                ), f"Actual PVC size {actual_pvc_size_mib} is different than the given PVC size {size}M"
+            log.info(
+                f"Created PVC {created_pvc_size_bytes} bytes where the requested was {requested_pvc_size_bytes} bytes"
+            )
