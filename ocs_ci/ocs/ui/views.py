025b57412bcd86889855bee338ff45c1c40d8649
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2024-04-26 10:28:51 +0200

    [release-4.15] Fix non admin user UI test (#9730)
    * Fix non admin user UI test
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    * Reduce unneccessary locator duplication
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    * Fix typo
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    * Fix administrator locator
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    
    ---------
    
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>
    Co-authored-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/ocs/ui/views.py
+++ ocs_ci/ocs/ui/views.py
@@ -1303,6 +1303,15 @@ validation = {
     ),
     "req_capacity_dropdown_namespace_input": ("search-bar", By.ID),
     "req_capacity_dropdown_namespace_input_select": ("//li[@id='{}-link']", By.XPATH),
+    "developer_dropdown": (
+        'button[data-test-id="perspective-switcher-toggle"]',
+        By.CSS_SELECTOR,
+    ),
+    "select_administrator": (
+        "//a[@class='pf-c-dropdown__menu-item']//h2[@class='pf-c-title pf-m-md'][normalize-space()='Administrator'] | "
+        "//a[@class='pf-m-icon pf-v5-c-dropdown__menu-item']//h2[normalize-space()='Administrator']",
+        By.XPATH,
+    ),
 }
 
 validation_4_7 = {
@@ -1461,14 +1470,6 @@ validation_4_9 = {
     ),
     "namespace-store": ("//a[normalize-space()='Namespace Store']", By.XPATH),
     "search-project": ("input[placeholder='Select project...']", By.CSS_SELECTOR),
-    "developer_dropdown": (
-        'button[data-test-id="perspective-switcher-toggle"]',
-        By.CSS_SELECTOR,
-    ),
-    "select_administrator": (
-        "//a[@class='pf-c-dropdown__menu-item']//h2[@class='pf-c-title pf-m-md'][normalize-space()='Administrator']",
-        By.XPATH,
-    ),
 }
 
 validation_4_10 = {
@@ -1488,14 +1489,6 @@ validation_4_10 = {
     ),
     "storage-system-health-card-hyperlink": (
         "//div[@class='odf-storageSystemPopup__item--margin']//a[contains(text(),'ocs-storagecluster-storagesystem')]",
-        By.XPATH,
-    ),
-    "developer_dropdown": (
-        'button[data-test-id="perspective-switcher-toggle"]',
-        By.CSS_SELECTOR,
-    ),
-    "select_administrator": (
-        "//a[@class='pf-c-dropdown__menu-item']//h2[@class='pf-c-title pf-m-md'][normalize-space()='Administrator']",
         By.XPATH,
     ),
 }
@@ -1518,14 +1511,6 @@ validation_4_11 = {
     "bucketclass-breadcrumb": ("//a[normalize-space()='BucketClasses']", By.XPATH),
     "bucketclass-breadcrumb-odf-4-10": (
         "//a[normalize-space()='noobaa.io~v1alpha1~BucketClass']",
-        By.XPATH,
-    ),
-    "developer_dropdown": (
-        'button[data-test-id="perspective-switcher-toggle"]',
-        By.CSS_SELECTOR,
-    ),
-    "select_administrator": (
-        "//a[@class='pf-c-dropdown__menu-item']//h2[@class='pf-c-title pf-m-md'][normalize-space()='Administrator']",
         By.XPATH,
     ),
 }
