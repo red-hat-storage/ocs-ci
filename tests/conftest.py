17ed18ed95ab4a7a2f808c67458d3e752dff1c67
prsurve <prsurve@redhat.com>
2024-05-12 22:33:07 +0530

    Fix Tox failure
    Signed-off-by: prsurve <prsurve@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -105,7 +105,6 @@ from ocs_ci.ocs.resources.objectbucket import BUCKET_MAP
 from ocs_ci.ocs.resources.ocs import OCS
 from ocs_ci.ocs.resources.pod import (
     get_rgw_pods,
-    delete_deploymentconfig_pods,
     get_pods_having_label,
     get_deployments_having_label,
     Pod,
@@ -115,7 +114,8 @@ from ocs_ci.ocs.resources.pod import (
     verify_data_integrity_for_multi_pvc_objs,
     get_noobaa_pods,
     get_pod_count,
-    wait_for_pods_by_label_count, delete_deployment_pods,
+    wait_for_pods_by_label_count,
+    delete_deployment_pods,
 )
 from ocs_ci.ocs.resources.pvc import (
     PVC,
@@ -3718,7 +3718,9 @@ def measurement_dir(tmp_path):
 
 
 @pytest.fixture()
-def multi_deployment_pods(multi_pvc_factory, deployment_pod_factory, service_account_factory):
+def multi_deployment_pods(
+    multi_pvc_factory, deployment_pod_factory, service_account_factory
+):
     """
     Prepare multiple deployment pods for the test
 
