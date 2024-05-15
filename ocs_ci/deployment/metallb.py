8b8a02037d078bf065954f7a6272a61b27e3997a
Daniel Osypenko <dosypenk@redhat.com>
2024-05-15 14:18:42 +0300

    retry when failed calling webhook \"ipaddresspoolvalidationwebhook.metallb.io\" received
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- ocs_ci/deployment/metallb.py
+++ ocs_ci/deployment/metallb.py
@@ -371,9 +371,8 @@ class MetalLBInstaller:
         )
         templating.dump_data_to_temp_yaml(ipaddresspool_data, ipaddresspool_file.name)
 
-        exec_cmd(
-            f"oc apply -f {ipaddresspool_file.name}",
-            timeout=240,
+        retry(CommandFailed, tries=3, delay=15)(exec_cmd)(
+            f"oc apply -f {ipaddresspool_file.name}", timeout=240
         )
 
         return self.ip_address_pool_created()
