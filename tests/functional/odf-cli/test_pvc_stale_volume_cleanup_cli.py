ebaa4c331d4e51701eee58c84de7566357624a08
Shivam Durgbuns <sdurgbun@redhat.com>
2024-05-15 12:49:29 +0530

    Adding test for RHSTOR5150
    Signed-off-by: Shivam Durgbuns <sdurgbun@redhat.com>

--- tests/functional/odf-cli/test_pvc_stale_volume_cleanup_cli.py
+++ tests/functional/odf-cli/test_pvc_stale_volume_cleanup_cli.py
@@ -6,6 +6,7 @@ from ocs_ci.framework.testlib import (
     tier1,
     acceptance,
 )
+from ocs_ci.ocs import constants
 from ocs_ci.helpers.helpers import retrieve_cli_binary
 from ocs_ci.utility.utils import run_cmd
 
@@ -16,7 +17,7 @@ logger = logging.getLogger(__name__)
 @acceptance
 class TestSubvolumesCommand(ManageTest):
     @skipif_ocs_version("<4.15")
-    def test_pvc_stale_volume_cleanup_cli(self):
+    def test_pvc_stale_volume_cleanup_cli(self, storageclass_factory, pvc_factory):
         """
         1. Create a new PVC with Retain strategy.
         2. Delete the PVC
@@ -26,8 +27,43 @@ class TestSubvolumesCommand(ManageTest):
         6. No stale volumes should be present of the deleted PVC.
         """
         retrieve_cli_binary(cli_type="odf")
-        inital_subvolume_list = run_cmd(
-            cmd="odf subvolume ls",
-        )
+        output = run_cmd(cmd="odf subvolume ls")
+        inital_subvolume_list = self.parse_subvolume_ls_output(output)
         logger.info(f"{inital_subvolume_list=}")
-        logger.info("I am here")
+        cephfs_sc_obj = storageclass_factory(
+            interface=constants.CEPHFILESYSTEM,
+            reclaim_policy=constants.RECLAIM_POLICY_RETAIN,
+        )
+        pvc_obj = pvc_factory(
+            storageclass=cephfs_sc_obj,
+            interface=constants.CEPHFILESYSTEM,
+            size=3,
+            access_mode=constants.ACCESS_MODE_RWX,
+            status=constants.STATUS_BOUND,
+        )
+        output = run_cmd(cmd="odf subvolume ls")
+        later_subvolume_list = self.parse_subvolume_ls_output(output)
+        old = set(inital_subvolume_list)
+        new = set(later_subvolume_list)
+        new_pvc = list(new.difference(old))[0]
+        logger.info(f"{new_pvc=}")
+
+        # Deleteting PVC and SC
+        cephfs_sc_obj.delete()
+        pvc_obj.delete()
+
+        # Deleteing stale subvolume
+        run_cmd(cmd=f"odf subvolume delete {new_pvc[0]} {new_pvc[1]} {new_pvc[2]}")
+
+        # Checking for stale volumes
+        output = run_cmd(cmd="odf subvolume ls --stale")
+        stale_volumes = self.parse_subvolume_ls_output(output)
+        assert len(stale_volumes) == 0  # No stale volumes available
+
+    def parse_subvolume_ls_output(self, output):
+        subvolumes = []
+        subvolumes_list = output.strip().split("\n")[1:]
+        for item in subvolumes_list:
+            fs, sv, svg, status = item.split(" ")
+            subvolumes.append((fs, sv, svg, status))
+        return subvolumes
