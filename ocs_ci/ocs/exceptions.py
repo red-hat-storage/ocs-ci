0b1f6f6d166bc25fecec09c826503e111b732bbd
am-agrawa <amagrawa@redhat.com>
2024-05-17 16:32:48 +0530

    revert unwanted code changes
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- ocs_ci/ocs/exceptions.py
+++ ocs_ci/ocs/exceptions.py
@@ -692,6 +692,22 @@ class UsernameNotFoundException(Exception):
     pass
 
 
+class MultiStorageClusterExternalCephHealth(Exception):
+    pass
+
+
+class StorageSizeNotReflectedException(Exception):
+    pass
+
+
+class ClusterNotInSTSModeException(Exception):
+    pass
+
+
+class APIRequestError(Exception):
+    pass
+
+
 class ACMObservabilityNotEnabled(Exception):
     pass
 
