596f8fb25f771363fc268e12e0b6a0d26349ede8
oviner <oviner@redhat.com>
2024-05-20 14:25:56 +0300

    add polrio id
    Signed-off-by: oviner <oviner@redhat.com>

--- tests/functional/object/mcg/test_noobaa_db_cleartext_postgres_password.py
+++ tests/functional/object/mcg/test_noobaa_db_cleartext_postgres_password.py
@@ -1,6 +1,6 @@
 import logging
 
-from ocs_ci.framework.testlib import tier2, BaseTest, bugzilla
+from ocs_ci.framework.testlib import tier2, BaseTest, bugzilla, polarion_id
 from ocs_ci.framework.pytest_customization.marks import red_squad, mcg
 from ocs_ci.framework import config
 from ocs_ci.ocs.resources import pod
@@ -14,13 +14,19 @@ log = logging.getLogger(__name__)
 class TestNoobaaSecurity(BaseTest):
     """
     Test Noobaa Security
+
     """
 
     @mcg
     @red_squad
     @bugzilla("2274193")
+    @polarion_id("OCS-5787")
     def test_noobaa_db_cleartext_postgres_password(self):
         """
+        Verify postgres password is not clear text
+
+        Test Process:
+
         1.Get noobaa db pod
         2.Get logs from all containers in pod oc logs "noobaa-db-pg-0 --all-containers"
         3.Verify postgres password does not exist in noobaa-db pod logs
