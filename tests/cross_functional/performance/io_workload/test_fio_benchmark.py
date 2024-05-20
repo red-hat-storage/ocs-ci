0006653b0fbdd718d0fc4051259c6eeb0961a8ec
pintojoy <jopinto@redhat.com>
2024-05-20 19:17:28 +0530

    Draft PR
    Signed-off-by: pintojoy <jopinto@redhat.com>

--- tests/cross_functional/performance/io_workload/test_fio_benchmark.py
+++ tests/cross_functional/performance/io_workload/test_fio_benchmark.py
@@ -203,7 +203,7 @@ class TestFIOBenchmark(PASTest):
 
         """
         if io_pattern == "sequential":
-            self.crd_data["spec"]["workload"]["args"]["jobs"] = ["read"]
+            self.crd_data["spec"]["workload"]["args"]["jobs"] = ["write"]
         if io_pattern == "random":
             self.crd_data["spec"]["workload"]["args"]["jobs"] = [
                 "randwrite",
