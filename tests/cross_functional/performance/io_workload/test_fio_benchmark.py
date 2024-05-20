30c49121f8270412eb3db4ebdd40b497d8bf2fda
pintojoy <jopinto@redhat.com>
2024-05-20 12:58:50 +0530

    Draft PR
    Signed-off-by: pintojoy <jopinto@redhat.com>

--- tests/cross_functional/performance/io_workload/test_fio_benchmark.py
+++ tests/cross_functional/performance/io_workload/test_fio_benchmark.py
@@ -187,9 +187,9 @@ class TestFIOBenchmark(PASTest):
             self.crd_data["spec"]["workload"]["args"][
                 "storagesize"
             ] = f"{int(self.filesize * 1.2)}Gi"
-        self.crd_data["spec"]["workload"]["args"]["servers"] = int(
-            self.total_data_set / self.filesize
-        ) + 3
+        self.crd_data["spec"]["workload"]["args"]["servers"] = (
+            int(self.total_data_set / self.filesize) + 3
+        )
         log.info(f"Total Data set to work on is : {self.total_data_set} GiB")
         log.info("Setting prefill value to False ")
         self.crd_data["spec"]["workload"]["args"]["prefill"] = False
