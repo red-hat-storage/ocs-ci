ce691b284a481109f8c5aeb9026f168a913d50de
prsurve <prsurve@redhat.com>
2024-05-28 12:38:07 +0530

    fix tox issue
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/helpers/dr_helpers.py
+++ ocs_ci/helpers/dr_helpers.py
@@ -171,10 +171,11 @@ def failover(
             namespace=namespace, resource_name=f"{workload_placement_name}-drpc"
         )
     elif discovered_apps:
-        failover_params = \
-            (f'{{"spec":{{"action":"{constants.ACTION_FAILOVER}",'
-             f'"failoverCluster":"{failover_cluster}",'
-             f'"preferredCluster":"{old_primary}"}}}}')
+        failover_params = (
+            f'{{"spec":{{"action":"{constants.ACTION_FAILOVER}",'
+            f'"failoverCluster":"{failover_cluster}",'
+            f'"preferredCluster":"{old_primary}"}}}}'
+        )
         namespace = constants.DR_OPS_NAMESAPCE
         drpc_obj = DRPC(namespace=namespace, resource_name=f"{workload_placement_name}")
     else:
@@ -227,10 +228,11 @@ def relocate(
             namespace=namespace, resource_name=f"{workload_placement_name}-drpc"
         )
     elif discovered_apps:
-        relocate_params = \
-            (f'{{"spec":{{"action":"{constants.ACTION_RELOCATE}",'
-             f'"failoverCluster":"{old_primary}",'
-             f'"preferredCluster":"{preferred_cluster}"}}}}')
+        relocate_params = (
+            f'{{"spec":{{"action":"{constants.ACTION_RELOCATE}",'
+            f'"failoverCluster":"{old_primary}",'
+            f'"preferredCluster":"{preferred_cluster}"}}}}'
+        )
         namespace = constants.DR_OPS_NAMESAPCE
         drpc_obj = DRPC(namespace=namespace, resource_name=f"{workload_placement_name}")
     else:
