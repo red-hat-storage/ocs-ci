ec712afce22b584017f88c5b842cc4b7b6da7696
prsurve <prsurve@redhat.com>
2024-06-10 13:33:56 +0530

    fix test failure
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/ocs/resources/drpc.py
+++ ocs_ci/ocs/resources/drpc.py
@@ -118,5 +118,6 @@ def get_drpc_name(namespace, switch_ctx=None):
     config.switch_acm_ctx()
     config.switch_ctx(switch_ctx) if switch_ctx else config.switch_acm_ctx()
     drpc_obj = OCP(kind=constants.DRPC, namespace=namespace).get()["items"][0]
+    drpc_name = drpc_obj["metadata"]["name"]
     config.switch_ctx(restore_index)
-    return drpc_obj["metadata"]["name"]
+    return drpc_name
