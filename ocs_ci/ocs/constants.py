3f8c4add83e252454148756eca6c8024b1b73b64
nagendra202 <nagreddy@redhat.com>
2024-06-10 11:58:19 +0530

    MDSCPUUsageHigh alert automation
    Signed-off-by: nagendra202 <nagreddy@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -1269,6 +1269,7 @@ ALERT_PERSISTENT_VOLUME_USAGE_NEAR_FULL = "PersistentVolumeUsageNearFull"
 ALERT_ODF_PERSISTENT_VOLUME_MIRROR_STATUS = "ODFPersistentVolumeMirrorStatus"
 ALERT_OBC_QUOTA_BYTES_ALERT = "ObcQuotaBytesAlert"
 ALERT_MDSCACHEUSAGEHIGH = "MDSCacheUsageHigh"
+ALERT_MDSCPUUSAGEHIGH = "MDSCPUUsageHigh"
 
 # OCS Deployment related constants
 OPERATOR_NODE_LABEL = "cluster.ocs.openshift.io/openshift-storage=''"
@@ -2936,3 +2937,7 @@ INF_RULES_YAML = os.path.join(TEMPLATE_DEPLOYMENT_DIR_INF, "rules.yaml")
 
 # helper script to perform meta data operations [create, rename, change permissions...etc] on app pod to fill MDS cache
 METAIO = os.path.join(TEMPLATE_WORKLOAD_DIR, "helper_scripts/meta_data_io.py")
+# helper script to perform file creation IO on app pod to fill MDS cpu
+FILE_CREATOR_IO = os.path.join(
+    TEMPLATE_WORKLOAD_DIR, "helper_scripts/file_creator_io.py"
+)
