2615ef4a19770ad0f0451721ba8305ba3ce84b15
am-agrawa <amagrawa@redhat.com>
2024-07-10 15:11:18 +0530

    moving ui to black squad
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- tests/functional/z_cluster/cluster_expansion/test_add_capacity.py
+++ tests/functional/z_cluster/cluster_expansion/test_add_capacity.py
@@ -142,7 +142,6 @@ class TestAddCapacity(ManageTest):
         add_capacity_test(ui_flag=False)
 
     @tier1
-    @brown_squad
     @black_squad
     def test_add_capacity_ui(self, reduce_and_resume_cluster_load):
         """
@@ -177,7 +176,6 @@ class TestAddCapacityLSO(ManageTest):
         storage_cluster.add_capacity_lso(ui_flag=False)
 
     @tier1
-    @brown_squad
     @black_squad
     def test_add_capacity_lso_ui(self, reduce_and_resume_cluster_load):
         """
