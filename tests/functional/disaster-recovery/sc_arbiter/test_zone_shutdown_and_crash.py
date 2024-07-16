ef4f2800986944284a38a15cea003f00f93bda72
Mahesh Shetty <mashetty@redhat.com>
2024-07-16 09:54:14 +0530

    add cnv deployment required marker
    Signed-off-by: Mahesh Shetty <mashetty@redhat.com>

--- tests/functional/disaster-recovery/sc_arbiter/test_zone_shutdown_and_crash.py
+++ tests/functional/disaster-recovery/sc_arbiter/test_zone_shutdown_and_crash.py
@@ -32,6 +32,7 @@ from ocs_ci.framework.pytest_customization.marks import (
     tier1,
     turquoise_squad,
     stretchcluster_required,
+    cnv_required,
 )
 
 log = logging.getLogger(__name__)
@@ -40,6 +41,7 @@ log = logging.getLogger(__name__)
 @tier1
 @stretchcluster_required
 @turquoise_squad
+@cnv_required
 class TestZoneShutdownsAndCrashes:
 
     zones = constants.DATA_ZONE_LABELS
