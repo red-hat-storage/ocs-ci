086043ab11b8f3eb53bff96b3007f0284aa599ae
pintojoy <jopinto@redhat.com>
2024-07-17 22:40:34 +0530

    Analyzing the failure
    Signed-off-by: pintojoy <jopinto@redhat.com>

--- tests/functional/z_cluster/test_performance_profile_validation.py
+++ tests/functional/z_cluster/test_performance_profile_validation.py
@@ -142,12 +142,12 @@ class TestProfileDefaultValuesCheck(ManageTest):
                 )
                 log.info(type(resource_dict["limits"]["cpu"]))
                 log.info(type(expected_cpu_limit_values[label]))
-                log.info(resource_dict["limits"]["memory"])
-                log.info(expected_memory_limit_values[label])
-                log.info(resource_dict["requests"]["cpu"])
-                log.info(resource_dict["requests"]["memory"])
-                log.info(expected_cpu_request_values[label])
-                log.info(expected_memory_request_values[label])
+                log.info(type(resource_dict["limits"]["memory"]))
+                log.info(type(expected_memory_limit_values[label]))
+                log.info(type(resource_dict["requests"]["cpu"]))
+                log.info(type(resource_dict["requests"]["memory"]))
+                log.info(type(expected_cpu_request_values[label]))
+                log.info(type(expected_memory_request_values[label]))
                 assert (
                     resource_dict["limits"]["cpu"] == expected_cpu_limit_values[label]
                     and resource_dict["limits"]["memory"]
