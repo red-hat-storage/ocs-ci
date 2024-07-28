f069f42811a4c2654a166ebf1984fa548386c2ee
am-agrawa <amagrawa@redhat.com>
2024-07-28 18:42:01 +0530

    rebase on 28july24
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- ocs_ci/ocs/acm/acm.py
+++ ocs_ci/ocs/acm/acm.py
@@ -248,7 +248,7 @@ class AcmAddClusters(AcmPageNavigator):
             except StaleElementReferenceException:
                 self.take_screenshot()
                 log.info(
-                    f'The stale element is {self.page_nav["install-submariner-btn"]}'
+                    f'The stale element is {self.page_nav["install-submariner-btn"]}, re-trying..'
                 )
                 if attempt == retries - 1:
                     raise
