9bd6cf076520d33688fb15d6011366337a500f09
nagendra202 <nagreddy@redhat.com>
2024-08-07 20:11:10 +0530

    Incorporated review comments
    Signed-off-by: nagendra202 <nagreddy@redhat.com>

--- tests/functional/pv/pv_services/test_cephfs_pvc_with_chunk_size.py
+++ tests/functional/pv/pv_services/test_cephfs_pvc_with_chunk_size.py
@@ -26,6 +26,12 @@ class TestCephfsWithChunkIo:
     This class takes care of create Cephfs PVC, create Fedora dc pod and run Chunk IO on fedora pod
     """
 
+    def teardown(self):
+
+        log.info("set ceph mds debug level to default value 1/5")
+        cluster.ceph_config_set_debug("1/5")
+        log.info("Ceph mds debug level has been set to default 1/5")
+
     def test_cephfs_with_large_chunk_io(self, pvc_factory, dc_pod_factory):
 
         """
@@ -41,57 +47,37 @@ class TestCephfsWithChunkIo:
         9. Check for warning "MDS_CLIENT_LATE_RELEASE" in ceph.
         10. If warning found, test will fail.
         """
+
         file = constants.CHUNK
         interface = constants.CEPHFILESYSTEM
-
-        # Creating PVC with cephfs as inetrface
-        log.info(f"Creating {interface} based PVC")
-        pvc_obj = pvc_factory(interface=interface, size="50")
-        # Creating a Fedora dc pod
         log.info("Creating fedora dc pod")
-        pod_obj = dc_pod_factory(pvc=pvc_obj, interface=interface)
-        # Copy chunk.py to fedora pod
+        pod_obj = dc_pod_factory(size="50", interface=interface)
         log.info("Copying chunk.py to fedora pod ")
-        cmd = f"oc cp {file} {pvc_obj.namespace}/{pod_obj.name}:/"
+        cmd = f"oc cp {file} {pod_obj.namespace}/{pod_obj.name}:/"
         helpers.run_cmd(cmd=cmd)
         log.info("chunk.py copied successfully ")
-
-        # set debug 25 in rook-ceph-tools for mds
         cluster.ceph_config_set_debug("25")
         log.info("mds debug has been set to 25 successfully ")
-        # Run chunk.py on fedora pod
         log.info("Running chunk file IO on fedora pod ")
-
         chunk_executor = ThreadPoolExecutor(max_workers=1)
         self.chunk_thread = chunk_executor.submit(
             pod_obj.exec_sh_cmd_on_pod, command="python3 chunk.py"
         )
-
-        # sleep script for 1 hr to run chunk IO
         log.info("Script will be in sleep for 15 minutes to run chunk IO on fedora pod")
         time.sleep(900)
-
-        # Get mds pod object
-        mds_obj = pod.get_mds_pods(namespace="openshift-storage")
-        # The below list contians three errors, these errors should not be seen in the mds logs
+        mds_obj = pod.get_mds_pods()
         err_msgs = ["mclientcaps(revoke)", "mclientcaps(import)", "mclientcaps(grant)"]
-        # Checking ceph health detail for warning MDS_CLIENT_LATE_RELEASE
+        log.info(f"These errors {err_msgs} should not be seen in the mds logs")
+        log.info("Checking ceph health detail for warning MDS_CLIENT_LATE_RELEASE")
         ceph_health_detail = cluster.ceph_health_detail()
-        if "MDS_CLIENT_LATE_RELEASE" in ceph_health_detail:
-            log.error("Found warning in ceph health " + ceph_health_detail)
-            assert False
-        else:
-            for err in err_msgs:
-                mds_0_log = pod.get_pod_logs(pod_name=mds_obj[0].name)
-                mds_1_log = pod.get_pod_logs(pod_name=mds_obj[1].name)
-                # The below if loop will fail if mds logs has any matching with the errors in err_msgs
-                if err in mds_0_log or err in mds_1_log:
-                    log.error(f"Found error {err} in MDS pod logs")
-                    assert False, f"Unexpected Error {err} found in the MDS logs"
-                else:
-                    log.info(f"No error {err} found in MDS pod logs")
 
-    def teardown(self):
-        # set ceph mds debug level to default value 1/5
-        cluster.ceph_config_set_debug("1/5")
-        log.info("Ceph mds debug level has been set to default 1/5")
+        assert (
+            "MDS_CLIENT_LATE_RELEASE" not in ceph_health_detail
+        ), f"Found warning in ceph health: {ceph_health_detail}"
+        mds_0_log = pod.get_pod_logs(pod_name=mds_obj[0].name)
+        mds_1_log = pod.get_pod_logs(pod_name=mds_obj[1].name)
+        combined_logs = mds_0_log + mds_1_log
+        errors_found = [err for err in err_msgs if err in combined_logs]
+        assert (
+            not errors_found
+        ), f"Unexpected Error(s) found in the MDS logs: {', '.join(errors_found)}"
