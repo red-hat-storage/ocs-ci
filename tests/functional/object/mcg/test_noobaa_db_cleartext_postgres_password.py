c8c4e82fdec7392f6dae4fb41680c24344466c93
Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>
2024-08-22 13:47:49 +0530

    Add coverage for the bz#2240778
    Signed-off-by: Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>

--- tests/functional/object/mcg/test_noobaa_db_cleartext_postgres_password.py
+++ tests/functional/object/mcg/test_noobaa_db_cleartext_postgres_password.py
@@ -4,7 +4,7 @@ from ocs_ci.framework.testlib import tier2, BaseTest, bugzilla, polarion_id
 from ocs_ci.framework.pytest_customization.marks import red_squad, mcg
 from ocs_ci.framework import config
 from ocs_ci.ocs.resources import pod
-
+from ocs_ci.ocs.resources.pod import filter_pod_logs
 
 log = logging.getLogger(__name__)
 
@@ -43,3 +43,64 @@ class TestNoobaaSecurity(BaseTest):
         assert (
             "set=password" not in nooobaa_db_pod_logs
         ), f"noobaa-db pod logs include password logs:{nooobaa_db_pod_logs}"
+
+    def test_nb_db_password_in_core_and_endpoint(self):
+        """
+        Verify that postgres password is not exposed in
+        noobaa core and endpoint logs
+
+        1. Get the noobaa core log
+        2. Get the noobaa endpoint log
+        3. Verify postgres password doesnt exist in the endpoint and core logs
+
+        """
+
+        # get noobaa core log and verify that the password is not
+        # present in the log
+        nooba_core_pod = pod.get_noobaa_core_pod()
+        noobaa_core_pod_logs = pod.get_pod_logs(nooba_core_pod.name)
+        filtered_log = filter_pod_logs(
+            pod_logs=noobaa_core_pod_logs,
+            filter=[
+                "host",
+                "noobaa-db-pg-0.noobaa-db-pg",
+                "user",
+                "noobaa",
+                "database",
+                "nbcore",
+                "port",
+                "5432",
+                "password",
+            ],
+        )
+        assert (
+            len(filtered_log) == 0
+        ), f"Noobaa db password seems to be present in the noobaa core logs:\n{filtered_log}"
+        log.info(
+            "Verified that noobaa db password is not present in the noobaa core log."
+        )
+
+        # get noobaa endpoint log and verify that the password is not
+        # present in the log
+        noobaa_endpoint_pod = pod.get_noobaa_endpoint_pods()[0]
+        noobaa_endpoint_logs = pod.get_pod_logs(noobaa_endpoint_pod.name)
+        filtered_log = filter_pod_logs(
+            pod_logs=noobaa_endpoint_logs,
+            filter=[
+                "host",
+                "noobaa-db-pg-0.noobaa-db-pg",
+                "user",
+                "noobaa",
+                "database",
+                "nbcore",
+                "port",
+                "5432",
+                "password",
+            ],
+        )
+        assert (
+            len(filtered_log) == 0
+        ), f"Noobaa db password seems to be present in the noobaa endpoint logs:\n{filtered_log}"
+        log.info(
+            "Verified that noobaa db password is not present in the noobaa endpoint log."
+        )
