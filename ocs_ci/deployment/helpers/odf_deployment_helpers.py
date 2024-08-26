6fee1daa828279901e5f1a3adbe18ef638825d82
Jilju Joy <jijoy@redhat.com>
2024-08-26 12:49:14 +0530

    Add cephcsi-operator in the list of required csv (#10331)
    * Add cephcsi-operator in the list of required csv
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    * Add cephcsi-operator in the list of required csv
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    ---------
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/deployment/helpers/odf_deployment_helpers.py
+++ ocs_ci/deployment/helpers/odf_deployment_helpers.py
@@ -34,4 +34,7 @@ def get_required_csvs():
             defaults.RECIPE_OPERATOR,
         ]
         ocs_operator_names.extend(operators_4_16_additions)
+    if ocs_version >= version.VERSION_4_17:
+        operators_4_17_additions = [defaults.CEPHCSI_OPERATOR]
+        ocs_operator_names.extend(operators_4_17_additions)
     return ocs_operator_names
