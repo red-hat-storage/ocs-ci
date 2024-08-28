301a3a2f0ae8f7ab09781d2bb2134440bd885155
nagendra202 <nagreddy@redhat.com>
2024-08-28 18:05:25 +0530

    Addressing review comments
    Signed-off-by: nagendra202 <nagreddy@redhat.com>

--- ocs_ci/ocs/node.py
+++ ocs_ci/ocs/node.py
@@ -3010,19 +3010,15 @@ def get_worker_node_where_ceph_toolbox_not_running():
 
     Returns:
         List of worker nodes other than the node where ceph tool box is already running.
+
     """
-    log.info(
-        "Testing cephtoolbox pod with nodeaffinity in the openshift-storage namespace."
-    )
-
-    ct_pod = pod.get_ceph_tools_pod()
-    # Identify on which node the ceph toolbox is running currently
-    ct_pod_running_node_name = ct_pod.data["spec"].get("nodeName")
-
+    ct_pod_running_node_name = get_ceph_tools_running_node()
     worker_nodes = get_worker_nodes()
-    log.info(worker_nodes)
-
+    log.info(
+        f"List of all worker nodes available in the cluster currently {worker_nodes}"
+    )
     other_nodes = [node for node in worker_nodes if node != ct_pod_running_node_name]
+    log.info(f"List of worker nodes where ceph tools pod is not running: {other_nodes}")
     return other_nodes
 
 
@@ -3034,8 +3030,8 @@ def apply_node_affinity_for_ceph_toolbox(node_name, wait_time=60):
         node_name = node name which need to be added in the node affinity
 
     Returns:
-        bool: return True --> if node affinity applied successfully .
-        bool: return False --> if node affinity fails.
+        bool: True if node affinity applied successfully
+
     """
     resource_name = constants.DEFAULT_CLUSTERNAME
     if config.DEPLOYMENT["external_mode"]:
@@ -3053,24 +3049,32 @@ def apply_node_affinity_for_ceph_toolbox(node_name, wait_time=60):
         f'"values": ["{node_name}"]}}]}}]}}}}}}}}'
     )
     param = f'{{"spec": {{"placement": {nodeaffinity}}}}}'
+    ct_pod = pod.get_ceph_tools_pod(skip_creating_pod=True)
+    ct_pod_name = ct_pod.name
     storagecluster_obj.patch(params=param, format_type="merge")
     log.info(
         f"Successfully applied node affinity for ceph toolbox pod with {node_name}"
     )
-    log.info(
-        "Script will be in sleep for a minute to make sure cephtools pod failovered with node affinity"
-    )
-    time.sleep(wait_time)
-    ct_new_pod = pod.get_ceph_tools_pod(skip_creating_pod=True)
+    ct_pod.ocp.wait_for_delete(ct_pod_name), f"Pod {ct_pod_name} is not deleted"
     log.info(
         "Identify on which node the ceph toolbox is running after failover due to node affinity"
     )
-    ct_new_pod_running_node_name = ct_new_pod.data["spec"].get("nodeName")
+    ct_new_pod_running_node_name = get_ceph_tools_running_node()
     if node_name == ct_new_pod_running_node_name:
         log.info(
             f"ceph toolbox pod failovered to the new node {ct_new_pod_running_node_name}"
             f" given in node affinity successfully "
         )
         return True
-    else:
-        return False
+
+
+def get_ceph_tools_running_node():
+    """
+    Get node name where the ceph tools pod is currently running
+
+    :return: Node name (str)
+
+    """
+    ct_pod = pod.get_ceph_tools_pod(wait=True, skip_creating_pod=True)
+    ct_pod_running_node = ct_pod.data["spec"].get("nodeName")
+    return ct_pod_running_node
--- tests/functional/pod_and_daemons/test_cephtoolbox_pod_nodeaffinity.py
+++ tests/functional/pod_and_daemons/test_cephtoolbox_pod_nodeaffinity.py
@@ -1,25 +1,23 @@
 import logging
 import pytest
+import random
 
 from ocs_ci.framework import config
 from ocs_ci.framework.pytest_customization.marks import bugzilla, brown_squad
 from ocs_ci.framework.testlib import tier1, tier4b, polarion_id
-from ocs_ci.helpers.sanity_helpers import Sanity
 from ocs_ci.ocs import ocp, constants
 from ocs_ci.ocs.node import (
-    unschedule_nodes,
+    apply_node_affinity_for_ceph_toolbox,
     drain_nodes,
-    schedule_nodes,
-    taint_nodes,
-    untaint_nodes,
+    get_ceph_tools_running_node,
     get_worker_nodes,
     get_worker_node_where_ceph_toolbox_not_running,
-    apply_node_affinity_for_ceph_toolbox,
+    schedule_nodes,
+    taint_nodes,
+    unschedule_nodes,
+    untaint_nodes,
 )
-from ocs_ci.ocs.resources import pod
-from ocs_ci.ocs.resources.pod import (
-    wait_for_pods_to_be_running,
-)
+from ocs_ci.ocs.resources.pod import wait_for_pods_to_be_running
 
 log = logging.getLogger(__name__)
 
@@ -27,16 +25,15 @@ log = logging.getLogger(__name__)
 @brown_squad
 @bugzilla("2249640")
 class TestCephtoolboxPod:
-    @pytest.fixture(autouse=True)
-    def init_sanity(self):
-        """
-        Initialize Sanity instance
-        """
-        self.sanity_helpers = Sanity()
-
     @pytest.fixture(scope="session", autouse=True)
     def teardown(self, request):
         def finalizer():
+            """
+            Finalizer will take care of below activities:
+            1. Untaint the nodes: remove taints from nodes
+            2. Removes nodeaffinity to bring storage cluster with default values.
+
+            """
             untaint_nodes()
             resource_name = constants.DEFAULT_CLUSTERNAME
             if config.DEPLOYMENT["external_mode"]:
@@ -60,12 +57,16 @@ class TestCephtoolboxPod:
     def test_node_affinity_to_ceph_toolbox_pod(self):
         """
         This test verifies whether ceph toolbox failovered or not after applying node affinity
+
         """
         other_nodes = get_worker_node_where_ceph_toolbox_not_running()
+        other_node_name = random.choice(other_nodes)
         log.info(
             "Apply node affinity with a node name other than currently running node."
         )
-        assert apply_node_affinity_for_ceph_toolbox(other_nodes[0])
+        assert apply_node_affinity_for_ceph_toolbox(
+            other_node_name
+        ), "Failed to apply node affinity for the Ceph toolbox on the specified node."
 
     @tier4b
     @polarion_id("OCS-6087")
@@ -74,9 +75,10 @@ class TestCephtoolboxPod:
         This test verifies ceph toolbox runs only on the node given in node-affinity.
         Reboot the node after applying node-affinity.
         Expectation is the pod should come up only on that node mentioned in affinity.
+
         """
         other_nodes = get_worker_node_where_ceph_toolbox_not_running()
-        node_name = other_nodes[0]
+        node_name = random.choice(other_nodes)
         apply_node_affinity_for_ceph_toolbox(node_name)
         log.info("Unschedule ceph tools pod running node.")
         unschedule_nodes([node_name])
@@ -85,15 +87,14 @@ class TestCephtoolboxPod:
         log.info(f"node {node_name} drained successfully")
         schedule_nodes([node_name])
         log.info(f"Scheduled the node {node_name}")
-        ct_pod = pod.get_ceph_tools_pod(skip_creating_pod=True, wait=True)
         log.info("Identify on which node the ceph toolbox is running after node drain")
-        ct_pod_running_node_name = ct_pod.data["spec"].get("nodeName")
+        ct_pod_running_node_name = get_ceph_tools_running_node()
         if node_name == ct_pod_running_node_name:
             log.info(
                 f"ceph toolbox pod is running on a node {node_name} which is in node-affinity"
             )
         else:
-            log.info(
+            log.error(
                 f"Ceph toolbox pod is not running on the nodeAffinity given node {node_name}."
             )
             assert False, "Ceph toolbox pod is not on the expected node."
@@ -103,28 +104,17 @@ class TestCephtoolboxPod:
     def test_nodeaffinity_to_ceph_toolbox_with_default_taints(self):
         """
         This test verifies whether ceph toolbox failovered or not after applying node affinity on tainted node
+
         """
         worker_nodes = get_worker_nodes()
         log.info(f"Current available worker nodes are {worker_nodes}")
         taint_nodes(worker_nodes)
+        log.info("Applied default taints on all the worker nodes")
         other_nodes = get_worker_node_where_ceph_toolbox_not_running()
+        other_node_name = random.choice(other_nodes)
         log.info(
             "Apply node affinity with a node name other than currently running node."
         )
-        assert apply_node_affinity_for_ceph_toolbox(other_nodes[0])
-
-    @tier4b
-    @polarion_id("OCS-6091")
-    def test_nodeaffinity_to_ceph_toolbox_with_custom_taints(self):
-        """
-        This test verifies whether ceph toolbox failovered or not after applying node affinity on custom tainted node.
-        """
-        worker_nodes = get_worker_nodes()
-        log.info(f"Current available worker nodes are {worker_nodes}")
-        # TODO: <<PR9808 yet to be merged. Once it is merged, the custom taints function need to be called here.>>
-        # <<This task will be implemented as part of https://github.com/red-hat-storage/ocs-ci/issues/10358 >>
-        other_nodes = get_worker_node_where_ceph_toolbox_not_running()
-        log.info(
-            "Apply node affinity with a node name other than currently running node."
-        )
-        assert apply_node_affinity_for_ceph_toolbox(other_nodes[0])
+        assert apply_node_affinity_for_ceph_toolbox(
+            other_node_name
+        ), "Failed to apply nodeaffinity with default taints"
