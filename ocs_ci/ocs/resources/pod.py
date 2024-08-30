3ec3b3c08e78e8ef01df157e56aa91386e576b0b
Vishakha Kathole <vkathole@redhat.com>
2024-08-30 12:11:31 +0530

    change warning to error
    Signed-off-by: Vishakha Kathole <vkathole@redhat.com>

--- ocs_ci/ocs/resources/pod.py
+++ ocs_ci/ocs/resources/pod.py
@@ -2608,7 +2608,7 @@ def check_toleration_on_pods(toleration_key=constants.TOLERATION_KEY):
         else:
             logger.info(f"The Toleration {toleration_key} exists on {resource_name}")
     if pods_missing_toleration:
-        logger.warning(
+        logger.error(
             f"Some pods are missing the toleration {toleration_key}: {', '.join(pods_missing_toleration)}"
         )
         raise TolerationNotFoundException(
@@ -2650,7 +2650,7 @@ def check_toleration_on_subscriptions(toleration_key=constants.TOLERATION_KEY):
             logger.info(f"The Toleration {toleration_key} exists on {sub}")
 
     if subs_missing_toleration:
-        logger.warning(
+        logger.error(
             f"Some subscriptions are missing the toleration {toleration_key}: {', '.join(subs_missing_toleration)}"
         )
         raise TolerationNotFoundException(
