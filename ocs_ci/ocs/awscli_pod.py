fef91e51dab364c21dfd7ab4baa754d241b50c35
Sagi Hirshfeld <shirshfe@redhat.com>
2024-09-09 10:05:30 +0300

    Use the secure external endpoint and the needed CA for RGW SSL external deployments (#10426)
    * Use the config file variables and add the missing CA to the awscli pod
    * Change the CA handling on the awscli pod
    
    Signed-off-by: Sagi Hirshfeld <shirshfe@redhat.com>

--- ocs_ci/ocs/awscli_pod.py
+++ ocs_ci/ocs/awscli_pod.py
@@ -1,12 +1,14 @@
 """
 Methods used in awscli_pod fixtures in tests/conftest.py
 """
+
 import logging
 
 from ocs_ci.framework import config
 from ocs_ci.helpers.helpers import (
     create_resource,
     create_unique_resource_name,
+    storagecluster_independent_check,
     wait_for_resource_state,
 )
 from ocs_ci.helpers.proxy import update_container_with_proxy_env
@@ -44,6 +46,7 @@ def create_awscli_pod(scope_name=None, namespace=None, service_account=None):
     s3cli_label_k, s3cli_label_v = constants.S3CLI_APP_LABEL.split("=")
     service_ca_data["metadata"]["labels"] = {s3cli_label_k: s3cli_label_v}
     log.info("Trying to create the AWS CLI service CA")
+
     service_ca_configmap = create_resource(**service_ca_data)
     OCP(namespace=namespace, kind="ConfigMap").wait_for_resource(
         resource_name=service_ca_configmap.name, column="DATA", condition="1"
@@ -69,6 +72,17 @@ def create_awscli_pod(scope_name=None, namespace=None, service_account=None):
         lambda: Pod(**get_pods_having_label(constants.S3CLI_LABEL, namespace)[0])
     )()
     wait_for_resource_state(awscli_pod_obj, constants.STATUS_RUNNING, timeout=180)
+
+    awscli_pod_obj.exec_cmd_on_pod(
+        f"cp {constants.SERVICE_CA_CRT_AWSCLI_PATH} {constants.AWSCLI_CA_BUNDLE_PATH}"
+    )
+
+    if storagecluster_independent_check() and config.EXTERNAL_MODE["rgw_secure"]:
+        log.info("Concatenating the RGW CA to the AWS CLI pod's CA bundle")
+        awscli_pod_obj.exec_cmd_on_pod(
+            f"bash -c 'wget -O - {config.EXTERNAL_MODE['rgw_cert_ca']} >> {constants.AWSCLI_CA_BUNDLE_PATH}'"
+        )
+
     return awscli_pod_obj
 
 
