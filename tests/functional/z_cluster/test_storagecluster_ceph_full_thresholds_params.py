6428ef56b1b71e079122647aba87df6a168d5978
oviner <oviner@redhat.com>
2024-09-09 17:16:14 +0300

    fix code
    Signed-off-by: oviner <oviner@redhat.com>

--- tests/functional/z_cluster/test_storagecluster_ceph_full_thresholds_params.py
+++ tests/functional/z_cluster/test_storagecluster_ceph_full_thresholds_params.py
@@ -27,6 +27,27 @@ class TestStorageClusterCephFullThresholdsParams(ManageTest):
 
     """
 
+    TRESHOLDS = [
+        {
+            "sc_key": "backfillFullRatio",
+            "value": "0.84",
+            "default_value": "0.85",
+            "ceph_key": "backfillfull_ratio",
+        },
+        {
+            "sc_key": "fullRatio",
+            "value": "0.79",
+            "default_value": "0.8",
+            "ceph_key": "full_ratio",
+        },
+        {
+            "sc_key": "nearFullRatio",
+            "value": "0.74",
+            "default_value": "0.75",
+            "ceph_key": "nearfull_ratio",
+        },
+    ]
+
     @pytest.fixture(autouse=True)
     def teardown_fixture(self, request):
         """
@@ -35,12 +56,7 @@ class TestStorageClusterCephFullThresholdsParams(ManageTest):
         """
 
         def finalizer():
-            params_dict = {
-                "backfillFullRatio": "0.85",
-                "fullRatio": "0.8",
-                "nearFullRatio": "0.75",
-            }
-            self.set_storage_cluster_ceph_full_thresholds_params(params_dict)
+            self.set_storage_cluster_ceph_full_thresholds_params(default_values=True)
 
         request.addfinalizer(finalizer)
 
@@ -50,16 +66,11 @@ class TestStorageClusterCephFullThresholdsParams(ManageTest):
         1.Configure storagecluster CR
         2.Wait 2 seconds
         3.Verify ceph full thresholds parameters on cephcluster CR and storagecluster CR are same
-        4.Verify parameters with ceph api- 'ceph osd dump'
+        4.Verify parameters with ceph CLI 'ceph osd dump'
         5.Configure the default params on storagecluster [treardown]
 
         """
-        params_dict = {
-            "backfillFullRatio": "0.862",
-            "fullRatio": "0.793",
-            "nearFullRatio": "0.744",
-        }
-        self.set_storage_cluster_ceph_full_thresholds_params(params_dict)
+        self.set_storage_cluster_ceph_full_thresholds_params()
 
         logger.info("Wait 2 sec the cephcluster will updated")
         time.sleep(2)
@@ -72,11 +83,11 @@ class TestStorageClusterCephFullThresholdsParams(ManageTest):
             namespace=config.ENV_DATA["cluster_namespace"],
             resource_name=constants.CEPH_CLUSTER_NAME,
         )
-        for parameter, parameter_value in params_dict.items():
-            actual_value = cephcluster_obj.data["spec"]["storage"][parameter]
+        for parameter in self.TRESHOLDS:
+            actual_value = cephcluster_obj.data["spec"]["storage"][parameter["sc_key"]]
             assert (
-                str(actual_value).lower() == str(parameter_value).lower()
-            ), f"The value of {parameter} is {actual_value} the expected value is {parameter_value}"
+                str(actual_value).lower() == str(parameter["value"]).lower()
+            ), f"The value of {parameter['sc_key']} is {actual_value} the expected value is {parameter['value']}"
 
         sample = TimeoutSampler(
             timeout=600,
@@ -84,9 +95,7 @@ class TestStorageClusterCephFullThresholdsParams(ManageTest):
             func=run_cmd_verify_cli_output,
             cmd="ceph osd dump",
             expected_output_lst=(
-                "full_ratio 0.793",
-                "backfillfull_ratio 0.862",
-                "nearfull_ratio 0.744",
+                tuple(f"{d['ceph_key']} {d['default_value']}" for d in self.TRESHOLDS)
             ),
             cephtool_cmd=True,
             ocs_operator_cmd=False,
@@ -97,23 +106,28 @@ class TestStorageClusterCephFullThresholdsParams(ManageTest):
                 "The ceph full thresholds storagecluster parameters are not updated in ceph tool"
             )
 
-    def set_storage_cluster_ceph_full_thresholds_params(self, params_dict):
+    def set_storage_cluster_ceph_full_thresholds_params(self, default_values=False):
         """
         Configure StorageCluster CR with ceph full thresholds params
 
         Args:
-            params_dict:
+            default_values: parameters to set in StorageCluster under /spec/managedResources/cephCluster/
 
         """
-        logger.info(f"Configure StorageCluster CR with upgrade params {params_dict}")
+        logger.info("Configure StorageCluster CR with upgrade params")
         storagecluster_obj = ocp.OCP(
             kind=constants.STORAGECLUSTER,
             namespace=config.ENV_DATA["cluster_namespace"],
             resource_name=constants.DEFAULT_CLUSTERNAME,
         )
-        for parameter, parameter_value in params_dict.items():
+        for parameter in self.TRESHOLDS:
+            sc_key = parameter["sc_key"]
+            if default_values:
+                parameter_value = parameter["default_value"]
+            else:
+                parameter_value = parameter["value"]
             param = (
-                f'[{{"op": "add", "path": "/spec/managedResources/cephCluster/{parameter}",'
+                f'[{{"op": "add", "path": "/spec/managedResources/cephCluster/{sc_key}",'
                 f' "value": {parameter_value}}}]'
             )
             storagecluster_obj.patch(params=param, format_type="json")
