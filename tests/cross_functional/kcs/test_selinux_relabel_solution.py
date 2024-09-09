6707ccf3a155de513822def1eb8903401487b302
Daniel Osypenko <dosypenk@redhat.com>
2024-09-09 19:53:13 +0300

    address ns and common issues shown up during tier1 execution
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- tests/cross_functional/kcs/test_selinux_relabel_solution.py
+++ tests/cross_functional/kcs/test_selinux_relabel_solution.py
@@ -165,7 +165,7 @@ class TestSelinuxrelabel(E2ETest):
             # Get the pod conditions
             pod = ocp.OCP(kind="pod", namespace=config.ENV_DATA["cluster_namespace"])
             conditions = pod.exec_oc_cmd(
-                f"get pod {pod_name} -n openshift-storage -o jsonpath='{{.status.conditions}}'"
+                f"get pod {pod_name} -o jsonpath='{{.status.conditions}}'"
             )
             conditions = [
                 {key: None if value == "null" else value for key, value in item.items()}
