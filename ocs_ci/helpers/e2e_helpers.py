923bfa534d0e38c1bf7621e189062f19a2698770
nagendra202 <nagreddy@redhat.com>
2024-09-30 17:45:44 +0530

    rebase
    Signed-off-by: nagendra202 <nagreddy@redhat.com>

--- ocs_ci/helpers/e2e_helpers.py
+++ ocs_ci/helpers/e2e_helpers.py
@@ -19,7 +19,25 @@ from ocs_ci.ocs.bucket_utils import (
 )
 from ocs_ci.ocs.resources.pod import get_rgw_pods, get_pod_logs
 from ocs_ci.utility.utils import exec_cmd, run_cmd
-
+from ocs_ci.ocs import constants
+from ocs_ci.utility.kms import is_kms_enabled
+from ocs_ci.ocs.constants import DEFAULT_NOOBAA_BUCKETCLASS, DEFAULT_NOOBAA_BACKINGSTORE
+from ocs_ci.ocs.ocp import OCP
+from ocs_ci.ocs.resources.pod import get_noobaa_pods
+from ocs_ci.ocs.resources.pvc import get_pvc_objs
+from ocs_ci.framework import config
+from ocs_ci.helpers import helpers
+from ocs_ci.ocs.benchmark_operator_fio import get_file_size
+from ocs_ci.ocs.benchmark_operator_fio import BenchmarkOperatorFIO
+from ocs_ci.ocs.exceptions import TimeoutExpiredError
+from ocs_ci.utility.utils import TimeoutSampler, ceph_health_check
+from ocs_ci.ocs.resources.pod import cal_md5sum
+from ocs_ci.ocs.cluster import (
+    change_ceph_full_ratio,
+    get_percent_used_capacity,
+    get_osd_utilization,
+    get_ceph_df_detail,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -392,3 +410,336 @@ def validate_mcg_object_expiration(
 
 def validate_mcg_nsfs_feature():
     logger.info("This is not implemented")
+
+
+def validate_noobaa_rebuild_system(self, bucket_factory_session, mcg_obj_session):
+    """
+    This function is to verify noobaa rebuild. Verifies KCS: https://access.redhat.com/solutions/5948631
+
+    1. Stop the noobaa-operator by setting the replicas of noobaa-operator deployment to 0.
+    2. Delete the noobaa deployments/statefulsets.
+    3. Delete the PVC db-noobaa-db-0.
+    4. Patch existing backingstores and bucketclasses to remove finalizer
+    5. Delete the backingstores/bucketclass.
+    6. Delete the noobaa secrets.
+    7. Restart noobaa-operator by setting the replicas back to 1.
+    8. Monitor the pods in openshift-storage for noobaa pods to be Running.
+
+    """
+
+    dep_ocp = OCP(
+        kind=constants.DEPLOYMENT, namespace=config.ENV_DATA["cluster_namespace"]
+    )
+    state_ocp = OCP(
+        kind=constants.STATEFULSET, namespace=config.ENV_DATA["cluster_namespace"]
+    )
+    noobaa_pvc_obj = get_pvc_objs(pvc_names=["db-noobaa-db-pg-0"])
+
+    # Scale down noobaa operator
+    logger.info(
+        f"Scaling down {constants.NOOBAA_OPERATOR_DEPLOYMENT} deployment to replica: 0"
+    )
+    dep_ocp.exec_oc_cmd(
+        f"scale deployment {constants.NOOBAA_OPERATOR_DEPLOYMENT} --replicas=0"
+    )
+
+    # Delete noobaa deployments and statefulsets
+    logger.info("Deleting noobaa deployments and statefulsets")
+    dep_ocp.delete(resource_name=constants.NOOBAA_ENDPOINT_DEPLOYMENT)
+    state_ocp.delete(resource_name=constants.NOOBAA_DB_STATEFULSET)
+    state_ocp.delete(resource_name=constants.NOOBAA_CORE_STATEFULSET)
+
+    # Delete noobaa-db pvc
+    pvc_obj = OCP(kind=constants.PVC, namespace=config.ENV_DATA["cluster_namespace"])
+    logger.info("Deleting noobaa-db pvc")
+    pvc_obj.delete(resource_name=noobaa_pvc_obj[0].name, wait=True)
+    pvc_obj.wait_for_delete(resource_name=noobaa_pvc_obj[0].name, timeout=300)
+
+    # Patch and delete existing backingstores
+    params = '{"metadata": {"finalizers":null}}'
+    bs_obj = OCP(
+        kind=constants.BACKINGSTORE, namespace=config.ENV_DATA["cluster_namespace"]
+    )
+    for bs in bs_obj.get()["items"]:
+        assert bs_obj.patch(
+            resource_name=bs["metadata"]["name"],
+            params=params,
+            format_type="merge",
+        ), "Failed to change the parameter in backingstore"
+        logger.info(f"Deleting backingstore: {bs['metadata']['name']}")
+        bs_obj.delete(resource_name=bs["metadata"]["name"])
+
+    # Patch and delete existing bucketclass
+    bc_obj = OCP(
+        kind=constants.BUCKETCLASS, namespace=config.ENV_DATA["cluster_namespace"]
+    )
+    for bc in bc_obj.get()["items"]:
+        assert bc_obj.patch(
+            resource_name=bc["metadata"]["name"],
+            params=params,
+            format_type="merge",
+        ), "Failed to change the parameter in bucketclass"
+        logger.info(f"Deleting bucketclass: {bc['metadata']['name']}")
+        bc_obj.delete(resource_name=bc["metadata"]["name"])
+
+    # Delete noobaa secrets
+    logger.info("Deleting noobaa related secrets")
+    if is_kms_enabled():
+        dep_ocp.exec_oc_cmd(
+            "delete secrets noobaa-admin noobaa-endpoints noobaa-operator noobaa-server"
+        )
+    else:
+        dep_ocp.exec_oc_cmd(
+            "delete secrets noobaa-admin noobaa-endpoints noobaa-operator "
+            "noobaa-server noobaa-root-master-key-backend noobaa-root-master-key-volume"
+        )
+
+    # Scale back noobaa-operator deployment
+    logger.info(
+        f"Scaling back {constants.NOOBAA_OPERATOR_DEPLOYMENT} deployment to replica: 1"
+    )
+    dep_ocp.exec_oc_cmd(
+        f"scale deployment {constants.NOOBAA_OPERATOR_DEPLOYMENT} --replicas=1"
+    )
+
+    # Wait and validate noobaa PVC is in bound state
+    pvc_obj.wait_for_resource(
+        condition=constants.STATUS_BOUND,
+        resource_name=noobaa_pvc_obj[0].name,
+        timeout=600,
+        sleep=120,
+    )
+
+    # Validate noobaa pods are up and running
+    pod_obj = OCP(kind=constants.POD, namespace=config.ENV_DATA["cluster_namespace"])
+    noobaa_pods = get_noobaa_pods()
+    pod_obj.wait_for_resource(
+        condition=constants.STATUS_RUNNING,
+        resource_count=len(noobaa_pods),
+        selector=constants.NOOBAA_APP_LABEL,
+        timeout=900,
+    )
+
+    # Verify everything running fine
+    logger.info("Verifying all resources are Running and matches expected result")
+    self.sanity_helpers.health_check(tries=120)
+
+    # Since the rebuild changed the noobaa-admin secret, update
+    # the s3 credentials in mcg_object_session
+    mcg_obj_session.update_s3_creds()
+
+    # Verify default backingstore/bucketclass
+    default_bs = OCP(
+        kind=constants.BACKINGSTORE, namespace=config.ENV_DATA["cluster_namespace"]
+    ).get(resource_name=DEFAULT_NOOBAA_BACKINGSTORE)
+    default_bc = OCP(
+        kind=constants.BUCKETCLASS, namespace=config.ENV_DATA["cluster_namespace"]
+    ).get(resource_name=DEFAULT_NOOBAA_BUCKETCLASS)
+    assert (
+        default_bs["status"]["phase"]
+        == default_bc["status"]["phase"]
+        == constants.STATUS_READY
+    ), "Failed: Default bs/bc are not in ready state"
+
+    # Create OBCs
+    logger.info("Creating OBCs after noobaa rebuild")
+    bucket_factory_session(amount=3, interface="OC", verify_health=True)
+
+
+def validate_noobaa_db_backup_recovery_locally_system(
+    self,
+    bucket_factory_session,
+    noobaa_db_backup_and_recovery_locally,
+    warps3,
+    mcg_obj_session,
+):
+    """
+    Test to verify Backup and Restore for Multicloud Object Gateway database locally
+    Backup procedure:
+        * Create a test bucket and write some data
+        * Backup noobaa secrets to local folder OR store it in secret objects
+        * Backup the PostgreSQL database and save it to a local folder
+        * For testing, write new data to show a little data loss between backup and restore
+    Restore procedure:
+        * Stop MCG reconciliation
+        * Stop the NooBaa Service before restoring the NooBaa DB.
+          There will be no object service after this point
+        * Verify that all NooBaa components (except NooBaa DB) have 0 replicas
+        * Login to the NooBaa DB pod and cleanup potential database clients to nbcore
+        * Restore DB from a local folder
+        * Delete current noobaa secrets and restore them from a local folder OR secrets objects.
+        * Restore MCG reconciliation
+        * Start the NooBaa service
+        * Restart the NooBaa DB pod
+        * Check that the old data exists, but not s3://testloss/
+    Run multi client warp benchamrking to verify bug https://bugzilla.redhat.com/show_bug.cgi?id=2141035
+
+    """
+
+    # create a bucket for warp benchmarking
+    bucket_name = bucket_factory_session()[0].name
+
+    # Backup and restore noobaa db using fixture
+    noobaa_db_backup_and_recovery_locally(bucket_factory_session)
+
+    # Verify everything running fine
+    logger.info("Verifying all resources are Running and matches expected result")
+    self.sanity_helpers.health_check(tries=120)
+
+    # Run multi client warp benchmarking
+    warps3.run_benchmark(
+        bucket_name=bucket_name,
+        access_key=mcg_obj_session.access_key_id,
+        secret_key=mcg_obj_session.access_key,
+        duration="10m",
+        concurrent=10,
+        objects=100,
+        obj_size="1MiB",
+        validate=True,
+        timeout=4000,
+        multi_client=True,
+        tls=True,
+        debug=True,
+        insecure=True,
+    )
+
+    # make sure no errors in the noobaa pod logs
+    search_string = (
+        "AssertionError [ERR_ASSERTION]: _id must be unique. "
+        "found 2 rows with _id=undefined in table bucketstats"
+    )
+    nb_pods = get_noobaa_pods()
+    for pod in nb_pods:
+        pod_logs = get_pod_logs(pod_name=pod.name)
+        for line in pod_logs:
+            assert (
+                search_string not in line
+            ), f"[Error] {search_string} found in the noobaa pod logs"
+    logger.info(f"No {search_string} errors are found in the noobaa pod logs")
+
+
+class Run_fio_till_cluster_full:
+    """
+    Run fio from multiple pods to fill cluster 85% of raw capacity.
+    """
+
+    def cleanup(self):
+        if self.benchmark_operator_teardown:
+            change_ceph_full_ratio(95)
+            self.benchmark_obj.cleanup()
+            ceph_health_check(tries=30, delay=60)
+        change_ceph_full_ratio(85)
+
+    def run_cluster_full_fio(
+        self,
+        teardown_project_factory,
+        pvc_factory,
+        pod_factory,
+    ):
+        """
+        1.Create PVC1 [FS + RBD]
+        2.Verify new PVC1 [FS + RBD] on Bound state
+        3.Run FIO on PVC1_FS + PVC1_RBD
+        4.Calculate Checksum PVC1_FS + PVC1_RBD
+        5.Fill the cluster to “Full ratio” (usually 85%) with benchmark-operator
+        """
+        self.benchmark_operator_teardown = False
+        project_name = "system-test-fullcluster"
+        self.project_obj = helpers.create_project(project_name=project_name)
+        teardown_project_factory(self.project_obj)
+
+        logger.info("Create PVC1 CEPH-RBD, Run FIO and get checksum")
+        pvc_obj_rbd1 = pvc_factory(
+            interface=constants.CEPHBLOCKPOOL,
+            project=self.project_obj,
+            size=2,
+            status=constants.STATUS_BOUND,
+        )
+        pod_rbd1_obj = pod_factory(
+            interface=constants.CEPHFILESYSTEM,
+            pvc=pvc_obj_rbd1,
+            status=constants.STATUS_RUNNING,
+        )
+        pod_rbd1_obj.run_io(
+            storage_type="fs",
+            size="1G",
+            io_direction="write",
+            runtime=60,
+        )
+        pod_rbd1_obj.get_fio_results()
+        logger.info(f"IO finished on pod {pod_rbd1_obj.name}")
+        pod_rbd1_obj.md5 = cal_md5sum(
+            pod_obj=pod_rbd1_obj,
+            file_name="fio-rand-write",
+            block=False,
+        )
+
+        logger.info("Create PVC1 CEPH-FS, Run FIO and get checksum")
+        pvc_obj_fs1 = pvc_factory(
+            interface=constants.CEPHFILESYSTEM,
+            project=self.project_obj,
+            size=2,
+            status=constants.STATUS_BOUND,
+        )
+        pod_fs1_obj = pod_factory(
+            interface=constants.CEPHFILESYSTEM,
+            pvc=pvc_obj_fs1,
+            status=constants.STATUS_RUNNING,
+        )
+        pod_fs1_obj.run_io(
+            storage_type="fs",
+            size="1G",
+            io_direction="write",
+            runtime=60,
+        )
+        pod_fs1_obj.get_fio_results()
+        logger.info(f"IO finished on pod {pod_fs1_obj.name}")
+        pod_fs1_obj.md5 = cal_md5sum(
+            pod_obj=pod_fs1_obj,
+            file_name="fio-rand-write",
+            block=False,
+        )
+
+        logger.info(
+            "Fill the cluster to “Full ratio” (usually 85%) with benchmark-operator"
+        )
+        size = get_file_size(100)
+        self.benchmark_obj = BenchmarkOperatorFIO()
+        self.benchmark_obj.setup_benchmark_fio(total_size=size)
+        self.benchmark_obj.run_fio_benchmark_operator(is_completed=False)
+        self.benchmark_operator_teardown = True
+
+        logger.info("Verify used capacity bigger than 85%")
+        sample = TimeoutSampler(
+            timeout=2500,
+            sleep=40,
+            func=verify_osd_used_capacity_greater_than_expected,
+            expected_used_capacity=85.0,
+        )
+        if not sample.wait_for_func_status(result=True):
+            logger.error("The after 1800 seconds the used capacity smaller than 85%")
+            raise TimeoutExpiredError
+
+
+def verify_osd_used_capacity_greater_than_expected(expected_used_capacity):
+    """
+    Verify OSD percent used capacity greate than ceph_full_ratio
+
+    Args:
+        expected_used_capacity (float): expected used capacity
+
+    Returns:
+         bool: True if used_capacity greater than expected_used_capacity, False otherwise
+
+    """
+    used_capacity = get_percent_used_capacity()
+    logger.info(f"Used Capacity is {used_capacity}%")
+    ceph_df_detail = get_ceph_df_detail()
+    logger.info(f"ceph df detail: {ceph_df_detail}")
+    osds_utilization = get_osd_utilization()
+    logger.info(f"osd utilization: {osds_utilization}")
+    for osd_id, osd_utilization in osds_utilization.items():
+        if osd_utilization > expected_used_capacity:
+            logger.info(f"OSD ID:{osd_id}:{osd_utilization} greater than 85%")
+            return True
+    return False
--- ocs_ci/helpers/keyrotation_helper.py
+++ ocs_ci/helpers/keyrotation_helper.py
@@ -646,3 +646,119 @@ class PVKeyrotation(KeyRotation):
 
         log.info("Completed key rotation state changes for all specified PVCs.")
         return True
+
+def enable_key_rotation():
+    """
+    Enable Key rotation and verify its status for Noobaa and Storage cluster.
+
+    """
+    osd_keyrotation = OSDKeyrotation()
+    noobaa_keyrotation = NoobaaKeyrotation()
+    osd_keyrotation.enable_keyrotation()
+    noobaa_keyrotation.enable_keyrotation()
+
+    assert (
+        osd_keyrotation.is_keyrotation_enable()
+    ), "Encryption Key rotation is not enabled for OSDs"
+    assert (
+        noobaa_keyrotation.is_keyrotation_enable
+    ), "Keyrotation is not enabled in the storagecluster object."
+    assert (
+        noobaa_keyrotation.is_noobaa_keyrotation_enable
+    ), "Keyrotation is not enabled in the noobaa object."
+
+
+def set_key_rotation_time(value):
+    """
+    This function will edit the storage cluster and add the value in key rotation schedule
+    Args:
+         value: (str) number of minutes
+
+    """
+    osd_keyrotation = OSDKeyrotation()
+    schedule = f"*/{value} * * * *"
+    osd_keyrotation.set_keyrotation_schedule(schedule)
+
+
+def verify_key_rotation_time(schedule):
+    """
+    Verify Key rotation schedule changed at storage cluster, rook and Noobaa object.
+
+    Args:
+        schedule: (str)
+
+    """
+    osd_keyrotation = OSDKeyrotation()
+    noobaa_keyrotation = NoobaaKeyrotation()
+    assert (
+        osd_keyrotation.get_keyrotation_schedule() == schedule
+    ), f"Keyrotation schedule is not set to {schedule} minutes in storage cluster object."
+    assert (
+        osd_keyrotation.get_osd_keyrotation_schedule() == schedule
+    ), "KeyRotation is not enabled in the Rook Object."
+    assert (
+        noobaa_keyrotation.get_noobaa_keyrotation_schedule() == schedule
+    ), f"Keyrotation schedule is not set to every {schedule} minutes in Noobaa object."
+
+
+def verify_new_key_after_rotation(tries, delays):
+    osd_keyrotation = OSDKeyrotation()
+    noobaa_keyrotation = NoobaaKeyrotation()
+
+    log.info("Record existing OSD keys before rotation is happened.")
+    osd_keys_before_rotation = {}
+    for device in osd_keyrotation.deviceset:
+        osd_keys_before_rotation[device] = osd_keyrotation.get_osd_dm_crypt(device)
+
+    log.info("Record Noobaa volume and backend keys before rotation.")
+    (
+        old_noobaa_backend_key,
+        old_noobaa_backend_secret,
+    ) = noobaa_keyrotation.get_noobaa_backend_secret()
+    log.info(
+        f" Noobaa backend secrets before Rotation {old_noobaa_backend_key} : {old_noobaa_backend_secret}"
+    )
+
+    (
+        old_noobaa_volume_key,
+        old_noobaa_volume_secret,
+    ) = noobaa_keyrotation.get_noobaa_volume_secret()
+    log.info(
+        f"Noobaa Volume secrets before Rotation {old_noobaa_volume_key} : {old_noobaa_volume_secret}"
+    )
+
+    @retry(UnexpectedBehaviour, tries=tries, delay=delays)
+    def compare_old_keys_with_new_keys():
+        """
+        Compare old keys with new keys.
+
+        """
+        (
+            new_noobaa_backend_key,
+            new_noobaa_backend_secret,
+        ) = noobaa_keyrotation.get_noobaa_backend_secret()
+        (
+            new_noobaa_volume_key,
+            new_noobaa_volume_secret,
+        ) = noobaa_keyrotation.get_noobaa_volume_secret()
+
+        if new_noobaa_backend_key == old_noobaa_backend_key:
+            raise UnexpectedBehaviour("Noobaa Key Rotation is not happend")
+
+        if new_noobaa_volume_key == old_noobaa_volume_key:
+            raise UnexpectedBehaviour("Noobaa Key Rotation is not happend.")
+
+        log.info(
+            f"Noobaa Backend key rotated {new_noobaa_backend_key} : {new_noobaa_backend_secret}"
+        )
+        log.info(
+            f"Noobaa Volume key rotated {new_noobaa_volume_key} : {new_noobaa_volume_secret}"
+        )
+
+    try:
+        osd_keyrotation.verify_keyrotation(osd_keys_before_rotation, tries=10, delay=30)
+        compare_old_keys_with_new_keys()
+
+    except UnexpectedBehaviour:
+        log.error("Key rotation is Not happened after schedule is passed. ")
+        assert False
