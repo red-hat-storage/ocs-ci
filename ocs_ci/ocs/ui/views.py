2afeb90bf38a25c15f8013bd32fd82c9fd94aa7f
Elena Bondarenko <ebondare@redhat.com>
2024-10-24 11:42:14 +0200

    Fix storagesystem link
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/ocs/ui/views.py
+++ ocs_ci/ocs/ui/views.py
@@ -1685,7 +1685,8 @@ validation_4_10 = {
         By.XPATH,
     ),
     "ocs-external-storagecluster-storagesystem": (
-        "a[href='/odf/system/ocs.openshift.io~v1~storagecluster/ocs-external-storagecluster-storagesystem/overview']",
+        "a[href='/odf/system/ocs.openshift.io~v1~storagecluster/ocs-external-storagecluster-storagesystem/overview'], "
+        "a[href='odf/system/ns/openshift-storage/ocs.openshift.io~v1~storagecluster/ocs-external-storagecluster-storagesystem/overview']",
         By.CSS_SELECTOR,
     ),
     "performance-card": ("//div[contains(text(),'Performance')]", By.XPATH),
