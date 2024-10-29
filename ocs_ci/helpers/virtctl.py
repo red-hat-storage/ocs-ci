7279553a7a7f990852e99288b442fce77b3670a2
Daniel Horak <dahorak@redhat.com>
2024-10-29 10:27:48 +0100

    add log message about fallback with exception
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

--- ocs_ci/helpers/virtctl.py
+++ ocs_ci/helpers/virtctl.py
@@ -23,13 +23,17 @@ def get_virtctl_tool():
         try:
             cnv_obj = CNVInstaller()
             cnv_obj.download_and_extract_virtctl_binary()
-            virtctl_version = exec_cmd("virtctl version -c")
-        except Exception:
+            virtctl_version = exec_cmd("virtctl version -c", silent=True)
+        except Exception as err:
+            logger.info(
+                f"Downloading or extracting virtctl binary from OCP Cluster failed: {err}. "
+                "Falling back to download it from upstream github repo."
+            )
             virtctl_release_tag = config.ENV_DATA.get("virtctl_release_tag", "latest")
             get_asset_from_github(
                 name="virtctl",
                 owner_repo=config.ENV_DATA.get("virtctl_owner_repo"),
                 release_tag=virtctl_release_tag,
             )
-            virtctl_version = exec_cmd("virtctl version -c")
+            virtctl_version = exec_cmd("virtctl version -c", silent=True)
     logger.info(f"virtctl tool is available: {virtctl_version.stdout.decode('utf-8')}")
