8d4bd967b2fa824bd850c7c142b86239089afcd4
prsurve <prsurve@redhat.com>
2024-11-05 18:07:57 +0530

    Fix tox
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/ocs/resources/storage_cluster.py
+++ ocs_ci/ocs/resources/storage_cluster.py
@@ -796,9 +796,10 @@ def ocs_install_verification(
             verify_device_class_in_osd_tree(ct_pod, device_class)
 
     # RDR with globalnet submariner
-    if (
-        config.MULTICLUSTER["multicluster_mode"] == "regional-dr" and
-        get_primary_cluster_config().ENV_DATA.get("enable_globalnet", True)
+    if config.MULTICLUSTER[
+        "multicluster_mode"
+    ] == "regional-dr" and get_primary_cluster_config().ENV_DATA.get(
+        "enable_globalnet", True
     ):
         validate_serviceexport()
 
