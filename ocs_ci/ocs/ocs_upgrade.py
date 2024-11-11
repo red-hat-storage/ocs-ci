25c43809738ceee079a04f532adf076599442d8e
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2024-11-11 23:56:40 +0530

    Commented acm upgrade for the time being and removed changes done for ocs_upgrade
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- ocs_ci/ocs/ocs_upgrade.py
+++ ocs_ci/ocs/ocs_upgrade.py
@@ -38,7 +38,6 @@ from ocs_ci.ocs.resources.daemonset import DaemonSet
 from ocs_ci.ocs.resources.csv import (
     CSV,
     check_all_csvs_are_succeeded,
-    get_csvs_start_with_prefix,
 )
 from ocs_ci.ocs.resources.install_plan import wait_for_install_plan_and_approve
 from ocs_ci.ocs.resources.pod import get_noobaa_pods, verify_pods_upgraded
@@ -69,7 +68,6 @@ from ocs_ci.utility.templating import dump_data_to_temp_yaml
 from ocs_ci.ocs.exceptions import (
     TimeoutException,
     ExternalClusterRGWAdminOpsUserException,
-    CSVNotFound,
 )
 from ocs_ci.ocs.ui.base_ui import logger, login_ui
 from ocs_ci.ocs.ui.views import locators, ODF_OPERATOR
@@ -419,7 +417,9 @@ class OCSUpgrade(object):
             config.REPORTING["ocs_must_gather_image"] = must_gather_image
             config.REPORTING["ocs_must_gather_latest_tag"] = must_gather_tag
 
-    def get_csv_name_pre_upgrade(self, resource_name=OCS_OPERATOR_NAME):
+    def get_csv_name_pre_upgrade(
+        self,
+    ):
         """
         Get pre-upgrade CSV name
 
@@ -431,16 +431,15 @@ class OCSUpgrade(object):
 
         """
 
-        csv_name = None
-        csv_list = get_csvs_start_with_prefix(resource_name, namespace=self.namespace)
-        for csv in csv_list:
-            if resource_name in csv.get("metadata").get("name"):
-                if config.PREUPGRADE_CONFIG.get("ENV_DATA").get(
-                    "ocs_version"
-                ) in csv.get("metadata").get("name"):
-                    csv_name = csv.get("metadata").get("name")
-                    return csv_name
-        raise CSVNotFound(f"No preupgrade CSV found for {resource_name}")
+        operator_selector = get_selector_for_ocs_operator()
+        package_manifest = PackageManifest(
+            resource_name=OCS_OPERATOR_NAME,
+            selector=operator_selector,
+            subscription_plan_approval=self.subscription_plan_approval,
+        )
+        channel = config.DEPLOYMENT.get("ocs_csv_channel")
+
+        return package_manifest.get_current_csv(channel)
 
     def get_pre_upgrade_image(self, csv_name_pre_upgrade):
         """
