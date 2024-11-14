4de75e7c6ee3556230a9ba829150aae01ba98bb2
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2024-11-14 12:49:32 +0530

    Updated ocs upgrade
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- ocs_ci/ocs/ocs_upgrade.py
+++ ocs_ci/ocs/ocs_upgrade.py
@@ -448,6 +448,8 @@ class OCSUpgrade(object):
         kind_name = (
             "subscription.operators.coreos.com"
             if config.multicluster
+            else "sub"
+            if config.ENV_DATA["cluster_type"].lower() == constants.HCI_PROVIDER
             else "subscription"
         )
         subscription = OCP(
