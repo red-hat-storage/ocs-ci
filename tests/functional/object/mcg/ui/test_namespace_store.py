c05247b713ce9f89b496a978c140cd4e7a13f8b9
gayathrimenath-ibm <82875471+gayathrimenath-ibm@users.noreply.github.com>
2024-11-20 21:44:03 +0530

    IBM Cloud node stop and start added (#10707)
    * IBM Cloud node stop and start added
    
    Signed-off-by: Gayathri M <Gayathri.M@ibm.com>
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- tests/functional/object/mcg/ui/test_namespace_store.py
+++ tests/functional/object/mcg/ui/test_namespace_store.py
@@ -4,7 +4,11 @@ import pytest
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs import constants
 from ocs_ci.framework import config
-from ocs_ci.framework.pytest_customization.marks import black_squad, runs_on_provider
+from ocs_ci.framework.pytest_customization.marks import (
+    black_squad,
+    runs_on_provider,
+    skipif_ibm_cloud_managed,
+)
 from ocs_ci.framework.testlib import tier1, ui, polarion_id
 from ocs_ci.ocs.ui.mcg_ui import NamespaceStoreUI
 from ocs_ci.ocs.resources.namespacestore import NamespaceStore
@@ -32,6 +36,7 @@ class TestNamespaceStoreUI(object):
     @ui
     @tier1
     @runs_on_provider
+    @skipif_ibm_cloud_managed
     @pytest.mark.bugzilla("2158922")
     @polarion_id("OCS-5125")
     def test_create_namespace_store_ui(self, setup_ui_class_factory, pvc_factory):
