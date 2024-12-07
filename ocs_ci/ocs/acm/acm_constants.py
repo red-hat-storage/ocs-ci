1714382a762d6788b429ec2fbfc3fce6c1e98899
Petr Balogh <pbalogh@redhat.com>
2024-12-08 00:22:03 +0100

    Change ACM Page title for IBM Cloud ROKS
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- ocs_ci/ocs/acm/acm_constants.py
+++ ocs_ci/ocs/acm/acm_constants.py
@@ -7,4 +7,5 @@ ACM_MANAGED_CLUSTERS = "managedclusters"
 # Misc.
 ACM_PAGE_TITLE = "Red Hat Advanced Cluster Management for Kubernetes"
 ACM_PAGE_TITLE_2_7_ABOVE = "Red Hat OpenShift"
+ACM_PAGE_TITLE_2_7_ABOVE_IBM_CLOUD_MANAGED = "Red Hat OpenShift Container Platform"
 ACM_2_7_MULTICLUSTER_URL = "/multicloud/infrastructure/clusters/managed"
