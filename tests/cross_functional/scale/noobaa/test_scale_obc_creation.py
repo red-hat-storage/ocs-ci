3c94039b1050ec9bd236ebfce0662c1f89995f25
udaysk23 <54358025+udaysk23@users.noreply.github.com>
2024-12-11 13:43:56 +0530

    Removes redundant tests from Orange squad (#10945)
    Signed-off-by: Uday Kurundwade <Uday.Shamrao.Kurundwade@ibm.com>

--- tests/cross_functional/scale/noobaa/test_scale_obc_creation.py
+++ tests/cross_functional/scale/noobaa/test_scale_obc_creation.py
@@ -41,7 +41,7 @@ class TestScaleOCBCreation(E2ETest):
     num_obc_batch = 50
 
     @pytest.mark.polarion_id("OCS-2478")
-    def test_scale_mcg_obc_creation(
+    def deprecated_test_scale_mcg_obc_creation(
         self, tmp_path, noobaa_storage_class_name, timeout=60
     ):
         """
@@ -84,7 +84,7 @@ class TestScaleOCBCreation(E2ETest):
 
     @vsphere_platform_required
     @pytest.mark.polarion_id("OCS-2479")
-    def test_scale_rgw_obc_creation(
+    def deprecated_test_scale_rgw_obc_creation(
         self, tmp_path, noobaa_storage_class_name, timeout=60
     ):
         """
