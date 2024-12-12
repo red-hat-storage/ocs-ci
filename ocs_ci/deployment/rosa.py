dfb973407d4c5101c108e12f836a050fe2024a1d
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2024-12-12 17:06:08 +0100

    [release-4.17] Rosa hcp node labeling (#11035)
    * label nodes for node stop and autoscaling
    
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- ocs_ci/deployment/rosa.py
+++ ocs_ci/deployment/rosa.py
@@ -100,6 +100,11 @@ class ROSAOCP(BaseOCPDeployment):
             machinepool_details.wait_replicas_ready(
                 target_replicas=config.ENV_DATA["worker_replicas"], timeout=1200
             )
+            if node_labels := config.ENV_DATA.get("node_labels"):
+                if machinepool_id := config.ENV_DATA.get("machine_pool"):
+                    rosa.label_nodes(
+                        self.cluster_name, machinepool_id, node_labels, rewrite=False
+                    )
 
         logger.info("generate kubeconfig and kubeadmin-password files")
         if config.ENV_DATA["ms_env_type"] == "staging":
