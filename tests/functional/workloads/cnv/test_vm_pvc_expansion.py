15443373024470866a9d849c5b8ce2452889ca71
AYUSH-D-PATNI <AYUSH.D.PATNI@ibm.com>
2024-12-20 14:02:12 +0530

    added pvc_clone_factory fixture and better logs
    Signed-off-by: AYUSH-D-PATNI <AYUSH.D.PATNI@ibm.com>

--- tests/functional/workloads/cnv/test_vm_pvc_expansion.py
+++ tests/functional/workloads/cnv/test_vm_pvc_expansion.py
@@ -18,26 +18,25 @@ class TestVmPvcExpansion(E2ETest):
 
     def test_pvc_expansion(self, cnv_workload):
         """
-        Test PVC expansion for a CNV VM workload
+        Test PVC expansion for a CNV VM workload.
         """
         vm_obj = cnv_workload(
             volume_interface=constants.VM_VOLUME_PVC,
             source_url=constants.CNV_FEDORA_SOURCE,
         )[-1]
         vm_pvc_obj = vm_obj.get_vm_pvc_obj()
-        log.info(vm_pvc_obj.size)
-        # writing data to the PVC
-        file_paths = "/source_file.txt"
-        source_csum = run_dd_io(vm_obj=vm_obj, file_path=file_paths, verify=True)
-        log.info(source_csum)
-        # Resize PVC to a random size between 31 and 35 GiB as 30GiB is default
-        new_size = random.randint(31, 35)
-        log.info(f"Size of VM PVC before expansion: {vm_pvc_obj.size}")
-        vm_pvc_obj.resize_pvc(new_size)
-        log.info(f"Size of VM PVC after expansion: {vm_pvc_obj.size}")
-        res_csum = cal_md5sum_vm(vm_obj=vm_obj, file_path=file_paths)
-        log.info(res_csum)
-        assert source_csum == res_csum and vm_obj.get_vm_pvc_obj().size == new_size, (
-            f"Failed: Either VM PVC Expansion or MD5 comparison of {vm_obj.name} before and after "
-            f"PVC expansion"
+        log.info(f"Initial PVC size: {vm_pvc_obj.size} GiB")
+        file_path = "/source_file.txt"
+        source_csum = run_dd_io(vm_obj=vm_obj, file_path=file_path, verify=True)
+        log.info(f"Checksum before resize: {source_csum}")
+        new_size = random.randint(vm_pvc_obj.size + 1, vm_pvc_obj.size + 5)
+        log.info(f"Resizing PVC to {new_size} GiB")
+        vm_pvc_obj.resize_pvc(new_size, True)
+        vm_pvc_obj_n = vm_obj.get_vm_pvc_obj()
+        log.info(f"New PVC size: {vm_pvc_obj_n.size} GiB")
+        res_csum = cal_md5sum_vm(vm_obj=vm_obj, file_path=file_path)
+        log.info(f"Checksum after resize: {res_csum}")
+        assert source_csum == res_csum and vm_pvc_obj_n.size == new_size, (
+            f"Failed: PVC expansion or MD5 mismatch for VM '{vm_obj.name}'. "
+            f"Expected size: {new_size} GiB, but got: {vm_pvc_obj.size} GiB."
         )
