24bc32760417f95caf5762f9ad8f0be6c8605c35
prsurve <prsurve@redhat.com>
2025-02-03 11:22:05 +0530

    Resolve comments
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/helpers/helpers.py
+++ ocs_ci/helpers/helpers.py
@@ -29,7 +29,7 @@ from ocs_ci.helpers.proxy import (
     get_cluster_proxies,
     update_container_with_proxy_env,
 )
-from ocs_ci.helpers.pvc_ops import delete_pods
+from ocs_ci.ocs.resources.pod import delete_pods
 from ocs_ci.ocs.utils import (
     get_non_acm_cluster_config,
     get_pod_name_by_pattern,
@@ -5453,7 +5453,7 @@ def update_volsync_icsp():
             namespace=constants.VOLSYNC_NAMESPACE,
             selector="app.kubernetes.io/name=volsync",
         )
-        delete_pods(volsync_pod_list)
+        delete_pods(volsync_pod_list, wait=True)
         logger.info("Verify volsync-controller-manager pods in Running state")
         sample = TimeoutSampler(
             timeout=300,
