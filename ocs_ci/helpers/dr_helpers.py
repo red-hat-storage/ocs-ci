56525fb8f73a20832e246440e920157559224edb
Jilju Joy <jijoy@redhat.com>
2025-02-03 22:38:44 +0530

    temporary change
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/helpers/dr_helpers.py
+++ ocs_ci/helpers/dr_helpers.py
@@ -10,7 +10,6 @@ from datetime import datetime
 
 from ocs_ci.framework import config
 from ocs_ci.ocs import constants, ocp
-from ocs_ci.ocs.cluster import is_hci_cluster
 from ocs_ci.ocs.defaults import RBD_NAME
 from ocs_ci.ocs.exceptions import (
     TimeoutExpiredError,
@@ -311,7 +310,8 @@ def check_mirroring_status_ok(
         NotFoundError: If the configuration is provider mode and the name of the cephblockpoolradosnamespace
             is not obtained
     """
-    if is_hci_cluster():
+    # TODO: Check based on provider mode or not
+    if not cephblockpoolradosns:
         logging.info(
             "Find the cephblockpoolradosnamespace the associated with storageclient"
         )
@@ -1012,7 +1012,8 @@ def verify_backend_volume_deletion(
         if config.DEPLOYMENT["external_mode"]
         else constants.DEFAULT_CEPHBLOCKPOOL
     )
-    if is_hci_cluster():
+    # TODO: Check based on provider mode or not
+    if not cephblockpoolradosns:
         cephbpradosns = (
             config.ENV_DATA.get("radosnamespace_name", False) or cephblockpoolradosns
         )
