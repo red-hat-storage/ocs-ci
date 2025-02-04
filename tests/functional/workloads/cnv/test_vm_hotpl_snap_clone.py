ca82ec8f7482fb24c1027e1c4b56d73f8ae669c2
AYUSH-D-PATNI <AYUSH.D.PATNI@ibm.com>
2025-02-04 18:02:49 +0530

    upd with timeoutsampler
    Signed-off-by: AYUSH-D-PATNI <AYUSH.D.PATNI@ibm.com>

--- tests/functional/workloads/cnv/test_vm_hotpl_snap_clone.py
+++ tests/functional/workloads/cnv/test_vm_hotpl_snap_clone.py
@@ -1,12 +1,16 @@
 import logging
-import time
 import pytest
 
 from ocs_ci.framework.pytest_customization.marks import magenta_squad, workloads
 from ocs_ci.framework.testlib import E2ETest
-from ocs_ci.helpers.cnv_helpers import cal_md5sum_vm, run_dd_io, verifyvolume
-from ocs_ci.helpers.helpers import create_pvc
+from ocs_ci.helpers.cnv_helpers import (
+    cal_md5sum_vm,
+    run_dd_io,
+    verifyvolume,
+    verify_hotplug,
+)
 from ocs_ci.ocs import constants
+from ocs_ci.utility.utils import TimeoutSampler
 
 log = logging.getLogger(__name__)
 
@@ -21,10 +25,11 @@ class TestVmHotPlugUnplugSnapClone(E2ETest):
 
     def test_vm_hotpl_snap_clone(
         self,
-        setup_cnv,
+        # setup_cnv,
         project_factory,
         cnv_workload,
         pvc_clone_factory,
+        pvc_factory,
     ):
         """
         A running DVT based VM and a PVC based VM
@@ -40,61 +45,68 @@ class TestVmHotPlugUnplugSnapClone(E2ETest):
         proj_obj = project_factory()
         file_paths = ["/source_file.txt", "/new_file.txt"]
 
-        # Create VMs
+        # Create a PVC-based VM (VM1)
         vm_obj_pvc = cnv_workload(
             namespace=proj_obj.namespace, volume_interface=constants.VM_VOLUME_PVC
         )
+
+        # Create the PVC for VM1
+        pvc_obj = pvc_factory(
+            project=proj_obj,
+            size=20,
+            access_mode=constants.ACCESS_MODE_RWX,
+            volume_mode=constants.VOLUME_MODE_BLOCK,
+        )
+        log.info(f"PVC {pvc_obj.name} created successfully")
+
+        # Create a DVT-based VM (VM2)
         vm_obj_dvt = cnv_workload(
             namespace=proj_obj.namespace, volume_interface=constants.VM_VOLUME_DVT
         )
-
-        dvt_obj = create_pvc(
-            sc_name=vm_obj_dvt.sc_name,
-            namespace=vm_obj_dvt.namespace,
-            size="20Gi",
+        dvt_obj = pvc_factory(
+            project=proj_obj,
+            size=20,
             access_mode=constants.ACCESS_MODE_RWX,
             volume_mode=constants.VOLUME_MODE_BLOCK,
         )
-        pvc_obj = create_pvc(
-            sc_name=vm_obj_pvc.sc_name,
-            namespace=vm_obj_pvc.namespace,
-            size="20Gi",
-            access_mode=constants.ACCESS_MODE_RWX,
-            volume_mode=constants.VOLUME_MODE_BLOCK,
-        )
-
-        log.info(f"PVCs {dvt_obj.name} and {pvc_obj.name} created successfully")
+        log.info(f"PVC {dvt_obj.name} created successfully")
 
         # List of VM-PVC pairs for hotplug testing
         vms_pvc = [(vm_obj_pvc, pvc_obj), (vm_obj_dvt, dvt_obj)]
-        before_disks_hotpl = []
+        before_disks_hotplug = []
 
         # Hotplug disks and perform I/O operations
         for i, (vm_obj, pvc) in enumerate(vms_pvc):
             # Verify disks before hotplugging
             before_disks = vm_obj.run_ssh_cmd("lsblk -o NAME,SIZE,MOUNTPOINT -P")
             log.info(f"Disks before hotplug on VM {vm_obj.name}:\n{before_disks}")
-            before_disks_hotpl.append(before_disks)
+            before_disks_hotplug.append(before_disks)
 
-            # Hotplug the PVC volume
+            # Hotplug the PVC volume to the VM
+            log.info(f"Hotplugging PVC {pvc.name} to VM {vm_obj.name}")
             vm_obj.addvolume(volume_name=pvc.name, verify=True)
+
+            # Wait for the disk to be hotplugged successfully
+            sample = TimeoutSampler(
+                timeout=600,
+                sleep=5,
+                func=verify_hotplug,
+                vm_obj=vm_obj,
+                hot_pl_disk_raw=before_disks,
+            )
+            sample.wait_for_func_value(value=True)
             log.info(f"Hotplugged PVC {pvc.name} to VM {vm_obj.name}")
-            time.sleep(5)
 
-            # Verify disks after hotplugging
-            after_disks = vm_obj.run_ssh_cmd("lsblk -o NAME,SIZE,MOUNTPOINT -P")
-            log.info(f"Disks after hotplug on VM {vm_obj.name}:\n{after_disks}")
-
-            # Run I/O operation on the new disk
+            # Run I/O operation
             log.info(f"Running I/O operation on VM {vm_obj.name}")
             source_csum = run_dd_io(vm_obj=vm_obj, file_path=file_paths[0], verify=True)
 
-            # Reboot the VM and verify disk persistence
+            # Reboot the VM
             log.info(f"Rebooting VM {vm_obj.name}")
             vm_obj.restart(wait=True, verify=True)
             log.info(f"Reboot Success for VM: {vm_obj.name}")
 
-            # Verify disk is still attached
+            # Verify disk is still attached after reboot
             assert verifyvolume(
                 vm_obj.name, volume_name=pvc.name, namespace=vm_obj.namespace
             ), f"Unable to find volume {pvc.name} mounted on VM: {vm_obj.name}"
@@ -106,41 +118,82 @@ class TestVmHotPlugUnplugSnapClone(E2ETest):
             ), f"MD5 mismatch after reboot for VM {vm_obj.name}"
 
         # Create PVC clones and attach them to opposite VMs
+        log.info("Creating clones of PVCs")
         clone_obj_pvc = pvc_clone_factory(pvc_obj)
         clone_obj_dvt = pvc_clone_factory(dvt_obj)
         log.info(f"Clones of PVCs {pvc_obj.name} and {dvt_obj.name} created!")
 
         # Attach clones to the opposite VMs
+        log.info(f"Attaching clone of {dvt_obj.name} to VM {vm_obj_pvc.name}")
+        before_disks_pvc = vm_obj_pvc.run_ssh_cmd("lsblk -o NAME,SIZE,MOUNTPOINT -P")
+        log.info(
+            f"Disks before clone hotplug on VM {vm_obj_pvc.name}:\n{before_disks_pvc}"
+        )
         vm_obj_pvc.addvolume(volume_name=clone_obj_dvt.name, persist=False)
+        sample = TimeoutSampler(
+            timeout=600,
+            sleep=5,
+            func=verify_hotplug,
+            vm_obj=vm_obj_pvc,
+            hot_pl_disk_raw=before_disks_pvc,
+        )
+        sample.wait_for_func_value(value=True)
+
+        log.info(f"Attaching clone of {pvc_obj.name} to VM {vm_obj_dvt.name}")
+        before_disks_dvt = vm_obj_dvt.run_ssh_cmd("lsblk -o NAME,SIZE,MOUNTPOINT -P")
+        log.info(
+            f"Disks before clone hotplug on VM {vm_obj_dvt.name}:\n{before_disks_dvt}"
+        )
         vm_obj_dvt.addvolume(volume_name=clone_obj_pvc.name, persist=False)
+        sample = TimeoutSampler(
+            timeout=600,
+            sleep=5,
+            func=verify_hotplug,
+            vm_obj=vm_obj_dvt,
+            hot_pl_disk_raw=before_disks_dvt,
+        )
+        sample.wait_for_func_value(value=True)
 
         # Run I/O on the cloned disks
+        log.info(f"Running I/O operation on cloned PVC {clone_obj_pvc.name}")
         run_dd_io(vm_obj=vm_obj_pvc, file_path=file_paths[1])
+        log.info(f"Running I/O operation on cloned DVT {clone_obj_dvt.name}")
         run_dd_io(vm_obj=vm_obj_dvt, file_path=file_paths[1])
 
         # Unplug cloned disks and verify detachment
+        log.info(f"Unplugging clone of {dvt_obj.name} from VM {vm_obj_pvc.name}")
+        before_disks_pvc_rm = vm_obj_pvc.run_ssh_cmd("lsblk -o NAME,SIZE,MOUNTPOINT -P")
+        log.info(
+            f"Disks before unplugging clone from VM {vm_obj_pvc.name}:\n{before_disks_pvc_rm}"
+        )
         vm_obj_pvc.removevolume(volume_name=clone_obj_dvt.name, verify=True)
+        sample = TimeoutSampler(
+            timeout=600,
+            sleep=5,
+            func=verify_hotplug,
+            vm_obj=vm_obj_pvc,
+            hot_pl_disk_raw=before_disks_pvc_rm,
+        )
+        sample.wait_for_func_value(value=True)
+
+        log.info(f"Unplugging clone of {pvc_obj.name} from VM {vm_obj_dvt.name}")
+        before_disks_dvt_rm = vm_obj_dvt.run_ssh_cmd("lsblk -o NAME,SIZE,MOUNTPOINT -P")
+        log.info(
+            f"Disks before unplugging clone from VM {vm_obj_dvt.name}:\n{before_disks_dvt_rm}"
+        )
         vm_obj_dvt.removevolume(volume_name=clone_obj_pvc.name, verify=True)
+        sample = TimeoutSampler(
+            timeout=600,
+            sleep=5,
+            func=verify_hotplug,
+            vm_obj=vm_obj_dvt,
+            hot_pl_disk_raw=before_disks_dvt_rm,
+        )
+        sample.wait_for_func_value(value=True)
 
-        # Verify disk detachment for both VMs
-        after_disks_hotpl_pvc = vm_obj_pvc.run_ssh_cmd(
-            "lsblk -o NAME,SIZE,MOUNTPOINT -P"
-        )
-        after_disks_hotpl_dvt = vm_obj_dvt.run_ssh_cmd(
-            "lsblk -o NAME,SIZE,MOUNTPOINT -P"
-        )
+        # Stop the VMs after the test
+        log.info(f"Stopping VM {vm_obj_pvc.name}")
+        vm_obj_pvc.stop()
 
-        log.info(
-            f"Disks after removing clone hotplug from PVC VM:\n{after_disks_hotpl_pvc}"
-        )
-        log.info(
-            f"Disks after removing clone hotplug from DVT VM:\n{after_disks_hotpl_dvt}"
-        )
-
-        # Verify that the disks are detached properly
-        assert (
-            after_disks_hotpl_pvc == before_disks_hotpl[0]
-        ), f"Hotplug removal failed on PVC VM {vm_obj_pvc} after clone removal"
-        assert (
-            after_disks_hotpl_dvt == before_disks_hotpl[1]
-        ), f"Hotplug removal failed on DVT VM {vm_obj_dvt} after clone removal"
+        log.info(f"Stopping VM {vm_obj_dvt.name}")
+        vm_obj_dvt.stop()
