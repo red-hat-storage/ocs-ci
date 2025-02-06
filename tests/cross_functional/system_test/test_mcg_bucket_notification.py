a1a59f05155858874c301cc736eb258a7eb961ec
Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>
2025-02-06 13:14:06 +0530

    More code
    Signed-off-by: Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>

--- tests/cross_functional/system_test/test_mcg_bucket_notification.py
+++ tests/cross_functional/system_test/test_mcg_bucket_notification.py
@@ -2,21 +2,26 @@ import pytest
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-# from ocs_ci.ocs import constants
 from ocs_ci.ocs.bucket_utils import (
     write_random_test_objects_to_bucket,
     s3_delete_object,
     s3_put_object_tagging,
+    expire_objects_in_bucket,
 )
 from ocs_ci.ocs.resources.bucket_notifications_manager import (
     BucketNotificationsManager,
     logger,
 )
+from ocs_ci.ocs.resources.mcg_lifecycle_policies import (
+    LifecyclePolicy,
+    ExpirationRule,
+    LifecycleFilter,
+)
 from ocs_ci.ocs.resources.pod import (
-    # get_noobaa_pods,
-    # get_pod_node,
+    get_pod_node,
     get_pods_having_label,
     Pod,
+    get_noobaa_core_pod,
 )
 from ocs_ci.utility.utils import TimeoutSampler
 
@@ -42,11 +47,13 @@ class TestBucketNotificationSystemTest:
         notif_manager.setup_kafka()
         return notif_manager
 
-    def verify_events(self, notify_manager, topic, bucket_names, event_name):
+    def verify_events(
+        self, notify_manager, topic, bucket_names, event_name, timeout=120, sleep=5
+    ):
 
         for events in TimeoutSampler(
-            timeout=120,
-            sleep=5,
+            timeout=timeout,
+            sleep=sleep,
             func=notify_manager.get_events,
             topic=topic,
         ):
@@ -69,9 +76,14 @@ class TestBucketNotificationSystemTest:
         nodes,
         notify_manager,
         bucket_factory,
+        reduce_expiration_interval,
         awscli_pod,
         test_directory_setup,
         mcg_obj,
+        noobaa_db_backup_and_recovery,
+        snapshot_factory,
+        setup_mcg_bg_features,
+        validate_mcg_bg_features,
     ):
         """
         This is to test MCG bucket notification feature on multiple MCG buckets
@@ -81,8 +93,14 @@ class TestBucketNotificationSystemTest:
         Steps:
         1. Enable bucket notifications on the NooBaa CR
         2. Create a Kafka topic and add a Kafka notification connection to the NooBaa CR
-        3. Create 5 buckets and configure bucket notificiation for each of the s3 operation
+        3. Create 5 buckets and configure bucket notification for each of the s3 operation
         4. Perform Object upload and verify object upload events are received for all the buckets
+        5. Tag object from the bucket and restart noobaa pod nodes.
+           Verify ObjectTagging:Put events has occurred for all the buckets.
+        6. Remove object from the bucket and restart kafka pods. Verify ObjectRemoved:Delete event has occurred
+        7. Enable object expiration for 1 day and expire the objects manually.
+           Perform Noobaa db backup and recovery.
+           Make sure notifications are updated for LifecycleExpiration:Delete event
 
         """
 
@@ -95,12 +113,19 @@ class TestBucketNotificationSystemTest:
             "s3:ObjectTagging:*",
         ]
 
+        feature_setup_map = setup_mcg_bg_features(
+            num_of_buckets=5,
+            object_amount=5,
+            is_disruptive=True,
+            skip_any_features=["nsfs", "rgw kafka", "caching"],
+        )
+
         # 1. Enable bucket notification on Noobaa CR
         notify_manager.enable_bucket_notifs_on_cr(use_provided_pvc=True)
 
         # 2. Create kafka topic and add it to the Noobaa CR
         topic = notify_manager.create_kafka_topic()
-        secret, conn_file_name = notify_manager.create_kafka_conn_secret(topic)
+        secret, conn_config_path = notify_manager.create_kafka_conn_secret(topic)
         notify_manager.add_notif_conn_to_noobaa_cr(secret)
 
         # 3. Create the five buckets that is needed for the testing
@@ -113,7 +138,7 @@ class TestBucketNotificationSystemTest:
                 mcg_obj,
                 bucket.name,
                 events=events_to_notify,
-                conn_file=conn_file_name,
+                conn_config_path=conn_config_path,
             )
             buckets_created.append(bucket.name)
 
@@ -139,12 +164,7 @@ class TestBucketNotificationSystemTest:
 
             # 5. Tag object from the bucket and restart noobaa pod nodes
             # Verify ObjectTagging:Put events has occurred for all the buckets
-            # noobaa_pod_nodes = [
-            #     get_pod_node(pod_obj)
-            #     for pod_obj in get_noobaa_pods(
-            #         namespace=constants.OPENSHIFT_STORAGE_NAMESPACE
-            #     )
-            # ]
+            noobaa_pod_nodes = [get_pod_node(get_noobaa_core_pod())]
             future_objs = []
             for bucket in buckets_created:
                 logger.info(f"Tagging object {obj_written[1]} from the bucket {bucket}")
@@ -154,12 +174,12 @@ class TestBucketNotificationSystemTest:
                         mcg_obj,
                         bucket,
                         obj_written[1],
-                        [{"type": "media"}],
+                        [{"Key": "type", "Value": "media"}],
                     )
                 )
 
             logger.info("Stopping Noobaa pod nodes")
-            # nodes.stop_nodes(nodes=noobaa_pod_nodes)
+            nodes.stop_nodes(nodes=noobaa_pod_nodes)
             for future in as_completed(future_objs):
                 future.result()
 
@@ -174,7 +194,7 @@ class TestBucketNotificationSystemTest:
             )
 
             logger.info("Starting Noobaa pod nodes")
-            # nodes.start_nodes(nodes=noobaa_pod_nodes)
+            nodes.start_nodes(nodes=noobaa_pod_nodes)
 
             # 6. Remove object from the bucket and restart kafka pods
             # Verify ObjectRemoved event has occurred
@@ -211,3 +231,44 @@ class TestBucketNotificationSystemTest:
                 bucket_names=buckets_created[:],
                 event_name="ObjectRemoved:Delete",
             )
+
+        # 7. Enable object expiration for 1 day and expire the objects manually
+        # Perform noobaa db backup and recovery. Make sure notifications are updated
+        # for LifecycleExpiration:Delete event
+
+        for bucket in buckets_created:
+            logger.info(f"Setting object expiration on bucket {bucket}")
+            lifecycle_policy = LifecyclePolicy(
+                ExpirationRule(days=1, filter=LifecycleFilter())
+            )
+            mcg_obj.s3_client.put_bucket_lifecycle_configuration(
+                Bucket=bucket, LifecycleConfiguration=lifecycle_policy.as_dict()
+            )
+            expire_objects_in_bucket(bucket)
+
+        reduce_expiration_interval(interval=5)
+        logger.info("Reduced the object expiration interval to 5 minutes")
+
+        # Backup noobaa db and recovery
+        noobaa_db_backup_and_recovery(snapshot_factory=snapshot_factory)
+        logger.info("Noobaa db backup and recovery is completed")
+
+        logger.info(
+            "Verifying if LifecycleExpiration:Delete events has occurred for all the buckets"
+        )
+        self.verify_events(
+            notify_manager,
+            topic,
+            bucket_names=buckets_created[:],
+            event_name="LifecycleExpiration:Delete",
+            timeout=600,
+            sleep=30,
+        )
+
+        validate_mcg_bg_features(
+            feature_setup_map,
+            run_in_bg=False,
+            skip_any_features=["nsfs", "rgw kafka", "caching"],
+            object_amount=5,
+        )
+        logger.info("No issues seen with the MCG bg feature validation")
