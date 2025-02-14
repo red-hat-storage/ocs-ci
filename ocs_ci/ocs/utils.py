b9032c6c839504de84b53d00b6abebe75d017544
Parag Kamble <pakamble@redhat.com>
2025-02-14 19:40:24 +0530

    Fixed keyrotaion tests for KV2 version
    Signed-off-by: Parag Kamble <pakamble@redhat.com>
    
    Co-authored-by: Petr Balogh <petr-balogh@users.noreply.github.com>

--- ocs_ci/ocs/utils.py
+++ ocs_ci/ocs/utils.py
@@ -1925,9 +1925,14 @@ def collect_pod_container_rpm_package(dir_name):
     pods = pod.get_all_pods(namespace=cluster_namespace)
     ocp_obj = OCP(namespace=cluster_namespace)
     for pod_obj in pods:
-        pod_object = pod_obj.get()
-        if "detect-version" in pod_obj.name:
+        ignore_pods = (
+            constants.CONTROLLER_DETECT_VERSION_NAME,
+            constants.OSD_KEY_ROTATION_POD_NAME,
+            constants.ROOK_CEPH_DETECT_VERSION_POD_NAME,
+        )
+        if any(pod in pod_obj.name for pod in ignore_pods):
             continue
+        pod_object = pod_obj.get()
         pod_containers = pod_object.get("spec").get("containers")
         ocp_pod_obj = OCP(kind=constants.POD, namespace=cluster_namespace)
         pod_status = ocp_pod_obj.get_resource_status(pod_obj.name)
