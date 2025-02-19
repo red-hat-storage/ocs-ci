a92e4193e63db5a85722d3440ce3c08d23bc3c7a
Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>
2025-02-19 16:41:25 +0530

    testing
    Signed-off-by: Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>

--- ocs_ci/deployment/provider_client/storage_client_deployment.py
+++ ocs_ci/deployment/provider_client/storage_client_deployment.py
@@ -168,15 +168,15 @@ class ODFAndNativeStorageClientDeploymentOnProvider(object):
         #     log.info("local storage is already installed")
 
         # odf subscription for provider
-        self.odf_subscription_on_provider()
+        # self.odf_subscription_on_provider()
 
         # Check for rook ceph pods
-        assert self.pod_obj.wait_for_resource(
-            condition="Running",
-            selector="app=rook-ceph-operator",
-            resource_count=1,
-            timeout=600,
-        )
+        # assert self.pod_obj.wait_for_resource(
+        #     condition="Running",
+        #     selector="app=rook-ceph-operator",
+        #     resource_count=1,
+        #     timeout=600,
+        # )
 
         if (
             self.ocs_version < version.VERSION_4_16
