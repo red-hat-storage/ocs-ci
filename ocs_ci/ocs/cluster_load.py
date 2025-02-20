977eb5e4c25dd6d15cb3353c743b7863fdd54b68
Elad Ben Aharon <ebenahar@redhat.com>
2025-02-20 16:02:30 +0200

    Revert "cluster load: Refresh prometheus_api connection in case of connection…"
    This reverts commit 6c3be3eaf5edf8cbd698257afc496c4b58a56013.

--- ocs_ci/ocs/cluster_load.py
+++ ocs_ci/ocs/cluster_load.py
@@ -5,7 +5,6 @@ A module for cluster load related functionalities
 
 import logging
 import time
-import requests
 from datetime import datetime
 from uuid import uuid4
 import math
@@ -362,24 +361,11 @@ class ClusterLoad:
         """
         now = datetime.now
         timestamp = datetime.timestamp
-        try:
-            result = float(
-                self.prometheus_api.query(
-                    query, str(timestamp(now())), mute_logs=mute_logs, log_debug=True
-                )[0]["value"][1]
-            )
-        except (requests.exceptions.RequestException, IndexError) as e:
-            logger.info(
-                f"Encountered error: {e}. Refreshing connection and retrying..."
-            )
-            self.prometheus_api.refresh_connection()
-            result = float(
-                self.prometheus_api.query(
-                    query, str(timestamp(now())), mute_logs=mute_logs, log_debug=True
-                )[0]["value"][1]
-            )
-
-        return result
+        return float(
+            self.prometheus_api.query(
+                query, str(timestamp(now())), mute_logs=mute_logs, log_debug=True
+            )[0]["value"][1]
+        )
 
     def calc_trim_metric_mean(self, metric, samples=5, mute_logs=False):
         """
