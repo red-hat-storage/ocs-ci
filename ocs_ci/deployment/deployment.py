54fe5e126bf6e5ff239d2bf56d74ed345267a697
Shivam Durgbuns <sdurgbun@redhat.com>
2025-02-25 18:01:50 +0530

    Adding kms policy name for same kms encryption keys
    Signed-off-by: Shivam Durgbuns <sdurgbun@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -192,6 +192,7 @@ class Deployment(object):
         self.cluster_path = config.ENV_DATA["cluster_path"]
         self.namespace = config.ENV_DATA["cluster_namespace"]
         self.sts_role_arn = None
+        self.clusters_combined_name = None
 
     class OCPDeployment(BaseOCPDeployment):
         """
@@ -337,6 +338,10 @@ class Deployment(object):
                     exec_cmd(
                         f"oc create -f {constants.CLUSTERROLEBINDING_APPSET_PULLMODEL_PATH}"
                     )
+                # Generating a common name by combining cluster names so that it can be used
+                # in future for example in kms policy name.
+                self.clusters_combined_name = ""
+                self.clusters_combined_name += cluster["metadata"]["name"]
 
     def do_deploy_ocs(self):
         """
@@ -1261,6 +1266,8 @@ class Deployment(object):
         # creating StorageCluster
         if config.DEPLOYMENT.get("kms_deployment"):
             kms = KMS.get_kms_deployment()
+            if hasattr(kms, "vault_policy_name"):
+                kms.vault_policy_name = f"kpn_{self.clusters_combined_name[:250]}"  # truncating to max character limit
             kms.deploy()
 
         if config.ENV_DATA["mcg_only_deployment"]:
