f809daa228e3fc4c11c04beaf24337b6ae9574c4
Avdhoot <asagare@redhat.com>
2025-03-03 17:57:34 +0530

    Parameterized tests with node replacement
    Signed-off-by: Avdhoot <asagare@redhat.com>

--- tests/functional/workloads/cnv/test_replacement_scenario.py
+++ tests/functional/workloads/cnv/test_replacement_scenario.py
@@ -3,21 +3,34 @@ import pytest
 import random
 
 from ocs_ci.framework.testlib import E2ETest
-from ocs_ci.framework.pytest_customization.marks import workloads, magenta_squad
+from ocs_ci.framework.pytest_customization.marks import (
+    workloads,
+    magenta_squad,
+    skipif_external_mode,
+)
 from ocs_ci.helpers.cnv_helpers import cal_md5sum_vm, run_dd_io
 from ocs_ci.ocs import constants
 from ocs_ci.ocs import osd_operations
 from ocs_ci.framework import config
+from ocs_ci.ocs.cluster import CephCluster
 from ocs_ci.ocs.resources.storage_cluster import osd_encryption_verification
+from tests.functional.z_cluster.nodes.test_node_replacement_proactive import (
+    delete_and_create_osd_node,
+    select_osd_node_name,
+)
+from ocs_ci.helpers.helpers import (
+    verify_storagecluster_nodetopology,
+)
 
 logger = logging.getLogger(__name__)
 
 
 @magenta_squad
 @workloads
+@skipif_external_mode
 class TestCnvDeviceReplace(E2ETest):
     """
-    Device replacement
+    Device and node replacement
 
     """
 
@@ -39,8 +52,16 @@ class TestCnvDeviceReplace(E2ETest):
 
         logger.info("All vms created successfully")
 
-    def test_vms_with_osd_device_replacement(
+    @pytest.mark.parametrize(
+        argnames=["replacement_type"],
+        argvalues=[
+            pytest.param("device", marks=pytest.mark.polarion_id("")),
+            pytest.param("node", marks=pytest.mark.polarion_id("")),
+        ],
+    )
+    def test_vms_with_device_and_node_replacement(
         self,
+        replacement_type,
         setup_cnv,
         setup,
         clone_vm_workload,
@@ -49,7 +70,26 @@ class TestCnvDeviceReplace(E2ETest):
         cnv_workload,
         nodes,
     ):
-        """ """
+        """
+        This test performs the behaviour of VMs and data integrity after Node or Device of cluster
+
+        1. Keep IO operations going on in the VMs. Make sure some snapshot and clones of the VMs present
+        2. Keep vms in different states(power on, paused, stoped).
+        3. Initiate Replace  scenarios by following the official procedure for device replacement.
+            5.1 If replacement_type = device - device replacement
+            5.2 If replacement_type = node - Node replacement
+        4. Check VM State Post-Replacement:
+            After the device or node  is replaced, verify the state of the VMs that were on the old node:
+            Check if running VMs are still running.
+            Check if paused VMs remain paused.
+            Check if stopped VMs remain stopped.
+            Check if all the snapshots and clones preserved their states and data integrity
+        5.Verify Cluster Stability:
+            Ensure the cluster is stable after the replacement:
+            All critical pods are running as expected.
+        6. Check for data Integrity
+        """
+
         all_vms = self.vm_objs_def + self.vm_objs_aggr
 
         file_paths = ["/source_file.txt", "/new_file.txt"]
@@ -102,10 +142,24 @@ class TestCnvDeviceReplace(E2ETest):
         vm_for_stop.stop()
         vm_for_snap.pause()
 
-        # Replace device
-        osd_operations.osd_device_replacement(nodes)
+        if replacement_type == "device":
+            logger.info("Replacing the device")
+            osd_operations.osd_device_replacement(nodes)
 
-        # Verify osd encryption
+        elif replacement_type == "node":
+            osd_node_name = select_osd_node_name()
+            delete_and_create_osd_node(osd_node_name)
+
+            ceph_cluster_obj = CephCluster()
+            assert ceph_cluster_obj.wait_for_rebalance(
+                timeout=1800
+            ), "Data re-balance failed to complete"
+
+            assert (
+                verify_storagecluster_nodetopology
+            ), "Storagecluster node topology is having an entry of non ocs node(s) - Not expected"
+
+        logger.info("Verify osd encryption")
         if config.ENV_DATA.get("encryption_at_rest"):
             osd_encryption_verification()
 
@@ -124,6 +178,9 @@ class TestCnvDeviceReplace(E2ETest):
         vm_for_stop.start()
         vm_for_clone.start()
         vm_for_snap.unpause()
+
+        for vm_obj in all_vms:
+            run_dd_io(vm_obj=vm_obj, file_path=file_paths[1], verify=True)
 
         # Perform post device replacement data integrity check
         for vm_obj in all_vms:
