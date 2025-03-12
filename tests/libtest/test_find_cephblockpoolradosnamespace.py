192944d92cf8bac4e2feab1ffcd4f6159e7c8f3f
Jilju Joy <jijoy@redhat.com>
2025-03-12 11:29:45 +0530

    add libtest
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/libtest/test_find_cephblockpoolradosnamespace.py
+++ tests/libtest/test_find_cephblockpoolradosnamespace.py
@@ -1,14 +1,18 @@
 import logging
 from ocs_ci.framework.testlib import ManageTest, libtest
-from ocs_ci.helpers.helpers import find_cephblockpoolradosnamespace
+from ocs_ci.helpers.helpers import (
+    find_cephblockpoolradosnamespace,
+    find_cephfilesystemsubvolumegroup,
+)
 
 logger = logging.getLogger(__name__)
 
 
 @libtest
-class TestFindCephblockpoolradosnamespace(ManageTest):
+class TestFindCephblockpoolnsCephfssubvolumegroup(ManageTest):
     """
-    Tests to find the default cephblockpoolradosnamespace linked to the native storageclient
+    Tests to find the default cephblockpoolradosnamespace and cephfilesystemsubvolumegroup
+    linked to the native storageclient
 
     """
 
@@ -20,3 +24,14 @@ class TestFindCephblockpoolradosnamespace(ManageTest):
         rados_ns = find_cephblockpoolradosnamespace()
         assert rados_ns, "The name of cephblockpoolradosnamespace was not found."
         logger.info(f"Cephblockpoolradosnamespace is {rados_ns}")
+
+    def test_find_cephfilesystemsubvolumegroup(ManageTest):
+        """
+        Test to find the default cephfilesystemsubvolumegroup linked to the native storageclient
+
+        """
+        cephfs_subvolumegroup = find_cephfilesystemsubvolumegroup()
+        assert (
+            cephfs_subvolumegroup
+        ), "The name of cephfilesystemsubvolumegroup was not found."
+        logger.info(f"Cephfilesystemsubvolumegroup is {cephfs_subvolumegroup}")
