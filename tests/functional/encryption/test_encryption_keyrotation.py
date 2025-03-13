55679b3b2b5690bf86a8ae76698c1a54874b5bc9
Shivam Durgbuns <sdurgbun@redhat.com>
2025-03-13 14:15:02 +0530

    Adding test for test/OCS-5963 Also refactoring compare_old_keys_with_new_keys function
    Signed-off-by: Shivam Durgbuns <sdurgbun@redhat.com>

--- tests/functional/encryption/test_encryption_keyrotation.py
+++ tests/functional/encryption/test_encryption_keyrotation.py
@@ -22,7 +22,11 @@ from ocs_ci.utility.retry import retry
 from ocs_ci.ocs import constants
 from ocs_ci.helpers.helpers import create_pods
 from concurrent.futures import ThreadPoolExecutor
-
+from ocs_ci.ocs.bucket_utils import (
+    sync_object_directory,
+    write_random_test_objects_to_bucket,
+    verify_s3_object_integrity,
+)
 
 log = logging.getLogger(__name__)
 
@@ -132,16 +136,52 @@ class TestEncryptionKeyrotation:
         log.info("Changing the keyrotation value to default.")
         osd_keyrotation.set_keyrotation_schedule("@weekly")
 
+    @retry(UnexpectedBehaviour, tries=10, delay=20)
+    def compare_noobaa_old_keys_with_new_keys(
+        self, noobaa_keyrotation, old_noobaa_backend_key, old_noobaa_volume_key
+    ):
+        """
+        Compare noobaa old keys with new keys.
+        args:
+            noobaa_keyrotation: obj: NoobaaKeyrotation object
+            old_noobaa_backend_key: str: old noobaa backend key
+            old_noobaa_volume_key: str: old noobaa_volume_key
+
+        """
+        (
+            new_noobaa_backend_key,
+            new_noobaa_backend_secret,
+        ) = noobaa_keyrotation.get_noobaa_backend_secret()
+        (
+            new_noobaa_volume_key,
+            new_noobaa_volume_secret,
+        ) = noobaa_keyrotation.get_noobaa_volume_secret()
+
+        if new_noobaa_backend_key == old_noobaa_backend_key:
+            raise UnexpectedBehaviour("Noobaa Key Rotation is not happend")
+
+        if new_noobaa_volume_key == old_noobaa_volume_key:
+            raise UnexpectedBehaviour("Noobaa Key Rotation is not happend.")
+
+        log.info(
+            f"Noobaa Backend key rotated {new_noobaa_backend_key} : {new_noobaa_backend_secret}"
+        )
+        log.info(
+            f"Noobaa Volume key rotated {new_noobaa_volume_key} : {new_noobaa_volume_secret}"
+        )
+
     @pytest.mark.polarion_id("OCS-5791")
-    def test_noobaa_keyrotation(self):
+    def test_noobaa_keyrotation(self, minutes=3):
         """
         Test to verify the keyrotation for noobaa.
+        args:
+            minutes: int: Minutes after which rotation will happen.
 
         Steps:
             1. Disable Keyrotation and verify its Disable status at noobaa and storagecluster end.
             3. Record existing NooBaa Volume and backend keys before rotation is happen.
-            4. Set keyrotation status to every 3 minutes.
-            5. wait for 3 minute.
+            4. Set keyrotation status to every given minutes (default is 3 min)
+            5. wait for given minute (default is 3 min).
             6. Verify the keyrotation is happen NooBaa volume and backend keys
                 by comparing the old keys with new keys.
             7. Change the keyrotation value to default.
@@ -188,47 +228,22 @@ class TestEncryptionKeyrotation:
             noobaa_keyrotation.is_noobaa_keyrotation_enable
         ), "Keyrotation is not enabled in the noobaa object."
 
-        # Set keyrotatiojn schedule to every 3 minutes.
-        schedule = "*/3 * * * *"
+        # Set keyrotatiojn schedule to every given minutes.
+        schedule = f"*/{minutes} * * * *"
         noobaa_keyrotation.set_keyrotation_schedule(schedule)
 
-        # Verify keyrotation is set for every 3 minute in storagecluster and noobaa object.
+        # Verify keyrotation is set for every given minute in storagecluster and noobaa object.
         assert (
             noobaa_keyrotation.get_keyrotation_schedule() == schedule
-        ), "Keyrotation schedule is not set to every 3 minutes in storagecluster object."
+        ), f"Keyrotation schedule is not set to every {minutes} minutes in storagecluster object."
         assert (
             noobaa_keyrotation.get_noobaa_keyrotation_schedule() == schedule
-        ), "Keyrotation schedule is not set to every 3 minutes in Noobaa object."
-
-        @retry(UnexpectedBehaviour, tries=10, delay=20)
-        def compare_old_keys_with_new_keys():
-            """
-            Compare old keys with new keys.
-            """
-            (
-                new_noobaa_backend_key,
-                new_noobaa_backend_secret,
-            ) = noobaa_keyrotation.get_noobaa_backend_secret()
-            (
-                new_noobaa_volume_key,
-                new_noobaa_volume_secret,
-            ) = noobaa_keyrotation.get_noobaa_volume_secret()
-
-            if new_noobaa_backend_key == old_noobaa_backend_key:
-                raise UnexpectedBehaviour("Noobaa Key Rotation is not happend")
-
-            if new_noobaa_volume_key == old_noobaa_volume_key:
-                raise UnexpectedBehaviour("Noobaa Key Rotation is not happend.")
-
-            log.info(
-                f"Noobaa Backend key rotated {new_noobaa_backend_key} : {new_noobaa_backend_secret}"
-            )
-            log.info(
-                f"Noobaa Volume key rotated {new_noobaa_volume_key} : {new_noobaa_volume_secret}"
-            )
+        ), f"Keyrotation schedule is not set to every {minutes} minutes in Noobaa object."
 
         try:
-            compare_old_keys_with_new_keys()
+            self.compare_old_keys_with_new_keys(
+                noobaa_keyrotation, old_noobaa_backend_key, old_noobaa_volume_key
+            )
         except UnexpectedBehaviour:
             log.info("Noobaa Key Rotation is not happend.")
             assert False
@@ -236,6 +251,63 @@ class TestEncryptionKeyrotation:
         # Change the keyrotation value to default.
         log.info("Changing the keyrotation value to default.")
         noobaa_keyrotation.set_keyrotation_schedule("@weekly")
+
+    def test_bucket_checksum_with_noobaa_keyrotation(
+        self, mcg_obj_session, awscli_pod_session, bucket_factory, test_directory_setup
+    ):
+        """
+        Test to verify the keyrotation for noobaa.
+
+        Steps:
+            1. Write object on bucket and get checksum
+            2. Disable Keyrotation and verify its Disable status at noobaa and storagecluster end.
+            3. Record existing NooBaa Volume and backend keys before rotation is happen.
+            4. Set keyrotation status to every 5 minutes.
+            5. wait for 5 minute.
+            6. Verify the keyrotation is happen NooBaa volume and backend keys
+                by comparing the old keys with new keys.
+            7. Change the keyrotation value to default.
+            8. Get checksum of the object after key rotation
+            9. Compate old and new checksums are same.
+        """
+        data_dir = test_directory_setup.origin_dir
+        bucketname = bucket_factory(1)[0].name
+        full_object_path = f"s3://{bucketname}"
+
+        write_random_test_objects_to_bucket(
+            awscli_pod_session,
+            bucketname,
+            data_dir,
+            mcg_obj=mcg_obj_session,
+            pattern="obj-",
+        )
+        # object before key rotation to be placed in seperate dir
+        awscli_pod_session.exec_cmd_on_pod("mkdir before_keyrotation_dir")
+        sync_object_directory(
+            awscli_pod_session,
+            full_object_path,
+            "before_keyrotation_dir",
+            mcg_obj_session,
+        )
+
+        # do noobaa keyrotation for 5 minutes
+        self.test_noobaa_keyrotation(minutes=5)
+
+        # object after key rotation to be placed in seperate dir
+        awscli_pod_session.exec_cmd_on_pod("mkdir after_keyrotation_dir")
+        sync_object_directory(
+            awscli_pod_session,
+            full_object_path,
+            "after_keyrotation_dir",
+            mcg_obj_session,
+        )
+
+        # checking checksum of objects before and after keyrotation
+        verify_s3_object_integrity(
+            original_object_path="before_keyrotation_dir/obj-0",
+            result_object_path="after_keyrotation_dir/obj-0",
+            awscli_pod=awscli_pod_session,
+        ),
 
 
 @green_squad
