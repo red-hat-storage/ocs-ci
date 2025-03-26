54930da96f437ac5ea2738efd8ffa808d5790fd8
Jilju Joy <jijoy@redhat.com>
2025-03-26 15:56:28 +0530

    update libtest
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/libtest/test_ibm_cloud_bm.py
+++ tests/libtest/test_ibm_cloud_bm.py
@@ -1,9 +1,9 @@
 import logging
 import pytest
 
+from ocs_ci.ocs.node import get_all_nodes, get_node_objs
 from ocs_ci.ocs.platform_nodes import IBMCloudBMNodes
 from ocs_ci.framework.testlib import libtest
-from ocs_ci.ocs import node
 from ocs_ci.framework.pytest_customization.marks import (
     provider_client_platform_required,
 )
@@ -26,8 +26,9 @@ def teardown(request):
 @provider_client_platform_required
 def test_restart_nodes_by_stop_and_start():
     """
-    Check basic consistency in platform handling.
+    Test all nodes stop and start in  IBM Cloud Bare Metal platform
     """
     ibmcloud = IBMCloudBMNodes()
-    worker_nodes = node.get_nodes(node_type="worker")
-    ibmcloud.restart_nodes_by_stop_and_start(worker_nodes)
+    nodes = get_all_nodes()
+    node_objs = get_node_objs(nodes)
+    ibmcloud.restart_nodes_by_stop_and_start(node_objs)
