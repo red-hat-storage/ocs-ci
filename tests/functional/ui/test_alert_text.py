dd639be5cc754c116fd06774b6f3ec7e6efcd26f
Daniel Osypenko <dosypenk@redhat.com>
2025-03-27 14:44:11 +0200

    add skipif ibm cloud managed in more ui tests (#11727)
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- tests/functional/ui/test_alert_text.py
+++ tests/functional/ui/test_alert_text.py
@@ -10,6 +10,7 @@ from ocs_ci.framework.pytest_customization.marks import (
     black_squad,
     tier2,
     ui,
+    skipif_ibm_cloud_managed,
 )
 from ocs_ci.ocs import constants
 from ocs_ci.ocs.cluster import is_hci_provider_cluster
@@ -76,6 +77,7 @@ def alerts_expected():
 
 @tier2
 @black_squad
+@skipif_ibm_cloud_managed
 @skipif_mcg_only
 @skipif_hci_client
 @polarion_id("OCS-5509")
