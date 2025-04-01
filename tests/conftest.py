87ccf877bdc60773a59aa84a12b9621658bddd93
prsurve <prsurve@redhat.com>
2025-04-01 13:14:13 +0530

    Fix workload name
    Signed-off-by: prsurve <prsurve@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -7154,7 +7154,10 @@ def discovered_apps_dr_workload(request):
                 workload_dir=workload_details["workload_dir"],
                 workload_pod_count=workload_details["pod_count"],
                 workload_pvc_count=workload_details["pvc_count"],
-                workload_namespace=workload_details["workload_namespace"],
+                workload_namespace=(
+                    workload_details["workload_namespace"] + "-multi-ns" if multi_ns else workload_details[
+                        "workload_namespace"]
+                ),
                 discovered_apps_pvc_selector_key=workload_details[
                     "dr_workload_app_pvc_selector_key"
                 ],
