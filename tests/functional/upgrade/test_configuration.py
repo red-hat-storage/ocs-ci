cc150babb352b60a9f2d7ae5fabb77938c9b53e8
Suchita Gatfane <sgatfane@redhat.com>
2025-04-07 14:32:59 +0530

    Test to verify zero value of max_unavaialable in update strategy configuration (#11795)
    * Test to verify zero value of max_unavaialable in update strategy configuration
    
    Signed-off-by: suchita-g <sgatfane@redhat.com>

--- tests/functional/upgrade/test_configuration.py
+++ tests/functional/upgrade/test_configuration.py
@@ -16,6 +16,8 @@ from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs.resources import pod
 from ocs_ci.ocs.resources.daemonset import DaemonSet
 from ocs_ci.utility.utils import exec_cmd
+from ocs_ci.helpers.helpers import get_logs_rook_ceph_operator
+from ocs_ci.ocs.ocs_upgrade import set_update_strategy
 
 log = logging.getLogger(__name__)
 
@@ -133,6 +135,53 @@ def test_update_strategy_config_change(
     reflected in respective daemonset.
     """
     if daemonset == "csi-rbdplugin":
+        set_update_strategy(rbd_max_unavailable=value_to_set)
+    elif daemonset == "csi-cephfsplugin":
+        set_update_strategy(cephfs_max_unavailable=value_to_set)
+
+    ds_obj = DaemonSet(
+        resource_name=daemonset, namespace=config.ENV_DATA["cluster_namespace"]
+    )
+    results = ds_obj.get_update_strategy()
+    assert str(expected_value) == str(results["rollingUpdate"]["maxUnavailable"])
+
+
+@pytest.mark.parametrize(
+    argnames=["daemonset", "value_to_set"],
+    argvalues=[
+        pytest.param(
+            "csi-rbdplugin",
+            0,
+            marks=[pytest.mark.tier4a, pytest.mark.polarion_id("OCS-6517")],
+        ),
+        pytest.param(
+            "csi-rbdplugin",
+            "0%",
+            marks=[pytest.mark.tier4a, pytest.mark.polarion_id("OCS-6518")],
+        ),
+        pytest.param(
+            "csi-cephfsplugin",
+            0,
+            marks=[pytest.mark.tier4a, pytest.mark.polarion_id("OCS-6517")],
+        ),
+        pytest.param(
+            "csi-cephfsplugin",
+            "0%",
+            marks=[pytest.mark.tier4a, pytest.mark.polarion_id("OCS-6518")],
+        ),
+    ],
+)
+@brown_squad
+@runs_on_provider
+def test_max_unavailable_zero_update_strategy_config(
+    daemonset, value_to_set, rook_operator_configmap_cleanup
+):
+    """
+    Test that tested value added to configmap rook-ceph-operator-config is
+    updated with zero or zero percent value and generate the respective error
+    in rook-ceph-operator log
+    """
+    if daemonset == "csi-rbdplugin":
         parameter_name = "CSI_RBD_PLUGIN_UPDATE_STRATEGY_MAX_UNAVAILABLE"
     elif daemonset == "csi-cephfsplugin":
         parameter_name = "CSI_CEPHFS_PLUGIN_UPDATE_STRATEGY_MAX_UNAVAILABLE"
@@ -142,8 +191,28 @@ def test_update_strategy_config_change(
         f"oc patch configmap -n {config.ENV_DATA['cluster_namespace']} "
         f"{constants.ROOK_OPERATOR_CONFIGMAP} -p {config_map_patch}"
     )
-    ds_obj = DaemonSet(
-        resource_name=daemonset, namespace=config.ENV_DATA["cluster_namespace"]
+
+    # Verify the error in rook-ceph-operator-logs
+    rook_ceph_operator_log = get_logs_rook_ceph_operator()
+
+    expected_error = (
+        "failed to reconcile failed to configure ceph csi: "
+        "failed to start ceph csi drivers: failed to start"
     )
-    results = ds_obj.get_update_strategy()
-    assert str(expected_value) == str(results["rollingUpdate"]["maxUnavailable"])
+    if daemonset == "csi-rbdplugin":
+        expected_error += (
+            ' rbdplugin daemonset "csi-rbdplugin": failed to '
+            'start csi-rbdplugin daemonset: DaemonSet.apps "csi-rbdplugin"'
+        )
+    elif daemonset == "csi-cephfsplugin":
+        expected_error += (
+            ' cephfs plugin daemonset "csi-cephfsplugin": to '
+            'start csi-cephfsplugin daemonset: DaemonSet.apps "csi-cephfsplugin"'
+        )
+    expected_error += (
+        "is invalid: spec.updateStrategy.rollingUpdate.maxUnavailable: "
+        "Required value: cannot be 0 when maxSurge is 0"
+    )
+    msg = f'Expected Error for "{parameter_name}": "{value_to_set}" value in rook-ceph-operator log'
+
+    assert expected_error not in rook_ceph_operator_log, msg
