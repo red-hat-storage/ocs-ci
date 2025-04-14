12b88138f73342c8033d179c0947e1128f20da66
fbalak <fbalak@redhat.com>
2025-04-14 13:43:10 +0200

    execute test_ephemeral_pod on all clients
    Signed-off-by: fbalak <fbalak@redhat.com>

--- tests/functional/pod_and_daemons/test_ephemeral_pod.py
+++ tests/functional/pod_and_daemons/test_ephemeral_pod.py
@@ -21,6 +21,7 @@ log = getLogger(__name__)
 
 @tier2
 @brown_squad
+@run_on_all_clients
 @polarion_id("OCS-5792")
 class TestEphemeralPod:
     # This a workaround for any cluster other than multiclent provider mode
