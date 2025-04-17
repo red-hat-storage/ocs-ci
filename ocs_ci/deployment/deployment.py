44b7c3fb44c54c32d673199d6d80dd6954782791
Jilju Joy <jijoy@redhat.com>
2025-04-17 16:10:24 +0530

    Configure ACM to discover and import Multicluster Engine Operator clusters (#11565)
    * Configure ACM to import MCE clusters
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    * Configure ACM to discover and import MCE clusters
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    * Configure ACM to discover and import MCE clusters
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    * additional logs
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    * additional check for existing addons pods
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    * update patch command
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    * update patch format
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    
    ---------
    
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -102,6 +102,9 @@ from ocs_ci.ocs.resources.pod import (
     wait_for_ceph_cmd_execute_successfully,
     get_operator_pods,
     delete_pods,
+    wait_for_pods_by_label_count,
+    wait_for_pods_to_be_running,
+    wait_for_pods_to_be_in_statuses,
 )
 from ocs_ci.ocs.resources.storage_cluster import (
     ocs_install_verification,
@@ -2455,6 +2458,121 @@ class Deployment(object):
         else:
             self.deploy_acm_hub_released()
             self.deploy_multicluster_hub()
+        if config.ENV_DATA.get("configure_acm_to_import_mce"):
+            self.configure_acm_to_import_mce_clusters()
+
+    def configure_acm_to_import_mce_clusters(self):
+        """
+        Configure ACM to import MCE operator cluster and hosted clusters
+        """
+
+        # Before starting the configuration, verify the presence of the pods cluster-proxy-proxy-agent,
+        # klusterlet-addon-workmgr and managed-serviceaccount-addon-agent in the default addons namespace
+        for pod_label in [
+            "open-cluster-management.io/addon=cluster-proxy",
+            "component=work-manager",
+            "addon-agent=managed-serviceaccount",
+        ]:
+            if not wait_for_pods_by_label_count(
+                label=pod_label,
+                expected_count=1,
+                namespace=constants.ACM_ADDONS_NAMESPACE,
+                timeout=300,
+                sleep=10,
+            ):
+                raise ResourceNotFoundError(
+                    f"Pod with label {pod_label} not found in the namespace {constants.ACM_ADDONS_NAMESPACE}"
+                )
+
+        # Verify the status of existing pods in the default addons namespace
+        all_pods = get_all_pods(namespace=constants.ACM_ADDONS_NAMESPACE)
+        if not wait_for_pods_to_be_in_statuses(
+            expected_statuses=[constants.STATUS_RUNNING, constants.STATUS_COMPLETED],
+            pod_names=[pod_obj.name for pod_obj in all_pods],
+            namespace=constants.ACM_ADDONS_NAMESPACE,
+            timeout=300,
+            sleep=10,
+        ):
+            raise ResourceWrongStatusException(
+                f"Some pods in the namespace {constants.ACM_ADDONS_NAMESPACE} are not in expected status."
+            )
+
+        # Create AddOnDeploymentConfig to install add-ons in a different multicluster engine operator namespace so that
+        # the multicluster engine operator can self-manage with the local-cluster add-ons while
+        # ACM manages multicluster engine operator at the same time
+        logger.info(
+            "Configuring Red Hat Advanced Cluster Management to import multicluster engine operator clusters"
+        )
+        addon_deployment_config = helpers.create_resource(
+            **templating.load_yaml(constants.ACM_ADDON_DEPLOYMENT_CONFIG_YAML)
+        )
+
+        # Update the existing ClusterManagementAddOn resources for the add-ons so that the add-ons are installed
+        # in the namespace that is specified in the AddOnDeploymentConfig
+        patch_cmd = (
+            f'{{"spec": {{"installStrategy": {{"placements": [{{"name": "global","namespace": '
+            f'"open-cluster-management-global-set","rolloutStrategy": {{"type": "All"}},"configs": [{{"group": '
+            f'"addon.open-cluster-management.io","name": "{addon_deployment_config.name}","namespace": '
+            f'"{addon_deployment_config.namespace}","resource":"addondeploymentconfigs"}}]}}]}}}}}}'
+        )
+
+        addon_obj = OCP(kind=constants.CLUSTERMANAGEMENTADDON)
+        for management_addon in [
+            "work-manager",
+            "managed-serviceaccount",
+            "cluster-proxy",
+        ]:
+            addon_obj.patch(
+                resource_name=management_addon, params=patch_cmd, format_type="merge"
+            )
+
+        # Verify the presence and Running status of the pods cluster-proxy-proxy-agent, klusterlet-addon-workmgr and
+        # managed-serviceaccount-addon-agent
+        for pod_label in [
+            "open-cluster-management.io/addon=cluster-proxy",
+            "component=work-manager",
+            "addon-agent=managed-serviceaccount",
+        ]:
+            wait_for_pods_by_label_count(
+                label=pod_label,
+                expected_count=1,
+                namespace=addon_deployment_config.data["spec"]["agentInstallNamespace"],
+                timeout=900,
+                sleep=20,
+            )
+        wait_for_pods_to_be_running(
+            namespace=addon_deployment_config.data["spec"]["agentInstallNamespace"],
+            timeout=900,
+            sleep=20,
+        )
+
+        # Create a KlusterletConfig resource that is used by ManagedCluster resources to import multicluster engine
+        # operator clusters so that the klusterlet is installed with a different name to avoid the conflict
+        klusterlet_config = helpers.create_resource(
+            **templating.load_yaml(constants.KLUSTERLET_CONFIG_MCE_IMPORT_YAML)
+        )
+
+        logger.info(
+            "Configured Red Hat ACM to import multicluster engine operator clusters"
+        )
+
+        # Configuration for backup and restore. Add backup label to the default and new addondeploymentconfig,
+        # clustermanagementaddon and KlusterletConfig
+        logger.info(
+            "Add label for backup in addondeploymentconfigs, clustermanagementaddons and klusterletconfig"
+        )
+        backup_label = "cluster.open-cluster-management.io/backup=true"
+        addon_deployment_config.add_label(label=backup_label)
+        addon_deployment_config.ocp.add_label(
+            resource_name="hypershift-addon-deploy-config", label=backup_label
+        )
+        for management_addon in [
+            "work-manager",
+            "managed-serviceaccount",
+            "cluster-proxy",
+        ]:
+            addon_obj.add_label(resource_name=management_addon, label=backup_label)
+        klusterlet_config.add_label(label=backup_label)
 
     def deploy_acm_hub_unreleased(self):
         """
--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -261,6 +261,7 @@ CRD_KIND = "CustomResourceDefinition"
 CLIENT_PROFILE = "ClientProfile"
 OCS_OPERATOR_CONFIG_MAP = "ocs-operator-config"
 SERVICE_TYPE_NODEPORT = "NodePort"
+CLUSTERMANAGEMENTADDON = "ClusterManagementAddOn"
 
 # Provisioners
 AWS_EFS_PROVISIONER = "openshift.org/aws-efs"
@@ -1303,6 +1304,9 @@ MULTICLUSTER_ENGINE = "multiclusterengine"
 BACKUP_SCHEDULE_YAML = os.path.join(TEMPLATE_MULTICLUSTER_DIR, "backupschedule.yaml")
 KLUSTERLET_CONFIG_YAML = os.path.join(
     TEMPLATE_MULTICLUSTER_DIR, "klusterlet_config.yaml"
+)
+KLUSTERLET_CONFIG_MCE_IMPORT_YAML = os.path.join(
+    TEMPLATE_MULTICLUSTER_DIR, "klusterlet_config_mce_import.yaml"
 )
 MDR_BACKUP_SCHEDULE_RESOURCE = "schedule-acm"
 ACM_POLICY_COMPLIANT = "Compliant"
@@ -2912,6 +2916,9 @@ ACM_CLUSTERSET_LABEL = "cluster.open-cluster-management.io/clusterset"
 ACM_ADDONS_NAMESPACE = "open-cluster-management-agent-addon"
 ACM_HUB_OPERATOR_NAME_WITH_NS = f"{ACM_HUB_OPERATOR_NAME}.{ACM_HUB_NAMESPACE}"
 ACM_MANAGEDCLUSTER_ADDONS = "managedclusteraddons.addon.open-cluster-management.io"
+ACM_ADDON_DEPLOYMENT_CONFIG_YAML = os.path.join(
+    TEMPLATE_DIR, "acm-deployment", "addon_deployment_config.yaml"
+)
 
 # GitOps
 GITOPS_NAMESPACE = "openshift-gitops"
