0eb10040aee19757edc32a5209b6aa2df5eafb4f
Avdhoot <asagare@redhat.com>
2025-04-22 19:05:28 +0530

    corrected code
    Signed-off-by: Avdhoot <asagare@redhat.com>

--- tests/functional/pod_and_daemons/test_standalone_noobaa_pod_node_affinity.py
+++ tests/functional/pod_and_daemons/test_standalone_noobaa_pod_node_affinity.py
@@ -1,21 +1,14 @@
 import logging
 import pytest
-import random
 
 from ocs_ci.framework import config
 from ocs_ci.ocs import ocp, constants
 from ocs_ci.ocs.node import (
-    check_taint_on_nodes,
-    get_worker_nodes,
-    taint_nodes,
-    untaint_nodes,
     apply_node_affinity_for_noobaa_pod,
 )
 from ocs_ci.ocs.resources.pod import (
-    get_pod_node,
-    get_noobaa_operator_pod,
+    wait_for_pods_to_be_running,
 )
-from ocs_ci.helpers.helpers import apply_custom_taint_and_toleration
 from ocs_ci.framework.pytest_customization.marks import brown_squad
 
 log = logging.getLogger(__name__)
@@ -28,12 +21,9 @@ class TestNoobaaPodNodeAffinity:
         def finalizer():
             """
             Finalizer will take care of below activities:
-            1. Untaint the nodes: remove taints from nodes
-            2. Removes nodeaffinity to bring storage cluster with default values.
+            1. Removes nodeaffinity to bring storage cluster with default values.
 
             """
-            if check_taint_on_nodes():
-                untaint_nodes()
             resource_name = constants.DEFAULT_CLUSTERNAME
             if config.DEPLOYMENT["external_mode"]:
                 resource_name = constants.DEFAULT_CLUSTERNAME_EXTERNAL_MODE
@@ -45,23 +35,12 @@ class TestNoobaaPodNodeAffinity:
             params = '[{"op": "remove", "path": "/spec/placement/noobaa-standalone"},]'
             storagecluster_obj.patch(params=params, format_type="json")
             log.info("Patched storage cluster  back to the default")
-            # assert (
-            #     wait_for_pods_to_be_running()
-            # ), "some of the pods didn't came up running"
+            assert (
+                wait_for_pods_to_be_running()
+            ), "some of the pods didn't came up running"
 
         request.addfinalizer(finalizer)
 
     def test_tolerations_on_standalone_noobaa(self):
-        worker_nodes = get_worker_nodes()
-        log.info(f"Current available worker nodes are {worker_nodes}")
-        taint_nodes(worker_nodes)
-        log.info("Applied default taints on all the worker nodes")
-        # noobaa_operator_pod_obj = get_pod_obj("noobaa-operator",namespace= "openshift-storage")
-        noobaa_operator_pod_obj = get_noobaa_operator_pod()
-        noobaa_running_node = get_pod_node(noobaa_operator_pod_obj)
-        other_nodes = [
-            node for node in worker_nodes if node != noobaa_running_node.name
-        ]
-        other_node_name = random.choice(other_nodes)
-        apply_custom_taint_and_toleration()
-        apply_node_affinity_for_noobaa_pod(other_node_name)
+
+        assert apply_node_affinity_for_noobaa_pod(), "Failed to apply node affinity."
