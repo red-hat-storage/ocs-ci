32912b451bdca06472b1bd1de2b9b26c126f71ae
fbalak <fbalak@redhat.com>
2025-04-23 14:27:31 +0200

    use indirect params
    Signed-off-by: fbalak <fbalak@redhat.com>

--- ocs_ci/framework/pytest_customization/marks.py
+++ ocs_ci/framework/pytest_customization/marks.py
@@ -399,7 +399,7 @@ try:
         pytest.param(*[idx]) for idx in config.get_consumer_indexes_list()
     ]
     run_on_all_clients = pytest.mark.parametrize(
-        argnames=["cluster_index"], argvalues=client_indexes
+        argnames=["cluster_index"], argvalues=client_indexes, indirect=True
     )
 except ClusterNotFoundException:
     pass
