cbf7defd3572254b2039af3540b7333fb35a6d2a
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2025-04-30 21:22:04 +0530

    Added new method to fetch machine config controller pod and updated wait_for_machineconfigpool_status method to show evicting error logs
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

---
+++ tests/functional/nfs_feature/test_machine_config_log.py
@@ -0,0 +1,8 @@
+from ocs_ci.utility.utils import wait_for_machineconfigpool_status
+
+
+def test_machine_controller_logs():
+    """
+    This is to test the log messahes for the machine-controller
+    """
+    wait_for_machineconfigpool_status(node_type="worker")
