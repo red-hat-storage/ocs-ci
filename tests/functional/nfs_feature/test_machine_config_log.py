80c9d6e86a14ee855869a9f9c717a4350793f37d
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2025-05-04 22:20:31 +0530

    Updated clean_up_pods_for_provider method
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- tests/functional/nfs_feature/test_machine_config_log.py
+++ tests/functional/nfs_feature/test_machine_config_log.py
@@ -5,4 +5,4 @@ def test_machine_controller_logs():
     """
     This is to test the log messahes for the machine-controller
     """
-    clean_up_pods_for_provider(node_type="worker")
+    clean_up_pods_for_provider(node_type="master")
