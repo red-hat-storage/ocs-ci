3bca2ecd3fab7f594c44aa805a2d77dfd305b958
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2025-05-05 11:01:34 +0530

    Removed temporary test file
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- tests/functional/nfs_feature/test_machine_config_log.py
+++
@@ -1,8 +0,0 @@
-from ocs_ci.utility.utils import clean_up_pods_for_provider
-
-
-def test_machine_controller_logs():
-    """
-    This is to test the log messahes for the machine-controller
-    """
-    clean_up_pods_for_provider(node_type="master")
