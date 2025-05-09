f6e8a56afaaa4a54440c6f29cb9bae04a8170295
Avdhoot <asagare@redhat.com>
2025-05-09 10:48:53 +0530

    updated to access values
    Signed-off-by: Avdhoot <asagare@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -945,17 +945,17 @@ def ceph_pool_factory_fixture(request, replica=3, compression=None):
 
         for instance in instances:
             try:
+                instance.delete(wait=False)
                 radosns_obj = ocp.OCP(
                     kind=constants.CEPHBLOCKPOOLRADOSNS, namespace=instance.namespace
                 )
                 radosnamespaces = [
                     OCS(**radosns)
-                    for radosns in radosns_obj.get()
+                    for radosns in radosns_obj.get()["items"]
                     if radosns["spec"]["blockPoolName"] == instance.name
                 ]
                 for radosnamespace in radosnamespaces:
                     radosnamespace.delete()
-                instance.delete()
             except CommandFailed as ex:
                 if "NotFound" in str(ex) and skip_resource_not_found_error:
                     log.info(
