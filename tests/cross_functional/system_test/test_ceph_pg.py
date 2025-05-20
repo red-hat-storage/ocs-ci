e1273bedf9ced2110aad41048edf391a843322d6
Elena Bondarenko <ebondare@redhat.com>
2025-05-20 17:54:13 +0200

    Adjust target pg test to latest changes
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- tests/cross_functional/system_test/test_ceph_pg.py
+++ tests/cross_functional/system_test/test_ceph_pg.py
@@ -1,10 +1,11 @@
-from ocs_ci.ocs.cluster import get_ceph_config_dump_property
+from ocs_ci.ocs.cluster import get_ceph_config_property
 from ocs_ci.framework.pytest_customization.marks import yellow_squad
 from ocs_ci.framework.testlib import (
     ManageTest,
     acceptance,
     skipif_ocs_version,
 )
+from ocs_ci.framework import config
 
 
 @yellow_squad
@@ -18,9 +19,19 @@ class TestCephPg(ManageTest):
     def test_mon_target_pg_per_osd(self):
         """
         Test the value of mon_target_pg_per_osd
-        It should be 400 for 4.19
+        It should be 100 in Lean, 200 in Balanced and 400 in Performance profile
         """
-        mon_target_pg = get_ceph_config_dump_property("mon_target_pg_per_osd")
-        assert (
-            mon_target_pg
-        ), f"Mon_target_pg_per_osd is {mon_target_pg}. It should be 400"
+        mon_target_pg = get_ceph_config_property("mon", "mon_target_pg_per_osd")
+        mode = config.ENV_DATA.get("performance_profile")
+        if mode == "lean":
+            assert (
+                int(mon_target_pg) == 100
+            ), f"Mon_target_pg_per_osd is {mon_target_pg}. It should be 100 for Lean profile"
+        elif mode == "balanced":
+            assert (
+                int(mon_target_pg) == 200
+            ), f"Mon_target_pg_per_osd is {mon_target_pg}. It should be 100 for Balanced profile"
+        else:
+            assert (
+                int(mon_target_pg) == 400
+            ), f"Mon_target_pg_per_osd is {mon_target_pg}. It should be 400 for Performance profile"
