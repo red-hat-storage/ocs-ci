746cc4c2295f097e112c5393306d68ea5546a668
Jilju Joy <jijoy@redhat.com>
2025-05-22 01:25:59 +0530

    do not create initial pods
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/functional/pv/pv_services/test_new_pvc_when_random_pv_available.py
+++ tests/functional/pv/pv_services/test_new_pvc_when_random_pv_available.py
@@ -1,6 +1,6 @@
 import logging
 import pytest
-
+from yaml import dump
 
 from ocs_ci.ocs import constants
 from ocs_ci.framework.pytest_customization.marks import green_squad
@@ -48,10 +48,10 @@ class TestNewPvcWhenRandomPvAvailable(ManageTest):
         Test to verify that an existing PV is not used for a new PVC when PV name is not given in PVC yaml
 
         """
-        old_pv_names = []
+        initial_pv_names = []
         for pvc_obj in self.pvcs:
             pv_obj = pvc_obj.backed_pv_obj
-            old_pv_names.append(pv_obj.name)
+            initial_pv_names.append(pv_obj.name)
 
             # Change the persistentVolumeReclaimPolicy of the PV to Retain
             reclaim_policy_change = constants.RECLAIM_POLICY_RETAIN
@@ -73,7 +73,7 @@ class TestNewPvcWhenRandomPvAvailable(ManageTest):
             teardown_factory(pv_obj)
 
             # Remove claimRef, so that the PV will become available
-            logger.info(f"Removing claimRef from PV {pv_obj.name}")
+            logger.info(f"Removing claimRef from {constants.PV} {pv_obj.name}")
             patch_result = pv_obj.ocp.patch(
                 resource_name=pv_obj.name,
                 params='[{ "op": "remove", "path": "/spec/claimRef" }]',
@@ -81,22 +81,24 @@ class TestNewPvcWhenRandomPvAvailable(ManageTest):
             )
             assert patch_result, f"Failed to remove claimRef from PV {pv_obj.name}"
             logger.info(f"Logging the details of the {constants.PV} {pv_obj.name}")
-            logger.info(pv_obj.get())
+            logger.info(dump(pv_obj.get()))
 
         # Create new PVCs and pods with a different size
-        self.new_pvcs, self.new_pods = create_pvcs_and_pods(
+        new_pvcs, new_pods = create_pvcs_and_pods(
             pvc_size=5,
             access_modes_rbd=[constants.ACCESS_MODE_RWO],
             num_of_rbd_pvc=1,
             num_of_cephfs_pvc=1,
         )
 
+        # Ensure that the old PV is not re-used
         new_pvc_old_pv = {}
-        for pvc_obj_new in self.new_pvcs:
-            if pvc_obj_new.backed_pv in old_pv_names:
+        for pvc_obj_new in new_pvcs:
+            pvc_obj_new.reload()
+            if pvc_obj_new.backed_pv in initial_pv_names:
                 logger.error(
-                    f"Old available PV {pvc_obj_new.backed_pv} is used for the new PVC {pvc_obj_new.name} "
-                    f"which is not expected."
+                    f"Old available {constants.PV} {pvc_obj_new.backed_pv} is used for the new {constants.PVC} "
+                    f"{pvc_obj_new.name} which is not expected."
                 )
                 pvc_storage_request = (
                     pvc_obj_new.data.get("spec")
@@ -110,15 +112,15 @@ class TestNewPvcWhenRandomPvAvailable(ManageTest):
                 if pvc_storage_request != pvc_storage_capacity:
                     logger.error(
                         f"The requested storage {pvc_storage_request} and capacity {pvc_storage_capacity} of the "
-                        f"PVC {pvc_obj_new.name} are not matching."
+                        f"{constants.PVC} {pvc_obj_new.name} are not matching."
                     )
                     logger.info(
                         f"Logging the details of the {constants.PVC} {pvc_obj_new.name}"
                     )
-                    logger.info(pvc_obj_new.get())
+                    logger.info(dump(pvc_obj_new.get()))
                 new_pvc_old_pv[pvc_obj_new.name] = pvc_obj_new.backed_pv
 
         assert not new_pvc_old_pv, (
-            f"This is the mapping of new PVC name and old PV name. The new PVC re-used an available PV instead of "
-            f"provisioning a new PV:\n {new_pvc_old_pv}"
+            f"This is the mapping of new {constants.PVC} name and old {constants.PV} name. The new {constants.PVC} "
+            f"re-used an available {constants.PV} instead of provisioning a new {constants.PV}:\n {new_pvc_old_pv}"
         )
