a697de93246999535c4e461225e33709ee9fcc9e
Jilju Joy <jijoy@redhat.com>
2025-05-22 00:12:16 +0530

    do not create initial pods
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/functional/pv/pv_services/test_new_pvc_when_random_pv_available.py
+++ tests/functional/pv/pv_services/test_new_pvc_when_random_pv_available.py
@@ -11,35 +11,43 @@ logger = logging.getLogger(__name__)
 
 
 @green_squad
-class TestNewPvcWhenPvAvailable(ManageTest):
+class TestNewPvcWhenRandomPvAvailable(ManageTest):
     """
-    Tests to verify PVC creation and provisioning of new volume when a PV is already present
+    Tests to verify PVC creation and provisioning of new volume when a ransom PV is available in Released state
 
     """
 
     @pytest.fixture(autouse=True)
-    def setup(self, project_factory, create_pvcs_and_pods):
+    def setup(self, pvc_factory):
         """
-        Create PVCs and pods
+        Create PVCs
 
         """
-        self.pvcs, self.pods = create_pvcs_and_pods(
-            pvc_size=10,
-            access_modes_rbd=[constants.ACCESS_MODE_RWO],
-            num_of_rbd_pvc=1,
-            num_of_cephfs_pvc=1,
-        )
+        self.pvcs = []
+        self.namespace = None
+        for interface in [constants.CEPHBLOCKPOOL, constants.CEPHFILESYSTEM]:
+            pvc_obj = pvc_factory(
+                interface=interface,
+                project=self.namespace,
+                storageclass=None,
+                size=10,
+                access_mode=constants.ACCESS_MODE_RWO,
+                status=constants.STATUS_BOUND,
+                volume_mode=constants.VOLUME_MODE_FILESYSTEM,
+                size_unit="Gi",
+                wait_for_resource_status_timeout=90,
+            )
+            self.namespace = pvc_obj.namespace
+            self.pvcs.append(pvc_obj)
 
     @tier2
-    def test_existing_pv_is_not_used_for_new_pvc(self, create_pvcs_and_pods):
+    def test_existing_pv_is_not_used_for_new_pvc(
+        self, teardown_factory, create_pvcs_and_pods
+    ):
         """
         Test to verify that an existing PV is not used for a new PVC when PV name is not given in PVC yaml
 
         """
-        for pod_obj in self.pods:
-            pod_obj.delete()
-            pod_obj.ocp.wait_for_delete(resource_name=pod_obj.name)
-
         old_pv_names = []
         for pvc_obj in self.pvcs:
             pv_obj = pvc_obj.backed_pv_obj
@@ -58,15 +66,24 @@ class TestNewPvcWhenPvAvailable(ManageTest):
                 f"Changed persistentVolumeReclaimPolicy of pv {pv_obj.name} to {reclaim_policy_change}"
             )
 
+            # Delete the PVC
+            pvc_obj.delete()
+            pvc_obj.ocp.wait_for_delete(resource_name=pvc_obj.name)
+            pvc_obj.set_deleted()
+            teardown_factory(pv_obj)
+
             # Remove claimRef, so that the PV will become available
-            logger.info(f"Dropping claimRef from PV {pv_obj.name}")
+            logger.info(f"Removing claimRef from PV {pv_obj.name}")
             patch_result = pv_obj.ocp.patch(
                 resource_name=pv_obj.name,
                 params='[{ "op": "remove", "path": "/spec/claimRef" }]',
                 format_type="json",
             )
             assert patch_result, f"Failed to remove claimRef from PV {pv_obj.name}"
+            logger.info(f"Logging the details of the {constants.PV} {pv_obj.name}")
+            logger.info(pv_obj.get())
 
+        # Create new PVCs and pods with a different size
         self.new_pvcs, self.new_pods = create_pvcs_and_pods(
             pvc_size=5,
             access_modes_rbd=[constants.ACCESS_MODE_RWO],
@@ -93,8 +110,12 @@ class TestNewPvcWhenPvAvailable(ManageTest):
                 if pvc_storage_request != pvc_storage_capacity:
                     logger.error(
                         f"The requested storage {pvc_storage_request} and capacity {pvc_storage_capacity} of the "
-                        f"PVC {pvc_obj_new.name} are not matching"
+                        f"PVC {pvc_obj_new.name} are not matching."
                     )
+                    logger.info(
+                        f"Logging the details of the {constants.PVC} {pvc_obj_new.name}"
+                    )
+                    logger.info(pvc_obj_new.get())
                 new_pvc_old_pv[pvc_obj_new.name] = pvc_obj_new.backed_pv
 
         assert not new_pvc_old_pv, (
