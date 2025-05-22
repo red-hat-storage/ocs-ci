e54a24c6abd9de7b72dcfc921335ece63bb6e594
Yulia Persky <ypersky@redhat.com>
2025-05-22 08:48:45 +0300

    increasing bucket deletion timeout (#12209)
    Signed-off-by: Yulia Persky <ypersky@redhat.com>

--- ocs_ci/ocs/resources/objectbucket.py
+++ ocs_ci/ocs/resources/objectbucket.py
@@ -231,7 +231,7 @@ class ObjectBucket(ABC):
             verify = True
         if verify:
             # Increase the timeout to 15 minutes if the test is tier4
-            timeout = 120
+            timeout = 180
             if any("tier4" in mark for mark in get_current_test_marks()):
                 timeout = 900
             self.verify_deletion(timeout)
