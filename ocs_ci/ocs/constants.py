478e730342f54e9ebd7f23d6e67a04b4b7df1f2b
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2025-05-26 17:44:13 +0530

    Updated subscription operator name for acm operator
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -2974,6 +2974,7 @@ ACM_MANAGEDCLUSTER_ADDONS = "managedclusteraddons.addon.open-cluster-management.
 ACM_ADDON_DEPLOYMENT_CONFIG_YAML = os.path.join(
     TEMPLATE_DIR, "acm-deployment", "addon_deployment_config.yaml"
 )
+ACM_OPERATOR_SUBSCRIPTION = "acm-operator-subscription"
 
 # GitOps
 GITOPS_NAMESPACE = "openshift-gitops"
