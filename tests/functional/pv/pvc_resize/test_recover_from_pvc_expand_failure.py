c5804cc9f80c9139777830708b34cd71c3bffcf1
Jilju Joy <jijoy@redhat.com>
2025-05-30 20:34:20 +0530

    remove benchmark operator due to a known error
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/functional/pv/pvc_resize/test_recover_from_pvc_expand_failure.py
+++ tests/functional/pv/pvc_resize/test_recover_from_pvc_expand_failure.py
@@ -1,13 +1,17 @@
 import logging
 import time
-
+from math import ceil
 import pytest
 
 from ocs_ci.framework import config
 from ocs_ci.framework.pytest_customization.marks import polarion_id
 from ocs_ci.framework.testlib import ManageTest, tier4b, green_squad, ignore_leftovers
 from ocs_ci.ocs import constants
-from ocs_ci.ocs.cluster import change_ceph_full_ratio
+from ocs_ci.ocs.cluster import (
+    change_ceph_full_ratio,
+    get_percent_used_capacity,
+    CephCluster,
+)
 from ocs_ci.ocs.exceptions import TimeoutExpiredError
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs.resources.pod import verify_data_integrity, cal_md5sum
@@ -57,12 +61,24 @@ class TestRecoverPvcExpandFailure(ManageTest):
     @green_squad
     @polarion_id("")
     def test_recover_from_pvc_expansion_failure(
-        self, benchmark_workload_storageutilization
+        self, pause_and_resume_cluster_load, pod_factory
     ):
         """
-        Test case to verify recovery from PVC expansion failure
+        Test case to verify recovery from PVC expansion failure. The PVC expansion will not complete due to the cluster
+        being full. Even after changing the size, the initial requested size for expansion will be applied.
+
 
         """
+        cephcluster = CephCluster()
+
+        # Calculating size to ceph full ratio of 85% here to consider sync delay, if any,
+        # after writing I/O from the pods self.pods
+        total_storage = cephcluster.get_ceph_capacity()
+        used_storage_percent = get_percent_used_capacity()
+        target_percentage = 85
+        storage_percent_for_io = target_percentage - used_storage_percent
+        size_to_ceph_full = (storage_percent_for_io * total_storage) / 100
+
         # Create files on the pods
         for pod_obj in self.pods:
             pod_obj.run_io(
@@ -73,21 +89,36 @@ class TestRecoverPvcExpandFailure(ManageTest):
                 fio_filename=pod_obj.name,
                 end_fsync=1,
             )
-
-        target_percentage = 85
-        logger.info(
-            f"Fill up the cluster to {target_percentage}% of it's storage capacity"
-        )
-        benchmark_workload_storageutilization(target_percentage)
-
-        pvc_size_expanded = 20
-        pvc_size_reduced = 10
+        for pod_obj in self.pods:
+            pod_obj.get_fio_results()
 
         # Find initial md5sum of file from pods
         # Add some wait time for proper sync of data before getting md5sum. This is to avoid false failure
         time.sleep(60)
         for pod_obj in self.pods:
             pod_obj.orig_md5_sum = cal_md5sum(pod_obj=pod_obj, file_name=pod_obj.name)
+
+        # Create a pod that can be used to fill up the cluster
+        pod_to_fill = pod_factory(interface=constants.CEPHBLOCKPOOL)
+
+        # The pods used 4G each to write. Calculate the remaining to ceph full ratio
+        total_used_by_app_pods = 4 * len(self.pods)
+        size_to_ceph_full = ceil(size_to_ceph_full - total_used_by_app_pods)
+
+        logger.info(
+            f"Fill up the cluster to {target_percentage}% of it's storage capacity"
+        )
+        pod_to_fill.run_io(
+            storage_type="fs",
+            size=f"{size_to_ceph_full}G",
+            io_direction="write",
+            runtime=300,
+            fio_filename=pod_to_fill.name,
+            end_fsync=1,
+        )
+
+        pvc_size_expanded = 20
+        pvc_size_reduced = 10
 
         logger.info(f"Expanding PVCs to {pvc_size_expanded} GiB")
         for pvc_obj in self.pvcs:
@@ -108,7 +139,9 @@ class TestRecoverPvcExpandFailure(ManageTest):
                 pvc_size_reduced, False
             ), f"Failed to reduce the size of the PVC '{pvc_obj.name}'"
 
+        # Increase the ceph full ratio
         change_ceph_full_ratio(95)
+
         for pvc_obj in self.pvcs:
             for pvc_data in TimeoutSampler(240, 2, pvc_obj.get):
                 capacity = pvc_data.get("status").get("capacity").get("storage")
@@ -138,7 +171,8 @@ class TestRecoverPvcExpandFailure(ManageTest):
     @polarion_id("")
     def test_recover_from_pending_pvc_expansion(self):
         """
-        Test case to verify recovery from pending PVC expansion
+        Test case to verify recovery from pending PVC expansion. The PVC will expand to the size given after the initial
+        expand request
 
         """
         # Create files on the pods
