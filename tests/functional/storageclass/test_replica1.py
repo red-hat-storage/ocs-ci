2044389e4bfddaca90997bc011a0f74eb9c64419
Aviadp <apolak@redhat.com>
2025-06-02 13:43:30 +0300

    Attempt to try fix fio timeout issue
    Signed-off-by: Aviadp <apolak@redhat.com>

--- tests/functional/storageclass/test_replica1.py
+++ tests/functional/storageclass/test_replica1.py
@@ -244,7 +244,7 @@ class TestReplicaOne:
         log.info(testing_pod)
         pgs_before_workload = get_osd_pgs_used()
         kb_before_workload = get_osd_kb_used_data()
-        testing_pod.run_io(storage_type="fs", size="50g")
+        testing_pod.run_io(storage_type="fs", size="50g", direct=1)
         testing_pod.get_fio_results()
         pgs_after_workload = get_osd_pgs_used()
         log.info(
