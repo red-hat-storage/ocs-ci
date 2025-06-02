54b73b4a77e88a69f64b39f0be0186dc0a4c2ab9
Avdhoot <asagare@redhat.com>
2025-06-02 11:03:09 +0530

    Used new fixture
    Signed-off-by: Avdhoot <asagare@redhat.com>

--- tests/functional/workloads/cnv/test_replacement_scenario.py
+++ tests/functional/workloads/cnv/test_replacement_scenario.py
@@ -12,15 +12,8 @@ from ocs_ci.helpers.cnv_helpers import cal_md5sum_vm, run_dd_io
 from ocs_ci.ocs import constants
 from ocs_ci.ocs import osd_operations
 from ocs_ci.framework import config
-from ocs_ci.ocs.cluster import CephCluster
 from ocs_ci.ocs.resources.storage_cluster import osd_encryption_verification
-from tests.functional.z_cluster.nodes.test_node_replacement_proactive import (
-    delete_and_create_osd_node,
-    select_osd_node_name,
-)
-from ocs_ci.helpers.helpers import (
-    verify_storagecluster_nodetopology,
-)
+
 
 logger = logging.getLogger(__name__)
 
@@ -30,7 +23,7 @@ logger = logging.getLogger(__name__)
 @skipif_external_mode
 class TestCnvDeviceReplace(E2ETest):
     """
-    Device and node replacement
+    Test case for Device replacement
 
     """
 
@@ -49,33 +42,25 @@ class TestCnvDeviceReplace(E2ETest):
             self.sc_obj_def_compr,
             self.sc_obj_aggressive,
         ) = multi_cnv_workload(namespace=proj_obj.namespace)
-
         logger.info("All vms created successfully")
 
-    @pytest.mark.parametrize(
-        argnames=["replacement_type"],
-        argvalues=[
-            pytest.param("device", marks=pytest.mark.polarion_id("")),
-            pytest.param("node", marks=pytest.mark.polarion_id("")),
-        ],
-    )
     def test_vms_with_device_and_node_replacement(
         self,
-        replacement_type,
         setup_cnv,
         setup,
         nodes,
+        vm_clone_fixture,
+        vm_snapshot_restore_fixture,
+        admin_client,
     ):
         """
         This test performs the behaviour of VMs and data integrity after Node or Device of cluster
 
         1. Keep IO operations going on in the VMs. Make sure some snapshot and clones of the VMs present
         2. Keep vms in different states(power on, paused, stoped).
-        3. Initiate Replace  scenarios by following the official procedure for device replacement.
-            5.1 If replacement_type = device - device replacement
-            5.2 If replacement_type = node - Node replacement
+        3. Initiate device Replace  scenarios by following the official procedure for device replacement.
         4. Check VM State Post-Replacement:
-            After the device or node  is replaced, verify the state of the VMs that were on the old node:
+            After the device is replaced, verify the state of the VMs that were on the old node:
             Check if running VMs are still running.
             Check if paused VMs remain paused.
             Check if stopped VMs remain stopped.
@@ -97,47 +82,25 @@ class TestCnvDeviceReplace(E2ETest):
         # Choose VMs randomaly
         vm_for_clone, vm_for_stop, vm_for_snap = random.sample(all_vms, 3)
 
-        # Uncomment code ones 11088 merged.
-        """
         # Create Clone of VM
-        cloned_vm = clone_or_snapshot_vm(
-            "clone",
-            vm_for_clone,
-            admin_client=admin_client,
-            all_vms=all_vms,
-            file_path=file_paths[0],
-        )
+        logger.info(f"Cloning VM {vm_for_clone.name}...")
+        cloned_vm = vm_clone_fixture(vm_for_clone, admin_client)
         csum = cal_md5sum_vm(vm_obj=cloned_vm, file_path=file_paths[0])
         source_csums[cloned_vm.name] = csum
+        all_vms.append(cloned_vm)
 
         # Create a snapshot
-        vm_for_snap = clone_or_snapshot_vm(
-            "snapshot", vm_for_snap, admin_client=admin_client, file_path=file_paths[0]
-        )
-        csum = cal_md5sum_vm(vm_obj=vm_for_snap, file_path=file_paths[0])
+        logger.info(f"Snapshot and restore VM {vm_for_snap.name}...")
+        restored_vm = vm_snapshot_restore_fixture(vm_for_snap, admin_client)
+        csum = cal_md5sum_vm(vm_obj=restored_vm, file_path=file_paths[0])
         source_csums[vm_for_snap.name] = csum
-        """
 
         # Keep vms in different states (power on, paused, stoped)
         vm_for_stop.stop()
         vm_for_snap.pause()
 
-        if replacement_type == "device":
-            logger.info("Replacing the device")
-            osd_operations.osd_device_replacement(nodes)
-
-        elif replacement_type == "node":
-            osd_node_name = select_osd_node_name()
-            delete_and_create_osd_node(osd_node_name)
-
-            ceph_cluster_obj = CephCluster()
-            assert ceph_cluster_obj.wait_for_rebalance(
-                timeout=1800
-            ), "Data re-balance failed to complete"
-
-            assert (
-                verify_storagecluster_nodetopology
-            ), "Storagecluster node topology is having an entry of non ocs node(s) - Not expected"
+        logger.info("Start Replacing the device")
+        osd_operations.osd_device_replacement(nodes)
 
         logger.info("Verify osd encryption")
         if config.ENV_DATA.get("encryption_at_rest"):
