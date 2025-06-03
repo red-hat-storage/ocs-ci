b077dd272eb0cb93f7c1d8e052f81a60a919bb87
fbalak <fbalak@redhat.com>
2025-06-03 11:25:20 +0200

    no-ssl for multicluster s3
    Signed-off-by: fbalak <fbalak@redhat.com>

--- ocs_ci/ocs/bucket_utils.py
+++ ocs_ci/ocs/bucket_utils.py
@@ -52,9 +52,13 @@ def craft_s3_command(cmd, mcg_obj=None, api=False, signed_request_creds=None):
 
     """
     api = "api" if api else ""
+    # TODO(fbalak): generate correct ssl certificate for multicluster scenarios that use external endpoint
     no_ssl = (
         "--no-verify-ssl"
-        if (signed_request_creds and signed_request_creds.get("ssl")) is False
+        if (
+            (signed_request_creds and signed_request_creds.get("ssl")) is False
+            or config.multicluster
+        )
         else ""
     )
     if mcg_obj:
