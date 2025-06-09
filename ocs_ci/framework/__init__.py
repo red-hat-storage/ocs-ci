a17290eea4cc7850c7a8220238b52ec71658fb1d
Shylesh Kumar Mohan <shmohan@redhat.com>
2025-06-09 18:35:17 +0530

    Fix tox issues
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- ocs_ci/framework/__init__.py
+++ ocs_ci/framework/__init__.py
@@ -45,6 +45,7 @@ class Config:
     # to the upgrade context. Applicable only in the multicluster upgrade
     # scenario
     PREUPGRADE_CONFIG: dict = field(default_factory=dict)
+    WORKLOAD: dict = field(default_factory=dict)
 
     def __post_init__(self):
         self.reset()
