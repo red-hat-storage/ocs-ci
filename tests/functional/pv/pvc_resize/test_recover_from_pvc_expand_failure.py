88dc0ce7659b7ef1397ebf0abd4b0b794747db6d
Jilju Joy <jijoy@redhat.com>
2025-06-12 19:35:47 +0530

    Test recover from pending PVC expansion 3 times on each PVC
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/functional/pv/pvc_resize/test_recover_from_pvc_expand_failure.py
+++ tests/functional/pv/pvc_resize/test_recover_from_pvc_expand_failure.py
@@ -8,7 +8,7 @@ from ocs_ci.framework.testlib import ManageTest, tier4c, green_squad, ignore_lef
 from ocs_ci.helpers.helpers import wait_for_resource_state
 from ocs_ci.ocs import constants
 
-from ocs_ci.ocs.exceptions import TimeoutExpiredError
+from ocs_ci.ocs.exceptions import TimeoutExpiredError, CommandFailed
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs.resources.pod import verify_data_integrity, cal_md5sum
 from ocs_ci.utility.utils import TimeoutSampler
@@ -62,10 +62,10 @@ class TestRecoverPvcExpandFailure(ManageTest):
     ):
         """
         Test case to verify recovery from pending PVC expansion. The PVC will expand to the size given after the initial
-        expand request
+        expand request. Test this scenario on PVCs created from snapshots also.
 
         """
-        # Create files on the pods
+        # Create file on the pods
         for pod_obj in self.pods:
             pod_obj.run_io(
                 storage_type="fs",
@@ -75,6 +75,8 @@ class TestRecoverPvcExpandFailure(ManageTest):
                 fio_filename=f"{pod_obj.name}",
                 end_fsync=1,
             )
+        for pod_obj in self.pods:
+            pod_obj.get_fio_results()
 
         # Create snapshots
         logger.info("Creating snapshot of all the PVCs")
@@ -92,7 +94,7 @@ class TestRecoverPvcExpandFailure(ManageTest):
                 condition="true",
                 resource_name=snap_obj.name,
                 column=constants.STATUS_READYTOUSE,
-                timeout=180,
+                timeout=360,
             )
             snap_obj.reload()
         logger.info("Snapshots are in Ready state")
@@ -116,7 +118,7 @@ class TestRecoverPvcExpandFailure(ManageTest):
         logger.info("Verifying that the restored PVCs are Bound")
         for pvc_obj in restore_pvcs:
             wait_for_resource_state(
-                resource=pvc_obj, state=constants.STATUS_BOUND, timeout=500
+                resource=pvc_obj, state=constants.STATUS_BOUND, timeout=480
             )
             pvc_obj.reload()
         logger.info("Verified that the PVCs created from the snapshots are Bound")
@@ -135,7 +137,7 @@ class TestRecoverPvcExpandFailure(ManageTest):
             )
             restore_pod_objs.append(restore_pod_obj)
 
-        # Create files on the pods
+        # Create file on the pods
         for pod_obj in restore_pod_objs:
             pod_obj.run_io(
                 storage_type="fs",
@@ -145,81 +147,119 @@ class TestRecoverPvcExpandFailure(ManageTest):
                 fio_filename=f"{pod_obj.name}",
                 end_fsync=1,
             )
-
-        # Scale down rbd and cephfs ctrlplugin pod. To do this scale down operator deployments first
-        logger.info(
-            "Scale down operator deployments first to avoid reconciling ctrlplugin deployments after scaled down"
-        )
-        dep_ocp = OCP(
-            kind=constants.DEPLOYMENT, namespace=config.ENV_DATA["cluster_namespace"]
-        )
-        deployments = [
-            "ceph-csi-controller-manager",
-            f"{config.ENV_DATA['cluster_namespace']}.cephfs.csi.ceph.com-ctrlplugin",
-            f"{config.ENV_DATA['cluster_namespace']}.rbd.csi.ceph.com-ctrlplugin",
-        ]
-        for dep in deployments:
-            logger.info(f"Scaling deployment {dep} to replica 0")
-            dep_ocp.exec_oc_cmd(f"scale deployment {dep} --replicas=0")
-            time.sleep(10)
-
-        pvc_size_expanded_initial = 30
-        pvc_size_reduced_final = 10
-        reduce_size_step = -10
+        for pod_obj in restore_pod_objs:
+            pod_obj.get_fio_results()
 
         # Find initial md5sum of file from pods
-        # Add some wait time for proper sync of data before getting md5sum. This is to avoid false failure
-        time.sleep(60)
         all_pods = self.pods + restore_pod_objs
         for pod_obj in all_pods:
             pod_obj.orig_md5_sum = cal_md5sum(pod_obj=pod_obj, file_name=pod_obj.name)
 
         all_pvcs = self.pvcs + restore_pvcs
-        logger.info(
-            f"Trying to expand the PVCs to {pvc_size_expanded_initial}Gi and when that is pending, reduce the size in "
-            f"steps of {reduce_size_step}Gi"
-        )
+        pvc_size_expanded_initial = 30
+        pvc_size_reduced_final = 10
+        reduce_size_step = -10
 
-        # Size in each stage will be 30, 20 and 10
-        for size in range(
-            pvc_size_expanded_initial,
-            pvc_size_reduced_final + reduce_size_step,
-            reduce_size_step,
-        ):
-            for pvc_obj in all_pvcs:
-                logger.info(f"Change the size of the PVC {pvc_obj.name} to {size}Gi")
-                try:
-                    assert not pvc_obj.resize_pvc(
-                        size, True, timeout=60
-                    ), f"Unexpected: Expansion of PVC '{pvc_obj.name}' completed"
-                    logger.debug(pvc_obj.describe())
-                except TimeoutExpiredError:
+        # Test recover from pending PVC expansion 3 times on each PVC
+        for count in range(3):
+            logger.info(
+                f"Iteration {count + 1} of recovering from pending PVC expansion"
+            )
+
+            # Scale down rbd and cephfs ctrlplugin pod. To do this scale down operator deployments first
+            logger.info(
+                "Scale down operator deployments first to avoid reconciling ctrlplugin deployments after scaled down"
+            )
+            dep_ocp = OCP(
+                kind=constants.DEPLOYMENT,
+                namespace=config.ENV_DATA["cluster_namespace"],
+            )
+            deployments = [
+                "ceph-csi-controller-manager",
+                f"{config.ENV_DATA['cluster_namespace']}.cephfs.csi.ceph.com-ctrlplugin",
+                f"{config.ENV_DATA['cluster_namespace']}.rbd.csi.ceph.com-ctrlplugin",
+            ]
+            for dep in deployments:
+                logger.info(f"Scaling deployment {dep} to replica 0")
+                dep_ocp.exec_oc_cmd(f"scale deployment {dep} --replicas=0")
+                time.sleep(10)
+
+            logger.info(
+                f"Trying to expand the PVCs to {pvc_size_expanded_initial}Gi and when that is pending, reduce the "
+                f"size in steps of {reduce_size_step}Gi"
+            )
+            # Size in each stage will be pvc_size_expanded_initial, pvc_size_expanded_initial-10 and
+            # pvc_size_reduced_final
+            for size in range(
+                pvc_size_expanded_initial,
+                pvc_size_reduced_final + reduce_size_step,
+                reduce_size_step,
+            ):
+                for pvc_obj in all_pvcs:
                     logger.info(
-                        f"Expected: Expansion of PVC {pvc_obj.name} to {size}Gi did not complete"
+                        f"Change the size of the PVC {pvc_obj.name} to {size}Gi"
                     )
-            logger.info(f"Expected: PVCs did not change capacity to the size {size}Gi")
-        logger.info(
-            f"Expected: PVCs did not change capacity to the different size in stages. Last applied size in the spec of "
-            f"all PVCs is {pvc_size_reduced_final}Gi"
-        )
-
-        # Scale back the ceph-csi-controller-manager. This will scale up all other deployments that was scaled down
-        dep_ocp.exec_oc_cmd("scale deployment ceph-csi-controller-manager --replicas=1")
-
-        # Now PVCs are expected to expand to the reduced size
-        for pvc_obj in all_pvcs:
-            for pvc_data in TimeoutSampler(240, 2, pvc_obj.get):
-                capacity = pvc_data.get("status").get("capacity").get("storage")
-                if capacity == f"{pvc_size_reduced_final}Gi":
-                    break
+                    try:
+                        assert not pvc_obj.resize_pvc(
+                            size, True, timeout=60
+                        ), f"Unexpected: Expansion of PVC '{pvc_obj.name}' completed"
+                        logger.debug(pvc_obj.describe())
+                    except TimeoutExpiredError:
+                        logger.info(
+                            f"Expected: Expansion of PVC {pvc_obj.name} to {size}Gi did not complete"
+                        )
                 logger.info(
-                    f"Capacity of PVC {pvc_obj.name} is not {pvc_size_reduced_final}Gi as "
-                    f"expected, but {capacity}. Retrying."
+                    f"Expected: PVCs did not change capacity to the size {size}Gi"
                 )
             logger.info(
-                f"Verified that the capacity of PVC {pvc_obj.name} is changed to "
-                f"{pvc_size_reduced_final}Gi."
+                f"Expected: PVCs did not change capacity to the different size in stages. Last applied size in the "
+                f"spec of all PVCs is {pvc_size_reduced_final}Gi"
             )
+
+            # Verify that the size cannot be reduced below the current capacity
+            logger.info(
+                "Verify that the size of the PVCs cannot be reduced below the current capacity"
+            )
+            for pvc_obj in all_pvcs:
+                try:
+                    pvc_obj.resize_pvc(self.pvc_size - 1, False)
+                except CommandFailed as err:
+                    expected_error = "field can not be less than status.capacity"
+                    if expected_error in str(err):
+                        logger.info(
+                            f"Verified: The size of the PVC {pvc_obj.name} cannot be reduced to {self.pvc_size - 1}Gi "
+                            f"which is below its current capacity of {self.pvc_size}Gi"
+                        )
+                    else:
+                        raise
+
+            # Scale back the ceph-csi-controller-manager. This will scale up all other deployments that was scaled down
+            dep_ocp.exec_oc_cmd(
+                "scale deployment ceph-csi-controller-manager --replicas=1"
+            )
+
+            # Now PVCs are expected to expand to the reduced size
+            for pvc_obj in all_pvcs:
+                for pvc_data in TimeoutSampler(240, 2, pvc_obj.get):
+                    capacity = pvc_data.get("status").get("capacity").get("storage")
+                    if capacity == f"{pvc_size_reduced_final}Gi":
+                        break
+                    logger.info(
+                        f"Capacity of PVC {pvc_obj.name} is not {pvc_size_reduced_final}Gi as "
+                        f"expected, but {capacity}. Retrying."
+                    )
+                logger.info(
+                    f"Verified that the capacity of PVC {pvc_obj.name} is changed to "
+                    f"{pvc_size_reduced_final}Gi."
+                )
+
+            logger.info(
+                f"Iteration {count + 1} of recovering from pending PVC expansion completed"
+            )
+            pvc_size_expanded_initial = (
+                pvc_size_expanded_initial + pvc_size_reduced_final
+            )
+            pvc_size_reduced_final += abs(reduce_size_step)
 
         # Verify md5sum
         for pod_obj in all_pods:
