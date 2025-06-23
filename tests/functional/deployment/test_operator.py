9996fa2d194f717b59f251e6b72c8acf033750a0
fbalak <fbalak@redhat.com>
2025-06-23 14:24:11 +0200

    address Petr's comment
    Signed-off-by: fbalak <fbalak@redhat.com>

--- tests/functional/deployment/test_operator.py
+++ tests/functional/deployment/test_operator.py
@@ -6,7 +6,7 @@ from ocs_ci.framework.testlib import (
     ManageTest,
     tier1,
 )
-from ocs_ci.ocs.resources.pod import get_pod_logs
+from ocs_ci.utility.utils import exec_cmd
 from ocs_ci.ocs.resources.storage_cluster import check_unnecessary_pods_present
 
 
@@ -33,17 +33,15 @@ class TestOperator(ManageTest):
         2. Check that there is no error in any of the logs.
         """
         pods_logs = {}
+        false_positives = [" sed 's/error: <nil>,//g' |"]
         for operator_pod in operator_pods:
-            pod_logs = get_pod_logs(
-                pod_name=operator_pod,
-                namespace=config.ENV_DATA["cluster_namespace"],
-                all_containers=True,
+            pod_logs = exec_cmd(
+                f"oc logs -n {config.ENV_DATA['cluster_namespace']} {operator_pod} |{''.join(false_positives)} grep -i error",
+                shell=True,
             )
             pods_logs[operator_pod] = pod_logs
         for operator_pod in operator_pods:
             test_string = pods_logs[operator_pod].lower()
-            # remove false positives
-            test_string = test_string.replace("error: <nil>,", "")
             assert (
                 "error" not in test_string
             ), f"error in {operator_pod} logs, operator pod logs: {test_string}"
