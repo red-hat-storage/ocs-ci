e81abbca42e92714d6fc468b9b9509e517c54108
am-agrawa <amagrawa@redhat.com>
2025-06-24 15:46:40 +0530

    doctext formatting issue
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- ocs_ci/ocs/acm/acm.py
+++ ocs_ci/ocs/acm/acm.py
@@ -162,7 +162,7 @@ class AcmAddClusters(AcmPageNavigator):
             globalnet (bool): Globalnet is set to True by default for ODF versions greater than or equal to 4.13
 
         Returns:
-            cluster_set_name (str): Returns cluster set name to which managed clusters are tied for Submariner
+            str: cluster set name created during the function to which managed clusters are tied for Submariner
             installation
 
         """
