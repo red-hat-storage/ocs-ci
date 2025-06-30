a792c9764afbed9b33f10a2a26d7b32520bb007b
fbalak <fbalak@redhat.com>
2025-06-30 17:41:18 +0200

    add libtest
    Signed-off-by: fbalak <fbalak@redhat.com>

---
+++ tests/libtest/test_multicluster.py
@@ -0,0 +1,18 @@
+import pytest
+
+from ocs_ci.framework.pytest_customization.marks import libtest, run_on_all_clients
+
+
+@libtest
+@run_on_all_clients
+def test_run_on_all_clients_marker(cluster_index):
+    pass
+
+
+@libtest
+@run_on_all_clients
+@pytest.mark.parametrize(argnames=["another_param"], argvalues=[1])
+def test_run_on_all_clients_marker_with_additional_parameters(
+    another_param, cluster_index
+):
+    pass
