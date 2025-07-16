e34b6133985567320ecfc5bcd2ebfabfb360c2ce
Daniel Osypenko <dosypenk@redhat.com>
2025-07-16 14:55:13 +0300

    review fix Daniel H
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- ocs_ci/deployment/hosted_cluster.py
+++ ocs_ci/deployment/hosted_cluster.py
@@ -2159,7 +2159,7 @@ def hypershift_cluster_factory(
                         ClusterKubeconfigNotFoundError,
                     )
 
-                    ClusterKubeconfigNotFoundError(
+                    raise ClusterKubeconfigNotFoundError(
                         f"Failed to download kubeconfig for cluster {cluster_name}"
                     )
                 else:
