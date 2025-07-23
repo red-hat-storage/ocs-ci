e2f33d8cd51b416bab3a59535fcb59a8e452d066
am-agrawa <amagrawa@redhat.com>
2025-07-23 23:20:30 +0530

    comment all code
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- tests/functional/disaster-recovery/regional-dr/test_cnv_failover_and_relocate_discovered_apps.py
+++ tests/functional/disaster-recovery/regional-dr/test_cnv_failover_and_relocate_discovered_apps.py
@@ -166,123 +166,119 @@ class TestCNVFailoverAndRelocateWithDiscoveredApps:
             phase=constants.STATUS_RUNNING,
         )
 
-        from pdb import set_trace
-
-        set_trace()
-
-        # Validating data integrity (file1) after failing-over VMs to secondary managed cluster
-        validate_data_integrity_vm(
-            cnv_workloads, vm_filepaths[0], md5sum_original, "Failover"
-        )
-
-        # Creating a file (file2) post failover
-        for cnv_wl in cnv_workloads:
-            md5sum_failover.append(
-                run_dd_io(
-                    vm_obj=cnv_wl.vm_obj,
-                    file_path=vm_filepaths[1],
-                    username=cnv_wl.vm_username,
-                    verify=True,
-                )
-            )
-
-        for cnv_wl, md5sum in zip(cnv_workloads, md5sum_failover):
-            logger.info(
-                f"Checksum of files written after Failover: {vm_filepaths[1]} on VM {cnv_wl.workload_name}: {md5sum}"
-            )
-
-        # logger.info("Recover the down managed cluster")
-        # nodes_multicluster[active_primary_index].start_nodes(
-        #     active_primary_cluster_node_objs
+        # # Validating data integrity (file1) after failing-over VMs to secondary managed cluster
+        # validate_data_integrity_vm(
+        #     cnv_workloads, vm_filepaths[0], md5sum_original, "Failover"
         # )
-        # wait_for_nodes_status([node.name for node in active_primary_cluster_node_objs])
-        # wait_for_pods_to_be_running(timeout=420, sleep=15)
-        # assert ceph_health_check(tries=10, delay=30)
-
-        logger.info("Doing Cleanup Operations")
-        dr_helpers.do_discovered_apps_cleanup(
-            drpc_name=cnv_workloads[0].discovered_apps_placement_name,
-            old_primary=primary_cluster_name_before_failover,
-            workload_namespace=cnv_workloads[0].workload_namespace,
-            workload_dir=cnv_workloads[0].workload_dir,
-            vrg_name=cnv_workloads[0].discovered_apps_placement_name,
-        )
-
-        # if custom_sc:
-        #     assert check_mirroring_status_for_custom_pool(
-        #         pool_name="rdr-test-storage-pool-2way"
-        #     ), "Mirroring status check for custom SC failed"
-        #     logger.info("Mirroring status check for custom SC passed")
-
-        # Doing Relocate in below code
-        primary_cluster_name_after_failover = (
-            dr_helpers.get_current_primary_cluster_name(
-                cnv_workloads[0].workload_namespace,
-                discovered_apps=True,
-                resource_name=cnv_workloads[0].discovered_apps_placement_name,
-            )
-        )
-        config.switch_to_cluster_by_name(primary_cluster_name_before_failover)
-        secondary_cluster_name = dr_helpers.get_current_secondary_cluster_name(
-            cnv_workloads[0].workload_namespace,
-            discovered_apps=True,
-            resource_name=cnv_workloads[0].discovered_apps_placement_name,
-        )
-
-        scheduling_interval = dr_helpers.get_scheduling_interval(
-            cnv_workloads[0].workload_namespace,
-            discovered_apps=True,
-            resource_name=cnv_workloads[0].discovered_apps_placement_name,
-        )
-        logger.info("Running Relocate Steps")
-        wait_time = 2 * scheduling_interval  # Time in minutes
-        logger.info(f"Waiting for {wait_time} minutes to run IOs")
-        sleep(60)
-
-        dr_helpers.relocate(
-            preferred_cluster=secondary_cluster_name,
-            namespace=cnv_workloads[0].workload_namespace,
-            workload_placement_name=cnv_workloads[0].discovered_apps_placement_name,
-            discovered_apps=True,
-            old_primary=primary_cluster_name_after_failover,
-            workload_instance=cnv_workloads[0],
-        )
-        # Verify resources creation on primary managed cluster
-        config.switch_to_cluster_by_name(primary_cluster_name_before_failover)
-        dr_helpers.wait_for_all_resources_creation(
-            cnv_workloads[0].workload_pvc_count,
-            cnv_workloads[0].workload_pod_count,
-            cnv_workloads[0].workload_namespace,
-            discovered_apps=True,
-            vrg_name=cnv_workloads[0].discovered_apps_placement_name,
-        )
-        dr_helpers.wait_for_cnv_workload(
-            vm_name=cnv_workloads[0].vm_name,
-            namespace=cnv_workloads[0].workload_namespace,
-            phase=constants.STATUS_RUNNING,
-        )
-
-        # Validating data integrity (file1) after relocating VMs back to primary managed cluster
-        validate_data_integrity_vm(
-            cnv_workloads, vm_filepaths[0], md5sum_original, "Relocate"
-        )
-
-        # Validating data integrity (file2) after relocating VMs back to primary managed cluster
-        validate_data_integrity_vm(
-            cnv_workloads, vm_filepaths[1], md5sum_failover, "Relocate"
-        )
-
-        # Creating a file (file3) post relocate
-        for cnv_wl in cnv_workloads:
-            run_dd_io(
-                vm_obj=cnv_wl.vm_obj,
-                file_path=vm_filepaths[2],
-                username=cnv_wl.vm_username,
-                verify=True,
-            )
-
-        # if custom_sc:
-        #     assert check_mirroring_status_for_custom_pool(
-        #         pool_name="rdr-test-storage-pool-2way"
-        #     ), "Mirroring status check for custom SC failed"
-        #     logger.info("Mirroring status check for custom SC passed")
+        #
+        # # Creating a file (file2) post failover
+        # for cnv_wl in cnv_workloads:
+        #     md5sum_failover.append(
+        #         run_dd_io(
+        #             vm_obj=cnv_wl.vm_obj,
+        #             file_path=vm_filepaths[1],
+        #             username=cnv_wl.vm_username,
+        #             verify=True,
+        #         )
+        #     )
+        #
+        # for cnv_wl, md5sum in zip(cnv_workloads, md5sum_failover):
+        #     logger.info(
+        #         f"Checksum of files written after Failover: {vm_filepaths[1]} on VM {cnv_wl.workload_name}: {md5sum}"
+        #     )
+        #
+        # # logger.info("Recover the down managed cluster")
+        # # nodes_multicluster[active_primary_index].start_nodes(
+        # #     active_primary_cluster_node_objs
+        # # )
+        # # wait_for_nodes_status([node.name for node in active_primary_cluster_node_objs])
+        # # wait_for_pods_to_be_running(timeout=420, sleep=15)
+        # # assert ceph_health_check(tries=10, delay=30)
+        #
+        # logger.info("Doing Cleanup Operations")
+        # dr_helpers.do_discovered_apps_cleanup(
+        #     drpc_name=cnv_workloads[0].discovered_apps_placement_name,
+        #     old_primary=primary_cluster_name_before_failover,
+        #     workload_namespace=cnv_workloads[0].workload_namespace,
+        #     workload_dir=cnv_workloads[0].workload_dir,
+        #     vrg_name=cnv_workloads[0].discovered_apps_placement_name,
+        # )
+        #
+        # # if custom_sc:
+        # #     assert check_mirroring_status_for_custom_pool(
+        # #         pool_name="rdr-test-storage-pool-2way"
+        # #     ), "Mirroring status check for custom SC failed"
+        # #     logger.info("Mirroring status check for custom SC passed")
+        #
+        # # Doing Relocate in below code
+        # primary_cluster_name_after_failover = (
+        #     dr_helpers.get_current_primary_cluster_name(
+        #         cnv_workloads[0].workload_namespace,
+        #         discovered_apps=True,
+        #         resource_name=cnv_workloads[0].discovered_apps_placement_name,
+        #     )
+        # )
+        # config.switch_to_cluster_by_name(primary_cluster_name_before_failover)
+        # secondary_cluster_name = dr_helpers.get_current_secondary_cluster_name(
+        #     cnv_workloads[0].workload_namespace,
+        #     discovered_apps=True,
+        #     resource_name=cnv_workloads[0].discovered_apps_placement_name,
+        # )
+        #
+        # scheduling_interval = dr_helpers.get_scheduling_interval(
+        #     cnv_workloads[0].workload_namespace,
+        #     discovered_apps=True,
+        #     resource_name=cnv_workloads[0].discovered_apps_placement_name,
+        # )
+        # logger.info("Running Relocate Steps")
+        # wait_time = 2 * scheduling_interval  # Time in minutes
+        # logger.info(f"Waiting for {wait_time} minutes to run IOs")
+        # sleep(60)
+        #
+        # dr_helpers.relocate(
+        #     preferred_cluster=secondary_cluster_name,
+        #     namespace=cnv_workloads[0].workload_namespace,
+        #     workload_placement_name=cnv_workloads[0].discovered_apps_placement_name,
+        #     discovered_apps=True,
+        #     old_primary=primary_cluster_name_after_failover,
+        #     workload_instance=cnv_workloads[0],
+        # )
+        # # Verify resources creation on primary managed cluster
+        # config.switch_to_cluster_by_name(primary_cluster_name_before_failover)
+        # dr_helpers.wait_for_all_resources_creation(
+        #     cnv_workloads[0].workload_pvc_count,
+        #     cnv_workloads[0].workload_pod_count,
+        #     cnv_workloads[0].workload_namespace,
+        #     discovered_apps=True,
+        #     vrg_name=cnv_workloads[0].discovered_apps_placement_name,
+        # )
+        # dr_helpers.wait_for_cnv_workload(
+        #     vm_name=cnv_workloads[0].vm_name,
+        #     namespace=cnv_workloads[0].workload_namespace,
+        #     phase=constants.STATUS_RUNNING,
+        # )
+        #
+        # # Validating data integrity (file1) after relocating VMs back to primary managed cluster
+        # validate_data_integrity_vm(
+        #     cnv_workloads, vm_filepaths[0], md5sum_original, "Relocate"
+        # )
+        #
+        # # Validating data integrity (file2) after relocating VMs back to primary managed cluster
+        # validate_data_integrity_vm(
+        #     cnv_workloads, vm_filepaths[1], md5sum_failover, "Relocate"
+        # )
+        #
+        # # Creating a file (file3) post relocate
+        # for cnv_wl in cnv_workloads:
+        #     run_dd_io(
+        #         vm_obj=cnv_wl.vm_obj,
+        #         file_path=vm_filepaths[2],
+        #         username=cnv_wl.vm_username,
+        #         verify=True,
+        #     )
+        #
+        # # if custom_sc:
+        # #     assert check_mirroring_status_for_custom_pool(
+        # #         pool_name="rdr-test-storage-pool-2way"
+        # #     ), "Mirroring status check for custom SC failed"
+        # #     logger.info("Mirroring status check for custom SC passed")
