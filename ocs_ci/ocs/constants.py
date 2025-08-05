a0981e5fd4c260fefb3c4297f5c455e3c65d5699
shylesh <shmohan@redhat.com>
2025-08-05 11:16:52 +0530

    Add RDR Upgrade scenario: Refactor the code to accomodate RDR upgrades and introduce relevant markers to the upgrade tests (#11977)
    * Add RDR Upgrade scenario: Refactor the code to accomodate RDR upgrades and introduce relevant markers to the upgrade tests
    
    * Address review comments - Update roles list in RDR if user wants PassiveACM as well
    
    * Include RDR as well while instantiating CephClusterMulticluster object
    
    * Address review comments
    
    ---------
    
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -3424,6 +3424,9 @@ DELETE = "delete"
 MACHINE_POOL_ACTIONS = [CREATE, EDIT, DELETE]
 # MDR multicluster roles
 MDR_ROLES = ["ActiveACM", "PassiveACM", "PrimaryODF", "SecondaryODF"]
+RDR_ROLES = ["ActiveACM", "PrimaryODF", "SecondaryODF"]
+ACM_RANK = 1
+MANAGED_CLUSTER_RANK = 2
 
 ENCRYPTION_DASHBOARD_CONTEXT_MAP = {
     "Cluster-wide encryption": "cluster_wide_encryption",
