ea0e4f2142ac3b0aff93522d2dfd5c0d287c46cc
Pratik Surve <prsurve@redhat.com>
2025-08-11 11:08:03 +0530

    Load More build for installing submariner unreleased (#12862)
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -2968,7 +2968,7 @@ ACM_CATSRC_NAME = "acm-catalogsource"
 SUBMARINER_DOWNSTREAM_UNRELEASED_BUILD_URL = (
     "https://datagrepper.engineering.redhat.com/raw?topic=/topic/"
     "VirtualTopic.eng.ci.redhat-container-image.pipeline.complete"
-    "&rows_per_page=25&delta=1296000&contains=submariner-operator-bundle-container-v"
+    "&rows_per_page=25&delta=12960000&contains=submariner-operator-bundle-container-v"
 )
 ACM_BREW_BUILD_URL = (
     "https://datagrepper.engineering.redhat.com/raw?topic=/topic/"
