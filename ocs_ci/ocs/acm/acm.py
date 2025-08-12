77fe0813f03dd9989ae69efd347a0c94296655d9
am-agrawa <amagrawa@redhat.com>
2025-08-12 10:23:11 +0530

    OCP 4.20 changes for RDR deployment
    Signed-off-by: am-agrawa <amagrawa@redhat.com>

--- ocs_ci/ocs/acm/acm.py
+++ ocs_ci/ocs/acm/acm.py
@@ -566,16 +566,16 @@ def login_to_acm():
     driver = login_ui(url)
     page_nav = AcmPageNavigator()
     page_nav.page_has_loaded(retries=10, sleep_time=5)
-    look_for_local_cluster = page_nav.wait_until_expected_text_is_found(
+    look_for_administrator_dropdown = page_nav.wait_until_expected_text_is_found(
         locator=page_nav.acm_page_nav["click-local-cluster"],
-        expected_text="local-cluster",
+        expected_text="Administrator",
         timeout=15,
     )
-    if look_for_local_cluster:
-        log.info("local-cluster dropdown found, navigating from OCP to ACM console")
+    if look_for_administrator_dropdown:
+        log.info("Administrator dropdown found, navigating from OCP to ACM console")
         page_nav.navigate_from_ocp_to_acm_cluster_page()
     else:
-        log.warning("local-cluster dropdown not found, view is already on ACM console")
+        log.warning("Administrator dropdown not found, view is already on ACM console")
 
     if compare_versions(cmp_str):
         page_title = ACM_PAGE_TITLE_2_7_ABOVE
