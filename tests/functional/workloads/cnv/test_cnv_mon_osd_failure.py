8989e129a04a46379c0875724605141ad9d4d54e
Avdhoot <asagare@redhat.com>
2025-08-19 22:40:45 +0530

    correctred code
    Signed-off-by: Avdhoot <asagare@redhat.com>

--- tests/functional/workloads/cnv/test_cnv_mon_osd_failure.py
+++ tests/functional/workloads/cnv/test_cnv_mon_osd_failure.py
@@ -17,25 +17,19 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="class")
-def setup_cnv_workload(
-    request,
-    setup_cnv,
-    project_factory_class,
-    multi_cnv_workload_class,
-):
+def setup_cnv_workload(request, setup_cnv, multi_cnv_workload_class):
     """
     Set up CNV workload and create initial data.
     """
     logger.info("Setting up CNV workload and creating initial data")
 
     file_paths = ["/source_file.txt", "/new_file.txt"]
-    proj_obj = project_factory_class()
     (
         vm_objs_def,
         vm_objs_aggr,
         _,
         _,
-    ) = multi_cnv_workload_class(namespace=proj_obj.namespace)
+    ) = multi_cnv_workload_class()
     all_vms = vm_objs_def + vm_objs_aggr
     source_csums = {
         vm_obj.name: run_dd_io(vm_obj, file_path=file_paths[0], verify=True)
@@ -52,7 +46,6 @@ def setup_cnv_workload(
             logger.info("Data integrity maintained for VM {vm_obj.name}")
 
             run_dd_io(vm_obj=vm_obj, file_path=file_paths[1])
-            vm_obj.stop()
 
     request.addfinalizer(finalizer)
 
