ab3b463eec62bca0a284e71037e50c477c665596
Petr Balogh <pbalogh@redhat.com>
2025-08-19 16:51:02 +0200

    Re-try exec_cmd in case of client connetion lost
    Fixes: #12987
    
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- ocs_ci/utility/retry.py
+++ ocs_ci/utility/retry.py
@@ -64,7 +64,15 @@ def retry(
         @wraps(f)
         def f_retry(*args, **kwargs):
             mtries, mdelay = tries, delay
+            attempts = 0
+            exception_summary = set()
             while mtries > 1:
+                attempts += 1
+                if attempts == 2:
+                    # Show log only for second attempt
+                    logger.debug(
+                        f"Executing {f.__name__}. Tries: {mtries}. Delay: {mdelay}. Backoff: {backoff}"
+                    )
                 try:
                     if func is not None:
                         func()
@@ -76,11 +84,8 @@ def retry(
                                 f"Text: {text_in_exception} found in exception: {e}"
                             )
                         else:
-                            logger.debug(
-                                f"Text: {text_in_exception} not found in exception: {e}"
-                            )
                             raise
-                    logger.warning("%s, Retrying in %d seconds..." % (str(e), mdelay))
+                    exception_summary.add(repr(e))
                     time.sleep(mdelay)
                     mtries -= 1
                     mdelay = min(
@@ -88,6 +93,9 @@ def retry(
                     )  # Cap the delay to max_delay
                     if func is not None:
                         func()
+            if exception_summary:
+                logger.debug(f"Retry exception summary: {exception_summary}")
+
             return f(*args, **kwargs)
 
         return f_retry
