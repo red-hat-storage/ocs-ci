55bace929e7f82ef30e59c76dac69b1f7bc688eb
Jilju Joy <jijoy@redhat.com>
2025-08-21 11:08:15 +0530

    temporary commit to skip gitops
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -768,7 +768,7 @@ class Deployment(object):
 
         self.do_deploy_lvmo()
         self.do_deploy_submariner()
-        self.do_gitops_deploy()
+        #self.do_gitops_deploy()
         self.do_deploy_oadp()
         self.do_deploy_ocs()
         self.do_deploy_rdr()
