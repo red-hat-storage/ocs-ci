4fa37fa85628ed5af7201fbe0a0014772d5e677d
Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>
2025-09-03 19:07:30 +0530

    Parametrize the test for breakpoint scenario and code changes for the same
    Signed-off-by: Mahesh Shetty <mashetty@Maheshs-MacBook-Pro.local>

--- tests/cross_functional/stress/test_noobaa_under_stress.py
+++ tests/cross_functional/stress/test_noobaa_under_stress.py
@@ -29,10 +29,11 @@ class TestNoobaaUnderStress:
     base_setup_buckets = list()
 
     @pytest.mark.parametrize(
-        argnames=["with_failure", "delay"],
+        argnames=["test_type"],
         argvalues=[
-            pytest.param(True, 1800),
-            pytest.param(False, 300),
+            pytest.param("bulk"),
+            pytest.param("breakpoint"),
+            pytest.param("failure"),
         ],
     )
     def test_noobaa_under_stress(
@@ -48,19 +49,25 @@ class TestNoobaaUnderStress:
         scale_noobaa_db_pod_pv_size,
         threading_lock,
         disable_debug_logs,
-        with_failure,
-        delay,
+        test_type,
     ):
         """
-        Stress Noobaa by performing bulk s3 operations under both disruptive and non-disruptive
+        Stress Noobaa by performing bulk s3 operations under non-disruptive, disruptive and breakpoint
         situations. This consists mainly 3 stages
         mentioned below
             1. Base setup: Here we create the buckets of all possible types and then
-            load them with million objects in deep directory
-            2. S3 bulk operations: Here we perform various s3 operations such as list,
-            download, delete, metadata intense op etc concurrently on each of the bucket
-            respectively. We also induce noobaa specific failure if the test is disruptive.
+            load them with a million objects in deep directory
+            2. S3 object upload & S3 bulk operations:
+                a. Upload objects to the bucket concurrently with objects count increased by multiplier times
+                b. Perform various s3 operations such as list, download, delete, metadata intense op etc.
+                   concurrently on each of the bucket respectively.
+                   We also induce Noobaa specific failure if the test is disruptive.
             3. At the end delete objects from all the bucket in batches
+
+        For,
+            1. bulk - Multiplier goes from 1,2,3,4,5
+            2. breakpoint - Multiplier goes from 1,2,3,4,5,4,3,2,1
+            3. failure - Multiplier goes from 1,2,3,4,5
 
         """
 
@@ -86,7 +93,7 @@ class TestNoobaaUnderStress:
 
         # In-case of disruptive testing,we need to make sure we attempt
         # some re-tries
-        if with_failure:
+        if test_type == "failure":
             tries = 8
         else:
             tries = 1
@@ -106,7 +113,16 @@ class TestNoobaaUnderStress:
             # Iterate and stress the cluster with object upload
             # and other IO operations
             total_iterations = 1
-            for i in range(1, total_iterations + 1):
+            if test_type == "breakpoint":
+                # In case of breakpoint test we increment the multiplier first and
+                # then decrement the multiplier
+                iterator = list(range(1, total_iterations + 1)) + list(
+                    range(total_iterations - 1, 0, -1)
+                )
+            else:
+                iterator = list(range(1, total_iterations + 1))
+
+            for i in iterator:
                 current_iteration = i
                 multiplier = current_iteration + 1
                 logger.info(
@@ -206,8 +222,8 @@ class TestNoobaaUnderStress:
 
                 # Induce the noobaa specific failure if the test
                 # is with failure
-                if with_failure:
-                    induce_noobaa_failures(nodes, delay)
+                if test_type == "failure":
+                    induce_noobaa_failures(nodes, delay=1800)
 
                 # Wait until all the object operations are done
                 logger.info(
