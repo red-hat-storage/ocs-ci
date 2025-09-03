46c481a8bcaace8e991c2dc726f1fbc40cf66b7e
Elena Bondarenko <ebondare@redhat.com>
2025-09-03 17:29:44 +0200

    Add test for clones and snapshot testcase
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- tests/functional/pv/pvc_clone/test_clone_alert.py
+++ tests/functional/pv/pvc_clone/test_clone_alert.py
@@ -12,6 +12,7 @@ from ocs_ci.framework.testlib import (
     skipif_ocp_version,
 )
 from ocs_ci.ocs.resources import pvc
+from ocs_ci.helpers import helpers
 
 log = logging.getLogger(__name__)
 
@@ -68,7 +69,9 @@ class TestAlertWhenTooManyClonesCreated(ManageTest):
         """
         Test that there is no alert in the UI when limit of 200 clones is not reached
         """
-        time.sleep(60)
+        wait_time = 60
+        log.info(f"Waiting for {wait_time} seconds for alert to appear")
+        time.sleep(wait_time)
         alert_ui_obj = PageNavigator()
         alert_ui_obj.navigate_alerting_page()
         alert_ui_obj.take_screenshot()
@@ -86,10 +89,51 @@ class TestAlertWhenTooManyClonesCreated(ManageTest):
             self.pvc_obj.namespace,
         )
         self.cloned_obj_list.append(new_clone)
-        time.sleep(60)
+        wait_time = 180
+        log.info(f"Waiting for {wait_time} seconds for alert to appear")
+        time.sleep(wait_time)
         alert_ui_obj = PageNavigator()
         alert_ui_obj.navigate_alerting_page()
         alert_ui_obj.take_screenshot()
         assert alert_ui_obj.check_element_text(
             element="a", expected_text="HighRBDCloneSnapshotCount"
         ), "Clones alert not found on Alerting page. Expected to be present"
+
+    def test_alert_clones_and_snapshot(self, setup_ui_class, teardown_factory):
+        """
+        Test that there is an alert in the UI when limit of 199 clones + 1 snapshot is reached
+        Also test that alert disappears after clone deletion
+        """
+        snap_yaml = constants.CSI_RBD_SNAPSHOT_YAML
+        snap_name = helpers.create_unique_resource_name("test", "snapshot")
+        snap_obj = pvc.create_pvc_snapshot(
+            self.pvc_obj.name,
+            snap_yaml,
+            snap_name,
+            self.pvc_obj.namespace,
+            helpers.default_volumesnapshotclass(constants.CEPHBLOCKPOOL).name,
+        )
+        snap_obj.ocp.wait_for_resource(
+            condition="true",
+            resource_name=snap_obj.name,
+            column=constants.STATUS_READYTOUSE,
+            timeout=60,
+        )
+        teardown_factory(snap_obj)
+        wait_time = 180
+        log.info(f"Waiting for {wait_time} seconds for alert to appear")
+        time.sleep(wait_time)
+        alert_ui_obj = PageNavigator()
+        alert_ui_obj.navigate_alerting_page()
+        alert_ui_obj.take_screenshot()
+        assert alert_ui_obj.check_element_text(
+            element="a", expected_text="HighRBDCloneSnapshotCount"
+        ), "Clones alert not found on Alerting page. Expected to be present"
+        snap_obj.delete()
+        log.info(f"Waiting for {wait_time} seconds for alert to disappear")
+        time.sleep(wait_time)
+        alert_ui_obj.navigate_alerting_page()
+        alert_ui_obj.take_screenshot()
+        assert not alert_ui_obj.check_element_text(
+            element="a", expected_text="HighRBDCloneSnapshotCount"
+        ), "Clones alert present on Alerting page. Expected to be absent"
