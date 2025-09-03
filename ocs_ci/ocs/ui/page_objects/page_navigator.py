0b2aae524c781f13694a04513d2d97239c188d88
Elena Bondarenko <ebondare@redhat.com>
2025-09-03 10:59:46 +0200

    Add storageclass for ibm cloud
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/ocs/ui/page_objects/page_navigator.py
+++ ocs_ci/ocs/ui/page_objects/page_navigator.py
@@ -51,6 +51,8 @@ class PageNavigator(BaseUI):
                 self.storage_class = "standard_sc"
             else:
                 self.storage_class = "standard_csi_sc"
+        elif config.ENV_DATA["platform"].lower() == constants.IBMCLOUD_PLATFORM:
+            self.storage_class = "ibmc-vpc-block-10iops-tier"
         self.page_has_loaded(30, 2, self.page_nav["page_navigator_sidebar"])
 
     def navigate_OCP_home_page(self):
