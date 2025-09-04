2834b0d66f3383bfc0e9d490fefbf67b70da8f42
Jilju Joy <jijoy@redhat.com>
2025-09-04 17:13:54 +0530

    add break loop
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- tests/functional/pv/pvc_resize/test_pvc_expansion.py
+++ tests/functional/pv/pvc_resize/test_pvc_expansion.py
@@ -259,9 +259,11 @@ class TestPvcExpand(ManageTest):
         log.info(
             "Running IO on all pods in different iterations when PVCs are being expanded."
         )
-        for process_done in TimeoutSampler(500, 3, pvc_expand_process.done):
-            if not process_done:
+        for process_running in TimeoutSampler(500, 3, pvc_expand_process.running):
+            if process_running:
                 self.run_io_and_verify(2, "during_expand")
+            else:
+                break
         log.info(
             "Verified IO result on all pods which ran during the expansion process."
         )
