5699fb8e140de21ceab782f56b0bd41d48cc8a9c
Joy John Pinto <86055382+pintojoy@users.noreply.github.com>
2025-09-16 14:56:26 +0530

    Fixing tox issue
    Signed-off-by: Joy John Pinto <86055382+pintojoy@users.noreply.github.com>

--- ocs_ci/ocs/ui/performance_lib_ui.py
+++ ocs_ci/ocs/ui/performance_lib_ui.py
@@ -93,13 +93,13 @@ def grafana_resource_consumption_ui(test_duration, url, username, password):
         time_range_selector.click()
         logger.info(f"Test duration {last_duration}")
         time.sleep(5)
-        '''
+        """
         last_duration_option = wait.until(
             ec.element_to_be_clickable(
                 (By.XPATH, f"//label[normalize-space()='{last_duration}']")
             )
         )
-        '''
+        """
         last_duration_option = wait.until(
             ec.element_to_be_clickable(
                 (By.XPATH, f"//label[contains(text(), '{last_duration}')]")
