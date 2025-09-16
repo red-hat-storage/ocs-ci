ea42b56677942a0a92ee79cb65e395dd2a8ba428
Daniel Osypenko <dosypenk@redhat.com>
2025-09-16 16:48:07 +0300

    remove deprecated validations
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- ocs_ci/ocs/ui/validation_ui.py
+++ ocs_ci/ocs/ui/validation_ui.py
@@ -2,8 +2,6 @@ import logging
 import math
 import pytest
 import time
-import warnings
-
 import pandas as pd
 from ocs_ci.framework import config
 from ocs_ci.framework.logger_helper import log_step
@@ -12,11 +10,9 @@ from ocs_ci.ocs.cluster import (
     get_age_of_cluster_in_days,
 )
 from ocs_ci.ocs.exceptions import UnexpectedODFAccessException
-from ocs_ci.ocs.ui.page_objects.storage_cluster import StorageClusterPage
 from ocs_ci.ocs.ui.page_objects.page_navigator import PageNavigator
 from ocs_ci.ocs.ui.block_pool import BlockPoolUI
 from ocs_ci.ocs import constants
-from ocs_ci.utility import version
 from ocs_ci.utility.utils import TimeoutSampler
 from selenium.common.exceptions import (
     TimeoutException,
@@ -520,56 +516,6 @@ class ValidationUI(PageNavigator):
             logger.error(f"The pod {pod_name} not found on capacity_breakdown")
             res = False
         return res
-
-    def validate_storage_cluster_ui(self):
-        """
-
-        Function to validate Storage Cluster on UI for ODF 4.9 and above
-
-        """
-        if self.ocp_version_semantic >= version.VERSION_4_9:
-            self.navigate_installed_operators_page()
-            logger.info("Search and select storage cluster namespace")
-            self.select_namespace(project_name=config.ENV_DATA["cluster_namespace"])
-            logger.info(
-                "Click on Storage System under Provided APIs on Installed Operators Page"
-            )
-            self.do_click(self.validation_loc["storage-system-on-installed-operators"])
-            logger.info(
-                "Click on 'ocs-storagecluster-storagesystem' on Operator details page"
-            )
-            self.do_click(
-                self.validation_loc["ocs-storagecluster-storgesystem"],
-                enable_screenshot=True,
-            )
-            logger.info("Click on Resources")
-            self.do_click(self.validation_loc["resources-tab"], enable_screenshot=True)
-            logger.info("Checking Storage Cluster status on CLI")
-            storage_cluster_name = config.ENV_DATA["storage_cluster_name"]
-            storage_cluster = StorageClusterPage(
-                resource_name=storage_cluster_name,
-                namespace=config.ENV_DATA["cluster_namespace"],
-            )
-            assert storage_cluster.check_phase("Ready")
-            logger.info("Storage Cluster Status Check on UI")
-            storage_cluster_status_check = self.wait_until_expected_text_is_found(
-                locator=self.validation_loc["storage_cluster_readiness"],
-                expected_text="Ready",
-                timeout=600,
-            )
-            assert (
-                storage_cluster_status_check
-            ), "Storage Cluster Status reported on UI is not 'Ready'"
-            logger.info(
-                "Storage Cluster Status reported on UI is 'Ready', verification successful"
-            )
-            logger.info("Click on 'ocs-storagecluster'")
-            self.do_click(
-                self.validation_loc["ocs-storagecluster"], enable_screenshot=True
-            )
-            logger.info("Test passed!")
-        else:
-            warnings.warn("Not supported for OCP version less than 4.9")
 
     def validate_unprivileged_access(self):
         """
