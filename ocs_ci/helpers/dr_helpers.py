c91be740c43c2b01bee082fdc93849047adc93c2
prsurve <prsurve@redhat.com>
2025-09-17 11:30:32 +0530

    fix tox
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/helpers/dr_helpers.py
+++ ocs_ci/helpers/dr_helpers.py
@@ -1394,7 +1394,9 @@ def get_all_drpolicy():
     drpolicy_list = drpolicy_obj.get(all_namespaces=True).get("items")
     for cluster_name in config.clusters:
         if cluster_name.ENV_DATA.get("rbd_dr_scenario"):
-            current_managed_clusters_list.append(cluster_name.ENV_DATA.get("cluster_name"))
+            current_managed_clusters_list.append(
+                cluster_name.ENV_DATA.get("cluster_name")
+            )
 
     current_managed_clusters_list.remove(acm_hub_name)
     for drpolicy in drpolicy_list:
@@ -2497,7 +2499,9 @@ def get_cluster_set_name(switch_ctx=None):
     managed_clusters = ocp.OCP(kind=constants.ACM_MANAGEDCLUSTER).get().get("items", [])
     for cluster_name in config.clusters:
         if cluster_name.ENV_DATA.get("rbd_dr_scenario"):
-            current_managed_clusters_list.append(cluster_name.ENV_DATA.get("cluster_name"))
+            current_managed_clusters_list.append(
+                cluster_name.ENV_DATA.get("cluster_name")
+            )
 
     # ignore local-cluster here
     for i in managed_clusters:
