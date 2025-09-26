e47e2c9a295ccdfb43fee5dcee0f9875aaa26675
Elena Bondarenko <ebondare@redhat.com>
2025-09-26 12:45:26 +0200

    Add more missing locators
    Signed-off-by: Elena Bondarenko <ebondare@redhat.com>

--- ocs_ci/ocs/ui/views.py
+++ ocs_ci/ocs/ui/views.py
@@ -1778,6 +1778,14 @@ validation = {
         "button[data-test='horizontal-link-Block and File'][aria-selected='true']",
         By.CSS_SELECTOR,
     ),
+    "object-tab": (
+        "button[data-test='horizontal-link-Object']",
+        By.CSS_SELECTOR,
+    ),
+    "block-and-file-tab": (
+        "button[data-test='horizontal-link-Block and File']",
+        By.CSS_SELECTOR,
+    ),
 }
 
 validation_4_7 = {
