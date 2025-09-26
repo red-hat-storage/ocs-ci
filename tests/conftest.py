0788b19f23976d41f7b61cf6157b8e7c8598b0d8
Daniel Osypenko <dosypenk@redhat.com>
2025-09-26 10:51:05 +0300

    switch context on deployment_pod_factory factory. Unblock pvc deletion. (#13231)
    * pytest_fixture_setup-pvc-tests-fixes-multicluste
    
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>
    
    * _switch_context_helper for cleanUp
    
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>
    
    ---------
    
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -636,7 +636,9 @@ def pytest_fixture_setup(fixturedef, request):
     if ocsci_config.multicluster and ocsci_config.UPGRADE.get("upgrade", ""):
         if request.fixturenames.index(fixturedef.argname) == 0:
             for mark in request.node.iter_markers():
-                if mark.name == "config_index":
+                if isinstance(
+                    request.node, pytest.Function
+                ) and request.node.callspec.params.get("cluster_index"):
                     ocsci_config.switch_ctx(mark.args[0])
     _switch_context_helper(request)
 
@@ -1770,6 +1772,7 @@ def service_account_factory_fixture(request):
         """
         Delete the service account
         """
+        _switch_context_helper(request)
         for instance in instances:
             original_cluster = None
             if instance.ocp.cluster_context:
@@ -1867,6 +1870,8 @@ def deployment_pod_factory(request, pvc_factory, service_account_factory):
         """
         Delete dc pods
         """
+        _switch_context_helper(request)
+
         for instance in instances:
             delete_deployment_pods(instance)
 
--- tests/functional/pv/pv_services/test_run_io_multiple_pods.py
+++ tests/functional/pv/pv_services/test_run_io_multiple_pods.py
@@ -10,6 +10,7 @@ from ocs_ci.framework.pytest_customization.marks import (
 from ocs_ci.framework.testlib import ManageTest, tier2
 
 
+@run_on_all_clients_push_missing_configs
 @provider_mode
 @green_squad
 @tier2
@@ -27,38 +28,45 @@ class TestIOMultiplePods(ManageTest):
     Run IO on multiple pods in parallel
     """
 
-    num_of_pvcs = 6
+    num_of_pvcs = 3
     pvc_size = 5
 
     @pytest.fixture()
-    def pods(self, interface, multi_pvc_factory, pod_factory, deployment_pod_factory):
+    def pods(self, interface, multi_pvc_factory, pod_factory):
         """
-        Prepare multiple pods for the test
-
-        Returns:
-            list: Pod instances
-
+        Prepare multiple regular pods for the test
         """
         pvc_objs = multi_pvc_factory(
             interface=interface, size=self.pvc_size, num_of_pvc=self.num_of_pvcs
         )
+        return [pod_factory(interface=interface, pvc=pvc) for pvc in pvc_objs]
 
-        pod_objs = list()
-        for pvc_obj in pvc_objs[: len(pvc_objs) // 2]:
-            pod_objs.append(deployment_pod_factory(interface=interface, pvc=pvc_obj))
+    @pytest.fixture()
+    def deployment_pods(self, interface, multi_pvc_factory, deployment_pod_factory):
+        """
+        Prepare multiple deployment pods for the test
+        """
+        pvc_objs = multi_pvc_factory(
+            interface=interface, size=self.pvc_size, num_of_pvc=self.num_of_pvcs
+        )
+        return [
+            deployment_pod_factory(interface=interface, pvc=pvc) for pvc in pvc_objs
+        ]
 
-        for pvc_obj in pvc_objs[len(pvc_objs) // 2 :]:
-            pod_objs.append(pod_factory(interface=interface, pvc=pvc_obj))
-
-        return pod_objs
-
-    @run_on_all_clients_push_missing_configs
     def test_run_io_multiple_pods(self, pods, cluster_index):
         """
-        Run IO on multiple pods in parallel
+        Run IO on multiple regular pods in parallel
         """
         for pod in pods:
             pod.run_io(storage_type="fs", size="1G")
-
         for pod in pods:
+            get_fio_rw_iops(pod)
+
+    def test_run_io_multiple_deployment_pods(self, deployment_pods, cluster_index):
+        """
+        Run IO on multiple deployment pods in parallel
+        """
+        for pod in deployment_pods:
+            pod.run_io(storage_type="fs", size="1G")
+        for pod in deployment_pods:
             get_fio_rw_iops(pod)
