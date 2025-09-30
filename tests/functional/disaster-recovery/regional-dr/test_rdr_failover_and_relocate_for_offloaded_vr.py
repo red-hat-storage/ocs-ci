155b866fdd20edb54f9534006b9e962543044628
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2025-09-30 20:14:19 +0530

    updated failoer method for offloaded vr
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- tests/functional/disaster-recovery/regional-dr/test_rdr_failover_and_relocate_for_offloaded_vr.py
+++ tests/functional/disaster-recovery/regional-dr/test_rdr_failover_and_relocate_for_offloaded_vr.py
@@ -10,7 +10,6 @@ from ocs_ci.helpers import dr_helpers
 from ocs_ci.helpers.dr_helpers_ui import (
     dr_submariner_validation_from_ui,
     check_cluster_status_on_acm_console,
-    failover_relocate_ui,
     verify_failover_relocate_status_ui,
 )
 from ocs_ci.ocs import constants
@@ -38,29 +37,13 @@ class TestFailoverAndRelocateForOffloadedVR:
         pytest.param(
             False,  # primary_cluster_down = False
             constants.CEPHBLOCKPOOL,
-            False,  # via_ui = False
-            marks=pytest.mark.polarion_id("OCS-4430"),
-            id="primary_up-rbd-cli",
-        ),
-        pytest.param(
-            True,  # primary_cluster_down = True
-            constants.CEPHBLOCKPOOL,
-            False,  # via_ui = False
-            marks=pytest.mark.polarion_id("OCS-4427"),
-            id="primary_down-rbd-cli",
-        ),
-        pytest.param(
-            False,  # primary_cluster_down = False
-            constants.CEPHBLOCKPOOL,
-            True,  # via_ui = True
-            marks=pytest.mark.polarion_id("OCS-6861"),
+            True,  # via_ui = True,
             id="primary_up-rbd-ui",
         ),
         pytest.param(
             True,  # primary_cluster_down = True
             constants.CEPHBLOCKPOOL,
-            True,  # via_ui = True
-            marks=pytest.mark.polarion_id("OCS-4743"),
+            True,  # via_ui = True,
             id="primary_down-rbd-ui",
         ),
     ]
@@ -88,33 +71,34 @@ class TestFailoverAndRelocateForOffloadedVR:
         if via_ui:
             acm_obj = AcmAddClusters()
 
-        workloads = dr_workload(num_of_appset=1, pvc_interface=pvc_interface)
-        drpc_subscription = DRPC(namespace=workloads[0].workload_namespace)
+        workload = dr_workload(
+            num_of_subscription=0, num_of_appset=1, pvc_interface=pvc_interface
+        )
+        # drpc_subscription = DRPC(namespace=workloads[0].workload_namespace)
         drpc_appset = DRPC(
             namespace=constants.GITOPS_CLUSTER_NAMESPACE,
-            resource_name=f"{workloads[1].appset_placement_name}-drpc",
+            resource_name=f"{workload.appset_placement_name}-drpc",
         )
-        drpc_objs = [drpc_subscription, drpc_appset]
 
         primary_cluster_name = dr_helpers.get_current_primary_cluster_name(
-            workloads[0].workload_namespace
+            workload.workload_namespace
         )
         config.switch_to_cluster_by_name(primary_cluster_name)
         primary_cluster_index = config.cur_index
         primary_cluster_nodes = get_node_objs()
         secondary_cluster_name = dr_helpers.get_current_secondary_cluster_name(
-            workloads[0].workload_namespace
+            workload.workload_namespace
         )
 
         scheduling_interval = dr_helpers.get_scheduling_interval(
-            workloads[0].workload_namespace
+            workload.workload_namespace
         )
         wait_time = 2 * scheduling_interval  # Time in minutes
         logger.info(f"Waiting for {wait_time} minutes to run IOs")
         sleep(wait_time * 60)
 
         before_failover_last_group_sync_time = []
-        for obj in drpc_objs:
+        for obj in drpc_appset:
             before_failover_last_group_sync_time.append(
                 dr_helpers.verify_last_group_sync_time(obj, scheduling_interval)
             )
@@ -142,37 +126,19 @@ class TestFailoverAndRelocateForOffloadedVR:
         elif via_ui:
             check_cluster_status_on_acm_console(acm_obj)
 
-        for wl in workloads:
-            if via_ui:
-                # Failover via ACM UI
-                failover_relocate_ui(
-                    acm_obj,
-                    scheduling_interval=scheduling_interval,
-                    workload_to_move=f"{wl.workload_name}-1",
-                    policy_name=wl.dr_policy_name,
-                    failover_or_preferred_cluster=secondary_cluster_name,
-                )
-            else:
-                # Failover action via CLI
-                dr_helpers.failover(
-                    secondary_cluster_name,
-                    wl.workload_namespace,
-                    wl.workload_type,
-                    (
-                        wl.appset_placement_name
-                        if wl.workload_type == constants.APPLICATION_SET
-                        else None
-                    ),
-                )
+            # Failover action via CLI
+            dr_helpers.failover_for_offloade_vr(
+                workload.workload_namespace,
+            )
 
         # Verify resources creation on secondary cluster (failoverCluster)
         config.switch_to_cluster_by_name(secondary_cluster_name)
-        for wl in workloads:
-            dr_helpers.wait_for_all_resources_creation(
-                wl.workload_pvc_count,
-                wl.workload_pod_count,
-                wl.workload_namespace,
-            )
+
+        dr_helpers.wait_for_all_resources_creation(
+            workload.workload_pvc_count,
+            workload.workload_pod_count,
+            workload.workload_namespace,
+        )
 
         # Verify resources deletion from primary cluster
         config.switch_to_cluster_by_name(primary_cluster_name)
@@ -196,25 +162,23 @@ class TestFailoverAndRelocateForOffloadedVR:
             logger.info("Checking for Ceph Health OK")
             ceph_health_check()
 
-        for wl in workloads:
-            dr_helpers.wait_for_all_resources_deletion(wl.workload_namespace)
+        dr_helpers.wait_for_all_resources_deletion(workload.workload_namespace)
 
         if pvc_interface == constants.CEPHFILESYSTEM:
-            for wl in workloads:
-                # Verify the deletion of ReplicationDestination resources on secondary cluster
-                config.switch_to_cluster_by_name(secondary_cluster_name)
-                dr_helpers.wait_for_replication_destinations_deletion(
-                    wl.workload_namespace
-                )
-                # Verify the creation of ReplicationDestination resources on primary cluster
-                config.switch_to_cluster_by_name(primary_cluster_name)
-                dr_helpers.wait_for_replication_destinations_creation(
-                    wl.workload_pvc_count, wl.workload_namespace
-                )
+            # Verify the deletion of ReplicationDestination resources on secondary cluster
+            config.switch_to_cluster_by_name(secondary_cluster_name)
+            dr_helpers.wait_for_replication_destinations_deletion(
+                workload.workload_namespace
+            )
+            # Verify the creation of ReplicationDestination resources on primary cluster
+            config.switch_to_cluster_by_name(primary_cluster_name)
+            dr_helpers.wait_for_replication_destinations_creation(
+                workload.workload_pvc_count, workload.workload_namespace
+            )
 
         if pvc_interface == constants.CEPHBLOCKPOOL:
             dr_helpers.wait_for_mirroring_status_ok(
-                replaying_images=sum([wl.workload_pvc_count for wl in workloads])
+                replaying_images=sum([workload.workload_pvc_count])
             )
 
         if via_ui:
@@ -226,7 +190,7 @@ class TestFailoverAndRelocateForOffloadedVR:
 
         post_failover_last_group_sync_time = []
         for obj, initial_last_group_sync_time in zip(
-            drpc_objs, before_failover_last_group_sync_time
+            drpc_appset, before_failover_last_group_sync_time
         ):
             post_failover_last_group_sync_time.append(
                 dr_helpers.verify_last_group_sync_time(
@@ -236,63 +200,46 @@ class TestFailoverAndRelocateForOffloadedVR:
         logger.info("Verified lastGroupSyncTime after failover.")
 
         # Relocate action
-        for wl in workloads:
-            if via_ui:
-                logger.info("Start the process of Relocate from ACM UI")
-                check_cluster_status_on_acm_console(acm_obj)
-                dr_submariner_validation_from_ui(acm_obj)
-                # Relocate via ACM UI
-                failover_relocate_ui(
-                    acm_obj,
-                    scheduling_interval=scheduling_interval,
-                    workload_to_move=f"{wl.workload_name}-1",
-                    policy_name=wl.dr_policy_name,
-                    failover_or_preferred_cluster=primary_cluster_name,
-                    action=constants.ACTION_RELOCATE,
-                )
-            else:
-                # Relocate action via CLI
-                dr_helpers.relocate(
-                    primary_cluster_name,
-                    wl.workload_namespace,
-                    wl.workload_type,
-                    (
-                        wl.appset_placement_name
-                        if wl.workload_type == constants.APPLICATION_SET
-                        else None
-                    ),
-                )
+        dr_helpers.relocate(
+            primary_cluster_name,
+            workload.workload_namespace,
+            workload.workload_type,
+            (
+                workload.appset_placement_name
+                if workload.workload_type == constants.APPLICATION_SET
+                else None
+            ),
+        )
 
         # Verify resources deletion from secondary cluster
         config.switch_to_cluster_by_name(secondary_cluster_name)
-        for wl in workloads:
-            dr_helpers.wait_for_all_resources_deletion(wl.workload_namespace)
+
+        dr_helpers.wait_for_all_resources_deletion(workload.workload_namespace)
 
         # Verify resources creation on primary cluster (preferredCluster)
         config.switch_to_cluster_by_name(primary_cluster_name)
-        for wl in workloads:
-            dr_helpers.wait_for_all_resources_creation(
-                wl.workload_pvc_count,
-                wl.workload_pod_count,
-                wl.workload_namespace,
-            )
+
+        dr_helpers.wait_for_all_resources_creation(
+            workload.workload_pvc_count,
+            workload.workload_pod_count,
+            workload.workload_namespace,
+        )
 
         if pvc_interface == constants.CEPHFILESYSTEM:
-            for wl in workloads:
-                # Verify the deletion of ReplicationDestination resources on primary cluster
-                config.switch_to_cluster_by_name(primary_cluster_name)
-                dr_helpers.wait_for_replication_destinations_deletion(
-                    wl.workload_namespace
-                )
-                # Verify the creation of ReplicationDestination resources on secondary cluster
-                config.switch_to_cluster_by_name(secondary_cluster_name)
-                dr_helpers.wait_for_replication_destinations_creation(
-                    wl.workload_pvc_count, wl.workload_namespace
-                )
+            # Verify the deletion of ReplicationDestination resources on primary cluster
+            config.switch_to_cluster_by_name(primary_cluster_name)
+            dr_helpers.wait_for_replication_destinations_deletion(
+                workload.workload_namespace
+            )
+            # Verify the creation of ReplicationDestination resources on secondary cluster
+            config.switch_to_cluster_by_name(secondary_cluster_name)
+            dr_helpers.wait_for_replication_destinations_creation(
+                workload.workload_pvc_count, workload.workload_namespace
+            )
 
         if pvc_interface == constants.CEPHBLOCKPOOL:
             dr_helpers.wait_for_mirroring_status_ok(
-                replaying_images=sum([wl.workload_pvc_count for wl in workloads])
+                replaying_images=sum([workload.workload_pvc_count])
             )
 
         if via_ui:
@@ -302,7 +249,7 @@ class TestFailoverAndRelocateForOffloadedVR:
             )
 
         for obj, initial_last_group_sync_time in zip(
-            drpc_objs, post_failover_last_group_sync_time
+            drpc_appset, post_failover_last_group_sync_time
         ):
             dr_helpers.verify_last_group_sync_time(
                 obj, scheduling_interval, initial_last_group_sync_time
