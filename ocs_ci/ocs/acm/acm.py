26d1348c74183a7df54101495025255e074e136d
prsurve <prsurve@redhat.com>
2025-10-06 11:04:52 +0530

    Revert
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/ocs/acm/acm.py
+++ ocs_ci/ocs/acm/acm.py
@@ -968,7 +968,7 @@ def install_clusteradm():
         exec_cmd(
             "bash -c 'curl -L https://raw.githubusercontent.com/open-cluster-management-io/clusteradm/main/install.sh "
             "| bash'",
-            shell=True
+            shell=True,
         )
 
 
