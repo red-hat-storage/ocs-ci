22189b7721e5bb558035729aef2aeb7b7f9361e6
Petr Balogh <pbalogh@redhat.com>
2025-10-09 16:25:30 +0200

    Adjustment for new Selenium 4 version
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- ocs_ci/ocs/ui/validation_ui.py
+++ ocs_ci/ocs/ui/validation_ui.py
@@ -22,6 +22,7 @@ from ocs_ci.ocs.resources.storage_cluster import StorageCluster
 from ocs_ci.utility import version
 from ocs_ci.utility.utils import TimeoutSampler
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 
 logger = logging.getLogger(__name__)
 
@@ -215,8 +216,8 @@ class ValidationUI(PageNavigator):
         self.navigate_installed_operators_page()
         logger.info("Click on project dropdown")
         self.do_click(self.validation_loc["project-dropdown"])
-        default_projects_is_checked = self.driver.find_element_by_xpath(
-            "//input[@type='checkbox']"
+        default_projects_is_checked = self.driver.find_element(
+            By.XPATH, "//input[@type='checkbox']"
         )
         if default_projects_is_checked.get_attribute("data-checked-state") == "false":
             logger.info("Show default projects")
