8223f074c8dd2c3ec760750176086c67a6879111
vkathole <vkathole@redhat.com>
2025-11-10 12:19:28 +0530

    change keys
    Signed-off-by: vkathole <vkathole@redhat.com>

--- tests/functional/workloads/app/amq/test_rgw_kafka_notifications.py
+++ tests/functional/workloads/app/amq/test_rgw_kafka_notifications.py
@@ -20,7 +20,6 @@ from ocs_ci.helpers.helpers import default_storage_class
 from ocs_ci.ocs.amq import AMQ
 from ocs_ci.ocs.bucket_utils import retrieve_verification_mode
 from ocs_ci.ocs import constants
-from ocs_ci.ocs.resources.objectbucket import OBC
 from ocs_ci.ocs.resources.rgw import RGW
 from ocs_ci.ocs.resources.pod import get_pod_logs, get_rgw_pods, get_pod_obj
 from ocs_ci.ocs.utils import get_pod_name_by_pattern
@@ -118,21 +117,20 @@ class TestRGWAndKafkaNotifications(E2ETest):
 
         # Initialise to put objects
         data = "A random string data to write on created rgw bucket"
-        obc_obj = OBC(bucketname)
         s3_resource = boto3.resource(
             "s3",
             verify=retrieve_verification_mode(),
             region_name=config.ENV_DATA["region"],
             endpoint_url=rgw_endpoint,
-            aws_access_key_id=obc_obj.access_key_id,
-            aws_secret_access_key=obc_obj.access_key,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
         )
         s3_client = s3_resource.meta.client
 
         # Initialize notify command to run
         notify_cmd = (
-            f"python {notify_path} -e {rgw_endpoint} -a {obc_obj.access_key_id} "
-            f"-s {obc_obj.access_key} -b {bucketname} -ke {constants.KAFKA_ENDPOINT} -t {self.kafka_topic.name}"
+            f"python {notify_path} -e {rgw_endpoint} -a {access_key} "
+            f"-s {secret_key} -b {bucketname} -ke {constants.KAFKA_ENDPOINT} -t {self.kafka_topic.name}"
         )
         log.info(f"Running cmd {notify_cmd}")
 
