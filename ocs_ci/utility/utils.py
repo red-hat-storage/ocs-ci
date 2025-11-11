e2285b9fb6d5a93d459bc306560573b08bb62558
prsurve <prsurve@redhat.com>
2025-11-11 11:23:09 +0530

    Changes to support running rdr test from ibm repo
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/utility/utils.py
+++ ocs_ci/utility/utils.py
@@ -3887,6 +3887,7 @@ def clone_repo(
     to_checkout=None,
     clone_type="shallow",
     force_checkout=False,
+    clone_token=None,
 ):
     """
     Clone a repository or checkout latest changes if it already exists at
@@ -3903,6 +3904,7 @@ def clone_repo(
             clone_type as "normal".
         force_checkout (bool): True for force checkout to branch.
             force checkout will ignore the unmerged entries.
+        clone_token (str): Token to clone repo
 
     Raises:
         UnknownCloneTypeException: In case of incorrect clone_type is used
@@ -3942,6 +3944,9 @@ def clone_repo(
 
     if not os.path.isdir(location) or (tmp_repo and os.path.isdir(location)):
         log.info("Cloning repository into %s", location)
+        if clone_token:
+            safe_token = config.AUTH["github_ibm_odf_qe_ocs_workloads"]["gh_token"]
+            url = url.replace("https://", f"https://{safe_token}@")
         run_cmd(f"git clone {git_params} {url} {location}")
     else:
         log.info("Repository already cloned at %s, skipping clone", location)
