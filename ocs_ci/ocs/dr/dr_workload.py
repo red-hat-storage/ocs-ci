3a67cd787d94b0f5e1fadbe3888a2cf0f597e4e9
prsurve <prsurve@redhat.com>
2025-11-12 00:28:01 +0530

    mask secret when doing git clone
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/ocs/dr/dr_workload.py
+++ ocs_ci/ocs/dr/dr_workload.py
@@ -77,6 +77,10 @@ class BusyBox(DRWorkload):
         log.info(f"Repo used: {workload_repo_url}")
         workload_repo_branch = config.ENV_DATA["dr_workload_repo_branch"]
         workload_repo_login = config.ENV_DATA.get("dr_workload_repo_login")
+        if workload_repo_login:
+            workload_repo_login = config.AUTH["github_ibm_odf_qe_ocs_workloads"][
+                "gh_token"
+            ]
         super().__init__(
             "busybox", workload_repo_url, workload_repo_branch, workload_repo_login
         )
@@ -560,6 +564,10 @@ class BusyBox_AppSet(DRWorkload):
         workload_repo_url = config.ENV_DATA["dr_workload_repo_url"]
         workload_repo_branch = config.ENV_DATA["dr_workload_repo_branch"]
         workload_repo_login = config.ENV_DATA.get("dr_workload_repo_login")
+        if workload_repo_login:
+            workload_repo_login = config.AUTH["github_ibm_odf_qe_ocs_workloads"][
+                "gh_token"
+            ]
         super().__init__(
             "busybox", workload_repo_url, workload_repo_branch, workload_repo_login
         )
@@ -658,6 +666,7 @@ class BusyBox_AppSet(DRWorkload):
 
         """
         # Clone workload repo
+
         clone_repo(
             url=self.workload_repo_url,
             location=self.target_clone_dir,
@@ -824,6 +833,10 @@ class CnvWorkload(DRWorkload):
         workload_repo_url = config.ENV_DATA["dr_workload_repo_url"]
         workload_repo_branch = config.ENV_DATA["dr_workload_repo_branch"]
         workload_repo_login = config.ENV_DATA.get("dr_workload_repo_login")
+        if workload_repo_login:
+            workload_repo_login = config.AUTH["github_ibm_odf_qe_ocs_workloads"][
+                "gh_token"
+            ]
         super().__init__(
             "cnv", workload_repo_url, workload_repo_branch, workload_repo_login
         )
@@ -1196,6 +1209,10 @@ class BusyboxDiscoveredApps(DRWorkload):
         log.info(f"Repo used: {workload_repo_url}")
         workload_repo_branch = config.ENV_DATA["dr_workload_repo_branch"]
         workload_repo_login = config.ENV_DATA.get("dr_workload_repo_login")
+        if workload_repo_login:
+            workload_repo_login = config.AUTH["github_ibm_odf_qe_ocs_workloads"][
+                "gh_token"
+            ]
         super().__init__(
             "busybox", workload_repo_url, workload_repo_branch, workload_repo_login
         )
@@ -1623,6 +1640,10 @@ class CnvWorkloadDiscoveredApps(DRWorkload):
         workload_repo_url = config.ENV_DATA["dr_workload_repo_url"]
         workload_repo_branch = config.ENV_DATA["dr_workload_repo_branch"]
         workload_repo_login = config.ENV_DATA.get("dr_workload_repo_login")
+        if workload_repo_login:
+            workload_repo_login = config.AUTH["github_ibm_odf_qe_ocs_workloads"][
+                "gh_token"
+            ]
         super().__init__(
             "cnv", workload_repo_url, workload_repo_branch, workload_repo_login
         )
