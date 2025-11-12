a075fc23d9fd93353ab79c72d87822a267ab1f9e
Prasad Desala <tdesala@redhat.com>
2025-11-12 14:21:36 +0530

    Cephfs Stress automation: cluster and verification checks
    Signed-off-by: Prasad Desala <tdesala@redhat.com>

--- tests/cross_functional/stress/cephfs/test_breakpoint.py
+++ tests/cross_functional/stress/cephfs/test_breakpoint.py
@@ -1,8 +1,16 @@
 import logging
+import threading
+import time
 
 from ocs_ci.framework.pytest_customization.marks import magenta_squad
 from ocs_ci.framework.testlib import E2ETest
-from ocs_ci.helpers.cephfs_stress_helpers import create_cephfs_stress_pod
+from ocs_ci.helpers.cephfs_stress_helpers import (
+    create_cephfs_stress_pod,
+    continuous_checks_runner,
+    verification_failures,
+    stop_event,
+)
+from ocs_ci.ocs import constants
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +41,48 @@ class TestCephfsStress(E2ETest):
         Perform a variety of file operations (e.g., create,append,rename,stat,chmod,ls-l) on the iter-(1) data
 
         """
-        cephfs_stress_pod_obj = create_cephfs_stress_pod()
-        logger.info(f"The CephFS-stress pod {cephfs_stress_pod_obj.name} is created ")
-        # TODO - Background checks and validations
+        CHECKS_RUNNER_INTERVAL_MINUTES = 30
+        POD_INTERVAL_SECONDS = 300
+        stress_checks_thread = threading.Thread(
+            target=continuous_checks_runner,
+            args=(CHECKS_RUNNER_INTERVAL_MINUTES,),
+            name="StressCheckRunnerThread",
+            daemon=True,
+        )
+        stress_checks_thread.start()
+
+        try:
+            cephfs_stress_pod_obj = create_cephfs_stress_pod()
+            logger.info(
+                f"The CephFS-stress pod {cephfs_stress_pod_obj.name} is created "
+            )
+
+            while True:
+                # Check for failure signal from the check-thread
+                if verification_failures:
+                    raise Exception(
+                        f"Test failed due to validation failure: {verification_failures[0]}"
+                    )
+
+                status = cephfs_stress_pod_obj.status()
+
+                if status == constants.STATUS_COMPLETED:
+                    logger.info(
+                        f"Pod '{cephfs_stress_pod_obj.name}' reached 'Completed' state"
+                    )
+                    break
+                elif status == constants.STATUS_RUNNING:
+                    logger.debug(
+                        f"Pod is 'Running'. Waiting for {POD_INTERVAL_SECONDS}s..."
+                    )
+                    time.sleep(POD_INTERVAL_SECONDS)
+                else:
+                    raise Exception(
+                        f"Pod '{cephfs_stress_pod_obj.name}' entered unexpected state '{status}' state"
+                    )
+
+        finally:
+            logger.info("Signaling check thread to stop...")
+            stop_event.set()
+            stress_checks_thread.join()
+            logger.info("StressCheckRunnerThread has stopped")
