68fa100ed5ee12bce3fb9163e4bcbbd1167072b4
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-11-18 13:26:54 +0100

    [release-4.19] OCSQECL-5384 corrective measures. test_prometheus_rule_failures (#13066)
    * OCSQECL-5384 corrective measures
    
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>
    
    
    ---------
    
    Signed-off-by: Daniel Osypenko <dosypenk@redhat.com>
    Co-authored-by: Daniel Osypenko <dosypenk@redhat.com>

--- tests/functional/monitoring/prometheus/alerts/test_alerting_works.py
+++ tests/functional/monitoring/prometheus/alerts/test_alerting_works.py
@@ -8,10 +8,11 @@ from ocs_ci.framework.pytest_customization.marks import (
     blue_squad,
     provider_mode,
 )
-from ocs_ci.ocs import constants
+from ocs_ci.ocs import constants, defaults
 from ocs_ci.ocs.ocp import OCP
 import ocs_ci.utility.prometheus
-
+from ocs_ci.ocs.resources.pod import get_pod_logs
+from ocs_ci.ocs.utils import get_pod_name_by_pattern
 
 log = logging.getLogger(__name__)
 
@@ -39,17 +40,41 @@ def test_alerting_works(threading_lock):
 def test_prometheus_rule_failures(threading_lock):
     """
     There should be no PrometheusRuleFailures alert when OCS is configured.
+    This test is extended to check for many-to-many matching errors in Prometheus logs (more in DFBUGS-2571).
+    If such error message found PrometheusRuleFailures alert must fire as well.
     """
+    # any check with state False will fail the test
+    test_results = {}
+
     prometheus = ocs_ci.utility.prometheus.PrometheusAPI(threading_lock=threading_lock)
     alerts_response = prometheus.get(
         "alerts", payload={"silenced": False, "inhibited": False}
     )
-    assert alerts_response.ok is True
+    test_results["alert-msg-ok-check"] = alerts_response.ok is True
     alerts = alerts_response.json()["data"]["alerts"]
     log.info(f"Prometheus Alerts: {alerts}")
-    assert constants.ALERT_PROMETHEUSRULEFAILURES not in [
-        alert["labels"]["alertname"] for alert in alerts
-    ]
+    test_results[f"{constants.ALERT_PROMETHEUSRULEFAILURES}-present-check"] = (
+        constants.ALERT_PROMETHEUSRULEFAILURES
+        not in [alert["labels"]["alertname"] for alert in alerts]
+    )
+    prometheus_pods = get_pod_name_by_pattern(
+        defaults.PROMETHEUS_ROUTE, constants.MONITORING_NAMESPACE
+    )
+    for pod_name in prometheus_pods:
+        log.info(f"Checking logs of pod {pod_name}")
+        pod_logs = get_pod_logs(
+            pod_name=pod_name,
+            namespace=constants.MONITORING_NAMESPACE,
+        ).splitlines()
+        pod_logs.reverse()
+        for log_line in pod_logs:
+            if "many-to-many matching not allowed" in log_line.lower():
+                test_results[f"many-to-many-error-present-{pod_name}-check"] = False
+                break
+        else:
+            test_results[f"many-to-many-error-present-{pod_name}-check"] = True
+
+    assert all(test_results.values()), f"One or more checks failed: {test_results}"
 
 
 def setup_module(module):
