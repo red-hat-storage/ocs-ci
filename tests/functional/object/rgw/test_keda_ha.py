b79bf94c6c3ded7d640d74370b3ccb8b1b97c251
Sagi Hirshfeld <shirshfe@redhat.com>
2025-11-20 18:33:16 +0200

    Add the ScaledObject functionality + fixes
    Signed-off-by: Sagi Hirshfeld <shirshfe@redhat.com>

--- tests/functional/object/rgw/test_keda_ha.py
+++ tests/functional/object/rgw/test_keda_ha.py
@@ -1,9 +1,12 @@
 import logging
 
+from ocs_ci.framework import config
 from ocs_ci.framework.pytest_customization.marks import (
     skipif_disconnected_cluster,
     tier1,
 )
+from ocs_ci.helpers.helpers import create_unique_resource_name
+from ocs_ci.ocs.ocp import OCP
 
 logger = logging.getLogger(__name__)
 
@@ -11,14 +14,28 @@ logger = logging.getLogger(__name__)
 @skipif_disconnected_cluster
 class TestKedaHA:
     """
-    Test RGW's integration with Keda autoscaler for high availability
+    Test RGW's integration with the KEDA autoscaler
     """
 
     @tier1
     def test_rgw_keda_ha(self, keda_class):
         """
-        Test RGW's integration with Keda autoscaler for high availability
+        Test RGW's integration with Keda autoscaler
         """
-        keda = keda_class  # just an alias
+        keda = keda_class
         logger.info(f"KEDA: {keda}")
         assert keda.is_installed(), "KEDA is not installed"
+
+        deployment_name = create_unique_resource_name("hello-world", "deployment")
+        OCP(namespace=config.ENV_DATA["cluster_namespace"]).exec_oc_cmd(
+            f"create deployment {deployment_name} --image=busybox -- sleep 3600"
+        )
+
+        scaled_object = keda.create_thanos_metric_scaled_object(
+            deployment=deployment_name,
+            query="sum(sin(vector(time())))",
+            threshold="0.5",
+        )
+        logger.info(f"ScaledObject: {scaled_object}")
+
+        print(5)
