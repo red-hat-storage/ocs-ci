7204fa2e2b9eb0345890c028def785524a12501f
Itzhak Kave <ikave@ibm.com>
2025-11-26 14:08:16 +0200

    Update the process of simulate the Ceph BlueStore label, add another script and functions to install a minimal ceph cluster
    Signed-off-by: Itzhak Kave <ikave@ibm.com>

--- tests/libtest/test_simulate_ceph_bluestore_label.py
+++ tests/libtest/test_simulate_ceph_bluestore_label.py
@@ -7,7 +7,14 @@ from ocs_ci.framework.testlib import (
     brown_squad,
     skipif_no_lso,
 )
-from ocs_ci.deployment.baremetal import simulate_ceph_bluestore_on_node_disk
+
+from ocs_ci.framework import config
+from ocs_ci.deployment.helpers.ceph_cluster import (
+    simulate_full_ceph_bluestore_process_on_wnodes,
+    simulate_ceph_bluestore_on_wnodes,
+    get_ceph_fsid,
+    get_ceph_admin_key,
+)
 from ocs_ci.ocs.node import get_nodes
 
 log = logging.getLogger(__name__)
@@ -16,7 +23,7 @@ log = logging.getLogger(__name__)
 @brown_squad
 @libtest
 @ignore_leftovers
-@skipif_no_lso
+# @skipif_no_lso
 class TestSimulateCephBlueStoreLabel(ManageTest):
     """
     Test that simulate_bluestore_label correctly stamps and verifies a BlueStore label on a test disk.
@@ -27,11 +34,13 @@ class TestSimulateCephBlueStoreLabel(ManageTest):
         Test simulates a Ceph BlueStore label on the worker node disks.
 
         """
-        results = []
-        wnodes = get_nodes()
-        for wnode in wnodes:
-            result = simulate_ceph_bluestore_on_node_disk(wnode)
-            results.append(result)
-
-        assert all(results), "BlueStore label simulation failed"
-        log.info("BlueStore label simulation succeeded ")
+        simulate_bluestore_label = config.ENV_DATA.get(
+            "simulate_bluestore_label", False
+        )
+        if simulate_bluestore_label:
+            # simulate_full_ceph_bluestore_process_on_wnodes()
+            wnodes = get_nodes()
+            simulate_ceph_bluestore_on_wnodes(wnodes)
+            get_ceph_fsid(wnodes[0])
+            get_ceph_admin_key(wnodes[0])
+            log.info("BlueStore label simulation succeeded on all worker nodes disks")
