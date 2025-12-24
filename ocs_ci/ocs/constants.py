1c6f6e4c6992352a8038135caf15a5b3afd7366d
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-12-24 12:52:32 +0100

    Correcting the expected value of mon for Performance resource profile (#13845)
    Signed-off-by: suchita-g <sgatfane@redhat.com>
    Co-authored-by: suchita-g <sgatfane@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -1606,7 +1606,7 @@ PERFORMANCE_PROFILE_REQUEST_CPU_VALUES = {
 }
 PERFORMANCE_PROFILE_REQUEST_MEMORY_VALUES = {
     "mgr": "2Gi",
-    "mon": "2Gi",
+    "mon": "3Gi",
     "osd": "8Gi",
     "mds": "8Gi",
     "rgw": "4Gi",
@@ -1653,7 +1653,7 @@ PERFORMANCE_PROFILE_CPU_LIMIT_VALUES = {
 }
 PERFORMANCE_PROFILE_MEMORY_LIMIT_VALUES = {
     "mgr": "4Gi",
-    "mon": "2Gi",
+    "mon": "3Gi",
     "osd": "8Gi",
     "mds": "8Gi",
     "rgw": "4Gi",
