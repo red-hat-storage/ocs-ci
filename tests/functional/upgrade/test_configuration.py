e8f3415e29ed8e3496d149839809d4c0c20abbd7
Suchita Gatfane <sgatfane@redhat.com>
2026-01-21 16:52:24 +0530

    Fix AttributeError in test_max_unavaialable_rbd/cephfs tests (#14165)
    Fixes issue #14164
    
    Signed-off-by: suchita-g <sgatfane@redhat.com>

--- tests/functional/upgrade/test_configuration.py
+++ tests/functional/upgrade/test_configuration.py
@@ -86,10 +86,11 @@ def test_max_unavaialable_rbd(upgrade_stats):
         namespace=config.ENV_DATA["cluster_namespace"],
         resource_name=constants.ROOK_OPERATOR_CONFIGMAP,
     ).get()
-    config_value = configmap.get("data").get(
-        "CSI_RBD_PLUGIN_UPDATE_STRATEGY_MAX_UNAVAILABLE"
-    )
-    assert config_value == upgrade_stats["odf_upgrade"]["rbd_max_unavailable"]
+    if config.UPGRADE.get("csi_rbd_plugin_update_strategy_max_unavailable") is not None:
+        config_value = configmap.get("data").get(
+            "CSI_RBD_PLUGIN_UPDATE_STRATEGY_MAX_UNAVAILABLE"
+        )
+        assert config_value == upgrade_stats["odf_upgrade"]["rbd_max_unavailable"]
 
 
 @post_upgrade
@@ -106,10 +107,14 @@ def test_max_unavaialable_cephfs(upgrade_stats):
         namespace=config.ENV_DATA["cluster_namespace"],
         resource_name=constants.ROOK_OPERATOR_CONFIGMAP,
     ).get()
-    config_value = configmap.get("data").get(
-        "CSI_CEPHFS_PLUGIN_UPDATE_STRATEGY_MAX_UNAVAILABLE"
-    )
-    assert config_value == upgrade_stats["odf_upgrade"]["cephfs_max_unavailable"]
+    if (
+        config.UPGRADE.get("csi_cephfs_plugin_update_strategy_max_unavailable")
+        is not None
+    ):
+        config_value = configmap.get("data").get(
+            "CSI_CEPHFS_PLUGIN_UPDATE_STRATEGY_MAX_UNAVAILABLE"
+        )
+        assert config_value == upgrade_stats["odf_upgrade"]["cephfs_max_unavailable"]
 
 
 @pytest.mark.parametrize(
