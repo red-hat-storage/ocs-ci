b56483ab0170d246fb7bea04be36377440ebaccb
Itzhak Kave <ikave@ibm.com>
2026-01-27 19:19:22 +0200

    Test attach storage UI
    This PR implements the Page Object Model (POM) for the "Attach Storage" feature in the OpenShift Console UI. It introduces a new UI class AttachStorage, updates the navigation logic, defines necessary locators, and adds a new Tier 2 functional test (TestMultipleDeviceClassesUI) to verify the creation of additional device classes via the UI.
    
    Signed-off-by: Itzhak Kave <ikave@ibm.com>

--- tests/functional/z_cluster/cluster_expansion/test_create_multiple_device_classes.py
+++ tests/functional/z_cluster/cluster_expansion/test_create_multiple_device_classes.py
@@ -12,6 +12,10 @@ from ocs_ci.framework.testlib import (
     skipif_bm,
     skipif_hci_provider_or_client,
     polarion_id,
+    ui,
+    black_squad,
+    skipif_external_mode,
+    skipif_mcg_only,
 )
 from ocs_ci.helpers.sanity_helpers import Sanity
 from ocs_ci.ocs import constants
@@ -21,6 +25,7 @@ from ocs_ci.ocs.device_classes import (
     verify_deviceclasses_steps,
     add_disks_matching_lvs_size,
     get_default_lvs_obj,
+    verify_available_pvs_for_deviceclass,
 )
 from ocs_ci.ocs.resources.pv import (
     wait_for_pvs_in_lvs_to_reach_status,
@@ -38,6 +43,7 @@ from ocs_ci.helpers.helpers import (
     create_rbd_deviceclass_storageclass,
 )
 from ocs_ci.utility.utils import ceph_health_check
+from ocs_ci.ocs.ui.page_objects.page_navigator import PageNavigator
 
 
 log = logging.getLogger(__name__)
@@ -48,6 +54,8 @@ log = logging.getLogger(__name__)
 @skipif_no_lso
 @skipif_bm
 @skipif_hci_provider_or_client
+@skipif_external_mode
+@skipif_mcg_only
 class TestMultipleDeviceClasses(ManageTest):
     """
     Automate the multiple device classes tests
@@ -250,3 +258,74 @@ class TestMultipleDeviceClasses(ManageTest):
         self.sanity_helpers.create_resources(
             pvc_factory, pod_factory, bucket_factory, rgw_bucket_factory
         )
+
+
+@brown_squad
+@ui
+@black_squad
+@ignore_leftovers
+@skipif_no_lso
+@skipif_bm
+@skipif_hci_provider_or_client
+@skipif_external_mode
+@skipif_mcg_only
+class TestMultipleDeviceClassesUI(ManageTest):
+    """
+    Automate the multiple device classes tests via UI
+
+    """
+
+    @pytest.fixture(autouse=True)
+    def init_sanity(self, pvc_factory, pod_factory, bucket_factory, rgw_bucket_factory):
+        """
+        Initialize Sanity instance
+
+        """
+        self.pvc_factory = pvc_factory
+        self.pod_factory = pod_factory
+        self.bucket_factory = bucket_factory
+        self.rgw_bucket_factory = rgw_bucket_factory
+        self.sanity_helpers = Sanity()
+
+    def post_deviceclass_checks(self):
+        """
+        Run the verification steps after adding a new device class:
+        1. Check the cluster and Ceph health.
+        2. Check basic cluster functionality by creating some resources.
+
+        """
+        log.info("Verification steps after adding a new deviceclass...")
+        verify_deviceclasses_steps()
+        log.info("Checking the cluster and Ceph health")
+        self.sanity_helpers.health_check(cluster_check=True, tries=40)
+        log.info("Check basic cluster functionality by creating some resources")
+        self.sanity_helpers.create_resources(
+            self.pvc_factory,
+            self.pod_factory,
+            self.bucket_factory,
+            self.rgw_bucket_factory,
+        )
+
+    @pytest.fixture(autouse=True)
+    def teardown(self):
+        """
+        Check that the ceph health is OK
+
+        """
+        log.info("Wait for the ceph health to be OK")
+        ceph_health_check(tries=20)
+
+    @tier2
+    def test_add_new_device_class_ui(self, setup_ui_session):
+        """
+        The test will perform the following steps:
+        1. Verify that there are available PVs for attaching a new device class.
+        2. Navigate to the 'Attach Storage' form in the UI.
+        3. Fill the form with the default values and submit it, which will add a new device class.
+        4. Run the verification steps as defined in the method 'post_deviceclass_checks'.
+
+        """
+        verify_available_pvs_for_deviceclass()
+        attach_storage = PageNavigator().nav_to_attach_storage_page()
+        attach_storage.send_form_with_default_values()
+        self.post_deviceclass_checks()
