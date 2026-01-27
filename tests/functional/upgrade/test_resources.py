30d39730808fb5d9a230ff857d8003989815d19f
anajain16 <anajain@redhat.com>
2026-01-27 13:59:57 +0530

    added mcg only deployment check
    Signed-off-by: anajain16 <anajain@redhat.com>

--- tests/functional/upgrade/test_resources.py
+++ tests/functional/upgrade/test_resources.py
@@ -15,6 +15,8 @@ from ocs_ci.framework.pytest_customization.marks import (
     purple_squad,
     skipif_external_mode,
     skipif_hci_client,
+    skipif_mcg_only,
+    jira,
 )
 from ocs_ci.ocs import constants
 from ocs_ci.ocs import ocp
@@ -168,6 +170,8 @@ def deprecated_test_noobaa_service_mon_after_ocs_upgrade():
 @post_upgrade
 @skipif_external_mode
 @skipif_hci_client
+@skipif_mcg_only
+@jira("DFBUGS-5211")
 @pytest.mark.polarion_id("OCS-7419")
 @purple_squad
 def test_blackbox_pod_after_upgrade():
