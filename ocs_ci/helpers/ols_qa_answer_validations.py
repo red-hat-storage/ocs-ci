319293b15a0ffbe226fd4467c2f8cbb059dfb2d8
Akarsha-rai <akrai@redhat.com>
2026-02-04 15:21:19 +0530

    Add a validation for response given by OLS
    Signed-off-by: Akarsha-rai <akrai@redhat.com>

--- ocs_ci/helpers/ols_qa_answer_validations.py
+++ ocs_ci/helpers/ols_qa_answer_validations.py
@@ -5,12 +5,22 @@ from ocs_ci.utility import templating
 
 
 def load_test_data():
+    """
+
+    Loads 20 sample QnA is set "qa-expectation.yaml" and returns dict generated from template file
+
+    """
     return templating.generate_yaml_from_jinja2_template_with_data(
         constants.OLS_QA_EXPECTATIONS
     )
 
 
 def calculate_accuracy(answer, keywords):
+    """
+
+    Calculates accuracy based on the keywords given in "qa-expectation.yaml"
+
+    """
     if not keywords:
         return 1.0
     matched = sum(1 for k in keywords if k.lower() in answer.lower())
@@ -18,40 +28,26 @@ def calculate_accuracy(answer, keywords):
 
 
 def calculate_consistency(ans1, ans2):
+    """
+
+    Calculate consistency based on the response given by OLS for 2 same question
+
+    """
     return SequenceMatcher(None, ans1, ans2).ratio()
 
 
 def is_uncertain(answer):
+    """
+
+    Verify that question is not valid based on the phrases
+
+    """
+
     phrases = [
         "not available",
         "no information",
         "not found",
         "i don't know",
-        "no data",
+        "i don't have" "no data",
     ]
     return any(p in answer.lower() for p in phrases)
-
-
-# def test_generate_graph(results=None, accuracy_threshold=0.7, consistency_threshold=0.75):
-#     """
-#
-#     """
-#     if not results:
-#         pytest.skip("No results to plot")
-#
-#     ids = [r["id"] for r in results]
-#     accuracy = [r["accuracy"] for r in results]
-#     consistency = [r["consistency"] for r in results]
-#
-#     plt.figure(figsize=(10, 5))
-#     plt.plot(ids, accuracy, label="Accuracy", marker="o")
-#     plt.plot(ids, consistency, label="Consistency", marker="x")
-#     plt.axhline(accuracy_threshold, linestyle="--", color="red", label="Accuracy Threshold")
-#     plt.axhline(consistency_threshold, linestyle="--", color="orange", label="Consistency Threshold")
-#
-#     plt.xlabel("Question ID")
-#     plt.ylabel("Score")
-#     plt.title("OLS Data Foundation Answer Validation")
-#     plt.legend()
-#     plt.grid(True)
-#     plt.show()
