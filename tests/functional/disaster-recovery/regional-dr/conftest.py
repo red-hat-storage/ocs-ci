335bcf8467c5971bfc46a1c068bbfc3d61512113
prsurve <prsurve@redhat.com>
2026-02-04 22:07:03 +0530

    scrap few function and implement as per acm
    Signed-off-by: prsurve <prsurve@redhat.com>

--- tests/functional/disaster-recovery/regional-dr/conftest.py
+++ tests/functional/disaster-recovery/regional-dr/conftest.py
@@ -5,7 +5,6 @@ import time
 from ocs_ci.framework import config
 from ocs_ci.helpers.dr_helpers import (
     create_gitops_private_repo_secret,
-    create_sub_app_private_repo_secret,
 )
 from ocs_ci.ocs import constants
 from ocs_ci.deployment import acm
@@ -65,24 +64,6 @@ def check_gitops_secret_offline_mode():
             except (CommandFailed, FileNotFoundError):
                 log.info("Secret not found creating New one")
                 create_gitops_private_repo_secret()
-
-
-@pytest.fixture(scope="session", autouse=True)
-def check_subscription_app_secret_offline_mode():
-    if config.MULTICLUSTER.get("multicluster_mode") != constants.RDR_MODE:
-        return
-    if config.ENV_DATA.get("dr_workload_repo_login"):
-        for cluster_index in range(config.nclusters):
-            config.switch_ctx(cluster_index)
-            try:
-                OCP(
-                    kind=constants.SECRET,
-                    namespace=constants.DEFAULT_NAMESPACE,
-                    resource_name=constants.PRIVATE_REPO_SUB_SECRET,
-                ).get()
-            except (CommandFailed, FileNotFoundError):
-                log.info("Secret not found creating New one")
-                create_sub_app_private_repo_secret()
 
 
 @pytest.fixture()
