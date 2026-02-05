a359655a53b6fb690ebd16f3c26b353f840fe152
Jilju Joy <jijoy@redhat.com>
2026-02-05 13:33:11 +0530

    Do not exclude master node when listing worker nodes in compact mode
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/ocs/node.py
+++ ocs_ci/ocs/node.py
@@ -33,7 +33,6 @@ from ocs_ci.utility.utils import TimeoutSampler, convert_device_size, get_az_cou
 from ocs_ci.ocs import machine
 from ocs_ci.ocs.resources import pod
 from ocs_ci.utility.utils import set_selinux_permissions, get_ocp_version
-from ocs_ci.helpers.helpers import check_cluster_is_compact
 from ocs_ci.ocs.resources.pv import (
     get_pv_objs_in_sc,
     get_pv_size,
@@ -1349,9 +1348,13 @@ def get_worker_nodes():
             if node.get("metadata").get("name") not in infra_node_ids
         ]
     worker_nodes_list = [node.get("metadata").get("name") for node in nodes]
-    if is_hci_provider_cluster() and not check_cluster_is_compact():
+    if is_hci_provider_cluster():
         master_node_list = get_master_nodes()
-        worker_nodes_list = list(set(worker_nodes_list) - set(master_node_list))
+        if not (
+            len(get_all_nodes()) == 3
+            and set(worker_nodes_list) == set(master_node_list)
+        ):
+            worker_nodes_list = list(set(worker_nodes_list) - set(master_node_list))
     return worker_nodes_list
 
 
