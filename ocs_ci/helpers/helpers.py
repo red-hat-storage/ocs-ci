1efa6d3628b576c009751b189ba549ffc98f4847
Poonam Sharma <posharma@redhat.com>
2026-02-06 12:05:49 +0530

    fixes for cluster full and monitor test skip
    Signed-off-by: Poonam Sharma <posharma@redhat.com>

--- ocs_ci/helpers/helpers.py
+++ ocs_ci/helpers/helpers.py
@@ -2888,7 +2888,7 @@ def wait_for_ct_pod_recovery():
                 condition=constants.STATUS_RUNNING,
                 selector=constants.TOOL_APP_LABEL,
                 resource_count=1,
-                timeout=120,
+                timeout=420,
                 sleep=10,
             )
             return True
@@ -4255,26 +4255,6 @@ def induce_mon_quorum_loss():
 
     for mon in mon_pod_obj:
         wait_for_resource_state(resource=mon, state=constants.STATUS_CLBO)
-
-    # # Check the operator log mon quorum lost
-    # operator_logs = get_logs_rook_ceph_operator()
-    # pattern = (
-    #     "op-mon: failed to check mon health. "
-    #     "failed to get mon quorum status: mon "
-    #     "quorum status failed: exit status 1"
-    # )
-    # logger.info(f"Check the operator log for the pattern : {pattern}")
-    # if not re.search(pattern=pattern, string=operator_logs):
-    #     logger.error(
-    #         f"Pattern {pattern} couldn't find in operator logs. "
-    #         "Mon quorum may not have been lost after deleting "
-    #         "var/lib/ceph/mon. Please check"
-    #     )
-    #     raise UnexpectedBehaviour(
-    #         f"Pattern {pattern} not found in operator logs. "
-    #         "Maybe mon quorum not failed or  mon crash failed Please check"
-    #     )
-    # logger.info(f"Pattern found: {pattern}. Mon quorum lost")
 
     return mon_pod_obj_list, mon_pod_running[0], ceph_mon_daemon_id
 
