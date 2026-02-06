e2e022e3560b29fd477cc96618cda915986ed339
Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>
2026-02-06 12:57:18 +0530

    Update conftest.py
    Signed-off-by: Amrita Mahapatra <49347640+amr1ta@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1257,7 +1257,6 @@ def storageclass_factory_fixture(
 
     """
     instances = []
-    additional_cephfs_data_pools = []
 
     def factory(
         interface=constants.CEPHBLOCKPOOL,
@@ -1372,7 +1371,6 @@ def storageclass_factory_fixture(
                         replica=ocsci_config.ENV_DATA.get("replica") or replica,
                     )
                     interface_name = f"ocs-storagecluster-cephfilesystem-{new_data_pool_name}"
-                    additional_cephfs_data_pools.append(new_data_pool_name)
                 else:
                     if pool_name is None:
                         interface_name = helpers.get_cephfs_data_pool_name()
