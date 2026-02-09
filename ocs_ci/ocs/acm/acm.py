de724212e481820fd92068e0da975b75c0741958
prsurve <prsurve@redhat.com>
2026-02-09 18:44:00 +0530

    Setup N-1 Submariner for IBM cloud RDR deployment
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/ocs/acm/acm.py
+++ ocs_ci/ocs/acm/acm.py
@@ -499,6 +499,13 @@ class AcmAddClusters(AcmPageNavigator):
                     if config.ENV_DATA.get("submariner_channel")
                     else config.ENV_DATA.get("submariner_version").rpartition(".")[0]
                 )
+
+                if (
+                    submariner_channel == "0.22"
+                    and config.ENV_DATA.get("platform") == constants.IBMCLOUD_PLATFORM
+                ):
+                    log.info("Temp Fix Until we have 0.22.1")
+                    submariner_channel = "0.21"
                 channel_name = "stable-" + submariner_channel
                 subscription_config = {
                     "source": "submariner-catalogsource",
