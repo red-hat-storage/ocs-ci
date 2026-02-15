2f8ba351fd9df320d8691daf7ed7e5452502c175
Poonam Sharma <posharma@redhat.com>
2026-02-15 23:29:29 +0530

    test log based replication
    Signed-off-by: Poonam Sharma <posharma@redhat.com>

--- tests/cross_functional/system_test/test_mcg_replication_with_disruptions.py
+++ tests/cross_functional/system_test/test_mcg_replication_with_disruptions.py
@@ -364,9 +364,7 @@ class TestLogBasedReplicationWithDisruptions:
             ), f"Deletion sync failed to complete for the objects {objs_to_delete} deleted in the first bucket set"
 
         logger.info("Taking backup of noobaa db")
-        cnpg_cluster_yaml, original_db_replica_count, secrets_obj = (
-            noobaa_db_backup_locally()
-        )
+        ocs_storage_obj, backup_name, noobaa_obj = noobaa_db_backup_locally()
 
         disable_deletion_sync = source_bucket.replication_policy
         disable_deletion_sync["rules"][0]["sync_deletions"] = False
@@ -382,9 +380,7 @@ class TestLogBasedReplicationWithDisruptions:
 
         # Do noobaa db recovery and see if the deletion sync works now
         logger.info("Recovering noobaa db from backup")
-        noobaa_db_recovery_from_local(
-            cnpg_cluster_yaml, original_db_replica_count, secrets_obj
-        )
+        noobaa_db_recovery_from_local(ocs_storage_obj, backup_name, noobaa_obj)
         wait_for_noobaa_pods_running(timeout=420)
 
         assert compare_bucket_object_list(
