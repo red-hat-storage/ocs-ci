e17df67c3bb2f2e00ecb03a887a5d2ca1f8ba5c8
s-selwin <sels@redhat.com>
2026-02-17 17:57:15 +0530

    updated formatting

--- tests/functional/z_cluster/nodes/test_automated_recovery_from_failed_nodes_reactive.py
+++ tests/functional/z_cluster/nodes/test_automated_recovery_from_failed_nodes_reactive.py
@@ -1,6 +1,10 @@
 import logging
 import pytest
-from ocs_ci.framework.pytest_customization.marks import brown_squad, skipif_compact_mode, skipif_ibm_power
+from ocs_ci.framework.pytest_customization.marks import (
+    brown_squad,
+    skipif_compact_mode,
+    skipif_ibm_power,
+)
 from ocs_ci.framework.testlib import (
     tier4a,
     tier4b,
