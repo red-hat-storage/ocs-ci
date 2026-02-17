red-hat-storage/ocs-ci

commit 69c27c7cb9ce0c09535949f14dfa0d49fa8aa133

Change pod.py::cal_md5sum to use oc exec instead of oc rsh (#6954)
* Use oc exec instead of oc rsh

This allows to run md5sum regardless of which type of shell is
installed on the container, as long as it's available in the
desired path.

Signed-off-by: Sagi Hirshfeld <shirshfe@redhat.com>

ocs_ci/ocs/resources/pod.py
@@ -1013,10 +1013,12 @@ def cal_md5sum(pod_obj, file_name, block=False, raw_path=False):
         )
     else:
         file_path = file_name
-    md5sum_cmd_out = pod_obj.exec_cmd_on_pod(
-        command=f'bash -c "md5sum {file_path}"', out_yaml_format=False
+
+    md5sum_cmd_out = pod_obj.ocp.exec_oc_cmd(
+        command=f"exec {pod_obj.name} -- md5sum {file_path}"
     )
     md5sum = md5sum_cmd_out.split()[0]
+
     logger.info(f"md5sum of file {file_name}: {md5sum}")
     return md5sum
 

commit de94deaf3ad9ae1521bf808d24277516a77e79da

Fix  bug introduced in commit 69c27c7cb
Signed-off-by: Petr Balogh <pbalogh@redhat.com>

ocs_ci/ocs/resources/pod.py
@@ -1402,7 +1402,8 @@ def cal_md5sum(pod_obj, file_name, block=False, raw_path=False):
         file_path = file_name
 
     md5sum_cmd_out = pod_obj.ocp.exec_oc_cmd(
-        command=f"exec {pod_obj.name} -- md5sum {file_path}"
+        command=f"exec {pod_obj.name} -- md5sum {file_path}",
+        out_yaml_format=False,
     )
     md5sum = md5sum_cmd_out.split()[0]
 