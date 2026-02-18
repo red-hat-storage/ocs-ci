c3d7e3cf0eeb8db71706b52289973632bca8f8ac
anajain16 <anajain@redhat.com>
2026-02-18 19:13:45 +0530

    added jira-id
    Signed-off-by: anajain16 <anajain@redhat.com>

--- tests/functional/ui/test_alert_text.py
+++ tests/functional/ui/test_alert_text.py
@@ -10,6 +10,7 @@ from ocs_ci.framework.pytest_customization.marks import (
     black_squad,
     tier2,
     ui,
+    jira,
     skipif_ibm_cloud_managed,
 )
 from ocs_ci.ocs import constants
@@ -83,6 +84,7 @@ def alerts_expected():
 
 @tier2
 @black_squad
+@jira("DFBUGS-5660")
 @skipif_ibm_cloud_managed
 @skipif_mcg_only
 @skipif_hci_client
