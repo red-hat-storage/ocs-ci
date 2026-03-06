29c5c8cdc5d18467deebf1cbcd063eba755889b7
Parag Kamble <pakamble@redhat.com>
2026-03-06 18:27:13 +0530

    FIxed snapshot attribute error
    Signed-off-by: Parag Kamble <pakamble@redhat.com>

--- ocs_ci/krkn_chaos/background_cluster_operations.py
+++ ocs_ci/krkn_chaos/background_cluster_operations.py
@@ -30,6 +30,7 @@ from collections import defaultdict
 from ocs_ci.ocs import constants, ocp
 from ocs_ci.ocs.resources import pod as pod_helpers
 from ocs_ci.ocs.resources import pvc as pvc_helpers
+from ocs_ci.ocs.resources.pvc import PVC
 from ocs_ci.ocs.resources import job as job_helpers
 from ocs_ci.ocs.resources.ocs import OCS
 from ocs_ci.ocs.resources.pvc import PVC
@@ -445,14 +446,13 @@ class BackgroundClusterOperations:
             log.info(f"Creating clone of PVC {workload_pvc.name}")
 
             # Determine clone YAML based on provisioner
-            if "rbd" in workload_pvc.provisioner:
+            provisioner = getattr(workload_pvc, "provisioner", "") or ""
+            if "rbd" in provisioner:
                 clone_yaml = constants.CSI_RBD_PVC_CLONE_YAML
-            elif "cephfs" in workload_pvc.provisioner:
+            elif "cephfs" in provisioner:
                 clone_yaml = constants.CSI_CEPHFS_PVC_CLONE_YAML
             else:
-                log.warning(
-                    f"Unsupported provisioner for clone: {workload_pvc.provisioner}"
-                )
+                log.warning(f"Unsupported provisioner for clone: {provisioner!r}")
                 return
 
             # Get actual capacity from source PVC (not converted size)
@@ -1185,7 +1185,8 @@ class BackgroundClusterOperations:
         from ocs_ci.utility import templating
 
         # Determine interface type based on PVC provisioner
-        if "rbd" in pvc_obj.provisioner.lower():
+        provisioner = getattr(pvc_obj, "provisioner", "") or ""
+        if "rbd" in provisioner.lower():
             pod_dict_path = constants.CSI_RBD_POD_YAML
         else:
             pod_dict_path = constants.CSI_CEPHFS_POD_YAML
