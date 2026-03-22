f66d453ec4ae5af13757b5160fc0e9c6a2453191
Daniel Osypenko <danielosypenko@redhat.com>
2026-03-22 18:47:45 +0200

    object-cephobjectstore-day1-deployment
    Signed-off-by: Daniel Osypenko <danielosypenko@redhat.com>

--- ocs_ci/ocs/resources/rgw.py
+++ ocs_ci/ocs/resources/rgw.py
@@ -1,9 +1,17 @@
 import base64
+import logging
 
 from ocs_ci.framework import config
 from ocs_ci.ocs.ocp import OCP
 from ocs_ci.ocs import constants
-from ocs_ci.helpers.helpers import storagecluster_independent_check
+from ocs_ci.helpers.helpers import (
+    create_resource,
+    storagecluster_independent_check,
+    wait_for_resource_state,
+)
+from ocs_ci.utility import templating
+
+logger = logging.getLogger(__name__)
 
 
 class RGW(object):
@@ -80,3 +88,50 @@ class RGW(object):
             creds_secret_obj.get("data").get("SecretKey")
         ).decode("utf-8")
         return endpoint, access_key, secret_key
+
+
+def create_ec_cephobjectstore():
+    """
+    Create an erasure-coded CephObjectStore with a replicated metadata pool
+    and an erasure-coded data pool, then create the corresponding bucket
+    StorageClass.
+
+    Erasure coding parameters are read from config.DEPLOYMENT:
+        ec_data_chunks (int): number of data chunks, k value (default: 2)
+        ec_coding_chunks (int): number of coding chunks, m value (default: 1)
+        ec_failure_domain (str): failure domain (default: "host")
+    """
+    k = config.DEPLOYMENT.get("ec_data_chunks", 2)
+    m = config.DEPLOYMENT.get("ec_coding_chunks", 1)
+    fd = config.DEPLOYMENT.get("ec_failure_domain", "host")
+    namespace = config.ENV_DATA["cluster_namespace"]
+    os_name = constants.CEPHOBJECTSTORE_NAME_EC
+    sc_name = constants.DEFAULT_STORAGECLASS_RGW_EC
+
+    logger.info(
+        f"Creating EC CephObjectStore '{os_name}' with k={k}, m={m}, "
+        f"failureDomain={fd}"
+    )
+
+    os_data = templating.load_yaml(constants.EC_CEPHOBJECTSTORE_YAML)
+    os_data["metadata"]["name"] = os_name
+    os_data["metadata"]["namespace"] = namespace
+    os_data["spec"]["dataPool"]["failureDomain"] = fd
+    os_data["spec"]["dataPool"]["erasureCoded"]["dataChunks"] = k
+    os_data["spec"]["dataPool"]["erasureCoded"]["codingChunks"] = m
+
+    os_obj = create_resource(**os_data)
+    logger.info(f"Waiting for EC CephObjectStore '{os_name}' to reach Ready phase")
+    wait_for_resource_state(os_obj, constants.STATUS_READY, timeout=600)
+
+    logger.info(f"Creating bucket StorageClass '{sc_name}' for EC CephObjectStore")
+
+    sc_data = templating.load_yaml(constants.EC_STORAGECLASS_RGW_YAML)
+    sc_data["metadata"]["name"] = sc_name
+    sc_data["parameters"]["objectStoreName"] = os_name
+    sc_data["parameters"]["objectStoreNamespace"] = namespace
+
+    create_resource(**sc_data)
+    logger.info(
+        f"EC CephObjectStore '{os_name}' and StorageClass '{sc_name}' created successfully"
+    )
--- ocs_ci/utility/storage_cluster_setup.py
+++ ocs_ci/utility/storage_cluster_setup.py
@@ -453,7 +453,10 @@ class StorageClusterSetup(object):
 
             ec_managed_resources.setdefault("cephObjectStores", {}).update(
                 {
-                    "dataPoolSpec": {"erasureCoded": ec_spec},
+                    "dataPoolSpec": {
+                        "failureDomain": fd,
+                        "erasureCoded": ec_spec,
+                    },
                 }
             )
 
