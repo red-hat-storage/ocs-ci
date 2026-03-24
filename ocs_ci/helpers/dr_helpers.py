19ad27a632023b576fa8735812224a8371ffefac
prsurve <prsurve@redhat.com>
2026-03-24 11:19:56 +0530

    add resource name
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/helpers/dr_helpers.py
+++ ocs_ci/helpers/dr_helpers.py
@@ -1149,6 +1149,7 @@ def wait_for_replication_resources_deletion(
                 kind=constants.VOLUME_GROUP_REPLICATION,
                 namespace=namespace,
                 timeout=timeout,
+                resource_name=vrg_name,
                 should_exist=False,
             )
 
