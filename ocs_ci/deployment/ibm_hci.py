0b7dabce067fca9873ef570aa7d964155a5bff3b
prsurve <prsurve@redhat.com>
2026-04-15 10:47:38 +0530

    refactor class name
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/deployment/ibm_hci.py
+++ ocs_ci/deployment/ibm_hci.py
@@ -1,13 +1,13 @@
 from ocs_ci.deployment.on_prem import OnPremDeploymentBase, IPIOCPDeployment
 
 
-class IMB_HCI_IPI(OnPremDeploymentBase):
+class IBM_HCI_IPI(OnPremDeploymentBase):
     """
-    A class to handle RHV IPI specific deployment
+    A class to handle IBM HCI IPI specific deployment
     """
 
     OCPDeployment = IPIOCPDeployment
 
     def __init__(self):
         self.name = self.__class__.__name__
-        super(IMB_HCI_IPI, self).__init__()
+        super(IBM_HCI_IPI, self).__init__()
