99ec1b57138cd91c0f69ba2e79b948045ab547b0
Jilju Joy <jijoy@redhat.com>
2026-05-07 19:24:54 +0530

    Remove duplicate constant HYPERSHIFT_ADDON_DISCOVERYPREFIX
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -3849,8 +3849,6 @@ RBD_CSI_ADDONS_SOCKET_NAME = "csi-addons.sock"
 HYPERSHIFT_ADDON_DISCOVERYPREFIX = "dr"
 CEPHFS_CSI_ADDONS_SOCKET_NAME = "csi-addons.sock"
 
-HYPERSHIFT_ADDON_DISCOVERYPREFIX = "dr"
-
 # Fill pool job and PVC Yaml files
 FILL_POOL_JOB_YAML = os.path.join(TEMPLATE_FIO_DIR, "fill_pool_job.yaml")
 FILL_POOL_PVC_YAML = os.path.join(TEMPLATE_FIO_DIR, "fill_pool_pvc.yaml")
