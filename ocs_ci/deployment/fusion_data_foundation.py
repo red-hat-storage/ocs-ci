ba6b2e60d2dbdd98c02625aaf9da5beaf7e0e4c5
Coady LaCroix <clacroix@redhat.com>
2026-05-11 12:51:59 -0700

    Add live deployment param to deploy-fdf
    Signed-off-by: Coady LaCroix <clacroix@redhat.com>

--- ocs_ci/deployment/fusion_data_foundation.py
+++ ocs_ci/deployment/fusion_data_foundation.py
@@ -35,6 +35,7 @@ logger = logging.getLogger(__name__)
 class FusionDataFoundationDeployment:
     def __init__(self):
         self.pre_release = config.DEPLOYMENT.get("fdf_pre_release", False)
+        self.live_deployment = config.DEPLOYMENT.get("live_deployment", False)
         self.kubeconfig = config.RUN["kubeconfig"]
         self.lso_enabled = config.DEPLOYMENT.get("local_storage", False)
         self.storage_class = (
@@ -47,7 +48,7 @@ class FusionDataFoundationDeployment:
         Installs IBM Fusion Data Foundation.
         """
         logger.info("Installing IBM Fusion Data Foundation")
-        if self.pre_release:
+        if self.pre_release and not self.live_deployment:
             self.create_image_tag_mirror_set()
             self.create_image_digest_mirror_set()
             self.setup_fdf_pre_release_deployment()
