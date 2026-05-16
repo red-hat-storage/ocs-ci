37a87a7e37ddc861c75809c789f4ffcd70f47422
Yulia Persky <yuliapersky@li-3c5a0b4c-351a-11b2-a85c-8ed46ee48f43.ibm.com>
2026-05-17 01:52:41 +0300

    adding a fix to test_default_backingstore_override_post_upgrade
    Signed-off-by: Yulia Persky <yuliapersky@li-3c5a0b4c-351a-11b2-a85c-8ed46ee48f43.ibm.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -9041,6 +9041,17 @@ def override_default_backingstore_session(
     )
 
 
+@pytest.fixture(scope="session")
+def override_default_backingstore_session_no_teardown(
+    mcg_obj_session,
+    backingstore_factory_session,
+    allow_default_backingstore_override,
+):
+    return override_default_backingstore_fixture(
+        None, mcg_obj_session, backingstore_factory_session
+    )
+
+
 @pytest.fixture(scope="function")
 def override_default_backingstore(
     request, mcg_obj_session, backingstore_factory, allow_default_backingstore_override
@@ -9120,7 +9131,8 @@ def override_default_backingstore_fixture(
             constants.DEFAULT_NOOBAA_BACKINGSTORE
         )
 
-    request.addfinalizer(finalizer)
+    if request is not None:
+        request.addfinalizer(finalizer)
     return _override_nb_default_backingstore_implementation
 
 
