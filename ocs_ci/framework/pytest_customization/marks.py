3d7e81a9c4ae7f01e4c2eb3bdf2c2fd09e6f842e
Shay Rozen <11279672+shyRozen@users.noreply.github.com>
2026-06-11 15:15:27 +0300

    Add zstream_4_20_14_naturalintelligence/fast_xml_parser to marks.py
    Signed-off-by: Shay Rozen <shay.rozen@gmail.com>

--- ocs_ci/framework/pytest_customization/marks.py
+++ ocs_ci/framework/pytest_customization/marks.py
@@ -866,3 +866,5 @@ skipif_lean_deployment = pytest.mark.skipif(
 )
 # z-stream marker
 zstream_4_20_14 = pytest.mark.zstream_4_20_14
+# z-stream marker
+zstream_4_20_14_naturalintelligence/fast_xml_parser = pytest.mark.zstream_4_20_14_naturalintelligence/fast_xml_parser
