44743a612d06a711c18acb0a6c4ea27c2f2eea3e
Shay Rozen <11279672+shyRozen@users.noreply.github.com>
2026-06-11 16:18:01 +0300

    Add zstream_4_20_14_rook to marks.py
    Signed-off-by: Shay Rozen <shay.rozen@gmail.com>

--- ocs_ci/framework/pytest_customization/marks.py
+++ ocs_ci/framework/pytest_customization/marks.py
@@ -882,3 +882,5 @@ zstream_4_20_14_upgrade = pytest.mark.zstream_4_20_14_upgrade
 zstream_4_20_14_red_hat_storage/odf_cli = pytest.mark.zstream_4_20_14_red_hat_storage/odf_cli
 # z-stream marker
 zstream_4_20_14_ocs_operator = pytest.mark.zstream_4_20_14_ocs_operator
+# z-stream marker
+zstream_4_20_14_rook = pytest.mark.zstream_4_20_14_rook
