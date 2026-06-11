18c66743e80ad217693b43806a3b645062d766dd
Shay Rozen <11279672+shyRozen@users.noreply.github.com>
2026-06-12 00:24:29 +0300

    Add zstream_4_20_14_odf_console to marks.py
    Signed-off-by: Shay Rozen <shay.rozen@gmail.com>

--- ocs_ci/framework/pytest_customization/marks.py
+++ ocs_ci/framework/pytest_customization/marks.py
@@ -884,3 +884,5 @@ zstream_4_20_14_odf_dr_multicluster_orchestrator = pytest.mark.zstream_4_20_14_o
 zstream_4_20_14_ocs_operator = pytest.mark.zstream_4_20_14_ocs_operator
 # z-stream marker
 zstream_4_20_14_rook = pytest.mark.zstream_4_20_14_rook
+# z-stream marker
+zstream_4_20_14_odf_console = pytest.mark.zstream_4_20_14_odf_console
