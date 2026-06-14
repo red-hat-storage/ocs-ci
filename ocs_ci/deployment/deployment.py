826bb18af1e6cb01bd9e86350da713eadccbb241
prsurve <prsurve@redhat.com>
2026-06-14 23:49:09 +0530

    Add more logging
    Signed-off-by: prsurve <prsurve@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -4920,7 +4920,9 @@ class RDRMultiClusterDROperatorsDeploy(MultiClusterDROperatorsDeploy):
         )
         exec_cmd(f"oc create -f {thanos_data_yaml.name}")
 
+        logger.info("Thanos secret created, checking observability status")
         self.check_observability_status()
+        logger.info("Observability status check passed, thanos_secret() completed")
 
     def enable_acm_observability(self):
         """
