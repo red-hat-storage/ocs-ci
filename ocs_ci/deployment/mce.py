242605371e31070c7b734c8045d3fd4d550b5bd3
Daniel Horak <dahorak@redhat.com>
2026-07-08 14:49:28 +0200

    add @retry to create_multiclusterengine_resource
    To avoid issue like this:
    Error is Error from server (InternalError): error when creating "/tmp/MultiClusterEngine54prty1k": Internal error occurred: failed calling webhook "multiclusterengines.multicluster.openshift.io": failed to call webhook: Post "https://multicluster-engine-operator-webhook-service.multicluster-engine.svc:443/validate-multicluster-openshift-io-v1-multiclusterengine?timeout=10s": no endpoints available for service "multicluster-engine-operator-webhook-service"
    
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

--- ocs_ci/deployment/mce.py
+++ ocs_ci/deployment/mce.py
@@ -22,6 +22,7 @@ from ocs_ci.utility.utils import (
     TimeoutSampler,
     get_acm_mce_build_tag,
 )
+from ocs_ci.utility.retry import retry
 from ocs_ci.ocs.resources.catalog_source import CatalogSource
 from ocs_ci.ocs.resources.deployment import Deployment
 from ocs_ci.utility.utils import get_running_ocp_version
@@ -131,6 +132,13 @@ class MCEInstaller(object):
             operatorgroup_yaml.create()
             logger.info("mce OperatorGroup created successfully")
 
+    @retry(
+        CommandFailed,
+        tries=5,
+        delay=10,
+        backoff=1,
+        text_in_exception="no endpoints available for service",
+    )
     def create_multiclusterengine_resource(self):
         """
         Creates multiclusterengine resource
