02a896b5c1fcd97723c7dd691b059450c356ba0c
Avdhoot <asagare@redhat.com>
2026-07-16 12:09:17 +0530

    Used exiting operator module.
    Signed-off-by: Avdhoot <asagare@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -803,6 +803,19 @@ class Deployment(object):
         if config.DEPLOYMENT.get("metallb_operator"):
             MetalLBInstaller().deploy_lb()
 
+    def do_deploy_fusion_access(self):
+        """
+        Deploy IBM Fusion Access Operator for SAN after OCP deployment.
+
+        Triggered when ``config.DEPLOYMENT["fusion_access_deployment"]`` is True.
+        The deployment is intentionally placed after OCS/ODF so that any
+        prerequisite storage infrastructure is already present.
+        """
+        if config.DEPLOYMENT.get("fusion_access_deployment"):
+            from ocs_ci.deployment.fusion_access import FusionAccessOperator
+
+            FusionAccessOperator().deploy()
+
     def do_deploy_hosted_spoke_clusters(self):
         """
         Deploy Hosted cluster(s)
@@ -935,6 +948,7 @@ class Deployment(object):
         self.do_deploy_cnv()
         self.do_deploy_hyperconverged()
         self.do_deploy_metallb()
+        self.do_deploy_fusion_access()
         self.do_deploy_hosted_spoke_clusters()
         self.do_deploy_external_spoke_clusters()
 
