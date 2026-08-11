b67c43d4d66b96ccc8f2a27f0367124b4034af7b
Rashmi Pathan <rpathan@redhat.com>
2026-08-11 23:35:27 +0530

    Fix navigate_installed_operators_page for OCP 4.23 UI changes
    Signed-off-by: Rashmi Pathan <rpathan@redhat.com>

--- ocs_ci/ocs/ui/views.py
+++ ocs_ci/ocs/ui/views.py
@@ -225,7 +225,7 @@ deployment_4_9 = {
     ),
     "storage_system_tab": (
         'a[data-test-id="horizontal-link-Storage System"],'
-        'a[data-test-id="horizontal-link-Storage cluster"]',
+        'a[data-test="nav"][href*="storage-cluster"]',
         By.CSS_SELECTOR,
     ),
     "internal_mode_odf": ('input[id="bs-existing"]', By.CSS_SELECTOR),
@@ -1987,7 +1987,7 @@ add_capacity = {
     ),
     "storage_system_tab": (
         'a[data-test-id="horizontal-link-Storage System"],'
-        'a[data-test-id="horizontal-link-Storage cluster"]',
+        'a[data-test="nav"][href*="storage-cluster"]',
         By.CSS_SELECTOR,
     ),
     "kebab_storage_cluster": ("//button[@data-test-id='kebab-button']", By.XPATH),
