6f316e0b7f5a22d193f9bd1d105790aac4e67508
Pratik Surve <prsurve@redhat.com>
2026-08-11 12:13:58 +0530

    Add 2az zone failure test (#14710)
     Add 2 az failure test

--- ocs_ci/helpers/dr_helpers.py
+++ ocs_ci/helpers/dr_helpers.py
@@ -1255,6 +1255,7 @@ def wait_for_replication_resources_deletion(
                 kind=constants.VOLUME_GROUP_REPLICATION,
                 namespace=namespace,
                 timeout=timeout,
+                resource_name=vrg_name,
                 should_exist=False,
             )
 
@@ -3243,6 +3244,9 @@ def wait_for_vrg_state(vrg_state, vrg_namespace, resource_name, timeout=900):
         vrg_namespace (str): VRG resource namespace
         resource_name (str): VRG resource name
         timeout (int): Timeout for wait
+
+    Raises:
+        TimeoutExpiredError: With VRG specific context to make failures actionable
 
     """
     wait_for_resource_state(
