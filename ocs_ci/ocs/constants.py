1fe3374720d118eadc20418e4e58c04c5fe4de89
Poonam Sharma <posharma@redhat.com>
2026-08-19 18:51:11 +0530

    fixing PR comments
    Signed-off-by: Poonam Sharma <posharma@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -2884,6 +2884,8 @@ RHCOS = "RHCOS"
 
 # Scale constants
 IBM_STORAGE_SCALE_NAMESPACE = "ibm-spectrum-scale"
+IBM_ENTITLEMENT_SECRET_NAME = "ibm-entitlement-key"
+IBM_QUAYIO_SECRET_NAME = "quayio-secret"
 REMOTE_CLUSTER = "RemoteCluster"
 SCALE_NODE_SELECTOR = {"scale-label": "app-scale"}
 SCALE_LABEL = "scale-label=app-scale"
