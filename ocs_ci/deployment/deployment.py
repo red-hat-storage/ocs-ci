c8be2590ee72e704fecb5a14bee714ac698dbe53
Keerthana Manoharan <kmanohar@redhat.com>
2026-08-25 18:04:46 +0530

    gitops fix
    Signed-off-by: Keerthana Manoharan <kmanohar@redhat.com>

--- ocs_ci/deployment/deployment.py
+++ ocs_ci/deployment/deployment.py
@@ -1042,17 +1042,14 @@ class Deployment(object):
         self.do_deploy_lvmo()
         self.do_deploy_submariner()
         ocp_version = version.get_semantic_ocp_version_from_config()
-        if ocp_version < version.VERSION_5_0:
-            self.do_gitops_deploy()
-            self.do_deploy_oadp()
         if config.DEPLOYMENT.get("unique_rack_node_labels"):
             create_unique_rack_labels()
         if ocp_version >= version.VERSION_5_0:
             # Temporary workaround: create the GitOps CatalogSource on all
             # clusters before deploying the GitOps operator and OADP.
             run_cmd_multicluster(f"oc create -f {constants.TEMP_GITOPS_WA_YAML}")
-            self.do_gitops_deploy()
-            self.do_deploy_oadp()
+        self.do_gitops_deploy()
+        self.do_deploy_oadp()
         self.do_deploy_ocs()
         self.do_deploy_rdr()
         self.do_deploy_mce()
