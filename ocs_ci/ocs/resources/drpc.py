73fb03cdf019ef196fb23d3f647c433b1d8a510b
Jilju Joy <jijoy@redhat.com>
2026-09-02 15:13:11 +0530

    Revert change from commit until a permanent fix is availble for MDR tests on HCI. This will unblock other DR tests
    Signed-off-by: Jilju Joy <jijoy@redhat.com>

--- ocs_ci/ocs/resources/drpc.py
+++ ocs_ci/ocs/resources/drpc.py
@@ -28,20 +28,19 @@ class DRPC(OCP):
             resource_name (str): Name of DRPC
 
         """
-        with config.RunWithConfigContext(
-            switch_ctx if switch_ctx is not None else config.get_active_acm_index()
-        ):
-            super(DRPC, self).__init__(
-                namespace=namespace,
-                resource_name=(
-                    resource_name
-                    if resource_name
-                    else get_drpc_name(namespace, switch_ctx=switch_ctx)
-                ),
-                kind=constants.DRPC,
-                *args,
-                **kwargs,
-            )
+        config.switch_ctx(switch_ctx) if switch_ctx else config.switch_acm_ctx()
+
+        super(DRPC, self).__init__(
+            namespace=namespace,
+            resource_name=(
+                resource_name
+                if resource_name
+                else get_drpc_name(namespace, switch_ctx=switch_ctx)
+            ),
+            kind=constants.DRPC,
+            *args,
+            **kwargs,
+        )
 
     @property
     def drpolicy(self):
