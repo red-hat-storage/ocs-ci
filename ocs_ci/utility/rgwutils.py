a5494b780ccd8b885237481315574fa1f403d4e0
vavuthu <vavuthu@redhat.com>
2026-09-04 12:01:18 +0530

    refactor get_rgw_count
    Signed-off-by: vavuthu <vavuthu@redhat.com>

--- ocs_ci/utility/rgwutils.py
+++ ocs_ci/utility/rgwutils.py
@@ -1,12 +1,11 @@
 import logging
 
 from ocs_ci.framework import config
-from ocs_ci.utility import version
 
 log = logging.getLogger(__name__)
 
 
-def get_rgw_count(ocs_version, is_upgrade, version_before_upgrade):
+def get_rgw_count():
     """
     Get RGW Count
 
@@ -17,38 +16,11 @@ def get_rgw_count(ocs_version, is_upgrade, version_before_upgrade):
 
     Otherwise, RGW Count is 1
 
-    Args:
-        ocs_version (str, float): OCS Version
-        is_upgrade (bool): If cluster was upgraded to current version
-        version_before_upgrade (str, float): OCS Version prior to upgrade
-
     Returns:
         int: RGW Count
 
     """
     if config.DEPLOYMENT.get("arbiter_deployment"):
-        log.debug("RGW Count: 2")
-        return 2
-
-    semantic_ocs_version = version.get_semantic_version(
-        ocs_version, only_major_minor=True
-    )
-    # Assume upgrade from prior version if one is not provided
-    if is_upgrade:
-        version_before_upgrade = version.get_semantic_version(
-            f"{semantic_ocs_version.major}.{semantic_ocs_version.minor - 1}",
-            only_major_minor=True,
-        )
-        log.info(
-            "version_before_upgrade not provided, assuming prior release is "
-            f"{version_before_upgrade}",
-        )
-
-    if (
-        semantic_ocs_version == version.VERSION_4_5
-        and not (is_upgrade and version_before_upgrade < version.VERSION_4_5)
-        or semantic_ocs_version == version.VERSION_4_6
-    ):
         log.debug("RGW Count: 2")
         return 2
     else:
